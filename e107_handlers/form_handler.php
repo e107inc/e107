df24b78c0dd16a4a0cc01de95288cd81ed36a326
loloirie <loloirie@localhost>
2004-03-15 16:15:27 +0000

    Add ID attributes with same values as NAME attributes (but keep NAME attributes for old js compatibility) and correct some function : name="name=$form_name"
    svn path=/trunk/e107/; revision=41

--- e107_handlers/form_handler.php
+++ e107_handlers/form_handler.php
@@ -18,64 +18,70 @@ class form{
 	function form_open($form_method, $form_action, $form_name="", $form_target = "", $form_enctype=""){
 		$method = ($form_method ? "method='".$form_method."'" : ""); 
 		$target = ($form_target ? " target='".$form_target."'" : "");
-		$name = ($form_name ? " name='".$form_name."'" : "");
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		return "\n<form action='".$form_action."' ".$method.$target.$name.$form_enctype.">";
 	}
 
 	function form_text($form_name, $form_size, $form_value, $form_maxlength, $form_class="tbox", $form_readonly="", $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$value = ($form_value ? " value='".$form_value."'" : "");
 		$size = ($form_size ? " size='".$form_size."'" : "");
 		$maxlength = ($form_maxlength ? " maxlength='".$form_maxlength."'" : "");
 		$readonly = ($form_readonly ? " readonly='readonly'" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
-		return "\n<input class='".$form_class."' type='text' name='".$form_name."' ".$value.$size.$maxlength.$readonly.$tooltip." />";
+		return "\n<input class='".$form_class."' type='text' ".$name.$value.$size.$maxlength.$readonly.$tooltip." />";
 	}
 
 	function form_password($form_name, $form_size, $form_value, $form_maxlength, $form_class="tbox", $form_readonly="", $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$value = ($form_value ? " value='".$form_value."'" : "");
 		$size = ($form_size ? " size='".$form_size."'" : "");
 		$maxlength = ($form_maxlength ? " maxlength='".$form_maxlength."'" : "");
 		$readonly = ($form_readonly ? " readonly='readonly'" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
-		return "\n<input class='".$form_class."' type='password' name='".$form_name."' ".$value.$size.$maxlength.$readonly.$tooltip." />";
+		return "\n<input class='".$form_class."' type='password' ".$name.$value.$size.$maxlength.$readonly.$tooltip." />";
 	}
 
 	function form_button($form_type, $form_name, $form_value, $form_js="", $form_image="", $form_tooltip=""){
-		$name =  ($form_name ? " name='".$form_name."' " : "");
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$image = ($form_image ? " src='".$form_image."' " : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."' " : "");
-		return "\n<input class='button' type='".$form_type."' ".$form_js." name='".$form_name."' value='".$form_value."'".$image.$tooltip."/>";
+		return "\n<input class='button' type='".$form_type."' ".$form_js." value='".$form_value."'".$name.$image.$tooltip."/>";
 	}
 
 	function form_textarea($form_name, $form_columns, $form_rows, $form_value, $form_js="", $form_style="", $form_wrap="", $form_readonly="", $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$readonly = ($form_readonly ? " readonly='readonly'" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
 		$wrap = ($form_wrap ? " wrap='".$form_wrap."'" : "");
 		$style = ($form_style ? " style='".$form_style."'" : "");
-		return "\n<textarea class='tbox' name='".$form_name."' cols='".$form_columns."' rows='".$form_rows."' ".$form_js.$style.$wrap.$readonly.$tooltip.">".$form_value."</textarea>";
+		return "\n<textarea class='tbox' cols='".$form_columns."' rows='".$form_rows."' ".$name.$form_js.$style.$wrap.$readonly.$tooltip.">".$form_value."</textarea>";
 	}
 
 	function form_checkbox($form_name, $form_value, $form_checked=0, $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$checked = ($form_checked ? " checked" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
-		return "\n<input type='checkbox' name='".$form_name."' value='".$form_value."'".$checked.$tooltip." />";
+		return "\n<input type='checkbox' value='".$form_value."'".$name.$checked.$tooltip." />";
 
 	}
 
 	function form_radio($form_name, $form_value, $form_checked=0, $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$checked = ($form_checked ? " checked" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
-		return "\n<input type='radio' name='".$form_name."' value='".$form_value."'".$checked.$tooltip." />";
+		return "\n<input type='radio' value='".$form_value."'".$name.$checked.$tooltip." />";
 
 	}
 
 	function form_file($form_name, $form_size, $form_tooltip=""){
+		$name = ($form_name ? " id='".$form_name."' name='".$form_name."'" : "");
 		$tooltip = ($form_tooltip ? " title='".$form_tooltip."'" : "");
-		return "<input type='file' class='tbox' name='".$form_name."' size='".$form_size."'".$tooltip." />";
+		return "<input type='file' class='tbox' size='".$form_size."'".$name.$tooltip." />";
 	}
 
 	function form_select_open($form_name){
-		return "\n<select name='".$form_name."' class='tbox'>";
+		return "\n<select id='".$form_name."' name='".$form_name."' class='tbox'>";
 	}
 
 	function form_select_close(){
@@ -89,7 +95,7 @@ class form{
 	}
 
 	function form_hidden($form_name, $form_value){
-		return "\n<input type='hidden' name='".$form_name."' value='".$form_value."' />";
+		return "\n<input type='hidden' id='".$form_name."' name='".$form_name."' value='".$form_value."' />";
 	}
 
 	function form_close(){
