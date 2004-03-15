82c12aff8dff31b7f97af45f56af0631516f20d6
e107steved <steved@e107.org>
2004-03-15 13:02:08 +0000

    New language file for top.php
    svn path=/trunk/e107/; revision=34

---
+++ e107_languages/English/lan_top.php
@@ -0,0 +1,13 @@
+<?php
+
+define("PAGE_NAME", "Top Users"); 
+
+define("TOP_LAN_0", "Top Forum Posters");
+define("TOP_LAN_1", "User Name");
+define("TOP_LAN_2", "Posts");
+define("TOP_LAN_3", "Top Comment Posters");
+define("TOP_LAN_4", "Comments");
+define("TOP_LAN_5", "Top Chatbox Posters");
+define("TOP_LAN_6", "Site Rating");
+
+?>
