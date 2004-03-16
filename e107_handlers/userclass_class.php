8ce42c330515f8e6e61be239b00bec6930a6084a
mcfly_e107 <mcfly_e107@localhost>
2004-03-16 02:37:36 +0000

    Added multilanguage to userclass handler
    svn path=/trunk/e107/; revision=46

--- e107_handlers/userclass_class.php
+++ e107_handlers/userclass_class.php
@@ -1,16 +1,19 @@
 <?php
 
+@include_once(e_LANGUAGEDIR.e_LANGUAGE."/lan_userclass.php");
+@include_once(e_LANGUAGEDIR."English/lan_userclass.php");
+
 function r_userclass($fieldname, $curval=0, $mode="off"){
 	$sql = new db;
 	$text="<select class='tbox' name='{$fieldname}'>\n";
 	($curval==e_UC_PUBLIC) ? $s=" selected" : $s="";
-	$text.="<option  value='".e_UC_PUBLIC."' ".$s.">Everyone (public)\n";
+	$text.="<option  value='".e_UC_PUBLIC."' ".$s.">".UC_LAN_0."\n";
 	($curval==e_UC_GUEST) ? $s=" selected" : $s="";
-	$text.="<option  value='".e_UC_GUEST."' ".$s.">Guests Only \n";
+	$text.="<option  value='".e_UC_GUEST."' ".$s.">".UC_LAN_1."\n";
 	($curval==e_UC_NOBODY) ? $s=" selected" : $s="";
-	$text.="<option value='".e_UC_NOBODY."' ".$s.">No one (Inactive)\n";
+	$text.="<option value='".e_UC_NOBODY."' ".$s.">".UC_LAN_2."\n";
 	($curval==e_UC_MEMBER) ? $s=" selected" : $s="";
-	$text.="<option value='".e_UC_MEMBER."' ".$s.">Members only\n";
+	$text.="<option value='".e_UC_MEMBER."' ".$s.">".UC_LAN_3."\n";
 	if($sql -> db_Select("userclass_classes")){
 		while($row = $sql -> db_Fetch()){
 			extract($row);
@@ -20,9 +23,9 @@ function r_userclass($fieldname, $curval=0, $mode="off"){
 	}
 	if($mode != "off"){
 		($curval==e_UC_READONLY) ? $s=" selected" : $s="";
-		$text.="<option  value='".e_UC_READONLY."' ".$s.">Read only\n";
+		$text.="<option  value='".e_UC_READONLY."' ".$s.">".UC_LAN_4."\n";
 		($curval==e_UC_ADMIN) ? $s=" selected" : $s="";
-		$text.="<option  value='".e_UC_ADMIN."' ".$s.">Admin only\n";
+		$text.="<option  value='".e_UC_ADMIN."' ".$s.">".UC_LAN_5."\n";
 	}
 	$text.="</select>\n";
 	return $text;
@@ -31,13 +34,13 @@ function r_userclass($fieldname, $curval=0, $mode="off"){
 function r_userclass_radio($fieldname,$curval=0){
 	$sql = new db;
 	($curval==e_UC_PUBLIC) ? $c=" checked" : $c="";
-	$text="<input type='radio' name='{$fieldname}' value='".e_UC_PUBLIC."' ".$c.">Everyone (public)<br />";
+	$text="<input type='radio' name='{$fieldname}' value='".e_UC_PUBLIC."' ".$c.">".UC_LAN_0."<br />";
 	($curval==e_UC_NOBODY) ? $c=" checked" : $c="";
-	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_NOBODY."' ".$c.">No one (Inactive)<br />";	
+	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_NOBODY."' ".$c.">".UC_LAN_1."<br />";	
 	($curval==e_UC_GUEST) ? $c=" checked" : $c="";
-	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_GUEST."' ".$c.">Guests Only<br />";
+	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_GUEST."' ".$c.">".UC_LAN_2."<br />";
 	($curval==e_UC_MEMBER) ? $c=" checked" : $c="";
-	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_MEMBER."' ".$c.">Members only<br />";
+	$text.="<input type='radio' name='{$fieldname}' value='".e_UC_MEMBER."' ".$c.">".UC_LAN_3."<br />";
 	if($sql -> db_Select("userclass_classes")){
 		while($row = $sql -> db_Fetch()){
 			extract($row);
