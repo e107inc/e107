497642a15a982529d3f29dc18ba3930d9612acbf
e107coders <e107coders@localhost>
2004-03-18 23:51:32 +0000

    modified to use go to login.php when members-only is enabled in preferences..
    svn path=/trunk/e107/; revision=62

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <?php
 /*
 +---------------------------------------------------------------+
-|	e107 website system
-|	/index.php
+|        e107 website system
+|        /index.php
 |
-|	©Steve Dunstan 2001-2002
-|	http://e107.org
-|	jalist@e107.org
+|        ©Steve Dunstan 2001-2002
+|        http://e107.org
+|        jalist@e107.org
 |
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
 require_once("class2.php");
+if($pref['membersonly_enabled']){
+   header("location: ".e_BASE."login.php");
+        exit;
+}
+
 if(!$pref['frontpage'] || $pref['frontpage_type'] == "splash"){
-	header("location: ".e_BASE."news.php");
-	exit;
+        header("location: ".e_BASE."news.php");
+        exit;
 }else if(is_numeric($pref['frontpage'])){
-	header("location: ".e_BASE."article.php?".$pref['frontpage'].".255");
-	exit;
+        header("location: ".e_BASE."article.php?".$pref['frontpage'].".255");
+        exit;
 }else if(eregi("http", $pref['frontpage'])){
-	header("location: ".e_BASE.$pref['frontpage']);
-	exit;
+        header("location: ".e_BASE.$pref['frontpage']);
+        exit;
 }else{
-	header("location: ".e_BASE.$pref['frontpage'].".php");
-	exit;
+        header("location: ".e_BASE.$pref['frontpage'].".php");
+        exit;
 }
 ?>
