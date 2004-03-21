9c6422a54159830921d03d04093681acb6c831d7
mcfly_e107 <mcfly_e107@localhost>
2004-03-21 17:01:12 +0000

    More secure image code stuff
    svn path=/trunk/e107/; revision=77

--- login.php
+++ login.php
@@ -15,8 +15,13 @@
 
 $membership_open = 1;
 
-
 require_once("class2.php");
+$use_imagecode = ($pref['logcode'] && extension_loaded("gd"));
+if($use_imagecode){
+	require_once(e_HANDLER."secure_img_handler.php");
+	$sec_img = new secure_image;
+}
+
 echo "<?xml version='1.0' encoding='iso-8859-1' ?>\n";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -27,7 +32,7 @@ echo "<?xml version='1.0' encoding='iso-8859-1' ?>\n";
 <style type="text/css">
 
 img {
-        behavior:        url("pngbehavior.htc");
+	behavior:        url("pngbehavior.htc");
 }
 
 </style>
@@ -36,18 +41,14 @@ img {
 <link rel="stylesheet" href="<?php echo THEME; ?>style.css" />
 <!-- <link rel="stylesheet" href="<?php echo e_BASE."e107_files/"; ?>e107.css" />-->
 <?php
-// if(file_exists(e_BASE."e107_themes/leap of elohimblue/style.css")){
- echo "\n<link rel='stylesheet' href='".THEME."style.css' />\n";
-
- // }
+echo "\n<link rel='stylesheet' href='".THEME."style.css' />\n";
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta http-equiv="content-style-type" content="text/css" />
 <?php
 echo $pref['meta_tag'][1]."\n";
 if(eregi("forum_post.php", e_SELF) && ($_POST['reply'] || $_POST['newthread'])){
-        $tmp = explode(".", e_QUERY);
-//        echo "<meta http-equiv=\"refresh\" content=\"5;url='".e_HTTP."forum_viewforum.php?".$tmp[1]."'>\n";
+	$tmp = explode(".", e_QUERY);
 }
 ?>
 
@@ -62,43 +63,49 @@ if(file_exists(e_BASE."e107_files/user.js")){echo "<script type='text/javascript
 <?php
 
 if(!USER){
- //       echo "<div style='text-align:center'><img src='".e_BASE."e107_themes/elohimnet/images/elohimnet_login.png' alt='' style='width:439px; height:62px'/></div><br />";
-        echo "<div style='text-align:center'>";
 
-
-
-        if(file_exists(THEME."images/login_logo.png")){
-        echo "<DIV STYLE=\"width:100%; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(
-     src='".THEME."images/login_logo.png', sizingMethod='image');\" ></DIV>\n";
-        } else{
-        echo "<DIV STYLE=\"width:100%; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(
-     src='".$IMAGES_DIRECTORY."logo.png', sizingMethod='image');\" ></DIV>\n";
-        }
-        echo "</div><br />";
-
-
-        require_once(e_HANDLER."form_handler.php");
-        $rs = new form;
-        $text = "
+	echo "<div style='text-align:center'>";
+	if(file_exists(THEME."images/login_logo.png")){
+		echo "<DIV STYLE=\"width:100%; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(
+		src='".THEME."images/login_logo.png', sizingMethod='image');\" ></DIV>\n";
+	} else{
+	echo "<DIV STYLE=\"width:100%; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(
+	src='".$IMAGES_DIRECTORY."logo.png', sizingMethod='image');\" ></DIV>\n";
+}
+echo "</div><br />";
+require_once(e_HANDLER."form_handler.php");
+$rs = new form;
+$text = "";
+if(LOGINMESSAGE != ""){
+	$text = "<div style='text-align:center'>".LOGINMESSAGE."</div>";
+}
+$text .= "
 <div style='text-align:center'>".
 $rs -> form_open("post", e_SELF)."
 <table class='fborder' style='width:30%'>
 <tr>
 <td class='forumheader3' style='width:30%'>
-User name
+".LAN_LOGIN_1."
 </td>
 <td class='forumheader3' style='width:70%; text-align:right'>".
 $rs -> form_text("username", 40, "", 100)."
 </td>
 </tr>
 <td class='forumheader3' style='width:30%'>
-User password
+".LAN_LOGIN_2."
 </td>
 <td class='forumheader3' style='width:70%; text-align:right'>".
 $rs -> form_password("userpass", 40, "", 100)."
 </td>
-</tr>
-<tr>
+</tr>";
+
+		if($use_imagecode){
+			$text .= "<tr><td class='forumheader3'>".LAN_LOGIN_7."</td><td class='forumheader3'><input type='hidden' name='rand_num' value='".$sec_img -> random_number."'>";
+			$text .= $sec_img -> r_image();
+			$text .= "<br /><input class='tbox' type='text' name='code_verify' size='15' maxlength='20'><br /></td></tr>";
+		}
+
+$text .= "<tr>
 <td class='forumheader' colspan='2' style='text-align:center'>".
 $rs -> form_button("submit", "userlogin", "Log In", "", "Click to login")."
 </td>
@@ -107,22 +114,23 @@ $rs -> form_button("submit", "userlogin", "Log In", "", "Click to login")."
 $rs -> form_close()."
 </div>";
 
-$login_message = "<center>Protected server | ".SITENAME." | please enter your details to gain access</center>";
+$login_message = "<center>".LAN_LOGIN_3." | ".SITENAME." | ".LAN_LOGIN_4."</center>";
+
 echo "<div style='text-align:center'><div align='center' style='text-align:center; width:75%'>";
+
 $ns -> tablerender($login_message, $text);
 echo "</div></div>";
 
-        if($membership_open == 1){
-        echo "<div style='text-align:center'><a href='".e_SIGNUP."'>Click here to Sign-Up</a></div>";
-        }else{
-        echo "<div style='text-align:center'>Not accepting new members at this time.</div>";
-        }
-
+if($membership_open == 1){
+	echo "<div style='text-align:center'><a href='".e_SIGNUP."'>".LAN_LOGIN_5."</a></div>";
 }else{
-        header("location:".e_BASE."news.php");
-        exit;
+echo "<div style='text-align:center'>".LAN_LOGIN_6."</div>";
 }
 
+}else{
+header("location:".e_BASE."news.php");
+exit;
+}
 
 echo "</body>
 </html>";
