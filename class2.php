904c0a5ec05ddb5f9df542aaadef0e64918204ea
loloirie <loloirie@localhost>
2004-03-22 13:10:30 +0000

    Allow different administrators to see admin area with their languages (if different lang required and installed)
    svn path=/trunk/e107/; revision=78

--- class2.php
+++ class2.php
@@ -815,8 +815,9 @@ function init_session(){
                         }
 
                         define("USERTHEME", ($user_pref['sitetheme'] && file_exists(e_THEME.$user_pref['sitetheme']."/theme.php") ? $user_pref['sitetheme'] : FALSE));
-                        define("USERLAN", ($user_pref['sitelanguage'] && file_exists(e_LANGUAGEDIR.$user_pref['sitelanguage']."/lan_".e_PAGE) ? $user_pref['sitelanguage'] : FALSE));
-
+                        global $ADMIN_DIRECTORY;
+						define("USERLAN", ($user_pref['sitelanguage'] && ((strpos(e_SELF,$ADMIN_DIRECTORY)===FALSE && file_exists(e_LANGUAGEDIR.$user_pref['sitelanguage']."/lan_".e_PAGE))||(strpos(e_SELF,$ADMIN_DIRECTORY)!==FALSE && file_exists(e_LANGUAGEDIR.$user_pref['sitelanguage']."/admin/lan_".e_PAGE))) ? $user_pref['sitelanguage'] : FALSE));
+						
                         if($user_currentvisit + 3600 < time()){
                                 $sql -> db_Update("user", "user_visits=user_visits+1 WHERE user_name='".USERNAME."' ");
                                 $sql -> db_Update("user", "user_lastvisit='$user_currentvisit', user_currentvisit='".time()."', user_viewed='$r' WHERE user_name='".USERNAME."' ");
