3d317ed2639fd25dc1a1c8a77fcf6b90e9e7c524
vilhelm <vilhelm@localhost>
2004-03-23 18:15:05 +0000

    print.php can now be used by plugins, similiar to the search
    svn path=/trunk/e107/; revision=96

--- print.php
+++ print.php
@@ -20,8 +20,10 @@ $table = $qs[0];
 $id = $qs[1];
 $con = new convert;
 $aj = new textparse;
-if($table == "news"){
-	$sql -> db_Select("news", "*", "news_id='$id' ");
+
+
+$print_info[] = array( 'table' => 'news',  'handler' => '
+	$sql -> db_Select("news", "*", "news_id=\"$id\" ");
 	$row = $sql -> db_Fetch(); extract($row);
 	$news_body = $aj -> tpa($news_body);
 	$news_extended = $aj -> tpa($news_extended);
@@ -29,19 +31,14 @@ if($table == "news"){
 		$a_name = "e107";
 		$category_name = "e107 welcome message";
 	}else{
-		$sql -> db_Select("news_category", "*", "category_id='$news_category' ");
+		$sql -> db_Select("news_category", "*", "category_id=\"$news_category\" ");
 		list($category_id, $category_name) = $sql-> db_Fetch();
-		$sql -> db_Select("user", "*", "user_id='$news_author' ");
+		$sql -> db_Select("user", "*", "user_id=\"$news_author\" ");
 		list($a_id, $a_name) = $sql-> db_Fetch();
 	}
 	$news_datestamp = $con -> convert_date($news_datestamp, "long");
 
-	$text = "<font style='font-size: 11px; color: black; font-family: tahoma, verdana, arial, helvetica; text-decoration: none'>
-	<div style='text-align:center'>
-	<img src='".e_IMAGE."logo.png' alt='Logo' />
-	</div>
-	<hr />
-	<br />
+	$text = "<font style=\"font-size: 11px; color: black; font-family: tahoma, verdana, arial, helvetica; text-decoration: none\">
 	<b>".LAN_135.": ".$news_title."</b>
 	<br />
 	(".LAN_86." ".$category_name.")
@@ -57,25 +54,21 @@ if($table == "news"){
 	$text .= "<br /><br /><hr />".
 	LAN_303.SITENAME."
 	<br />
-	( http://".$_SERVER['HTTP_HOST'].e_HTTP."comment.php?".$news_id." )
+	( http://".$_SERVER[HTTP_HOST].e_HTTP."comment.php?".$news_id." )
 	</font>";
-	
-}else{
-	$sql -> db_Select("content", "*", "content_id='$id' ");
+');
+
+$print_info[] = array( 'table' => 'content', 'handler' => '
+	$sql -> db_Select("content", "*", "content_id=\"$id\" ");
 	$row = $sql -> db_Fetch();
 	extract($row);
 	$content_heading = $aj -> tpa($content_heading);
 	$content_subheading = $aj -> tpa($content_subheading);
 	$content_content = ereg_replace("\{EMAILPRINT\}|\[newpage\]", "", $aj -> tpa($content_content));
-	$sql -> db_Select("user", "*", "user_id='$content_author' ");
+	$sql -> db_Select("user", "*", "user_id=\"$content_author\" ");
 	list($a_id, $a_name) = $sql-> db_Fetch();
 	$content_datestamp = $con -> convert_date($content_datestamp, "long");
-	$text = "<font style='FONT-SIZE: 11px; COLOR: black; FONT-FAMILY: Tahoma, Verdana, Arial, Helvetica; TEXT-DECORATION: none'>
-	<div style='text-align:center'>
-	<img src='".e_IMAGE."logo.png' alt='Logo' />
-	</div>
-	<hr />
-	<br />
+	$text = "<font style=\"FONT-SIZE: 11px; COLOR: black; FONT-FAMILY: Tahoma, Verdana, Arial, Helvetica; TEXT-DECORATION: none\">
 	<b>".LAN_304.$content_heading."</b>
 	<br />
 	".LAN_305.$content_subheading."
@@ -87,9 +80,36 @@ if($table == "news"){
 	<br /><br /><hr />
 	".LAN_306.SITENAME."
 	<br />
-	( http://".$_SERVER['HTTP_HOST'].e_HTTP."article.php?article.".$content_id." )
+	( http://".$_SERVER[HTTP_HOST].e_HTTP."article.php?article.".$content_id." )
 	</font>";
+');
+
+
+//load the others from plugins
+$handle=opendir(e_PLUGIN);
+while(false !== ($file = readdir($handle))){
+	if($file != "." && $file != ".." && is_dir(e_PLUGIN.$file)){
+		$plugin_handle=opendir(e_PLUGIN.$file."/");
+		while(false !== ($file2 = readdir($plugin_handle))){
+			if($file2 == "e_print.php"){
+				require_once(e_PLUGIN.$file."/".$file2);
+			}
+		}
+	}
 }
+
+//eval the handler
+foreach($print_info as $key => $si){
+	if($print_info[$key]['table'] == $table){
+		eval($print_info[$key]['handler']);
+	}
+}
+echo "<div style=\"text-align:center\">
+	<img src=\"".e_IMAGE."logo.png\" alt=\"Logo\" />
+	</div>
+	<hr />
+	<br />";
+
 echo $text;
 
 echo "<br /><br /><div style='text-align:center'><form><input type='button' value='".LAN_307."' onClick='window.print()'></form></div>";
