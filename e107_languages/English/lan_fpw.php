754d6a64832a28d2693cfbe30144cffcc7697672
mcfly_e107 <mcfly_e107@localhost>
2004-03-26 05:07:48 +0000

    Added new security measures.
    svn path=/trunk/e107/; revision=115

--- e107_languages/English/lan_fpw.php
+++ e107_languages/English/lan_fpw.php
@@ -6,13 +6,13 @@ define("LAN_01", "New password sent to ".$_POST['email'].", please follow the in
 define("LAN_02", "Sorry, unable to send email - please contact the main site administrator.");
 define("LAN_03", "Password Reset");
 define("LAN_04", "Please enter your email address");
-define("LAN_05", "To reset your password please enter the email address you used when signing up ...");
+define("LAN_05", "To reset your password please enter the following information");
 define("LAN_06", "Attempted password reset");
 define("LAN_07", "Someone with ip address ");
 define("LAN_08", "attempted to reset the main admin password.");
 define("LAN_09", "Password reset from ");
 
-define("LAN_112", "Email Address: ");
+define("LAN_112", "Email Address used when signing up ");
 define("LAN_156", "Submit");
 define("LAN_213", "That username/email address was not found in database.");
 define("LAN_214", "Unable to reset password");
@@ -22,5 +22,16 @@ define("LAN_217", "Your new password is now validated, you may now login using y
 
 define("LAN_218", "Your username is:");
 define("LAN_219", "The password associated with that email address has already been reset and cannot be reset again. Please contact the site administrator for more details.");
+
+define("LAN_FPW1","User Name");
+define("LAN_FPW2","Enter code");
+define("LAN_FPW3","Incorrect code entered");
+define("LAN_FPW4","A request has already been sent to reset this password, if you did not receive the email, please contact the site administrator for help.");
+define("LAN_FPW5","A request to reset your password for ".SITENAME." has been submitted by someone with the IP of:".getip().".\n\nThis does not mean your password has yet been reset.  You must navigate to the link shown below to complete the reset process.\n\nIf it was not you the requested the password be reset and you do NOT want it reset, you can simply ignore this email\n\nThe link below will be valid for 48 hours.");
+define("LAN_FPW6","An email has been sent to you with a link that will allow you to reset your password.");
+define("LAN_FPW7","This is not a valid link to reset your password.<br />Please contact the site administrator for more details.");
+define("LAN_FPW8","The password for username");
+define("LAN_FPW9","has been successfully changed.<br /><br />The new password is:");
+define("LAN_FPW10","Please log in now and immediately change your password, for security purposes.");
 
 ?>
--- fpw.php
+++ fpw.php
@@ -14,9 +14,46 @@
 +---------------------------------------------------------------+
 */
 require_once("class2.php");
+$use_imagecode = ($pref['logcode'] && extension_loaded("gd"));
+if($use_imagecode){
+	require_once(e_HANDLER."secure_img_handler.php");
+	$sec_img = new secure_image;
+}
+
 require_once(HEADERF);
 
+function fpw_error($txt){
+	global $ns;
+	$ns -> tablerender(LAN_03,"<div class='forumheader3' style='text-align:center'>".$txt."</div>");
+	require_once(FOOTERF);
+	exit;
+}
+
 if(e_QUERY){
+
+	if($sql -> db_Select("tmp","*","tmp_info LIKE '%.".e_QUERY."' ")){
+		$row = $sql -> db_Fetch();
+		extract($row);
+		$sql -> db_Delete("tmp","tmp_info LIKE '%.".e_QUERY."' ");
+		$newpw="";
+		$pwlen = rand(8, 12);
+		for($a=0; $a<=$pwlen;$a++){
+			$newpw .= chr(rand(97, 122));
+		}
+		$mdnewpw = md5($newpw);
+
+		list($username,$md5) = explode(".",$tmp_info);
+		$sql -> db_Update("user", "user_password='$mdnewpw', user_sess='', user_viewed='' WHERE user_name='$username' ");
+		cookie($pref['cookie_name'], "", (time()-2592000));
+		$_SESSION[$pref['cookie_name']] = "";
+		
+		$txt = LAN_FPW8." {$username} ".LAN_FPW9." {$newpw}<br /><br />".LAN_FPW10;
+		fpw_error($txt);
+		
+	} else {
+		fpw_error(LAN_FPW7);
+	}
+
 	if($sql -> db_Select("user", "*", "user_viewed='".e_QUERY."' ")){
 		$row = $sql -> db_Fetch(); extract($row);
 		$sql -> db_Update("user", "user_password='$user_sess', user_sess='', user_viewed='' WHERE user_id='$user_id' ");
@@ -31,7 +68,14 @@ if(e_QUERY){
 if(IsSet($_POST['pwsubmit'])){
 	require_once(e_HANDLER."mail.php");
 	$email = $_POST['email'];
-	if($sql -> db_Select("user", "*", "user_email='".$_POST['email']."' ")){
+	
+	if($pref['logcode'] && extension_loaded("gd")){
+		if(!$sec_img -> verify_code($_POST['rand_num'],$_POST['code_verify'])){
+			fpw_error(LAN_FPW3);
+		}
+	}
+	
+	if($sql -> db_Select("user", "*", "user_email='{$_POST['email']}' AND user_name='{$_POST['username']}' ")){
 		$row = $sql -> db_Fetch(); extract($row);
 
 		if(is_numeric($user_login) && strlen($user_sess) == 32){
@@ -41,30 +85,30 @@ if(IsSet($_POST['pwsubmit'])){
 		}
 
 		if($user_id == 1 && $user_perms == 0){
-			sendemail($pref['siteadminemail'], ".LAN_06.", "".LAN_07."".getip()." ".LAN_08");
+			sendemail($pref['siteadminemail'], ".LAN_06.", "".LAN_07."".getip()." ".LAN_08);
 			echo "<script type='text/javascript'>document.location.href='index.php'</script>\n";
 			die();
 		}
 
-		$pwlen = rand(6, 12);
-		for($a=0; $a<=$pwlen;$a++){
-			$newpw .= chr(rand(97, 122));
+		if($sql -> db_Select("tmp","*","tmp_ip = 'pwreset' AND tmp_info LIKE '{$user_name}.%'")){
+			fpw_error(LAN_FPW4);
+			exit;
 		}
-		$mdnewpw = md5($newpw);
 
-		$pwlen = rand(20, 30);
-		for($a=0; $a<=$pwlen;$a++){
-			$validate .= chr(rand(48, 57));
-		}
-		
-		
-		$sql -> db_Update("user", "user_sess='$mdnewpw', user_viewed='$validate' WHERE user_email='".$_POST['email']."' ");
-		$returnaddress = (substr(SITEURL, -1) == "/" ? SITEURL."fpw.php" : SITEURL."/fpw.php");
-		$message = LAN_215.$newpw."\n\n".LAN_218." $user_name\n\n".LAN_216."\n\n".$returnaddress."?".$validate;
+		mt_srand ((double)microtime()*1000000);
+		$maxran = 1000000;
+		$rand_num = mt_rand(0, $maxran);
+		$datekey = date("r");
+		$rcode = md5($_SERVER[HTTP_USER_AGENT] . serialize($pref). $rand_num . $datekey);
 
-		
+		$link = SITEURL."fpw.php?{$rcode}";
+		$message = LAN_FPW5."\n\n{$link}";
+
+		$deltime = time()+86400*2;  //Set timestamp two days ahead so it doesn't get auto-deleted
+		$sql -> db_Insert("tmp","'pwreset',{$deltime},'{$user_name}.{$rcode}'");
+
 		if(sendemail($_POST['email'], "".LAN_09."".SITENAME, $message)){
-			$text = "<div style='text-align:center'>".LAN_01."</div>";
+			$text = "<div style='text-align:center'>".LAN_FPW6."</div>";
 		}else{
 			$text = "<div style='text-align:center'>".LAN_02."</div>";
 		}
@@ -87,12 +131,31 @@ $text = "<div style='text-align:center'>
 </tr>
 
 <tr>
-<td class='forumheader3' style='width:20%'>".LAN_112."</td>
-<td class='forumheader3' style='width:80%' style='text-align:center'>
-<input class='tbox' type='text' name='email' size='60' value='' maxlength='100' />
+<td class='forumheader3' style='width:25%'>".LAN_FPW1."</td>
+<td class='forumheader3' style='width:75%' style='text-align:center'>
+<input class='tbox' type='text' name='username' size='60' value='' maxlength='100' />
 </td>
 </tr>
 
+<tr>
+<td class='forumheader3' style='width:25%'>".LAN_112."</td>
+<td class='forumheader3' style='width:75%' style='text-align:center'>
+<input class='tbox' type='text' name='email' size='60' value='' maxlength='100' />
+</td>
+</tr>";
+
+if($use_imagecode){
+	$text .= "
+				<tr>
+					<td class='forumheader3' style='width:25%'>".LAN_FPW2."</td>
+					<td class='forumheader3' style='width:75%' style='text-align:left'>
+					<input type='hidden' name='rand_num' value='".$sec_img -> random_number."'>";
+			$text .= $sec_img -> r_image();
+			$text .= "<br /><input class='tbox' type='text' name='code_verify' size='15' maxlength='20'><br />";
+			$text .= "</td></tr>";
+}
+
+$text .="
 </tr>
 <tr style='vertical-align:top'> 
 <td class='forumheader' colspan='2'  style='text-align:center'>
