016e9c99b15943068b3c1f9ccb46b92104b03e8d
chavo <chavo@localhost>
2004-03-30 23:33:16 +0000

    No link to e107_plugins/list_new/new.php if there are no new items.
    svn path=/trunk/e107/; revision=141

--- e107_plugins/login_menu/login_menu.php
+++ e107_plugins/login_menu/login_menu.php
@@ -20,21 +20,28 @@ $text = "";
 			if(!$sql -> db_Select("online", "*", "online_ip='$ip' AND online_user_id='0' ")){
 				$sql -> db_Delete("online", "online_ip='$ip' AND online_user_id='0' ");
 			}
-
+			$new_total = 0;
 			$time = USERLV;
-			$new_news = $sql -> db_Count("news", "(*)", "WHERE news_datestamp>'".$time."' "); if(!$new_news){ $new_news = LOGIN_MENU_L26; }
-			$new_comments = $sql -> db_Count("comments", "(*)", "WHERE comment_datestamp>'".$time."' "); if(!$new_comments){ $new_comments = LOGIN_MENU_L26; }
-			$new_chat = $sql -> db_Count("chatbox", "(*)", "WHERE cb_datestamp>'".$time."' "); if(!$new_chat){ $new_chat = LOGIN_MENU_L26; }
-			$new_forum = $sql -> db_Count("forum_t", "(*)", "WHERE thread_datestamp>'".$time."' "); if(!$new_forum){ $new_forum = LOGIN_MENU_L26; }
-			$new_users = $sql -> db_Count("user", "(*)", "WHERE user_join>'".$time."' "); if(!$new_users){ $new_users = LOGIN_MENU_L26; }
+			$new_news = $sql -> db_Count("news", "(*)", "WHERE news_datestamp>'".$time."' ");$new_total = $new_total + $new_news;
+			if(!$new_news){ $new_news = LOGIN_MENU_L26; }			
+			$new_comments = $sql -> db_Count("comments", "(*)", "WHERE comment_datestamp>'".$time."' "); $new_total = $new_total + $new_comments;
+			if(!$new_comments){ $new_comments = LOGIN_MENU_L26; }
+			$new_chat = $sql -> db_Count("chatbox", "(*)", "WHERE cb_datestamp>'".$time."' "); $new_total = $new_total + $new_chat;
+			if(!$new_chat){ $new_chat = LOGIN_MENU_L26; }
+			$new_forum = $sql -> db_Count("forum_t", "(*)", "WHERE thread_datestamp>'".$time."' "); $new_total = $new_total + $new_forum;
+			if(!$new_forum){ $new_forum = LOGIN_MENU_L26; }
+			$new_users = $sql -> db_Count("user", "(*)", "WHERE user_join>'".$time."' "); $new_total = $new_total + $new_users;
+			if(!$new_users){ $new_users = LOGIN_MENU_L26; }
 
 			$text .= "<br /><br />\n<span class='smalltext'>\n".LOGIN_MENU_L25." 
 			$new_news ".($new_news == 1 ? LOGIN_MENU_L14 : LOGIN_MENU_L15).", 
 			$new_chat ".($new_chat == 1 ? LOGIN_MENU_L16 : LOGIN_MENU_L17).", 
 			$new_comments ".($new_comments == 1 ? LOGIN_MENU_L18 : LOGIN_MENU_L19).", 
 			$new_forum ".($new_forum == 1 ? LOGIN_MENU_L20 : LOGIN_MENU_L21)." ".LOGIN_MENU_L27." 
-			$new_users ".($new_users == 1 ? LOGIN_MENU_L22 : LOGIN_MENU_L23).".</span>
-			<br /><a href='".e_PLUGIN."list_new/new.php'>".LOGIN_MENU_L24."</a>";
+			$new_users ".($new_users == 1 ? LOGIN_MENU_L22 : LOGIN_MENU_L23).".</span>";
+			if($new_total){
+				$text .= "<br /><a href='".e_PLUGIN."list_new/new.php'>".LOGIN_MENU_L24."</a>";
+			}
 			$caption = (file_exists(THEME."images/login_menu.png") ? "<img src='".THEME."images/login_menu.png' alt='' /> ".LOGIN_MENU_L5." ".USERNAME : LOGIN_MENU_L5." ".USERNAME);
 			$ns -> tablerender($caption, $text);
 		}else{
