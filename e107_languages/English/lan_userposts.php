6bdedd8e20cdd79b75727d0e2e72ef82bac10e9b
chavo <chavo@localhost>
2004-04-01 22:07:05 +0000

    New file userposts.php. Lists forum posts and comments by a user.
    svn path=/trunk/e107/; revision=150

---
+++ e107_languages/English/lan_userposts.php
@@ -0,0 +1,18 @@
+<?php
+
+define("PAGE_NAME", "User Posts"); 
+
+define("UP_LAN_0", "All Forum Posts for ");
+define("UP_LAN_1", "All Comments for ");
+define("UP_LAN_2", "Thread");
+define("UP_LAN_3", "Views");
+define("UP_LAN_4", "Replies");
+define("UP_LAN_5", "Lastpost");
+define("UP_LAN_6", "Threads");
+define("UP_LAN_7", "No Comments");
+define("UP_LAN_8", "No Posts");
+define("UP_LAN_9", " on ");
+define("UP_LAN_10", "Re");
+define("UP_LAN_11", "Posted on: ");
+define("UP_LAN_12", "Search");
+?>
