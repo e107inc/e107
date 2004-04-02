8d726d21151a767461e4107d86bab7b071866f1a
chavo <chavo@localhost>
2004-04-02 02:17:52 +0000

    Addtext buttons were broken, fix sent to me by Lolo Irie.
    svn path=/trunk/e107/; revision=152

--- e107_admin/meta.php
+++ e107_admin/meta.php
@@ -1,15 +1,15 @@
 <?php
 /*
 +---------------------------------------------------------------+
-|	e107 website system
-|	/admin/meta.php
+|    e107 website system
+|    /admin/meta.php
 |
-|	©Steve Dunstan 2001-2002
-|	http://e107.org
-|	jalist@e107.org
+|    ©Steve Dunstan 2001-2002
+|    http://e107.org
+|    jalist@e107.org
 |
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
+|    Released under the terms and conditions of the
+|    GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
 require_once("../class2.php");
@@ -17,14 +17,15 @@ if(!getperms("C")){ header("location:".e_BASE."index.php"); exit; }
 require_once("auth.php");
 
 if(IsSet($_POST['metasubmit'])){
-	$aj = new textparse;
-	$pref['meta_tag'] = $aj -> formtpa($_POST['meta'], "admin");
-	save_prefs();
-	$message = METLAN_1;
+    $aj = new textparse;
+    $pref['meta_tag'] = $aj -> formtpa($_POST['meta'], "admin");
+    save_prefs();
+    $message = METLAN_1;
 }
 
 if($message){
-	$ns -> tablerender(METLAN_4, "<div style='text-align:center'>".METLAN_1.".</div>");
+    $ns -> tablerender(METLAN_4, "<div 
+style='text-align:center'>".METLAN_1.".</div>");
 }
 
 $text = "<div style='text-align:center'>
@@ -34,18 +35,35 @@ $text = "<div style='text-align:center'>
 
 <td style='width:30%' class='forumheader3'>".METLAN_2.": </td>
 <td style='width:70%' class='forumheader3'>
-<textarea class='tbox' name='meta' cols='70' rows='10'>".$pref['meta_tag']."</textarea>
+<textarea class='tbox' name='meta' cols='70' 
+rows='10'>".$pref['meta_tag']."</textarea>
 <br />";
 $text .= <<< EOT
-<input class="button" type="button" value="description" onclick="addtext2('<meta name=\'description\' content=\'".METLAN_5."\' />')">
-<input class="button" type="button" value="keywords" onclick="addtext2('<meta name=\'keywords\' content=\'".METLAN_6."\' />')">
-<input class="button" type="button" value="copyright" onclick="addtext2('<meta name=\'copyright\' content=\'".METLAN_7."\' />')">
+<input class="button" type="button" value="description" 
+onclick="addtext2('<meta name=\'description\' content=\'
+EOT;
+$text .= METLAN_5;
+$text .= <<< EOT
+\' />')">
+<input class="button" type="button" value="keywords" 
+onclick="addtext2('<meta name=\'keywords\' content=\'
+EOT;
+$text .= METLAN_6;
+$text .= <<< EOT
+\' />')">
+<input class="button" type="button" value="copyright" 
+onclick="addtext2('<meta name=\'copyright\' content=\'
+EOT;
+$text .= METLAN_7;
+$text .= <<< EOT
+\' />')">
 EOT;
 $text .= "</td>
 </tr>
 
 <td colspan='2' style='text-align:center' class='forumheader'>
-<input class='button' type='submit' name='metasubmit' value='".METLAN_3."' />
+<input class='button' type='submit' name='metasubmit' 
+value='".METLAN_3."' />
 </td>
 </tr>
 </table>
@@ -56,7 +74,7 @@ $ns -> tablerender(METLAN_8, $text);
 ?>
 <script type="text/javascript">
 function addtext2(str){
-	document.dataform.meta.value += str;
+    document.dataform.meta.value += str;
 }
 </script>
 <?php
