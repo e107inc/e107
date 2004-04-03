dd84ec436e5725ae7faf88e0e132c3839dab26eb
mcfly_e107 <mcfly_e107@localhost>
2004-04-03 16:19:50 +0000

    Separated render code from handler, much faster now.
    svn path=/trunk/e107/; revision=154

--- e107_handlers/secure_img_handler.php
+++ e107_handlers/secure_img_handler.php
@@ -1,21 +1,17 @@
 <?php
 /*
-	This is a handler for displaying a security image and verifying its validity.
-	Some of the code was ripped from postnuke...thanks :)
-	
++---------------------------------------------------------------+
+|	e107 website system
+|	/e107_handlers/secure_img_handler.php
+|
+|	http://e107.org
+|
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
 */
 
-global $mySQLserver;
-if(!$mySQLserver){
-	if(file_exists("../class2.php")){
-		require_once("../class2.php");
-	} elseif(file_exists("class2.php")){
-		require_once("class2.php");
-	}
-}
-
 class secure_image {
-	
 	var $random_number;
 
 	function secure_image() {
@@ -24,7 +20,8 @@ class secure_image {
 	}
 	
 	function create_code(){
-		global $pref, $sql;
+		global $pref, $sql, $IMAGES_DIRECTORY;
+		$imgp = SITEURL.$IMAGES_DIRECTORY;
 		mt_srand ((double)microtime()*1000000);
 		$maxran = 1000000;
 		$rand_num = mt_rand(0, $maxran);
@@ -33,7 +30,7 @@ class secure_image {
 		$code = substr($rcode, 2, 6);
 		$recnum = $this -> random_number;
 		$del_time = time()+1200;
-		$sql -> db_Insert("tmp","'{$recnum}',{$del_time},'{$code}'");
+		$sql -> db_Insert("tmp","'{$recnum}',{$del_time},'{$code},{$imgp}'");
 		return $recnum;
 	}
 
@@ -42,39 +39,16 @@ class secure_image {
 		if($sql -> db_Select("tmp","tmp_info","tmp_ip = '{$rec_num}'")){
 			$row = $sql -> db_Fetch();
 			$sql -> db_Delete("tmp","tmp_ip = '{$rec_num}'");
-			return ($checkstr == $row[0]);
-		} else {
-			return FALSE;
+			list($code,$path) = explode(",",$row[0]);
+			return ($checkstr == $code);
 		}
+		return FALSE;
 	}
-
-	function render_image($rec_num){
-		global $sql;
-		ob_clean();
-		if($sql -> db_Select("tmp","tmp_info","tmp_ip = '{$rec_num}'")){
-			$row = $sql -> db_Fetch();
-			$code = $row['tmp_info'];
-			$image = ImageCreateFromJPEG(e_IMAGE."generic/code_bg.jpg");
-    		$text_color = ImageColorAllocate($image, 80, 80, 80);
-    		Header("Content-type: image/jpeg");
-    		ImageString ($image, 5, 12, 2, $code, $text_color);
-    		ImageJPEG($image, '', 75);
-    		ImageDestroy($image);
-    		die();
-    	}
-    }
 
 	function r_image(){
 		global $HANDLERS_DIRECTORY;
 		$code = $this -> create_code();
-		return "<img src='".e_BASE.$HANDLERS_DIRECTORY."secure_img_handler.php?REC.{$code}' />";
+		return "<img src='".e_BASE.$HANDLERS_DIRECTORY."secure_img_render.php?REC.{$code}' />";
 	}
 }
-
-if(preg_match("#REC\.#",e_QUERY)){
-	$qs = explode(".",e_QUERY,2);
-	$si = new secure_image;
-	$si -> render_image($qs[1]);
-}
-
 ?>
