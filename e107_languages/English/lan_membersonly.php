40414c751f280ae7d83cd9d769df9a894ee4ff04
mcfly_e107 <mcfly_e107@localhost>
2004-04-04 18:35:52 +0000

    New page to show members only message.  Now uses theme info and can be customized without editing class2.php
    svn path=/trunk/e107/; revision=156

---
+++ e107_languages/English/lan_membersonly.php
@@ -0,0 +1,11 @@
+<?php
+
+define("PAGE_NAME", "Members Only"); 
+
+define("LAN_MEMBERS_0", "restricted area");
+define("LAN_MEMBERS_1", "This is a ".LAN_MEMBERS_0);
+define("LAN_MEMBERS_2","to access it either log in or");
+define("LAN_MEMBERS_3","register as a member");
+define("LAN_MEMBERS_4","Click here to return to front page");
+
+?>
