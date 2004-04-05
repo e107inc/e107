1007d24659f686ae0aa981ff1f0e9c78016d8830
e107coders <e107coders@localhost>
2004-04-05 19:27:12 +0000

    added email validator function for future use.
    svn path=/trunk/e107/; revision=171

--- e107_handlers/mail.php
+++ e107_handlers/mail.php
@@ -18,7 +18,6 @@ function sendemail($send_to, $subject, $message,$format="plain"){
         $send_to = "<".$send_to.">";
         $headers = "From: \"".$pref['siteadmin']."\" <".$pref['siteadminemail']."> \n";
         $headers .= "Reply-To: ".$pref['siteadmin']." <".$pref['siteadminemail'].">\n";
-        $headers .= "Return-Path: <".$pref['siteadminemail'].">\n";
         $headers .= "X-Sender: ".$pref['siteadminemail']."\n";
         $headers .= "X-Mailer: Microsoft Outlook Express 6.00.2720.3000\n";
         $headers .= "X-MimeOLE: Produced By e107 website system\n";
@@ -50,5 +49,66 @@ function sendemail($send_to, $subject, $message,$format="plain"){
         }
 
 }
+
+
+function validatemail($Email) {
+    global $HTTP_HOST;
+    $result = array(); ;
+
+    if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $Email)) {
+        $result[0]=false;
+        $result[1]="$Email is not properly formatted";
+        return $result;
+    }
+
+    list ( $Username, $Domain ) = split ("@",$Email);
+
+    if (getmxrr($Domain, $MXHost)){
+        $ConnectAddress = $MXHost[0];
+    } else {
+        $ConnectAddress = $Domain;
+    }
+
+    $Connect = fsockopen ( $ConnectAddress, 25 );
+
+    if ($Connect){
+
+        if (ereg("^220", $Out = fgets($Connect, 1024))) {
+
+           fputs ($Connect, "HELO $HTTP_HOST\r\n");
+           $Out = fgets ( $Connect, 1024 );
+           fputs ($Connect, "MAIL FROM: <{$Email}>\r\n");
+           $From = fgets ( $Connect, 1024 );
+           fputs ($Connect, "RCPT TO: <{$Email}>\r\n");
+           $To = fgets ($Connect, 1024);
+           fputs ($Connect, "QUIT\r\n");
+           fclose($Connect);
+            if (!ereg ("^250", $From) ||
+            !ereg ( "^250", $To )) {
+               $result[0]=false;
+               $result[1]="Server rejected address";
+               $result[2] = $From;
+               return $result;
+            }
+            } else {
+            $result[0] = false;
+            $result[1] = "No response from server";
+            $result[2] = $From;
+            return $result;
+          }
+
+    }  else {
+
+        $result[0]=false;
+        $result[1]="Can not connect E-Mail server.";
+        $result[2] = $From;
+        return $result;
+    }
+  $result[0]=true;
+    $result[1]="$Email appears to be valid.";
+    $result[2] = $From;
+    return $result;
+} // end of function
+
 
 ?>
