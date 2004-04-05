19ae38da79b5d3b3c5bc587f12dc1486a0b08429
loloirie <loloirie@localhost>
2004-04-05 14:56:40 +0000

    Better multilanguage (submitted by Juan)
    svn path=/trunk/e107/; revision=170

--- e107_handlers/ren_help.php
+++ e107_handlers/ren_help.php
@@ -9,7 +9,7 @@ function ren_help($mode=1){
 	if(strstr(e_SELF, "article")){
 		$code[0] = array("newpage", "[newpage]", LANHELP_34);
 	}
-	$code[1] = array("link", "[link=hyperlink url][/link]", LANHELP_23);
+	$code[1] = array("link", "[link=".LANHELP_35."][/link]", LANHELP_23);
 	$code[2] = array("b", "[b][/b]", LANHELP_24);
 	$code[3] = array("i", "[i][/i]", LANHELP_25);
 	$code[4] = array("u", "[u][/u]", LANHELP_26);
@@ -23,20 +23,20 @@ function ren_help($mode=1){
 		$code[11] = array("html", "[html][/html]", LANHELP_33);
 	}
 
-	$colours[0] = LANHELP_1;
-	$colours[1] = LANHELP_2;
-	$colours[2] = LANHELP_3;
-	$colours[3] = LANHELP_4;
-	$colours[4] = LANHELP_5;
-	$colours[5] = LANHELP_6;
-	$colours[6] = LANHELP_7;
-	$colours[7] = LANHELP_8;
-	$colours[8] = LANHELP_9;
-	$colours[9] = LANHELP_10;
-	$colours[10] = LANHELP_11;
-	$colours[11] = LANHELP_12;
-	$colours[12] = LANHELP_13;
-	$colours[13] = LANHELP_14;
+	$colours[0] = array("black", LANHELP_1);
+	$colours[1] = array("blue", LANHELP_2);
+	$colours[2] = array("brown", LANHELP_3);
+	$colours[3] = array("cyan", LANHELP_4);
+	$colours[4] = array("darkblue", LANHELP_5);
+	$colours[5] = array("darkred", LANHELP_6);
+	$colours[6] = array("green", LANHELP_7);
+	$colours[7] = array("indigo", LANHELP_8);
+	$colours[8] = array("olive", LANHELP_9);
+	$colours[9] = array("orange", LANHELP_10);
+	$colours[10] = array("red", LANHELP_11);
+	$colours[11] = array("violet", LANHELP_12);
+	$colours[12] = array("white", LANHELP_13);
+	$colours[13] = array("yellow", LANHELP_14);
 
 	$fontsizes[0] = array("7", LANHELP_15);
 	$fontsizes[1] = array("9", LANHELP_16);
@@ -51,7 +51,7 @@ function ren_help($mode=1){
 	if($mode){
 		$string .= "<br />\n<select class=\"tbox\" name=\"fontcol\" onchange=\"addtext('[color=' + this.options[this.selectedIndex].value + '][/color]');this.selectedIndex=0;\"".($mode != 2 ? " onmouseover=\"help('Font Color: [color]Blue[/color]')\" onmouseout=\"help('')\"" : "").">\n<option value=\"\">".LANHELP_21."</option>\n";
 		while(list($key, $bbcode) = each($colours)){
-			$string .= "<option style=\"color:".strtolower($bbcode)."\" value=\"".strtolower($bbcode)."\">".$bbcode."</option>\n";
+			$string .= "<option style=\"color:".strtolower($bbcode[0])."\" value=\"".strtolower($bbcode[0])."\">".$bbcode[1]."</option>\n";
 		}
 		$string .= "</select>\n<select class=\"tbox\" name=\"fontsiz\" onchange=\"addtext('[size=' + this.options[this.selectedIndex].value + '][/size]');this.selectedIndex=0;\"".($mode != 2 ? " onmouseover=\"help('Font Size: [size]Big[/size]')\" onmouseout=\"help('')\">" : "" )."\n<option>".LANHELP_22."</option>\n";
 
