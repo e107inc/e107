2438a46c4d2bba90b53967e727558b13771fd515
mcfly_e107 <mcfly_e107@localhost>
2004-04-07 02:26:57 +0000

    secure image code now supports jpg, png, gif (auto-detects)
    svn path=/trunk/e107/; revision=179

--- e107_handlers/secure_img_render.php
+++ e107_handlers/secure_img_render.php
@@ -10,6 +10,8 @@
 |	GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
+$imgtypes=array("jpeg", "png", "gif");
+
 define("e_QUERY", eregi_replace("&|/?PHPSESSID.*", "", $_SERVER['QUERY_STRING']));
 $qs = explode(".",e_QUERY,2);
 ob_clean();
@@ -25,11 +27,39 @@ mysql_select_db($mySQLdefaultdb);
 $result = mysql_query("SELECT tmp_info FROM {$mySQLprefix}tmp WHERE tmp_ip = '{$qs[1]}'");
 $row = mysql_fetch_array($result);
 list($code,$url) = explode(",",$row['tmp_info']);
-$image = ImageCreateFromJPEG($url."generic/code_bg.jpg");
+
+$type="none";
+foreach($imgtypes as $t){
+	if(function_exists("imagecreatefrom".$t)){
+		$type = $t;
+		break;
+	}
+}
+switch($type){
+	case "jpeg":
+		$image = ImageCreateFromJPEG($url."generic/code_bg.jpg");
+		break;
+	case "png":
+		$image = ImageCreateFromPNG($url."generic/code_bg.png");
+		break;
+	case "gif":
+		$image = ImageCreateFromGIF($url."generic/code_bg.gif");
+		break;
+}
 $text_color = ImageColorAllocate($image, 80, 80, 80);
-Header("Content-type: image/jpeg");
+Header("Content-type: image/{$type}");
 ImageString ($image, 5, 12, 2, $code, $text_color);
-ImageJPEG($image, '', 75);
+switch($type){
+	case "jpeg":
+		ImageJPEG($image,'',75);
+		break;
+	case "png":
+		ImagePNG($image,'',75);
+		break;
+	case "gif":
+		ImageGIF($image,'',75);
+		break;
+}
 ImageDestroy($image);
 die();
 ?>
