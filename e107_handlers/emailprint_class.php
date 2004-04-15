2aa8df80511f8e2eaa46e0d86b6cc02d92df76ec
loloirie <loloirie@localhost>
2004-04-15 08:32:28 +0000

    Allow to use different icons for printer and email for each theme
    svn path=/trunk/e107/; revision=236

--- e107_handlers/emailprint_class.php
+++ e107_handlers/emailprint_class.php
@@ -16,8 +16,8 @@
 class emailprint{
 	function render_emailprint($mode, $id, $look=0){
 		// $look = 0  --->  display all icons
-		// $look = 1  --->  display email icons
-		// $look = 2  --->  display print icons
+		// $look = 1  --->  display email icon only
+		// $look = 2  --->  display print icon only
 		$text_emailprint="";
 		switch($mode){
 			case "article":
@@ -30,10 +30,20 @@ class emailprint{
 			break;
 		}
 		if($look==0 || $look==1){
-			$text_emailprint .= "<a href='email.php?".$email.".".$id."'><img src='".e_IMAGE."generic/friend.gif' style='border:0' alt='email to someone' /></a> ";
+			if(defined("ICONMAIL")&&file_exists(THEME."images/".ICONMAIL)){
+				$ico_mail = THEME."images/".ICONMAIL;
+			}else{
+				$ico_mail = e_IMAGE."generic/friend.gif";
+			}
+			$text_emailprint .= "<a href='email.php?".$email.".".$id."'><img src='".$ico_mail."' style='border:0' alt='email to someone' /></a> ";
 		}
 		if($look==0 || $look==2){
-			$text_emailprint .= "<a href='print.php?".$print.".".$id."'><img src='".e_IMAGE."generic/printer.gif' style='border:0' alt='printer friendly' /></a>";
+			if(defined("ICONPRINT")&&file_exists(THEME."images/".ICONPRINT)){
+				$ico_print = THEME."images/".ICONPRINT;
+			}else{
+				$ico_print = e_IMAGE."generic/printer.gif";
+			}
+			$text_emailprint .= "<a href='print.php?".$print.".".$id."'><img src='".$ico_print."' style='border:0' alt='printer friendly' /></a>";
 		}
 		return $text_emailprint;
 	}
