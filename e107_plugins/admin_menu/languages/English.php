b2eb292267b9a3b361ea6ee475a79128542d83f7
loloirie <loloirie@localhost>
2004-04-16 08:55:04 +0000

    Added multilanguage by Juan
    svn path=/trunk/e107/; revision=245

--- e107_plugins/admin_menu/languages/English.php
+++ e107_plugins/admin_menu/languages/English.php
@@ -15,4 +15,27 @@
 define("ADMIN_MENU_L1", "Select ...");
 define("ADMIN_MENU_L2", "Admin");
 
+define("ADMIN_MENU_L3", "News");
+define("ADMIN_MENU_L4", "Catégories News");
+define("ADMIN_MENU_L5", "Preferences");
+define("ADMIN_MENU_L6", "Menus");
+define("ADMIN_MENU_L7", "Administrators");
+
+define("ADMIN_MENU_L8", "Admin settings");
+define("ADMIN_MENU_L9", "Forums");
+define("ADMIN_MENU_L10", "Articles");
+define("ADMIN_MENU_L11", "Content");
+define("ADMIN_MENU_L12", "Reviews");
+
+define("ADMIN_MENU_L13", "Links");
+define("ADMIN_MENU_L14", "Link Categories");
+define("ADMIN_MENU_L15", "Welcome Message");
+define("ADMIN_MENU_L16", "Upload");
+define("ADMIN_MENU_L17", "Submitted News");
+
+define("ADMIN_MENU_L18", "Banlist");
+define("ADMIN_MENU_L19", "Users");
+define("ADMIN_MENU_L20", "Maintainance");
+define("ADMIN_MENU_L21", "Logout");
+
 ?>
