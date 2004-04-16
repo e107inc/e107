04a6cc7b7c8a5839164ce6e17fb18d14ffd3cf0f
loloirie <loloirie@localhost>
2004-04-16 08:34:39 +0000

    Add multilanguage
    svn path=/trunk/e107/; revision=243

--- e107_plugins/articles_menu/languages/English.php
+++ e107_plugins/articles_menu/languages/English.php
@@ -15,4 +15,13 @@
 define("ARTICLE_MENU_L1", "Articles Front Page");
 define("ARTICLE_MENU_L2", "Uncategorized");
 
+define("ARTICLE_MENU_L3", "Caption");
+define("ARTICLE_MENU_L4", "Number of articles to display");
+define("ARTICLE_MENU_L5", "Show article categories in menu?");
+define("ARTICLE_MENU_L6", "Title for articles list page:");
+define("ARTICLE_MENU_L7", "Articles Menu Configuration");
+define("ARTICLE_MENU_L8", "Update Menu Settings");
+define("ARTICLE_MENU_L9", "Articles menu configuration saved");
+
+
 ?>
