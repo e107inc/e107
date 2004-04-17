46e1a5f339e24bd25888e0d5a5751ddea364669e
mcfly_e107 <mcfly_e107@localhost>
2004-04-17 14:43:43 +0000

    Small language change
    svn path=/trunk/e107/; revision=252

--- e107_languages/English/admin/lan_e107_update.php
+++ e107_languages/English/admin/lan_e107_update.php
@@ -6,7 +6,7 @@ define("LAN_UPDATE_3", "Not Needed");
 define("LAN_UPDATE_4", "Update");
 define("LAN_UPDATE_5", "Update available");
 define("LAN_UPDATE_6", "Update all");
-define("LAN_UPDATE_7", "Executing");
+define("LAN_UPDATE_7", "Executed");
 define("LAN_UPDATE_8", "Update from");
 define("LAN_UPDATE_9", "to");
 define("LAN_UPDATE_10", "Available updates");
