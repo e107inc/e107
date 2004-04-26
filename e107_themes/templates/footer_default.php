3da55e42e0c7c028323d746a1bd21f9bde3300ff
loloirie <loloirie@localhost>
2004-04-26 15:11:21 +0000

    Allow to use these files for popups without menu (Require a variable $e107_popup in the file opened as popup)
    svn path=/trunk/e107/; revision=311

--- e107_themes/templates/footer_default.php
+++ e107_themes/templates/footer_default.php
@@ -19,25 +19,25 @@ if(!is_object($sql)){
 	$sql -> db_Connect($mySQLserver, $mySQLuser, $mySQLpassword, $mySQLdefaultdb);
 }
 
-
-$custompage = explode(" ", $CUSTOMPAGES);
-if($CUSTOMFOOTER){
-	while(list($key, $kpage) = each($custompage)){
-		if(strstr(e_SELF, $kpage)){
-			$fh = TRUE;
-			break;
+if($e107_popup!=1){
+	$custompage = explode(" ", $CUSTOMPAGES);
+	if($CUSTOMFOOTER){
+		while(list($key, $kpage) = each($custompage)){
+			if(strstr(e_SELF, $kpage)){
+				$fh = TRUE;
+				break;
+			}
 		}
 	}
+	parseheader(($fh ? $CUSTOMFOOTER : $FOOTER));
+	
+	$timing_stop = explode(' ', microtime());
+	$rendertime = number_format((($timing_stop[0]+$timing_stop[1])-($timing_start[0]+$timing_start[1])), 4);
+	if($pref['displayrendertime']){ $rinfo .= "Render time: ".$rendertime." second(s). "; }
+	if($pref['displaysql']){ $rinfo .= "DB queries: ".$dbq.". "; }
+	if($pref['displaycacheinfo']){ $rinfo .= $cachestring."."; }
+	echo ($rinfo ? "<div style='text-align:center' class='smalltext'>$rinfo</div>" : "");
 }
-parseheader(($fh ? $CUSTOMFOOTER : $FOOTER));
-
-$timing_stop = explode(' ', microtime());
-$rendertime = number_format((($timing_stop[0]+$timing_stop[1])-($timing_start[0]+$timing_start[1])), 4);
-if($pref['displayrendertime']){ $rinfo .= "Render time: ".$rendertime." second(s). "; }
-if($pref['displaysql']){ $rinfo .= "DB queries: ".$dbq.". "; }
-if($pref['displaycacheinfo']){ $rinfo .= $cachestring."."; }
-echo ($rinfo ? "<div style='text-align:center' class='smalltext'>$rinfo</div>" : "");
-
 echo "</body>
 </html>";
 
