f9b977a90c6d9b6756e1857979557a0a84e4b8d4
e107coders <e107coders@localhost>
2004-04-27 17:16:52 +0000

    - thumb.php - fixed whitespaces in filename and absolute-path issues - you can thank McFly for these.
    svn path=/trunk/e107/; revision=317

--- e107_images/thumb.php
+++ e107_images/thumb.php
@@ -1,27 +1,29 @@
 <?php
 
 /*
- Usage: simple replace your <img src='filename.jpg'
+ Usage: simply replace your <img src='filename.jpg'
  with
- <img src='".e_IMAGE."thumb.php?filename.jpg+size)"'>
+ <img src='".e_IMAGE."thumb.php?filename.jpg+size"' />
+ or
+ <img src='".e_IMAGE."thumb.php?<full path to file>/filename.jpg+size"' />
+ eg <img src='".e_IMAGE."thumb.php?home/images/myfilename.jpg+100)"' />
 
 */
 
 require_once("../class2.php");
 require_once(e_HANDLER."resize_handler.php");
 
-  if (e_QUERY){
-    $tmp = explode("+",e_QUERY);
-    if(eregi("home",$tmp[0]) || eregi($_SERVER['DOCUMENT_ROOT'],$tmp[0])){
-    $source = $tmp[0];
-    }else{
-    $source = "../".str_replace("../","",$tmp[0]);
-    }
+    if (e_QUERY){
+        $tmp = explode("+",rawurldecode(e_QUERY));
+        if(preg_match("#^/#",$tmp[0]) || preg_match("#.:#",$tmp[0])){
+            $source = $tmp[0];
+        }else{
+            $source = "../".str_replace("../","",$tmp[0]);
+        }
 
-    $newsize = $tmp[1];
-    if(!resize_image($source, "stdout", $newsize)){
-    echo "Couldn't find: ".$source;
+        $newsize = $tmp[1];
+        if(!resize_image($source, "stdout", $newsize)){
+            echo "Couldn't find: ".$source;
+        }
     }
-
-}
 ?>
