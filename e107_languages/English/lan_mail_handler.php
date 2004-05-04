272dfa9d0ff18a0952c4e17c21ab373274b0edb0
loloirie <loloirie@localhost>
2004-05-04 10:50:53 +0000

    Initial revision
    svn path=/trunk/e107/; revision=404

---
+++ e107_languages/English/lan_mail_handler.php
@@ -0,0 +1,13 @@
+<?php
+
+define("LANMAILH_1", "Produced By e107 website system");
+define("LANMAILH_2", "This is a multi-part message in MIME format.");
+define("LANMAILH_3", " is not properly formatted");
+define("LANMAILH_4", "Server rejected address");
+define("LANMAILH_5", "No response from server");
+define("LANMAILH_6", "Cannot find E-Mail server.");
+define("LANMAILH_7", " appears to be valid.");
+define("LANMAILH_8", "");
+define("LANMAILH_9", "");
+
+?>
