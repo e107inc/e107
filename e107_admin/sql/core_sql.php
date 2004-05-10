1811c9c66ea543e339c0384ae1f34ee28380ea8f
chavo <chavo@localhost>
2004-05-10 08:51:17 +0000

    Changed cb_nick in chatbox table to varchar(30) to allow usernames over 20 chars to be linked to their account.
    svn path=/trunk/e107/; revision=455

--- e107_admin/sql/core_sql.php
+++ e107_admin/sql/core_sql.php
@@ -69,7 +69,7 @@ CREATE TABLE cache (
 
 CREATE TABLE chatbox (
   cb_id int(10) unsigned NOT NULL auto_increment,
-  cb_nick varchar(20) NOT NULL default '',
+  cb_nick varchar(30) NOT NULL default '',
   cb_message text NOT NULL,
   cb_datestamp int(10) unsigned NOT NULL default '0',
   cb_blocked tinyint(3) unsigned NOT NULL default '0',
