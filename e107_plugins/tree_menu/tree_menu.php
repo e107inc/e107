6c5962cca388e915af520c1dc39c6b6998e37388
e107coders <e107coders@localhost>
2004-05-15 01:15:31 +0000

    -  Xhtml fixes
    svn path=/trunk/e107/; revision=489

--- e107_plugins/tree_menu/tree_menu.php
+++ e107_plugins/tree_menu/tree_menu.php
@@ -1,15 +1,15 @@
 <?php
 /*
 +---------------------------------------------------------------+
-|	e107 website system
-|	/tree_menu.php
+|        e107 website system
+|        /tree_menu.php
 |
-|	©Steve Dunstan 2001-2002
-|	http://e107.org
-|	jalist@e107.org
+|        ©Steve Dunstan 2001-2002
+|        http://e107.org
+|        jalist@e107.org
 |
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
 
@@ -30,52 +30,52 @@ unset($text);
 $sql2 = new db;
 $sql -> db_Select("links", "*", "link_category='1' AND link_name NOT REGEXP('submenu') ORDER BY link_order");
 while($row = $sql -> db_Fetch()){
-	extract($row);
-	$link_name=strip_tags($link_name);
-	if($sql2 -> db_Select("links", "*", "link_name REGEXP('submenu.".$link_name."') ORDER BY link_order")){
-		if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
-			$mlink_name = $link_name;
-			$text .= "
-			<div class='spacer'>
-			<div class='button' style='width:100%; cursor: pointer; cursor: hand' onClick='expandit(this);updatecook(\"".$link_name."\");' >&raquo; ".$link_name."</div>
-			<span style=\"display:none\" style=&{head}; id=\"span_".$link_name."\">";			
-			while($row = $sql2 -> db_Fetch()){
-				extract($row);
-				if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
-					$link_name = str_replace("submenu.".$mlink_name.".", "", $link_name);
-					$text .= "&middot; ".(strstr($link_url, "http") ? setlink($link_name, $link_url, $link_open) : setlink($link_name, $link_url, $link_open))."\n<br />";
-				}
-			}
-			$text .= "</span></div>";
-		}
-			}else{		
-				if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
-					$text .= "<div class='spacer'><div class='button' style='width:100%; cursor: pointer; cursor: hand' onClick=\"clearcook();\">&middot; ".
-					(strstr($link_url, "http") ? setlink($link_name, $link_url, $link_open) : setlink($link_name, $link_url, $link_open))."
-					</div></div>";
-				}
-			}
-		}
+        extract($row);
+        $link_name=strip_tags($link_name);
+        if($sql2 -> db_Select("links", "*", "link_name REGEXP('submenu.".$link_name."') ORDER BY link_order")){
+                if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
+                        $mlink_name = $link_name;
+                        $text .= "
+                        <div class='spacer'>
+                        <div class='button' style='width:100%; cursor: pointer; cursor: hand' onclick='expandit(this);updatecook(\"".$link_name."\");' >&raquo; ".$link_name."</div>
+                        <span style=\"display:none\" style=&{head}; id=\"span_".$link_name."\">";
+                        while($row = $sql2 -> db_Fetch()){
+                                extract($row);
+                                if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
+                                        $link_name = str_replace("submenu.".$mlink_name.".", "", $link_name);
+                                        $text .= "&middot; ".(strstr($link_url, "http") ? setlink($link_name, $link_url, $link_open) : setlink($link_name, $link_url, $link_open))."\n<br />";
+                                }
+                        }
+                        $text .= "</span></div>";
+                }
+                        }else{
+                                if(!$link_class || check_class($link_class) || ($link_class==254 && USER)){
+                                        $text .= "<div class='spacer'><div class='button' style='width:100%; cursor: pointer; cursor: hand' onclick=\"clearcook();\">&middot; ".
+                                        (strstr($link_url, "http") ? setlink($link_name, $link_url, $link_open) : setlink($link_name, $link_url, $link_open))."
+                                        </div></div>";
+                                }
+                        }
+                }
 
 $text .= "
-<sc"."ript>
+<script type='text/javascript'>
 <!--
 function updatecook(itemmenu){
-	cookitem='span_'+itemmenu;
-	if(document.getElementById(cookitem).style.display!='none'){
-		var expireDate = new Date;
-		expireDate.setMinutes(expireDate.getMinutes()+10);
-		document.cookie = \"treemenustatus=\" + itemmenu + \"; expires=\" + expireDate.toGMTString();
-	}
-	else{
-		clearcook();
-	}
+        cookitem='span_'+itemmenu;
+        if(document.getElementById(cookitem).style.display!='none'){
+                var expireDate = new Date;
+                expireDate.setMinutes(expireDate.getMinutes()+10);
+                document.cookie = \"treemenustatus=\" + itemmenu + \"; expires=\" + expireDate.toGMTString();
+        }
+        else{
+                clearcook();
+        }
 }\n
 
 function clearcook(){
-	var expireDate = new Date;
-	expireDate.setMinutes(expireDate.getMinutes()+10);
-	document.cookie = \"treemenustatus=\" + \"0\" + \"; expires=\" + expireDate.toGMTString();
+        var expireDate = new Date;
+        expireDate.setMinutes(expireDate.getMinutes()+10);
+        document.cookie = \"treemenustatus=\" + \"0\" + \"; expires=\" + expireDate.toGMTString();
 }\n
 
 //-->\n
@@ -83,32 +83,32 @@ function clearcook(){
 
 (($treemenustatus!="0")?$text .= "window.onload=document.getElementById('span_".$treemenustatus."').style.display=''":"");
 
-$text .= "</sc"."ript>
+$text .= "</script>
 ";
 $ns -> tablerender(LAN_183, $text);
 
 
 function setlink($link_name, $link_url, $link_open){
-		switch ($link_open){ 
-			case 1:
-				$link_append = "rel='external'";
-			break; 
-			case 2:
-				$link_append = "";
-			break;
-			case 3:
-				$link_append = "";
-			break;
-			default:
-				unset($link_append);
-		}
-		if(!strstr($link_url, "http:")){ $link_url = e_BASE.$link_url; }
-		if($link_open == 4){
-			$link =  "<a style='text-decoration:none' href=\"javascript:open_window('".$link_url."')\">".$link_name."</a>\n";
-		}else{
-			$link =  "<a style='text-decoration:none' href=\"".$link_url."\"".$link_append.">".$link_name."</a>\n";
-		}
-	return $link;
+                switch ($link_open){
+                        case 1:
+                                $link_append = "rel='external'";
+                        break;
+                        case 2:
+                                $link_append = "";
+                        break;
+                        case 3:
+                                $link_append = "";
+                        break;
+                        default:
+                                unset($link_append);
+                }
+                if(!strstr($link_url, "http:")){ $link_url = e_BASE.$link_url; }
+                if($link_open == 4){
+                        $link =  "<a style='text-decoration:none' href=\"javascript:open_window('".$link_url."')\">".$link_name."</a>\n";
+                }else{
+                        $link =  "<a style='text-decoration:none' href=\"".$link_url."\"".$link_append.">".$link_name."</a>\n";
+                }
+        return $link;
 }
 
 
