4077e5fe9c9fea10330230455f6c7168df51c53b
chavo <chavo@localhost>
2004-05-17 13:44:40 +0000

    Missing semicolon. Dev Bugtracker ID #50.
    svn path=/trunk/e107/; revision=526

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -2,5 +2,5 @@
 
 setlocale(LC_ALL, 'en');
 define("CHARSET", "iso-8859-1");
-define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.")
+define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 ?>
