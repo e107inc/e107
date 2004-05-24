3dc1e4df204a89e3bff024597cd04173dc2af7cd
chavo <chavo@localhost>
2004-05-24 23:26:32 +0000

    Moved some hardcoded text into the lan file.
    svn path=/trunk/e107/; revision=590

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -3,4 +3,8 @@
 setlocale(LC_ALL, 'en');
 define("CHARSET", "iso-8859-1");
 define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
+
+//v.616
+define("CORE_LAN2","Originally posted by");
+define("CORE_LAN3","file attachment disabled");
 ?>
