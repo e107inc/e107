dc512b28f7ebc1773d7930da01f776b3107e73fb
mcfly_e107 <mcfly_e107@localhost>
2004-05-24 23:47:53 +0000

    Fixed href paths for news.xml and news.txt
    svn path=/trunk/e107/; revision=592

--- e107_plugins/backend_menu/backend_menu.php
+++ e107_plugins/backend_menu/backend_menu.php
@@ -1,7 +1,8 @@
 <?php
+global $FILES_DIRECTORY;
 $text = "<div style='text-align:center' class='smalltext'>
 ".BACKEND_MENU_L1."<br />
-<a href='".e_FILE."backend/news.xml'>news.xml</a> - <a href='".e_FILE."backend/news.txt'>news.txt</a>
+<a href='".SITEURL.$FILES_DIRECTORY."backend/news.xml'>news.xml</a> - <a href='".SITEURL.$FILES_DIRECTORY."backend/news.txt'>news.txt</a>
 </div>";
 
 $caption = (file_exists(THEME."images/backend_menu.png") ? "<img src='".THEME."images/backend_menu.png' alt='' style='vertical-align:middle' /> ".BACKEND_MENU_L2 : BACKEND_MENU_L2);
