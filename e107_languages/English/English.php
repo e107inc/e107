3f5f82749b3a35f717bed2d21d0def3557a2f1c7
chavo <chavo@localhost>
2004-05-25 00:02:38 +0000

    Moved the reference to the username in quote strings into the lan file as well. This will be much more flexible for different languages or wording it differently.
    svn path=/trunk/e107/; revision=593

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -5,6 +5,6 @@ define("CHARSET", "iso-8859-1");
 define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 
 //v.616
-define("CORE_LAN2","Originally posted by");
+define("CORE_LAN2"," \\1 wrote:");// "\\1" represents the username.
 define("CORE_LAN3","file attachment disabled");
 ?>
