170922340196be21b8ccff7527bd556a7863424f
chavo <chavo@localhost>
2004-06-18 00:48:03 +0000

    Dev Bugtracker #92. Banlist reason didn't show up anywhere. Added ability to edit banlist.
    svn path=/trunk/e107/; revision=683

--- e107_languages/English/admin/lan_banlist.php
+++ e107_languages/English/admin/lan_banlist.php
@@ -4,13 +4,16 @@ define("BANLAN_1", "Ban removed.");
 define("BANLAN_2", "No bans.");
 define("BANLAN_3", "Existing Bans");
 define("BANLAN_4", "Remove ban");
-define("BANLAN_5", "Ban by IP");
+define("BANLAN_5", "Enter IP or Email Address");
 define("BANLAN_6", "Ban by email address"); 
 define("BANLAN_7", "Reason");
 define("BANLAN_8", "Ban User");
 define("BANLAN_9", "Ban users from site");
 
-define("BANLAN_10", "IP / Email");
+define("BANLAN_10", "IP / Email / Reason");
 define("BANLAN_11", "Options");
+define("BANLAN_12", "Edit");
+define("BANLAN_13", "Update Ban");
+define("BANLAN_14", "Ban Updated");
 
 ?>
