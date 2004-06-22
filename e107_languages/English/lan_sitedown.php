9ecb764d60b50fbd519c489ac6e4f30ee277bea7
loloirie <loloirie@localhost>
2004-06-22 08:53:43 +0000

    title (bug #216)
    svn path=/trunk/e107/; revision=691

--- e107_languages/English/lan_sitedown.php
+++ e107_languages/English/lan_sitedown.php
@@ -1,5 +1,5 @@
 <?php
-
+define("PAGE_NAME", SITENAME." temporarily closed"); 
 define("LAN_00", "<b>- Temporarily Closed -</b><br /><br />We have temporarily closed the site for some essential maintainance. This shouldn't take too long - please check back soon, apologies for the inconvenience.");
 
 ?>
