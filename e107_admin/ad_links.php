f575540e4205f9fdccedffbdb0bbd948a925f92a
loloirie <loloirie@localhost>
2004-07-12 11:34:34 +0000

    Links for admin frontpage and admin header extracted from main code + 1 new parameter for links for the frontpage + new admin frontpage
    svn path=/trunk/e107/; revision=714

---
+++ e107_admin/ad_links.php
@@ -0,0 +1,45 @@
+<?php
+
+// Info about attributes
+/*
+attribute 1 = link
+attribute 2 = title
+attribute 3 = description
+attribute 4 = perms
+attribute 5 = category
+*/
+
+$array_functions = array(
+        0 => array(e_ADMIN."administrator.php", ADLAN_8, ADLAN_9, "3", 2),
+        1 => array(e_ADMIN."updateadmin.php", ADLAN_10, ADLAN_11, "", 2),
+        2 => array(e_ADMIN."article.php", ADLAN_14, ADLAN_15, "J", 3),
+        3 => array(e_ADMIN."banlist.php", ADLAN_34, ADLAN_35, "4", 2),
+        4 => array(e_ADMIN."banner.php", ADLAN_54, ADLAN_55, "D", 6),
+        5 => array(e_ADMIN."cache.php", ADLAN_74, ADLAN_75, "0", 1),
+        6 => array(e_ADMIN."chatbox.php", ADLAN_56, ADLAN_57, "C", 4),
+        7 => array(e_ADMIN."content.php", ADLAN_16, ADLAN_17, "L", 3),
+        8 => array(e_ADMIN."custommenu.php", ADLAN_42, ADLAN_43, "2", 6),
+        9 => array(e_ADMIN."db.php",ADLAN_44, ADLAN_45,"0", 1),
+        10 => array(e_ADMIN."download.php", ADLAN_24, ADLAN_25, "R", 5),
+        11 => array(e_ADMIN."emoticon.php", ADLAN_58, ADLAN_59, "F", 6),
+        12 => array(e_ADMIN."filemanager.php", ADLAN_30, ADLAN_31, "6", 5),
+        13 => array(e_ADMIN."forum.php", ADLAN_12, ADLAN_13, "5", 4),
+        14 => array(e_ADMIN."frontpage.php", ADLAN_60, ADLAN_61, "G", 1),
+        15 => array(e_ADMIN."image.php", ADLAN_105, ADLAN_106, "5", 6),
+        16 => array(e_ADMIN."links.php", ADLAN_20, ADLAN_21, "I", 3),
+        17 => array(e_ADMIN."wmessage.php", ADLAN_28, ADLAN_29, "M", 3),
+        18 => array(e_ADMIN."log.php", ADLAN_64, ADLAN_65, "S", 1),
+        19 => array(e_ADMIN."ugflag.php", ADLAN_40, ADLAN_41, "9", 1),
+        20 => array(e_ADMIN."menus.php", ADLAN_6, ADLAN_7, "2", 1),
+        21 => array(e_ADMIN."meta.php", ADLAN_66, ADLAN_67, "T", 1),
+        22 => array(e_ADMIN."newspost.php", ADLAN_0, ADLAN_1, "H", 3),
+        23 => array(e_ADMIN."newsfeed.php", ADLAN_62, ADLAN_63, "E", 6),
+        24 => array(e_ADMIN."phpinfo.php", ADLAN_68, ADLAN_69, "0", 6),
+        25 => array(e_ADMIN."poll.php", ADLAN_70, ADLAN_71, "U", 6),
+        26 => array(e_ADMIN."prefs.php", ADLAN_4, ADLAN_5, "1", 1),
+        27 => array(e_ADMIN."upload.php", ADLAN_72, ADLAN_73, "V", 5),
+        28 => array(e_ADMIN."review.php", ADLAN_18, ADLAN_19, "K", 3),
+        29 => array(e_ADMIN."users.php", ADLAN_36, ADLAN_37, "4", 2),
+        30 => array(e_ADMIN."userclass2.php", ADLAN_38, ADLAN_39, "4", 2)
+);
+?>
