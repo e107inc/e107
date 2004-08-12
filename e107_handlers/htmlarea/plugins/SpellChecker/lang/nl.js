dd8a180eecf5ba1696fd668aac63c9929aae4721
e107coders <e107coders@localhost>
2004-08-12 14:32:27 +0000

    - extra Htmlarea files added
    svn path=/trunk/e107/; revision=786

---
+++ e107_handlers/htmlarea/plugins/SpellChecker/lang/nl.js
@@ -0,0 +1,39 @@
+// I18N constants
+
+// LANG: "nl", ENCODING: UTF-8 | ISO-8859-1
+// Author: A.H van den Broek  http://www.kontaktfm.nl
+// Email : tonbroek@kontaktfm.nl
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+SpellChecker.I18N = {
+	"CONFIRM_LINK_CLICK"                    : "Weet u zeker dat u deze link wilt openen?",
+	"Cancel"                                : "Annuleer",
+	"Dictionary"                            : "Woordenboek",
+	"Finished list of mispelled words"      : "klaar met de lijst van fouten woorden",
+	"I will open it in a new page."         : "Ik zal het in een nieuwe pagina openen.",
+	"Ignore all"                            : "alles overslaan",
+	"Ignore"                                : "Overslaan",
+	"NO_ERRORS"                             : "Geen fouten gevonden met dit woordenboek.",
+	"NO_ERRORS_CLOSING"                     : "Spell checking is klaar, geen fouten gevonden.  spell checking word gesloten...",
+	"OK"                                    : "OK",
+	"Original word"                         : "Originele woord",
+	"Please wait.  Calling spell checker."  : "Even wachten. spell checker wordt geladen.",
+	"Please wait: changing dictionary to"   : "even wachten: woordenboek wordt veranderd naar",
+	"QUIT_CONFIRMATION"                     : "Dit zal alle veranderingen annuleren en de spell checker sluiten.  Weet u het zeker?",
+	"Re-check"                              : "Opnieuw",
+	"Replace all"                           : "Alles vervangen",
+	"Replace with"                          : "Vervangen met",
+	"Replace"                               : "Vervangen",
+	"Revert"                                : "Omkeren",
+	"SC-spell-check"                        : "Spell-check",
+	"Suggestions"                           : "Suggestie",
+	"pliz weit ;-)"                         : "Even wachten ;-)"
+};
