a9aa13d371528a289a4e703627f52923232880d7
mcfly_e107 <mcfly_e107@localhost>
2004-08-20 00:04:31 +0000

    Admin function list is now sorted in multilanguage (dev bugtracker #119)
    svn path=/trunk/e107/; revision=813

--- e107_admin/header.php
+++ e107_admin/header.php
@@ -2,7 +2,7 @@
 /*
 +---------------------------------------------------------------+
 |        e107 website system
-|        /admin/header.php
+|        /e107_admin/header.php
 |
 |        ©Steve Dunstan 2001-2002
 |        http://e107.org
@@ -10,6 +10,11 @@
 |
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
+|
+|   $Source: /cvs_backup/e107/e107_admin/header.php,v $
+|   $Revision: 1.21 $
+|   $Date: 2004-08-20 00:04:31 $
+|   $Author: mcfly_e107 $
 +---------------------------------------------------------------+
 */
 echo (defined("STANDARDS_MODE") ? "" : "<?xml version='1.0' encoding='iso-8859-1' ?>");
@@ -45,15 +50,20 @@ echo "<div style='text-align:center'>
 <img src='".e_IMAGE."adminlogo.png' alt='Logo' />
 <br />";
 
-if(ADMIN == TRUE){
-        $str = str_replace(".", "", ADMINPERMS);
-        if(ADMINPERMS == "0"){
-                echo ADLAN_48.": ".ADMINNAME." (".ADLAN_49.")";
-        }else{
-                echo ADLAN_48.": ".ADMINNAME." (".ADLAN_50.":  ".$str.")";
-        }
-}else{
-        echo ADLAN_51." ...";
+if(ADMIN == TRUE)
+{
+	$str = str_replace(".", "", ADMINPERMS);
+	if(ADMINPERMS == "0"){
+		echo ADLAN_48.": ".ADMINNAME." (".ADLAN_49.")";
+	}
+	else
+	{
+		echo ADLAN_48.": ".ADMINNAME." (".ADLAN_50.":  ".$str.")";
+	}
+}
+else
+{
+	echo ADLAN_51." ...";
 }
 
 $adminfpage = (!$pref['adminstyle'] || $pref['adminstyle'] == "default" ? "admin.php" : $pref['adminstyle'].".php");
@@ -66,92 +76,92 @@ echo "
 
 if(ADMIN == TRUE){
 
-        if(!strstr(e_SELF, "/".$adminfpage) || strstr(e_SELF, "/".$adminfpage."?")){
-                                $e107_var['x']['text']=ADLAN_52;
-                                $e107_var['x']['link']=e_ADMIN.$adminfpage;
+	if(!strstr(e_SELF, "/".$adminfpage) || strstr(e_SELF, "/".$adminfpage."?")){
+		$e107_var['x']['text']=ADLAN_52;
+		$e107_var['x']['link']=e_ADMIN.$adminfpage;
 
-                                $e107_var['y']['text']=ADLAN_53;
-                                $e107_var['y']['link']=e_BASE."index.php";
+		$e107_var['y']['text']=ADLAN_53;
+		$e107_var['y']['link']=e_BASE."index.php";
 
-    //    $text = "<a href='".e_ADMIN.$adminfpage."'>".ADLAN_52."</a><br /><a href='".e_BASE."index.php'>".ADLAN_53."</a><br /><br />";
-    //     $text ="<div style='text-align:center'>";
-                        $text .= show_admin_menu("",$act,$e107_var)."<br />";
-//         $text .= "<input type='button' class='button' style='width:100%' onClick=\"document.location='".e_ADMIN.$adminfpage."'\" value='".ADLAN_52."' /><br />
-//         <input type='button' class='button' style='width:100%' onClick=\"document.location='".e_BASE."index.php'\" value='".ADLAN_53."' /><br /><br />";
+		$text .= show_admin_menu("",time(),$e107_var)."<br />";
 
+		require_once("header_links.php");
+		$text .= get_admin_treemenu(ADLAN_93,time(),$e107_var,TRUE);
+		unset($e107_var);
 
-                require_once("header_links.php");
+		// Plugin links menu
 
-                $text .= get_admin_treemenu(ADLAN_93,$act,$e107_var);
+		$sql2 = new db;
+		if($sql2 -> db_Select("plugin", "*", "plugin_installflag=1"))
+		{
+			while($row = $sql2 -> db_Fetch())
+			{
+				extract($row);
+				include(e_PLUGIN.$plugin_path."/plugin.php");
 
+				//Link Plugin Manager
+				$e107_var['x']['text'] = "<b>".ADLAN_98."</b>";
+				$e107_var['x']['link'] = e_ADMIN."plugin.php";
+				$e107_var['x']['perm'] = "P";
 
+				// Links Plugins
+				if($eplug_conffile)
+				{
+					$e107_var['x'.$plugin_id]['text'] = $eplug_caption;
+					$e107_var['x'.$plugin_id]['link'] = e_PLUGIN.$plugin_path."/".$eplug_conffile;
+					$e107_var['x'.$plugin_id]['perm'] = "P".$plugin_id;
+				}
+				unset($eplug_conffile, $eplug_name, $eplug_caption);
+			}
+			$text .= get_admin_treemenu(ADLAN_95,time(),$e107_var,TRUE);
+			unset($e107_var);
+		}
+		unset($e107_var);
+		$e107_var['x']['text']=ADLAN_46;
+		$e107_var['x']['link']=e_ADMIN."admin.php?logout";
+		$text .= "<br />".show_admin_menu("",$act,$e107_var);
+		$ns -> tablerender(LAN_head_1, $text);
 
-                unset($e107_var);
+	}
+	else
+	{
+		$text = "<div style='text-align:center'>";
+		unset($e107_var);
+		$e107_var['x']['text']=ADLAN_53;
+		$e107_var['x']['link']=e_ADMIN."../index.php";
+		$text .= show_admin_menu("",$act,$e107_var);
+		$text  .="</div>";
+		$ns -> tablerender(LAN_head_1, $text);
+		unset($text);
+	}
 
-                // Plugin links menu
-
-                $sql2 = new db;
-                if($sql2 -> db_Select("plugin", "*", "plugin_installflag=1")){
-                        while($row = $sql2 -> db_Fetch()){
-                                extract($row);
-                                include(e_PLUGIN.$plugin_path."/plugin.php");
-
-                                //Link Plugin Manager
-                                $e107_var['x']['text'] = "<b>".ADLAN_98."</b>";
-                                $e107_var['x']['link'] = e_ADMIN."plugin.php";
-                                $e107_var['x']['perm'] = "P";
-
-                                // Links Plugins
-                                if($eplug_conffile){
-                        $e107_var['x'.$plugin_id]['text'] = $eplug_caption;
-                                        $e107_var['x'.$plugin_id]['link'] = e_PLUGIN.$plugin_path."/".$eplug_conffile;
-                                        $e107_var['x'.$plugin_id]['perm'] = "P".$plugin_id;
-                                }
-                                unset($eplug_conffile, $eplug_name, $eplug_caption);
-                        }
-                        $text .= get_admin_treemenu(ADLAN_95,$act,$e107_var);
-                        unset($e107_var);
-                }
-                unset($e107_var);
-               $e107_var['x']['text']=ADLAN_46;
-               $e107_var['x']['link']=e_ADMIN."admin.php?logout";
-               $text .= "<br />".show_admin_menu("",$act,$e107_var);
-      $ns -> tablerender(LAN_head_1, $text);
-
- }else{
-
-     $text = "<div style='text-align:center'>";
-                unset($e107_var);
-                $e107_var['x']['text']=ADLAN_53;
-                $e107_var['x']['link']=e_ADMIN."../index.php";
-                $text .= show_admin_menu("",$act,$e107_var);
-     $text  .="</div>";
-     $ns -> tablerender(LAN_head_1, $text);
-        unset($text);
- }
-
-if(ADMINPERMS == "0"){
-        if((ADMINPWCHANGE+2592000) < time()){
-                $text = "<div style='mediumtext; text-align:center'>".ADLAN_102." <a href='".e_ADMIN."updateadmin.php'>".ADLAN_103."</a></div>";
-                $ns -> tablerender(ADLAN_104, $text);
-        }
+if(ADMINPERMS == "0")
+{
+	if((ADMINPWCHANGE+2592000) < time())
+	{
+		$text = "<div style='mediumtext; text-align:center'>".ADLAN_102." <a href='".e_ADMIN."updateadmin.php'>".ADLAN_103."</a></div>";
+		$ns -> tablerender(ADLAN_104, $text);
+	}
 }
 
 $handle=opendir(e_ADMIN."help/");
-        $text = "";
-        while(false !== ($file = readdir($handle))){
-                if($file != "." && $file != ".." && $file != "CVS"){
-                         if(eregi($file, e_SELF)){
-                                @require_once("help/".$file);
-                         }
-                }
-        }
-        closedir($handle);
+$text = "";
+while(false !== ($file = readdir($handle)))
+{
+	if($file != "." && $file != ".." && $file != "CVS")
+	{
+		if(eregi($file, e_SELF))
+		{
+			@require_once("help/".$file);
+		}
+	}
+}
+closedir($handle);
 }
 
 $plugpath = e_PLUGIN.substr(strrchr(substr(e_SELF, 0, strrpos(e_SELF, "/")), "/"), 1)."/help.php";
 if(file_exists($plugpath)){
-        @require_once($plugpath);
+	@require_once($plugpath);
 }
 
 echo "<br />";
@@ -170,48 +180,76 @@ echo "</td>
 <td style='width:60%; vertical-align: top;'>";
 
 function show_admin_menu($title,$page,$e107_vars){
-        global $ns;
-        $text = "<div style='text-align:center; width:100%'><table class='fborder' style='width:98%;'>";
-        foreach(array_keys($e107_vars) as $act){
-                $pre = "";
-                $post = "";
-                if($page == $act){
-                        $pre = "<b>&laquo;&nbsp;";
-                        $post = "&nbsp;&raquo;</b>";
-                }
-                $t=str_replace(" ","&nbsp;",$e107_vars[$act]['text']);
-                if(!$e107_vars[$act]['perm'] || getperms($e107_vars[$act]['perm'])){
-                        $text .= "<tr><td class='button'><div style='width:100%; text-align:center'><a style='cursor:hand; cursor:pointer; text-decoration:none;' href='{$e107_vars[$act]['link']}'>{$pre}{$t}{$post}</a></div></td></tr>";
-                }
-        }
-        $text .= "</table></div>";
-        if($title==""){
-                return $text;
-        }
-        $ns -> tablerender($title,$text);
+	global $ns;
+	$text = "<div style='text-align:center; width:100%'><table class='fborder' style='width:98%;'>";
+	foreach(array_keys($e107_vars) as $act)
+	{
+		$pre = "";
+		$post = "";
+		if($page == $act){
+			$pre = "<b>&laquo;&nbsp;";
+			$post = "&nbsp;&raquo;</b>";
+		}
+		$t=str_replace(" ","&nbsp;",$e107_vars[$act]['text']);
+		if(!$e107_vars[$act]['perm'] || getperms($e107_vars[$act]['perm']))
+		{
+			$text .= "<tr><td class='button'><div style='width:100%; text-align:center'><a style='cursor:hand; cursor:pointer; text-decoration:none;' href='{$e107_vars[$act]['link']}'>{$pre}{$t}{$post}</a></div></td></tr>";
+		}
+	}
+	$text .= "</table></div>";
+	if($title=="")
+	{
+		return $text;
+	}
+	$ns -> tablerender($title,$text);
 }
 
-function get_admin_treemenu($title,$page,$e107_vars){
-        global $ns;
-        $idtitle="yop_".str_replace(" ","",$title);
-        $text = "<div style='text-align:center; width:100%'><table class='fborder' style='width:100%;'>";
-        $text .= "<tr><td class='button'><a style='text-align:center; cursor:hand; cursor:pointer; text-decoration:none;' onclick=\"expandit('{$idtitle}');\" >{$title}</a></td></tr>";
-        $text .= "<tr id=\"{$idtitle}\" style=\"display: none;\" ><td class='forumheader3' style='text-align:left;'>";
-        foreach(array_keys($e107_vars) as $act){
-                $pre = "";
-                $post = "";
-                if($page == $act){
-                        $pre = "<b> &laquo; ";
-                        $post = " &raquo; </b>";
-                }
-                if(!$e107_vars[$act]['perm'] || getperms($e107_vars[$act]['perm'])){
-                        $text .= "{$pre}<a style='text-decoration:none;' href='{$e107_vars[$act]['link']}'>{$e107_vars[$act]['text']}</a>{$post}<br />";
-                }
-        }
+function get_admin_treemenu($title,$page,$e107_vars,$sortlist=FALSE)
+{
+	global $ns;
 
-        $text .= "</td></tr>";
-        $text .= "</table></div>";
-        return $text;
-//        $ns -> tablerender($title,$text);
+	if($sortlist == TRUE)
+	{
+		$temp = $e107_vars;
+		unset($e107_vars);
+		foreach(array_keys($temp) as $key)
+		{
+			$func_list[]=$temp[$key]['text'];
+		}
+		sort($func_list);
+		foreach($func_list as $func_text)
+		{
+			foreach(array_keys($temp) as $key)
+			{
+				if($temp[$key]['text'] == $func_text)
+				{
+					$e107_vars[] = $temp[$key];
+				}
+			}
+		}
+	}
+	
+	$idtitle="yop_".str_replace(" ","",$title);
+	$text = "<div style='text-align:center; width:100%'><table class='fborder' style='width:100%;'>";
+	$text .= "<tr><td class='button'><a style='text-align:center; cursor:hand; cursor:pointer; text-decoration:none;' onclick=\"expandit('{$idtitle}');\" >{$title}</a></td></tr>";
+	$text .= "<tr id=\"{$idtitle}\" style=\"display: none;\" ><td class='forumheader3' style='text-align:left;'>";
+	foreach(array_keys($e107_vars) as $act)
+	{
+		$pre = "";
+		$post = "";
+		if($page == $act)
+		{
+			$pre = "<b> &laquo; ";
+			$post = " &raquo; </b>";
+		}
+		if(!$e107_vars[$act]['perm'] || getperms($e107_vars[$act]['perm']))
+		{
+			$text .= "{$pre}<a style='text-decoration:none;' href='{$e107_vars[$act]['link']}'>{$e107_vars[$act]['text']}</a>{$post}<br />";
+		}
+	}
+
+	$text .= "</td></tr>";
+	$text .= "</table></div>";
+	return $text;
 }
 ?>
