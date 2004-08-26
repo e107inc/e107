d81be22ba0d5249b036bb27fd52cb26c34c11020
mcfly_e107 <mcfly_e107@localhost>
2004-08-26 01:55:46 +0000

    Added language phrases to clock_menu
    svn path=/trunk/e107/; revision=818

--- e107_plugins/clock_menu/config.php
+++ e107_plugins/clock_menu/config.php
@@ -16,13 +16,16 @@ require_once("../../class2.php");
 if(!getperms("1")){ header("location:".e_BASE."index.php"); exit ;}
 require_once(e_ADMIN."auth.php");
 
+@include_once(e_PLUGIN."clock_menu/languages/".e_LANGUAGE.".php");
+@include_once(e_PLUGIN."clock_menu/languages/English.php");
+
 if(IsSet($_POST['update_menu'])){
 	while(list($key, $value) = each($_POST)){
-		if($value != "Update Menu Settings"){ $menu_pref[$key] = $value; }
+		if($key != "update_menu"){ $menu_pref[$key] = $value; }
 	}
 	$tmp = addslashes(serialize($menu_pref));
 	$sql -> db_Update("core", "e107_value='$tmp' WHERE e107_name='menu_pref' ");
-	$ns -> tablerender("", "<div style=\"text-align:center\"><b>Comments menu configuration saved</b></div>");
+	$ns -> tablerender("", "<div style=\"text-align:center\"><b>".CLOCK_MENU_L1."</b></div>");
 }
 
 $text = "<div style='text-align:center'>
@@ -30,7 +33,7 @@ $text = "<div style='text-align:center'>
 <table style=\"width:85%\" class=\"fborder\">
 
 <tr>
-<td style=\"width:40%\" class='forumheader3'>Caption: </td>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_MENU_L2.": </td>
 <td style=\"width:60%\" class='forumheader3'>
 <input class=\"tbox\" type=\"text\" name=\"clock_caption\" size=\"20\" value=\"".$menu_pref['clock_caption']."\" maxlength=\"100\" />
 </td>
@@ -38,12 +41,12 @@ $text = "<div style='text-align:center'>
 
 <tr style=\"vertical-align:top\"> 
 <td colspan=\"2\"  style=\"text-align:center\" class='forumheader'>
-<input class=\"button\" type=\"submit\" name=\"update_menu\" value=\"Update menu Settings\" />
+<input class=\"button\" type=\"submit\" name=\"update_menu\" value=\"".CLOCK_MENU_L3."\" />
 </td>
 </tr>
 </table>
 </form>
 </div>";
-$ns -> tablerender("Clock Menu Config", $text);
+$ns -> tablerender(CLOCK_MENU_L4, $text);
 require_once(e_ADMIN."footer.php");
 ?>
