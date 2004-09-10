859ad7a6d0074a6b32d4b5bd1286e6918fccab38
loloirie <loloirie@localhost>
2004-09-10 09:41:21 +0000

    Change label for custom menus/pages
    svn path=/trunk/e107/; revision=878

--- e107_languages/English/admin/lan_admin.php
+++ e107_languages/English/admin/lan_admin.php
@@ -42,7 +42,7 @@ define("ADLAN_38", "User Classes");
 define("ADLAN_39", "Create/edit user classes");
 define("ADLAN_40", "Maintenance");
 define("ADLAN_41", "Take site down for maintenance");
-define("ADLAN_42", "Custom Menus");
+define("ADLAN_42", "Custom Menus/Pages");
 define("ADLAN_43", "Create custom menu items");
 define("ADLAN_44", "Database");
 define("ADLAN_45", "Database utilities");
