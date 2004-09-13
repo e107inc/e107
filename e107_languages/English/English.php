3193311d43ba50e74f36eeb3476c2095587562df
loloirie <loloirie@localhost>
2004-09-13 09:46:12 +0000

    Back to iso, waiting for a better support of utf-8 by browsers and full recoding of headers by e107
    svn path=/trunk/e107/; revision=884

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -3,7 +3,7 @@
 setlocale(LC_ALL, 'en');
 define("CORE_LC", 'en');
 define("CORE_LC2", 'gb');
-define("CHARSET", "utf-8");
+define("CHARSET", "iso-8859-1");
 define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 
 //v.616
