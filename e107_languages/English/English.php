09669bae2f40e4ee61b4c93dd99556f1ca8f832d
e107coders <e107coders@localhost>
2004-09-17 23:35:56 +0000

    removed useless file
    svn path=/trunk/e107/; revision=889

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -3,7 +3,8 @@
 setlocale(LC_ALL, 'en');
 define("CORE_LC", 'en');
 define("CORE_LC2", 'gb');
-define("CHARSET", "iso-8859-1");
+// define("CHARSET", "iso-8859-1");
+define("CHARSET", "utf-8");
 define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 
 //v.616
