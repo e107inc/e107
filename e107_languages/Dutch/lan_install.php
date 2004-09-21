b6f2458bed1aad892f5933732d90c66df4b3ef88
e107coders <e107coders@localhost>
2004-09-21 19:58:20 +0000

    *** empty log message ***
    svn path=/trunk/e107_0.7/; revision=899

---
+++ e107_languages/Dutch/lan_install.php
@@ -0,0 +1,98 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website system
+|        lan_install.php Dutch-utf language file 
+|
+|        ©Steve Dunstan 2001-2002
+|        http://e107.org
+|        jalist@e107.org
+|
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
+|        Translation Updated by: koot on the 13th Sep 2004
++---------------------------------------------------------------+
+*/
+
+define("INSLAN1", "Installatie Stadium");
+define("INSLAN2", "PHP / mySQL versie controle / bestandsrechten controle");
+define("INSLAN3", "PHP Versie");
+define("INSLAN4", "Mislukt");
+define("INSLAN5", "Je gebruikt een versie van PHP die niet in combinatie met e107 draait");
+define("INSLAN6", "Script gepauseerd.");
+define("INSLAN7", "Geslaagd");
+define("INSLAN8", "mySQL Versie");
+define("INSLAN9", "e107 was niet in staat het mySQL versie nummer te achterhalen.");
+define("INSLAN10", "Bestandsrechten");
+define("INSLAN11", "is niet beschrijfbaar");
+define("INSLAN12", "mappen zijn niet beschrijfbaar");
+define("INSLAN13", "Controleer of de rechten voor de bestanden zoals hierboven getoond correct op de server&lt;br /&gt;zijn ingesteld, de rechten dienen als 777 ingesteld te worden. Om de rechten&lt;br /&gt; in te stellen, kun je rechtsklikken op het bestand in uw FTP programma en kiezen voor Chmod of&lt;br /&gt;Zet bestandsrechten, typ dan 777 in, indien het dialoog dat verschijnt vinkhokjes heeft,&lt;br /&gt;vink alle hokjes aan.&lt;br /&gt;&lt;br /&gt;Probeer het nogmaals na het correct instellen van de bestandsrechten.");
+define("INSLAN14", "e107 installeren");
+define("INSLAN15", "FATAAL PROBLEEM");
+define("INSLAN16", "Ondanks dat het niet mogelijk was de status van uw mySQL installatie te achterhalen,&lt;br /&gt;kun je door naar de volgende stap.");
+define("INSLAN17", "Ga door");
+define("INSLAN18", "Herhaal controle bestandsrechten");
+define("INSLAN19", "Alle server testen zijn geslaagd, klik op de knop om verder te gaan naar het volgende stadium");
+define("INSLAN20", "mySQL informatie");
+define("INSLAN21", "Vul je mySQL instellingen hier in.&lt;br /&gt;Indien je root rechten hebt, kunt je een nieuwe database aanmaken door het hokje aan te vinken, anders moet je een database maken of een reeds bestaande database gebruiken. &lt;br /&gt;Als je slechts &eacute;&eacute;n database hebt, kun je een voorvoegsel gebruiken zodat andere scripts dezelfde database kunnen delen.&lt;br /&gt;Als je je mySQL instellingen niet weet, kun je je webbeheerder benaderen.");
+define("INSLAN22", "mySQL Server");
+define("INSLAN23", "mySQL Gebruikersnaam");
+define("INSLAN24", "mySQL wachtwoord");
+define("INSLAN25", "mySQL Database");
+define("INSLAN26", "Tabel voorvoegsel");
+define("INSLAN27", "Probleem");
+define("INSLAN28", "Probleem ontdekt");
+define("INSLAN29", "Je hebt vereiste velden niet ingevuld, vul opnieuw alle mySQL gegevens in");
+define("INSLAN30", "e107 was niet in staat een connectie te maken met mySQL volgens de ingestelde gegevens.&lt;br /&gt;Keer terug naar de vorige pagina and controleer of de gegevens correct zijn");
+define("INSLAN31", "mySQL controle");
+define("INSLAN32", "Connectie met mySQL gemaakt en gecontroleerd.");
+define("INSLAN33", "Poging de database aan te maken");
+define("INSLAN34", "Niet in staat de database te maken, controleer of je de juiste rechten hebt om databases op je server aan te maken.");
+define("INSLAN35", "Database succesvol aangemaakt.");
+define("INSLAN36", "Klik op de knop om verder te gaan naar het volgende stadium.");
+define("INSLAN37", "Terug naar laatste pagina");
+define("INSLAN38", "Beheerdersinformatie");
+define("INSLAN39", "Vul de gebruikersnaam, het wachtwoord en het e-mailadres van je hoofdbeheerder in.&lt;br /&gt;Deze gegevens zullen gebruikt worden om toegang te krijgen tot het beheerdersgedeelte van uw website.&lt;br /&gt;Let op dat je je gebruikersnaam en wachtwoord opschrijft en op een veilige plaats bewaart, &lt;br /&gt;aangezien ze niet meer zijn te achterhalen wanneer je deze kwijtraakt.");
+define("INSLAN40", "Naam Beheerder");
+define("INSLAN41", "Wachtwoord Beheerder");
+define("INSLAN42", "Bevestig wachtwoord");
+define("INSLAN43", "E-mailadres Beheerder");
+define("INSLAN44", "Je hebt vereiste velden niet ingevuld, vul de gegevens opnieuw in.");
+define("INSLAN45", "De twee wachtwoorden zijn niet gelijk, vul deze opnieuw in.");
+define("INSLAN46", "is geen geldig e-mailadres, vul deze opnieuw in.");
+define("INSLAN47", "Alles ingesteld!");
+define("INSLAN48", "e107 heeft nu alle informatie om de installatie af te maken.&lt;br /&gt;klik op de knop om de tabellen van de database te maken en alle instellingen op te slaan.");
+define("INSLAN49", "e107 was niet in staat het hoofd configuratiebestand op je server te bewaren&lt;br /&gt;Controleer of het &lt;b&gt;e107_config.php&lt;/b&gt; bestand de juiste bestandsrechten heeft");
+define("INSLAN50", "Installatie Voltooid!");
+define("INSLAN51", "Alles voltooid");
+define("INSLAN52", "e107 is succesvol ge&iuml;nstalleerd!&lt;br /&gt;Voor veiligheidsredenen moet je de bestandsrechten van het&lt;br /&gt;&lt;b&gt;e107_config.php&lt;/b&gt; bestand terug te zetten naar 644.&lt;br /&gt;Ook moet je het /install.php bestand van ja server te verwijderen nadat je op de onderstaande knop hebt geklikt");
+define("INSLAN53", "Klik hier om je nieuwe website te bekijken!");
+define("INSLAN54", "Niet in staat het sql databestand te lezen&lt;br /&gt;&lt;br /&gt;Controleer of het bestand &lt;b&gt;core_sql.php&lt;/b&gt; bestaat in de &lt;b&gt;/e107_admin/sql&lt;/b&gt; map.");
+define("INSLAN55", "e107 was niet in staat alle vereiste tabellen te cre&euml;ren.&lt;br /&gt;Maak de database leeg en herstel de problemen voordat je het opnieuw probeert.");
+define("INSLAN56", "Welkom op je nieuwe website!");
+define("INSLAN57", "e107 is succesvol ge&iuml;nstalleerd en is nu klaar om gebruikt te worden.");
+define("INSLAN58", "Je kunt de FAQ en documentatie hier vinden.");
+define("INSLAN59", "Bedankt dat je e107 wilt gebruiken, wij hopen dat het de behoefte voor je website vervultn(je kunt dit bericht verwijderen in het beheerdersgedeelte.)");
+define("INSLAN60", "aanvinken om te maken");
+define("INSLAN61", "map");
+define("INSLAN62", "of");
+define("INSLAN63", "Bestandsrechten probleem");
+define("INSLAN64", "Dit bestand is gegenereerd door het installatiescript.");
+define("INSLAN65", "e107 vereist minimaal versie 4.1.0");
+define("INSLAN66", "Als je een lokale server op je computer gebruikt, zal je ");
+define("INSLAN67", "je PHP-versie  moeten vernieuwen om verder te gaan, zie");
+define("INSLAN68", "voor instructies. Als je ");
+define("INSLAN69", "e107 probeert te installeren op een hosted server, zul je de");
+define("INSLAN70", "beheerders van de server moeten benaderen en vragen of zij de versie van PHP voor je willen vernieuwen.");
+define("INSLAN71", "Start dit script opnieuw nadat je de versie van PHP heeft vernieuwd.");
+define("INSLAN72", "Dit kan betekenen dat mySQL niet is ge&iuml;nstalleerd of niet draait, of");
+define("INSLAN73", "je gebruikt een versie die het versie nummer niet correct toont");
+define("INSLAN74", "(v5.x heeft dit probleem). Als de volgende stap van de installatie");
+define("INSLAN75", "mislukt, moet je de status van mySQL te controleren.");
+define("INSLAN76", "Je beheerscherm is");
+define("INSLAN77", "hier");
+define("INSLAN78", "klik om daar nu naar toe te gaan. Je zult moeten inloggen met de gebruikersnaam en het wachtwoord dat je tijdens de installatie hebt opgegeven.");
+
+
+?>
