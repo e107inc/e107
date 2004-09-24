91bc998cb85f192e3432250a004c81c7b565f71f
loloirie <loloirie@localhost>
2004-09-24 10:37:47 +0000

    Allow multilanguage (Thx Jens)
    svn path=/trunk/e107_0.7/; revision=909

--- e107_plugins/clock_menu/clock_menu.php
+++ e107_plugins/clock_menu/clock_menu.php
@@ -1,39 +1,49 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
++---------------------------------------------------------------+
+|	e107 Clock Menu
+|	/clock_menu.php
 |
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.7/e107_plugins/clock_menu/clock_menu.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:12:07 $
-|     $Author: e107coders $
-+----------------------------------------------------------------------------+
+|	Compatible with the e107 content management system
+|		http://e107.org
+|	
+|	Originally written by jalist, modified for greater 
+|	detail and cross browser compatiblity by Caveman
+|	Last modified 19:11 08/04/2003
+|	
+|	Works with Mozilla 1.x, NS6, NS7, IE5, IE5.5, Opera 7
+|	
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
 */
+$ec_dir = e_PLUGIN."clock_menu/";
+$lan_file = $ec_dir."languages/".e_LANGUAGE.".php";
+include(file_exists($lan_file) ? $lan_file : e_PLUGIN."calendar_menu/languages/English.php");
 if(!defined("e_HTTP")){exit;}
 $text = "\n\n<!-- ### clock ### //-->\n<div id='Clock'>&nbsp;</div>\n";
 if(!$clock_flat){
-        $ns -> tablerender($menu_pref['clock_caption'], "<div style='text-align:center'>".$text."</div>");
+	$ns -> tablerender($menu_pref['clock_caption'], "<div style='text-align:center'>".$text."</div>");
 }else{
-        echo $text;
+	echo $text;
 }
 
 ?>
 <script type="text/javascript">
 <!--
 var DayNam = new Array(
-"<?php echo isset($LAN_407)?$LAN_407:"Sunday"; ?>","<?php echo isset($LAN_401)?$LAN_401:"Monday"; ?>","<?php echo isset($LAN_402)?$LAN_402:"Tuesday"; ?>","<?php echo isset($LAN_403)?$LAN_403:"Wednesday"; ?>","<?php echo isset($LAN_404)?$LAN_404:"Thursday"; ?>","<?php echo isset($LAN_405)?$LAN_405:"Friday"; ?>","<?php echo isset($LAN_406)?$LAN_406:"Saturday"; ?>");
+"<?php echo isset($LAN_407)?$LAN_407:"".CLOCK_MENU_L11; ?>","<?php echo isset($LAN_401)?$LAN_401:"".CLOCK_MENU_L5; ?>","<?php echo isset($LAN_402)?$LAN_402:"".CLOCK_MENU_L6; ?>","<?php echo isset($LAN_403)?$LAN_403:"".CLOCK_MENU_L7; ?>","<?php echo isset($LAN_404)?$LAN_404:"".CLOCK_MENU_L8; ?>","<?php echo isset($LAN_405)?$LAN_405:"".CLOCK_MENU_L9; ?>","<?php echo isset($LAN_406)?$LAN_406:"".CLOCK_MENU_L10; ?>");
 var MnthNam = new Array(
-"<?php echo isset($LAN_411)?$LAN_411:"January"; ?>","<?php echo isset($LAN_412)?$LAN_412:"February"; ?>","<?php echo isset($LAN_413)?$LAN_413:"March"; ?>","<?php echo isset($LAN_414)?$LAN_414:"April"; ?>","<?php echo isset($LAN_415)?$LAN_415:"May"; ?>","<?php echo isset($LAN_416)?$LAN_416:"June"; ?>","<?php echo isset($LAN_417)?$LAN_417:"July"; ?>","<?php echo isset($LAN_418)?$LAN_418:"August"; ?>","<?php echo isset($LAN_419)?$LAN_419:"September"; ?>","<?php echo isset($LAN_420)?$LAN_420:"October"; ?>","<?php echo isset($LAN_421)?$LAN_421:"November"; ?>","<?php echo isset($LAN_422)?$LAN_422:"December"; ?>");
+"<?php echo isset($LAN_411)?$LAN_411:"".CLOCK_MENU_L12; ?>","<?php echo isset($LAN_412)?$LAN_412:"".CLOCK_MENU_L13; ?>","<?php echo isset($LAN_413)?$LAN_413:"".CLOCK_MENU_L14; ?>","<?php echo isset($LAN_414)?$LAN_414:"".CLOCK_MENU_L15; ?>","<?php echo isset($LAN_415)?$LAN_415:"".CLOCK_MENU_L16; ?>","<?php echo isset($LAN_416)?$LAN_416:"".CLOCK_MENU_L17; ?>","<?php echo isset($LAN_417)?$LAN_417:"".CLOCK_MENU_L18; ?>","<?php echo isset($LAN_418)?$LAN_418:"".CLOCK_MENU_L19; ?>","<?php echo isset($LAN_419)?$LAN_419:"".CLOCK_MENU_L20; ?>","<?php echo isset($LAN_420)?$LAN_420:"".CLOCK_MENU_L21; ?>","<?php echo isset($LAN_421)?$LAN_421:"".CLOCK_MENU_L22; ?>","<?php echo isset($LAN_422)?$LAN_422:"".CLOCK_MENU_L23; ?>");
 //-->
 </script>
 <?php
+if (file_exists(e_PLUGIN."clock_menu/languages/clock_".e_LANGUAGE.".js" ))
+{
+echo "<script type='text/javascript' src='".e_PLUGIN."clock_menu/languages/clock_".e_LANGUAGE.".js'></script>\n<!-- ### end clock ### //-->\n\n"; 
+}
+else
+{
 echo "<script type='text/javascript' src='".e_PLUGIN."clock_menu/clock.js'></script>\n<!-- ### end clock ### //-->\n\n";
-
+}
 ?>
