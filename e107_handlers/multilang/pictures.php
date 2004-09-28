c68c7fa85a13552f01dfde0367fb4a5019ef4885
loloirie <loloirie@localhost>
2004-09-28 11:20:00 +0000

    Multilanguage: Allow to use translated pictures when text is displayed on icons...
    svn path=/trunk/e107_0.7/; revision=925

---
+++ e107_handlers/multilang/pictures.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+*
+* Functions to manage spictures with mutlilanguage support
+*
+* eMLANG_path() : Allow to display pictures in regard of theme/language used.
+*
+*/
+
+
+function eMLANG_path($file_name,$sub_folder){  
+  if (file_exists(THEME.$sub_folder."/".e_LANGUAGE."/".$file_name)){
+    $imgpath = THEME.$sub_folder."/".e_LANGUAGE."/".$file_name;
+  }else if (file_exists(THEME.$sub_folder."/".$file_name)){
+    $imgpath = THEME.$sub_folder."/".$file_name;
+  }else if (file_exists(e_IMAGE.$sub_folder."/".e_LANGUAGE."/".$file_name)){
+    $imgpath = e_IMAGE.$sub_folder."/".e_LANGUAGE."/".$file_name;
+  }else{
+    $imgpath = e_IMAGE.$sub_folder."/".$file_name;
+  }
+  return $imgpath;
+}
+
+?>
