f384910cee21cec0d8e6c8f9faad57fdd5ff8b2f
loloirie <loloirie@localhost>
2004-09-28 11:53:33 +0000

    Original version
    svn path=/trunk/e107_0.7/; revision=927

---
+++ e107_languages/English/admin/lan_adminb.php
@@ -0,0 +1,151 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_adminb.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2004-09-28 11:53:33 $
+|     $Author: loloirie $
++----------------------------------------------------------------------------+
+*/
+define("ADLAN_0", "News");
+define("ADLAN_1", "Add/edit/delete news items");
+define("ADLAN_2", "News Categories");
+define("ADLAN_3", "Add/edit/delete news categories");
+define("ADLAN_4", "Preferences");
+define("ADLAN_5", "Edit Site Preferences");
+define("ADLAN_6", "Menus");
+define("ADLAN_7", "Alter the order of your menus");
+define("ADLAN_8", "Administrators");
+define("ADLAN_9", "Add/delete site administrators");
+define("ADLAN_10", "Admin password");
+define("ADLAN_11", "Change your password");
+define("ADLAN_12", "Forums");
+define("ADLAN_13", "Add/Edit Forums");
+define("ADLAN_14", "Articles");
+define("ADLAN_15", "Add new/edit/delete articles");
+define("ADLAN_16", "Content");
+define("ADLAN_17", "Add new/edit/delete content pages");
+define("ADLAN_18", "Reviews");
+define("ADLAN_19", "Add new/edit/delete reviews");
+define("ADLAN_20", "Links");
+define("ADLAN_21", "Add new/edit/delete links");
+define("ADLAN_22", "Link Categories");
+define("ADLAN_23", "Add new/edit/delete link categories");
+define("ADLAN_24", "Downloads");
+define("ADLAN_25", "Manage Downloads");
+define("ADLAN_26", "Download Categories");
+define("ADLAN_27", "Add new/edit/delete download categories");
+define("ADLAN_28", "Welcome Message");
+define("ADLAN_29", "Set static welcome message");
+define("ADLAN_30", "File Manager");
+define("ADLAN_31", "Manage/upload files");
+define("ADLAN_32", "Submitted News");
+define("ADLAN_33", "Review user submitted news items");
+define("ADLAN_34", "Banlist");
+define("ADLAN_35", "Ban visitors");
+define("ADLAN_36", "Users");
+define("ADLAN_37", "Moderate site members");
+define("ADLAN_38", "User Classes");
+define("ADLAN_39", "Create/edit user classes");
+define("ADLAN_40", "Maintenance");
+define("ADLAN_41", "Take site down for maintenance");
+define("ADLAN_42", "Custom Menus/Pages");
+define("ADLAN_43", "Create custom menu items");
+define("ADLAN_44", "Database");
+define("ADLAN_45", "Database utilities");
+define("ADLAN_46", "Logout");
+define("ADLAN_47", "Welcome");
+define("ADLAN_48", "Logged in");
+define("ADLAN_49", "main site administrator");
+define("ADLAN_50", "permissions");
+define("ADLAN_51", "Please log in to gain access to administration area");
+define("ADLAN_52", "Admin Front Page");
+define("ADLAN_53", "Leave Admin Area");
+define("ADLAN_54", "Banners");
+define("ADLAN_55", "Configure banners");
+define("ADLAN_56", "Chatbox");
+define("ADLAN_57", "Configure chatbox");
+define("ADLAN_58", "Emoticons");
+define("ADLAN_59", "Configure emoticons");
+define("ADLAN_60", "Front Page");
+define("ADLAN_61", "Configure front page content");
+define("ADLAN_62", "News Feeds");
+define("ADLAN_63", "Configure news feeds");
+define("ADLAN_64", "Log Stats");
+define("ADLAN_65", "Log stats/counter etc");
+define("ADLAN_66", "Meta Tags");
+define("ADLAN_67", "Add/Edit site meta tags");
+define("ADLAN_68", "PHP Info");
+define("ADLAN_69", "PHPInfo page");
+define("ADLAN_70", "Polls");
+define("ADLAN_71", "Add/Edit Polls");
+define("ADLAN_72", "Public Uploads");
+define("ADLAN_73", "Configure public file uploads");
+define("ADLAN_74", "Cache");
+define("ADLAN_75", "Set cache status");
+define("ADLAN_77", "You have had a news item submitted - please click here to check.");
+define("ADLAN_78", "Edit");
+define("ADLAN_79", "Delete");
+define("ADLAN_80", "tick to confirm");
+define("ADLAN_81", "Update ");
+define("ADLAN_82", "Create ");
+define("ADLAN_83", "Existing ");
+define("ADLAN_84", "Open HTML Editor ");
+define("ADLAN_85", "Submit ");
+define("ADLAN_86", "Incorrect password ");
+define("ADLAN_87", "Administrator name not found in database ");
+define("ADLAN_88", "Unable to login ");
+define("ADLAN_89", "Admin Name ");
+define("ADLAN_90", "Admin Password ");
+define("ADLAN_91", "Log In");
+define("ADLAN_92", "Please log in to proceed to administration area ...");
+define("ADLAN_93", "Show Admin Functions");
+define("ADLAN_94", "Show Installed Plugins");
+define("ADLAN_95", "Plugin Management");
+define("ADLAN_96", "None");
+define("ADLAN_97", "Click here for FAQ");
+define("ADLAN_98", "Plugin Manager");
+define("ADLAN_99", "Install/upgrade etc plugins");
+define("ADLAN_100", "Theme Layout");
+define("ADLAN_101", "Template Creator");
+define("ADLAN_102", "It has been more than 30 days since you changed the main administrator password - ");
+define("ADLAN_103", "Click here to change it now");
+define("ADLAN_104", "Security");
+
+define("ADLAN_105", "Images");
+define("ADLAN_106", "Image Settings");
+
+define("ADLAN_107", "Unchecked submitted news items");
+define("ADLAN_108", "Unchecked file uploads");
+define("ADLAN_109", "Information");
+define("ADLAN_110", "Registered members");
+define("ADLAN_111", "Unverified members");
+define("ADLAN_112", "Banned users");
+define("ADLAN_113", "Forum posts");
+define("ADLAN_114", "Comments");
+define("ADLAN_115", "Chatbox posts");
+define("ADLAN_116", "Admin log ...");
+define("ADLAN_117", "Show all entries");
+define("ADLAN_118", "Clear log");
+
+define("ADLAN_119", "Unchecked submitted links");
+
+define("ADLAN_120", "There is a database update available, please click button to install ...");
+define("ADLAN_121", "Install");
+define("ADLAN_122", "Update");
+
+define("ADLAN_123", "Unchecked submitted articles");
+define("ADLAN_124", "Unchecked submitted reviews");
+
+define("ADLAN_125", "Unchecked reported forum posts");
+
+define("ADLAN_126", "Main Options");
+define("ADLAN_127", "Users");
+define("ADLAN_128", "Content");
+define("ADLAN_129", "Communication");
+define("ADLAN_130", "Files Management");
+define("ADLAN_131", "Other tools");
+
+?>
