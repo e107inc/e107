1f2a92e34609372f6b3513cc8950e7a952db4311
mcfly_e107 <mcfly_e107@localhost>
2004-10-03 16:01:16 +0000

    Type fixes - Code submitted by MrPete
    svn path=/trunk/e107/; revision=939

--- e107_languages/English/admin/lan_userclass.php
+++ e107_languages/English/admin/lan_userclass.php
@@ -1,14 +1,14 @@
 <?php
 
 define("UCSLAN_1", "Sending notification email to");
-define("UCSLAN_2", "Updated Privelages");
+define("UCSLAN_2", "Updated Privileges");
 define("UCSLAN_3", "Dear");
-define("UCSLAN_4", "Your privelages have been updated at");
+define("UCSLAN_4", "Your privileges have been updated at");
 define("UCSLAN_5", "You now have access to the following area(s)");
 define("UCSLAN_6", "Set class for user");
 define("UCSLAN_7", "Set Classes");
 define("UCSLAN_8", "Notify User");
-define("UCSLAN_8", "Classes Updated.");
-define("UCSLAN_9", "Regards");
+define("UCSLAN_9", "Classes Updated.");
+define("UCSLAN_10", "Regards");
 
 ?>
