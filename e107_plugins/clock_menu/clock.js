bdc586c0e5bb6707b863e1c6e804fe6a98b73cd7
loloirie <loloirie@localhost>
2004-10-15 11:56:37 +0000

    International clock (multilanguage and new display options)
    svn path=/trunk/e107/; revision=974

--- e107_plugins/clock_menu/clock.js
+++ e107_plugins/clock_menu/clock.js
@@ -1,6 +1,7 @@
 <!--
 
-function tick() {
+function tick(e107_datepref,e107_dateformat,e107_datesuff1,e107_datesuff2,e107_datesuff3,e107_datesuff4) {
+  if(e107_datepref=='undefined'){e107_datepref = '';}
   var hours, minutes, seconds, ap;
   var intHours, intMinutes, intSeconds;  var today;
   today = new Date();
@@ -11,33 +12,42 @@ function tick() {
   intHours = today.getHours();
   intMinutes = today.getMinutes();
   intSeconds = today.getSeconds();
-  timeString = DayNam[intDay]+" "+intDate;
+  timeString = DayNam[intDay]+" "+e107_datepref+" "+intDate;
   if (intDate == 1 || intDate == 21 || intDate == 31) {
-    timeString= timeString + "st ";
+    timeString= timeString + e107_datesuff1 + " ";
   } else if (intDate == 2 || intDate == 22) {
-    timeString= timeString + "nd ";
+    timeString= timeString  + e107_datesuff2 + " ";
   } else if (intDate == 3 || intDate == 23) {
-    timeString= timeString + "rd ";
+    timeString= timeString  + e107_datesuff3 + " ";
   } else {
-    timeString = timeString + "th ";
+    timeString = timeString  + e107_datesuff4 + " ";
   } 
   if (intYear < 2000){
 	intYear += 1900;
   }
-  timeString = timeString+" of "+MnthNam[intMonth]+" "+intYear;
-  if (intHours == 0) {
-     hours = "12:";
-     ap = "am.";
-  } else if (intHours < 12) { 
-     hours = intHours+":";
-     ap = "am.";
-  } else if (intHours == 12) {
-     hours = "12:";
-     ap = "pm.";
-  } else {
-     intHours = intHours - 12
-     hours = intHours + ":";
-     ap = "pm.";
+  timeString = timeString+" "+MnthNam[intMonth]+" "+intYear;
+  if(e107_dateformat == 1){
+    if (intHours == 0) {
+       hours = "12:";
+       ap = "am.";
+    } else if (intHours < 12) { 
+       hours = intHours+":";
+       ap = "am.";
+    } else if (intHours == 12) {
+       hours = "12:";
+       ap = "pm.";
+    } else {
+       intHours = intHours - 12
+       hours = intHours + ":";
+       ap = "pm.";
+    }
+  }else{
+    if (intHours < 10) {
+       hours = "0" + intHours + ":";
+    } else {
+       hours = intHours + ":";
+    }
+    ap = '';
   }
   if (intMinutes < 10) {
      minutes = "0"+intMinutes;
@@ -52,7 +62,7 @@ function tick() {
   timeString = (document.all)? timeString+", "+hours+minutes+seconds+" "+ap:timeString+" "+hours+minutes+" "+ap;
   var clock = (document.all) ? document.all("Clock") : document.getElementById("Clock");
   clock.innerHTML = timeString;
-  (document.all)?window.setTimeout("tick();", 1000):window.setTimeout("tick();", 6000);
+  (document.all)?window.setTimeout("tick('"+e107_datepref+"','"+e107_dateformat+"','"+e107_datesuff1+"','"+e107_datesuff2+"','"+e107_datesuff3+"','"+e107_datesuff4+"');", 1000):window.setTimeout("tick('"+e107_datepref+"','"+e107_dateformat+"','"+e107_datesuff1+"','"+e107_datesuff2+"','"+e107_datesuff3+"','"+e107_datesuff4+"');", 6000);
 }
 
 ////tick();
--- e107_plugins/clock_menu/config.php
+++ e107_plugins/clock_menu/config.php
@@ -1,52 +1,103 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|	e107 website system
-|	/admin/menu_conf/comment_conf.php
++ ----------------------------------------------------------------------------+
+|     e107 website system
 |
-|	©Edwin van der Wal 2003
-|	http://e107.org
-|	evdwal@xs4all.nl
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
 |
-|	Released under the terms and conditions of the	
-|	GNU General Public License (http://gnu.org).
-+---------------------------------------------------------------+
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107/e107_plugins/clock_menu/config.php,v $
+|     $Revision: 1.3 $
+|     $Date: 2004-10-15 11:56:37 $
+|     $Author: loloirie $
++----------------------------------------------------------------------------+
 */
 require_once("../../class2.php");
 if(!getperms("1")){ header("location:".e_BASE."index.php"); exit ;}
 require_once(e_ADMIN."auth.php");
-
-@include_once(e_PLUGIN."clock_menu/languages/".e_LANGUAGE.".php");
-@include_once(e_PLUGIN."clock_menu/languages/English.php");
+@include_once(e_PLUGIN."clock_menu/languages/admin/".e_LANGUAGE.".php");
+@include_once(e_PLUGIN."clock_menu/languages/admin/English.php");
+require_once(e_HANDLER."form_handler.php");
+$rs = new form;
 
 if(IsSet($_POST['update_menu'])){
-	while(list($key, $value) = each($_POST)){
-		if($key != "update_menu"){ $menu_pref[$key] = $value; }
-	}
-	$tmp = addslashes(serialize($menu_pref));
-	$sql -> db_Update("core", "e107_value='$tmp' WHERE e107_name='menu_pref' ");
-	$ns -> tablerender("", "<div style=\"text-align:center\"><b>".CLOCK_MENU_L1."</b></div>");
+        while(list($key, $value) = each($_POST)){
+                if($key != "update_menu"){ $menu_pref[$key] = $value; }
+        }
+        if($_POST['clock_format']!=1){$menu_pref['clock_format'] = 0;}
+        $tmp = addslashes(serialize($menu_pref));
+        $sql -> db_Update("core", "e107_value='$tmp' WHERE e107_name='menu_pref' ");
+        $ns -> tablerender("", "<div style=\"text-align:center\"><b>".CLOCK_AD_L1."</b></div>");
 }
 
 $text = "<div style='text-align:center'>
 <form method=\"post\" action=\"".e_SELF."?".e_QUERY."\" name=\"menu_conf_form\">
-<table style=\"width:85%\" class=\"fborder\">
+<table style=\"width:85%\" class=\"fborder\">";
 
-<tr>
-<td style=\"width:40%\" class='forumheader3'>".CLOCK_MENU_L2.": </td>
+//  Title
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L2.": </td>
 <td style=\"width:60%\" class='forumheader3'>
 <input class=\"tbox\" type=\"text\" name=\"clock_caption\" size=\"20\" value=\"".$menu_pref['clock_caption']."\" maxlength=\"100\" />
 </td>
-</tr>
+</tr>";
 
-<tr style=\"vertical-align:top\"> 
+// Format Time
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L5.": </td>
+<td style=\"width:60%\" class='forumheader3'>".( $menu_pref['clock_format'] == 1 ? $rs -> form_checkbox("clock_format",1,1) : $rs -> form_checkbox("clock_format",1,0) )."
+<br /><b class='smalltext'>".CLOCK_AD_L6."</b></td>
+</tr>";
+
+//  Date Prefix
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L7.": </td>
+<td style=\"width:60%\" class='forumheader3'>
+<input class=\"tbox\" type=\"text\" name=\"clock_dateprefix\" size=\"10\" value=\"".$menu_pref['clock_dateprefix']."\" maxlength=\"50\" />
+<br /><b class='smalltext'>".CLOCK_AD_L8."</b></td>
+</tr>";
+
+//  Date Suffix
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L9.": </td>
+<td style=\"width:60%\" class='forumheader3'>
+1<input class=\"tbox\" type=\"text\" name=\"clock_datesuffix1\" size=\"10\" value=\"".$menu_pref['clock_datesuffix1']."\" maxlength=\"50\" />
+<br /><b class='smalltext'>".CLOCK_AD_L13."</b></td>
+</tr>";
+
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L10.": </td>
+<td style=\"width:60%\" class='forumheader3'>
+2<input class=\"tbox\" type=\"text\" name=\"clock_datesuffix2\" size=\"10\" value=\"".$menu_pref['clock_datesuffix2']."\" maxlength=\"50\" />
+<br /><b class='smalltext'>".CLOCK_AD_L13."</b></td>
+</tr>";
+
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L11.": </td>
+<td style=\"width:60%\" class='forumheader3'>
+3<input class=\"tbox\" type=\"text\" name=\"clock_datesuffix3\" size=\"10\" value=\"".$menu_pref['clock_datesuffix3']."\" maxlength=\"50\" />
+<br /><b class='smalltext'>".CLOCK_AD_L13."</b></td>
+</tr>";
+
+$text .= "<tr>
+<td style=\"width:40%\" class='forumheader3'>".CLOCK_AD_L12.": </td>
+<td style=\"width:60%\" class='forumheader3'>
+4<input class=\"tbox\" type=\"text\" name=\"clock_datesuffix4\" size=\"10\" value=\"".$menu_pref['clock_datesuffix4']."\" maxlength=\"50\" />
+<br /><b class='smalltext'>".CLOCK_AD_L13."</b></td>
+</tr>";
+
+$text .= "<tr style=\"vertical-align:top\">
 <td colspan=\"2\"  style=\"text-align:center\" class='forumheader'>
-<input class=\"button\" type=\"submit\" name=\"update_menu\" value=\"".CLOCK_MENU_L3."\" />
+<input class=\"button\" type=\"submit\" name=\"update_menu\" value=\"".CLOCK_AD_L3."\" />
 </td>
 </tr>
 </table>
 </form>
 </div>";
-$ns -> tablerender(CLOCK_MENU_L4, $text);
+$ns -> tablerender(CLOCK_AD_L4, $text);
 require_once(e_ADMIN."footer.php");
 ?>
