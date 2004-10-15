f97821701ecc390fdf703473ea082e67b2c3eaaa
loloirie <loloirie@localhost>
2004-10-15 12:04:52 +0000

    International clock (multilanguage and new display options)
    svn path=/trunk/e107/; revision=975

--- e107_plugins/clock_menu/languages/English.php
+++ e107_plugins/clock_menu/languages/English.php
@@ -1,21 +1,44 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|	e107 website system
-|	/e107_plugins/clock_menu/languages/English.php
++ ----------------------------------------------------------------------------+
+|     e107 website system
 |
-|	©Steve Dunstan 2001-2002
-|	http://e107.org
-|	jalist@e107.org
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
 |
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
-+---------------------------------------------------------------+
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107/e107_plugins/clock_menu/languages/English.php,v $
+|     $Revision: 1.2 $
+|     $Date: 2004-10-15 12:04:52 $
+|     $Author: loloirie $
++----------------------------------------------------------------------------+
 */
 
 define('CLOCK_MENU_L1','Clock menu configuration saved');
 define('CLOCK_MENU_L2','Caption');
 define('CLOCK_MENU_L3','Update Menu Settings');
 define('CLOCK_MENU_L4','Clock Menu Config');
-
+define('CLOCK_MENU_L5','Monday,');
+define('CLOCK_MENU_L6','Tuesday,');
+define('CLOCK_MENU_L7','Wednesday,');
+define('CLOCK_MENU_L8','Thursday,');
+define('CLOCK_MENU_L9','Friday,');
+define('CLOCK_MENU_L10','Saturday,');
+define('CLOCK_MENU_L11','Sunday,');
+define('CLOCK_MENU_L12','January');
+define('CLOCK_MENU_L13','February');
+define('CLOCK_MENU_L14','March');
+define('CLOCK_MENU_L15','April');
+define('CLOCK_MENU_L16','May');
+define('CLOCK_MENU_L17','June');
+define('CLOCK_MENU_L18','July');
+define('CLOCK_MENU_L19','August');
+define('CLOCK_MENU_L20','September');
+define('CLOCK_MENU_L21','October');
+define('CLOCK_MENU_L22','November');
+define('CLOCK_MENU_L23','December');
+define('CLOCK_MENU_L24','');
 ?>
