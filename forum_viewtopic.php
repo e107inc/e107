a40d6b86516ddba280d524e9b3c673721d2bd5a7
mcfly_e107 <mcfly_e107@localhost>
2004-10-29 18:02:45 +0000

    Quote post icon no longer shown if user not allowed to post to forum (dev bugtracker #168)
    svn path=/trunk/e107/; revision=998

--- forum_viewtopic.php
+++ forum_viewtopic.php
@@ -317,9 +317,12 @@ if(!$post_author_id || !$sql -> db_Select("user", "*", "user_id='".$post_author_
 }
 
 $EDITIMG = ($post_author_id != "0" && $post_author_name == USERNAME && $thread_active ? "<a href='forum_post.php?edit.".$forum_id.".".$thread_id."'>".IMAGE_edit."</a> " : "");
-if($thread_active){
+if($thread_active && (USER == TRUE || 	$pref['anon_post']))
+{
 	$QUOTEIMG = "<a href='forum_post.php?quote.".$forum_id.".".$thread_id."'>".IMAGE_quote."</a>";
-}else{
+}
+else
+{
 	$T_ACTIVE = TRUE;
 }
 $REPORTIMG = (USER ? "<a href='forum_viewtopic.php?".$forum_id.".".$thread_id.".".$from.".report'>".IMAGE_report."</a> " : "");
