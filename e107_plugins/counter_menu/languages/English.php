c74d77d3493380f1b4981690225d9c7e83f65a1b
mcfly_e107 <mcfly_e107@localhost>
2004-10-29 16:24:01 +0000

    Hardcoded text removed (Bugtracker #391)
    svn path=/trunk/e107_0.7/; revision=997

--- e107_plugins/counter_menu/languages/English.php
+++ e107_plugins/counter_menu/languages/English.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/counter_menu/languages/English.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:12:10 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2004-10-29 16:24:01 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 
@@ -16,5 +16,6 @@ define("COUNTER_L3", "Ever");
 define("COUNTER_L4", "Record");
 define("COUNTER_L5", "Stat logging is disabled.");
 define("COUNTER_L6", "Click here to enable.");
+define("COUNTER_L7", "Unique");
 
 ?>
