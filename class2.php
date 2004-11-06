93b97b975a05a2e559edf76f0483c6720f00ff18
loloirie <loloirie@localhost>
2004-11-06 01:27:34 +0000

    Minor fix on precedent add-on...
    svn path=/trunk/e107_0.7/; revision=1012

--- class2.php
+++ class2.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/class2.php,v $
-|     $Revision: 1.16 $
-|     $Date: 2004-11-04 13:48:15 $
+|     $Revision: 1.17 $
+|     $Date: 2004-11-06 01:27:34 $
 |     $Author: loloirie $
 +----------------------------------------------------------------------------+
 */
@@ -615,7 +615,7 @@ function chk_class($var, $userclass, $debug)
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 function check_class($var, $userclass=USERCLASS, $debug=FALSE)
 {
-	if($!var || $var==""){return TRUE;}
+	if(!$var || $var == ""){return TRUE;}
   if(strpos($var,",") == FALSE)
 	{
 		return chk_class($var,$userclass,$debug);
