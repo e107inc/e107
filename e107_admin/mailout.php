d6d26e7d0ff8a693ca953407aacbfbb53e2af2b6
e107coders <e107coders@localhost>
2004-11-06 23:52:23 +0000

    now uses phpmailer class and can send attachments.
    svn path=/trunk/e107_0.7/; revision=1015

--- e107_admin/mailout.php
+++ e107_admin/mailout.php
@@ -11,45 +11,25 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/mailout.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:10:20 $
+|     $Revision: 1.2 $
+|     $Date: 2004-11-06 23:52:23 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
-/*  User-Mailout for the e107 core release. .
- This code is experimental
- Still to be added:
-  - pulldown menu with a list of files in the download area.
-  - the {USERNAME} stuff.
-  - bulk mailing with BCC option.. ?
-  - confirm number of recipients, before sending.
 
-*/
+    require_once("../class2.php");
+    require_once(e_ADMIN."auth.php");
+    require_once(e_LANGUAGEDIR.e_LANGUAGE."/admin/lan_users.php");
 
-require_once("../class2.php");
-require_once(e_ADMIN."auth.php");
-require_once(e_LANGUAGEDIR.e_LANGUAGE."/admin/lan_users.php");
-
-require_once(e_HANDLER."userclass_class.php");
+    require_once(e_HANDLER."userclass_class.php");
     if($pref['htmlarea']){
     require_once(e_HANDLER."htmlarea/htmlarea.inc.php");
     htmlarea('email_body');
     }
 
-$send_amount = 5; // amount of people in BCC per email.
-
-
-
-
-
-
-
-
 if(IsSet($_POST['submit'])){
 
-
     if($_POST['email_to'] == "all"){   // send to all.
-
        $sql -> db_Select("user", "user_name, user_email, user_class", "ORDER BY user_name", "no-where");
        $c=0;
            while($row = $sql -> db_Fetch()){
@@ -73,57 +53,92 @@ if(IsSet($_POST['submit'])){
         }  // end while
 
     }
-// ===================
 
-   /* $text = "<div style='text-align:center'>
-   <form method='post' action='".e_SELF."' id='confirmform'>
-   You have chosen to send an email to <b>$c</b> users<br />
-   Are you sure you want to continue ?<br />
-   <input type='submit' class='button' value='submit' name='Yes'>
-   <input type='hidden' name='";*/
+    // ===== phpmailer version.
 
+    require(e_HANDLER."phpmailer/class.phpmailer.php");
 
-      /*
-            $chunk=0;
-            for ($j=0; $i<count($recipients); $j++) {
-            $bcc_bulk = "";
-            for ($i=($chunk); $i<($chunk+$amount); $i++) {
-            $bcc_bulk .= $recipients[$i];
-            };
-            $j = $j +amount;
-            $chunk = $chunk+$amount;
-      */
-    require_once(e_HANDLER."mail.php");
+    $mail = new PHPMailer();
+
+    $mail->From     = ($_POST['email_from_email'])? $_POST['email_from_email']:$pref['siteadminemail'];
+    $mail->FromName = ($_POST['email_from_name'])? $_POST['email_from_name']:$pref['siteadmin'];
+  //  $mail->Host     = "smtp1.site.com;smtp2.site.com";
+    $mail->Mailer   = "mail";
+    $mail->AddCC = ($_POST['email_cc']);
+
+    $mail->Subject = $_POST['email_subject'];
+    $attach = chop($_POST['email_attachment']);
+
+    $root = (preg_match("#^/#",$DOWNLOADS_DIRECTORY) || preg_match("#.:#",$DOWNLOADS_DIRECTORY))? "" : e_BASE;
+
+    if(!$mail->AddAttachment($root.$DOWNLOADS_DIRECTORY."/".$_POST['email_attachment'],$attach)){
+    $mss = "There is a problem with the attachment";
+    $ns -> tablerender("Error", $mss);
+    require_once(e_ADMIN."footer.php");
+    exit;
+    }
+    // ============================  Render Results and Mailit =========
+
     $text = "<div style='overflow:auto;height:300px'>";
     $text .= "<table class='fborder' style='width:100%'>";
     $text .= "<tr><td class='fcaption'>Username</td><td class='fcaption'>Email</td><td class='fcaption'>Status</td></tr>";
     $message_body = eregi_replace('src="','src="'.SITEURL,$_POST['email_body']);
     $message_body .="<br />";
-    $
-  //  echo $message_body;
-     $sent_no = 0;
+
+    $sent_no = 0;
+
     for ($i=0; $i<count($recipient); $i++) {
+
+    // --- start loop ----
+
     $text .="<tr>";
     $text .="<td class='forumheader3' style='width:40%'>".$recipient_name[$i]."</td>";
     $text .="<td class='forumheader3' style='width:40%'>".$recipient[$i]."</td>";
-        if(sendemail($recipient[$i],$_POST['email_subject'],$message_body,$recipient_name[$i],$_POST['email_from_email'],$_POST['email_from_name'],$attach,$_POST['email_cc'],$_POST['email_bcc'],"","")){
-        $stat = "<span style='color:green'>Sent</span>";
-        $sent_no ++;
-        }else{
-        $stat = "<span style='color:red'>Error</span>";
-        }
-        $text .="<td class='forumheader3'>&nbsp;&nbsp; $stat </td></tr>";
-    };
-        $text .="</table></div>";
 
+    $mail->Body    = $message_body;
+    $mail->AltBody = $message_body;
+    $mail->AddAddress($recipient[$i], $recipient_name[$i]);
+
+
+
+       if($mail->Send()){
+       $stat = "<span style='color:green'>Sent</span>";
+       $sent_no ++;
+       }else{
+       $stat = "<span style='color:red'>Error</span>";
+       }
+       $text .="<td class='forumheader3'>&nbsp;&nbsp; $stat </td></tr>";
+
+
+
+    $mail->ClearAddresses();
+
+
+
+     // ---- end loop. ---
+
+    };
+
+     $mail->ClearAttachments();
+
+
+    $text .="</table></div>";
     $rec_text = $c > 1 ? "recipients":"recipient";
 
     if($c == 0 ){ $text = "<div style='text-align:center'>No Recipients Found</div>"; }
 
     $ns -> tablerender("Emailing $c $rec_text", $text);
     require_once(e_ADMIN."footer.php");
-exit;
+    exit;
 }
+
+
+
+
+
+// Display Form.
+
+
 
    $text = "";
    $text .= ($pref['smtp_enable']==0)? "<div style='text-align:center'>It is recommended that you enable <a href='prefs.php'>SMTP</a> for sending large numbers of emails.<br /><br /></div>":"";
@@ -175,9 +190,29 @@ exit;
    <input type='text' name='email_subject' class='tbox' style='width:80%' value='' />
    $email_subject
    </td>
-   </tr>
+   </tr>";
 
-   <tr>
+
+   // Attachment.
+
+   $text .= "<tr>
+   <td style='width:30%' class='forumheader3'>Attachment: </td>
+   <td style='width:70%' class='forumheader3'>";
+   $text .= "<select class='tbox' name='email_attachment' >
+       <option></option>";
+        $sql -> db_Select("download", "download_url,download_name", "download_id !='' ORDER BY download_name");
+        while($row = $sql-> db_Fetch()){
+        extract($row);
+        $selected = ($_POST['email_attachment'] == $download_url) ?  "selected='selected'" : "";
+       $text .="<option value=\"$download_url \" $selected>$download_name</option>";
+       }
+   $text .= " </select>";
+
+   $text .="</td>
+   </tr>";
+
+
+   $text .="   <tr>
    <td colspan='2' style='width:30%' class='forumheader3'>
 
    <textarea rows='10' cols='20' id='email_body' name='email_body'  class='tbox' style='border:1px solid black;width:100%;height:200px'>
@@ -187,7 +222,6 @@ exit;
    </tr>
 
    ";
-
 
 
    $text .="<tr style='vertical-align:top'>
