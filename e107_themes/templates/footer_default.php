d6fe73bf7c2f043af533fe8bad41e15cd4141761
mcfly_e107 <mcfly_e107@localhost>
2004-11-17 00:25:12 +0000

    Code cleanup, render time when cache enabled fixed.
    svn path=/trunk/e107/; revision=1037

--- e107_themes/templates/footer_default.php
+++ e107_themes/templates/footer_default.php
@@ -12,7 +12,8 @@
 |	GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
-if(!is_object($sql)){
+if(!is_object($sql))
+{
 	// reinstigate db connection if another connection from third-party script closed it ...
 	global $sql, $mySQLserver, $mySQLuser, $mySQLpassword, $mySQLdefaultdb, $CUSTOMFOOTER, $FOOTER;
 	$sql = new db;
@@ -20,23 +21,39 @@ if(!is_object($sql)){
 }
 
 unset($fh);
-if($e107_popup!=1){
+if($e107_popup!=1)
+{
 	$custompage = explode(" ", $CUSTOMPAGES);
-	if($CUSTOMFOOTER){
-		while(list($key, $kpage) = each($custompage)){
-			if(strstr(e_SELF, $kpage)){
+	if($CUSTOMFOOTER)
+	{
+		while(list($key, $kpage) = each($custompage))
+		{
+			if(strstr(e_SELF, $kpage))
+			{
 				$fh = TRUE;
 				break;
 			}
 		}
 	}
 	parseheader(($fh ? $CUSTOMFOOTER : $FOOTER));
-	
-	$timing_stop = explode(' ', microtime());
-	$rendertime = number_format((($timing_stop[0]+$timing_stop[1])-($timing_start[0]+$timing_start[1])), 4);
-	if($pref['displayrendertime']){ $rinfo .= "Render time: ".$rendertime." second(s). "; }
-	if($pref['displaysql']){ $rinfo .= "DB queries: ".$dbq.". "; }
-	if($pref['displaycacheinfo']){ $rinfo .= $cachestring."."; }
+
+	if($pref['displayrendertime'])
+	{
+		global $timing_start;
+		$timing_stop = explode(' ', microtime());
+		$rendertime = number_format((($timing_stop[0]+$timing_stop[1])-($timing_start[0]+$timing_start[1])), 4);
+		$rinfo .= "Render time: ".$rendertime." second(s). ";
+	}
+	if($pref['displaysql'])
+	{
+		global $dbq;
+		$rinfo .= "DB queries: ".$dbq.". ";
+	}
+	if($pref['displaycacheinfo'])
+	{ 
+		global $cachestring;
+		$rinfo .= $cachestring."."; 
+	}
 	echo ($rinfo ? "<div style='text-align:center' class='smalltext'>$rinfo</div>" : "");
 }
 echo "</body>
