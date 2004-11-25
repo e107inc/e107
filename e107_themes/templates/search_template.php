4442212cc5463ba5e25afb6b147e3194eaabd7e6
lisa_ <lisa_@localhost>
2004-11-25 21:09:18 +0000

    added template file for search.php
    svn path=/trunk/e107_0.7/; revision=1054

---
+++ e107_themes/templates/search_template.php
@@ -0,0 +1,39 @@
+<?php
+
+// ##### MAIN TABLE -------------------------------------------------------------------------------
+if(!$SEARCH_MAIN_TABLE){
+		$SEARCH_MAIN_TABLE = "
+		<div style='text-align:center'>
+		<form name='searchform' method='post' action='".e_SELF."'>
+		<table style='width:95%' class='fborder'>\n
+			<tr>
+				<td colspan='2' class='forumheader'>".LAN_180."</td>
+			</tr>
+			<tr>
+				<td class='forumheader2' style='width:20%'>".LAN_199." </td>
+				<td class='forumheader3' style='width:80%'>
+					{SEARCH_MAIN_SEARCHFIELD}<br />
+					<span class='smalltext'>".LAN_417."</span>
+				</td>
+			</tr>
+			<tr>
+				<td style='width:20%' class='forumheader2'>&nbsp;".LAN_200."</td>
+				<td style='width:80%' class='forumheader3'>{SEARCH_MAIN_CHECKBOXES}
+				<br /><br /> 
+				{SEARCH_MAIN_CHECKALL}
+				{SEARCH_MAIN_UNCHECKALL}
+				<br />
+				</td>
+			</tr>
+			<tr>
+				<td colspan='2' class='forumheader' style='text-align:center'>
+					{SEARCH_MAIN_SUBMIT}
+				</td>
+			</tr>
+		</table>
+		</form>
+		</div>\n";
+}
+// ##### ------------------------------------------------------------------------------------------
+
+?>
