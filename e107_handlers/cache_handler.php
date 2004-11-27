003a71228e41ddeacbb3fa82da79f5e5c4ecde64
streaky <streaky@localhost>
2004-11-27 14:39:40 +0000

    Code tidy up, added PHPDoc format documentation comments.
    svn path=/trunk/e107_0.7/; revision=1061

--- e107_handlers/cache_handler.php
+++ e107_handlers/cache_handler.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system
@@ -11,95 +12,126 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/cache_handler.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2004-11-07 20:39:18 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.7 $
+|     $Date: 2004-11-27 14:39:40 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
-
-class ecache
-{
-	function e107cache_page_md5()
-	{
+/**
+ * Class to cache data as files, improving site speed and throughput.
+ *
+ * @package     e107
+ * @version     $Revision: 1.7 $
+ * @author      $Author: streaky $
+ */
+class ecache {
+	/**
+	* @return string
+	* @desc Internal class function that returns an md5 hash of e_BASE.e_LANGUAGE.THEME.USERCLASS.e_QUERY - used for the filename of the cache file, making sure, for example, that the file is unique to the theme.
+	* @scope private
+	*/
+	function e107cache_page_md5() {
 		return md5(e_BASE.e_LANGUAGE.THEME.USERCLASS.e_QUERY);
 	}
 
-	function cache_fname($query)
-	{
+	/**
+	* @return string
+	* @param string $query
+	* @desc Internal class function that returns the filename of a cache file based on the query.
+	* @scope private
+	*/
+	function cache_fname($query) {
 		global $FILES_DIRECTORY;
-		$q = preg_replace("#\W#","_",$query);
-		$fname = "./".e_BASE.$FILES_DIRECTORY."cache/".$q."-".$this -> e107cache_page_md5().".cache.php";
+		$q = preg_replace("#\W#", "_", $query);
+		$fname = "./".e_BASE.$FILES_DIRECTORY."cache/".$q."-".$this->e107cache_page_md5().".cache.php";
 		return $fname;
 	}
 
-	function retrieve($query)
-	{
+	/**
+	* @return string
+	* @param string $query
+	* @desc Returns the data from the cache file associated with $query, else it returns false if there is no cache for $query.
+	* @scope public
+	*/
+	function retrieve($query) {
 		global $pref, $FILES_DIRECTORY;
-		if($pref['cachestatus'])  //Save to file
+		if ($pref['cachestatus']) //Save to file
 		{
-			if($cache_file = $this -> cache_fname($query))
-			{
+			if ($cache_file = $this->cache_fname($query)) {
 				$ret = file_get_contents($cache_file);
 				$ret = substr($ret, 6);
-				if($ret == false){ return FALSE; }
-				return ('' == $ret) ? '<!-- null -->' : $ret;
-			}
-			else
-			{
+				if ($ret == false) {
+					return FALSE;
+				}
+				return ('' == $ret) ? '<!-- null -->' :
+				$ret;
+			} else {
 				return FALSE;
 			}
 		}
 	}
 
-	function set($query, $text)
-	{
-		global $pref,$FILES_DIRECTORY;
-		if($pref['cachestatus'])
-		{
-			$cache_file = $this -> cache_fname($query);
+	/**
+	* @return void
+	* @param string $query
+	* @param string $text
+	* @desc Creates / overwrites the cache file for $query, $text is the data to store for $query.
+	* @scope public
+	*/
+	function set($query, $text) {
+		global $pref, $FILES_DIRECTORY;
+		if ($pref['cachestatus']) {
+			$cache_file = $this->cache_fname($query);
 			file_put_contents($cache_file, "<?php\n<!-- BEGIN CACHE FILE: $query -->\n\n".$text."\n\n<!-- END CACHE FILE: $query -->");
 			@chmod($cache_file, 0777);
 		}
 	}
 
-	function clear($query)
-	{
-		global $pref,$FILES_DIRECTORY;
-		if($pref['cachestatus'] || !$query)
-		{
-			$file = ($query) ? preg_replace("#\W#","_",$query)."*.cache.php" : "*.cache.php";
+	/**
+	* @return bool
+	* @param string $query
+	* @desc Deletes cache files. If $query is set, deletes files named {$query}*.cache.php, if not it deletes all cache files - (*.cache.php)
+	*/
+	function clear($query) {
+		global $pref, $FILES_DIRECTORY;
+		if ($pref['cachestatus'] || !$query) {
+			$file = ($query) ? preg_replace("#\W#", "_", $query)."*.cache.php" :
+			"*.cache.php";
 			$dir = "./".e_BASE.$FILES_DIRECTORY."cache/";
-			$ret = $this -> delete($dir, $file);
+			$ret = $this->delete($dir, $file);
 		}
 		return $ret;
 	}
 
-	function delete($dir, $pattern = "*.*")
-	{
+	/**
+	* @return bool
+	* @param string $dir
+	* @param string $pattern
+	* @desc Internal class function to allow deletion of cache files using a pattern, default '*.*'
+	* @scope private
+	*/
+	function delete($dir, $pattern = "*.*") {
 		$deleted = false;
-		$pattern = str_replace(array("\*","\?"), array(".*","."), preg_quote($pattern));
-		if (substr($dir,-1) != "/"){$dir .= "/";}
-		if (is_dir($dir))
-		{
+		$pattern = str_replace(array("\*", "\?"), array(".*", "."), preg_quote($pattern));
+		if (substr($dir, -1) != "/") {
+			$dir .= "/";
+		}
+		if (is_dir($dir)) {
 			$d = opendir($dir);
-			while ($file = readdir($d))
-			{
-				if (is_file($dir.$file) && ereg("^".$pattern."$", $file))
-				{
-					if (unlink($dir.$file))
-					{
+			while ($file = readdir($d)) {
+				if (is_file($dir.$file) && ereg("^".$pattern."$", $file)) {
+					if (unlink($dir.$file)) {
 						$deleted[] = $file;
 					}
 				}
 			}
 			closedir($d);
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 }
+
 ?>
