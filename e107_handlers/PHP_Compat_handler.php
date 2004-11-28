3c1f59e4a4aa0d17f3ac065d9eed593830825de9
streaky <streaky@localhost>
2004-11-28 14:33:50 +0000

    Added documentation tags for functions.
    svn path=/trunk/e107_0.7/; revision=1079

--- e107_handlers/PHP_Compat_handler.php
+++ e107_handlers/PHP_Compat_handler.php
@@ -1,6 +1,11 @@
 <?php
 
 if (!function_exists('file_get_contents')) {
+	/**
+	* @return string
+	* @param string $filename
+	* @desc Reads entire file into a string
+	*/
 	function file_get_contents($filename) {
 		$fd = fopen("$filename", "rb");
 		$content = fread($fd, filesize($filename));
@@ -9,6 +14,12 @@ if (!function_exists('file_get_contents')) {
 	}
 }
 if (!function_exists('file_put_contents')) {
+	/**
+	* @return int
+	* @param string $filename
+	* @param mixed $data
+	* @desc Write a string to a file
+	*/
 	function file_put_contents($filename, $data) {
 		if (($h = @fopen($filename, 'w+')) === false) {
 			return false;
