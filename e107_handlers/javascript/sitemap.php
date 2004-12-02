ac593f1323bc00a0f449669004731a4e1da5caef
loloirie <loloirie@localhost>
2004-12-02 20:53:13 +0000

    Shorter js code and working lol
    svn path=/trunk/e107_0.7/; revision=1093

--- e107_handlers/javascript/sitemap.php
+++ e107_handlers/javascript/sitemap.php
@@ -1,66 +1,39 @@
 <?php
-/*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.7/e107_handlers/javascript/sitemap.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:10:40 $
-|     $Author: e107coders $
-+----------------------------------------------------------------------------+
-*/
 echo "
 var ejs_func_todo='view';\n
 function ejs_expandall(ejs_cl_name,ejs_func_todo,ejs_cl_name2){\n
-        tot_object = document.getElementsByTagName('p').length;\n
-        for(i=0;i<tot_object;i++){\n
-                sp = document.getElementsByTagName('span').item(i);\n
-                p = document.getElementsByTagName('p').item(i);\n
-                if(p.id!=''){\n
-                        if(p.className==ejs_cl_name){\n
-                                if(ejs_func_todo=='view'){\n
-                                        if(p!=0)p.style.display='block';\n
-                                }else if(ejs_func_todo=='hide'){\n
-                                        if(p!=0)p.style.display='none';\n
-                                }\n
-                        }\n
-                }else if(sp){\n
-                        if(sp.className==ejs_cl_name || (ejs_cl_name2!='undefined' && sp.className==ejs_cl_name2)){\n
-                                if(ejs_func_todo=='view'){\n
-                                        if(sp!=0)sp.style.display='block';\n
-                                }else if(ejs_func_todo=='hide'){\n
-                                        if(sp!=0)sp.style.display='none';\n
-                                }\n
-                        }\n
-                }\n
-        }\n
-        if(ejs_func_todo=='hide'){ejs_func_todo='view';}else{ejs_func_todo='hide';}\n
-        ejs_expandpics('icoexp',ejs_func_todo2,'".SM_ICO_URL."','".SM_ICO_URL2."');\n
-        return ejs_func_todo;\n
+    tot_object = document.getElementsByTagName('div').length;\n
+	for(i=0;i<tot_object;i++){\n
+		p = document.getElementsByTagName('div').item(i);\n
+		
+		if(p.className==ejs_cl_name || p.className==ejs_cl_name2){\n
+			if(ejs_func_todo=='view'){\n
+				if(p!=0)p.style.display='block';\n
+			}else if(ejs_func_todo=='hide'){\n
+				if(p!=0)p.style.display='none';\n
+			}\n
+		}
+	}\n
+	if(ejs_func_todo=='hide'){ejs_func_todo='view';}else{ejs_func_todo='hide';}\n
+	ejs_expandpics('icoexp',ejs_func_todo2,'".SM_ICO_URL."','".SM_ICO_URL2."');\n
+	return ejs_func_todo;\n
 }\n\n
 
 var ejs_func_todo2='hide';\n
 function ejs_expandpics(ejs_cl_name,ejs_func_todo2,src1,src2){\n
-        tot_object2 = document.getElementsByTagName('img').length;\n\n
-        for(i=0;i<tot_object2;i++){\n
-                im = document.getElementsByTagName('img').item(i);\n
-                if(im.className==ejs_cl_name){\n
-                                if(ejs_func_todo2=='view'){\n
-                                        im.src=src1;\n
-                                }else if(ejs_func_todo2=='hide'){\n
-                                        im.src=src2;\n
-                                }\n
-                }\n
-        }\n
-        if(ejs_func_todo2=='hide'){ejs_func_todo2='view';}else{ejs_func_todo2='hide';}\n
-        return ejs_func_todo2;\n
+	tot_object2 = document.getElementsByTagName('img').length;\n\n
+	for(i=0;i<tot_object2;i++){\n
+		im = document.getElementsByTagName('img').item(i);\n
+		if(im.className==ejs_cl_name){\n
+				if(ejs_func_todo2=='view'){\n
+					im.src=src1;\n
+				}else if(ejs_func_todo2=='hide'){\n
+					im.src=src2;\n
+				}\n
+		}\n
+	}\n
+	if(ejs_func_todo2=='hide'){ejs_func_todo2='view';}else{ejs_func_todo2='hide';}\n
+	return ejs_func_todo2;\n
 }\n
 ";
 
