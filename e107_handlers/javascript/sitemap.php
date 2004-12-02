28d73d74d2cecf3965bc80ce76be89518cc90b36
loloirie <loloirie@localhost>
2004-12-02 21:21:42 +0000

    Shorter js code and working lol
    svn path=/trunk/e107/; revision=1095

--- e107_handlers/javascript/sitemap.php
+++ e107_handlers/javascript/sitemap.php
@@ -2,27 +2,17 @@
 echo "
 var ejs_func_todo='view';\n
 function ejs_expandall(ejs_cl_name,ejs_func_todo,ejs_cl_name2){\n
-	tot_object = document.getElementsByTagName('p').length;\n
+    tot_object = document.getElementsByTagName('div').length;\n
 	for(i=0;i<tot_object;i++){\n
-		sp = document.getElementsByTagName('span').item(i);\n
-		p = document.getElementsByTagName('p').item(i);\n
-		if(p.id!=''){\n
-			if(p.className==ejs_cl_name){\n
-				if(ejs_func_todo=='view'){\n
-					if(p!=0)p.style.display='block';\n
-				}else if(ejs_func_todo=='hide'){\n
-					if(p!=0)p.style.display='none';\n
-				}\n
+		p = document.getElementsByTagName('div').item(i);\n
+		
+		if(p.className==ejs_cl_name || p.className==ejs_cl_name2){\n
+			if(ejs_func_todo=='view'){\n
+				if(p!=0)p.style.display='block';\n
+			}else if(ejs_func_todo=='hide'){\n
+				if(p!=0)p.style.display='none';\n
 			}\n
-		}else if(sp){\n
-			if(sp.className==ejs_cl_name || (ejs_cl_name2!='undefined' && sp.className==ejs_cl_name2)){\n
-				if(ejs_func_todo=='view'){\n
-					if(sp!=0)sp.style.display='block';\n
-				}else if(ejs_func_todo=='hide'){\n
-					if(sp!=0)sp.style.display='none';\n
-				}\n
-			}\n
-		}\n
+		}
 	}\n
 	if(ejs_func_todo=='hide'){ejs_func_todo='view';}else{ejs_func_todo='hide';}\n
 	ejs_expandpics('icoexp',ejs_func_todo2,'".SM_ICO_URL."','".SM_ICO_URL2."');\n
