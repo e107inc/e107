128e45f23db24c8a376f1a1427411ca47c67b2f8
streaky <streaky@localhost>
2004-12-09 10:20:48 +0000

    Rewrote sections of the code, changing the way it handles cache, reboved the ob code, tidied up code. Now doesn't take .5 secs to load the sitelinks from cache. :D
    svn path=/trunk/e107_0.7/; revision=1104

--- e107_handlers/sitelinks_class.php
+++ e107_handlers/sitelinks_class.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 +---------------------------------------------------------------+
 |        e107 website system
@@ -12,92 +13,78 @@
 |        GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
+
 @include(e_LANGUAGEDIR.e_LANGUAGE."/lan_sitelinks.php");
 @include(e_LANGUAGEDIR."English/lan_sitelinks.php");
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-function sitelinks(){
-	/*
-	# Render style links
-	# - parameters    	none
-	# - return        	parsed text
-	# - scope          	null
-	*/
-	global $pref,$ns, $tp, $sql, $sql2, $ml, $e107cache;
-	if(!is_object($sql)){$sql = new db;}
-  if(!is_object($sql2)){$sql2 = new db;}
-	if($cache = $e107cache->retrieve("sitelinks")){
-		echo $tp -> toHTML($cache,TRUE,'nobreak');
-		return;
-	}
-	ob_start();
 
-	if(LINKDISPLAY == 4){
-		require_once(e_PLUGIN."ypslide_menu/ypslide_menu.php");
-		return;
+/**
+* @return void
+* @desc Outputs sitelinks
+*/
+function sitelinks() {
+	global $pref, $ns, $tp, $sql, $sql2, $ml, $e107cache;
+
+	if (!is_object($sql)) {
+		$sql = new db;
+	}
+	if (!is_object($sql2)) {
+		$sql2 = new db;
 	}
 
-	define(PRELINKTITLE, "");
-	define(POSTLINKTITLE, "");
-	$menu_count=0;
-	$text = PRELINK;
-	if(defined("LINKCLASS")){
-		$linkadd = " class='".LINKCLASS."' ";
-	}
-	/*
-	if(ADMIN == TRUE){
-	$linkstart = (file_exists(e_IMAGE."link_icons/admin.png") ? preg_replace("/\<img.*\>/si", "", LINKSTART)." " : LINKSTART);
-	if(LINKDISPLAY != 3) {
-	$text .= $linkstart.(file_exists(e_IMAGE."link_icons/admin.png") ? "<img src='".e_IMAGE."link_icons/admin.png' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"".e_ADMIN.(!$pref['adminstyle'] || $pref['adminstyle'] == "default" ? "admin.php" : $pref['adminstyle'].".php")."\">".LAN_502."</a>".LINKEND."\n";
-	} else {
-	$menu_main .= $linkstart.(file_exists(e_IMAGE."link_icons/admin.png") ? "<img src='".e_IMAGE."link_icons/admin.png' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"".e_ADMIN.(!$pref['adminstyle'] || $pref['adminstyle'] == "default" ? "admin.php" : $pref['adminstyle'].".php")."\">".LAN_502."</a>".LINKEND."\n";
-	}
-	}
-	*/
-       
-    $sql -> db_Select("links", "*", "link_category='1' && link_name NOT REGEXP('submenu') ORDER BY link_order ASC");
-	while($row = $sql -> db_Fetch()){
-		extract($row);
-		if(!$link_class || check_class($link_class)){
-			if(!preg_match("#(http:|mailto:|ftp:)#",$link_url)){ $link_url = e_BASE.$link_url; }
-			$linkstart = ($link_button ? preg_replace("/\<img.*\>/si", "", LINKSTART) : LINKSTART);
-			switch ($link_open)	{
-				case 1:
-				$link_append = " rel='external'";
-				break;
-				case 2:
-				$link_append = "";
-				break;
-				case 3:
-				$link_append = "";
-				break;
-				default:
-				unset($link_append);
-			}
-
-			if($link_open == 4)	{
-				$_link =  $linkstart.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "").($link_url ? "<a".$linkadd.($pref['linkpage_screentip'] ? " title = '$link_description' " : "")." href=\"javascript:open_window('".$link_url."')\">".$link_name."</a>" : $link_name)."\n";
-			}else{
-				$_link =  $linkstart.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "").($link_url ? "<a".$linkadd.($pref['linkpage_screentip'] ? " title = '$link_description' " : "")." href=\"".$link_url."\"".$link_append.">".$link_name."</a>" : $link_name)."\n";
-			}
-			if(LINKDISPLAY == 3){
-				$menu_title=$link_name;
-			}else{
-				$text .= $_link.LINKEND;
-			}
-
-                if($sql2 -> db_Select("links", "*", "link_name REGEXP('submenu.".$link_name."') ORDER BY link_order ASC") && !HIDESUBSECTIONS){
+	if (!$data = $e107cache->retrieve('sitelinks')) {
+		if (LINKDISPLAY == 4) {
+			require_once(e_PLUGIN.'ypslide_menu/ypslide_menu.php');
+			return;
+		}
+		define(PRELINKTITLE, '');
+		define(POSTLINKTITLE, '');
+		$menu_count = 0;
+		$text = PRELINK;
+		if (defined('LINKCLASS')) {
+			$linkadd = ' class="'.LINKCLASS.'" ';
+		}
+		$sql->db_Select('links', '*', "link_category='1' && link_name NOT REGEXP('submenu') ORDER BY link_order ASC");
+		while ($row = $sql->db_Fetch()) {
+			extract($row);
+			if (!$link_class || check_class($link_class)) {
+				if (!preg_match('#(http:|mailto:|ftp:)#', $link_url)) {
+					$link_url = e_BASE.$link_url;
+				}
+				$linkstart = ($link_button ? preg_replace('/\<img.*\>/si', '', LINKSTART) : LINKSTART);
+				switch ($link_open) {
+					case 1:
+					$link_append = ' rel=\'external\'';
+					break;
+					case 2:
+					$link_append = '';
+					break;
+					case 3:
+					$link_append = '';
+					break;
+					default:
+					unset($link_append);
+				}
+				if ($link_open == 4) {
+					$_link = $linkstart.($link_button ? '<img src="'.e_IMAGE."link_icons/$link_button\" alt='' style='vertical-align:middle' /> " : '').($link_url ? '<a'.$linkadd.($pref['linkpage_screentip'] ? " title = '$link_description' " : "")." href=\"javascript:open_window('".$link_url."')\">".$link_name.'</a>' : $link_name)."\n";
+				} else {
+					$_link = $linkstart.($link_button ? '<img src="'.e_IMAGE."link_icons/$link_button\" alt='' style='vertical-align:middle' /> " : '').($link_url ? '<a'.$linkadd.($pref['linkpage_screentip'] ? " title = '$link_description' " : "")." href=\"".$link_url."\"".$link_append.">".$link_name.'</a>' : $link_name)."\n";
+				}
+				if (LINKDISPLAY == 3) {
+					$menu_title = $link_name;
+				} else {
+					$text .= $_link.LINKEND;
+				}
+				if ($sql2->db_Select('links', "*", "link_name REGEXP('submenu.".$link_name."') ORDER BY link_order ASC") && !HIDESUBSECTIONS) {
 					$menu_count++;
 					$main_linkname = $link_name;
-					while($row = $sql2 -> db_Fetch()){
+					while ($row = $sql2->db_Fetch()) {
 						extract($row);
-						$link_name = str_replace("submenu.".$main_linkname.".", "", $link_name);
-						// if(!$link_class || check_class($link_class) || ($link_class==254 && USER))
-            if(check_class($link_class)){
-							$linkstart = ($link_button ? preg_replace("/\<img.*\>/si", "", LINKSTART)." " : LINKSTART);
-							switch ($link_open){
+						$link_name = str_replace('submenu.'.$main_linkname.'.', '', $link_name);
+						if (check_class($link_class)) {
+							$linkstart = ($link_button ? preg_replace('/\<img.*\>/si', '', LINKSTART)." " : LINKSTART);
+							switch ($link_open) {
 								case 1:
-								$link_append = "rel='external'";
-
+								$link_append = 'rel=\'external\'';
 								break;
 								case 2:
 								$link_append = "";
@@ -108,52 +95,54 @@ function sitelinks(){
 								default:
 								unset($link_append);
 							}
-
-							if(!preg_match("#(http:|mailto:|ftp:)#",$link_url)){ $link_url = e_BASE.$link_url; }
-							$indent=(LINKDISPLAY == 3) ? "" : "&nbsp;&nbsp;";
-							if($link_open == 4)	{
-								$_link =  $linkstart.$indent.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"javascript:open_window('".$link_url."')\">".$link_name."</a>".LINKEND."\n";
-							}else{
-								$_link =  $linkstart.$indent.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"".$link_url."\"".$link_append.">".$link_name."</a>".LINKEND."\n";
+							if (!preg_match('#(http:|mailto:|ftp:)#', $link_url)) {
+								$link_url = e_BASE.$link_url;
 							}
-							if(LINKDISPLAY == 3){
+							$indent = (LINKDISPLAY == 3) ? "" :
+							"&nbsp;&nbsp;";
+							if ($link_open == 4) {
+								$_link = $linkstart.$indent.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"javascript:open_window('".$link_url."')\">".$link_name."</a>".LINKEND."\n";
+							} else {
+								$_link = $linkstart.$indent.($link_button ? "<img src='".e_IMAGE."link_icons/$link_button' alt='' style='vertical-align:middle' /> " : "")."<a".$linkadd." href=\"".$link_url."\"".$link_append.">".$link_name."</a>".LINKEND."\n";
+							}
+							if (LINKDISPLAY == 3) {
 								$menu_text .= $_link;
-							}else{
+							} else {
 								$text .= $_link;
 							}
 						}
 					}
-					if(LINKDISPLAY == 3 && $menu_title)	{
-						$link_menu[]= $ns -> tablerender(PRELINKTITLE.$menu_title.POSTLINKTITLE,$menu_text,"",TRUE);
-						$menu_title="";
-						$menu_text="";
+					if (LINKDISPLAY == 3 && $menu_title) {
+						$link_menu[] = $ns->tablerender(PRELINKTITLE.$menu_title.POSTLINKTITLE, $menu_text, "", TRUE);
+						$menu_title = "";
+						$menu_text = "";
 					}
-				}else{
-					if(LINKDISPLAY == 3){$menu_main .= $_link.LINKEND;        }
+				} else {
+					if (LINKDISPLAY == 3) {
+						$menu_main .= $_link.LINKEND;
+					}
 				}
 			}
-	
-
-	}
-	$text .= POSTLINK;
-
-	$text = $tp -> toHTML($text,TRUE,'nobreak');
-
-	if(LINKDISPLAY == 2){
-		$ns = new e107table;
-		$ns -> tablerender(LAN_183, $text);
-	}else{
-		if(LINKDISPLAY != 3) {echo $text;}
-	}
-	if(LINKDISPLAY == 3){
-		$ns -> tablerender(LAN_183,$menu_main);
-		foreach($link_menu as $m){
-			echo $m;
 		}
+		$text .= POSTLINK;
+		$text = $tp->toHTML($text, TRUE, 'nobreak');
+		if (LINKDISPLAY == 2) {
+			$ns = new e107table;
+			$ns->tablerender(LAN_183, $text);
+		} else {
+			if (LINKDISPLAY != 3) {
+				$data = $text;
+			}
+		}
+		if (LINKDISPLAY == 3) {
+			$ns->tablerender(LAN_183, $menu_main);
+			foreach($link_menu as $m) {
+				$data = $m;
+			}
+		}
+		$e107cache->set('sitelinks', $data);
 	}
-
-		$cache = ob_get_contents();
-		$e107cache->set("sitelinks", $cache);
+	echo $data;
 }
 
 ?>
