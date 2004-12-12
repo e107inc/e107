9f1920d743f71f5f79e680bd499b9bdb976b7d8a
sweetas <sweetas@localhost>
2004-12-12 14:16:25 +0000

    rearranged register function args
    svn path=/trunk/e107_0.7/; revision=1124

--- e107_handlers/event_class.php
+++ e107_handlers/event_class.php
@@ -1,23 +1,27 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|	e107 website system
-|	/event_class.php
++ ----------------------------------------------------------------------------+
+|     e107 website system
 |
-|	©Steve Dunstan 2001-2002
-|	http://e107.org
-|	jalist@e107.org
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
 |
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
-+---------------------------------------------------------------+
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_handlers/event_class.php,v $
+|     $Revision: 1.2 $
+|     $Date: 2004-12-12 14:16:25 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
 */
 
 class e107_event {
 	var $functions = array();
 	var $includes = array();
 
-	function register($eventname, $include, $function) {
+	function register($eventname, $function, $include) {
 		if ($include) {
 			$this->includes[$eventname][] = $include;
 		}
@@ -25,7 +29,7 @@ class e107_event {
 	}
 
 	function trigger($eventname, $data) {
-		foreach($this->includes[$eventname] as $evt_inc){
+		foreach($this->includes[$eventname] as $evt_inc) {
 			if (file_exists($evt_inc)) {
 				include_once($evt_inc);
 			}
