00bba583146b164b2430716079b73531b243dcd2
streaky <streaky@localhost>
2004-12-15 18:35:42 +0000

    Response to bug #463 - more of a feature improvement than a bugfix
    svn path=/trunk/e107/; revision=1155

--- e107_plugins/compliance_menu/compliance_menu.php
+++ e107_plugins/compliance_menu/compliance_menu.php
@@ -1,8 +1,8 @@
 <?php
 $text = "<div style='text-align:center'>
-<a href='http://validator.w3.org/check/referer'><img style='border:0' src='http://www.w3.org/Icons/valid-xhtml11' alt='Valid XHTML 1.1!' height='31' width='88' /></a>
+<a href='http://validator.w3.org/check?uri=".e_SELF.(e_QUERY ? '?'.e_QUERY : '')."'><img style='border:0' src='http://www.w3.org/Icons/valid-xhtml11' alt='Valid XHTML 1.1!' height='31' width='88' /></a>
 <br />
-<a href='http://jigsaw.w3.org/css-validator/'><img style='border:0' src='http://jigsaw.w3.org/css-validator/images/vcss' alt='Valid CSS!' height='31' width='88' /></a>
+<a href='http://jigsaw.w3.org/css-validator/validator?uri=".e_SELF.(e_QUERY ? '?'.e_QUERY : '')."'><img style='border:0' src='http://jigsaw.w3.org/css-validator/images/vcss' alt='Valid CSS!' height='31' width='88' /></a>
 </div>";
 $caption = (file_exists(THEME."images/compliance_menu.png") ? "<img src='".THEME."images/compliance_menu.png' alt='' /> ".COMPLIANCE_L1 : COMPLIANCE_L1);
 $ns -> tablerender($caption, $text);
