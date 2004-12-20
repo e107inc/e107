d3b40817d08d4a61265a6b22a80854a113309c4e
streaky <streaky@localhost>
2004-12-20 18:47:22 +0000

    *FINAL* (Honestly, it is.. o_O) fix for the articles issue, and a performance optimisation for the new user count :)
    svn path=/trunk/e107_0.7/; revision=1182

--- e107_plugins/login_menu/login_menu.php
+++ e107_plugins/login_menu/login_menu.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/login_menu.php,v $
-|     $Revision: 1.9 $
-|     $Date: 2004-12-17 21:34:36 $
-|     $Author: loloirie $
+|     $Revision: 1.10 $
+|     $Date: 2004-12-20 18:47:22 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
@@ -62,7 +62,7 @@ if(USER == TRUE || ADMIN == TRUE){
 	}
 	if(!$menu_pref['login_menu'] || $menu_pref['login_menu']['new_articles'] == true){
 		$new_articles = 0;
-		$new_articles = $sql -> db_Select('content', '*', 'content_type < 4 AND content_datestamp > '.$time);
+		$new_articles = $sql -> db_Select('content', '(content_class)', 'content_type = 0 AND `content_datestamp` > '.$time);
 		while($row = $sql -> db_Fetch()){
 			if(!check_class($row['content_class'])){
 				$new_articles--;
@@ -176,7 +176,7 @@ if(USER == TRUE || ADMIN == TRUE){
 		$NewItems[] = $new_forum.' '.($new_forum == 1 ? LOGIN_MENU_L20 : LOGIN_MENU_L21);
 	}
 	if(!$menu_pref['login_menu'] || $menu_pref['login_menu']['new_members'] == true){
-		$new_users = $sql -> db_Count('user', '(*)', 'WHERE `user_join` > '.$time.'\'');
+		$new_users = $sql -> db_Count('user', '(user_join)', 'WHERE user_join > '.$time);
 		$new_total += $new_users;
 		if(!$new_users){
 			$new_users = LOGIN_MENU_L26;
