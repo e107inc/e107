b3ffbd4ba055875bc3e73735df946491d80cbcc8
streaky <streaky@localhost>
2004-12-20 16:29:41 +0000

    Updated some function ref information
    svn path=/trunk/e107_0.7/; revision=1180

--- e107_handlers/mysql_class.php
+++ e107_handlers/mysql_class.php
@@ -12,8 +12,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/mysql_class.php,v $
-|     $Revision: 1.12 $
-|     $Date: 2004-12-19 02:26:46 $
+|     $Revision: 1.13 $
+|     $Date: 2004-12-20 16:29:41 $
 |     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
@@ -24,7 +24,7 @@ $db_time = 0.0;
 * MySQL Abstraction class
 *
 * @package e107
-* @version $Revision: 1.12 $
+* @version $Revision: 1.13 $
 * @author $Author: streaky $
 */
 class db {
@@ -43,6 +43,7 @@ class db {
 	/**
 	* @return db
 	* @desc db constructor - what does this do??
+	* @scope internal
 	*/
 	function db() {
 		global $pref;
@@ -85,6 +86,7 @@ class db {
 	* @return void
 	* @param unknown $sMarker
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Mark_Time($sMarker) {
 		if (E107_DEBUG_LEVEL > 0) {
@@ -96,6 +98,7 @@ class db {
 	/**
 	* @return void
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Show_Performance() {
 		if (E107_DEBUG_LEVEL > 0) {
@@ -108,6 +111,7 @@ class db {
 	* @param unknown $query
 	* @param unknown $rli
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Query($query, $rli = NULL) {
 		global $dbq, $e107_debug, $db_time, $sDBdbg, $aTimeMarks, $aDBbyTable, $curTimeMark;
@@ -131,34 +135,48 @@ class db {
 	}
 
 	/**
-	* @return unknown
-	* @param unknown $table
-	* @param unknown $fields
-	* @param unknown $arg
-	* @param unknown $mode
-	* @param unknown $debug
-	* @desc Enter description here...
+	* @return int Number of rows or false on error
+	* 
+	* @param string $table		Table name to select data from
+	* @param string $fields		Table fields to be retrieved, default * (all in table)
+	* @param string $arg		Query arguments, default null 
+	* @param string $mode		Argument has WHERE or not, default=default (WHERE)
+	* 
+	* @param bool $debug 		Debug mode on or off
+	* 
+	* @desc Perform a mysql_query() using the arguments suplied by calling db::db_Query()<br />
+	* <br />
+	* If you need more requests think to call the class.<br />
+	* <br />
+	* Example using a unique connection to database:<br />
+	* $sql->db_Select("comments", "*", "comment_item_id='$id' AND comment_type='1' ORDER BY comment_datestamp");<br />
+	* <br />
+	* OR as second connection:<br />
+	* $sql2 = new db;<br />
+	* $sql2->db_Select("chatbox", "*", "ORDER BY cb_datestamp DESC LIMIT $from, ".$view, $mode="no_where");<br />
+	* 
+	* @access public
 	*/
-	function db_Select($table, $fields = "*", $arg = '', $mode = "default", $debug = FALSE) {
+	function db_Select($table, $fields = '*', $arg = '', $mode = 'default', $debug = FALSE) {
 		$table = $this->db_IsLang($table);
 		$this->mySQLcurTable = $table;
-		if ($arg != '' && $mode == "default") {
+		if ($arg != '' && $mode == 'default') {
 			if ($debug) {
-				echo "SELECT ".$fields." FROM ".MPREFIX.$table." WHERE ".$arg."<br />";
+				echo 'SELECT '.$fields.' FROM '.MPREFIX.$table.' WHERE '.$arg.'<br />';
 			}
-			if ($this->mySQLresult = $this->db_Query("SELECT ".$fields." FROM ".MPREFIX.$table." WHERE ".$arg)) {
-				$this->dbError("dbQuery");
+			if ($this->mySQLresult = $this->db_Query('SELECT '.$fields.' FROM '.MPREFIX.$table.' WHERE '.$arg)) {
+				$this->dbError('dbQuery');
 				return $this->db_Rows();
 			} else {
 				$this->dbError("db_Select (SELECT $fields FROM ".MPREFIX."$table WHERE $arg)");
 				return FALSE;
 			}
-		} elseif($arg != '' && $mode != "default") {
+		} elseif($arg != '' && $mode != 'default') {
 			if ($debug) {
-				echo "@@SELECT ".$fields." FROM ".MPREFIX.$table." ".$arg."<br />";
+				echo '@@SELECT '.$fields.' FROM '.MPREFIX.$table.' '.$arg.'<br />';
 			}
-			if ($this->mySQLresult = $this->db_Query("SELECT ".$fields." FROM ".MPREFIX.$table." ".$arg)) {
-				$this->dbError("dbQuery");
+			if ($this->mySQLresult = $this->db_Query('SELECT '.$fields.' FROM '.MPREFIX.$table.' '.$arg)) {
+				$this->dbError('dbQuery');
 				return $this->db_Rows();
 			} else {
 				$this->dbError("db_Select (SELECT $fields FROM ".MPREFIX."$table $arg)");
@@ -166,10 +184,10 @@ class db {
 			}
 		} else {
 			if ($debug) {
-				echo "SELECT ".$fields." FROM ".MPREFIX.$table."<br />";
+				echo 'SELECT '.$fields.' FROM '.MPREFIX.$table.'<br />';
 			}
-			if ($this->mySQLresult = $this->db_Query("SELECT ".$fields." FROM ".MPREFIX.$table)) {
-				$this->dbError("dbQuery");
+			if ($this->mySQLresult = $this->db_Query('SELECT '.$fields.' FROM '.MPREFIX.$table)) {
+				$this->dbError('dbQuery');
 				return $this->db_Rows();
 			} else {
 				$this->dbError("db_Select (SELECT $fields FROM ".MPREFIX."$table)");
@@ -184,20 +202,21 @@ class db {
 	* @param unknown $arg
 	* @param unknown $debug
 	* @desc Enter description here...
+	* @scope public
 	*/
 	function db_Insert($table, $arg, $debug = FALSE) {
 		$table = $this->db_IsLang($table);
 
 		$this->mySQLcurTable = $table;
 		if ($debug) {
-			echo "INSERT INTO ".MPREFIX.$table." VALUES (".htmlentities($arg).")";
+			echo 'INSERT INTO '.MPREFIX.$table.' VALUES ('.htmlentities($arg).')';
 		}
 
-		if ($result = $this->mySQLresult = $this->db_Query("INSERT INTO ".MPREFIX.$table." VALUES (".$arg.")" )) {
+		if ($result = $this->mySQLresult = $this->db_Query('INSERT INTO '.MPREFIX.$table.' VALUES ('.$arg.')' )) {
 			$tmp = mysql_insert_id();
 
-			if (strstr(e_SELF, ADMINDIR) && $table != "online") {
-				mysql_query("INSERT INTO ".MPREFIX."tmp VALUES ('adminlog', '".time()."', '<br /><b>Insert</b> - <b>$table</b> table (field id <b>$tmp</b>)<br />by <b>".USERNAME."</b>') ");
+			if (strstr(e_SELF, ADMINDIR) && $table != 'online') {
+				mysql_query('INSERT INTO '.MPREFIX.'tmp VALUES (\'adminlog\', \''.time()."', '<br /><b>Insert</b> - <b>$table</b> table (field id <b>$tmp</b>)<br />by <b>".USERNAME."</b>') ");
 			}
 			return $tmp;
 		} else {
@@ -212,19 +231,20 @@ class db {
 	* @param unknown $arg
 	* @param unknown $debug
 	* @desc Enter description here...
+	* @scope public
 	*/
 	function db_Update($table, $arg, $debug = FALSE) {
 		$table = $this->db_IsLang($table);
 		$this->mySQLcurTable = $table;
 		if ($debug) {
-			echo "UPDATE ".MPREFIX.$table." SET ".$arg."<br />";
+			echo 'UPDATE '.MPREFIX.$table.' SET '.$arg.'<br />';
 		}
-		if ($result = $this->mySQLresult = $this->db_Query("UPDATE ".MPREFIX.$table." SET ".$arg)) {
+		if ($result = $this->mySQLresult = $this->db_Query('UPDATE '.MPREFIX.$table.' SET '.$arg)) {
 			$result = mysql_affected_rows();
-			if (strstr(e_SELF, ADMINDIR) && $table != "online") {
-				if (!strstr($arg, "link_order")) {
-					$str = addslashes(str_replace("WHERE", '', substr($arg, strpos($arg, "WHERE"))));
-					mysql_query("INSERT INTO ".MPREFIX."tmp VALUES ('adminlog', '".time()."', '<br /><b>Update</b> - <b>$table</b> table (string: <b>$str</b>)<br />by <b>".USERNAME."</b>') ");
+			if (strstr(e_SELF, ADMINDIR) && $table != 'online') {
+				if (!strstr($arg, 'link_order')) {
+					$str = addslashes(str_replace('WHERE', '', substr($arg, strpos($arg, 'WHERE'))));
+					mysql_query('INSERT INTO '.MPREFIX."tmp VALUES ('adminlog', '".time()."', '<br /><b>Update</b> - <b>$table</b> table (string: <b>$str</b>)<br />by <b>".USERNAME."</b>') ");
 				}
 			}
 			return $result;
@@ -238,18 +258,19 @@ class db {
 	* @return unknown
 	* @param unknown $mode
 	* @desc Enter description here...
+	* @scope public
 	*/
-	function db_Fetch($mode = "strip") {
+	function db_Fetch($mode = 'strip') {
 		if ($row = @mysql_fetch_array($this->mySQLresult)) {
 			if ($mode == 'strip') {
 				while (list($key, $val) = each($row)) {
 					$row[$key] = stripslashes($val);
 				}
 			}
-			$this->dbError("db_Fetch");
+			$this->dbError('db_Fetch');
 			return $row;
 		} else {
-			$this->dbError("db_Fetch");
+			$this->dbError('db_Fetch');
 			return FALSE;
 
 		}
@@ -261,11 +282,12 @@ class db {
 	* @param unknown $fields
 	* @param unknown $arg
 	* @desc Enter description here...
+	* @scope public
 	*/
-	function db_Count($table, $fields = "(*)", $arg = '') {
+	function db_Count($table, $fields = '(*)', $arg = '') {
 		$table = $this->db_IsLang($table);
 		$this->mySQLcurTable = $table;
-		if ($fields == "generic") {
+		if ($fields == 'generic') {
 			if ($this->mySQLresult = $this->db_Query($table)) {
 				$rows = $this->mySQLrows = @mysql_fetch_array($this->mySQLresult);
 				return $rows[0];
@@ -273,7 +295,7 @@ class db {
 				$this->dbError("dbCount ($query)");
 			}
 		}
-		if ($this->mySQLresult = $this->db_Query("SELECT COUNT".$fields." FROM ".MPREFIX.$table." ".$arg)) {
+		if ($this->mySQLresult = $this->db_Query('SELECT COUNT'.$fields.' FROM '.MPREFIX.$table.' '.$arg)) {
 			$rows = $this->mySQLrows = @mysql_fetch_array($this->mySQLresult);
 			return $rows[0];
 		} else {
@@ -284,10 +306,11 @@ class db {
 	/**
 	* @return void
 	* @desc Enter description here...
+	* @scope public
 	*/
 	function db_Close() {
 		mysql_close();
-		$this->dbError("dbClose");
+		$this->dbError('dbClose');
 	}
 
 	/**
@@ -295,13 +318,14 @@ class db {
 	* @param unknown $table
 	* @param unknown $arg
 	* @desc Enter description here...
+	* @scope public
 	*/
 	function db_Delete($table, $arg = '') {
 		$table = $this->db_IsLang($table);
 		$this->mySQLcurTable = $table;
 		if (!$arg) {
-			if ($result = $this->mySQLresult = $this->db_Query("DELETE FROM ".MPREFIX.$table)) {
-				if (strstr(e_SELF, ADMINDIR) && $table != "online" && $table != "tmp") {
+			if ($result = $this->mySQLresult = $this->db_Query('DELETE FROM '.MPREFIX.$table)) {
+				if (strstr(e_SELF, ADMINDIR) && $table != 'online' && $table != 'tmp') {
 					$str = addslashes(str_replace('WHERE', '', substr($arg, strpos($arg, 'WHERE'))));
 					mysql_query('INSERT INTO '.MPREFIX."tmp VALUES ('adminlog', '".time()."', '$string') ");
 				}
@@ -313,7 +337,7 @@ class db {
 		} else {
 			if ($result = $this->mySQLresult = $this->db_Query('DELETE FROM '.MPREFIX.$table.' WHERE '.$arg)) {
 				$tmp = mysql_affected_rows();
-				if (strstr(e_SELF, ADMINDIR) && $table != "online" && $table != "tmp") {
+				if (strstr(e_SELF, ADMINDIR) && $table != 'online' && $table != 'tmp') {
 					$str = addslashes(str_replace('WHERE', '', substr($arg, strpos($arg, 'WHERE'))));
 					mysql_query('INSERT INTO '.MPREFIX."tmp VALUES ('adminlog', '".time()."', '<b>Delete</b> - <b>$table</b> table (string: <b>$str</b>) by <b>".USERNAME."</b>') ");
 				}
@@ -328,6 +352,7 @@ class db {
 	/**
 	* @return unknown
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Rows() {
 		$rows = $this->mySQLrows = @mysql_num_rows($this->mySQLresult);
@@ -339,6 +364,7 @@ class db {
 	* @return unknown
 	* @param unknown $from
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function dbError($from) {
 		if ($error_message = @mysql_error()) {
@@ -353,6 +379,7 @@ class db {
 	* @return void
 	* @param unknown $mode
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_SetErrorReporting($mode) {
 		$this->mySQLerror = $mode;
@@ -363,6 +390,7 @@ class db {
 	* @return unknown
 	* @param unknown $arg
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Select_gen($arg) {
 		if ($this->mySQLresult = $this->db_Query($arg)) {
@@ -378,6 +406,7 @@ class db {
 	* @return unknown
 	* @param unknown $offset
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Fieldname($offset) {
 		$result = @mysql_field_name($this->mySQLresult, $offset);
@@ -387,6 +416,7 @@ class db {
 	/**
 	* @return unknown
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Field_info() {
 		$result = @mysql_fetch_field($this->mySQLresult);
@@ -396,6 +426,7 @@ class db {
 	/**
 	* @return unknown
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_Num_fields() {
 		$result = @mysql_num_fields($this->mySQLresult);
@@ -406,6 +437,7 @@ class db {
 	* @return unknown
 	* @param unknown $table
 	* @desc Enter description here...
+	* @scope internal
 	*/
 	function db_IsLang($table) {
 		global $pref, $mySQLtablelist;
