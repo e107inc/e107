38c91560c1b81f23faa7198dd699d0522d952e72
pholzmann <pholzmann@localhost>
2004-12-23 15:49:22 +0000

    Just a test. Now it's gone again. Wonder if any trace left behind?
    svn path=/trunk/e107/; revision=1195

--- e107_themes/templates/plhtest.php
+++
@@ -1,7 +0,0 @@
-<? echo "plh test - can i check this in?
-yes... did that.
-
-now to mod and checkin. -- yay!
-then get pageant working -- hate always typing pw's. -- got that too (needed command line ref to pvt key file, that's all)
-then delete.
-"; ?>
