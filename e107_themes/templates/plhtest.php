8b7410460ca199a67942012c111669cda9918e25
pholzmann <pholzmann@localhost>
2004-12-23 15:47:21 +0000

    Just a test. Did an update. And now pageant takes care of passphrase.
    svn path=/trunk/e107/; revision=1194

--- e107_themes/templates/plhtest.php
+++ e107_themes/templates/plhtest.php
@@ -1,8 +1,7 @@
 <? echo "plh test - can i check this in?
 yes... did that.
 
-now to mod.
-then checkin.
-then get pageant working -- hate always typing pw's.
+now to mod and checkin. -- yay!
+then get pageant working -- hate always typing pw's. -- got that too (needed command line ref to pvt key file, that's all)
 then delete.
 "; ?>
