8f5922d2357652bf16c5820c5ece2b5255251624
pholzmann <pholzmann@localhost>
2004-12-23 15:43:52 +0000

    Just a test. My first CVS commit ever! ;)
    svn path=/trunk/e107/; revision=1193

--- e107_themes/templates/plhtest.php
+++ e107_themes/templates/plhtest.php
@@ -1,1 +1,8 @@
-<? echo "plh test - can i check this in?" ?>
+<? echo "plh test - can i check this in?
+yes... did that.
+
+now to mod.
+then checkin.
+then get pageant working -- hate always typing pw's.
+then delete.
+"; ?>
