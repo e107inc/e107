8316462f89bdf17a0aa1b3be93187b231b55002c
streaky <streaky@localhost>
2004-12-24 21:47:12 +0000

    Removed support for PHP < 4.3.0 from e107, nobody is using less, and nobody *should* be using less..
    svn path=/trunk/e107_0.7/; revision=1200

--- e107_handlers/PHP_Compat_handler.php
+++ e107_handlers/PHP_Compat_handler.php
@@ -1,18 +1,8 @@
 <?php
 
-if (!function_exists('file_get_contents')) {
-	/**
-	* @return string
-	* @param string $filename
-	* @desc Reads entire file into a string
-	*/
-	function file_get_contents($filename) {
-		$fd = fopen("$filename", "rb");
-		$content = fread($fd, filesize($filename));
-		fclose($fd);
-		return $content;
-	}
-}
+// e107 requires PHP > 4.3.0, all functions that are used in e107, introduced in newer
+// versions than that should be recreated in here for compatabilty reasons..
+
 if (!function_exists('file_put_contents')) {
 	/**
 	* @return int
@@ -31,36 +21,5 @@ if (!function_exists('file_put_contents')) {
 		return $bytes;
 	}
 }
-if (!defined('ENT_NOQUOTES')) {
-	define('ENT_NOQUOTES', 0);
-}
-if (!defined('ENT_COMPAT')) {
-	define('ENT_COMPAT', 2);
-}
-if (!defined('ENT_QUOTES')) {
-	define('ENT_QUOTES', 3);
-}
-
-if (!function_exists('html_entity_decode')) {
-	function html_entity_decode($string, $quote_style = ENT_COMPAT, $charset = null) {
-		if (!is_int($quote_style)) {
-			echo 'html_entity_decode() expects parameter 2 to be long, '.gettype($quote_style).' given';
-			return;
-		}
-		$trans_tbl = get_html_translation_table(HTML_ENTITIES);
-		$trans_tbl = array_flip($trans_tbl);
-		$trans_tbl['&#039;'] = '\'';
-		if ($quote_style & ENT_NOQUOTES) {
-			unset($trans_tbl['&quot;']);
-		}
-		return strtr($string, $trans_tbl);
-	}
-}
-if (!function_exists('ob_get_level')) {
-	function ob_get_level(){
-		return 0;
-	}
-}
-
 
 ?>
