36fbe1ff8ea09ad5f643844f82bb55f56fbea343
pholzmann <pholzmann@localhost>
2005-01-05 10:35:42 +0000

    error handling fixes - give user useful msg
    svn path=/trunk/e107_0.7/; revision=1228

--- error.php
+++ error.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/error.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:12:45 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-01-05 10:35:22 $
+|     $Author: pholzmann $
 +----------------------------------------------------------------------------+
 */
 
@@ -21,6 +21,9 @@
 
 require_once("class2.php");
 require_once(HEADERF);
+
+$errFrom=$_SERVER['HTTP_REFERER'];
+$errTo  =$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 switch(e_QUERY){
         case 401:
@@ -31,16 +34,19 @@ switch(e_QUERY){
                 $text = "<div class='installe'>".LAN_4."</div><br /><div class='installh'>".LAN_5."</div><br /><div class='smalltext'>".LAN_6."</div>
                 <br /><div class='installh'>".LAN_2."<a href='index.php'>".LAN_20."</a></div>";
         break;
+        default:
         case 404:
                 $text = "<div class='installe'>".LAN_7."</div><br /><div class='installh'>".LAN_8."</div><br /><div class='smalltext'>".LAN_9."</div>
-                <br /><div class='installh'>".LAN_2."<a href='index.php'>".LAN_20."</a></div>";
+                <br /><div class='installh'>".LAN_2."
+                <br />Your unsuccessful attempt to access <b>{$errTo}</b> has been recorded.";
+                if (strlen($errFrom)) $text .= "<br />Apparently, you were referred here by $errFrom -- Unfortunately, there's an obsolete or just plain wrong link at that address.";
+                $text .="<br /> <a href='index.php'>".LAN_20."</a></div>";
         break;
         case 500:
                 $text = "<div class='installe'>".LAN_10."</div><br /><div class='installh'>".LAN_11."</div><br /><div class='smalltext'>".LAN_12."</div>
                 <br /><div class='installh'>".LAN_2."<a href='index.php'>".LAN_20."</a></div>";
         break;
-        default:
-                $text = "<div class='installe'>".LAN_13." (".$_SERVER['QUERY_STRING'].")</div><br /><div class='installh'>".LAN_14."</div><br /><div class='smalltext'>".LAN_15."</div>
+                 $text = "<div class='installe'>".LAN_13." (".$_SERVER['QUERY_STRING'].")</div><br /><div class='installh'>".LAN_14."</div><br /><div class='smalltext'>".LAN_15."</div>
                 <br /><div class='installh'>".LAN_2."<a href='index.php'>".LAN_20."</a></div>";
 }
 $ns -> tablerender(PAGE_NAME." ".e_QUERY, $text);
