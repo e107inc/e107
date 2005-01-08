3193dfaeba5b77e8084cff9d00699c7a4bded671
e107coders <e107coders@localhost>
2005-01-08 02:32:59 +0000

    Pref added and Lan changes.
    svn path=/trunk/e107_0.7/; revision=1257

--- e107_languages/English/admin/lan_language.php
+++ e107_languages/English/admin/lan_language.php
@@ -1,4 +1,14 @@
 <?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_language.php,v $
+|     $Revision: 1.3 $
+|     $Date: 2005-01-08 02:32:59 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
 
 define("LANG_LAN_00","could not be created.(already exists)");
 define("LANG_LAN_01","was deleted(if existing) and created.");
@@ -7,15 +17,15 @@ define("LANG_LAN_03","Tables");
 define("LANG_LAN_04","Options");
 define("LANG_LAN_05","Not Installed");
 
-
 define("LANG_LAN_06", "Create tables");
 define("LANG_LAN_07", "Drop existing tables?");
 define("LANG_LAN_08", "Replace existing tables (data will be lost).");
 
 define("LANG_LAN_10", "Confirm delete");
 define("LANG_LAN_11", "Delete unchecked tables above (if they exist).");
-define("LANG_LAN_12", "Enable Multi-Language");
+define("LANG_LAN_12", "Enable Multi-Language Tables");
 define("LANG_LAN_13", "Multi-Language Preferences");
 
+define("LANG_LAN_14", "Default Site Language");
 
 ?>
