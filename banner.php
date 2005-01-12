71bd2d4cb647cf34e2486dcabd98b6e3fa80814b
lisa_ <lisa_@localhost>
2005-01-12 10:57:08 +0000

    banner area and menu upgraded; added visibility and userclass for each banner and for each campaign. Added multipe banners in menu and the use of multiple campaigns
    svn path=/trunk/e107_0.7/; revision=1296

--- banner.php
+++ banner.php
@@ -11,100 +11,95 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/banner.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:09:30 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-01-12 10:56:22 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
 
 if(e_QUERY){
-        $sql -> db_Select("banner", "*", "banner_id='".e_QUERY."' ");
-        $row = $sql -> db_Fetch(); extract($row);
-        $ip = getip();
-        $newip = (preg_match("/".$ip."\^/", $banner_ip) ? $banner_ip : $banner_ip.$ip."^");
-        $sql -> db_Update("banner", "banner_clicks=banner_clicks+1, banner_ip='$newip' WHERE banner_id='".e_QUERY."' ");
-        header("location: ".$banner_clickurl);
-        exit;
+	$sql -> db_Select("banner", "*", "banner_id='".e_QUERY."' ");
+	$row = $sql -> db_Fetch(); extract($row);
+	$ip = getip();
+	$newip = (preg_match("/".$ip."\^/", $banner_ip) ? $banner_ip : $banner_ip.$ip."^");
+	$sql -> db_Update("banner", "banner_clicks=banner_clicks+1, banner_ip='$newip' WHERE banner_id='".e_QUERY."' ");
+	header("location: ".$banner_clickurl);
+	exit;
 }
 
 require_once(HEADERF);
 
 if(IsSet($_POST['clientsubmit'])){
 
-        if(!$sql -> db_Select("banner", "*", "banner_clientlogin='".$_POST['clientlogin']."' AND banner_clientpassword='".$_POST['clientpassword']."' ")){
-                $ns -> tablerender("Error", "<br /><div style='text-align:center'>".LAN_20."</div><br />");
-                require_once(FOOTERF);
-                exit;
-        }
+		if(!$sql -> db_Select("banner", "*", "banner_clientlogin='".$_POST['clientlogin']."' AND banner_clientpassword='".$_POST['clientpassword']."' ")){
+			$ns -> tablerender("Error", "<br /><div style='text-align:center'>".LAN_20."</div><br />");
+			require_once(FOOTERF);
+			exit;
+		}
 
-        $row = $sql -> db_Fetch(); extract($row);
+		$row = $sql -> db_Fetch(); extract($row);
 
-        $banner_total = $sql -> db_Select("banner", "*", "banner_clientname='$banner_clientname' ");
+		$banner_total = $sql -> db_Select("banner", "*", "banner_clientname='$banner_clientname' ");
 
-        $text = "<table class='fborder' style='width:98%'>
-        <tr><td colspan='7' style='text-align:center' class='fcaption'>".LAN_21."</td></tr>
-        <tr>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_22."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_23."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_24."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_25."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_26."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_27."</span></td>
-        <td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_28."</span></td>
-        </tr>";
+		$text = "<table class='fborder' style='width:98%'>
+		<tr><td colspan='7' style='text-align:center' class='fcaption'>".LAN_21."</td></tr>
+		<tr>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_22."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_23."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_24."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_25."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_26."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_27."</span></td>
+		<td class='forumheader' style='text-align:center'><span class='smallblacktext'>".LAN_28."</span></td>
+		</tr>";
 
-        if(!$banner_total){
-                $text .= "<tr>
-                <td colspan='7' class='forumheader2' style='text-align:center'>".LAN_29."</td>";
-        }else{
-                while($row = $sql-> db_Fetch()){
-                        extract($row);
+		if(!$banner_total){
+			$text .= "<tr><td colspan='7' class='forumheader2' style='text-align:center'>".LAN_29."</td>";
+		}else{
+			while($row = $sql-> db_Fetch()){
+				extract($row);
 
-                        $clickpercentage = ($banner_clicks && $banner_impressions ? round(($banner_clicks / $banner_impressions) * 100)."%" : "-");
-                        $impressions_left = ($banner_impurchased ? $banner_impurchased - $banner_impressions : LAN_30);
-                        $impressions_purchased = ($banner_impurchased ? $banner_impurchased : LAN_30);
+				$clickpercentage = ($banner_clicks && $banner_impressions ? round(($banner_clicks / $banner_impressions) * 100)."%" : "-");
+				$impressions_left = ($banner_impurchased ? $banner_impurchased - $banner_impressions : LAN_30);
+				$impressions_purchased = ($banner_impurchased ? $banner_impurchased : LAN_30);
 
-                        $start_date = ($banner_startdate ? strftime("%d %B %Y", $banner_startdate) : LAN_31);
-                        $end_date = ($banner_enddate ? strftime("%d %B %Y", $banner_enddate) : LAN_31);
+				$start_date = ($banner_startdate ? strftime("%d %B %Y", $banner_startdate) : LAN_31);
+				$end_date = ($banner_enddate ? strftime("%d %B %Y", $banner_enddate) : LAN_31);
 
-                        $text.="<tr>
-                        <td class='forumheader3' style='text-align:center'>".$banner_clientname."</td>
-                        <td class='forumheader3' style='text-align:center'>".$banner_id."</td>
-                        <td class='forumheader3' style='text-align:center'>".$banner_clicks."</td>
-                        <td class='forumheader3' style='text-align:center'>".$clickpercentage."</td>
-                        <td class='forumheader3' style='text-align:center'>".$banner_impressions."</td>
-                        <td class='forumheader3' style='text-align:center'>".$impressions_purchased."</td>
-                        <td class='forumheader3' style='text-align:center'>".$impressions_left."</td>
-                        </tr>
-                        <td colspan='7' class='forumheader3' style='text-align:center'>
+				$text.= "<tr>
+				<td class='forumheader3' style='text-align:center'>".$banner_clientname."</td>
+				<td class='forumheader3' style='text-align:center'>".$banner_id."</td>
+				<td class='forumheader3' style='text-align:center'>".$banner_clicks."</td>
+				<td class='forumheader3' style='text-align:center'>".$clickpercentage."</td>
+				<td class='forumheader3' style='text-align:center'>".$banner_impressions."</td>
+				<td class='forumheader3' style='text-align:center'>".$impressions_purchased."</td>
+				<td class='forumheader3' style='text-align:center'>".$impressions_left."</td>
+				</tr>
+				<tr>
+				<td class='forumheader3' style='text-align:center'>&nbsp;</td>
+				<td colspan='2' class='forumheader3' style='text-align:center'><span class='smalltext'>".LAN_36.($banner_active != "255" ? LAN_32 : "<b>".LAN_33."</b>")."</span></td>
+				<td colspan='4' class='forumheader3' style='text-align:center'><span class='smalltext'>".LAN_37. $start_date.", ".LAN_34.": ".$end_date."</span></td>";
 
-                        ".LAN_36. ($banner_active ? LAN_32 : "<b>".LAN_33."</b>")." |
+				if($banner_ip){
+					$tmp = explode("^", $banner_ip);
+					$text .= "<tr><td class='forumheader3'>".LAN_35.": ".(count($tmp)-1)."</td>
+					<td colspan='6' class='forumheader3'>";
+					for($a=0; $a<=(count($tmp)-2); $a++){
+						$text .= $tmp[$a]."<br />";
+					}
+				}
 
-                        ".LAN_36. $start_date.", ".LAN_34.": ".$end_date."</td></tr>";
+				$text .= "</td><tr><td colspan='7'>&nbsp;</td></tr>";		
+			}
+		}
 
-                        if($banner_ip){
-                                $tmp = explode("^", $banner_ip);
-                                $text .= "<tr><td class='forumheader3'>
-                                ".LAN_35.": ".(count($tmp)-1)."</td>
-                                <td colspan='6' class='forumheader3'>";
-                                for($a=0; $a<=(count($tmp)-2); $a++){
-                                        $text .= $tmp[$a]."<br />";
-                                }
-                        }
+		$text .= "</table>";
 
+		echo $text;
 
-                        $text .= "</td>
-                        <tr><td colspan='8'>&nbsp;</td></tr>";
-                }
-        }
-
-        $text .= "</table>";
-
-        echo $text;
-
-        require_once(FOOTERF);
-        exit;
+		require_once(FOOTERF);
+		exit;
 }
 
 $text = "<div style='align:center'>\n
@@ -125,6 +120,7 @@ $text = "<div style='align:center'>\n
 </td>
 </tr>
 </table></form></div>";
+
 $ns -> tablerender(LAN_19, $text);
 require_once(FOOTERF);
 
--- e107_plugins/banner_menu/config.php
+++ e107_plugins/banner_menu/config.php
@@ -11,26 +11,49 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/banner_menu/config.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-01-05 10:23:00 $
-|     $Author: pholzmann $
+|     $Revision: 1.3 $
+|     $Date: 2005-01-12 10:57:08 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 require_once("../../class2.php");
 if(!getperms("1")){ header("location:".e_BASE."index.php"); exit ;}
 require_once(e_ADMIN."auth.php");
+require_once(e_HANDLER."form_handler.php");
+$rs = new form;
 
 @include_once(e_PLUGIN."banner_menu/languages/".e_LANGUAGE.".php");
 @include_once(e_PLUGIN."banner_menu/languages/English.php");
 
+$lan_file = e_LANGUAGEDIR.e_LANGUAGE."/admin/lan_menus.php";
+include(file_exists($lan_file) ? $lan_file : e_LANGUAGEDIR.e_LANGUAGE."/admin/lan_menus.php");
+
+if(e_QUERY){
+	$tmp = explode(".", e_QUERY);
+	$action = $tmp[0];
+	$sub_action = $tmp[1];
+	$id = $tmp[2];
+	unset($tmp);
+}
+
 if(IsSet($_POST['update_menu'])){
-        foreach($_POST as $k => $v){
-                if(preg_match("#^banner_#",$k)){
-                        $menu_pref[$k] = $v;
-                }
-        }
+	foreach($_POST as $k => $v){
+		if(preg_match("#^banner_#",$k)){
+			$menu_pref[$k] = $v;
+		}
+	}
+
+	if($_POST['catid']){
+		$array_cat = explode("-", $_POST['catid']);
+		for($i=0;$i<count($array_cat);$i++){
+			$cat .= $array_cat[$i]."|";
+		}
+		$cat = substr($cat,0,-1);
+		$menu_pref['banner_campaign'] = $cat;
+	}
+
 	$sysprefs->setArray('menu_pref');
-        $ns -> tablerender("", "<div style='text-align:center'><b>".BANNER_MENU_L2."</b></div>");
+	$ns -> tablerender("", "<div style='text-align:center'><b>".BANNER_MENU_L2."</b></div>");
 }
 
 if(!$menu_pref['banner_caption']){
@@ -38,7 +61,7 @@ if(!$menu_pref['banner_caption']){
 }
 
 $text = "<div style='text-align:center'>
-<form method='post' action='".e_SELF."?".e_QUERY."' name='menu_conf_form'>
+<form method='post' action='".e_SELF."' name='menu_conf_form'>
 <table style='width:85%' class='fborder' >
 
 <tr>
@@ -46,23 +69,186 @@ $text = "<div style='text-align:center'>
 <td style='width:60%' class='forumheader3'>
 <input class='tbox' type='text' name='banner_caption' size='20' value='".$menu_pref['banner_caption']."' maxlength='100' />
 </td>
-</tr>
+</tr>";
 
+$array_cat_in = explode("|", $menu_pref['banner_campaign']);
 
+$c=0; $d=0;
+$sql2 = new db;
+$category_total = $sql2 -> db_Select("banner", "DISTINCT(SUBSTRING_INDEX(banner_campaign, '^', 1)) as banner_campaign", "ORDER BY banner_campaign", "mode=no_where");
+while($row = $sql2 -> db_Fetch()){
+extract($row);
+	if(in_array($banner_campaign, $array_cat_in)){
+		$in_catname[$c] = $banner_campaign;
+		$c++;
+	}else{
+		$out_catname[$d] = $banner_campaign;
+		$d++;
+	}
+}
+
+$text .= "
 <tr>
-<td style='width:40%' class='forumheader3'>".BANNER_MENU_L4.": </td>
+<td style='width:40%' class='forumheader3'>".BANNER_MENU_L6."</td>
 <td style='width:60%' class='forumheader3'>
-<input class='tbox' type='text' name='banner_campaign' size='30' value='".$menu_pref['banner_campaign']."' maxlength='100' />
+
+	<table style='width:90%'>
+	<tr>
+	<td style='width:45%; vertical-align:top'>".BANNER_MENU_L7."<br />
+	<select class='tbox' id='catout' name='catout' size='10' style='width:180px' multiple='multiple' onchange='moveOver();'>";
+	for($a=0; $a<=($d-1); $a++){
+		$text .= "<option value='".$out_catname[$a]."'>".$out_catname[$a]."</option>";
+	}
+	$text .= "</select>
+	</td>
+	<td style='width:45%; vertical-align:top'>".BANNER_MENU_L8."<br />
+	<select class='tbox' id='catin' name='catin' size='10' style='width:180px' multiple='multiple'>";
+	for($a=0; $a<=($c-1); $a++){
+		$catidvalues .= $in_catname[$a]."-";
+		$text .= "<option value='".$in_catname[$a]."'>".$in_catname[$a]."</option>";
+	}
+	$catidvalues = substr($catidvalues,0,-1);
+	$text .= "</select><br /><br />
+	<input class='button' type='button' value='".BANNER_MENU_L9."' onclick='removeMe();' />
+	<input type='hidden' name='catid' id='catid' value='".$catidvalues."'>
+	</td>
+	</tr>
+	</table>
+
 </td>
 </tr>
 
 <tr>
-<td colspan='2' class='forumheader' style='text-align:center'><input class='button' type='submit' name='update_menu' value='Update Menu Settings' /></td>
+<td style='width:40%' class='forumheader3'>".BANNER_MENU_L10."</td>
+<td style='width:60%' class='forumheader3'>
+	<select class='tbox' id='banner_rendertype' name='banner_rendertype' size='1'  >
+	".$rs -> form_option(BANNER_MENU_L11, (!$menu_pref['banner_rendertype'] || $menu_pref['banner_rendertype'] == "0" ? "1" : "0"), 0)."
+	".$rs -> form_option("1 - ".BANNER_MENU_L12."", ($menu_pref['banner_rendertype'] == "1" ? "1" : "0"), 1)."
+	".$rs -> form_option("2 - ".BANNER_MENU_L13."", ($menu_pref['banner_rendertype'] == "2" ? "1" : "0"), 2)."
+	".$rs -> form_option("3 - ".BANNER_MENU_L14."", ($menu_pref['banner_rendertype'] == "3" ? "1" : "0"), 3)."
+	".$rs -> form_select_close()."
+</td>
 </tr>
+
+<tr>
+<td style='width:40%' class='forumheader3'>".BANNER_MENU_L15."<br /><span class='smalltext' style='font-style:italic;'>".BANNER_MENU_L16."</span></td>
+<td style='width:60%' class='forumheader3'>
+	<select class='tbox' id='banner_amount' name='banner_amount' size='1'  >
+	".$rs -> form_option(BANNER_MENU_L17, (!$menu_pref['banner_amount'] ? "1" : "0"), 0);
+	for($b=1; $b<6; $b++){
+		$text .= $rs -> form_option($b, ($menu_pref['banner_amount'] == $b ? "1" : "0"), $b);
+	}
+	$text .= $rs -> form_select_close()."
+</td>
+</tr>
+
+<tr>
+<td colspan='2' class='forumheader' style='text-align:center'><input class='button' type='submit' name='update_menu' value='".BANNER_MENU_L18."' /></td>
+</tr>
+
 </table>
 </form>
 </div>";
+
 $ns -> tablerender(BANNER_MENU_L5, $text);
 
+
 require_once(e_ADMIN."footer.php");
+
+function headerjs(){
+
+$script_js= "<script type=\"text/javascript\">
+//<!--
+//<!-- Adapted from original:  Kathi O'Shea (Kathi.O'Shea@internet.com) -->
+
+function moveOver(){
+	var boxLength = document.getElementById('catin').length;
+	var selectedItem = document.getElementById('catout').selectedIndex;
+	var selectedText = document.getElementById('catout').options[selectedItem].text;
+	var selectedValue = document.getElementById('catout').options[selectedItem].value;
+
+	var i;
+	var isNew = true;
+	if (boxLength != 0) {
+		for (i = 0; i < boxLength; i++) {
+			thisitem = document.getElementById('catin').options[i].text;
+			if (thisitem == selectedText) {
+				isNew = false;
+				break;
+			}
+		}
+	}
+	if (isNew) {
+		newoption = new Option(selectedText, selectedValue, false, false);
+		document.getElementById('catin').options[boxLength] = newoption;
+		document.getElementById('catout').options[selectedItem].text = '';
+	}
+	document.getElementById('catout').selectedIndex=-1;
+	
+	saveMe();
+}
+
+function removeMe() {
+	var boxLength = document.getElementById('catin').length;
+	var boxLength2 = document.getElementById('catout').length;
+	arrSelected = new Array();
+	var count = 0;
+	for (i = 0; i < boxLength; i++) {
+		if (document.getElementById('catin').options[i].selected) {
+			arrSelected[count] = document.getElementById('catin').options[i].value;
+			var valname = document.getElementById('catin').options[i].text;
+			for (j = 0; j < boxLength2; j++) {
+				if(document.getElementById('catout').options[j].value == arrSelected[count]){
+					document.getElementById('catout').options[j].text = valname;
+				}
+			}
+		}
+		count++;
+	}
+	var x;
+	for (i = 0; i < boxLength; i++) {
+		for (x = 0; x < arrSelected.length; x++) {
+			if (document.getElementById('catin').options[i].value == arrSelected[x]) {
+				document.getElementById('catin').options[i] = null;
+			}
+		}
+		boxLength = document.getElementById('catin').length;
+	}
+
+	saveMe();
+}
+
+//function clearMe(clid){
+//	location.href = document.location + \"?clear.\" + clid;
+//}
+
+function saveMe(clid) {
+	var strValues = \"\";
+	var boxLength = document.getElementById('catin').length;
+	var count = 0;
+	if (boxLength != 0) {
+		for (i = 0; i < boxLength; i++) {
+			if (count == 0) {
+				strValues = document.getElementById('catin').options[i].value;
+			}
+			else {
+				strValues = strValues + \"-\" + document.getElementById('catin').options[i].value;
+			}
+			count++;
+		}
+	}
+	if (strValues.length == 0) {
+		//alert(\"You have not made any selections\");
+		document.getElementById('catid').value = \"\";
+	}
+	else {
+		document.getElementById('catid').value = strValues;
+	}
+}
+
+// -->
+</script>\n";
+ return $script_js;
+}
+
 ?>
