645ab61babff19bed602eba9747a2fa2d7bc459f
e107steved <steved@e107.org>
2005-01-22 16:48:39 +0000

    Added ability to add a new date header to news posts. If the pref option in admin -> news -> prefs is ticked, news items posted on a new day will have a date header rendered, this is useful to distinguish news items posted on different days. If no class is defined in theme.php, the default class of nextprev will be used for the new date header.
    svn path=/trunk/e107_0.7/; revision=1407

--- e107_languages/English/admin/lan_newspost.php
+++ e107_languages/English/admin/lan_newspost.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_newspost.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2004-12-27 22:36:24 $
-|     $Author: loloirie $
+|     $Revision: 1.3 $
+|     $Date: 2005-01-22 16:48:39 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 define("NWSLAN_1", "News story deleted.");
@@ -118,6 +118,9 @@ define("NWSLAN_107", "Enable HtmlArea on Submit-News page.");
 define("NWSLAN_108","Display messages instead not allowed news");
 define("NWSLAN_109","Check the box if you want to display a standard message for visitors not allowed to see a news (userclass restriction) instead the news itself.");
 define("NWSLAN_110","is not writable, you need to CHMOD 777 the folder before uploading");
+
+define("NWSLAN_111", "Show new date header");
+define("NWSLAN_112", "If this box is ticked, a box containing the date will be displayed above news items posted on a new day, useful for distinguishing posts on different days");
 
 define("LAN_NEWS_5", "Error! - Was unable to update news item into database!");
 define("LAN_NEWS_6", "News entered into database.");
