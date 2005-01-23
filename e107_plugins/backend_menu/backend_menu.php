3d9780ace1b313f8a260a393785f8854f682af54
e107steved <steved@e107.org>
2005-01-23 10:08:42 +0000

    Updated backend menu to reflect new rss.php script.
    svn path=/trunk/e107_0.7/; revision=1416

--- e107_plugins/backend_menu/backend_menu.php
+++ e107_plugins/backend_menu/backend_menu.php
@@ -11,15 +11,21 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/backend_menu/backend_menu.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2004-12-13 13:20:42 $
-|     $Author: sweetas $
+|     $Revision: 1.3 $
+|     $Date: 2005-01-23 10:08:41 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 global $FILES_DIRECTORY;
 $text = "<div style='text-align:center' class='smalltext'>
 ".BACKEND_MENU_L1."<br />
-<a href='".SITEURL.$FILES_DIRECTORY."backend/news.xml'>news.xml</a> - <a href='".SITEURL.$FILES_DIRECTORY."backend/news.txt'>news.txt</a>
+
+<div class='spacer'><a href='".e_BASE."rss.php?1.1'><img src='".e_PLUGIN."backend_menu/images/rss1.png' alt='rss1.0' style='border:0' /></a></div>
+<div class='spacer'><a href='".e_BASE."rss.php?1.2'><img src='".e_PLUGIN."backend_menu/images/rss2.png' alt='rss2.0' style='border:0' /></a></div>
+<div class='spacer'><a href='".e_BASE."rss.php?1.3'><img src='".e_PLUGIN."backend_menu/images/rss3.png' alt='rdf' style='border:0' /></a><br /></div>
+
+
+
 </div>";
 
 $caption = (file_exists(THEME."images/backend_menu.png") ? "<img src='".THEME."images/backend_menu.png' alt='' style='vertical-align:middle' /> ".BACKEND_MENU_L2 : BACKEND_MENU_L2);
--- e107_plugins/backend_menu/languages/English.php
+++ e107_plugins/backend_menu/languages/English.php
@@ -4,13 +4,13 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/backend_menu/languages/English.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:11:59 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-01-23 10:08:42 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 
-define("BACKEND_MENU_L1", "Our headlines can be syndicated by using either our rss or text feeds.");
+define("BACKEND_MENU_L1", "Our headlines can be syndicated by using these rss feeds.");
 define("BACKEND_MENU_L2", "Backend");
 
 ?>
