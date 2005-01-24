3a53b58cb848c9ddbb15dc0654976805d447700c
mrpete <mrpete@localhost>
2005-01-24 13:46:09 +0000

    Fix link order sequencing -- force 1,2,3... order. (While there, inserted/repaired data display so link data is visible.)
    svn path=/trunk/e107/; revision=1435

--- e107_admin/links.php
+++ e107_admin/links.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107/e107_admin/links.php,v $
-|     $Revision: 1.27 $
-|     $Date: 2004-11-10 07:22:02 $
-|     $Author: e107coders $
+|     $Revision: 1.28 $
+|     $Date: 2005-01-24 13:46:09 $
+|     $Author: mrpete $
 +----------------------------------------------------------------------------+
 */
 
@@ -236,7 +236,7 @@ class links{
                                 $text .= "<tr>
                                 <td style='width:5%' class='forumheader3'>$link_id</td>
                                 <td style='width:10%' class='forumheader3'>".$cat[$link_category]."</td>
-                                <td style='width:50%' class='forumheader3'><a href='".e_BASE."comment.php?comment.news.$link_id'></a>$link_name</td>
+                                <td style='width:60%' class='forumheader3'><a href='".e_BASE."comment.php?comment.news.$link_id'></a>$link_name<br /><div style='overflow:auto'>({$link_url})</div><span class='smalltext'><i>$link_description</i></span></td>
                                 <td style='width:25%; text-align:center' class='forumheader3'>".
                                 $rs -> form_open("post", e_SELF,"myform_{$link_id}","",""," onsubmit=\"return confirm_('create',$link_id)\"")."<div>".
                                 $rs -> form_button("button", "main_edit_{$link_id}", LCLAN_9, "onclick=\"document.location='".e_SELF."?create.edit.$link_id'\"")."
@@ -456,37 +456,45 @@ class links{
                 global $sql, $ns, $aj;
                 $text = "<div style='text-align:center'>
                 <form method='post' action='".e_SELF."?order'>
-                <table style='width:85%' class='fborder'>";
+                <table style='width:100%' class='fborder'>";
 
                 $sql -> db_Select("link_category");
                 $sql2 = new db;
+		$sql3 = new db;
                 while(list($link_category_id, $link_category_name, $link_category_description) = $sql-> db_Fetch()){
                         if($lamount = $sql2 -> db_Select("links", "*", "link_category ='$link_category_id' ORDER BY link_order ASC ")){
-                                $text .= "<tr><td colspan='3' class='forumheader'>$link_category_name ".LCLAN_59."</td></tr>";
+                                $text .= "<tr><td colspan='3' width='100%' class='forumheader'>$link_category_name ".LCLAN_59."</td></tr>";
+                                $local_link_sequence = 1;
                                 while(list($link_id, $link_name, $link_url, $link_description, $link_button, $link_category, $link_order, $link_refer) = $sql2-> db_Fetch()){
-                                        $text .= "<tr>\n<td style='width:30%' class='forumheader3'>".$link_order." - ".$link_name."</td>\n<td style='width:30%; text-align:center' class='forumheader3'>\n";
-                                        $text .= "<table><tr><td><select name='link_order[]' class='tbox'>";
+                                        if ($link_order != $local_link_sequence) {
+                                                            $sql3 -> db_Update("links", "link_order=".$local_link_sequence." WHERE link_id=".$link_id);
+                                        }
+                                        $text .= "<tr>\n<td style='width:30%' class='forumheader3'>".$local_link_sequence." - ".$link_name."</td>\n<td style='width:30%; text-align:center' class='forumheader3'>\n<table><tr><td><select name='link_order[]' class='tbox'>";
                                         for($a=1; $a<= $lamount; $a++){
-                                                $text .= ($link_order == $a ? "<option value='$link_id.$a' selected='selected'>$a</option>\n" : "<option value='$link_id.$a'>$a</option>\n");
+                                                $text .= "<option value='$link_id.$a' ".
+                                                ($local_link_sequence == $a ? "selected='selected'":'').">$a</option>\n";
                                         }
 
-                                        $text .= "</select>&nbsp;</td><td>";
+                                        $text .= "</select></td><td>&nbsp;</td><td>";
 
-                                        $text .= "<a href='links.php?inc.".$link_id.".".$link_order.".".$link_category."' ><img src='".e_IMAGE."generic/up.png' style='border:0px' alt='".LCLAN_30."' title='".LCLAN_30."' /></a>";
+                                        $text .= "<a href='links.php?inc.".$link_id.".".$local_link_sequence.".".$link_category."' ><img src='".e_IMAGE."generic/up.png' style='border:0px' alt='".LCLAN_30."' title='".LCLAN_30."' /></a>";
                                         $text .= "<br />";
-                                        $text .= "<a href='links.php?dec.".$link_id.".".$link_order.".".$link_category."' ><img src='".e_IMAGE."generic/down.png' style='border:0px' alt='".LCLAN_31."' title='".LCLAN_31."' /></a>";
+                                        $text .= "<a href='links.php?dec.".$link_id.".".$local_link_sequence.".".$link_category."' ><img src='".e_IMAGE."generic/down.png' style='border:0px' alt='".LCLAN_31."' title='".LCLAN_31."' /></a>";
+                                        $text .="</td><td>&nbsp;</td>\n";
+                                        $text .="<td><input class='button' type='button' onclick=\"document.location='".e_SELF."?create.edit.$link_id'\" value='".LCLAN_9."' />";
                                         $text .= "</td></tr></table>";
 
                                         $text .="
                                         </td>
                                         <td style='width:40%' class='forumheader3'>&nbsp;".$aj->tpa($link_description)."</td>
                                         </tr>";
+                                        $local_link_sequence++;
                                 }
                         }
                 }
                 $text .= "
                 <tr>
-                <td colspan='3' style='text-align:center' class='forumheader'>
+                <td colspan='3' width='100%' style='text-align:center' class='forumheader'>
                 <input class='button' type='submit' name='update_order' value='".LCLAN_32."' />
                 </td>
                 </tr>
