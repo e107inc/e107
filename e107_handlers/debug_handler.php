42aed189f55c37414ac07dbcde71bae9a8f293e7
mrpete <mrpete@localhost>
2005-01-26 23:50:11 +0000

    Complete refactoring of debug code. Timing repaired to work correctly. Query analysis (no longer one big string) now in a php table that is rendered various ways based on debug level. Easy to add further analysis as desired. See class2 for revised debug level definitions, implemented systemwide.
    svn path=/trunk/e107_0.7/; revision=1475

--- e107_handlers/debug_handler.php
+++ e107_handlers/debug_handler.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/debug_handler.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-01-26 02:56:37 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.4 $
+|     $Date: 2005-01-26 23:50:11 $
+|     $Author: mrpete $
 +----------------------------------------------------------------------------+
 */
 
@@ -41,7 +41,7 @@ class e107_debug {
 	}
 		
 	function set_error_reporting() {
-		if ($this->debug_level > 1) {
+		if ($this->debug_level & 32768) { // Debug constants are defined in class2 for now
 			error_reporting(E_ALL);
 		} else {
 			error_reporting(E_ERROR | E_WARNING | E_PARSE);
@@ -51,30 +51,54 @@ class e107_debug {
 
 class e107_db_debug {
 	
-	var $sDBdbg;		// DB debug string
+	var $sDBdbg;		// DB debug string (deprecated)
+	var $aSQLdetails;       // DB query analysis (in pieces for further analysis)
 	var $aDBbyTable;
 	var $aOBMarks;		// Track output buffer level at each time mark
 	var $aMarkNotes;	// Other notes can be added and output...
 	var $aTimeMarks;	// Overall time markers
 	var $curTimeMark;
+	var $nTimeMarks;     // Provide an array index for time marks. Stablizes 'current' function
 	
 	function e107_db_debug() {
+	    global $eTimingStart,$sDBdbg;
 		$this->sDBdbg = '';
 		$this->aDBbyTable = array();
-		$this->aOBMarks = array();
+		$this->aSQLdetails = array();
+		$this->aOBMarks = array(0 =>'');
 		$this->aMarkNotes = array();
-		$this->aTimeMarks['Start'] = array('What' => 'Start', '%Time'=>0,'%DB Time'=>0,'%DB Count'=>0,'Time' => $timing_start, 'DB Time'=>0,'DB Count'=>0);
+		$this->aTimeMarks[0] = array('Index'=>0,'What' => 'Start', '%Time'=>0,'%DB Time'=>0,'%DB Count'=>0,'Time' =>($eTimingStart), 'DB Time'=>0,'DB Count'=>0);
 		$this->curTimeMark = 'Start';
+		$this->aGoodQueries = array();
+		$this->aBadQueries = array();
+		$this->nTimeMarks = 0;
 	}
 
 	function Mark_Time($sMarker) {
+		$nMarks = ++$this->nTimeMarks;
 		$timeNow = explode(' ',microtime());
-		$this->aTimeMarks[$sMarker]=array('What' => $sMarker, '%Time'=>0, '%DB Time'=>0, '%DB Count'=>0,'Time' => $timeNow, 'DB Time'=>0, 'DB Count'=>0);
-		$this->aOBMarks[$sMarker] = ob_get_level().'('.ob_get_length().')';
+		if (!strlen($sMarker)) {
+		    $sMarker = "Mark not set";
+		}
+		$this->aTimeMarks[$nMarks]=array(
+		    'Index'=>($this->nTimeMarks),
+		    'What' => $sMarker, 
+		    '%Time'=>0, 
+		    '%DB Time'=>0, 
+		    '%DB Count'=>0,
+		    'Time' => $timeNow, 
+		    'DB Time'=>0, 
+		    'DB Count'=>0);
+		$this->aOBMarks[$nMarks] = ob_get_level().'('.ob_get_length().')';
 		$this->curTimeMark = $sMarker;
-	}
+
+// Add any desired notes to $aMarkNotes[$nMarks]... e.g.
+//global $eTimingStart;
+//$this->aMarkNotes[$nMarks] .= "verify start: ".$eTimingStart[0]." / ".$eTimingStart[1]."<br/>";
+ 	}
 
 	function Mark_Query($query, $rli, $aTrace) {
+		global $sql;
 		$query = str_replace(","," , ",$query);
 		$sQryRes =  is_null($rli) ? mysql_query("EXPLAIN $query") : mysql_query("EXPLAIN $query",$rli);
 		$nFields = "";
@@ -83,88 +107,202 @@ class e107_db_debug {
 		}
 		$sCallingFile=$aTrace[1]['file'];
 		$sCallingLine=$aTrace[1]['line'];
-
-		$this->sDBdbg .= "\n<table width=\"100%\" border='1' cellpadding='2' cellspacing='1'>\n";
-		$this->sDBdbg .= "<tr><td colspan=\"$nFields\"><b>Query:</b> [{$this->curTimeMark} - $sCallingFile($sCallingLine)]<br/>$query</td></tr>\n";
+                $iQuery=$sql->db_QueryCount();
+                $this->aSQLdetails[$iQuery]['marker']= $this->curTimeMark;
+                $this->aSQLdetails[$iQuery]['caller']= "$sCallingFile($sCallingLine)";
+                $this->aSQLdetails[$iQuery]['query']= $query;
+                $this->aSQLdetails[$iQuery]['ok']= $sQryRes ? TRUE : FALSE;
+                $this->aSQLdetails[$iQuery]['error']= $sQryRes ? '' : mysql_error();
+                $this->aSQLdetails[$iQuery]['nFields']= $nFields;
 		if ($sQryRes) {
 			$bRowHeaders = FALSE;
 			while ($row = @mysql_fetch_assoc($sQryRes)) {
 				if (!$bRowHeaders) {
-					$bRowHeaders = TRUE;
-					$sDBdbg .= "<tr><td><b>".implode("</b></td><td><b>",array_keys($row))."</b></td></tr>\n";
+				    $bRowHeaders = TRUE;
+                                    $this->aSQLdetails[$iQuery]['explain'] =
+					"<tr><td class='forumheader3'><b>".
+			                implode("</b></td><td class='forumheader3'><b>",array_keys($row)).
+			                "</b></td></tr>\n";
 				}
-				$this->sDBdbg .= "<tr><td>".implode("&nbsp;</td><td>",array_values($row))."&nbsp;</td></tr>\n";
+				$this->aSQLdetails[$iQuery]['explain'] .= "<tr><td class='forumheader3'>".
+				  implode("&nbsp;</td><td class='forumheader3'>",array_values($row)).
+				  "&nbsp;</td></tr>\n";
 			}
-		}
+		} else {
+		    $this->aSQLdetails[$iQuery]['explain'] = '';
+                }
+
 		return $nFields;
 	}
 
 	function Mark_Query_Results($mytime, $curtable, $nFields) {
-		$this->aTimeMarks[$this->curTimeMark]['DB Time']+=$mytime;
-		$this->aTimeMarks[$this->curTimeMark]['DB Count']++;
+	        global $sql;
+		$this->aTimeMarks[$this->nTimeMarks]['DB Time']+=$mytime;
+		$this->aTimeMarks[$this->nTimeMarks]['DB Count']++;
 
-		$this->aDBbyTable[$curtable]['Table'] = $curtable;
-		$this->aDBbyTable[$curtable]['%DB Time'] = 0; // placeholder
-		$this->aDBbyTable[$curtable]['%DB Count'] = 0; // placeholder
-		if(array_key_exists('DB Time',$this->aDBbyTable[$curtable])) {
-			$this->aDBbyTable[$curtable]['DB Time'] += $mytime;
-		} else {
-			$this->aDBbyTable[$curtable]['DB Time'] = $mytime;
-		}
-				
-		if(array_key_exists('DB Count',$this->aDBbyTable[$curtable])) {
-			$this->aDBbyTable[$curtable]['DB Count'] ++;
-		} else {
-			$this->aDBbyTable[$curtable]['DB Count'] = 1;
+		if(array_key_exists($curtable,$this->aDBbyTable)) {
+		    $this->aDBbyTable[$curtable]['DB Time'] += $mytime;
+		    $this->aDBbyTable[$curtable]['DB Count'] ++;
+                } else {
+		    $this->aDBbyTable[$curtable]['Table'] = $curtable;
+		    $this->aDBbyTable[$curtable]['%DB Time'] = 0; // placeholder
+		    $this->aDBbyTable[$curtable]['%DB Count'] = 0; // placeholder
+		    $this->aDBbyTable[$curtable]['DB Time'] = $mytime;
+		    $this->aDBbyTable[$curtable]['DB Count'] = 1;
 		}
 
-		$mytime = number_format($mytime,4);  //round for local display
-		$this->sDBdbg .=  "<tr><td colspan=\"$nFields\"><b>Query time:</b> $mytime</td></tr></table><br />";
+                $iQuery=$sql->db_QueryCount();
+                $this->aSQLdetails[$iQuery]['time'] = $mytime;
 	}
+	
+	function Show_SQL_Details() {
+	    global $sql;
+	    //
+	    // Show stats from aSQLdetails array
+	    //
+	    $text = '';
+	    $nQueries = $sql->db_QueryCount();
+	    if (!$nQueries) return $text;
+	    
+	    //
+	    // ALWAYS summarize query errors
+	    //
+            $badCount = 0;
+            $okCount = 0;
+            foreach ($this->aSQLdetails as $cQuery) {
+                if ($cQuery['ok']) {
+                    $okCount++;
+                } else {
+                    $badCount++;
+                }
+            }
+            
+                
+            if ($badCount) {
+    	        $text .= "\n<table class='fborder' style='width: 100%;'>\n";
+                $text .="<tr><td class='fcaption' colspan='2'><b>$badCount Query Errors!</b></td></tr>\n";
+                $text .="<tr><td class='fcaption'><b>Index</b></td><td class='fcaption'><b>Query</b></td></tr>\n";
+                foreach ($this->aSQLdetails as $idx => $cQuery) {
+    	            if (!$cQuery['ok']) {
+                        $text .= "<tr><td class='forumheader3' style='text-align:right'>{$idx}&nbsp;</td>
+    	       	        <td class='forumheader3'>".$cQuery['query']."</td></tr>\n";
+    	            }
+                }
+    	        $text .= "\n</table><br/>\n";
+    	    }
+    
+	    //
+	    // Optionally list good queries
+	    //
+            if ($okCount && E107_DBG_SQLQUERIES) {
+                $text .= "\n<table class='fborder' style='width: 100%;'>\n";
+                $text .="<tr><td class='fcaption' colspan='3'><b>{$okCount[TRUE]} Good Queries</b></td></tr>\n";
+                $text .="<tr><td class='fcaption'><b>Index</b></td><td class='fcaption'><b>Qtime</b></td><td class='fcaption'><b>Query</b></td></tr>\n";
+                foreach ($this->aSQLdetails as $idx => $cQuery) {
+	            if ($cQuery['ok']) {
+	       	        $text .= "<tr><td class='forumheader3' style='text-align:right'>{$idx}&nbsp;</td>
+	       	        <td class='forumheader3' style='text-align:right'>".
+	       	        number_format($cQuery['time']*1000.0,4)."&nbsp;</td><td class='forumheader3'>".
+                        $cQuery['query'].'<br/>['.
+                        $cQuery['marker']." - ".$cQuery['caller']."]</td></tr>\n";
+	            }
+                }
+	        $text .= "\n</table><br/>\n";
+	    }
+    
+	    
+	    //
+	    // Optionally list query details
+	    //
+	    if (E107_DBG_SQLDETAILS) {
+                foreach ($this->aSQLdetails as $idx => $cQuery) {
+		    $text .= "\n<table class='fborder' style='width: 100%;'>\n";
+		    $text .= "<tr><td class='forumheader3' colspan='".$cQuery['nFields']."'><b>".
+		                $idx.") Query:</b> [".
+		                $cQuery['marker']." - ".
+		                $cQuery['caller']."]<br/>".
+		                $cQuery['query']."</td></tr>\n";
+                    $text .=  $cQuery['explain'];
+                    if (strlen($cQuery['error'])) {
+                        $text .= "<tr><td class='forumheader3' ><b>Error in query:</b></td></tr>\n<tr><td class='forumheader3'>".
+		            $cQuery['error'].
+		            "</td></tr>\n";
+		    }
+    		    $text .=  "<tr><td class='forumheader3'  colspan='".
+    		        $cQuery['nFields'].
+    		        "'><b>Query time:</b> ".
+    		        number_format($cQuery['time']*1000.0,4).
+    		        ' msec.</td></tr></table><br />'."\n";
+    	        }
+            }
+            
+            return $text;
+        }
 	
 	function Show_Performance() {
 		//
 		// Stats by Time Marker
 		//
-		global $dbq;
 		global $db_time;
 		global $sql;
+		global $eTimingStart,$eTimingStop;
 		
 		$this->Mark_Time('Stop');
+		if (!E107_DBG_TIMEDETAILS) return '';
 		
 		$text = '';
-		$startTime=$timing_start[0]+$timing_start[1];
-		$stopTime=$timing_stop[0]+$timing_stop[1];
+		$startTime=$eTimingStart[0]+$eTimingStart[1];
+		$stopTime=$eTimingStop[0]+$eTimingStop[1];
 		$totTime =$stopTime-$startTime;
-		$text .= "\n<table border='1' cellpadding='2' cellspacing='1'>\n";
+		$text .= "\n<table class='fborder' style='width: 100%;'>\n";
 		$bRowHeaders = FALSE;
-		foreach ($this->aTimeMarks as $tMarker) {
+		reset( $this->aTimeMarks );
+		$aSum=$this->aTimeMarks[0];
+		$aSum['Index']=''; 
+		$aSum['What']='Total';
+		$aSum['Time']=0; $aSum['DB Count']=0; $aSum['DB Time']=0;
+		while (list($tKey, $tMarker) = each($this->aTimeMarks)) {
 			if (!$bRowHeaders) {
 				// First time: emit headers
 				$bRowHeaders = TRUE;
-				$text .= "<tr><td><b>".implode("</b></td><td><b>",array_keys($tMarker))."</b></td><td><b>OB Lev</b></td></tr>\n";
+				$text .= "<tr><td class='fcaption' style='text-align:right'><b>".
+				        implode("</b>&nbsp;</td><td class='fcaption' style='text-align:right'><b>",array_keys($tMarker)).
+				        "</b>&nbsp;</td><td class='fcaption' style='text-align:right'><b>OB Lev&nbsp;</b></td></tr>\n";
 			}
 			if ($tMarker['What'] == 'Stop') {
 				break;        // We're on the 'stop' mark
 			}
 			// Convert from start time to delta time, i.e. from now to next entry
 			$nextMarker = current($this->aTimeMarks);
-			$nextTime = $nextMarker['Time'][0]+$nextMarker['Time'][1];
-			$thisTime = $tMarker['Time'][0]+$tMarker['Time'][1];
+                        $aNextT = $nextMarker['Time'];
+                        $aThisT = $tMarker['Time'];
+			$nextTime = $aNextT[0]+$aNextT[1];
+			$thisTime = $aThisT[0]+$aThisT[1];
 			$thisDelta=$nextTime-$thisTime;
 			$thisWhat = $tMarker['What'];
+			$aSum['Time']+=$thisDelta;
+                        $aSum['DB Time'] += $tMarker['DB Time'];
+                        $aSum['DB Count'] += $tMarker['DB Count'];
 			$tMarker['Time'] = number_format($thisDelta, 4);
 			$tMarker['%Time'] = $totTime ? number_format(100.0*($thisDelta/$totTime),0) : 0;
-			$tMarker['%DB Count'] = number_format(100.0*$tMarker['DB Count']/$sql->mySQLquerycount,0);
-			$tMarker['%DB Time'] = number_format(100.0*$tMarker['DB Time']/$db_time,0);
+			$tMarker['%DB Count'] = number_format(100.0*$tMarker['DB Count']/$sql->db_QueryCount(),0);
+			$tMarker['%DB Time'] = $db_time ? number_format(100.0*$tMarker['DB Time']/$db_time,0) : 0;
 			$tMarker['DB Time'] = number_format($tMarker['DB Time'],4);
-			$tMarker['OB Lev'] = $aOBMarks[$thisWhat];
-			$text .= "<tr><td>".implode("&nbsp;</td><td style='text-align:right'>",array_values($tMarker))."&nbsp;</td></tr>\n";
-			if (strlen($this->aMarkNotes[$thisWhat])) {
-				$text .= '<tr><td>&nbsp;</td><td colspan="6">';
-				$text .= $this->aMarkNotes[$thisWhat].'</td></tr>\n';
+			$tMarker['OB Lev'] = $this->aOBMarks[$tKey];
+			$text .= "<tr><td class='forumheader3' >".implode("&nbsp;</td><td class='forumheader3'  style='text-align:right'>",array_values($tMarker))."&nbsp;</td></tr>\n";
+			if (isset($this->aMarkNotes[$tKey])) {
+				$text .= "<tr><td class='forumheader3' >&nbsp;</td><td class='forumheader3' colspan='4'>";
+				$text .= $this->aMarkNotes[$tKey]."</td></tr>\n";
 			}
+                        
 		}
+		$aSum['%Time']=$totTime ? number_format(100.0*($aSum['Time']/$totTime),0) : 0;
+		$aSum['%DB Time']=$db_time ? number_format(100.0*($aSum['DB Time']/$db_time),0) : 0;
+		$aSum['%DB Count']=($sql->db_QueryCount()) ? number_format(100.0*($aSum['DB Count']/($sql->db_QueryCount())),0) : 0;
+		$aSum['Time'] = number_format($aSum['Time'], 4);
+		$aSum['DB Time'] = number_format($aSum['DB Time'], 4);
+
+		$text .= "<tr><td class='fcaption'><b>".implode("</b>&nbsp;</td><td class='fcaption' style='text-align:right'><b>",$aSum)."</b>&nbsp;</td><td class='fcaption'>&nbsp;</td></tr>\n";
 		$text .= "\n</table><br/>\n";
 
 		//
@@ -177,14 +315,15 @@ class e107_db_debug {
 		foreach ($this->aDBbyTable as $curTable) {
 			if (!$bRowHeaders) {
 				$bRowHeaders = TRUE;
-				$text .= "<tr><td><b>".implode("</b></td><td><b>",array_keys($curTable))."</b></td></tr>\n";
+				$text .= "<tr><td class='fcaption'><b>".implode("</b></td><td class='fcaption'><b>",array_keys($curTable))."</b></td></tr>\n";
 			}
-			$curTable['%DB Count'] = number_format(100.0*$curTable['DB Count']/$sql->mySQLquerycount,0);
+			$curTable['%DB Count'] = number_format(100.0*$curTable['DB Count']/$sql->db_QueryCount(),0);
 			$curTable['%DB Time'] = number_format(100.0*$curTable['DB Time']/$db_time,0);
 			$curTable['DB Time'] = number_format($curTable['DB Time'],4);
-			$text .= "<tr><td>".implode("&nbsp;</td><td style='text-align:right'>",array_values($curTable))."&nbsp;</td></tr>\n";
+			$text .= "<tr><td class='forumheader3'>".implode("&nbsp;</td><td class='forumheader3' style='text-align:right'>",array_values($curTable))."&nbsp;</td></tr>\n";
 		}
 		$text .= "\n</table><br/>\n";
+
 		return $text;
 	}
 
