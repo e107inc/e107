0759845e7bb3b9a70068d500d551986d1056be7e
e107steved <steved@e107.org>
2005-01-26 18:40:03 +0000

    Custom template for new forum posts menu for the human condition theme.
    svn path=/trunk/e107_0.7/; revision=1468

---
+++ e107_themes/human_condition/newforumpost.php
@@ -0,0 +1,11 @@
+<?php
+
+$NEWFORUMPOSTSTYLE_HEADER = "
+<!-- newforumposts -->\n<ul type='square'>\n";
+
+$NEWFORUMPOSTSTYLE_MAIN = "<li><span class='smalltext'>{THREAD} by {POSTER} [ views: {VIEWS}, replies: {REPLIES}, lastpost: {LASTPOST} ]\n</span>\n</li>\n";
+
+$NEWFORUMPOSTSTYLE_FOOTER = "<br /><br />\n</ul><span class='smalltext'>".LAN_6.": <b>{TOTAL_TOPICS}</b> | ".LAN_4.": <b>{TOTAL_REPLIES}</b> | ".LAN_3.": <b>{TOTAL_VIEWS}</b></span>";
+
+
+?>
