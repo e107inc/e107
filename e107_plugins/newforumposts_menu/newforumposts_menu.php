5a23e679182125ac01b0e621fcc6929087c9fc0c
e107steved <steved@e107.org>
2005-01-28 15:03:27 +0000

    Rewrite, thanks to McFly and asperon for help with main query.
    svn path=/trunk/e107_0.7/; revision=1520

--- e107_plugins/newforumposts_menu/newforumposts_menu.php
+++ e107_plugins/newforumposts_menu/newforumposts_menu.php
@@ -12,82 +12,85 @@
 | GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
-	
-if (!is_object($aj)) {
-	$aj = new textparse;
+global $tp;
+$gen = new convert;
+
+$query = ($pref['nfp_posts'] ? "thread_lastpost" : "thread_datestamp");
+
+
+$query2 = "SELECT tp.thread_name AS parent_name, t.thread_thread, t.thread_id, t.thread_name, t.thread_datestamp, t.thread_parent, t.thread_user, t.thread_views, t.thread_lastpost, t.thread_anon, t.thread_lastuser, t.thread_total_replies, f.forum_id, f.forum_name, f.forum_class, u.user_name FROM e107_forum_t AS t 
+LEFT JOIN e107_user AS u ON t.thread_user = u.user_id 
+LEFT JOIN e107_forum_t AS tp ON t.thread_parent = tp.thread_id 
+LEFT JOIN e107_forum AS f ON f.forum_id = t.thread_forum_id 
+WHERE f.forum_class  IN (".USERCLASS_LIST.") 
+ORDER BY t.$query DESC LIMIT 0, ".$pref['nfp_amount'];
+
+$results = $sql->db_Select_gen($query2);
+
+
+if(!$results)
+{
+	// no posts yet ..
+	$text = NFP_2;
 }
-	
-if ($cache = $e107cache->retrieve("newforumposts")) {
-	$text = $aj->formtparev($cache);
-} else {
-	$gen = new convert;
-	$sql2 = new db;
-	$text = "<span class='smalltext'>";
-	if (!$results = $sql->db_Select_gen("SELECT * FROM ".MPREFIX."forum_t, ".MPREFIX."forum WHERE ".MPREFIX."forum.forum_id=".MPREFIX."forum_t.thread_forum_id ORDER BY ".MPREFIX."forum_t.thread_datestamp DESC LIMIT 0, ".$menu_pref['newforumposts_display'])) {
-		$text = "<span class='mediumtext'>".NFP_2;
-	} else {
-		while ($row = $sql->db_Fetch()) {
-			extract($row);
-			if (check_class($forum_class)) {
-				$poster = substr($thread_user, (strpos($thread_user, ".")+1));
-				if (strstr($poster, chr(1))) {
-					$tmp = explode(chr(1), $poster);
-					$poster = $tmp[0];
-				}
-				$datestamp = $gen->convert_date($thread_datestamp, "short");
-				 
-				if ($thread_parent) {
-					 
-					if ($cachevar[$thread_parent]) {
-						$thread_name = $cachevar[$thread_parent];
-					} else {
-						$tmp = $thread_parent;
-						$sql2->db_Select("forum_t", "thread_name", "thread_id = $thread_parent");
-						list($thread_name) = $sql2->db_Fetch();
-						$cachevar[$thread_parent] = $thread_name;
-					}
-					$topic = "[re: <i>$thread_name</i>]";
-				} else {
-					$tmp = $thread_id;
-					$topic = "[thread: <i>$thread_name</i>]";
-				}
-				 
-				$thread_thread = $aj->tpa($thread_thread);
-				 
-				if ($pref['cb_linkreplace']) {
-					$thread_thread .= " ";
-					$thread_thread = preg_replace("#\>(.*?)\</a\>[\s]#si", ">".$pref['cb_linkc']."</a> ", $thread_thread);
-					$thread_thread = $aj->tpa(strip_tags($thread_thread));
-				}
-				 
-				if (!eregi("<a href|<img|&#", $thread_thread)) {
-					$message_array = explode(" ", $thread_thread);
-					for($i = 0; $i <= (count($message_array)-1); $i++) {
-						if (strlen($message_array[$i]) > 20) {
-							$message_array[$i] = preg_replace("/([^\s]{20})/", "$1<br />", $message_array[$i]);
-						}
-					}
-					$thread_thread = implode(" ", $message_array);
-				}
-				if (strlen($thread_thread) > $menu_pref['newforumposts_characters']) {
-					$thread_thread = substr($thread_thread, 0, $menu_pref['newforumposts_characters']).$menu_pref['newforumposts_postfix'];
-				}
-				 
-				$text .= "<img src='".THEME."images/bullet2.gif' alt='' /> <a href='".e_BASE."forum_viewtopic.php?$thread_forum_id.$tmp'><b>".$poster."</b> on ".$datestamp."</a><br/>";
-				if ($menu_pref['newforumposts_title']) {
-					$text .= $topic."<br />";
-				}
-				$text .= $thread_thread."<br /><br />";
-			} else {
-				$results -- ;
-			}
+else
+{
+	$text = "";
+	$forumArray = $sql->db_getList();
+	foreach($forumArray as $forumInfo)
+	{
+		extract($forumInfo);
+		$datestamp = $gen->convert_date($thread_datestamp, "short");
+		
+		$topic = ($parent_name ? "[re: <i>$parent_name</i>]" : "[thread: <i>$thread_name</i>]");
+
+		$id = ($thread_parent ? $thread_parent : $thread_id);
+
+		if(!$thread_user)
+		{
+			// anonymous post ...
+			list($poster, $ip) = explode(chr(1), $thread_anon);				
 		}
+		else
+		{
+			$poster = $user_name;
+			$poster_id = $thread_user;
+		}
+
+		$thread_thread = strip_tags(eregi_replace("\[.*\]", "", $thread_thread));
+				 
+		if ($pref['cb_linkreplace'])
+		{
+			$thread_thread .= " ";
+			$thread_thread = preg_replace("#\>(.*?)\</a\>[\s]#si", ">".$pref['cb_linkc']."</a> ", $thread_thread);
+		}
+		
+				 
+		if (!eregi("<a href|<img|&#", $thread_thread))
+		{
+			$message_array = explode(" ", $thread_thread);
+			for($i = 0; $i <= (count($message_array)-1); $i++)
+			{
+				if (strlen($message_array[$i]) > 20)
+				{
+					$message_array[$i] = preg_replace("/([^\s]{20})/", "$1<br />", $message_array[$i]);
+				}
+			}
+			$thread_thread = implode(" ", $message_array);
+		}
+		if (strlen($thread_thread) > $menu_pref['newforumposts_characters'])
+		{
+			$thread_thread = substr($thread_thread, 0, $menu_pref['newforumposts_characters']).$menu_pref['newforumposts_postfix'];
+		}
+				 
+		$text .= "<img src='".THEME."images/bullet2.gif' alt='' /> <a href='".e_PLUGIN."forum/forum_viewtopic.php?$id'><b>".$poster."</b> on ".$datestamp."</a><br/>";
+		if ($menu_pref['newforumposts_title']) {
+			$text .= $topic."<br />";
+		}
+		$text .= $thread_thread."<br /><br />";
 	}
-	$text = "</span>".preg_replace("/\<br \/\>$/", "", $text);
-	$cache = $aj->formtpa($text, "admin");
-	$e107cache->set("newforumposts", $cache);
 }
-	
+
 $ns->tablerender($menu_pref['newforumposts_caption'], $text, 'nfp_menu');
 	
 ?>
