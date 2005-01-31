25af8c99646bd9e583b850b67c2a8ab0c6be1152
sweetas <sweetas@localhost>
2005-01-31 23:01:50 +0000

    reworked the index to fix multiple bugs and to use request instead of header location
    svn path=/trunk/e107_0.7/; revision=1620

--- index.php
+++ index.php
@@ -11,42 +11,58 @@ e107 website system
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/index.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-01-31 03:47:04 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.6 $
+|     $Date: 2005-01-31 23:01:50 $
+|     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 	
 require_once("class2.php");
+if ($pref['frontpage'] == 'links') {
+	$pref['frontpage'] = $PLUGINS_DIRECTORY."links_page/links.php";
+	save_prefs();
+} else if ($pref['frontpage'] == 'forum') {
+	$pref['frontpage'] = $PLUGINS_DIRECTORY."forum/forum.php";
+	save_prefs();
+} else if (is_numeric($pref['frontpage'])) {
+	$pref['frontpage'] = "content.php?content.".$pref['frontpage'];
+	save_prefs();
+} else if (strpos($pref['frontpage'], ".")===FALSE) {
+	if (!preg_match("#/$#",$pref['frontpage'])) {
+		$pref['frontpage'] = $pref['frontpage'].'.php';
+		save_prefs();
+	}
+}
+
 if ($pref['membersonly_enabled'] && !USER)
 {
-	header("location: ".e_LOGIN);
+	require_once(e_LOGIN);
 	exit;
 }
-	
-if (!$pref['frontpage'] || $pref['frontpage_type'] == "splash")
+else if (!$pref['frontpage'] || $pref['frontpage_type'] == "splash")
 {
-	header("location: ".e_BASE."news.php");
+	require_once(e_BASE."news.php");
 	exit;
 }
-elseif(is_numeric($pref['frontpage'])) 
+else if(strpos($pref['frontpage'], "http")!==FALSE)
 {
-	header("location: ".e_BASE."content.php?content.".$pref['frontpage']."");
-	exit;
-}
-elseif(eregi("http", $pref['frontpage']))
-{
-	header("location: ".e_BASE.$pref['frontpage']);
-	exit;
-}
-elseif ($pref['frontpage'] == 'forum')
-{
-	header("location: ".e_PLUGIN."forum/forum.php");
-	exit;
+	if (strpos($pref['frontpage'], ".php")!==FALSE) {
+		require_once($pref['frontpage']);
+		exit;
+	} else {
+		header("location: ".$pref['frontpage']);
+		exit;
+	}
 }
 else
 {
-	header("location: ".e_BASE.$pref['frontpage'].".php");
-	exit;
+	if (strpos($pref['frontpage'], ".php")!==FALSE) {
+		require_once(e_BASE.$pref['frontpage']);
+		exit;
+	} else {
+		header("location: ".e_BASE.$pref['frontpage']);
+		exit;
+	}
 }
+
 ?>
