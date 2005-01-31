77d6ef14984020ab6420cb64a49171a34de14e13
e107steved <steved@e107.org>
2005-01-31 08:06:37 +0000

    Rewritten. (Work in progress)
    svn path=/trunk/e107_0.7/; revision=1611

--- e107_plugins/backend_menu/languages/English.php
+++ e107_plugins/backend_menu/languages/English.php
@@ -4,13 +4,18 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/backend_menu/languages/English.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-01-27 19:52:34 $
-|     $Author: streaky $
+|     $Revision: 1.4 $
+|     $Date: 2005-01-31 08:06:29 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 	
-define("BACKEND_MENU_L1", "Our headlines can be syndicated by using these rss feeds.");
-define("BACKEND_MENU_L2", "Backend");
+define("BACKEND_MENU_L1", " can be syndicated by using these rss feeds.");
+define("BACKEND_MENU_L2", "RSS Feeds");
+
+define("BACKEND_MENU_L3", "Our news");
+define("BACKEND_MENU_L4", "Our comments");
+define("BACKEND_MENU_L5", "Our forum posts");
+
 	
 ?>
