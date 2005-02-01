cfee22777b7cf9dacc989a777030a57516093745
sweetas <sweetas@localhost>
2005-02-01 00:45:08 +0000

    front page system updated
    svn path=/trunk/e107_0.7/; revision=1622

--- e107_admin/frontpage.php
+++ e107_admin/frontpage.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/frontpage.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2005-02-01 00:42:02 $
+|     $Revision: 1.7 $
+|     $Date: 2005-02-01 00:45:08 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
@@ -125,13 +125,13 @@ if ($flag != TRUE) {
 	$text .= "checked='checked'";
 }
 	
-$text .= " />".FRTLAN_8."
-	<input class='tbox' type='text' name='frontpage_url' size='50' value='";
+$text .= " />".FRTLAN_8.":
+	&nbsp;<input class='tbox' type='text' name='frontpage_url' size='50' value='";
 if ($flag != TRUE) {
 	$text .= $pref['frontpage'];
 }
 	
-$text .= "' maxlength='100' /> ".FRTLAN_14."
+$text .= "' maxlength='100' />
 	</td>
 	</tr>
 	 
