6f369f2ea34fa946cf2a30dfc341e6cf5adc5d75
sweetas <sweetas@localhost>
2005-02-01 00:50:47 +0000

    front page system updated
    svn path=/trunk/e107_0.7/; revision=1623

--- e107_languages/English/admin/help/frontpage.php
+++ e107_languages/English/admin/help/frontpage.php
@@ -1,5 +1,5 @@
 <?php
 $caption = "Front Page Help";
-$text = "From this screen you can choose what to display on the front page of your site, the default is news. You can also use this page to set up a 'splashscreen', a page that will only display when a visitor first enters your site.";
+$text = "From this screen you can choose what to display as the front page of your site, the default is news.";
 $ns -> tablerender($caption, $text);
 ?>
