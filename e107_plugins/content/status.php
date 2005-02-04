e2a015d36f7a9383995fd387d76b1aefbde5da39
lisa_ <lisa_@localhost>
2005-02-04 00:47:07 +0000

    Content: status correction
    svn path=/trunk/e107_0.7/; revision=1689

--- e107_plugins/content/status.php
+++ e107_plugins/content/status.php
@@ -1,9 +1,11 @@
 <?php
-$maincat = $sql -> db_Select("pcontent", "content_id", "content_parent = '0' ORDER BY content_heading");
+
+if(!is_object($sql2)){ $sql2 = new db; }
+$maincat = $sql -> db_Select("pcontent", "content_id, content_heading", "content_parent = '0' ORDER BY content_heading");
 while($row = $sql -> db_Fetch()){
 	extract($row);
-		$count = $sql -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,1) = '".$content_id."' AND content_refer != 'sa' ");
-		$text .= "<div style='padding-bottom: 2px;'>".$content_heading.": ".$count."</div>";
+		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,1) = '".$content_id."' AND content_refer != 'sa' ");
+		$text .= "<div style='padding-bottom: 2px;'>".E_16_ARTICLE." ".$content_heading.": ".$count."</div>";
 }
 
 ?>
