9ca32e773a8866be6e84143bc81cceea81809529
mcfly_e107 <mcfly_e107@localhost>
2005-02-04 15:49:47 +0000

    relocating nfp to forum directory
    svn path=/trunk/e107_0.7/; revision=1709

---
+++ e107_plugins/forum/languages/English/lan_newforumposts_menu.php
@@ -0,0 +1,31 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/languages/English/lan_newforumposts_menu.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-02-04 15:49:47 $
+|     $Author: mcfly_e107 $
++----------------------------------------------------------------------------+
+*/
+	
+define("NFP_1", "All latest posts are outside of your user class, unable to display.");
+define("NFP_2", "No posts yet");
+define("NFP_3", "New Forum Posts menu configuration saved");
+define("NFP_4", "Caption");
+define("NFP_5", "Number of posts to display?");
+define("NFP_6", "Number of characters to display?");
+define("NFP_7", "Postfix for too long posts?");
+define("NFP_8", "Show original topics in menu?");
+define("NFP_9", "Update menu Settings");
+define("NFP_10", "New Forum Posts Menu Configuration");
+	
+?>
