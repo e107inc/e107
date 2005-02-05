2c082247d75731b44c10a1378deb6c53e367e7f0
e107coders <e107coders@localhost>
2005-02-05 03:49:22 +0000

    uses generic JS now.
    svn path=/trunk/e107_0.7/; revision=1716

--- e107_admin/language.php
+++ e107_admin/language.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/language.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2005-01-27 19:52:24 $
-|     $Author: streaky $
+|     $Revision: 1.11 $
+|     $Date: 2005-02-05 03:49:22 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 require_once("../class2.php");
@@ -25,26 +25,26 @@ if (!getperms("ML")) {
 }
 require_once(e_HANDLER."form_handler.php");
 $rs = new form;
-	
-	
+
+
 // still requires some "LAN" work - once fully tested.
-	
+
 $tabs = table_list(); // array("news","content","links");
 $lanlist = get_langlist();
 // list of languages.
-	
+
 if (isset($_POST['submit_prefs']) ) {
-	 
+
 	$pref['multilanguage'] = $_POST['multilanguage'];
 	$pref['sitelanguage'] = $_POST['sitelanguage'];
-	 
+
 	save_prefs();
 	$ns->tablerender("Saved", "<div style='text-align:center'>Saved</div>");
-	 
+
 }
 // ----------------- delete tables ---------------------------------------------
 if (isset($_POST['del_existing']) && $_POST['lang_choices']) {
-	 
+
 	$lang = strtolower($_POST['lang_choices']);
 	foreach ($tabs as $del_table) {
 		if (db_Table_exists($lang."_".$del_table)) {
@@ -52,24 +52,24 @@ if (isset($_POST['del_existing']) && $_POST['lang_choices']) {
 			 $_POST['lang_choices']." $del_table couldn't be deleted<br />";
 		}
 	}
-	 
-	 
+
+
 	$ns->tablerender("Result", $message);
 }
-	
-	
+
+
 // ----------create tables -----------------------------------------------------
-	
+
 if (isset($_POST['create_tables']) && $_POST['language']) {
-	 
+
 	$table_to_copy = array();
 	$lang_to_create = array();
 	$message = "";
-	 
+
 	foreach ($tabs as $value) {
 		if ($_POST[$value]) {
 			$lang = strtolower($_POST['language']);
-			 
+
 			if (copy_table($value, $lang."_".$value, $_POST['drop'])) {
 				$message .= " ".$_POST['language']." ".$value." created<br />";
 			} else {
@@ -89,19 +89,19 @@ if (isset($_POST['create_tables']) && $_POST['language']) {
 	}
 	$ns->tablerender("Result", $message);
 }
-	
+
 // ------------- render form ---------------------------------------------------
-	
-	
-	
+
+
+
 $caption = "Languages";
 $text = MLAD_LAN_4."<br /><br />";
-	
-	
+
+
 // Choose Language to Edit:
 $text = "<div style='text-align:center'>
 	<div style='".ADMIN_WIDTH."; height:150px; overflow:auto; margin-left: auto; margin-right: auto;'>";
-	
+
 $text .= "<table class='fborder' style='width:99%; margin-top: 1px;'>\n";
 $text .= "<tr><td class='fcaption'>".ADLAN_132."</td>";
 $text .= "<td class='fcaption'>".LANG_LAN_03."</td><td class='fcaption'>".LANG_LAN_04."</td>";
@@ -109,25 +109,25 @@ $text .= "</tr>\n\n";
 sort($lanlist);
 for($i = 0; $i < count($lanlist); $i++) {
 	$installed = 0;
-	 
+
 	$text .= "<tr><td class='forumheader3' style='width:30%'>".$lanlist[$i]."</td><td class='forumheader3'>\n";
 	foreach ($tabs as $tab_name) {
 		if (db_Table_exists(strtolower($lanlist[$i])."_".$tab_name)) {
 			$text .= $tab_name.", ";
 			$installed++;
 		}
-		 
+
 	}
-	 
+
 	$text .= (!$installed)? "<div style='text-align:center'><i>".LANG_LAN_05."</i></div>" :
 	 "";
 	$text .= "</td><td class='forumheader3' style='width:10%;white-space:nowrap;text-align:right'>\n";
 	$text .= $rs->form_open("post", e_SELF."?modify", "lang_form_".str_replace(" ", "_", $lanlist[$i]));
 	$text .= "<div style='text-align: center'>\n";
-	 
+
 	if ($installed) {
 		$text .= " <input type='submit' class='button' name='edit_existing' value='".ADLAN_78."' />\n";
-		$text .= " <input type='submit' class='button' name='del_existing' value='".ADLAN_79."' onclick=\"return checkit('".$lanlist[$i]."')\" />\n";
+		$text .= " <input type='submit' class='button' name='del_existing' value='".ADLAN_79."' onclick=\"return jsconfirm('Delete all tables in ".$lanlist[$i]." ?')\" />\n";
 	} else {
 		$text .= "<input type='submit' class='button' name='edit_existing' value='".ADLAN_82."' />\n";
 	}
@@ -135,30 +135,30 @@ for($i = 0; $i < count($lanlist); $i++) {
 	$text .= "</div>";
 	$text .= $rs->form_close();
 	$text .= "</td></tr>";
-	 
+
 }
-	
+
 $text .= "</table></div></div>";
-	
+
 $ns->tablerender($caption, $text);
-	
+
 if (!$_POST['language'] && !$_POST['edit_existing']) {
 	multilang_prefs();
 }
 unset($text);
-	
-	
-	
-	
-	
+
+
+
+
+
 // Grab Language configuration. ---
 if ($_POST['edit_existing']) {
-	 
-	 
+
+
 	$text = $rs->form_open("post", e_SELF);
 	$text .= "<div style='text-align:center'>";
 	$text .= "<table class='fborder' style='".ADMIN_WIDTH."'>\n";
-	 
+
 	foreach ($tabs as $table_name) {
 		$installed = strtolower($_POST['lang_choices'])."_".$table_name;
 		if (!eregi($installed, $_POST['lang_choices'])) {
@@ -170,24 +170,24 @@ if ($_POST['edit_existing']) {
 			$text .= "<input type=\"checkbox\" name=\"$table_name\" value=\"1\" $selected />";
 			$text .= "</td></tr>\n";
 		}
-		 
+
 	}
-	 
+
 	$text .= "<tr><td class='forumheader3' colspan='2'>&nbsp;";
 	$text .= "<input type='hidden' name='language' value='".$_POST['lang_choices']."' />";
 	$text .= "</td></tr>";
-	 
+
 	// ===========================================================================
-	 
+
 	// Drop tables ?
 	$text .= "<tr><td class='forumheader3'><b>".LANG_LAN_07."</b></td>
 		<td class='forumheader3'>".$rs->form_checkbox("drop", 1)."\n
 		<span class=\"smalltext\" >".LANG_LAN_08."</span></td></tr>\n";
-	 
+
 	$text .= "<tr><td class='forumheader3'><b>".LANG_LAN_10."</b></td>
 		<td class='forumheader3'>".$rs->form_checkbox("remove", 1)."\n
 		<span class=\"smalltext\" >".LANG_LAN_11."</span></td></tr>\n";
-	 
+
 	$text .= "<tr>\n
 		<td colspan='2' style='width:100%; text-align: center;' class='forumheader' >\n
 		".$rs->form_button("submit", "create_tables", LANG_LAN_06, "", LANG_LAN_06, "disabled");
@@ -195,19 +195,19 @@ if ($_POST['edit_existing']) {
 	$text .= "</td>\n
 		</tr>\n";
 	$text .= "</table></div>\n";
-	 
+
 	$text .= $rs->form_close();
 	$ns->tablerender($_POST['lang_choices'], $text);
 }
-	
-	
-	
+
+
+
 require_once(e_ADMIN."footer.php");
-	
+
 // ---------------------------------------------------------------------------
 function multilang_prefs() {
 	global $ns, $pref;
-	 
+
 	$handle = opendir(e_LANGUAGEDIR);
 	while ($file = readdir($handle)) {
 		if ($file != "." && $file != ".." && $file != "/") {
@@ -215,20 +215,20 @@ function multilang_prefs() {
 		}
 	}
 	closedir($handle);
-	 
-	 
+
+
 	$text = "<div style='text-align:center'>
 		<form method='post' action='".e_SELF."' name='linkform'>
 		<table style='".ADMIN_WIDTH."' class='fborder'>";
-	 
-	 
+
+
 	$text .= "<tr>
-		 
+
 		<td style='width:40%' class='forumheader3'>".LANG_LAN_14.": </td>
 		<td style='width:60%; text-align:center' class='forumheader3'>";
-	 
+
 	// $text .= "<a href='".e_ADMIN."lancheck.php'>".PRFLAN_86."</a>";
-	 
+
 	$text .= "
 		<select name='sitelanguage' class='tbox'>\n";
 	$counter = 0;
@@ -244,12 +244,12 @@ function multilang_prefs() {
 	$text .= "</select>
 		</td>
 		</tr>";
-	 
-	 
-	 
-	 
-	 
-	 
+
+
+
+
+
+
 	$text .= "
 		<tr>
 		<td style='width:40%' class='forumheader3'>".LANG_LAN_12.": </td>
@@ -260,8 +260,8 @@ function multilang_prefs() {
 		</td>
 		</tr>
 		";
-	 
-	 
+
+
 	$text .= "<tr style='vertical-align:top'>
 		<td colspan='2' style='text-align:center' class='forumheader'>";
 	$text .= "<input class='button' type='submit' name='submit_prefs' value='Save' />";
@@ -270,18 +270,18 @@ function multilang_prefs() {
 		</table>
 		</form>
 		</div>";
-	 
+
 	$caption = LANG_LAN_13; // "Multi-Language Preferences";
 	$ns->tablerender($caption, $text);
-	 
+
 }
-	
-	
-	
-	
-	
+
+
+
+
+
 // ----------------------------------------------------------------------------
-	
+
 function db_Table_exists($table) {
 	global $mySQLdefaultdb;
 	$tables = mysql_list_tables($mySQLdefaultdb);
@@ -295,9 +295,9 @@ function db_Table_exists($table) {
 // ----------------------------------------------------------------------------
 // Cam's Alternative - requires MySQL 4.1+
 // eg. copy_table("news","french_news",1);
-	
+
 function copy_table($oldtable, $newtable, $drop) {
-	 
+
 	$request = ($drop)? "DROP TABLE IF EXISTS $newtable":
 	"";
 	$request .= "CREATE TABLE ".MPREFIX.strtolower($newtable)." LIKE ".MPREFIX.$oldtable;
@@ -306,15 +306,15 @@ function copy_table($oldtable, $newtable, $drop) {
 	} else {
 		return FALSE;
 	}
-	 
+
 }
-	
+
 // ----------------------------------------------------------------------------
-	
+
 function table_list() {
 	// grab default language lists.
 	global $mySQLdefaultdb;
-	 
+
 	$exclude[] = "banlist";
 	 $exclude[] = "banner";
 	$exclude[] = "cache";
@@ -334,12 +334,12 @@ function table_list() {
 	$exclude[] = "submit_news";
 	 $exclude[] = "rate";
 	$exclude[] = "stat_counter";
-	 
+
 	$exclude[] = "french_news"; // still trying to find an efficient way to remove
 	//  tables of non-default languages eg french_news. any ideas?
-	 
+
 	//   print_r($search);
-	 
+
 	$tables = mysql_list_tables($mySQLdefaultdb);
 	while (list($temp) = mysql_fetch_array($tables)) {
 		$e107tab = str_replace(MPREFIX, "", $temp);
@@ -347,37 +347,25 @@ function table_list() {
 			$tabs[] = $e107tab;
 		}
 	}
-	 
+
 	return $tabs;
 }
-	
+
 // ----------------------------------------------------------------------------
-	
+
 function get_langlist() {
 	global $pref;
 	$handle = opendir(e_LANGUAGEDIR);
 	while ($file = readdir($handle)) {
 		if (!strstr($file, ".") && $file != $pref['sitelanguage'] && $file != "CVS") {
 			$lanlist[] = $file;
-			 
+
 		}
 	}
 	closedir($handle);
-	 
+
 	return $lanlist;
-	 
+
 }
-	
-	
-	
-function headerjs() {
-	$text = "
-		<script type='text/javascript'>
-		function checkit(langtab){
-		return confirm('Delete all '+langtab+' tables?');
-		}
-		</script>";
-	return $text;
-}
-	
+
 ?>
