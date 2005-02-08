04f937b93c50b221a47462b36e164b15c67cae50
e107coders <e107coders@localhost>
2005-02-08 22:03:29 +0000

    News shortcode added {NEWS_CATEGORIES} . Tried to cleanup the old multi-language code in alt_news - still needs testing. new_category.sc renamed to news_alt.sc
    svn path=/trunk/e107_0.7/; revision=1784

--- e107_plugins/alt_news/alt_news.php
+++ e107_plugins/alt_news/alt_news.php
@@ -11,13 +11,13 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/alt_news/alt_news.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-01-27 19:52:33 $
-|     $Author: streaky $
+|     $Revision: 1.5 $
+|     $Date: 2005-02-08 22:01:52 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 require_once(e_HANDLER."news_class.php");
-	
+
 function alt_news($news_category) {
 	global $ml, $sql, $aj, $ns;
 	$ix = new news;
@@ -26,13 +26,7 @@ function alt_news($news_category) {
 		if ($category != 0) {
 			$gen = new convert;
 			$sql2 = new db;
-			// ML
-			if (e_MLANG == 1) {
-				$ml->e107_ml_Select("news_category", "*", "category_id='$category'");
-			} else {
-				$sql->db_Select("news_category", "*", "category_id='$category'");
-			}
-			// END ML
+			$sql->db_Select("news_category", "*", "category_id='$category'");
 			list($category_id, $category_name, $category_icon) = $sql->db_Fetch();
 			$category_name = $aj->tpa($category_name);
 			if (strstr($category_icon, "../")) {
@@ -40,8 +34,8 @@ function alt_news($news_category) {
 			} else {
 				$category_icon = THEME.$category_icon;
 			}
-			// ML
-			if ((e_MLANG != 1 && $count = $sql->db_Select("news", "*", "news_category='$category' ORDER BY news_datestamp DESC")) || (e_MLANG == 1 && $count = $ml->e107_ml_Select("news", "*", "news_category='$category' ORDER BY news_datestamp DESC"))) {
+
+			if ($count = $sql->db_Select("news", "*", "news_category='$category' ORDER BY news_datestamp DESC")) {
 				while ($row = $sql->db_Fetch()) {
 					extract($row);
 					if ($news_title == "") {
@@ -52,7 +46,7 @@ function alt_news($news_category) {
 					$comment_total = $sql2->db_Count("comments", "(*)", "WHERE comment_item_id='$news_id' AND comment_type='0' ");
 					$text .= "<div class='mediumtext'>
 						<img src='".THEME."images/bullet2.gif' alt='bullet' /> ";
-					 
+
 					if ($news_allow_comments) {
 						$text .= "<a href='news.php?extend.".$news_id."'>".$news_title."</a>";
 					} else {
@@ -76,14 +70,13 @@ function alt_news($news_category) {
 		}
 		return TRUE;
 	}
-	 
-	// ML
-	if ((e_MLANG != 1 && $sql->db_Select("news", "*", "news_class<255 AND (news_start=0 || news_start < ".time().") AND (news_end=0 || news_end>".time().") AND news_category='$news_category' ORDER BY news_datestamp DESC LIMIT 0,".ITEMVIEW)) || (e_MLANG == 1 && $ml->e107_ml_Select("news", "*", "news_class<255 AND (news_start=0 || news_start < ".time().") AND (news_end=0 || news_end>".time().") AND news_category='$news_category' ORDER BY news_datestamp DESC LIMIT 0,".ITEMVIEW))) {
+
+	if ($sql->db_Select("news", "*", "news_class<255 AND (news_start=0 || news_start < ".time().") AND (news_end=0 || news_end>".time().") AND news_category='$news_category' ORDER BY news_datestamp DESC LIMIT 0,".ITEMVIEW)) {
 		$sql2 = new db;
 		while (list($news['news_id'], $news['news_title'], $news['data'], $news['news_extended'], $news['news_datestamp'], $news['admin_id'], $news_category, $news['news_allow_comments'], $news['news_start'], $news['news_end'], $news['news_class']) = $sql->db_Fetch()) {
-			 
+
 			if (check_class($news['news_class']) || !$news['news_class']) {
-				 
+
 				if ($news['admin_id'] == 1 && $pref['siteadmin']) {
 					$news['admin_name'] = $pref['siteadmin'];
 				}
@@ -92,14 +85,9 @@ function alt_news($news_category) {
 					list($news['admin_name']) = $sql2->db_Fetch();
 					cachevars($news['admin_id'], $news['admin_name']);
 				}
-				// ML
-				if (e_MLANG == 1) {
-					$ml->e107_ml_Select("news_category", "*", "category_id='$news_category' ", "default", FALSE, "sql2");
-				} else {
+
 					$sql2->db_Select("news_category", "*", "category_id='$news_category' ");
-				}
-				// END ML
-				 
+
 				list($news['category_id'], $news['category_name'], $news['category_icon']) = $sql2->db_Fetch();
 				$news['comment_total'] = $sql2->db_Count("comments", "(*)", "WHERE comment_item_id='".$news['news_id']."' AND comment_type='0' ");
 				$ix->render_newsitem($news);
@@ -113,13 +101,9 @@ function alt_news($news_category) {
 						list($news['admin_name']) = $sql2->db_Fetch();
 						cachevars($news['admin_id'], $news['admin_name']);
 					}
-					// ML
-					if (e_MLANG == 1) {
-						$ml->e107_ml_Select("news_category", "*", "category_id='$news_category' ", "default", FALSE, "sql2");
-					} else {
-						$sql2->db_Select("news_category", "*", "category_id='$news_category' ");
-					}
-					// END ML
+
+					$sql2->db_Select("news_category", "*", "category_id='$news_category' ");
+
 					list($news['category_id'], $news['category_name'], $news['category_icon']) = $sql2->db_Fetch();
 					$ix->render_newsitem($news, "", "userclass");
 				}
@@ -127,5 +111,5 @@ function alt_news($news_category) {
 		}
 	}
 }
-	
+
 ?>
