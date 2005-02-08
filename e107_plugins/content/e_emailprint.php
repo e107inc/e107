1135a49c726839e429006aa0ab3e8bef624a943b
lisa_ <lisa_@localhost>
2005-02-08 00:16:01 +0000

    sql improvements
    svn path=/trunk/e107_0.7/; revision=1752

--- e107_plugins/content/e_emailprint.php
+++ e107_plugins/content/e_emailprint.php
@@ -1,52 +1,14 @@
 <?php
 
-// ##### give $email and $print a value : the value would be the same and used exclusively for your plugin
-$email = "pcontent";
-$print = "pcontent";
-
-//method: include this handler in your file from where you want to be able to email/print content
-	//require_once(e_HANDLER."emailprint_class.php");
-	//$ep = new emailprint;
-//calling: use this methode to call the email and print icons
-	//$ep -> render_emailprint($plugintable, $content_id, "0");
-	//0=both email and print icons; 1=email only; 2=print only
-
-// ##### EMAIL ROUTINE ----------------------------------------------------------------------------
-if(eregi('email.php', e_SELF)){
-
-		//create your own [$message .= "blah"] text to be used in the email that will be send
-		//$id is inherited from the email.php file and holds the unique id from the table
-		
-		$sql = new db;
-		if($sql -> db_Select($table, "content_id, content_heading, content_subheading, content_text, content_author, content_parent, content_datestamp, content_class", "content_id='$id' ")){
-			while($row = $sql -> db_Fetch()){
-				extract($row);
-
-				$tmp = explode(".",$content_parent);
-				$type_id = ($tmp[0] == "0" ? $tmp[1] : $tmp[0]);
-
-				//just create your message here, this will be added to the already existing $message var, so start with [$message .= "blah"] and not with [$message = "blah"] !
-				$message .= SITEURL.e_PLUGIN."content/content.php?type.".$type_id.".content.".$id."\n\n";
-				$message .= $content_heading."\n";
-				$message .= $content_subheading."\n";
-			}
-		}
-}
-
-
-// ##### PRINT ROUTINE ----------------------------------------------------------------------------
-if(eregi('print.php', e_SELF)){
-		
-		//create your own print page layout with the content you need
-		//use $text = "blah" and have it contain a full html page as it will be echo'ed out
-		//$id is inherited from the email.php file and holds the unique id from the table
-
+function print_item($id)
+{
 		global $tp;
 		$con = new convert;
 
 		require_once(e_PLUGIN."content/handlers/content_class.php");
 		$aa = new content;
 		
+		if(!is_object($sql)){ $sql = new db; }
 		$sql -> db_Select($plugintable, "content_id, content_heading, content_subheading, content_text, content_author, content_parent, content_datestamp, content_class", "content_id='$id' ");
 		$row = $sql -> db_Fetch();
 		extract($row);
@@ -54,7 +16,6 @@ if(eregi('print.php', e_SELF)){
 		if(!check_class($content_class)){
 			header("location:".e_PLUGIN."content/content.php"); exit;
 		}
-		
 		$content_heading = $tp -> toHTML($content_heading);
 		$content_subheading = $tp -> toHTML($content_subheading);
 		$content_text = ereg_replace("\{EMAILPRINT\}|\[newpage\]", "", $tp -> toHTML($content_text, TRUE));
@@ -64,14 +25,6 @@ if(eregi('print.php', e_SELF)){
 		$type_id = ($tmp[0] == "0" ? $tmp[1] : $tmp[0]);
 
 		$text = "
-		<html>
-		<head>
-		<style type='text/css'>
-		body{ font-size:11px; color: #000; font-family: Tahoma, Verdana, Arial, Helvetica; text-decoration: none; }
-		table tr td{ font-size:11px; color: #000; font-family: Tahoma, Verdana, Arial, Helvetica; text-decoration: none; }
-		</style>
-		</head>
-		<body>        
 		<b>".$content_heading."</b>
 		<br />
 		".$content_subheading."
@@ -83,8 +36,27 @@ if(eregi('print.php', e_SELF)){
 		this content item is from ".SITENAME."
 		<br />
 		( http://".$_SERVER[HTTP_HOST].e_HTTP.e_PLUGIN."content/content.php?type.".$type_id.".content.".$content_id." )        
-		</body>
-		</html>";
+		";
+
+		return $text;
+}
+
+function email_item($id)
+{
+	$plugintable = "pcontent";
+	if(!is_object($sql)){ $sql = new db; }
+	if($sql -> db_Select($plugintable, "content_id, content_heading, content_subheading, content_text, content_author, content_parent, content_datestamp, content_class", "content_id='$id' ")){
+		while($row = $sql -> db_Fetch()){
+		extract($row);
+			$tmp = explode(".",$content_parent);
+			$type_id = ($tmp[0] == "0" ? $tmp[1] : $tmp[0]);
+			if(!check_class($content_class)){
+				header("location:".e_PLUGIN."content/content.php?type.".$type_id); exit;
+			}
+			$message = SITEURL.e_PLUGIN."content/content.php?type.".$type_id.".content.".$id."\n\n".$content_heading."\n".$content_subheading."\n";
+		}
+		return $message;
+	}
 }
 
 ?>
