109ac1b3b3f0fae0df475f305dbb3c7d6d4cb114
e107coders <e107coders@localhost>
2005-02-09 12:54:17 +0000

    Templating of othernews - $OTHERNEWS_STYLE
    svn path=/trunk/e107_0.7/; revision=1795

--- e107_plugins/other_news_menu/other_news_menu.php
+++ e107_plugins/other_news_menu/other_news_menu.php
@@ -11,23 +11,39 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/other_news_menu/other_news_menu.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-01-27 19:53:12 $
-|     $Author: streaky $
+|     $Revision: 1.4 $
+|     $Date: 2005-02-09 12:54:17 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 unset($text);
-	
-	
+global $OTHERNEWS_STYLE;
+if(!$OTHERNEWS_STYLE){
+   //	$OTHERNEWS_STYLE = "<img src='".THEME."images/bullet2.gif' alt='bullet' />&nbsp;{OTHERNEWS_LINK}<br />";
+}
+
 if ($sql->db_Select("news", "*", "news_render_type=2 ORDER BY news_datestamp DESC LIMIT 0,10")) {
 	unset($text);
 	while ($row = $sql->db_Fetch()) {
 		extract($row);
 		if (check_class($news_class)) {
-			$text .= "<img src='".THEME."images/bullet2.gif' alt='bullet' /> <a href='".e_BASE."news.php?item.$news_id'>$news_title</a><br />\n";
+
+            $search[0] = "/\{OTHERNEWS_LINK\}(.*?)/si";
+			$replace[0] = ($news_id) ? "<a href='".e_BASE."news.php?item.$news_id'>$news_title</a>" : "";
+
+            $search[1] = "/\{OTHERNEWS_SUMMARY\}(.*?)/si";
+			$replace[1] =  ($news_summary) ? $tp->toHTML($news_summary) : "" ;
+
+			$search[2] = "/\{OTHERNEWS_THUMBNAIL\}(.*?)/si";
+			$replace[2] = ($news_thumb) ? "<a href='news.php?extend.".$news_id."'><img src='".e_IMAGE."newspost_images/".$news_thumb."' alt='' style='border:0px' /></a>" : "";
+
+
+
+            $text .= preg_replace($search, $replace,$OTHERNEWS_STYLE);
+		  //	$text .= "<img src='".THEME."images/bullet2.gif' alt='bullet' /> <a href='".e_BASE."news.php?item.$news_id'>$news_title</a><br />\n";
 		}
 	}
 	$ns->tablerender(TD_MENU_L1, $text, 'other_news');
 }
-	
+
 ?>
