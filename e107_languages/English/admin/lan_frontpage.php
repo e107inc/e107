98dd3bd3590bfcb9fc2017051704f79f2bd48382
lisa_ <lisa_@localhost>
2005-02-10 23:03:35 +0000

    Frontpage : reworked to show content main, category and items
    svn path=/trunk/e107_0.7/; revision=1852

--- e107_languages/English/admin/lan_frontpage.php
+++ e107_languages/English/admin/lan_frontpage.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_frontpage.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-02-01 00:49:39 $
-|     $Author: sweetas $
+|     $Revision: 1.3 $
+|     $Date: 2005-02-10 23:03:27 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 define("FRTLAN_1", "Front Page settings updated.");
@@ -19,6 +19,16 @@ define("FRTLAN_7", "Content Page");
 define("FRTLAN_12", "Update Front Page Settings");
 define("FRTLAN_13", "Front Page Settings");
 define("FRTLAN_15", "Other (enter url):");
+define("FRTLAN_16", "error: no content main parent selected");
+define("FRTLAN_17", "error: no content sub category selected");
+define("FRTLAN_18", "error: no content item selected");
+define("FRTLAN_19", "content main parent");
+define("FRTLAN_20", "content category");
+define("FRTLAN_21", "content item");
+define("FRTLAN_22", "");
+define("FRTLAN_23", "");
+define("FRTLAN_24", "");
+define("FRTLAN_25", "");
 
 
 ?>
