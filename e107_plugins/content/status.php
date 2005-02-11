d752d2c1fb4d1ac0dbd682955726d9afe71bb119
lisa_ <lisa_@localhost>
2005-02-11 08:15:01 +0000

    Content : small query fix
    svn path=/trunk/e107_0.7/; revision=1860

--- e107_plugins/content/status.php
+++ e107_plugins/content/status.php
@@ -4,7 +4,7 @@ if(!is_object($sql2)){ $sql2 = new db; }
 $maincat = $sql -> db_Select("pcontent", "content_id, content_heading", "content_parent = '0' ORDER BY content_heading");
 while($row = $sql -> db_Fetch()){
 	extract($row);
-		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,1) = '".$content_id."' AND content_refer != 'sa' ");
+		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,".strlen($content_id).") = '".$content_id."' AND content_refer != 'sa' ");
 		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' /> ".$content_heading.": ".$count."</div>";
 }
 
