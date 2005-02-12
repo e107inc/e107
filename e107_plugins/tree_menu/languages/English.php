d21e6b129c45d7b2aca6acfb2c437b30534f89c7
e107steved <steved@e107.org>
2005-02-12 21:33:27 +0000

    Added the ability to set which css classes the tree menu uses from admin area.
    svn path=/trunk/e107_0.7/; revision=1887

---
+++ e107_plugins/tree_menu/languages/English.php
@@ -0,0 +1,26 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/tree_menu/languages/English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-02-12 21:33:27 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+	
+	
+define("TREE_L1", "Configure Tree Menu");
+define("TREE_L2", "Update Tree Menu Settings");
+define("TREE_L3", "Tree Menu configuration saved.");
+define("TREE_L4", "On");
+define("TREE_L5", "Off");
+?>
