4601848ba8fc55a6971488084c54984cea06ca33
sweetas <sweetas@localhost>
2005-02-14 15:58:22 +0000

    chatbox handler converted to use new search class
    svn path=/trunk/e107_0.7/; revision=1939

--- e107_handlers/search/search_chatbox.php
+++ e107_handlers/search/search_chatbox.php
@@ -11,27 +11,32 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/search/search_chatbox.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-01-27 19:52:31 $
-|     $Author: streaky $
+|     $Revision: 1.3 $
+|     $Date: 2005-02-14 15:58:22 $
+|     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
-	
-if ($results = $sql->db_Select("chatbox", "*", "(cb_nick REGEXP('".$query."') OR cb_message REGEXP('".$query."')) AND cb_blocked='0' ")) {
-	while (list($cb_id, $cb_nick, $cb_message, $cb_datestamp, $cb_blocked, $cb_ip) = $sql->db_Fetch()) {
-		$cb_nick = eregi_replace("[0-9]+\.", "", $cb_nick);
-		 
-		$cb_nick_ = parsesearch($cb_nick, $query);
-		$cb_message_ = parsesearch($cb_message, $query);
-		if (!$cb_nick_) {
-			$cb_nick_ = $cb_nick;
-		}
-		if (!$cb_message_) {
-			$cb_message_ = $cb_message;
-		}
-		$text .= "<img src=\"".THEME."images/bullet2.gif\" alt=\"bullet\" /> <a href=\"chat.php?".$cb_id.".fs\">$cb_nick_</a><br />$cb_message_<br /><br />";
-	}
-} else {
-	$text .= LAN_198;
+
+$return_fields = 'cb_id, cb_nick, cb_message, cb_datestamp';
+$search_fields = array('cb_nick', 'cb_message');
+$weights = array('1', '1');
+$time = time();
+$where = "";
+$sql_order = ", cb_datestamp DESC";
+$php_order = array("cb_datestamp", SORT_DESC);
+$no_results = LAN_198;
+$ps = $sch -> parsesearch('chatbox', $return_fields, $search_fields, $weights, 'search_chatbox', $no_results, $where, $sql_order, $php_order);
+$text .= $ps['text'];
+$results = $ps['results'];
+
+function search_chatbox($row) {
+	global $con;
+	$row['cb_nick'] = eregi_replace("[0-9]+\.", "", $row['cb_nick']);
+	$res['link'] = e_PLUGIN."chatbox_menu/chat.php?".$row['cb_id'].".fs";
+	$res['title'] = $row['cb_nick'];
+	$res['summary'] = $row['cb_message'];
+	$res['detail'] = $con -> convert_date($row['cb_datestamp'], "long");
+	return $res;
 }
+
 ?>
