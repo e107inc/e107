91659d6a85431a28d4eeb774a99d8171d7598a8f
e107coders <e107coders@localhost>
2005-02-17 02:01:34 +0000

    Fixed some visual bugs. Added query option of 'referer' - allows you to add the "email" option to any page of e107.
    svn path=/trunk/e107_0.7/; revision=1991

--- email.php
+++ email.php
@@ -11,14 +11,14 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/email.php,v $
-|     $Revision: 1.8 $
-|     $Date: 2005-02-10 14:40:03 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.9 $
+|     $Date: 2005-02-17 02:01:34 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
 require_once(HEADERF);
-	
+
 $qs = explode(".", e_QUERY, 2);
 if ($qs[0] == "") {
 	header("location:".e_BASE."index.php");
@@ -26,48 +26,45 @@ if ($qs[0] == "") {
 }
 $source = $qs[0];
 $parms = $qs[1];
-	
+
+$emailurl = ($source == "referer") ? $_SERVER['HTTP_REFERER'] : SITEURL;
+
+
 $comments = $tp->post_toHTML($_POST['comment'], TRUE, 'retain_nl');
 $author = $tp->post_toHTML($_POST['author_name']);
 $email_send = check_email($_POST['email_send']);
 
-if (isset($_POST['emailsubmit']))
-{
-	if (!$email_send)
-	{
+if (isset($_POST['emailsubmit'])){
+	if (!$email_send){
 		$error .= LAN_106;
 	}
-	if ($comments == "")
-	{
+	if ($comments == ""){
 		$message = LAN_188." ".SITENAME." (".SITEURL.")";
-		if (USER == TRUE)
-		{
+		if (USER == TRUE){
 			$message .= "\n\n".LAN_email_1." ".USERNAME;
 		}
-		else
-		{
+		else {
 			$message .= "\n\n".LAN_email_1." ".$author;
 		}
-	}
-	else
-	{
+	} else {
 		$message .= $comments;
 	}
 	$ip = getip();
 	$message .= "\n\n".LAN_email_2." ".$ip."\n\n";
-	 
-	if(strpos($source,'plugin:') !== FALSE)
-	{
+
+	if(strpos($source,'plugin:') !== FALSE) {
 		$plugin = substr($source,7);
-		if(file_exists(e_PLUGIN.$plugin."/e_emailprint.php"))
-		{
+		if(file_exists(e_PLUGIN.$plugin."/e_emailprint.php")){
 			include_once(e_PLUGIN.$plugin."/e_emailprint.php");
 			$text = email_item($parms);
+			$emailurl = SITEURL;
 		}
 		$message .= $text;
-	}
-	else
-	{
+	}elseif($source == "referer"){
+            $message .= $_POST['referer'];
+            $emailurl = $_POST['referer'];
+	} else {
+		$emailurl = SITEURL;
 		$sql->db_Select("news", "*", "news_id='$parms'");
 		list($news_id, $news_title, $news_body, $news_extended, $news_datestamp, $news_author, $news_source, $news_url, $news_category, $news_allow_comments) = $sql->db_Fetch();
 		$message .= $tp->toHTML($news_title, TRUE)."\n".$tp->toHTML($news_body, TRUE)."\n".$tp->toHTML($news_extended, TRUE)."\n\n".SITEURL.e_BASE."comment.php?comment.news.".$parms;
@@ -84,49 +81,53 @@ if (isset($_POST['emailsubmit']))
 		$ns->tablerender(LAN_12, "<div style='text-align:center'>".$error."</div>");
 	}
 }
-	
+
+
+
+
 $text = "<form method='post' action='".e_SELF."?".e_QUERY."'>\n
 	<table>";
-	
+
 if (USER != TRUE) {
 	$text .= "<tr>
-		<td style='width:20%'>".LAN_7."</td>
-		<td style='width:80%'>
-		<input class='tbox' type='text' name='author_name' size='60' value='$author' maxlength='100' />
+		<td style='width:25%'>".LAN_7."</td>
+		<td style='width:75%'>
+		<input class='tbox' type='text' name='author_name' size='60' style='width:95%' value='$author' maxlength='100' />
 		</td>
 		</tr>";
 }
 $text .= "<tr>
-	<td style='width:20%'>".LAN_8."</td>
-	<td style='width:80%'>
-	<textarea class='tbox' name='comment' cols='70' rows='4'>".LAN_email_6." ".SITENAME." (".SITEURL.")";
+	<td style='width:25%'>".LAN_8."</td>
+	<td style='width:75%'>
+	<textarea class='tbox' name='comment' cols='70' rows='4' style='width:95%'>".LAN_email_6." ".SITENAME." (".$emailurl.")";
 if (USER == TRUE) {
 	$text .= "\n\n".LAN_email_1." ".USERNAME;
 }
-	
+
 $text .= "</textarea>
 	</td>
 	</tr>
-	 
+
 	<tr>
-	<td style='width:20%'>".LAN_187."</td>
-	<td style='width:80%'>
-	<input class='tbox' type='text' name='email_send' size='60' value='$email_send' maxlength='100' />
+	<td style='width:25%'>".LAN_187."</td>
+	<td style='width:75%'>
+	<input class='tbox' type='text' name='email_send' size='60' value='$email_send' style='width:95%' maxlength='100' />
 	</td>
 	</tr>
-	 
+
 	<tr style='vertical-align:top'>
-	<td style='width:20%'></td>
-	<td style='width:80%'>
+	<td style='width:25%'></td>
+	<td style='width:75%'>
 	<input class='button' type='submit' name='emailsubmit' value='".LAN_email_4."' />
-	</td>
+	<input type='hidden' name='referer' value='".$_SERVER['HTTP_REFERER']."' />
+</td>
 	</tr>
 	</table>
 	</form>";
-	
+
 $ns->tablerender(LAN_email_5, $text);
-	
-	
-	
+
+
+
 require_once(FOOTERF);
 ?>
