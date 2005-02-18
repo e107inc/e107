9039806e4e935b6ecedeb38cfac1fdd2a2d69a92
e107steved <steved@e107.org>
2005-02-18 20:13:43 +0000

    New blog theme, thanks to CraHan for permission to port it.
    svn path=/trunk/e107_0.7/; revision=2016

---
+++ e107_themes/crahan/theme.php
@@ -0,0 +1,129 @@
+<?php
+/*
++---------------------------------------------------------------+
+|	e107 website system
+|
+|	©Steve Dunstan 2001-2005
+|	http://e107.org
+|	jalist@e107.org
+|
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+
+// [theme]
+$themename = "CraHan";
+$themeversion = "1.0";
+$themeauthor = "Steve Dunstan [jalist]";
+$themedate = "29/01/2005";
+$themeinfo = "Based on the theme by CraHan at his homepage http://n00.be, design used with permission *smooch*";
+define("STANDARDS_MODE", TRUE);
+
+$logo = "logo".rand(1, 4);
+
+// [layout]
+
+$layout = "_default";
+
+$HEADER = "
+<div id='wrapper'>
+<div id='menu'>
+<div class='content'>
+<div class='fixfuckedie'>
+{MENU=1}
+</div>
+</div>
+</div>
+<div id='main'>
+<div id='sitebanner'>
+<div id='$logo'><div class='smalltext' style='text-align: right; vertical-align: bottom;'>{SITENAME}&nbsp;</div></div>
+<div id='navbar'>
+{SITELINKS}
+</div>
+</div>
+<div class='content'>
+<div class='fixfuckedie'>
+";
+
+$FOOTER = "
+</div>
+</div>
+<br />
+<br />
+<div class='smalltext' style='text-align: center;'>{SITEDISCLAIMER}</div>
+</div>
+</div>
+";
+
+$NEWSSTYLE = "
+<div class='header'>
+<div class='left'>{STICKY_ICON} {NEWSTITLE}</div>
+<div class='right'>{NEWSDATE}</div>
+</div>
+<div class='bodytable' style='text-align:left'>
+{NEWSBODY}
+{EXTENDED}
+</div>
+<br />
+<div class='newssmalltext'>[ {NEWSAUTHOR} :: {NEWSCOMMENTS}{TRACKBACK} ]</div>
+<br />
+<br />";
+
+define("ICONSTYLE", "float: left; border:0");
+define("COMMENTLINK", "comment(s): ");
+define("COMMENTOFFSTRING", "Comments are turned off for this item");
+define("PRE_EXTENDEDSTRING", "<br /><br />[ ");
+define("EXTENDEDSTRING", "Read the rest ...");
+define("POST_EXTENDEDSTRING", " ]<br />");
+define("TRACKBACKSTRING", "Trackbacks: ");
+define("TRACKBACKBEFORESTRING", " :: ");
+
+
+// [linkstyle]
+
+define('PRELINK', "");
+define('POSTLINK', " ::");
+define('LINKSTART', ":: ");
+define('LINKEND', "");
+define('LINKDISPLAY', 1);
+define('LINKALIGN', "left");
+
+
+//	[tablestyle]
+
+function tablestyle($caption, $text, $mode)
+{
+	echo "<div class='header'>$caption</div>\n$text\n<br /><br />\n";	
+}
+
+$COMMENTSTYLE = "
+
+<div style='padding-left: 25px;'>{COMMENT}<br /><br />
+<div class='newssmalltext'>[ Comment by {USERNAME} :: {TIMEDATE} ]</div>
+</div>
+<br /><br />
+
+
+
+";
+
+$POLLSTYLE = <<< EOF
+<b>Poll:</b> {QUESTION}
+<br /><br />
+{OPTIONS=OPTION<br />BAR<br /><span class='smalltext'>PERCENTAGE VOTES</span><br />\n}
+<br /><div style='text-align:center' class='smalltext'>{AUTHOR}<br />{VOTE_TOTAL} {COMMENTS}
+<br />
+{OLDPOLLS}
+</div>
+EOF;
+
+$CHATBOXSTYLE = "
+<img src='".e_IMAGE."admin_images/chatbox_16.png' alt='' style='vertical-align: middle;' />
+<b>{USERNAME}</b>
+<div class='smalltext'>
+{MESSAGE}
+</div>
+<br />";
+
+?>
