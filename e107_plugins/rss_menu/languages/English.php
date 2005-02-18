bee247f0dd59484ffdc12e669378c8bf8fd05533
e107coders <e107coders@localhost>
2005-02-18 20:41:59 +0000

    rss language file
    svn path=/trunk/e107_0.7/; revision=2019

---
+++ e107_plugins/rss_menu/languages/English.php
@@ -0,0 +1,21 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/rss_menu/languages/English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-02-18 20:41:59 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+	
+define("BACKEND_MENU_L1", " can be syndicated by using these rss feeds.");
+define("BACKEND_MENU_L2", "RSS Feeds");
+
+define("BACKEND_MENU_L3", "Our news");
+define("BACKEND_MENU_L4", "Our comments");
+define("BACKEND_MENU_L5", "Our forum threads");
+define("BACKEND_MENU_L6", "Our forum posts");
+	
+?>
