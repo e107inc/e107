c3e783f7887f442c46268d86c372bed3a885c5b6
e107steved <steved@e107.org>
2005-02-19 16:53:45 +0000

    Bugfix.
    svn path=/trunk/e107_0.7/; revision=2023

--- e107_languages/English/admin/lan_plugin.php
+++ e107_languages/English/admin/lan_plugin.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_plugin.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-02-19 16:33:28 $
+|     $Revision: 1.6 $
+|     $Date: 2005-02-19 16:53:45 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -50,7 +50,7 @@ define("EPL_ADLAN_36", "Parser code insert failed, incorrectly formatted.");
 
 define("EPL_ADLAN_37", "Upload plugin (.zip or .tar.gz format)");
 define("EPL_ADLAN_38", "Upload Plugin");
-define("EPL_ADLAN_39", "The file could not be uploaded as the ".e_PLUGIN." folder does not have the correct permissions - please CHMOD to 755 and reupload the file.");
+define("EPL_ADLAN_39", "The file could not be uploaded as the ".e_PLUGIN." folder does not have the correct permissions - please CHMOD to 777 and reupload the file.");
 define("EPL_ADLAN_40", "Admin Message");
 define("EPL_ADLAN_41", "That file does not appear to be a valid .zip or .tar archive.");
 define("EPL_ADLAN_42", "An error has occurred, unable to un-archive the file");
