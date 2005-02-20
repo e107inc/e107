f91f76e0068b0587969ee6c589a738acdce5b9af
e107coders <e107coders@localhost>
2005-02-20 01:41:54 +0000

    lang files for some plugins.
    svn path=/trunk/e107_0.7/; revision=2031

---
+++ e107_plugins/log/languages/French.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/log/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-02-20 01:41:54 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+define("ADSTAT_L1", "Ce plugin va enregistrer toutes les visites sur votre site, et construire des pages de statistiques détaillés sur la base des informations obtenues.");
+define("ADSTAT_L2", "L'enregistreur de statistiques a été installé avec succès.");
+define("ADSTAT_L3", "Enregistrement de Statistiques");
+?>
---
+++ e107_plugins/login_menu/languages/French.php
@@ -0,0 +1,56 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-02-20 01:41:54 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+
+define("LOGIN_MENU_L1", "Nom : ");
+define("LOGIN_MENU_L2", "Mot de passe : ");
+define("LOGIN_MENU_L3", "S'inscrire");
+define("LOGIN_MENU_L4", "Oubli&eacute; ?");
+define("LOGIN_MENU_L5", "Bienvenue");
+define("LOGIN_MENU_L6", "Se souvenir de moi");
+define("LOGIN_MENU_L7", "ID unique d'utilisateur non reconnu (possibilit&eacute; de cookie corrompu).<br />Veuillez <a href=\"".e_BASE."index.php?logout\">cliquer ici</a> pour d&eacute;truire le cookie.");	// Les liens hypertextes avec "cliquer ici" sont très mauvais ... J'ai préféré ne rien changer
+define("LOGIN_MENU_L8", "Se deconnecter");
+define("LOGIN_MENU_L9", "Erreur de connexion");
+define("LOGIN_MENU_L10", "Le drapeau de maintenance est activ&eacute;, cela veut dire que les visiteurs seront redirig&eacute;s vers la page sitedown.php. Pour annuler, aller dans la zone d'administration.");
+define("LOGIN_MENU_L11", "Administration");
+define("LOGIN_MENU_L12", "Param&egrave;tres");
+define("LOGIN_MENU_L13", "Profil");
+
+define("LOGIN_MENU_L14", "news");
+define("LOGIN_MENU_L15", "news");
+define("LOGIN_MENU_L16", "message (chatbox)");
+define("LOGIN_MENU_L17", "messages (chatbox)");
+define("LOGIN_MENU_L18", "commentaire");
+define("LOGIN_MENU_L19", "commentaires");
+define("LOGIN_MENU_L20", "post (forums)");
+define("LOGIN_MENU_L21", "posts (forums)");
+define("LOGIN_MENU_L22", "nouveau membre");
+define("LOGIN_MENU_L23", "nouveaux membres");
+define("LOGIN_MENU_L24", "Cliquez ici pour afficher la liste des nouveaut&eacute;s");
+define("LOGIN_MENU_L25", "Depuis votre derni&egrave;re visite, il y'a eu");
+define("LOGIN_MENU_L26", "aucun");
+define("LOGIN_MENU_L27", "et");
+
+define("LOGIN_MENU_L28", "Se connecter");
+
+define("LOGIN_MENU_L29", "nouvel article");
+define("LOGIN_MENU_L30", "nouveaux articles");
+
+define('LOGIN_MENU_L31', 'Indique Les News Nouvellement Postées');
+define('LOGIN_MENU_L32', 'Indique Les Nouveaux Articles Postés');
+define('LOGIN_MENU_L33', 'Indique Les Nouvelles Chatbox Postées');
+define('LOGIN_MENU_L34', 'Indique Les Nouveaux Commentaires Postés');
+define('LOGIN_MENU_L35', 'Indique Les Nouveaux Forums Postés');
+define('LOGIN_MENU_L36', 'Indique Les Nouveaux Membres');
+define('LOGIN_MENU_L37', 'Mise à jour des Préférences');
+define('LOGIN_MENU_L38', 'Préférences Mises à jour');
+
+?>
