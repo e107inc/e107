b8ed7d23669f068b71c69614afe41384fee23268
streaky <streaky@localhost>
2005-02-22 19:40:17 +0000

    Updated to reflect the new prefs sytem - although core backup isn't quite working corectly yet...
    svn path=/trunk/e107_0.7/; revision=2060

--- e107_files/resetcore.php
+++ e107_files/resetcore.php
@@ -11,15 +11,19 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_files/resetcore.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-01-27 20:07:11 $
+|     $Revision: 1.4 $
+|     $Date: 2005-02-22 19:40:17 $
 |     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
+
 require_once("../e107_config.php");
 mysql_connect($mySQLserver, $mySQLuser, $mySQLpassword);
 mysql_select_db($mySQLdefaultdb);
-	
+
+require_once('../'.$HANDLERS_DIRECTORY.'arraystorage_class.php');
+$eArrayStorage = new ArrayData();
+
 echo "<?xml version='1.0' encoding='iso-8859-1' ?>\n";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -31,30 +35,30 @@ echo "<?xml version='1.0' encoding='iso-8859-1' ?>\n";
 <meta http-equiv="content-style-type" content="text/css" />
 </head>
 <body>
-
 <div class='mainbox'>
-
 <a href="http://e107.org"><img src="../e107_images/logo.png" alt="Logo" style="border:0" /></a>
-<br /><br />
-<span class="smalltext">©Steve Dunstan 2002-2004. See gpl.txt for license details.</span><br /><br /><br />
-
-
+<br />
+<br />
+<span class="smalltext">©Steve Dunstan 2002-2004. See gpl.txt for license details.</span>
+<br />
+<br />
+<br />
 <?php
-	
+
 if (isset($_POST['usubmit'])) {
-	 
+
 	$a_name = $_POST['a_name'];
 	$a_password = md5($_POST['a_password']);
-	 
+
 	if ($result = mysql_query("SELECT * FROM ".$mySQLprefix."user WHERE user_name='$a_name' AND user_password='$a_password' AND user_perms=0")) {
 		if ($row = mysql_fetch_array($result)) {
 			extract($row);
-			 
+
 			$result = mysql_query("SELECT * FROM ".$mySQLprefix."core WHERE e107_name='pref_backup' ");
 			$bu_exist = ($row = mysql_fetch_array($result) ? TRUE : FALSE);
-			 
+
 			$admin_directory = "e107_admin";
-			 
+
 			echo "<span class='headertext2'><b>Please select which method you want to use, then click the button to proceed ...</b></span><br /><br /><br /><br />
 				<div style='text-align:center'>
 				<form method='post' action='".$_SERVER['PHP_SELF']."'>
@@ -76,8 +80,8 @@ if (isset($_POST['usubmit'])) {
 		$END = TRUE;
 	}
 }
-	
-	
+
+
 if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 2) {
 	$a_name = $_POST['a_name'];
 	$a_password = $_POST['a_password'];
@@ -88,7 +92,7 @@ if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 2) {
 	if (!$at) {
 		exit;
 	}
-	 
+
 	$tmpr = substr(str_replace($_SERVER['DOCUMENT_ROOT'], "", $_SERVER['SCRIPT_FILENAME']), 1);
 	$root = "/".substr($tmpr, 0, strpos($tmpr, "/"))."/";
 	define("e_HTTP", $root);
@@ -98,7 +102,7 @@ if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 2) {
 	for($i = 1; $i <= $num_levels; $i++) {
 		$link_prefix .= "../";
 	}
-	 
+
 	define("e_ADMIN", e_HTTP.$admin_directory."/");
 	define("e_SELF", "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
 	define("e_QUERY", eregi_replace("&|/?PHPSESSID.*", "", $_SERVER['QUERY_STRING']));
@@ -106,20 +110,21 @@ if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 2) {
 	$e_path = (!strpos($_SERVER['SCRIPT_FILENAME'], ".php") ? $_SERVER['PATH_TRANSLATED'] : $_SERVER['SCRIPT_FILENAME']);
 	define("e_PATH", $e_path);
 	require_once(e_BASE.$FILES_DIRECTORY."def_e107_prefs.php");
-	 
-	$tmp = addslashes(serialize($pref));
-	 
-	mysql_query("DELETE FROM ".$mySQLprefix."core WHERE e107_name='pref'");
-	if (!mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('pref', '$tmp')")) {
+
+	$PrefOutput = $eArrayStorage->WriteArray($pref);
+	
+	mysql_query("DELETE FROM ".$mySQLprefix."core WHERE e107_name='SitePrefs' OR e107_name='SitePrefs_Backup'");
+	if (!mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs', '{$PrefOutput}')")) {
 		$message = "Rebuild failed ...";
 		$END = TRUE;
 	} else {
+		mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs_Backup', '{$PrefOutput}')");
 		$message = "Core reset. <br /><br /><a href='../index.php'>Click here to continue</a>";
 		$END = TRUE;
 	}
 }
-	
-	
+
+
 if (isset($_POST['coreedit_sub'])) {
 	$a_name = $_POST['a_name'];
 	$a_password = $_POST['a_password'];
@@ -130,23 +135,22 @@ if (isset($_POST['coreedit_sub'])) {
 	if (!$at) {
 		exit;
 	}
-	 
-	 
-	 
+
 	while (list($key, $prefvalue) = each($_POST)) {
 		$pref[$key] = formtpa($prefvalue);
+		$pref[$key] = str_replace('©', '&copy;', $pref[$key]);
 	}
-	 
-	$tmp = addslashes(serialize($pref));
-	 
-	@mysql_query("UPDATE ".$mySQLprefix."core set e107_value='$tmp' WHERE e107_name='pref' ");
+
+	$PrefOutput = $eArrayStorage->WriteArray($pref);
+	
+	mysql_query("DELETE FROM ".$mySQLprefix."core WHERE e107_name='SitePrefs' OR e107_name='SitePrefs_Backup'");
+	mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs', '{$PrefOutput}')");
+	mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs_Backup', '{$PrefOutput}')");
+
 	$message = "Core settings successfully updated. <br /><br /><a href='../index.php'>Click here to continue</a>";
 	$END = TRUE;
-	 
-	 
-	 
 }
-	
+
 if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 3) {
 	$a_name = $_POST['a_name'];
 	$a_password = $_POST['a_password'];
@@ -157,38 +161,44 @@ if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 3) {
 	if (!$at) {
 		exit;
 	}
-	 
-	$result = @mysql_query("SELECT * FROM ".$mySQLprefix."core WHERE e107_name='pref_backup' ");
-	$row = @mysql_fetch_array($result);
-	$tmp = stripslashes($row['e107_value']);
-	$pref = unserialize($tmp);
-	if (!is_array($pref)) {
-		$pref = unserialize($row['e107_value']);
-	}
-	@mysql_query("UPDATE ".$mySQLprefix."core set e107_value='".$row['e107_value']."' WHERE e107_name='pref' ");
+
+	$result = mysql_query("SELECT * FROM ".$mySQLprefix."core WHERE e107_name='pref_backup'");
+	$row = mysql_fetch_array($result);
+	//$tmp = stripslashes($row['e107_value']);
+	echo $row['e107_value'];
+	$pref = unserialize($row['e107_value']);
+	
+	print_r($pref);
+	
+	$PrefOutput = $eArrayStorage->WriteArray($pref);
+	
+	mysql_query("DELETE FROM ".$mySQLprefix."core WHERE `e107_name` = 'SitePrefs' OR `e107_name` = 'SitePrefs_Backup'");
+	mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs', '{$PrefOutput}')");
+	mysql_query("INSERT INTO ".$mySQLprefix."core VALUES ('SitePrefs_Backup', '{$PrefOutput}')");
+	
 	$message = "Core backup successfully restored. <br /><br /><a href='../index.php'>Click here to continue</a>";
 	$END = TRUE;
 }
-	
-	
+
+
 if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 1) {
-	 
+
 	$a_name = $_POST['a_name'];
 	$a_password = $_POST['a_password'];
 	if (!$result = mysql_query("SELECT * FROM ".$mySQLprefix."user WHERE user_name='$a_name' AND user_password='$a_password' AND user_perms=0")) {
 		exit;
 	}
-	 
-	$result = @mysql_query("SELECT * FROM ".$mySQLprefix."core WHERE e107_name='pref' ");
+
+	$result = @mysql_query("SELECT * FROM ".$mySQLprefix."core WHERE e107_name='SitePrefs'");
 	$row = @mysql_fetch_array($result);
-	$tmp = stripslashes($row['e107_value']);
-	$pref = unserialize($tmp);
-	 
+
+	$pref = $eArrayStorage->ReadArray($row['e107_value']);
+
 	echo "
 		<span class='headertext2'><b>Edit your individual core items and click the button to save - <span class='headertext'>use this script with caution</span>.</b></span><br /><br />
 		<form method='post' action='".$_SERVER['PHP_SELF']."'>
 		<table style='width:95%'>\n";
-	 
+
 	while (list($key, $prefr) = each($pref)) {
 		echo "<tr><td class='headertext' style='width:50%; text-align:right;'>$key&nbsp;&nbsp;</td>
 			<td style='width:50%'><input type='text' name='$key' value='$prefr' size='50' maxlength='100' /></td></tr>\n";
@@ -203,16 +213,16 @@ if (isset($_POST['reset_core_sub']) && $_POST['mode'] == 1) {
 		</form>";
 	$END = TRUE;
 }
-	
+
 if ($message) {
 	echo "<br /><br /><div style='text-align:center'><span class='headertext'>".$message."</span></div><br />";
 }
-	
+
 if ($END) {
 	echo "<br /></div></body></html>";
 	exit;
 }
-	
+
 echo "<span class='headertext2'>
 	This is the e107 resetcore utility. It allows you to completely rebuild your core if it becomes corrupt, or to restore a backup, or to change core settings manually. It won't affect your actual content (news posts, forum posts, articles etc).<br />
 	<b>Only run this utility if your site is failing to load due to a critical core error, or if you need to change a setting and can't log into your admin area.</b></span><br /><br /><br /><br />
@@ -243,12 +253,12 @@ echo "<span class='headertext2'>
 	</div>
 	</body>
 	</html>";
-	
+
 function formtpa($text) {
 	$search = array("\"", "'", "\\", '\"', "\'", "$");
 	$replace = array("&quot;", "&#39;", "&#92;", "&quot;", "&#39;", "&#036;");
 	$text = str_replace($search, $replace, $text);
 	return $text;
 }
-	
+
 ?>
