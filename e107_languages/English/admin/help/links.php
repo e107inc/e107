d8979a77a145666974000aebd81891978da67e75
e107steved <steved@e107.org>
2005-02-26 18:38:58 +0000

    Bugtracker #85.
    svn path=/trunk/e107_0.7/; revision=2123

--- e107_languages/English/admin/help/links.php
+++ e107_languages/English/admin/help/links.php
@@ -1,5 +1,5 @@
 <?php
-$text = "Enter all your site links here. For main site links (the ones displayed in your main navigation bar) set the category to 'Main', any others will be displayed on the Links page. You can seperate these links into different categories.
+$text = "Enter all your site links here. Links added here will be shown in your main navigation menu, for other links please use the Links Page plugin.
 <br />
 <br />
 The submenus generator is ONLY usefull for e107 DHTML menus (TreeMenu, UltraTreeMenu, eDynamicMenu, ypSlideMenu...)";
