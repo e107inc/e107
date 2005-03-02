cf356844da42a5a6951a0126e18126e0b0057931
e107steved <steved@e107.org>
2005-03-02 12:56:33 +0000

    New file, credits.php, lists third-party scripts and resources used in e107.
    svn path=/trunk/e107_0.7/; revision=2148

---
+++ e107_admin/credits.php
@@ -0,0 +1,85 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_admin/credits.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-03-02 12:55:44 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+require_once("../class2.php");
+require_once("auth.php");
+
+
+$creditsArray = array(
+	array(	"name" => "MagpieRSS", 
+				"url" => "http://magpierss.sourceforge.net/", 
+				"description" => "MagpieRSS provides an XML-based (expat) RSS parser in PHP.", 
+				"version" => "0.71.1", 
+				"licence" => "GPL"
+			),
+	array(	"name" => "PCLZip", 
+				"url" => "http://www.phpconcept.net/pclzip/index.en.php", 
+				"description" => "PclZip library offers compression and extraction functions for Zip formatted archives (WinZip, PKZIP).", 
+				"version" => "2.3", 
+				"licence" => "GPL"
+			),
+	array(	"name" => "PCLTar", 
+				"url" => "http://www.phpconcept.net/pcltar/index.en.php", 
+				"description" => "PclTar offer the ability to archive a list of files or directories with or without compression. The archives created by PclTar are readeable by most of gzip/tar applications and by the Windows WinZip application.", 
+				"version" => "1.3", 
+				"licence" => "GPL"
+			),
+	array(	"name" => "TinyMCE", 
+				"url" => "http://tinymce.moxiecode.com/", 
+				"description" => "TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor control released as Open Source under LGPL by Moxiecode Systems AB. It has the ability to convert HTML TEXTAREA fields or other HTML elements to editor instances.", 
+				"version" => "1.42", 
+				"licence" => "GPL"
+			),
+	array(	"name" => "Nuvolo Icons", 
+				"url" => "http://www.icon-king.com", 
+				"description" => "Icons used in e107", 
+				"version" => "1.0", 
+				"licence" => "GPL"
+			),
+	array(	"name" => "PHPMailer", 
+				"url" => "http://phpmailer.sourceforge.net/", 
+				"description" => "Full featured email transfer class for PHP", 
+				"version" => "1.72", 
+				"licence" => "GPL"
+			),
+	);
+
+$text = "<div style='text-align: center; margin-left: auto; margin-right: auto;'><table style='width: 90%;' class='fborder'>
+
+<tr>
+<td class='forumheader'><b>".CRELAN_2."</b></td>
+</tr>\n";
+
+$type = 4;
+foreach($creditsArray as $credits)
+{
+	$text .= "<tr>
+	<td style='forumheader".$type."'><br /><a href='".$credits['url']."' rel='external'>".$credits['name']."</a> ".CRELAN_7." ".$credits['version']."<br /></td>
+	</tr>
+	<tr>
+	<td style='forumheader".$type."'>".$credits['description']."</td>
+	</tr>\n";
+	$type = ($type == 3 ? 4 : 3);
+}
+
+$text .= "</table></div>";
+
+$ns->tablerender(CRELAN_1, $text);
+	
+require_once("footer.php");
+?>
---
+++ e107_languages/English/admin/lan_credits.php
@@ -0,0 +1,22 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_credits.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-03-02 12:56:33 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+define("CRELAN_1", "Credits");
+define("CRELAN_2", "Below is a list of third-party software / resources used in e107. The e107 development team would like to personally thank the developers of the following for allowing us to redistribute their code with e107, and for releasing their software under the GPL licence.");
+define("CRELAN_3", "Resource");
+define("CRELAN_4", "Description");
+define("CRELAN_5", "Website");
+define("CRELAN_6", "Permission");
+define("CRELAN_7", "version");
+
+
+
+?>
