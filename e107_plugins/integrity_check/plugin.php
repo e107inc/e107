0b7d55e00efd244d46dfc48d27293e5cec4be8e8
e107coders <e107coders@localhost>
2005-03-07 08:44:53 +0000

    small change to reduce description
    svn path=/trunk/e107_0.7/; revision=2201

--- e107_plugins/integrity_check/plugin.php
+++ e107_plugins/integrity_check/plugin.php
@@ -11,7 +11,7 @@
 | ©HeX0R 2004
 | http://h3x0r.ath.cx
 | hex0r@h3x0r.ath.cx
-	
+
 | Released under the terms and conditions of the
 | GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
@@ -22,49 +22,49 @@ $eplug_version = "0.03";
 $eplug_author = "HeX0R";
 $eplug_url = "http://h3x0r.ath.cx";
 $eplug_email = "hex0r@h3x0r.ath.cx";
-$eplug_description = "Integrity Check is a plugin that checks checksums of your files, helping to find corrupted files.";
+$eplug_description = "This plugin checks the checksums of your files, helping to find corrupted files.";
 $eplug_compatible = "e107v6";
 $eplug_readme = "";
-	
+
 // Name of the plugin's folder -------------------------------------------------------------------------------------
 $eplug_folder = "integrity_check";
-	
+
 // Name of menu item for plugin ----------------------------------------------------------------------------------
 $eplug_menu_name = "";
-	
+
 // Name of the admin configuration file --------------------------------------------------------------------------
 $eplug_conffile = "admin_integrity_check.php";
-	
+
 // Icon image and caption text ------------------------------------------------------------------------------------
 $eplug_icon = $eplug_folder."/images/integrity_32.png";
 $eplug_icon_small = $eplug_folder."/images/integrity_16.png";
 $eplug_caption = "Check Integrity";
-	
+
 // List of preferences -----------------------------------------------------------------------------------------------
-	
+
 $eplug_prefs = "";
-	
+
 // List of table names -----------------------------------------------------------------------------------------------
 $eplug_table_names = "";
-	
-	
+
+
 // Create a link in main menu (yes=TRUE, no=FALSE) -------------------------------------------------------------
 $eplug_link = FALSE;
 $eplug_link_name = "";
 $eplug_link_url = "";
-	
-	
+
+
 // Text to display after plugin successfully installed ------------------------------------------------------------------
 $eplug_done = "Integrity-Check-Plugin successfully installed!";
-	
+
 // upgrading ... //
-	
+
 $upgrade_add_prefs = "";
-	
+
 $upgrade_remove_prefs = "";
-	
+
 $upgrade_alter_tables = "";
-	
+
 $eplug_upgrade_done = "Integrity-Check-Plugin successfully updated!";
-	
+
 ?>
