677d009afcb7b189fa1c6e85563bb912a223d92b
e107steved <steved@e107.org>
2005-03-08 11:34:20 +0000

    Tidied up template.
    svn path=/trunk/e107_0.7/; revision=2213

--- e107_themes/templates/search_template.php
+++ e107_themes/templates/search_template.php
@@ -7,14 +7,14 @@ if(!isset($SEARCH_MAIN_TABLE)){
 		<form name='searchform' method='post' action='".e_SELF."'>
 		<table style='width:95%' class='fborder'>\n
 			<tr>
-				<td class='forumheader2' style='width:20%'>".LAN_199." </td>
-				<td class='forumheader3' style='width:80%'>
-					{SEARCH_MAIN_SEARCHFIELD} <span class='smalltext'>".LAN_417."</span>
+				<td class='forumheader3' style='width:30%'>".LAN_199."<br /><span class='smalltext'>".LAN_417."</span></td>
+				<td class='forumheader3' style='width:70%'>
+					{SEARCH_MAIN_SEARCHFIELD} 
 				</td>
 			</tr>
 			<tr>
-				<td style='width:20%' class='forumheader2'>".LAN_200."</td>
-				<td style='width:80%' class='forumheader3'>{SEARCH_MAIN_CHECKBOXES}
+				<td style='width:30%' class='forumheader3'>".LAN_200."</td>
+				<td style='width:70%' class='forumheader3'>{SEARCH_MAIN_CHECKBOXES}
 				<br /><br /> 
 				{SEARCH_MAIN_CHECKALL}
 				{SEARCH_MAIN_UNCHECKALL}
@@ -30,6 +30,9 @@ if(!isset($SEARCH_MAIN_TABLE)){
 		</form>
 		</div>\n";
 }
+
+$AFTERCHECKBOXES = "<br />";	/* string thats printed after each category checkbox */
+
 // ##### ------------------------------------------------------------------------------------------
 
 ?>
