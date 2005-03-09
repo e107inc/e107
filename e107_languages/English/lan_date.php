5a2f720e3c7d59ef1d2ca79800a57bb362ff05f9
e107steved <steved@e107.org>
2005-03-09 12:59:25 +0000

    Rewrote computeElpased method.
    svn path=/trunk/e107_0.7/; revision=2247

--- e107_languages/English/lan_date.php
+++ e107_languages/English/lan_date.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_date.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-03-09 10:54:45 $
+|     $Revision: 1.2 $
+|     $Date: 2005-03-09 12:59:25 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -17,7 +17,14 @@ define("LANDT_03", "week");
 define("LANDT_04", "day");
 define("LANDT_05", "hour");
 define("LANDT_06", "minute");
+define("LANDT_07", "second");
+define("LANDT_01s", "years");
+define("LANDT_02s", "months");
+define("LANDT_03s", "weeks");
+define("LANDT_04s", "days");
+define("LANDT_05s", "hours");
+define("LANDT_06s", "minutes");
+define("LANDT_07s", "seconds");
 
-define("LANDT_PLURAL", "s");
 
 ?>
