7fd67c5324dc2e8ea9355a69ade0af8c2cf21d7f
e107steved <steved@e107.org>
2005-03-10 11:13:12 +0000

    Moved hardcoded text into lan file :: submitted by rezso, display update to request.php
    svn path=/trunk/e107_0.7/; revision=2267

--- e107_languages/English/lan_download.php
+++ e107_languages/English/lan_download.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_download.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-03-01 19:09:50 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.5 $
+|     $Date: 2005-03-10 11:12:34 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Downloads");
@@ -78,5 +78,8 @@ define("LAN_dl_60", "Broken download report from");
 
 define("LAN_dl_61", "Download Error");
 define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded you download quota");
+define("LAN_dl_63", "You do not have the correct permissions to download this file.");
+define("LAN_dl_64", "Back");
+define("LAN_dl_65", "Not Found");
 
 ?>
