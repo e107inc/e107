cddec804def29274239586afa33612e1d1bd8094
e107steved <steved@e107.org>
2005-03-10 14:12:12 +0000

    Bugtracker #164
    svn path=/trunk/e107_0.7/; revision=2273

--- e107_languages/English/admin/lan_language.php
+++ e107_languages/English/admin/lan_language.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_language.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-01-08 02:32:59 $
-|     $Author: e107coders $
+|     $Revision: 1.4 $
+|     $Date: 2005-03-10 14:12:12 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 
@@ -16,16 +16,15 @@ define("LANG_LAN_02","couldn't be deleted");
 define("LANG_LAN_03","Tables");
 define("LANG_LAN_04","Options");
 define("LANG_LAN_05","Not Installed");
-
 define("LANG_LAN_06", "Create tables");
 define("LANG_LAN_07", "Drop existing tables?");
 define("LANG_LAN_08", "Replace existing tables (data will be lost).");
-
 define("LANG_LAN_10", "Confirm delete");
 define("LANG_LAN_11", "Delete unchecked tables above (if they exist).");
 define("LANG_LAN_12", "Enable Multi-Language Tables");
 define("LANG_LAN_13", "Multi-Language Preferences");
-
 define("LANG_LAN_14", "Default Site Language");
+define("LANG_LAN_15", "Saving your parameters");
+define("LANG_LAN_16", "You parameters were saved");
 
 ?>
