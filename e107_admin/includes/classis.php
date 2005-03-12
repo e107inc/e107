97b97395aaea0fca51cf7a38359804b5ee4fe837
sweetas <sweetas@localhost>
2005-03-12 10:43:46 +0000

    plugins sorted alphabetically
    svn path=/trunk/e107_0.7/; revision=2306

--- e107_admin/includes/classis.php
+++ e107_admin/includes/classis.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/includes/classis.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-03-08 16:58:38 $
+|     $Revision: 1.6 $
+|     $Date: 2005-03-12 10:43:46 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
@@ -40,12 +40,16 @@ if ($sql->db_Select("plugin", "*", "plugin_installflag=1")) {
 		extract($row);
 		include(e_PLUGIN.$plugin_path."/plugin.php");
 		if ($eplug_conffile) {
-			$plugin_icon = $eplug_icon ? "<img src='".e_PLUGIN.$eplug_icon."' alt='' style='border:0px; width: 32px; height: 32px' />" :
-			 E_32_CAT_PLUG;
-			$text .= render_links(e_PLUGIN.$plugin_path."/".$eplug_conffile, $eplug_name, $eplug_caption, "P".$plugin_id, $plugin_icon, "classis");
+			$plugin_icon = $eplug_icon ? "<img src='".e_PLUGIN.$eplug_icon."' alt='' style='border:0px; width: 32px; height: 32px' />" : E_32_CAT_PLUG;
+			$plugin_array[ucfirst($eplug_name)] = array('link' => e_PLUGIN.$plugin_path."/".$eplug_conffile, 'title' => $eplug_name, 'caption' => $eplug_caption, 'perms' => "P".$plugin_id, 'icon' => $plugin_icon);
 		}
 		unset($eplug_conffile, $eplug_name, $eplug_caption, $eplug_icon);
 	}
+}
+
+ksort($plugin_array, SORT_STRING);
+foreach ($plugin_array as $plug_key => $plug_value) {
+	$text .= render_links($plug_value['link'], $plug_value['title'], $plug_value['caption'], $plug_value['perms'], $plug_value['icon'], 'classis');
 }
 	
 $text .= render_clean();
