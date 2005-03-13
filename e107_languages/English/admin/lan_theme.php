6ff4b048e7a3bc81ecd13ab79de565ba80dd6ae3
e107steved <steved@e107.org>
2005-03-13 12:42:46 +0000

    Language file didn't get uploaded to cvs properly.
    svn path=/trunk/e107_0.7/; revision=2322

--- e107_languages/English/admin/lan_theme.php
+++ e107_languages/English/admin/lan_theme.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_theme.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-03-13 12:33:26 $
+|     $Revision: 1.6 $
+|     $Date: 2005-03-13 12:42:46 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -33,11 +33,11 @@ define("TPVLAN_20", "Auto theme upload and extraction is disabled as your themes
 
 define("TPVLAN_21", "This is the currently selected site theme");
 
-define("TPVLAN_22", "Version");
-define("TPVLAN_23", "Version");
-define("TPVLAN_24", "Version");
-define("TPVLAN_25", "Version");
-define("TPVLAN_26", "Version");
+define("TPVLAN_22", "this theme has multiple stylesheets");
+define("TPVLAN_23", "default stylesheet");
+define("TPVLAN_24", "no information");
+define("TPVLAN_25", "To choose which stylesheet to use, please go to <a href='".e_ADMIN."prefs.php'>preferences</a> and click on 'Theme'.");
+
 
 
 
