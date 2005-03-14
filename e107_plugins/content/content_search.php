31bcf13e849ecfe2c1d5bfcb839b533baee9524d
sweetas <sweetas@localhost>
2005-03-14 15:13:10 +0000

    search update
    svn path=/trunk/e107_0.7/; revision=2332

--- e107_plugins/content/content_search.php
+++ e107_plugins/content/content_search.php
@@ -1,20 +1,41 @@
 <?php
-$plugintable = "pcontent";
-$search_info[$key]['qtype'] = "content";
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/content/content_search.php,v $
+|     $Revision: 1.2 $
+|     $Date: 2005-03-14 15:13:10 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
+*/
 
-if($results = $sql -> db_Select($plugintable, "*", "content_heading REGEXP('".$query."') OR content_subheading REGEXP('".$query."') OR content_summary REGEXP('".$query."') OR content_text REGEXP('".$query."') ")){
-	while($row = $sql -> db_Fetch()){
-	extract($row);
-		$content_heading = parsesearch($content_heading, $query);
-		$content_subheading = parsesearch($content_subheading, $query);
-		$content_summary = parsesearch($content_summary, $query);
-		$content_text = parsesearch($content_text , $query);
+$return_fields = 'content_id, content_heading, content_subheading, content_summary, content_text, content_datestamp, content_parent, content_author';
+$search_fields = array('content_heading', 'content_subheading', 'content_summary', 'content_text');
+$weights = array('1.2', '0.9', '0.6', '0.6');
+$no_results = LAN_198;
+$where = "";
+$order = array('content_datestamp' => DESC);
 
-		$text .= "<img src='".THEME."images/bullet2.gif' alt='bullet' /> <a href='".e_PLUGIN."content/content.php?".$content_id."'>".$content_heading."</a><br />
-		".$content_author."<br />".$content_text."<br />";
-	}
-}else{
-	$text .= "No matches.";
+$ps = $sch -> parsesearch('pcontent', $return_fields, $search_fields, $weights, 'search_content', $no_results, $where, $order);
+$text .= $ps['text'];
+$results = $ps['results'];
+
+function search_content($row) {
+	global $con;
+	$type = explode('.', $row['content_parent']);
+	$res['link'] = e_PLUGIN."content/content.php?type.".$type[0].".content.".$row['content_id'];
+	$res['title'] = $row['content_heading'];
+	$res['summary'] = $row['content_summary'].' '.$row['content_text'];
+	$res['detail'] = LAN_SEARCH_3.$con -> convert_date($row['content_datestamp'], "long");
+	return $res;
 }
 
 ?>
