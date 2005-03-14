cd612f03f38592435f9981fcd5da978c702cdcf9
sweetas <sweetas@localhost>
2005-03-14 09:09:23 +0000

    search update
    svn path=/trunk/e107_0.7/; revision=2325

--- e107_handlers/search/search_download.php
+++ e107_handlers/search/search_download.php
@@ -11,19 +11,18 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/search/search_download.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-03-13 16:16:33 $
+|     $Revision: 1.5 $
+|     $Date: 2005-03-14 09:05:15 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 
-//$return_fields = 'download_id, download_category, download_name, download_description, download_author, download_author_website';
-$return_fields = 'd.download_id, d.download_category, d.download_name, d.download_description, d.download_author, d.download_author_website, c.download_category_name';
+$return_fields = 'd.download_id, d.download_category, d.download_name, d.download_description, d.download_author, d.download_author_website, d.download_datestamp, c.download_category_name';
 $search_fields = array('d.download_name', 'd.download_description', 'd.download_author', 'd.download_author_website');
 $weights = array('1.2', '0.6', '0.6', '0.4');
 $no_results = LAN_198;
-$where = "";
-$order = "";
+$where = "download_active = '1' AND";
+$order = array('download_datestamp' => DESC);
 $table = "download AS d LEFT JOIN #download_category AS c ON d.download_category = c.download_category_id";
 
 $ps = $sch -> parsesearch($table, $return_fields, $search_fields, $weights, 'search_downloads', $no_results, $where, $order);
@@ -38,32 +37,4 @@ function search_downloads($row) {
 	return $res;
 }
 
-/*
-$c = 0;
-if ($results = $sql->db_Select("download", "download_id, download_category, download_name, download_author, download_description, 
-download_author_website", "(download_name REGEXP('".$query."') OR download_author REGEXP('".$query."') OR download_description  
-REGEXP('".$query."') OR download_author_website REGEXP('".$query."')) AND download_active='1'  ")) {
-	while (list($download_id, $download_category, $download_name, $download_author, $download_description, $download_author_website) = $sql->db_Fetch()) {
-		 
-		$download_name = parsesearch($download_name, $query);
-		 
-		$download_author = parsesearch($download_author, $query);
-		 
-		$download_description = parsesearch($download_description , $query);
-		 
-		$download_author_website = parsesearch($download_author_website, $query);
-		 
-		 
-		$action = "download.php?view.".$download_id."";
-		$text .= "<form method='post' action='$action' id='download_".$c."'>
-			<input type='hidden' name='highlight_search' value='1' /><input type='hidden' name='search_query' value='$query' />
-			<img src='".THEME."images/bullet2.gif' alt='bullet' /> <b><a href='javascript:document.getElementById(\"download_".$c."\").submit()'>
-			$download_name</a></b></form><br />$download_description<br /><br />";
-		$c ++;
-		 
-	}
-} else {
-	$text .= LAN_198;
-}
-*/
 ?>
