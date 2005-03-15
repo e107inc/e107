e34821528788d970684e1ce28df850ed699c23e2
e107steved <steved@e107.org>
2005-03-15 14:34:10 +0000

    Userposts can now be shown by IP address.
    svn path=/trunk/e107_0.7/; revision=2354

--- e107_languages/English/lan_userposts.php
+++ e107_languages/English/lan_userposts.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_userposts.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-03-10 11:15:38 $
+|     $Revision: 1.3 $
+|     $Date: 2005-03-15 14:34:10 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -27,5 +27,5 @@ define("UP_LAN_12", "Search");
 define("UP_LAN_13", "Comments");
 define("UP_LAN_14", "Forum Posts");
 define("UP_LAN_15", "Re");
-
+define("UP_LAN_16", "IP Address");
 ?>
