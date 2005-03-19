e639d19e059bc498d46250113d63450ff3bbaddd
mcfly_e107 <mcfly_e107@localhost>
2005-03-19 03:03:01 +0000

    Extended user field enhancements
    svn path=/trunk/e107_0.7/; revision=2395

--- e107_languages/English/lan_user_extended.php
+++ e107_languages/English/lan_user_extended.php
@@ -4,4 +4,5 @@ define("UE_LAN_2", "Radio Buttons");
 define("UE_LAN_3", "Drop-Down Menu");
 define("UE_LAN_4", "DB Table Field");
 define("UE_LAN_5", "Textarea");
+define("UE_LAN_6", "Integer");
 ?>
