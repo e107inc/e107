f518bc7842481e06f5ffb0d1aa279cc213fd4532
e107steved <steved@e107.org>
2005-03-20 18:16:23 +0000

    New theme.
    svn path=/trunk/e107_0.7/; revision=2409

---
+++ e107_themes/sebes/theme.php
@@ -0,0 +1,142 @@
+<?php
+/*
++---------------------------------------------------------------+
+|	e107 website system
+|
+|	©Steve Dunstan 2001-2002
+|	http://e107.org
+|	jalist@e107.org
+|
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+if(!defined("e_THEME")){ exit; }
+// [theme]
+$themename = "sebes";
+$themeversion = "1";
+$themeauthor = "jalist";
+$themedate = "18/03/2005";
+$themeinfo = "Designed specifically for the e107 Doc Project";
+define("STANDARDS_MODE", TRUE);
+// [layout]
+
+$layout = "_default";
+
+$HEADER = "
+<div class='mainbox'>
+
+<div id='banner'>
+{BANNER}
+</div>
+
+<table style='width: 100%;' cellpadding='0' cellspacing='0'>
+<tr>
+<td><div id='logo1'></div></td>
+<td id='heading'></td>
+</tr>
+
+<tr>
+<td id='logo2'>&nbsp;</td>
+<td id='searchbox'>
+{CUSTOM=search+25+".THEME."images/search.png+19+18}
+</td>
+</tr>
+</table>
+
+
+
+<table style='width: 100%;' cellpadding='0' cellspacing='0'>
+<tr>
+<td id='doclinkcolumn'>
+<div class='docinnerbox'>
+{SETSTYLE=menu}
+<div class='center'>[ <a href='".e_BASE."index.php'>{SITENAME}</a> ]</div><br /><br />
+{SITELINKS}
+{MENU=1}
+</div>
+</td>
+<td id='docmaincolumn'>
+<div class='docinnerbox'>
+{SETSTYLE=main}
+";
+
+
+$FOOTER = "
+</div>
+</td>
+</tr>
+</table>
+</div>
+";
+
+
+define('PRELINK', "");
+define('POSTLINK', "");
+define('LINKSTART', "<img src='".THEME."images/arrow.png' alt='' /> ");
+define("LINKSTART_HILITE", "<img src='".THEME."images/selarrow.png' alt='' /> ");
+define('LINKEND', "<br />");
+define('LINKDISPLAY', 2);
+define('LINKALIGN', "left");
+
+
+define("BULLET", "arrow.png");
+
+/*	[newsstyle]	*/
+
+$NEWSSTYLE = "
+<div class='captiontext'><img src='".THEME."images/marrow.png' alt='' /> {NEWSTITLE}</div>
+<div class='smalltext'>{NEWSAUTHOR} on {NEWSDATE} | {NEWSCOMMENTS}{TRACKBACK}
+</div>
+{NEWSBODY}
+{EXTENDED}
+<br /><br />";
+
+define("ICONSTYLE", "");
+define("COMMENTLINK", "Read/Post Comment: ");
+define("COMMENTOFFSTRING", "Comments are turned off for this item");
+define("PRE_EXTENDEDSTRING", "<br /><br />[ ");
+define("EXTENDEDSTRING", "Read the rest ...");
+define("POST_EXTENDEDSTRING", " ]<br />");
+define("TRACKBACKSTRING", "Trackbacks: ");
+define("TRACKBACKBEFORESTRING", " | ");
+
+
+//	[tablestyle]
+
+function tablestyle($caption, $text, $mode)
+{
+	global $style;
+	if($style == "menu")
+	{
+		echo "<div class='spacer'><div class='caption'><img src='".THEME."images/oarrow.png' alt='' /> $caption</div><div class='text'>$text</div></div><br />\n";
+	}
+	else
+	{
+		if($caption)
+		{
+			echo "<div class='spacer'><div class='caption'><img src='".THEME."images/marrow.png' alt='' /> $caption</div><br /></div>$text\n";
+		}
+		else
+		{
+			echo $text."\n";
+		}
+	}
+}
+
+$COMMENTSTYLE = "
+<div class='comment'> 
+<div class='lowlight'><div class='compad'>{USERNAME} {TIMEDATE}</div></div>
+<div class='compad'>{COMMENT}</div>
+</div>
+";
+
+
+$CHATBOXSTYLE = "
+<div class='link2'><div class='linktext'><hr /><img src='".THEME."images/arrow.png' alt='' style='vertical-align: middle;' /> {USERNAME} | <span class='cbdate'>{TIMEDATE}</span></div></div>
+<div class='smalltext'>
+{MESSAGE}
+</div>";
+
+
+?>
