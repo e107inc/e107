3e863c4bd49619ff57f965d13ba6b0f4b15f0059
e107steved <steved@e107.org>
2005-03-20 18:43:30 +0000

    Added multilan,
    svn path=/trunk/e107_0.7/; revision=2410

---
+++ e107_plugins/forum/languages/English/lan_forum_stats.php
@@ -0,0 +1,38 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/languages/English/lan_forum_stats.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-03-20 18:43:30 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+define("e_PAGETITLE", "Forum Statistics");
+
+define("FSLAN_1", "General");
+define("FSLAN_2", "Forum opened");
+define("FSLAN_3", "Open for");
+define("FSLAN_4", "Total posts");
+define("FSLAN_5", "Forum topics");
+define("FSLAN_6", "Forum replies");
+define("FSLAN_7", "Forum thread views");
+define("FSLAN_8", "Database size (forum tables only)");
+define("FSLAN_9", "Average row length in forum table");
+define("FSLAN_10", "Most active topics");
+define("FSLAN_11", "Rank");
+define("FSLAN_12", "Topic");
+define("FSLAN_13", "Replies");
+define("FSLAN_14", "Started by");
+define("FSLAN_15", "Date");
+define("FSLAN_16", "Most viewed topics");
+define("FSLAN_17", "Views");
+define("FSLAN_18", "Top posters");
+define("FSLAN_19", "Name");
+define("FSLAN_20", "Posts");
+define("FSLAN_21", "Top topic starters");
+define("FSLAN_22", "Top repliers");
+define("FSLAN_23", "Forum Statistics");
+
+?>
