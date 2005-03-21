1ffe36d2a29b7d040fac655b6756884fb93e027a
e107steved <steved@e107.org>
2005-03-21 11:49:34 +0000

    Theme tidy.
    svn path=/trunk/e107_0.7/; revision=2430

--- e107_themes/kubrick/languages/English.php
+++ e107_themes/kubrick/languages/English.php
@@ -10,19 +10,18 @@
 |     Released under the terms and conditions of the
 |     GNU General Public License (http://gnu.org).
 |
-|     $Source: /cvs_backup/e107_0.7/e107_themes/crahan/languages/English.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-03-21 11:49:22 $
+|     $Source: /cvs_backup/e107_0.7/e107_themes/kubrick/languages/English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-03-21 11:48:34 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_THEME_1", "'CraHan' by <a href='http://e107.org' rel='external'>jalist</a>, based on the theme by CraHan at his homepage <a href='http://n00.be' rel='external'>n00.be</a>");
+define("LAN_THEME_1", "'kubrick' by <a href='http://e107.org' rel='external'>jalist</a>, Based on original theme by Michael Heilemann (<a href='http://binarybonsai.com/kubrick/' rel='external'>http://binarybonsai.com/kubrick/</a>).");
 define("LAN_THEME_2", "Comments are turned off for this item");
-define("LAN_THEME_3", "comment(s): ");
+define("LAN_THEME_3", "comment: ");
 define("LAN_THEME_4", "Read the rest ...");
 define("LAN_THEME_5", "Trackbacks: ");
-define("LAN_THEME_6", "Comment by");
 
 
 ?>
