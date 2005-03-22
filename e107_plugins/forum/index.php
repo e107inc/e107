25fb5d19cce80eea5fc9bcd148d32368e91e6e0d
e107steved <steved@e107.org>
2005-03-22 23:14:28 +0000

    Was asked to add a redirect file, some people wanted to call the forum without the file, like forum/ instead of forum/forum.php.
    svn path=/trunk/e107_0.7/; revision=2462

---
+++ e107_plugins/forum/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/index.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-03-22 23:14:28 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+
+header("location: forum.php");
+exit;
+
+?>
