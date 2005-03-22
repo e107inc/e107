e422c18fbefe0ce5b6efb1cb3f8c62e84b407450
e107steved <steved@e107.org>
2005-03-22 20:58:15 +0000

    *** empty log message ***
    svn path=/trunk/e107_0.7/; revision=2461

--- e107_plugins/compliance_menu/languages/English.php
+++ e107_plugins/compliance_menu/languages/English.php
@@ -4,12 +4,12 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/compliance_menu/languages/English.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-01-27 19:52:47 $
-|     $Author: streaky $
+|     $Revision: 1.3 $
+|     $Date: 2005-03-22 20:58:14 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 	
 define("COMPLIANCE_L1", "W3C Compliance");
-	
+
 ?>
--- e107_plugins/compliance_menu/languages/French.php
+++ e107_plugins/compliance_menu/languages/French.php
@@ -4,12 +4,12 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/compliance_menu/languages/French.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-02-26 10:36:19 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-03-22 20:58:15 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 
-define("COMPLIANCE_L1", "Compatibilit&eacute; W3C");	
+define("COMPLIANCE_L1", "Compatibilit&eacute; W3C");
 
 ?>
