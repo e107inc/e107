9f4e1a998374ec888ae1fa4455ee3954f18f62e9
e107steved <steved@e107.org>
2005-03-30 15:13:34 +0000

    Email / print class now has definable images.
    svn path=/trunk/e107_0.7/; revision=2592

--- e107_handlers/emailprint_class.php
+++ e107_handlers/emailprint_class.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/emailprint_class.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-03-24 10:33:41 $
+|     $Revision: 1.6 $
+|     $Date: 2005-03-30 15:11:13 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -24,59 +24,31 @@ class emailprint {
 		// $look = 2  --->display print icon only
 		$text_emailprint = "";
 
-		
-		/*
-		//because of new method, this old method for emailprint_class is ignored
-		switch($mode) {
-			case "article":
-			$email = "article";
-			$print = "article";
-			break;
-			case "news":
-			$email = "news";
-			$print = "news";
-			break;
-			case "content":
-			$email = "content";
-			$print = "content";
-			break;
-		}
-		*/
-
 		//new method emailprint_class : (only news is core, rest is plugin: searched for e_emailprint.php which should hold $email and $print values)
 		if($mode == "news"){
-				$email = "news";
-				$print = "news";
+			$email = "news";
+			$print = "news";
 		}else{
-				//load the others from plugins
-				$handle = opendir(e_PLUGIN);
-				while (false !== ($file = readdir($handle))) {
-					if ($file != "." && $file != ".." && is_dir(e_PLUGIN.$file)) {
-						$plugin_handle = opendir(e_PLUGIN.$file."/");
-						while (false !== ($file2 = readdir($plugin_handle))) {
-							if ($file2 == "e_emailprint.php") {
-								require_once(e_PLUGIN.$file."/".$file2);
-							}
+			//load the others from plugins
+			$handle = opendir(e_PLUGIN);
+			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != ".." && is_dir(e_PLUGIN.$file)) {
+					$plugin_handle = opendir(e_PLUGIN.$file."/");
+					while (false !== ($file2 = readdir($plugin_handle))) {
+						if ($file2 == "e_emailprint.php") {
+							require_once(e_PLUGIN.$file."/".$file2);
 						}
 					}
 				}
+			}
 		}
 
-
 		if ($look == 0 || $look == 1) {
-			if (defined("ICONMAIL") && file_exists(THEME."images/".ICONMAIL)) {
-				$ico_mail = THEME."images/".ICONMAIL;
-			} else {
-				$ico_mail = e_IMAGE."generic/friend.gif";
-			}
+			$ico_mail = (file_exists(THEME."generic/email.png") ? THEME."generic/email.png" : e_IMAGE."generic/".IMODE."/email.png");
 			$text_emailprint .= "<a href='".e_BASE."email.php?".$email.".".$id."'><img src='".$ico_mail."' style='border:0' alt='email to someone' title='email to someone' /></a> ";
 		}
 		if ($look == 0 || $look == 2) {
-			if (defined("ICONPRINT") && file_exists(THEME."images/".ICONPRINT)) {
-				$ico_print = THEME."images/".ICONPRINT;
-			} else {
-				$ico_print = e_IMAGE."generic/printer_".IMODE.".png";
-			}
+			$ico_print = (file_exists(THEME."generic/printer.png") ? THEME."generic/printer.png" : e_IMAGE."generic/".IMODE."/printer.png");
 			$text_emailprint .= "<a href='".e_BASE."print.php?".$print.".".$id."'><img src='".$ico_print."' style='border:0' alt='printer friendly' title='printer friendly'/></a>";
 		}
 		return $text_emailprint;
