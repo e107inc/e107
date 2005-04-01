8e40d7df7a9e624bca4f74a13354ceff653824f9
e107coders <e107coders@localhost>
2005-04-01 15:42:27 +0000

    fixed icons that had changed
    svn path=/trunk/e107_0.7/; revision=2656

--- e107_admin/includes/beginner.php
+++ e107_admin/includes/beginner.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/includes/beginner.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-03-27 17:10:25 $
+|     $Revision: 1.2 $
+|     $Date: 2005-04-01 15:42:27 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -22,8 +22,8 @@ $text = "<div style='text-align:center;vertical-align:middle'><br /><br />
 
 	$newarray[27] = array(e_ADMIN."plugin.php", ADLAN_98, ADLAN_99, "Z", 2, E_16_PLUGMANAGER, E_32_PLUGMANAGER);
 
-	$selection = array(19,10,15,22,5,17,7,25,27,23);
-
+  //	$selection = array(19,10,15,22,5,17,7,25,27,23);
+    	$selection = array(20,11,16,23,5,18,7,26,27,23);    
 	foreach($selection as $id){
 	$text .= render_links($newarray[$id][0],$newarray[$id][1],$newarray[$id][2],$newarray[$id][3],$newarray[$id][6],'beginner');
 	}
