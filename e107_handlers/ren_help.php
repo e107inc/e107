6aff82badec4eb85cd20b13fa6e93d37fda37091
e107steved <steved@e107.org>
2005-04-03 17:16:12 +0000

    Admin area dialogs use new colour and size selectors.
    svn path=/trunk/e107_0.7/; revision=2692

--- e107_handlers/ren_help.php
+++ e107_handlers/ren_help.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/ren_help.php,v $
-|     $Revision: 1.22 $
-|     $Date: 2005-04-03 16:42:29 $
-|     $Author: sweetas $
+|     $Revision: 1.23 $
+|     $Date: 2005-04-03 17:16:12 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 @include(e_LANGUAGEDIR.e_LANGUAGE."/lan_ren_help.php");
@@ -94,28 +94,8 @@ function display_help($tagid="helpb", $mode = 1, $addtextfunc = "addtext", $help
 	$code[9] = array("bq", "[blockquote][/blockquote]", LANHELP_31);
 	$code[10] = array("code", "[code][/code]", LANHELP_32 );
 	$code[11] = array("list", "[list][/list]", LANHELP_36);
-
-	$colours[0] = array("black", LANHELP_1);
-	$colours[1] = array("blue", LANHELP_2);
-	$colours[2] = array("brown", LANHELP_3);
-	$colours[3] = array("cyan", LANHELP_4);
-	$colours[4] = array("darkblue", LANHELP_5);
-	$colours[5] = array("darkred", LANHELP_6);
-	$colours[6] = array("green", LANHELP_7);
-	$colours[7] = array("indigo", LANHELP_8);
-	$colours[8] = array("olive", LANHELP_9);
-	$colours[9] = array("orange", LANHELP_10);
-	$colours[10] = array("red", LANHELP_11);
-	$colours[11] = array("violet", LANHELP_12);
-	$colours[12] = array("white", LANHELP_13);
-	$colours[13] = array("yellow", LANHELP_14);
-
-	$fontsizes[0] = array("7", LANHELP_15);
-	$fontsizes[1] = array("9", LANHELP_16);
-	$fontsizes[2] = array("11", LANHELP_17);
-	$fontsizes[3] = array("16", LANHELP_18);
-	$fontsizes[4] = array("20", LANHELP_19);
-	$fontsizes[5] = array("28", LANHELP_20);
+	$code[12] = array("fontcol", "[color][/color]", LANHELP_21);
+	$code[13] = array("fontsize", "[size][/size]", LANHELP_22);
 
 	$img[1] = "link.png";
 	$img[2] = "bold.png";
@@ -128,13 +108,21 @@ function display_help($tagid="helpb", $mode = 1, $addtextfunc = "addtext", $help
 	$img[9] = "blockquote.png";
 	$img[10] = "code.png";
 	$img[11] = "list.png";
+	$img[12] = "fontcol.png";
+	$img[13] = "fontsize.png";
 
 	$imgpath = (file_exists(THEME."bbcode/bold.png") ? THEME."bbcode/" : e_IMAGE."generic/bbcode/");
 
-	while (list($key, $bbcode) = each($code)) {
-		//$string .= "<input class=\"button\" type=\"button\" value=\"".$bbcode[0]."\" onclick=\"{$addtextfunc}('".$bbcode[1]."')\" ".($mode != 2 ? "onmouseout=\"{$helpfunc}('','{$tagid}')\" onmouseover=\"{$helpfunc}('".$bbcode[2]."','{$tagid}')\"" : "" ).($bbcode[3] ? " style='".$bbcode[3]."'" :"")." />\n";
-		$string .= "<img src='".$imgpath.$img[$key]."' alt='' title='".$bbcode[2]."' onclick=\"{$addtextfunc}('".$bbcode[1]."')\" ".($mode != 2 ? "onmouseout=\"{$helpfunc}('')\" onmouseover=\"{$helpfunc}('".$bbcode[2]."')\"" : "" )." />";
+	foreach($code as $key=>$bbcode){
+		if($key == 12){
+			$text .= "<img class='bbcode' src='".$imgpath.$img[$key]."' alt='' title='".$bbcode[2]."' onclick=\"expandit('col_selector')\" ".($mode != 2 ? "onmouseout=\"{$helpfunc}('')\" onmouseover=\"{$helpfunc}('".$bbcode[2]."')\"" : "" )." />\n";
+		}else if($key == 13){
+			$text .= "<img class='bbcode' src='".$imgpath.$img[$key]."' alt='' title='".$bbcode[2]."' onclick=\"expandit('size_selector')\" ".($mode != 2 ? "onmouseout=\"{$helpfunc}('')\" onmouseover=\"{$helpfunc}('".$bbcode[2]."')\"" : "" )." />\n";
+		}else{
+		  $text .= "<img class='bbcode' src='".$imgpath.$img[$key]."' alt='' title='".$bbcode[2]."' onclick=\"{$addtextfunc}('".$bbcode[1]."')\" ".($mode != 2 ? "onmouseout=\"{$helpfunc}('')\" onmouseover=\"{$helpfunc}('".$bbcode[2]."')\"" : "" )." />\n";
+		}
 	}
+
 
 /*
 	if(file_exists(e_PLUGIN."sphpell/spellcheckpageinc.php"))
@@ -144,21 +132,11 @@ function display_help($tagid="helpb", $mode = 1, $addtextfunc = "addtext", $help
 	}
 */
 	if ($mode) {
-		$string .= "<br />\n<select class=\"tbox\" name=\"fontcol\" onchange=\"{$addtextfunc}('[color=' + this.options[this.selectedIndex].value + '][/color]');this.selectedIndex=0;\"".($mode != 2 ? " onmouseover=\"{$helpfunc}('Font Color: [color]Blue[/color]','{$tagid}')\" onmouseout=\"{$helpfunc}('','{$tagid}')\"" : "")." >\n<option value=\"\">".LANHELP_21."</option>\n";
-		while (list($key, $bbcode) = each($colours)) {
-			$string .= "<option style=\"color:".strtolower($bbcode[0])."\" value=\"".strtolower($bbcode[0])."\">".$bbcode[1]."</option>\n";
-		}
-		$string .= "</select>\n
-
-		<select class=\"tbox\" name=\"fontsiz\" onchange=\"{$addtextfunc}('[size=' + this.options[this.selectedIndex].value + '][/size]');this.selectedIndex=0;\"".($mode != 2 ? " onmouseover=\"{$helpfunc}('Font Size: [size]Big[/size]','{$tagid}')\" onmouseout=\"{$helpfunc}('','{$tagid}')\"" : "" )." >\n<option>".LANHELP_22."</option>\n";
-
-		while (list($key, $bbcode) = each($fontsizes)) {
-			$string .= "<option value=\"".$bbcode[0]."\">".$bbcode[1]."</option>\n";
-		}
-		$string .= "</select>";
+		$text .= Size_Select();
+		$text .= Color_Select('color_'.$key);
 	}
 
-	return $string;
+	return $text;
 }
 
 function Color_Select() {
