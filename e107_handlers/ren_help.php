d43e413b55088700b7e95a60b0aab3db3927152c
sweetas <sweetas@localhost>
2005-04-03 16:42:29 +0000

    colour selector cleanup
    svn path=/trunk/e107_0.7/; revision=2691

--- e107_handlers/ren_help.php
+++ e107_handlers/ren_help.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/ren_help.php,v $
-|     $Revision: 1.21 $
-|     $Date: 2005-04-01 12:24:37 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.22 $
+|     $Date: 2005-04-03 16:42:29 $
+|     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 @include(e_LANGUAGEDIR.e_LANGUAGE."/lan_ren_help.php");
@@ -161,29 +161,13 @@ function display_help($tagid="helpb", $mode = 1, $addtextfunc = "addtext", $help
 	return $string;
 }
 
-function Color_Select($field, $embed = FALSE){
-	if (!$embed) {
-		$text = "<!-- Start of Color selector -->
-		<div style='margin-left: 0px; margin-right: 0px; width: 221px; position: relative; z-index: 1000; float: right; display: none' id='col_selector' onclick=\"this.style.display='none'\">
-		<div style='position: absolute; bottom: 30px; right: 145px; width: 221px'>";
-		
-		$render_td = "var tdtop = '<td style=\'background-color: #';
-		var tdmid = '; height: 10px; width: 10px;\' ';
-		var tdmid2 = ' onclick=\"addtext(\'[color=#';
-		var tdbot = '][/color]\')\"></td>';";
-	} else {
-		$text ="<table border='0' cellspacing='0' cellpadding='0' style='width: 100%; border: 0px; padding: 4px;'>
-		<tr>
-		<td colspan='3'>
-		<div id='gxhzct_".$field."' onclick=\"window.close();\">";
+function Color_Select() {
+	$text = "<!-- Start of Color selector -->
+	<div style='margin-left: 0px; margin-right: 0px; width: 221px; position: relative; z-index: 1000; float: right; display: none' id='col_selector' onclick=\"this.style.display='none'\">
+	<div style='position: absolute; bottom: 30px; right: 145px; width: 221px'>";
 
-		$render_td = "var tdtop = '<td style=\'background-color: #';
-		var tdmid = '; height: 10px; width: 10px;\' onmouseover=\"View(\'".$field."\',\'';
-		var tdmid2 = '\')\" onclick=\"Set(\'".$field."\',\'';
-		var tdbot = '\')\"></td>';";
-	}
-
-	$text .= "<script>
+	$text .= "<script type='text/javascript'>
+	//<![CDATA[
 	var maxtd = 18; 
 	var maxtddiv = -1; 
 	var coloursrgb = new Array('00', '33', '66', '99', 'cc', 'ff'); 
@@ -194,10 +178,13 @@ function Color_Select($field, $embed = FALSE){
 	var rows1 = '';
 	var rows2 = '';
 	var notr = 0;
-	".$render_td."
 	var tdblk = '<td style=\'background-color: #000000; cursor: default; height: 10px; width: 10px;\'></td>';
 	var g = 1;
 	var s = 0;
+	
+	function td_render(color) {
+		return '<td style=\'background-color: #' + color + '; height: 10px; width: 10px;\' onclick=\"addtext(\'[color=#' + color + '][/color]\')\"></td>';
+	}
 
 	for (i=0; i < coloursrgb.length; i++) { 
 		for (j=0; j < coloursrgb.length; j++) { 
@@ -206,19 +193,19 @@ function Color_Select($field, $embed = FALSE){
 				if (maxtddiv % maxtd == 0) { 
 					if (rowswitch) {
 						if (notr < 5){
-							rows1 += '</tr><tr>'+tdtop+coloursgrey[g]+tdmid+coloursgrey[g]+tdmid2+coloursgrey[g]+tdbot+tdblk;
+							rows1 += '</tr><tr>' + td_render(coloursgrey[g]) + tdblk;
 							g++;
 						}
 						rowswitch = 0;
 						notr++;
 					}else{
-						rows2 += '</tr><tr>'+tdtop+colourssol[s]+tdmid+colourssol[s]+tdmid2+colourssol[s]+tdbot+tdblk;
+						rows2 += '</tr><tr>' + td_render(colourssol[s]) + tdblk;
 						s++;
 						rowswitch = 1;
 					}
 					maxtddiv = 0; 
 				}
-				rowline = tdtop+coloursrgb[j]+coloursrgb[k]+coloursrgb[i]+tdmid+coloursrgb[j]+coloursrgb[k]+coloursrgb[i]+tdmid2+coloursrgb[j]+coloursrgb[k]+coloursrgb[i]+tdbot;
+				rowline = td_render(coloursrgb[j] + coloursrgb[k] + coloursrgb[i]);
 				if (rowswitch) {
 					rows1 += rowline;
 				}else{
@@ -227,19 +214,15 @@ function Color_Select($field, $embed = FALSE){
 			}
 		}
 	}
-	document.write('<table border=\'0\' cellspacing=\'1\' cellpadding=\'0\' style=\'cursor: hand; cursor: pointer; background-color: #000000; width: 100%; border: 0px\'><tr>'+tdtop+coloursgrey[0]+tdmid+coloursgrey[0]+tdmid2+coloursgrey[0]+tdbot+tdblk+rows1+rows2+'</tr></table>');
+	document.write('<table cellspacing=\'1\' cellpadding=\'0\' style=\'cursor: hand; cursor: pointer; background-color: #000; width: 100%; border: 0px\'><tr>');
+	document.write(td_render(coloursgrey[0]) + tdblk + rows1 + rows2);
+	document.write('</tr></table>');
+	//]]>
 	</script>";
-	
-	if (!$embed) {
-		$text .="</div>
-		</div>
-		<!-- End of Color selector -->";
-	} else {
-		$text .="</div>
-		</td>
-		</tr>
-		</table>";
-	}
+
+	$text .="</div>
+	</div>
+	<!-- End of Color selector -->";
 
 	return $text;
 }
