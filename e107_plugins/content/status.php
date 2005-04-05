1b584a4f674d632eaf5390b9e2f52295b48fd5ac
e107coders <e107coders@localhost>
2005-04-05 06:44:03 +0000

    xhtml fixes
    svn path=/trunk/e107_0.7/; revision=2713

--- e107_plugins/content/status.php
+++ e107_plugins/content/status.php
@@ -4,7 +4,7 @@ $maincat = $sql -> db_Select("pcontent", "content_id, content_heading", "content
 while($row = $sql -> db_Fetch()){
 	extract($row);
 		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,".strlen($content_id).") = '".$content_id."' AND content_refer != 'sa' ");
-		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' /> ".$content_heading.": ".$count."</div>";
+		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ".$content_heading.": ".$count."</div>";
 }
 
 ?>
