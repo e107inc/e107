ee45cc0e51f7fe36fa0e3847d3826ee1c521b14d
e107coders <e107coders@localhost>
2005-04-05 06:44:03 +0000

    xhtml fixes
    svn path=/trunk/e107_0.7/; revision=2713

--- e107_plugins/content/latest.php
+++ e107_plugins/content/latest.php
@@ -3,7 +3,7 @@ $plugintable = "pcontent";
 $reported_content = $sql -> db_Count($plugintable, '(*)', "WHERE content_refer='sa' ");
 $text .= "
 <div style='padding-bottom: 2px;'>
-<img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' />
+<img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' />
 ";
 if($reported_content) {
 	$text .= " <a href='".e_PLUGIN."content/admin_content_config.php?type.0.sa'>Submitted Content Items: $reported_content</a>";
--- e107_plugins/forum/latest.php
+++ e107_plugins/forum/latest.php
@@ -1,7 +1,7 @@
 <?php
 $reported_posts = $sql->db_Count('generic', '(*)', "WHERE gen_type='reported_post'");
 $text .= "<div style='padding-bottom: 2px;'>
-<img src='".e_PLUGIN."forum/images/forums_16.png' style='width: 16px; height: 16px; vertical-align: bottom' /> ";
+<img src='".e_PLUGIN."forum/images/forums_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ";
 
 if ($reported_posts) {
 	$text .= "<a href='".e_PLUGIN."forum/forum_admin.php'>Reported forum posts: ".$reported_posts."</a>";
