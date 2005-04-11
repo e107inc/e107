548efabcaaa148ee05ed794a1a0fca264c2514a7
mcfly_e107 <mcfly_e107@localhost>
2005-04-11 18:28:57 +0000

    db verify fix
    svn path=/trunk/e107_0.7/; revision=2820

--- e107_plugins/linkwords/linkwords_sql.php
+++ e107_plugins/linkwords/linkwords_sql.php
@@ -1,7 +1,7 @@
 CREATE TABLE linkwords (
-	linkword_id INT UNSIGNED NOT NULL auto_increment,
-	linkword_active tinyint(1) unsigned NOT NULL default '0',
-	linkword_word varchar(100) NOT NULL default '',
-	linkword_link varchar(150) NOT NULL default '',
-	PRIMARY KEY ( linkword_id )
-	) TYPE=MyISAM AUTO_INCREMENT=1;
+linkword_id INT(10) UNSIGNED NOT NULL auto_increment,
+linkword_active tinyint(1) unsigned NOT NULL default '0',
+linkword_word varchar(100) NOT NULL default '',
+linkword_link varchar(150) NOT NULL default '',
+PRIMARY KEY  (linkword_id)
+) TYPE=MyISAM AUTO_INCREMENT=1;
