cc31f30997ce468592930f8684b94dd28e95614f
mcfly_e107 <mcfly_e107@localhost>
2005-04-12 02:27:06 +0000

    Added header info
    svn path=/trunk/e107_0.7/; revision=2838

--- e107_plugins/forum/forum_class.php
+++ e107_plugins/forum/forum_class.php
@@ -1,11 +1,22 @@
 <?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/forum_class.php,v $
+|     $Revision: 1.18 $
+|     $Date: 2005-04-12 02:27:06 $
+|     $Author: mcfly_e107 $
++----------------------------------------------------------------------------+
+*/
 class e107forum {
-	 
-	function showvar($var) {
-		echo "<pre>";
-		print_r($var);
-		echo "</pre>";
-	}
 	 
 	function thread_postnum($thread_id)
 	{
