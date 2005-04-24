01638c47086dbab30146ddd27f3ecd566e5ccd6f
e107coders <e107coders@localhost>
2005-04-24 10:58:06 +0000

    languages for tinymce
    svn path=/trunk/e107_0.7/; revision=2921

---
+++ e107_handlers/tiny_mce/langs/fa.js
@@ -0,0 +1,43 @@
+﻿// IR lang variables
+// Persian (Farsi) language pack (for IRAN)
+// By: Morteza Zafari
+// Lost@LostLord.com
+// http://www.LostLord.com
+
+tinyMCELang['lang_dir'] = 'rtl';
+tinyMCELang['lang_bold_desc'] = 'ضخيم';
+tinyMCELang['lang_italic_desc'] = 'کجکي';
+tinyMCELang['lang_underline_desc'] = 'زير خط دار';
+tinyMCELang['lang_striketrough_desc'] = 'خط خورده';
+tinyMCELang['lang_justifyleft_desc'] = 'چپ چين';
+tinyMCELang['lang_justifycenter_desc'] = 'وسط چين';
+tinyMCELang['lang_justifyright_desc'] = 'راست چين';
+tinyMCELang['lang_justifyfull_desc'] = 'کشيده چين';
+tinyMCELang['lang_bullist_desc'] = 'ليست';
+tinyMCELang['lang_numlist_desc'] = 'ليست عددي';
+tinyMCELang['lang_outdent_desc'] = 'کاهش زبانه';
+tinyMCELang['lang_indent_desc'] = 'افزايش زبانه';
+tinyMCELang['lang_undo_desc'] = 'برگردونک';
+tinyMCELang['lang_redo_desc'] = 'سرگردونک';
+tinyMCELang['lang_link_desc'] = 'درج و ويرايش لينک';
+tinyMCELang['lang_unlink_desc'] = 'حذف لينک';
+tinyMCELang['lang_image_desc'] = 'درج و ويرايش عکس';
+tinyMCELang['lang_cleanup_desc'] = 'پاکسازي کد';
+tinyMCELang['lang_focus_alert'] = 'A editor instance must be focused before using this command.';
+tinyMCELang['lang_edit_confirm'] = 'Do you want to use the WYSIWYG mode for this textarea?';
+tinyMCELang['lang_insert_link_title'] = 'درج و ويرايش لينک';
+tinyMCELang['lang_insert'] = '  تاييد  ';
+tinyMCELang['lang_update'] = '  تاييد  ';
+tinyMCELang['lang_cancel'] = ' انصراف ';
+tinyMCELang['lang_insert_link_url'] = 'URL لينک';
+tinyMCELang['lang_insert_link_target'] = 'مقصد';
+tinyMCELang['lang_insert_link_target_same'] = 'لينک را در همان صفحه باز کن';
+tinyMCELang['lang_insert_link_target_blank'] = 'لينک را در صفحه جديد باز کن';
+tinyMCELang['lang_insert_image_title'] = 'درج و ويرايش عکس';
+tinyMCELang['lang_insert_image_src'] = 'URL عکس';
+tinyMCELang['lang_insert_image_alt'] = 'توضيح';
+tinyMCELang['lang_help_desc'] = 'راهنما';
+tinyMCELang['lang_bold_img'] = "bold.gif";
+tinyMCELang['lang_italic_img'] = "italic.gif";
+tinyMCELang['lang_underline_img'] = "underline.gif";
+tinyMCELang['lang_clipboard_msg'] = 'Copy/Cut/Paste is not available in Mozilla and Firefox.\nDo you want more information about this issue?';
