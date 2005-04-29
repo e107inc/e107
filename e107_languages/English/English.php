f2f84fee2162f909e309c0ba92018d491a957712
e107coders <e107coders@localhost>
2005-04-29 07:43:11 +0000

    utf-8 character set enabled for multi-language.
    svn path=/trunk/e107_0.7/; revision=2958

--- e107_languages/English/English.php
+++ e107_languages/English/English.php
@@ -4,15 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/English.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2004-09-21 19:11:04 $
+|     $Revision: 1.2 $
+|     $Date: 2005-04-29 07:43:11 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 setlocale(LC_ALL, 'en');
 define("CORE_LC", 'en');
 define("CORE_LC2", 'gb');
-define("CHARSET", "iso-8859-1");
+define("CHARSET", "utf-8");  // for a true multi-language site. :)
 define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 
 //v.616
