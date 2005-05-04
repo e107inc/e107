96a4c5a6b057e8780947ac74f0faa28c5c60957e
e107steved <steved@e107.org>
2005-05-04 14:04:52 +0000

    Initial commit of custom pages rewrite, not usable yet, just committing for safe keeping :)
    svn path=/trunk/e107_0.7/; revision=3063

---
+++ e107_admin/cpage.php
@@ -0,0 +1,310 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_admin/cpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-05-04 14:04:50 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+
+require_once("../class2.php");
+if (!getperms("2")){
+	header("location:".e_BASE."index.php");
+	exit;
+}
+
+$e_sub_cat = 'custom';
+$e_wysiwyg = "data";
+
+$page = new page;
+
+require_once("auth.php");
+require_once(e_HANDLER."ren_help.php");
+require_once(e_HANDLER."userclass_class.php");
+$custpage_lang = ($sql->mySQLlanguage) ? $sql->mySQLlanguage : $pref['sitelanguage'];
+unset($message);
+
+if (e_QUERY)
+{
+	$tmp = explode(".", e_QUERY);
+	$action = $tmp[0];
+	$sub_action = $tmp[1];
+	$id = $tmp[2];
+	$from = ($tmp[3] ? $tmp[3] : 0);
+	unset($tmp);
+}
+
+if(IsSet($_POST['submitPage']))
+{
+	$page -> submitPage();
+}
+
+if(IsSet($_POST['updatePage']))
+{
+	$page -> submitPage($_POST['pe_id']);
+}
+
+$delm = array_pop(array_flip($_POST));
+if (preg_match("#delete_(.*?)_#", $delm, $match))
+{
+	$page -> delete_page($match[1]);
+}
+
+if($page -> $message)
+{
+	$ns->tablerender("", "<div style='text-align:center'><b>".$message."</b></div>");
+}
+
+
+
+
+if(!e_QUERY)
+{
+	$page -> showExistingPages();
+}
+else
+{
+	$function = $action."Page";
+	$page -> $function();
+}
+
+
+
+
+
+
+require_once(e_ADMIN."footer.php");
+
+
+
+class page
+{
+
+	var $message;
+
+	function showExistingPages()
+	{
+		global $sql, $tp, $ns;
+
+		$text = "<div style='text-align: center;'>
+		";
+
+		if(!$sql -> db_Select("page", "*", "ORDER BY page_datestamp DESC", "nowhere"))
+		{
+			$text .= "No pages defined yet";
+		}
+		else
+		{
+			$pages = $sql -> db_getList();
+			$text .= "<form action='".e_SELF."' id='newsform' method='post'>
+			<table style='".ADMIN_WIDTH."' class='fborder'>
+			<tr>
+			<td style='width:5%; text-align: center;' class='forumheader'>ID</td>
+			<td style='width:70%' class='forumheader3'>Title</td>
+			<td style='width:25%; text-align: center;' class='forumheader3'>Options</td>
+			</tr>
+			";
+
+			foreach($pages as $pge)
+			{
+				extract($pge);
+				$text .= "
+				<tr>
+				<td style='width:5%; text-align: center;' class='forumheader'>$page_id</td>
+				<td style='width:70%' class='forumheader3'><a href='".e_BASE."page.php?$page_id'>$page_title</a></td>
+				<td style='width:25%; text-align: center;' class='forumheader3'>
+				<a href='".e_SELF."?create.edit.{$page_id}'>".ADMIN_EDIT_ICON."</a>
+				<input type='image' title='".LAN_DELETE."' name='delete_$page_id' src='".ADMIN_DELETE_ICON_PATH."' onclick=\"return jsconfirm('Delete this page? [ ID: $page_id ]')\"/>
+				</td>
+				</tr>
+				";
+			}
+		
+			$text .= "</form>
+			</table>
+			";
+		}
+
+		$text .= "</div>
+		";
+
+		$ns -> tablerender("Existing Pages", $text);
+
+	}
+
+
+	function createPage()
+	{
+		
+		global $sql, $tp, $ns, $pref, $sub_action, $id;
+
+		if ($sub_action == "edit" && !$_POST['preview'] && !$_POST['submit'])
+		{
+			if ($sql->db_Select("page", "*", "page_id='$id' "))
+			{
+				$row = $sql->db_Fetch();
+				extract($row);
+				$page_title = $tp -> toFORM($page_text);
+				$data = $tp -> toFORM($page_text);
+				$edit = TRUE;
+			}
+		}
+
+		require_once(e_HANDLER."theme_handler.php");
+		$theme = new themeHandler;
+		$themelist = $theme -> themeArray;
+
+		$text = "<div style='text-align:center'>
+		<form method='post' action='".e_SELF."' id='dataform'>
+		<table style='".ADMIN_WIDTH."' class='fborder'>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Title / Caption</td>
+		<td style='width:70%' class='forumheader3'><input class='tbox' type='text' name='page_title' size='50' value='".$page_title."' maxlength='250' /></td>
+		</tr>
+		
+		</td>
+		</tr>
+		
+		<tr>
+		<td style='width:30%' class='forumheader3'>Text</td>
+		<td style='width:70%' class='forumheader3'>
+		";
+
+		$insertjs = (!$pref['wysiwyg'])?"rows='15' onselect='storeCaret(this);' onclick='storeCaret(this);' onkeyup='storeCaret(this);'":
+		"rows='25' style='width:100%' ";
+		$data = $tp->toForm($data);
+		$text .= "<textarea class='tbox' id='data' name='data' cols='80'  style='width:95%' $insertjs>".(strstr($data, "[img]http") ? $data : str_replace("[img]../", "[img]", $data))."</textarea>
+		";
+
+		if (!$pref['wysiwyg']) {
+			$text .= "<input id='helpb' class='helpbox' type='text' name='helpb' size='100' style='width:95%'/>
+			<br />". display_help("helpb");
+		}
+
+		$text .= "
+		</td>
+		</tr>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Allow page to be rated</td>
+		<td style='width:70%;' class='forumheader3'>
+		<input type='radio' name='page_rating_flag' value='1'".($page_rating_flag ? " checked='checked'" : "")." /> yes&nbsp;&nbsp;
+		<input type='radio' name='page_rating_flag' value='0'".(!$page_rating_flag ? " checked='checked'" : "")." /> no
+		</td>
+		</tr>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Allow comments</td>
+		<td style='width:70%;' class='forumheader3'>
+		<input type='radio' name='page_comment_flag' value='1'".($page_comment_flag ? " checked='checked'" : "")." /> yes&nbsp;&nbsp;
+		<input type='radio' name='page_comment_flag' value='0'".(!$page_comment_flag ? " checked='checked'" : "")." /> no
+		</td>
+		</tr>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Password protect page<br /><span class='smalltext'>enter password to protect page</span></td>
+		<td style='width:70%' class='forumheader3'><input class='tbox' type='text' name='page_password' size='20' value='".$page_password."' maxlength='50' /></td>
+		</tr>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Page visible to</td>
+		<td style='width:70%' class='forumheader3'>".r_userclass("page_class", $page_class)."</td>
+		</tr>
+
+		<tr>
+		<td style='width:30%' class='forumheader3'>Page theme</td>
+		<td style='width:70%' class='forumheader3'>
+		<select name='page_theme' class='tbox'>
+		";
+
+		$tmatch = ($page_theme ? $page_theme : $pref['sitetheme']);
+		foreach($themelist as $key => $info)
+		{
+			$text .= "<option".($tmatch == $key ? " selected='selected'" : "").">".$key."</option>\n";
+		}
+
+		$text .= "</select>
+		</td>
+		</tr>
+
+
+		<tr>
+		<td colspan='2' style='text-align:center' class='forumheader'>". 
+		($edit ? "<input class='button' type='submit' name='updatePage' value='Update Page' /><input type='hidden' name='pe_id' value='$id' />" : "<input class='button' type='submit' name='submitPage' value='Create Page' />")."
+		</td>
+		</tr>
+
+		</table>
+		</form>
+		</div>
+		";
+
+		$caption = ($edit ? "Edit page" : "Create new page");
+		$ns -> tablerender($caption, $text);
+	}
+
+
+	function submitPage($mode = FALSE)
+	{
+		global $sql, $tp;
+
+		$page_title = $tp -> toDB($_POST['page_title']);
+		$page_text = $tp -> toDB($_POST['data']);
+
+		if($mode)
+		{
+			$sql -> db_Update("page", "page_title='$page_title', page_text='$page_text', page_rating_flag='".$_POST['page_rating_flag']."', page_comment_flag='".$_POST['page_comment_flag']."', page_password='".$_POST['page_password']."', page_class='".$_POST['page_class']."', page_ip_restrict='".$_POST['page_ip_restrict']."', page_theme='".$_POST['page_theme']."' WHERE page_id='$mode' ");
+			$this -> message = "Page updated in database.";
+		}
+		else
+		{
+			$sql -> db_Insert("page", "0, '$page_title', '$page_text', '".USERID."', '".time()."', '".$_POST['page_rating_flag']."', '".$_POST['page_comment_flag']."', '".$_POST['page_password']."', '".$_POST['page_class']."', '', '".$_POST['page_theme']."' ");
+			$this -> message = "Page saved to database.";
+		}
+	}
+
+	function delete_page($del_id)
+	{
+		global $sql;
+		$del_id = $matches[1];
+		$sql -> db_Delete("page", "page_id='$del_id' ");
+		$this -> message = "Page deleted";
+	}
+
+
+	function show_options($action)
+	{
+		if ($action == "")
+		{
+			$action = "main";
+		}
+		$var['main']['text'] = "Front page";
+		$var['main']['link'] = e_SELF;
+
+		$var['create']['text'] = "Create page";
+		$var['create']['link'] = e_SELF."?create";
+
+		$var['options']['text'] = LAN_OPTIONS;
+		$var['options']['link'] = e_SELF."?options";
+
+		show_admin_menu("Page Options", $action, $var);
+	}
+
+}
+
+function cpage_adminmenu() {
+	global $page;
+	global $action;
+	$page -> show_options($action);
+}
