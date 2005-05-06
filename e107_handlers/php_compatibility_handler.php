082fe089330764b4caac4ba1660d5a6e55f6e788
streaky <streaky@localhost>
2005-05-06 15:30:04 +0000

    added stripos() from PEAR::Compat
    svn path=/trunk/e107_0.7/; revision=3115

--- e107_handlers/php_compatibility_handler.php
+++ e107_handlers/php_compatibility_handler.php
@@ -21,5 +21,59 @@ if (!function_exists('file_put_contents')) {
 		return $bytes;
 	}
 }
+
+
+// Following code is borrowed from PEAR::Compat - <http://pear.php.net/package/PHP_Compat/>
+
+
+/**
+ * Replace stripos()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @link        http://php.net/function.stripos
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     $Revision: 1.2 $
+ * @since       PHP 5
+ * @require     PHP 4.0.1 (trigger_error)
+ */
+if (!function_exists('stripos')) {
+    function stripos($haystack, $needle, $offset = null)
+    {
+        if (!is_scalar($haystack)) {
+            trigger_error('stripos() expects parameter 1 to be string, ' . gettype($haystack) . ' given', E_USER_WARNING);
+            return false;
+        }
+
+        if (!is_scalar($needle)) {
+            trigger_error('stripos() needle is not a string or an integer.', E_USER_WARNING);
+            return false;
+        }
+
+        if (!is_int($offset) && !is_bool($offset) && !is_null($offset)) {
+            trigger_error('stripos() expects parameter 3 to be long, ' . gettype($offset) . ' given', E_USER_WARNING);
+            return false;
+        }
+
+        // Manipulate the string if there is an offset
+        $fix = 0;
+        if (!is_null($offset)) {
+            if ($offset > 0) {
+                $haystack = substr($haystack, $offset, strlen($haystack) - $offset);
+                $fix = $offset;
+            }
+        }
+
+        $segments = explode(strtolower($needle), strtolower($haystack), 2);
+
+        // Check there was a match
+        if (count($segments) == 1) {
+            return false;
+        }
+
+        $position = strlen($segments[0]) + $fix;
+        return $position;
+    }
+}
 	
 ?>
