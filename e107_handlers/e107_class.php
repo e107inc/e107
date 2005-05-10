b79fcc4efed5d87eca18c5e0e8604c9a6fdba592
streaky <streaky@localhost>
2005-05-10 17:10:50 +0000

    Added header block - note: last commit should have noted that it fixed bugs #625 and #626
    svn path=/trunk/e107_0.7/; revision=3148

--- e107_handlers/e107_class.php
+++ e107_handlers/e107_class.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_handlers/e107_class.php,v $
+|     $Revision: 1.11 $
+|     $Date: 2005-05-10 17:10:50 $
+|     $Author: streaky $
++----------------------------------------------------------------------------+
+*/
+
 class e107{
 	var $server_path;
 	var $http_path;
