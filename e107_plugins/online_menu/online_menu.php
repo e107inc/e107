feb72ec01514b5424c5649659d773f655f756b30
streaky <streaky@localhost>
2005-05-14 16:51:49 +0000

    More online fixes
    svn path=/trunk/e107_0.7/; revision=3193

--- e107_plugins/online_menu/online_menu.php
+++ e107_plugins/online_menu/online_menu.php
@@ -11,30 +11,36 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/online_menu/online_menu.php,v $
-|     $Revision: 1.7 $
-|     $Date: 2005-05-09 17:18:39 $
-|     $Author: stevedunstan $
+|     $Revision: 1.8 $
+|     $Date: 2005-05-14 16:51:49 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
 
 if(!defined("e_PLUGIN")){ exit; }
-$text = ONLINE_L1.GUESTS_ONLINE."<br />";
-//if($pref['user_reg'] == 1){
-$text .= ONLINE_L2.MEMBERS_ONLINE.(MEMBERS_ONLINE ? ", ": "").MEMBER_LIST."<br />";
-//}
-$text .= ONLINE_L3.ON_PAGE;
+
 $caption = (file_exists(THEME."images/online_menu.png") ? "<img src='".THEME."images/online_menu.png' alt='' /> ".ONLINE_L4 : ONLINE_L4);
-	
-$total_members = $sql->db_Count("user");
-	
-if ($total_members > 1) {
-	$newest_member = $sql->db_Select("user", "user_id, user_name", "user_ban='0' ORDER BY user_join DESC LIMIT 0,1");
-	$row = $sql->db_Fetch();
-	 extract($row);
-	$text .= "<br />".ONLINE_L5.": ".$total_members.", ".ONLINE_L6.": <a href='".e_BASE."user.php?id.$user_id'>$user_name</a>";
+
+if(!e_TRACKING_DISABLED) {
+	$text = ONLINE_L1.GUESTS_ONLINE."<br />";
+	//if($pref['user_reg'] == 1){
+	$text .= ONLINE_L2.MEMBERS_ONLINE.(MEMBERS_ONLINE ? ", ": "").MEMBER_LIST."<br />";
+	//}
+	$text .= ONLINE_L3.ON_PAGE;
+
+	$total_members = $sql->db_Count("user");
+
+	if ($total_members > 1) {
+		$newest_member = $sql->db_Select("user", "user_id, user_name", "user_ban='0' ORDER BY user_join DESC LIMIT 0,1");
+		$row = $sql->db_Fetch();
+		extract($row);
+		$text .= "<br />".ONLINE_L5.": ".$total_members.", ".ONLINE_L6.": <a href='".e_BASE."user.php?id.$user_id'>$user_name</a>";
+	}
+} else {
+	$text = TRACKING_MESSAGE;
 }
-	
+
 $ns->tablerender($caption, $text, 'online');
 
 
