0df884b4f4b3249e92cec462c279ace5a7edbe41
e107steved <steved@e107.org>
2005-05-18 11:16:11 +0000

    Initial rewrite of list_new plugin.
    svn path=/trunk/e107_0.7/; revision=3254

--- e107_plugins/list_new/new.php
+++ e107_plugins/list_new/new.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/list_new/new.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2005-03-20 19:47:53 $
+|     $Revision: 1.11 $
+|     $Date: 2005-05-18 11:16:11 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -27,94 +27,117 @@ require_once(HEADERF);
 @include(e_PLUGIN."list_new/languages/".e_LANGUAGE.".php");
 @include(e_PLUGIN."list_new/languages/English.php");
 	
-$bullet = (defined("BULLET") ? "<img src='".THEME."images/".BULLET."' alt='' style='vertical-align: middle;' />" : "<img src='".THEME."images/".(defined("BULLET") ? BULLET : "bullet2.gif")."' alt='bullet' style='vertical-align: middle;' />");
-	
-$sql2 = new db;
+$BULLET = (defined("BULLET") ? "<img src='".THEME."images/".BULLET."' alt='' style='vertical-align: middle;' />" : "<img src='".THEME."images/".(defined("BULLET") ? BULLET : "bullet2.gif")."' alt='bullet' style='vertical-align: middle;' />");
 $lvisit = USERLV;
-$news_items = $sql->db_Select("news", "*", "news_datestamp>$lvisit  ORDER BY news_datestamp DESC LIMIT 0,10");
-	
-while ($row = $sql->db_Fetch()) {
-	extract($row);
-	if (check_class($news_class)) {
-		$str .= "$bullet<a href='".e_BASE."comment.php?comment.news.$news_id'>$news_title</a><br />";
-	} else {
-		$news_items = $news_items - 1;
+
+/* --- get template --- */
+if (file_exists(THEME."list_new_template.php"))
+{
+	require_once(THEME."list_new_template.php");
+}
+else if(!$LISTNEW_MAIN_START)
+{
+	require_once(e_PLUGIN."list_new/templates/list_new_template.php");
+}
+
+
+$mainStr = preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_START);
+
+/* --- news --- */
+if($ITEMS = $sql->db_Select("news", "*", "news_class REGEXP '".e_CLASS_REGEXP."' AND news_datestamp>$lvisit  ORDER BY news_datestamp DESC LIMIT 0,10"))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_1;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_NEWS);
+	foreach($newArray as $item)
+	{
+		$TITLE = "<a href='".e_BASE."comment.php?comment.news.".$item['news_id']."'>".($item['news_title'] ? $item['news_title'] : "( ".LIST_2." )")."</a>";
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_NEWS);
 	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_NEWS);
 }
-if (!$news_items) {
-	$str = LIST_4;
-}
-	
-$text = "<div style='text-align:center'>
-	<table style='width:95%' class='fborder'>
-	<tr>
-	<td class='fcaption'>".LIST_1." ($news_items)</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-// Articles/content/reviews
-unset($str);
-$content_items = $sql->db_Select("content", "*", "content_type < 4 AND content_datestamp>$lvisit  ORDER BY content_datestamp DESC LIMIT 0,10");
-while ($row = $sql->db_Fetch()) {
-	extract($row);
-	if (check_class($content_class)) {
-		//        find out whether article, review or content page ...
-		switch($content_type) {
-			case 0:
-			//        article
-			$str .= $bullet."[ ".LIST_14." ] : <a href='".e_BASE."content.php?article.$content_id'>$content_heading</a><br />";
-			break;
-			case 1:
-			//        content page
-			$str .= $bullet."[ ".LIST_15." ] : <a href='".e_BASE."content.php?content.$content_id'>$content_heading</a><br />";
-			break;
-			case 3:
-			//        review
-			$str .= $bullet."[ ".LIST_16." ] : <a href='".e_BASE."content.php?review.$content_id.'>$content_heading</a><br />";
-			break;
-		}
-	} else {
-		$content_items = $content_items - 1;
+/* --- end news --- */
+
+/* --- chatbox --- */
+if($ITEMS = $sql->db_Select("chatbox", "*", "cb_datestamp>$lvisit ORDER BY cb_datestamp DESC LIMIT 0,50"))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_5;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_CHATBOX);
+	foreach($newArray as $item)
+	{
+		extract($item);
+		$cb_id = substr($cb_nick , 0, strpos($cb_nick , "."));
+		$cb_nick = substr($cb_nick , (strpos($cb_nick , ".")+1));
+		$cb_message = ($cb_blocked ? LIST_6 : str_replace("<br />", "", $tp -> toHTML($cb_message)));
+		$USER = "<a href='".e_BASE."user.php?id.$cb_id'>$cb_nick</a>";
+		$POST = $cb_message;
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_CHATBOX);
 	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_CHATBOX);
 }
-if (!$content_items) {
-	$str = LIST_4;
+/* --- end chatbox --- */
+
+
+
+
+/* --- forum --- */
+$query = "SELECT tp.thread_name AS parent_name, t.thread_thread, t.thread_id, t.thread_name, t.thread_parent, f.forum_id, f.forum_name FROM #forum_t AS t 
+LEFT JOIN #forum_t AS tp ON t.thread_parent = tp.thread_id 
+LEFT JOIN #forum AS f ON f.forum_id = t.thread_forum_id 
+WHERE f.forum_class  IN (".USERCLASS_LIST.") 
+AND t.thread_datestamp > $lvisit
+ORDER BY t.thread_datestamp DESC LIMIT 0, 50";
+if($ITEMS = $sql->db_Select_gen($query))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_7;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_FORUM);
+	foreach($newArray as $item)
+	{
+		extract($item);
+		$FORUM = "<a href='".e_PLUGIN."forum/forum_viewforum.php?$forum_id'>$forum_name</a>";
+		$TITLE = ($thread_parent ? "Re: <a href='".e_PLUGIN."forum/forum_viewtopic.php?$thread_parent#$thread_id'>".$tp -> toHTML($parent_name)."</a>" : "<a href='".e_PLUGIN."forum/forum_viewtopic.php?$thread_id'>".$tp -> toHTML($thread_name)."</a>");
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_FORUM);
+	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_FORUM);
 }
+/* --- end forum --- */
+
+
+
+/* --- comments --- */
+if($ITEMS = $sql->db_Select("comments", "*", "comment_datestamp>$lvisit  ORDER BY comment_datestamp DESC LIMIT 0,10"))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_9;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_COMMENT);
+	foreach($newArray as $item)
+	{
+		extract($item);
 	
-$text .= "
-	<tr>
-	<td class='fcaption'>".LIST_21." ($content_items)</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-unset($str);
-$comment_count = 0;
-if ($comments = $sql->db_Select("comments", "*", "comment_datestamp>$lvisit ORDER BY comment_datestamp DESC LIMIT 0,10")) {
-	while ($row = $sql->db_Fetch()) {
-		extract($row);
 		switch($comment_type) {
 
-			case "ideas":
-				$sql2->db_Select("ideas", "ideas_summary", "ideas_id=$comment_item_id ");
-				$row = $sql2->db_Fetch();
+			case "ideas":	// ideas
+				$sql -> db_Select("ideas", "ideas_summary", "ideas_id=$comment_item_id ");
+				$row = $sql -> db_Fetch();
 				extract($row);
-				$str .= $bullet."[ ".LIST_22." ] Re: <a href='".e_PLUGIN."ideas/ideas.php?show.$comment_item_id'>".$tp -> toHTML($ideas_summary)."</a><br />";
+				$TYPE = LIST_10;
+				$TITLE = "Re: <a href='".e_PLUGIN."ideas/ideas.php?show.$comment_item_id'>".$tp -> toHTML($ideas_summary)."</a>";
 			break;
 
-			case 0:
-			// news
-			$sql2->db_Select("news", "*", "news_id=$comment_item_id ");
-			$row = $sql2->db_Fetch();
-			 extract($row);
-			if (check_class($news_class)) {
-				$news_title = $tp -> toHTML($news_title);
-				$str .= $bullet."[ ".LIST_1." ] Re: <a href='".e_BASE."comment.php?comment.news.$comment_item_id'>".$tp -> toHTML($news_title, "admin")."</a><br />";
-				$comment_count++;
-			}
+			case 0: // news
+				$sql -> db_Select("news", "*", "news_id=$comment_item_id ");
+				$row = $sql -> db_Fetch();
+				 extract($row);
+				if (check_class($news_class))
+				{
+					$TYPE = LIST_12;
+					$TITLE = "Re: <a href='".e_BASE."comment.php?comment.news.$comment_item_id'>".$tp -> toHTML($news_title, TRUE)."</a>";
+				}
 			break;
-			 
+
+/*
 			case 1:
 			//        article, review or content page
 			//        find out whether article, review or content page ...
@@ -139,45 +162,51 @@ if ($comments = $sql->db_Select("comments", "*", "comment_datestamp>$lvisit ORDE
 				}
 			}
 			break;
-			 
+*/
+
+
+
+
 			case 2: //        downloads
-			$mp = MPREFIX;
-			$qry = "SELECT download_name, {$mp}download_category.download_category_class FROM {$mp}download LEFT JOIN {$mp}download_category ON {$mp}download.download_category={$mp}download_category.download_category_id WHERE {$mp}download.download_id={$comment_item_id}";
-			$sql2->db_Select_gen($qry);
-			$row = $sql2->db_Fetch();
-			extract($row);
-			if (check_class($download_category_class)) {
-				$str .= $bullet."[ ".LIST_17." ] Re: <a href='".e_BASE."download.php?view.$comment_item_id'>".$tp -> toHTML($download_name, "admin")."</a><br />";
-				$comment_count++;
-			}
+				$mp = MPREFIX;
+				$qry = "SELECT download_name, {$mp}download_category.download_category_class FROM {$mp}download LEFT JOIN {$mp}download_category ON {$mp}download.download_category={$mp}download_category.download_category_id WHERE {$mp}download.download_id={$comment_item_id}";
+				$sql -> db_Select_gen($qry);
+				$row = $sql -> db_Fetch();
+				extract($row);
+				if (check_class($download_category_class))
+				{
+					$TYPE = LIST_11;
+					$TITLE = " Re: <a href='".e_BASE."download.php?view.$comment_item_id'>".$tp -> toHTML($download_name, "admin")."</a>";
+				}
 			break;
 			 
 			case 3: //        faq
-			$sql2->db_Select("faq", "faq_question", "faq_id=$comment_item_id ");
-			$row = $sql2->db_Fetch();
-			 extract($row);
-			$str .= $bullet."[ ".LIST_18." ] Re: <a href='".e_BASE."faq.php?view.$comment_item_id'>".$tp -> toHTML($faq_question, "admin")."</a><br />";
-			$comment_count++;
+				$sql -> db_Select("faq", "faq_question", "faq_id=$comment_item_id ");
+				$row = $sql -> db_Fetch();
+				extract($row);
+				$TYPE = LIST_15;
+				$TITLE = "Re: <a href='".e_BASE."faq.php?view.$comment_item_id'>".$tp -> toHTML($faq_question, TRUE)."</a>";
 			break;
 			 
-			case 4:
-			//        poll comment
-			$sql2->db_Select("poll", "*", "poll_id=$comment_item_id ");
-			$row = $sql2->db_Fetch();
-			 extract($row);
-			$str .= $bullet."[ ".LIST_19." ] Re: <a href='".e_BASE."comment.php?comment.poll.$comment_item_id'>".$tp -> toHTML($poll_title, "admin")."</a><br />";
-			$comment_count++;
+			case 4: //        poll comment
+				$sql -> db_Select("polls", "*", "poll_id=$comment_item_id ");
+				$row = $sql -> db_Fetch();
+				extract($row);
+				$TYPE = LIST_14;
+				$TITLE = "Re: <a href='".e_BASE."comment.php?comment.poll.$comment_item_id'>".$tp -> toHTML($poll_title, TRUE)."</a>";
 			break;
 			 
-			case 6:
-			//        bugtracker
-			$sql2->db_Select("bugtrack2_bugs", "bugtrack2_bugs_summary", "bugtrack2_bugs_id=$comment_item_id ");
-			$row = $sql2->db_Fetch();
-			 extract($row);
-			$str .= $bullet."[ ".LIST_20." ] Re: <a href='".e_PLUGIN."bugtracker2/bugtracker2.php?0.bug.$comment_item_id'>".$tp -> toHTML($bugtrack_summary)."</a><br />";
-			$comment_count++;
+			case 6: //        bugtracker
+				$sql -> db_Select("bugtrack2_bugs", "bugtrack2_bugs_summary", "bugtrack2_bugs_id=$comment_item_id ");
+				$row = $sql -> db_Fetch();
+				 extract($row);
+				$TYPE = LIST_13;
+				$TITLE = "Re: <a href='".e_PLUGIN."bugtracker2/bugtracker2.php?0.bug.$comment_item_id'>".$tp -> toHTML($bugtrack2_bugs_summary)."</a>";
 			break;
 		}
+		
+		
+		
 		$handle = opendir(e_PLUGIN);
 		while (false !== ($file = readdir($handle))) {
 			if ($file != "." && $file != ".." && is_dir(e_PLUGIN.$file)) {
@@ -186,8 +215,8 @@ if ($comments = $sql->db_Select("comments", "*", "comment_datestamp>$lvisit ORDE
 					if ($file2 == "e_comment.php") {
 						include(e_PLUGIN.$file."/".$file2);
 						if ($comment_type == $e_plug_table) {
-							$sql2->db_Select("".$db_table."", "".$link_name."", "".$db_id."=$comment_item_id ");
-							$row = $sql2->db_Fetch();
+							$sql -> db_Select("".$db_table."", "".$link_name."", "".$db_id."=$comment_item_id ");
+							$row = $sql -> db_Fetch();
 							 extract($row);
 							$nid = $comment_item_id;
 							$link = $row[0];
@@ -199,107 +228,102 @@ if ($comments = $sql->db_Select("comments", "*", "comment_datestamp>$lvisit ORDE
 				}
 			}
 		}
+
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_COMMENT);
 	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_COMMENT);
 }
-if ($comment_count == 0) {
-	$str = LIST_4;
-}
-	
-$text .= "
-	<tr>
-	<td class='fcaption'>".LIST_2." ({$comment_count})</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-unset($str);
-if ($chatbox_posts = $sql->db_Select("chatbox", "*", "cb_datestamp>$lvisit ORDER BY cb_datestamp DESC LIMIT 0,50")) {
-	while ($row = $sql->db_Fetch()) {
-		extract($row);
-		$cb_id = substr($cb_nick , 0, strpos($cb_nick , "."));
-		$cb_nick = substr($cb_nick , (strpos($cb_nick , ".")+1));
-		$cb_message = ($cb_blocked ? CHATBOX_L6 : str_replace("<br />", "", $tp -> toHTML($cb_message)));
-		$str .= $bullet."[ <a href='".e_BASE."user.php?id.$cb_id'>$cb_nick</a> ] {$cb_message}<br />";
-	}
-} else {
-	$str = LIST_4;
-}
-	
-$text .= "
-	<tr>
-	<td class='fcaption'>".LIST_5." ($chatbox_posts)</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-unset($str);
-
-
-$query = "SELECT tp.thread_name AS parent_name, t.thread_thread, t.thread_id, t.thread_name, t.thread_parent, f.forum_id, f.forum_name FROM #forum_t AS t 
-LEFT JOIN #forum_t AS tp ON t.thread_parent = tp.thread_id 
-LEFT JOIN #forum AS f ON f.forum_id = t.thread_forum_id 
-WHERE f.forum_class  IN (".USERCLASS_LIST.") 
-AND t.thread_datestamp > $lvisit
-ORDER BY t.thread_datestamp DESC LIMIT 0, 50";
+/* --- end comments --- */
 
 
 
 
 
-if($forum_posts = $sql->db_Select_gen($query))
+
+
+
+/* --- bugs --- */
+if($ITEMS = $sql->db_Select("bugtrack2_bugs", "*", "bugtrack2_bugs_datestamp>$lvisit ORDER BY bugtrack2_bugs_datestamp DESC LIMIT 0,20"))
 {
-	while ($row = $sql->db_Fetch()) {
-		extract($row);
-		if ($thread_parent) {
-			$str .= $bullet."[ <a href='".e_PLUGIN."forum/forum_viewforum.php?$forum_id'>$forum_name</a> ] Re: <a href='".e_PLUGIN."forum/forum_viewtopic.php?$thread_parent#$thread_id'>".$tp -> toHTML($parent_name)."</a><br />";
-		}
-		else
-		{
-			$str .= $bullet."[ <a href='".e_PLUGIN."forum/forum_viewforum.php?$forum_id'>$forum_name</a> ] <a href='".e_PLUGIN."forum/forum_viewtopic.php?$thread_id'>".$tp -> toHTML($thread_name)."</a><br/>";
-		}
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_16;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_BUG);
+	foreach($newArray as $item)
+	{
+		extract($item);
+		$MEMBER = "<a href='".e_PLUGIN."bugtracker2/bugtracker2.php?0.bug.$bugtrack2_bugs_id'>".$tp -> toHTML($bugtrack2_bugs_summary)."</a>";
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_BUG);
 	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_BUG);
 }
-if (!$forum_posts) {
-	$str = LIST_4;
-}
-	
-$text .= "
-	<tr>
-	<td class='fcaption'>".LIST_6." ($forum_posts)</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-unset($str);
-if ($new_members = $sql->db_Select("user", "*", "user_join>$lvisit ORDER BY user_join DESC LIMIT 0,30")) {
-	while ($row = $sql->db_Fetch()) {
-		extract($row);
-		$str .= "$bullet<a href='".e_BASE."user.php?id.$user_id'>$user_name</a><br />";
+/* --- end bugs --- */
+
+
+/* --- ideas --- */
+if($ITEMS = $sql->db_Select("ideas", "*", "ideas_datestamp>$lvisit ORDER BY deas_datestamp DESC LIMIT 0,20"))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_17;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_IDEA);
+	foreach($newArray as $item)
+	{
+		extract($item);
+		$MEMBER = "<a href='".e_PLUGIN."ideas/ideas.php?show.$ideas_id'>".$tp -> toHTML($ideas_summary)."</a>";
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_IDEA);
 	}
-} else {
-	$str = LIST_4;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_IDEA);
 }
-	
-$text .= "
-	<tr>
-	<td class='fcaption'>".LIST_7." ($new_members)</td>
-	</tr>
-	<td class='forumheader3'>".$str."</td>
-	</tr>\n";
-	
-	
-$text .= "</table>\n</div>";
-$ns->tablerender(LIST_3, $text);
-	
-/*
-$time = USERLV;
-$new_news = $sql->db_Count("news", "(*)", "WHERE news_datestamp>'".$time."' "); if(!$new_news){ $new_news = "no"; }
-$new_comments = $sql->db_Count("comments", "(*)", "WHERE comment_datestamp>'".$time."' "); if(!$new_comments){ $new_comments = "no"; }
-$new_chat = $sql->db_Count("chatbox", "(*)", "WHERE cb_datestamp>'".$time."' "); if(!$new_chat){ $new_chat = "no"; }
-$new_forum = $sql->db_Count("forum_t", "(*)", "WHERE thread_datestamp>'".$time."' "); if(!$new_forum){ $new_forum = "no"; }
-$new_users = $sql->db_Count("user", "(*)", "WHERE user_join>'".$time."' "); if(!$new_users){ $new_users = "no"; }
-*/
-	
-	
+/* --- end ideas --- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --- new members --- */
+if($ITEMS = $sql->db_Select("user", "user_id, user_name", "user_join>$lvisit AND user_ban='0' ORDER BY user_join DESC LIMIT 0,30"))
+{
+	$newArray = $sql -> db_getList();
+	$HEADING = LIST_8;
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_START_MEMBER);
+	foreach($newArray as $item)
+	{
+		extract($item);
+		$MEMBER = "<a href='".e_BASE."user.php?id.$user_id'>$user_name</a>";
+		$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_MAIN_MEMBER);
+	}
+	$mainStr .= preg_replace("/\{(.*?)\}/e", '$\1', $LISTNEW_END_MEMBER);
+}
+/* --- end new members --- */
+
+
+
+
+
+
+
+if(trim(chop($mainStr)) == "")
+{
+	$mainStr = LIST_4;
+}
+if($LISTNEW_ENCLOSE)
+{
+	$ns -> tablerender($LISTNEW_HEADING, $mainStr);
+}
+else
+{
+	echo $mainStr;
+}
+
 require_once(FOOTERF);
 ?>
---
+++ e107_plugins/list_new/templates/list_new_template.php
@@ -0,0 +1,115 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/list_new/templates/list_new_template.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-05-18 11:16:11 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+
+
+$LISTNEW_ENCLOSE = TRUE;
+$LISTNEW_HEADING = LIST_3;
+
+
+$LISTNEW_MAIN_START = "
+";
+
+$LISTNEW_MAIN_END = "
+";
+
+
+
+
+
+$LISTNEW_START_NEWS = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_NEWS = "
+{BULLET} {TITLE}<br />
+";
+$LISTNEW_END_NEWS = "
+<br /><br />
+";
+
+
+
+$LISTNEW_START_CHATBOX = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_CHATBOX = "
+{BULLET} {USER}: {POST}<br />
+";
+$LISTNEW_END_CHATBOX = "
+<br /><br />
+";
+
+
+$LISTNEW_START_FORUM = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_FORUM = "
+{BULLET} [ {FORUM} ]: {TITLE}<br />
+";
+$LISTNEW_END_FORUM = "
+<br /><br />
+";
+
+
+$LISTNEW_START_MEMBER = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_MEMBER = "
+{BULLET} {MEMBER}<br />
+";
+$LISTNEW_END_MEMBER = "
+<br /><br />
+";
+
+
+$LISTNEW_START_COMMENT = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_COMMENT = "
+{BULLET} [ {TYPE} ]: {TITLE}<br />
+";
+$LISTNEW_END_COMMENT = "
+<br /><br />
+";
+
+
+
+$LISTNEW_START_BUG = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_BUG = "
+{BULLET} [ {TYPE} ]: {TITLE}<br />
+";
+$LISTNEW_END_BUG = "
+<br /><br />
+";
+
+
+
+$LISTNEW_START_IDEA = "
+<b>{HEADING} [{ITEMS}]</b><br />
+";
+$LISTNEW_MAIN_IDEA = "
+{BULLET} [ {TYPE} ]: {TITLE}<br />
+";
+$LISTNEW_END_IDEA = "
+<br /><br />
+";
+
+
+?>
