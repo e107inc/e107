113948308833084308facc23eb19e209bba3f5e7
e107steved <steved@e107.org>
2005-05-18 11:16:11 +0000

    Initial rewrite of list_new plugin.
    svn path=/trunk/e107_0.7/; revision=3254

--- e107_plugins/list_new/languages/English.php
+++ e107_plugins/list_new/languages/English.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/list_new/languages/English.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-03-12 20:18:23 $
+|     $Revision: 1.5 $
+|     $Date: 2005-05-18 11:16:11 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -20,22 +20,29 @@
 define("PAGE_NAME", "List New Items");
 	
 define("LIST_1", "News Items");
-define("LIST_2", "Comments");
+define("LIST_2", "untitled");
 define("LIST_3", "New since your last visit ...");
-define("LIST_4", "None");
+define("LIST_4", "No new items found.");
 define("LIST_5", "Chatbox posts");
-define("LIST_6", "Forum posts");
-define("LIST_7", "New members");
-define("LIST_8", "found");
-define("LIST_9", "news items");
-define("LIST_10", "returned");
-define("LIST_11", "Admin");
-define("LIST_12", "Settings");
+define("LIST_6", "blocked by admin");
+define("LIST_7", "Forum Posts");
+define("LIST_8", "New members");
+define("LIST_9", "Comments");
+define("LIST_10", "Ideas");
+define("LIST_11", "Download");
+define("LIST_12", "News");
+define("LIST_13", "Bugtracker");
+define("LIST_14", "Poll");
+define("LIST_15", "FAQ");
+define("LIST_16", "Bugs");
+define("LIST_17", "Ideas");
+
+
 define("LIST_13", "Profile");
 define("LIST_14", "Article");
 define("LIST_15", "Content");
-define("LIST_16", "Review");
-define("LIST_17", "Download");
+
+
 define("LIST_18", "FAQ");
 define("LIST_18", "Poll");
 define("LIST_19", "Docs");
