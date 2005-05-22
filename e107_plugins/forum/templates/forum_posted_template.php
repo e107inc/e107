c83312404cdaf44d0f1f853c87114d69377b2912
e107steved <steved@e107.org>
2005-05-22 13:47:27 +0000

    Tidy up of message display after forum attachment (now displays in thankyou box).
    svn path=/trunk/e107_0.7/; revision=3319

--- e107_plugins/forum/templates/forum_posted_template.php
+++ e107_plugins/forum/templates/forum_posted_template.php
@@ -46,6 +46,7 @@ $FORUMREPLYPOSTED = "
 <td style='text-align:right; vertical-align:center; width:20%' class='forumheader2'>".IMAGE_e."&nbsp;</td>
 <td style='vertical-align:center; width:80%' class='forumheader2'>
 <br />".LAN_415."<br />
+".(defined("FORUM_MESSAGE") ? FORUM_MESSAGE."<br />" : "")."
 <span class='defaulttext'><a href='".e_PLUGIN."forum/forum_viewtopic.php?{$iid}.last'>".LAN_325."</a><br />
 <a href='".e_PLUGIN."forum/forum_viewforum.php?".$forum_id."'>".LAN_326."</a></span><br /><br />
 </td></tr></table>";
