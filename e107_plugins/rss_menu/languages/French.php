5ef4f8e7423c7b91e0c986564d05b03ddbe1b435
e107coders <e107coders@localhost>
2005-05-25 23:44:53 +0000

    first commit
    svn path=/trunk/e107_langpacks/; revision=3375

---
+++ e107_plugins/rss_menu/languages/French.php
@@ -0,0 +1,20 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - French Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/rss_menu/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-05-25 23:44:53 $
+|     $Author: e107coders $
+|     Encoding: UTF-8
++----------------------------------------------------------------------------+
+*/
+define("BACKEND_MENU_L1", " peuvent être syndiqués en utilisant ces sources rss.");
+define("BACKEND_MENU_L2", "RSS sources");
+define("BACKEND_MENU_L3", "Nos nouvelles");
+define("BACKEND_MENU_L4", "Nos commentaires");
+define("BACKEND_MENU_L5", "Nos sujets de forum");
+define("BACKEND_MENU_L6", "Nos messages de forum");
+
+?>
