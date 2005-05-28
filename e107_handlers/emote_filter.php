1dc0fb0a1292ccb5b061ea59ca81d46fbfce303b
e107steved <steved@e107.org>
2005-05-28 05:05:32 +0000

    Re-commit, seems some people dont have the new files.
    svn path=/trunk/e107_0.7/; revision=3408

--- e107_handlers/emote_filter.php
+++ e107_handlers/emote_filter.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/emote_filter.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-05-26 19:42:48 $
+|     $Revision: 1.6 $
+|     $Date: 2005-05-28 05:05:32 $
 |     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
@@ -53,10 +53,6 @@ class e_emotefilter {
 				}
 			}
 		}
-
-	
-
-		
 	}
 	 
 	function filterEmotes($text)
