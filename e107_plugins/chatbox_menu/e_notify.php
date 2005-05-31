334777c559c5a545916e4a3bf61f8686568b6f96
sweetas <sweetas@localhost>
2005-05-31 21:39:16 +0000

    notify lanned
    svn path=/trunk/e107_0.7/; revision=3447

--- e107_plugins/chatbox_menu/e_notify.php
+++ e107_plugins/chatbox_menu/e_notify.php
@@ -11,21 +11,24 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/chatbox_menu/e_notify.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-05-31 21:05:06 $
+|     $Revision: 1.3 $
+|     $Date: 2005-05-31 21:38:58 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 
-$config_category = 'Chatbox Events';
-$config_events = array('cboxpost' => 'Message posted');
+@include_once(e_PLUGIN.'chatbox_menu/languages/'.e_LANGUAGE.'_notify.php');
+@include_once(e_PLUGIN.'chatbox_menu/languages/English_notify.php');
+
+$config_category = NT_LAN_CB_1;
+$config_events = array('cboxpost' => NT_LAN_CB_2);
 
 if (!function_exists('notify_cboxpost')) {
 	function notify_cboxpost($data) {
 		global $nt;
-		$message = 'Posted by: '.USERNAME.' (IP Address: '.$data['ip'].' )<br />';
-		$message .= 'Message:<br />'.$data['cmessage'].'<br /><br />';
-		$nt -> send('cboxpost', 'Chatbox Message Posted', $message);
+		$message = NT_LAN_CB_3.': '.USERNAME.' ('.NT_LAN_CB_4.': '.$data['ip'].' )<br />';
+		$message .= NT_LAN_CB_5.':<br />'.$data['cmessage'].'<br /><br />';
+		$nt -> send('cboxpost', NT_LAN_CB_6, $message);
 	}
 }
 
---
+++ e107_plugins/links_page/languages/English_notify.php
@@ -0,0 +1,18 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/links_page/languages/English_notify.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-05-31 21:39:16 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
+*/
+
+define("NT_LAN_LP_1", "Links Page Events");
+define("NT_LAN_LP_2", "Link submitted by user");
+define("NT_LAN_LP_3", "Link Submitted");
+
+
+?>
