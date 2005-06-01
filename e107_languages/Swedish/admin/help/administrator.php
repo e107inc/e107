ca0641cfc74af80412ca466ace44923687202dda
mrpiercer <mrpiercer@localhost>
2005-06-01 13:50:23 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=3458

---
+++ e107_languages/Swedish/admin/help/administrator.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Swedish/admin/help/administrator.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-01 13:50:23 $
+|     $Author: mrpiercer $
++----------------------------------------------------------------------------+
+*/
+$caption = "Sajtadmin hjälp";
+$text = "Använd denna sida för att lägga till eller ta bort sajtadministratörer. Administratören kommer endast att ha tillgång till de markerade funktionerna.";
+$ns -> tablerender($caption, $text);
+?>
