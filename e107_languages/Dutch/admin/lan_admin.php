6ef13b1ced5f5df99e63643c7f13fdf9e027c4b6
mijnheer <mijnheer@localhost>
2005-06-02 05:10:17 +0000

    Updates june 1
    svn path=/trunk/e107_langpacks/; revision=3474

--- e107_languages/Dutch/admin/lan_admin.php
+++ e107_languages/Dutch/admin/lan_admin.php
@@ -204,6 +204,7 @@ define('ADLAN_CL_4', "Coms");
 define('ADLAN_CL_5', "Bestanden");
 define('ADLAN_CL_6', "Hulpmiddelen");
 define('ADLAN_CL_7', "Plugins");
+define('ADLAN_CL_8', "Docs");
 define("ADLAN_LAT_1", "Laatste");
 define("ADLAN_LAT_2", "Aangemelde nieuwsberichten");
 define("ADLAN_LAT_3", "Aangemelde artikelen");
