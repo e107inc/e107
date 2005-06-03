0f087c2ff634e0fc5170cd62c6c62d6390a25bab
alkas <alkas@localhost>
2005-06-03 15:47:59 +0000

    Issaugota utf-8 formatu
    svn path=/trunk/e107_langpacks/; revision=3511

--- e107_languages/Lithuanian/lan_submitnews.php
+++ e107_languages/Lithuanian/lan_submitnews.php
@@ -1,11 +1,11 @@
-<?php
+﻿<?php
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Lithuanian/lan_submitnews.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-02 22:18:36 $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-03 15:47:59 $
 |     $Author: alkas $
 +----------------------------------------------------------------------------+
 */
