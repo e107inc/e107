b51e6cdeea94203b91f18a6c70170baf0b299afc
mijnheer <mijnheer@localhost>
2005-06-03 20:49:24 +0000

    Chatbox cleanup june 3rd
    svn path=/trunk/e107_langpacks/; revision=3521

---
+++ e107_plugins/chatbox_menu/languages/Dutch/Dutch_config.php
@@ -0,0 +1,46 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/chatbox_menu/languages/Dutch/Dutch_config.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-03 20:49:24 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+define("CHBLAN_1", "Chatbox instellingen bijgewerkt.");
+define("CHBLAN_2", "Gemodereerd.");
+define("CHBLAN_3", "Nog geen chatbox berichten.");
+define("CHBLAN_4", "Lid");
+define("CHBLAN_5", "Gast");
+define("CHBLAN_6", "deblokkeer");
+define("CHBLAN_7", "blokkeer");
+define("CHBLAN_8", "verwijder");
+define("CHBLAN_9", "Modereren chatbox");
+define("CHBLAN_10", "Modereren berichten");
+define("CHBLAN_11", "Aantal te tonen chatbox berichten");
+define("CHBLAN_12", "aantal in de chatbox te tonen berichten ");
+define("CHBLAN_13", "Vervang links");
+define("CHBLAN_14", "wanneer aangekruist, worden ingevoerde links vervangen door de tekst in het onderstaande veld");
+define("CHBLAN_15", "Vervangende tekst als geactiveerd");
+define("CHBLAN_16", "links worden vervangen door deze tekst");
+define("CHBLAN_17", "Wordomslag lengte");
+define("CHBLAN_18", "woorden langer dan dit aantal worden omgeslagen");
+define("CHBLAN_19", "Bijwerken chatbox instellingen");
+define("CHBLAN_20", "Chatbox instellingen");
+define("CHBLAN_21", "Snoeien");
+define("CHBLAN_22", "Verwijder berichten ouder dan een bepaald tijdvak");
+define("CHBLAN_23", "Verwijder berichten ouder dan ");
+
+define("CHBLAN_24", "Een dag");
+define("CHBLAN_25", "Een week");
+define("CHBLAN_26", "Een maand");
+define("CHBLAN_27", "- Verwijder alle berichten -");
+define("CHBLAN_28", "Chatbox geschoond.");
+
+define("CHBLAN_29", "Toon chatbox binnen een scrolling layer");
+define("CHBLAN_30", "Layer hoogte");
+define("CHBLAN_31", "Toon emoticons");
+
+?>
