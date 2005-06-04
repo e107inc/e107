68d9d0a6221792329ba6d7a6af57801d1904e506
sweetas <sweetas@localhost>
2005-06-04 19:02:30 +0000

    Missing help files added
    svn path=/trunk/e107_0.7/; revision=3532

---
+++ e107_languages/English/admin/help/upload.php
@@ -0,0 +1,4 @@
+<?php
+$text = "From here you can allow / disallow the ability for users to upload files and manage those files that have been uploaded.";
+$ns -> tablerender("Public Uploads Help", $text);
+?>
