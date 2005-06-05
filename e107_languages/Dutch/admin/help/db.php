d42490ba08ceedf9533c8f22c73395595a5c86bc
mijnheer <mijnheer@localhost>
2005-06-05 06:00:22 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=3550

---
+++ e107_languages/Dutch/admin/help/db.php
@@ -0,0 +1,4 @@
+<?php
+$text = "Dit zijn de hulpmiddelen waarmee je de e107 database kunt beheren.";
+$ns -> tablerender("Database Hulpmiddelen", $text);
+?>
---
+++ e107_languages/Dutch/admin/help/upload.php
@@ -0,0 +1,4 @@
+<?php
+$text = "Met deze functie kun je gebruikers al dan niet toestaan om bestanden te uploaden naar je site en kun je de geuploade bestanden ook beheren.";
+$ns -> tablerender("Openbare Uploads hulp", $text);
+?>
