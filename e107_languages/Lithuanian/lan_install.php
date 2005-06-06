aa72a9de3998fafa699116a0075c5ce2d8501792
alkas <alkas@localhost>
2005-06-06 23:20:21 +0000

    isversta
    svn path=/trunk/e107_langpacks/; revision=3594

--- e107_languages/Lithuanian/lan_install.php
+++ e107_languages/Lithuanian/lan_install.php
@@ -1,99 +1,98 @@
-﻿<?php
+<?php
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Lithuanian/lan_install.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-03 15:47:58 $
+|     $Revision: 1.3 $
+|     $Date: 2005-06-06 23:20:21 $
 |     $Author: alkas $
 +----------------------------------------------------------------------------+
 */
 
-define("INSLAN1", "Installation Stage");
-define("INSLAN2", "PHP / mySQL version check / file permissions check");
-define("INSLAN3", "PHP Version");
-define("INSLAN4", "Fail");
-define("INSLAN5", "You are running a version of PHP that is not compatible with e107");
-define("INSLAN6", "Script halted.");
-define("INSLAN7", "Pass");
-define("INSLAN8", "mySQL Version");
-define("INSLAN9", "e107 was unable to determine the mySQL version number.");
-define("INSLAN10", "File permissions");
-define("INSLAN11", "is not writable");
-define("INSLAN12", "folder are not writable");
-define("INSLAN13", "Please ensure the permissions for the files shown above have been set<br />correctly on your server, the permissions should be set to 777. To set the<br />permissions, right click on the file in your FTP client and choose Chmod or<br />Set File Permissions, then type in 777, if the dialog that appears has boxes<br />then tick all the boxes.<br /><br />Please retest after setting the file permissions.");
-define("INSLAN14", "e107 installing");
-define("INSLAN15", "FATAL ERROR");
-define("INSLAN16", "Although it was not possible to determine the status of your mySQL installation,<br />please continue to next stage.");
-define("INSLAN17", "Continue");
-define("INSLAN18", "Retest File Permissions");
-define("INSLAN19", "All server tests have been passed, please click on the button to proceed to next stage");
-define("INSLAN20", "mySQL information");
-define("INSLAN21", "Please enter your mySQL settings here.<br />If you have root permissions you can create a new database by ticking the box, if not you must<br />create a database or use a pre-existing one. <br />If you have only one database use a prefix so that other scripts can share the same database.<br />If you do not know your mySQL details contact your web host.");
-define("INSLAN22", "mySQL Server");
-define("INSLAN23", "mySQL Username");
-define("INSLAN24", "mySQL Password");
-define("INSLAN25", "mySQL Database");
-define("INSLAN26", "Table prefix");
-define("INSLAN27", "Error");
-define("INSLAN28", "Error encountered");
-define("INSLAN29", "You left required fields blank, please re-enter mySQL information");
-define("INSLAN30", "e107 was unable to establish a connection to mySQL using the information you entered.<br />Please return to the last page and ensure the information is correct.");
-define("INSLAN31", "mySQL check");
-define("INSLAN32", "Connection to mySQL established and verified.");
-define("INSLAN33", "Attempting to create database");
-define("INSLAN34", "Unable to create database, please ensure you have the correct permissions to create databases on your server.");
-define("INSLAN35", "Successfully created database.");
-define("INSLAN36", "Please click on the button to proceed to next stage.");
+define("INSLAN1", "Įdiegimo žingsnis");
+define("INSLAN2", "PHP / mySQL versijų patikrinimas / teisių byloms patikrinimas");
+define("INSLAN3", "PHP versija");
+define("INSLAN4", "Nepavyko");
+define("INSLAN5", "Jūsų serveryje įdiegtos PHP versija netinka e107 portalo sistemai");
+define("INSLAN6", "Skriptas sustabdytas.");
+define("INSLAN7", "Pavyko");
+define("INSLAN8", "mySQL versija");
+define("INSLAN9", "e107 sistemai nepavyko nustatyti mySQL versijos numerio.");
+define("INSLAN10", "Teisės byloms");
+define("INSLAN11", "bylą uždrausta keisti");
+define("INSLAN12", "aplanko turinį uždrausta keisti");
+define("INSLAN13", "Prašome patikrinti redagavimo teises išvardintoms byloms.<br />Bylų teisių atributas serveryje turėtų būti nustatytas 777.<br />FTP kliento programoje pažymėkite bylą ir, paspaudę dešinįjį pelės klavišą,<br />pasirinkite CHMOD arba 'Set File Permissions', tuomet įrašykite 777,<br />jeigu dialogo lange yra žymėjimo langeliai, tuomet pažymėkite visus.<br /><br />Prašome vėl patikrinti teises byloms.");
+define("INSLAN14", "e107 įdiegimas");
+define("INSLAN15", "KRITINĖ KLAIDA");
+define("INSLAN16", "Nepavyko nustatyti mySQL duomenų bazės būsenos,<br />prašome mySQL duomenų bazės nuostatas surašyti sekančiame žingsnyje.");
+define("INSLAN17", "Tęsti");
+define("INSLAN18", "Iš naujo patikrinti bylų atributus");
+define("INSLAN19", "Visi serverio testai įvykdyti, prašome tęsti toliau");
+define("INSLAN20", "mySQL informacija");
+define("INSLAN21", "Prašome čia surašyti mySQL duomenų bazės nuostatas.<br />Jei serveryje turite administratoriaus teises, galite sukurti naują duomenų bazę pažymėję langelį, jei ne -<br />turite patys sukurti duomenų bazę arba naudoti jau esamą. <br />Jei turite tik vieną duomenų bazę, tai jos pavadinime panaudokite prefiksą _, kad kitos makrokomandos galėtų dalintis ta pačia duomenų baze.<br />Jie nežinote mySQL parametrų, tai susisiekite su serverio administratoriumi.");
+define("INSLAN22", "mySQL serveris");
+define("INSLAN23", "mySQL vartotojo vardas");
+define("INSLAN24", "mySQL slaptažodis");
+define("INSLAN25", "mySQL duomenų bazė");
+define("INSLAN26", "Lentelių prefiksas");
+define("INSLAN27", "Klaida");
+define("INSLAN28", "Įvyko klaida");
+define("INSLAN29", "Palikote tuščių langelių; prašome pakartotinai įveskite mySQL nuostatas");
+define("INSLAN30", "e107 nesugebėjo nustatyti ryšio su mySQL naudojant įvestą informaciją.<br />Prašome sugrįžti atgal ir įsitikinti įvestos informacijos teisingumu.");
+define("INSLAN31", "mySQL patikra");
+define("INSLAN32", "Ryšys su mySQL nustatytas, informacija patikrinta.");
+define("INSLAN33", "Bandoma sukurti duomenų bazę");
+define("INSLAN34", "Duomenų bazės sukurti nepavyko; prašome įsitikinti, kad turite reikiamas teises jūsų serveryje sukurti duomenų bazei.");
+define("INSLAN35", "Duomenų bazė sukurta sėkmingai.");
+define("INSLAN36", "Prašome tęsti toliau.");
 
 
-define("INSLAN37", "Back to last page");
-define("INSLAN38", "Administrator information");
-define("INSLAN39", "Please enter your main administrator username, password and email address here.<br />These details will be used to gain access to the administration area of your website.<br />Please make sure you write your username and password down in a safe place as if they are lost they<br />cannot be retrieved.");
-define("INSLAN40", "Admin Name");
-define("INSLAN41", "Admin Password");
-define("INSLAN42", "Confirm Password");
-define("INSLAN43", "Admin Email Address");
-define("INSLAN44", "You left required fields blank, please re-enter admin information.");
-define("INSLAN45", "The two passwords do not match, please re-enter.");
-define("INSLAN46", "doesn't appear to be a valid email address, please re-enter.");
-define("INSLAN47", "All set!");
-define("INSLAN48", "e107 now has all the information it needs to complete the installation.<br />Please click the button to create the database tables and save all your settings.");
-define("INSLAN49", "e107 was unable to save the main config file to your server<br />Please ensure the <b>e107_config.php</b> file has the correct permissions");
-define("INSLAN50", "Installation Completed!");
-define("INSLAN51", "All done");
-define("INSLAN52", "e107 has been successfully installed!<br />For security reasons you should now set the file permissions on the<br /><b>e107_config.php</b> file back to 644.<br />Also please delete /install.php from your server after you have clicked the button below");
-define("INSLAN53", "Click here to go to your new website!");
-define("INSLAN54", "Unable to read the sql datafile<br /><br />Please ensure the file <b>core_sql.php</b> exists in the <b>/e107_admin/sql</b> directory.");
-define("INSLAN55", "e107 was unable to create all of the required database tables.<br />Please clear the database and rectify any problems before trying again.");
-define("INSLAN56", "Welcome to your new website!\n");
+define("INSLAN37", "Grįžkite prie ankstesnio žingsnio");
+define("INSLAN38", "Administratoriaus informacija");
+define("INSLAN39", "Įveskite pagrindinio administratoriaus vardą, slaptažodį ir el. pašto adresą.<br />Šie duomenys reikalingi norint prisijungti prie administratoriaus srities svetainėje.<br />Įsidėmėkite šiuos duomenis, nes jų nebus galima atkurti.");
+define("INSLAN40", "Administratoriaus vardas");
+define("INSLAN41", "Administratoriaus slaptažodis");
+define("INSLAN42", "Patvirtinkite slaptažodį");
+define("INSLAN43", "El. pašto adresas");
+define("INSLAN44", "Palikote tuščių laukelių.");
+define("INSLAN45", "Slaptažodžiai neatitinka vienas kito, įveskite iš naujo.");
+define("INSLAN46", "regis, netaisingas el. pašto adresas. Prašome įrašyti teisingai.");
+define("INSLAN47", "Puiku!");
+define("INSLAN48", "e107 surinko visą reikalingą informaciją diegimo procesui užbaigti.<br />Paspauskite mygtuką duomenų bazės lentelėms sukurti ir visų nuostatų išsaugojimui.");
+define("INSLAN49", "e107 negalėjo įrašyti pagrindinės konfigūracijos bylos į jūsų serverį<br />Įsitikinkite, kad <b>e107_config.php</b> byla turi tinkamus atributus");
+define("INSLAN50", "Diegimo procesas baigtas!");
+define("INSLAN51", "Viskas atlikta");
+define("INSLAN52", "e107 sėkmingai įdiegta!<br />Siekdami apsaugoti serverį jūs turėtumėte bylai<br /><b>e107_config.php</b> nustatyti 644 atributą.<br />Taip pat prašome ištrinti /install.php bylą po to, kai nuspausite žemiau esantį mygtuką");
+define("INSLAN53", "Spauskite čia, jei norite patekti į jūsų naująją svetainę!");
+define("INSLAN54", "Negalima perskaityti sql duomenų bylos<br /><br />Įsitikinkite kad <b>core_sql.php</b> byla yra <b>/e107_admin/sql</b> aplankale.");
+define("INSLAN55", "e107 negalėjo sukurti visų būtinų duomenų bazės lentelių.<br />Prieš bandydami vėl, išvalykite duomenų bazę ir ištaisykite kitas klaidas.");
+define("INSLAN56", "Sveiki atvykę į savo naująją svetainę!\n");
 
-define("INSLAN57", "e107 has installed successfully and is now ready to accept content.\n");
-define("INSLAN58", "you will find the FAQ and documentation here.");
-define("INSLAN59", "Thankyou for trying e107, we hope it fulfills your website needs.\n(You can delete this message from your admin section.)\n\n<b>Please note this version of e107 is a beta version and is not intended to be used on live websites.</b>");
-define("INSLAN60", "tick to create");
-define("INSLAN61", "folder");
-define("INSLAN62", "or");
-define("INSLAN63", "File permission error");
-define("INSLAN64", "This file has been generated by the installation script.");
+define("INSLAN57", "e107 sėkmingai įdiegta ir pasirengusi priimti jūsų informaciją.");
+define("INSLAN58", "DUK ir dokumentaciją rasite čia.");
+define("INSLAN59", "Dėkojame už e107 sistemos pasirinkimą; tikimės, kad ji patenkins jūsų poreikius.\nŠį pranešimą galite ištrinti administratoriaus skiltyje.\nVertė ir suderino su lietuviškais datos formatais : www.NeoLex.net, © 2004");
+define("INSLAN60", "sukurti DB");
+define("INSLAN61", "aplankalas");
+define("INSLAN62", "arba");
+define("INSLAN63", "Failo atributo klaida");
+define("INSLAN64", "Šį failą sugeneravo diegimo makrokomanda.");
 
-define("INSLAN65", "e107 requires at least version 4.1.0");
-define("INSLAN66", "If you are using a local server on your computer you will need to upgrade your");
-define("INSLAN67", "version of PHP to continue, please see");
-define("INSLAN68", "for instructions. If you are");
-define("INSLAN69", "attempting to install e107 on a hosted server you will need to contact the");
-define("INSLAN70", "server administrators and ask them  to upgrade PHP for you.");
-define("INSLAN71", "Please rerun this script after upgrading your PHP version.");
+define("INSLAN65", "e107 reikalauja bent 4.1.0 versijos");
+define("INSLAN66", "Jei naudojate savo kompiuterį kaip vietinį serverį, tai turėtumėte atnaujinti");
+define("INSLAN67", "PHP, prašome laikytis");
+define("INSLAN68", "instrukcijų. Jei bandote");
+define("INSLAN69", "įdiegti e107 nuotoliniame serveryje, tai turėtumėte");
+define("INSLAN70", "susisiekti su serverio administratoriumi ir paprašyti atnaujinti PHP.");
+define("INSLAN71", "Prašome pakartotinai įvykdyti šią makrokomandą po PHP atnaujinimo.");
 
-define("INSLAN72", "This could mean that mySQL is not installed or not currently running, or");
-define("INSLAN73", "you could be running a version that doesn't correctly report the version");
-define("INSLAN74", "number (v5.x is known to have this problem). If the next step of the installation");
-define("INSLAN75", "fails you will need to check your mySQL status.");
+define("INSLAN72", "Tai galėtų reikšti, kad mySQL neįdiegta arba šiuo metu neveikia, arba");
+define("INSLAN73", "veikia tokia versija kuri neteisingai informuoja apie save");
+define("INSLAN74", "(v5.x versija gali turėti šią problemą). Jei sekanti diegimo stadija");
+define("INSLAN75", "nutrūks, turėtumėte patikrinti mySQL būklę.");
 
-define("INSLAN76", "Your administration section is");
-define("INSLAN77", "located here");
-define("INSLAN78", "click to go there now. You will have to login using the name and password you entered during the installation process.");
-
+define("INSLAN76", "Jūsų administravimo sritis yra");
+define("INSLAN77", "čia");
+define("INSLAN78", "paspauskite, jei norite ten patekti. Prisijunkite tuo vardu ir nurodykite tą slaptažodį, kurį pasirinkote diegimo proceso metu.");
 
 ?>
