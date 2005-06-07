d1f5d703d3f4d5b075cb2e6d2a82c7ebc9d72595
tpruvot <tpruvot@localhost>
2005-06-07 18:31:32 +0000

    Real French
    svn path=/trunk/e107_langpacks/; revision=3610

--- e107_plugins/login_menu/languages/French.php
+++ e107_plugins/login_menu/languages/French.php
@@ -4,9 +4,9 @@
 |     e107 website system - French Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/login_menu/languages/French.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-05-29 02:07:46 $
-|     $Author: e107coders $
+|     $Revision: 1.3 $
+|     $Date: 2005-06-07 18:31:32 $
+|     $Author: tpruvot $
 |     Encoding: UTF-8
 +----------------------------------------------------------------------------+
 */
@@ -14,43 +14,43 @@
 
 define("LOGIN_MENU_L1", "Nom d'utilisateur: ");
 define("LOGIN_MENU_L2", "Mot de passe: ");
-define("LOGIN_MENU_L3", "Se Connecter");
-define("LOGIN_MENU_L4", "Oublié mot de passe?");
+define("LOGIN_MENU_L3", "Connexion");
+define("LOGIN_MENU_L4", "Mot de passe oublié?");
 define("LOGIN_MENU_L5", "Bienvenue");
 define("LOGIN_MENU_L6", "Se souvenir de moi");
-define("LOGIN_MENU_L7", "Identité de l'utilisateur non reconnue (peut -être un problème de cookie).<br />Veuillez <a href=\"".e_BASE."index.php?logout\">cliquer ici</a> pour détruire le cookie.");
-define("LOGIN_MENU_L8", "se déconnecter");
-define("LOGIN_MENU_L9", "Erreur de connection");
-define("LOGIN_MENU_L10", "Le drapeau de maintenance est vrai - cela signifie que les visiteurs normaux sont redirigés sur sitedown.php. Pour réactiver le drapeau, allez sur admin/maintenance.");
-define("LOGIN_MENU_L11", "Zone Admin");
+define("LOGIN_MENU_L7", "Identité de l'utilisateur non reconnue (peut-être un problème de cookie).<br />Veuillez <a href=\"".e_BASE."index.php?logout\">cliquer ici</a> pour détruire le cookie.");
+define("LOGIN_MENU_L8", "Déconnexion");
+define("LOGIN_MENU_L9", "Erreur de connexion");
+define("LOGIN_MENU_L10", "L'indicateur de maintenance est activé - cela signifie que les visiteurs normaux sont redirigés sur sitedown.php. Pour désactiver cet indicateur, allez dans admin/maintenance.");
+define("LOGIN_MENU_L11", "Administration");
 define("LOGIN_MENU_L12", "Coordonnées");
 define("LOGIN_MENU_L13", "Profil");
-define("LOGIN_MENU_L14", "élément de nouvelles");
-define("LOGIN_MENU_L15", "éléments de nouvelles");
-define("LOGIN_MENU_L16", "message de chatbox");
-define("LOGIN_MENU_L17", "messages de chatbox");
+define("LOGIN_MENU_L14", "nouvelle");
+define("LOGIN_MENU_L15", "nouvelles");
+define("LOGIN_MENU_L16", "message dans le Chatbox");
+define("LOGIN_MENU_L17", "messages dans le Chatbox");
 define("LOGIN_MENU_L18", "commentaire");
 define("LOGIN_MENU_L19", "commentaires");
-define("LOGIN_MENU_L20", "message de forum");
-define("LOGIN_MENU_L21", "messages de forum");
-define("LOGIN_MENU_L22", "nouveau membre du site");
-define("LOGIN_MENU_L23", "nouveau membres du site");
+define("LOGIN_MENU_L20", "message dans le Forum");
+define("LOGIN_MENU_L21", "messages dans le Forum");
+define("LOGIN_MENU_L22", "nouveau membre");
+define("LOGIN_MENU_L23", "nouveaux membres");
 define("LOGIN_MENU_L24", "Cliquer ici pour voir la liste des nouvelles");
 define("LOGIN_MENU_L25", "Depuis votre dernière visite, il y a eu");
 define("LOGIN_MENU_L26", "aucun");
 define("LOGIN_MENU_L27", "et");
-define("LOGIN_MENU_L28", "Connection");
+define("LOGIN_MENU_L28", "Connexion");
 
-define("LOGIN_MENU_L29", "article de nouvelles");
-define("LOGIN_MENU_L30", "articles de nouvelles");
+define("LOGIN_MENU_L29", "article");
+define("LOGIN_MENU_L30", "articles");
 
 // New config options
-define('LOGIN_MENU_L31', "Montrer les nouveaux messages de nouvelles");
-define('LOGIN_MENU_L32', "Montrer les nouveaux messages d'articles");
-define('LOGIN_MENU_L33', "Montrer les nouveaux messages de Chatbox");
-define('LOGIN_MENU_L34', "Montrer les nouveaux messages de commentaires");
-define('LOGIN_MENU_L35', "Montrer les nouveaux messages de forum");
-define('LOGIN_MENU_L36', "Montrer les nouveaux membres");
+define('LOGIN_MENU_L31', "Afficher les dernières nouvelles");
+define('LOGIN_MENU_L32', "Afficher les nouveaux articles");
+define('LOGIN_MENU_L33', "Afficher les nouveaux messages du Chatbox");
+define('LOGIN_MENU_L34', "Afficher les nouveaux commentaires");
+define('LOGIN_MENU_L35', "Afficher les nouveaux messages du Forum");
+define('LOGIN_MENU_L36', "Afficher les nouveaux membres");
 define('LOGIN_MENU_L37', "Mettre à jour les coordonnées");
 define('LOGIN_MENU_L38', "Coordonnées mises à jour");
 
