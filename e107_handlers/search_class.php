5236d157a36668581221d0f9606cbf2618a5a933
sweetas <sweetas@localhost>
2005-06-07 06:37:24 +0000

    stopwords updated
    svn path=/trunk/e107_0.7/; revision=3596

--- e107_handlers/search_class.php
+++ e107_handlers/search_class.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/search_class.php,v $
-|     $Revision: 1.27 $
-|     $Date: 2005-06-07 05:35:07 $
+|     $Revision: 1.28 $
+|     $Date: 2005-06-07 06:37:24 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
@@ -24,38 +24,8 @@ class e_search {
 	var $pos;
 	var $bullet;
 	var $keywords;
-	var $stop_keys;
-	// '||' = precedes php and mysql stopword / '|' precedes mysql only stopword
-	var $stopwords = "||a|a's|able||about|above|according|accordingly|across|actually|after|afterwards|again|against|ain't|all|allow|
-		allows|almost|alone|along|already|also|although|always|am|among|amongst||an||and|another|any|anybody|anyhow|anyone|anything|
-		anyway|anyways|anywhere|apart|appear|appreciate|appropriate||are|aren't|around||as|aside|ask|asking|associated||at|available|
-		away|awfully||be|became|because|become|becomes|becoming|been|before|beforehand|behind|being|believe|below|beside|besides|
-		best|better|between|beyond|both|brief|but||by|c'mon|c's|came|can|can't|cannot|cant|cause|causes|certain|certainly|changes|
-		clearly|co||com|come|comes|concerning|consequently|consider|considering|contain|containing|contains|corresponding|could|
-		couldn't|course|currently|definitely|described|despite|did|didn't|different|do|does|doesn't|doing|don't|done|down|downwards|
-		during|each||edu|eg|eight|either|else|elsewhere|enough|entirely|especially|et|etc|even|ever|every|everybody|everyone|
-		everything|everywhere|ex|exactly|example|except|far|few|fifth|first|five|followed|following|follows||for|former|formerly|
-		forth|four||from|further|furthermore|get|gets|getting|given|gives|go|goes|going|gone|got|gotten|greetings|had|hadn't|
-		happens|hardly|has|hasn't|have|haven't|having|he|he's|hello|help|hence|her|here|here's|hereafter|hereby|herein|hereupon|
-		hers|herself|hi|him|himself|his|hither|hopefully||how|howbeit|however||i||i'd||i'll||i'm||i've||ie||if||ignored||immediate||in||
-		inasmuch||inc||indeed||indicate||indicated||indicates||inner||insofar||instead||into||inward||is||isn't||it||it'd||it'll||it's||its||itself||
-		just||keep||keeps||kept||know||knows||known||last||lately||later||latter||latterly||least||less||lest||let||let's||like||liked||likely||
-		little||look||looking||looks||ltd||mainly||many||may||maybe||me||mean||meanwhile||merely||might||more||moreover||most||mostly||much||must||
-		my||myself||name||namely||nd||near||nearly||necessary||need||needs||neither||never||nevertheless||new||next||nine||no||nobody||non||none||
-		noone||nor||normally||not||nothing||novel||now||nowhere||obviously||of||off||often||oh||ok||okay||old||on||once||one||ones||only||onto||or||
-		other||others||otherwise||ought||our||ours||ourselves||out||outside||over||overall||own||particular||particularly||per||perhaps||placed||
-		please||plus||possible||presumably||probably||provides||que||quite||qv||rather||rd||re||really||reasonably||regarding||regardless||regards||
-		relatively||respectively||right||said||same||saw||say||saying||says||second||secondly||see||seeing||seem||seemed||seeming||seems||seen||self||
-		selves||sensible||sent||serious||seriously||seven||several||shall||she||should||shouldn't||since||six||so||some||somebody||somehow||someone||
-		something||sometime||sometimes||somewhat||somewhere||soon||sorry||specified||specify||specifying||still||sub||such||sup||sure||t's||take||
-		taken||tell||tends||th||than||thank||thanks||thanx||that||that's||thats||the||their||theirs||them||themselves||then||thence||there||there's||
-		thereafter||thereby||therefore||therein||theres||thereupon||these||they||they'd||they'll||they're||they've||think||third||this||thorough||
-		thoroughly||those||though||three||through||throughout||thru||thus||to||together||too||took||toward||towards||tried||tries||truly||try||trying||
-		twice||two||un||under||unfortunately||unless||unlikely||until||unto||up||upon||us||use||used||useful||uses||using||usually||value||various||
-		very||via||viz||vs||want||wants||was||wasn't||way||we||we'd||we'll||we're||we've||welcome||well||went||were||weren't||what||what's||whatever||
-		when||whence||whenever||where||where's||whereafter||whereas||whereby||wherein||whereupon||wherever||whether||which||while||whither||who||who's||
-		whoever||whole||whom||whose||why||will||willing||wish||with||within||without||won't||wonder||would||would||wouldn't||yes||yet||you||you'd||
-		you'll||you're||you've||your||yours||yourself||yourselves||zero|";
+	var $stopwords_php = "|a|about|an|and|are|as|at|be|by|com|edu|for|from|how|i|in|is|it|of|on|or|that|the|this|to|was|what|when|where|who|will|with|the|www|";
+	var $stopwords_mysql = "|a|a's|able|about|above|according|accordingly|across|actually|after|afterwards|again|against|ain't|all|allow|allows|almost|alone|along|already|also|although|always|am|among|amongst|an|and|another|any|anybody|anyhow|anyone|anything|anyway|anyways|anywhere|apart|appear|appreciate|appropriate|are|aren't|around|as|aside|ask|asking|associated|at|available|away|awfully|be|became|because|become|becomes|becoming|been|before|beforehand|behind|being|believe|below|beside|besides|best|better|between|beyond|both|brief|but|by|c'mon|c's|came|can|can't|cannot|cant|cause|causes|certain|certainly|changes|clearly|co|com|come|comes|concerning|consequently|consider|considering|contain|containing|contains|corresponding|could|couldn't|course|currently|definitely|described|despite|did|didn't|different|do|does|doesn't|doing|don't|done|down|downwards|during|each|edu|eg|eight|either|else|elsewhere|enough|entirely|especially|et|etc|even|ever|every|everybody|everyone|everything|everywhere|ex|exactly|example|except|far|few|fifth|first|five|followed|following|follows|for|former|formerly|forth|four|from|further|furthermore|get|gets|getting|given|gives|go|goes|going|gone|got|gotten|greetings|had|hadn't|happens|hardly|has|hasn't|have|haven't|having|he|he's|hello|help|hence|her|here|here's|hereafter|hereby|herein|hereupon|hers|herself|hi|him|himself|his|hither|hopefully|how|howbeit|however|i|i'd|i'll|i'm|i've|ie|if|ignored|immediate|in|inasmuch|inc|indeed|indicate|indicated|indicates|inner|insofar|instead|into|inward|is|isn't|it|it'd|it'll|it's|its|itself|just|keep|keeps|kept|know|knows|known|last|lately|later|latter|latterly|least|less|lest|let|let's|like|liked|likely|little|look|looking|looks|ltd|mainly|many|may|maybe|me|mean|meanwhile|merely|might|more|moreover|most|mostly|much|must|my|myself|name|namely|nd|near|nearly|necessary|need|needs|neither|never|nevertheless|new|next|nine|no|nobody|non|none|noone|nor|normally|not|nothing|novel|now|nowhere|obviously|of|off|often|oh|ok|okay|old|on|once|one|ones|only|onto|or|other|others|otherwise|ought|our|ours|ourselves|out|outside|over|overall|own|particular|particularly|per|perhaps|placed|please|plus|possible|presumably|probably|provides|que|quite|qv|rather|rd|re|really|reasonably|regarding|regardless|regards|relatively|respectively|right|said|same|saw|say|saying|says|second|secondly|see|seeing|seem|seemed|seeming|seems|seen|self|selves|sensible|sent|serious|seriously|seven|several|shall|she|should|shouldn't|since|six|so|some|somebody|somehow|someone|something|sometime|sometimes|somewhat|somewhere|soon|sorry|specified|specify|specifying|still|sub|such|sup|sure|t's|take|taken|tell|tends|th|than|thank|thanks|thanx|that|that's|thats|the|their|theirs|them|themselves|then|thence|there|there's|thereafter|thereby|therefore|therein|theres|thereupon|these|they|they'd|they'll|they're|they've|think|third|this|thorough|thoroughly|those|though|three|through|throughout|thru|thus|to|together|too|took|toward|towards|tried|tries|truly|try|trying|twice|two|un|under|unfortunately|unless|unlikely|until|unto|up|upon|us|use|used|useful|uses|using|usually|value|various|very|via|viz|vs|want|wants|was|wasn't|way|we|we'd|we'll|we're|we've|welcome|well|went|were|weren't|what|what's|whatever|when|whence|whenever|where|where's|whereafter|whereas|whereby|wherein|whereupon|wherever|whether|which|while|whither|who|who's|whoever|whole|whom|whose|why|will|willing|wish|with|within|without|won't|wonder|would|would|wouldn't|yes|yet|you|you'd|you'll|you're|you've|your|yours|yourself|yourselves|zero|";
 	
 	function e_search() {
 		global $query;
@@ -173,10 +143,8 @@ class e_search {
 			foreach ($search_fields as $field_key => $field) {
 				$crop_fields[] = preg_replace('/(.*?)\./', '', $field);
 			}
-			// print_r($crop_fields); echo '<br />';
 			while ($row = $sql -> db_Fetch()) {
 				$weight = 0;
-				// print_r($search_fields);
 				foreach ($crop_fields as $field_key => $field) {
 					$this -> text = $row[$field];
 					foreach ($this -> keywords['match'] as $k_key => $this -> query) {
@@ -307,12 +275,12 @@ class e_search {
 		global $search_prefs;
 		if (($key{(strlen($key) - 1)} != '*') && ($key{0} != '+')) {
 			if (strlen($key) > 2) {
-				if (!$search_prefs['mysql_sort']) {
-					$delimiter = '||';
+				if ($search_prefs['mysql_sort']) {
+					$stopword_list = $this -> stopwords_mysql;
 				} else {
-					$delimiter = '|';
+					$stopword_list = $this -> stopwords_php;
 				}
-				if (strpos($this -> stopwords, $delimiter.$key.'|') !== FALSE) {
+				if (strpos($stopword_list, '|'.$key.'|') !== FALSE) {
 					$this -> stop_keys[] = $key;
 					return TRUE;
 				} else {
