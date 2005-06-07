2e143dc3a2d135dad87b4cca022c3dad2cf9ef58
e107steved <steved@e107.org>
2005-06-07 19:50:59 +0000

    Added default emote pack if pref not created.
    svn path=/trunk/e107_0.7/; revision=3616

--- e107_handlers/emote.php
+++ e107_handlers/emote.php
@@ -11,16 +11,16 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/emote.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2005-06-03 13:24:02 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.11 $
+|     $Date: 2005-06-07 19:50:59 $
+|     $Author: stevedunstan $
 +----------------------------------------------------------------------------+
 */
 function r_emote()
 {
 	global $sysprefs, $pref;
+	if(!$pref['emotepack'])	$pref['emotepack'] = "default";
 	$emotes = $sysprefs->getArray("emote_".$pref['emotepack']);
-
 	foreach($emotes as $key => $value)
 	{
 		$key = str_replace("!", ".", $key);
