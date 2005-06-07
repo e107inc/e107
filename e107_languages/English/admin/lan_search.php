1d131ba2dcd5cd937b9a55d94c833c5707c4522e
sweetas <sweetas@localhost>
2005-06-07 05:35:07 +0000

    default search sort method switched to mysql if servers mysql version supports this. also added prevention of switching to this method when the server doesnt support it.
    svn path=/trunk/e107_0.7/; revision=3595

--- e107_languages/English/admin/lan_search.php
+++ e107_languages/English/admin/lan_search.php
@@ -4,20 +4,16 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_search.php,v $
-|     $Revision: 1.12 $
-|     $Date: 2005-05-06 13:52:21 $
+|     $Revision: 1.13 $
+|     $Date: 2005-06-07 05:35:07 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 define("SEALAN_1", "Search Configuration");
 define("SEALAN_2", "Number of characters displayed in search result summary:");
 define("SEALAN_3", "Search sort method:");
-// define("SEALAN_4", "Searchable areas:");
-// define("SEALAN_5", "News");
 define("SEALAN_6", "Comments");
 define("SEALAN_7", "Registered Members");
-// define("SEALAN_8", "Downloads");
-// define("SEALAN_9", "Number of results to display per page:");
 define("SEALAN_10", "Display relevance value:");
 define("SEALAN_11", "Allow user to select searchable areas:");
 define("SEALAN_12", "Restrict time allowed between searches (max 5 mins):");
@@ -37,9 +33,10 @@ define("SEALAN_25", "User Class");
 define("SEALAN_26", "Pre-Title Text");
 define("SEALAN_27", "Chars");
 define("SEALAN_28", "Results");
-// define("SEALAN_29", "Order");
 define("SEALAN_30", "Highlight keywords on referred too page:");
 define("SEALAN_31", "PHP limited to");
 define("SEALAN_32", "results (leave blank for no limit)");
+define("SEALAN_33", "Could not switch to MySql sort method as this requires at least version 4.0.1 of MySql.");
+define("SEALAN_34", "Your version is currently");
 
 ?>
