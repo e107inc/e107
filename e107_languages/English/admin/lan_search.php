0aa05cc1ee2be58ede71bcf73c8f865f75b78504
sweetas <sweetas@localhost>
2005-06-07 17:21:22 +0000

    drop down selection of search areas added
    svn path=/trunk/e107_0.7/; revision=3607

--- e107_languages/English/admin/lan_search.php
+++ e107_languages/English/admin/lan_search.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_search.php,v $
-|     $Revision: 1.13 $
-|     $Date: 2005-06-07 05:35:07 $
+|     $Revision: 1.14 $
+|     $Date: 2005-06-07 17:21:22 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
@@ -23,7 +23,6 @@ define("SEALAN_15", "Search page accessible to user class");
 define("SEALAN_16", "On");
 define("SEALAN_17", "Off");
 define("SEALAN_18", "Searchable Comments Areas (when comments search is activated)");
-define("SEALAN_19", "Allow users to search more than one area at a time:");
 define("SEALAN_20", "General Settings");
 define("SEALAN_21", "Searchable Areas");
 define("SEALAN_22", "Default");
@@ -38,5 +37,9 @@ define("SEALAN_31", "PHP limited to");
 define("SEALAN_32", "results (leave blank for no limit)");
 define("SEALAN_33", "Could not switch to MySql sort method as this requires at least version 4.0.1 of MySql.");
 define("SEALAN_34", "Your version is currently");
+define("SEALAN_35", "Searchable areas selection method:");
+define("SEALAN_36", "Dropdown");
+define("SEALAN_37", "Checkbox");
+define("SEALAN_38", "Radio");
 
 ?>
