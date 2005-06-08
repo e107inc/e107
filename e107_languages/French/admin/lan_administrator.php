25b984a8c03f12bbff4cef8b1deb1adbc09cbea2
tpruvot <tpruvot@localhost>
2005-06-08 16:38:52 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3650

---
+++ e107_languages/French/admin/lan_administrator.php
@@ -0,0 +1,77 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/French/admin/lan_administrator.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-08 16:38:52 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("ADMSLAN_0", "Nouvel utilisateur/admin créé pour");
+define("ADMSLAN_1", "a maintenant le statut d'administrateur.");
+define("ADMSLAN_2", "base de données mise à jour.");
+define("ADMSLAN_3", "est l'administrateur principal et ne peut pas être édité.");
+define("ADMSLAN_4", "Continuer");
+define("ADMSLAN_6", "est l'administrateur principal et ne peut pas être supprimé."); 
+
+define("ADMSLAN_13", "Administrateurs existants");
+
+
+define("ADMSLAN_16", "Compte Admin");
+define("ADMSLAN_17", "Mot de passe");
+define("ADMSLAN_18", "Permissions");
+define("ADMSLAN_19", "Modifier les Préférences du site"); 
+define("ADMSLAN_20", "Modifier les Menus");
+define("ADMSLAN_21", "Ajouter un administrateur");
+define("ADMSLAN_22", "Modérer les membres/bans ...");
+define("ADMSLAN_23", "Configurer le forum");
+define("ADMSLAN_24", "Définir les catégories de téléchargements");
+define("ADMSLAN_25", "Uploader/gérer le gestionnaire de fichiers");
+define("ADMSLAN_26", "Voir les catégories de nouvelles");
+define("ADMSLAN_27", "Voir les catégories de liens");
+define("ADMSLAN_28", "Désactiver le site pour effectuer une maintenance");
+define("ADMSLAN_29", "Configurer les bannières");
+define("ADMSLAN_30", "Configurer les flux de news externes");
+define("ADMSLAN_31", "Configurer les smileys");
+define("ADMSLAN_32", "Configurer la page d'accueil");
+define("ADMSLAN_33", "Activer/désactiver/configurer les statistiques");
+define("ADMSLAN_34", "Ajouter/modifier les méta-tags");
+define("ADMSLAN_35", "Configurer l'upload public de fichiers");
+define("ADMSLAN_36", "Modérer le forum");
+define("ADMSLAN_37", "Modérer les commentaires");
+define("ADMSLAN_38", "Modérer/configurer le Chatbox");
+define("ADMSLAN_39", "Ajouter des nouvelles");
+define("ADMSLAN_40", "Ajouter des liens");
+define("ADMSLAN_41", "Ajouter des articles");
+define("ADMSLAN_42", "Ajouter des revues");
+define("ADMSLAN_43", "Ajouter des pages de contenu");
+define("ADMSLAN_44", "Ajouter des fichiers à télécharger");
+define("ADMSLAN_45", "Ajouter des sondages");
+define("ADMSLAN_46", "Configurer les messages de bienvenue");
+define("ADMSLAN_47", "Modérer les nouvelles soumises");
+
+define("ADMSLAN_49", "Tout cocher");
+define("ADMSLAN_51", "Tout décocher");
+define("ADMSLAN_52", "Mettre à jour l'administrateur");
+define("ADMSLAN_53", "Ajouter un administrateur");
+define("ADMSLAN_54", "Administrateurs du site");
+
+define("ADMSLAN_55", "Un ou plusieurs champs ont été laissés vides");
+
+define("ADMSLAN_56", "Administrateur du Site");
+
+define("ADMSLAN_58", "Administrateur Principal du Site");
+define("ADMSLAN_59", "Retirer le statut d'administrateur");
+define("ADMSLAN_60", "Etes-vous sûr de vouloir retirer le statut d'administrateur de");
+define("ADMSLAN_61", "Administrateur supprimé");
+
+define("ADMSLAN_62", "Gestionnaire de Plugins");
+
+define("ADMSLAN_64", "Effacer le cache système");
+define("ADMSLAN_65", "Configurer les envois d'emails");
+define("ADMSLAN_66", "Configurer la recherche");
+define("ADMSLAN_67", "Vérifier avec l'inspecteur de fichiers");
+define("ADMSLAN_68", "Configurer les alertes par email");
+?>
