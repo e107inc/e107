6daee48bb1ba7e6c6c2ab2c246ec118bfda7e9f3
tpruvot <tpruvot@localhost>
2005-06-08 16:53:19 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3653

---
+++ e107_languages/French/admin/lan_article.php
@@ -0,0 +1,96 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/French/admin/lan_article.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-08 16:53:19 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("ARLAN_0", "L'article a été sauvegardé dans la base de données.");
+define("ARLAN_1", "Des champs du formulaire ont été laissés vides.");
+define("ARLAN_2", "L'article a été mis à jour dans la base de données.");
+define("ARLAN_3", "Catégorie ajoutée à la base de données.");
+define("ARLAN_4", "Catégorie mise à jour dans la base de données.");
+define("ARLAN_5", "Catégorie supprimée.");
+define("ARLAN_6", "Veuillez cocher cette case pour confirmer la suppression de l'article"); 
+define("ARLAN_7", "Articles");
+define("ARLAN_8", "Aucune catégorie pour le moment");
+define("ARLAN_9", "Catégories existantes");
+define("ARLAN_10", "Titre de la catégorie");
+define("ARLAN_11", "Description de la catégorie");
+define("ARLAN_12", "Catégorie");
+define("ARLAN_13", "Catégories");
+define("ARLAN_14", "Aucun Article");
+define("ARLAN_15", "Articles");
+define("ARLAN_16", "Aucune catégorie");
+define("ARLAN_17", "Titre");
+define("ARLAN_18", "Sous-titre");
+define("ARLAN_19", "Résumé");
+define("ARLAN_20", "Article");
+define("ARLAN_21", "Autoriser les commentaires ?");
+define("ARLAN_22", "Oui");
+define("ARLAN_23", "Non");
+define("ARLAN_24", "Ajouter les icônes email/impression ?");
+define("ARLAN_25", "Oui");
+define("ARLAN_26", "Non");
+define("ARLAN_27", "Nouvel aperçu");
+define("ARLAN_28", "Aperçu");
+define("ARLAN_29", "Cochez cette case pour confirmer la suppression de l'article");
+define("ARLAN_30", "Article supprimé.");
+define("ARLAN_31", "Premières lettres.");
+define("ARLAN_32", "Veuillez choisir une lettre ci-dessus.");
+
+define("ARLAN_55", "Visible par");
+define("ARLAN_56", "Catégorie enregistrée");
+define("ARLAN_57", "Catégorie mise à jour");
+define("ARLAN_58", "Catégorie supprimée");
+define("ARLAN_59", "Catégorie");
+define("ARLAN_60", "Options");
+define("ARLAN_61", "Modifier");
+define("ARLAN_62", "Supprimer");
+define("ARLAN_63", "Aucune catégorie d'article");
+define("ARLAN_64", "Catégories d'articles existantes");
+define("ARLAN_65", "Nom de la catégorie");
+define("ARLAN_66", "Icône de la catégorie");
+define("ARLAN_67", "Voir les icônes");
+define("ARLAN_68", "Description de la catégorie");
+define("ARLAN_69", "Mettre à jour");
+define("ARLAN_70", "Effacer tout");
+define("ARLAN_71", "Créer une catégorie d'article");
+define("ARLAN_72", "Articles existants");
+define("ARLAN_73", "Ouvrir l'éditeur HTML");
+define("ARLAN_74", "Catégorie");
+define("ARLAN_75", "Aucune");
+define("ARLAN_76", "Page principale des articles");
+define("ARLAN_77", "Créer un nouvel article");
+define("ARLAN_78", "Catégories");
+define("ARLAN_79", "Options");
+define("ARLAN_80", "Etes-vous sûr de vouloir supprimer cette catégorie ?");
+define("ARLAN_81", "Etes-vous sûr de vouloir supprimer cet article ?");
+define("ARLAN_82", "Détails sur l'auteur");
+define("ARLAN_83", "Ne pas modifier si vous êtes l'auteur de cet article");
+define("ARLAN_84", "Auteur");
+define("ARLAN_85", "Email");
+
+define("ARLAN_86", "Autoriser la proposition d'article");
+define("ARLAN_87", "Autoriser les visiteurs anonymes à proposer des articles");
+define("ARLAN_88", "Groupe autorisé");
+define("ARLAN_89", "Sélectionner les utilisateurs qui peuvent envoyer des articles");
+define("ARLAN_90", "Mettre à jour les options");
+define("ARLAN_91", "Options de l'article");
+define("ARLAN_92", "Les options ont été mises à jour");
+define("ARLAN_93", "Articles en attente de validation");
+define("ARLAN_94", "Pas d'article en attente");
+define("ARLAN_95", "Pas d'adresse email fournie");
+define("ARLAN_96", "Titre de l'Article");
+define("ARLAN_97", "Valider");
+define("ARLAN_98", "Valider l'article proposé par l'utilisateur");
+define("ARLAN_99", "L'article proposé par l'utilisateur a été sauvegardé dans la base de données");
+define("ARLAN_100", "Cliquez ici pour remplir les champs de l'auteur");
+
+define("ARLAN_101", "par");
+
+?>
