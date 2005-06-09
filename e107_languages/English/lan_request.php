a8bbe8c4cc108c912e23da96ec1d6318ef5ab6de
e107coders <e107coders@localhost>
2005-06-09 15:38:12 +0000

    fixed a small typo
    svn path=/trunk/e107_0.7/; revision=3687

--- e107_languages/English/lan_request.php
+++ e107_languages/English/lan_request.php
@@ -4,14 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_request.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-03-10 11:12:35 $
-|     $Author: stevedunstan $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-09 15:38:12 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
 define("LAN_dl_61", "Download Error");
-define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded you download quota");
+define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded your download quota");
 define("LAN_dl_63", "You do not have the correct permissions to download this file.");
 define("LAN_dl_64", "Back");
 define("LAN_dl_65", "File Not Found");
