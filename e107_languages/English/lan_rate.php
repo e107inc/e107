64c72770893be82b703e1fb40530482fc6729349
lisa_ <lisa_@localhost>
2005-06-10 10:21:43 +0000

    lan rate : added lan_rate.php language file
    svn path=/trunk/e107_0.7/; revision=3712

---
+++ e107_languages/English/lan_rate.php
@@ -0,0 +1,23 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_rate.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-10 10:21:43 $
+|     $Author: lisa_ $
++----------------------------------------------------------------------------+
+*/
+
+define("PAGE_NAME", "Rating");
+
+define("RATELAN_0", "vote");
+define("RATELAN_1", "votes");
+define("RATELAN_2", "how do you rate this item?");
+define("RATELAN_3", "thank you for your vote");
+define("RATELAN_4", "not rated");
+
+
+
+?>
