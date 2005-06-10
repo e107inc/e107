6265e8488e33b136235c21485365c3095feef5c1
natxocc <natxocc@localhost>
2005-06-10 19:47:12 +0000

    Some grammar corrections
    svn path=/trunk/e107_langpacks/; revision=3732

--- e107_languages/Spanish/admin/lan_links.php
+++ e107_languages/Spanish/admin/lan_links.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Spanish/admin/lan_links.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-06-03 22:17:40 $
+|     $Revision: 1.5 $
+|     $Date: 2005-06-10 19:47:12 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
@@ -14,7 +14,7 @@ define("LCLAN_2", "Enlace añadido a base de datos.");
 define("LCLAN_3", "Enlace actualizado en base de datos.");
 //define("LCLAN_4", "Enlace borrado.");
 
-define("LCLAN_6", "Orden actualizada.");
+define("LCLAN_6", "Orden actualizado.");
 
 define("LCLAN_8", "Enlaces existentes");
 
@@ -23,19 +23,19 @@ define("LCLAN_12", "Tipo de renderizado del enlace");
 define("LCLAN_15", "Nombre de enlace");
 define("LCLAN_16", "URL del enlace");
 define("LCLAN_17", "Descripción del enlace");
-define("LCLAN_18", "Botón de enlace");
+define("LCLAN_18", "Botón del enlace");
 define("LCLAN_19", "Abrir enlace como");
-define("LCLAN_20", "abrir en la misma ventana");
+define("LCLAN_20", "Abrir en la misma ventana");
 
-define("LCLAN_23", "abrir en una nueva ventana");
-define("LCLAN_24", "abrir en miniventana (600x400)");
-define("LCLAN_25", "Clase de enlace");
+define("LCLAN_23", "Abrir en una nueva ventana");
+define("LCLAN_24", "Abrir en miniventana (600x400)");
+define("LCLAN_25", "Clase del enlace");
 define("LCLAN_26", "Hacer que el enlace sea visible solo para usuarios en esa clase");
 define("LCLAN_27", "Actualizar enlace");
 define("LCLAN_28", "Añadir enlace");
 define("LCLAN_29", "Enlaces");
-define("LCLAN_30", "mover arriba");
-define("LCLAN_31", "mover abajo");
+define("LCLAN_30", "Mover arriba");
+define("LCLAN_31", "Mover abajo");
 
 define("LCLAN_39", "Ver imágenes");
 
@@ -46,16 +46,16 @@ define("LCLAN_58", "¿Está seguro de querer borrar este enlace?");
 
 define("LCLAN_61", "No hay enlaces");
 define("LCLAN_62", "Página de inicio de enlaces");
-define("LCLAN_63", "Crear Nuevo Enlace");
+define("LCLAN_63", "Crear nuevo enlace");
 
 define("LCLAN_68", "Opciones de enlaces");
 
 define("LCLAN_78", "Mostrar Descripción como Screen-Tip");
 define("LCLAN_79", "Se mostrará la descripción cuando el cursor del ratón pase por encima del enlace");
 define("LCLAN_80", "Activar submenús expandidos");
-define("LCLAN_81", "Los submenús se mostrarán después de hacer click en su padre. (Desactivado enlace Padre)");
+define("LCLAN_81", "Los submenús se mostrarán después de hacer click en su raíz. (Desactivado enlace Raíz)");
 define("LCLAN_82", "Abrir en ventana 800x600");
-define("LCLAN_83", "Generador de Submenus");
+define("LCLAN_83", "Generador de Submenús");
 
 define("LCLAN_88", "Opciones de los enlaces");
 define("LCLAN_89", "Imagen");
@@ -65,6 +65,6 @@ define("LCLAN_91", "Mover");
 define("LCLAN_95", "Clase");
 
 define("LINKLAN_1", "Abrir en ventana 800x600");
-define("LINKLAN_2", "Padre");
-define("LINKLAN_3", "Sin padre (enlace normal)");
+define("LINKLAN_2", "Raíz");
+define("LINKLAN_3", "Sin raíz (nueva raíz)");
 ?>
