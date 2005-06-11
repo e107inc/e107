ac8edc2045d2319d6b8ed1626176b0119580b36a
streaky <streaky@localhost>
2005-06-11 22:22:22 +0000

    Fixed an issue with sitedown.
    svn path=/trunk/e107_0.7/; revision=3763

--- sitedown.php
+++ sitedown.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/sitedown.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-04-09 03:57:28 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.5 $
+|     $Date: 2005-06-11 22:22:22 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
@@ -21,25 +21,18 @@ require_once("class2.php");
 global $pref;
 global $tp;
 
-if($pref['maintainance_text'])
-{
+if($pref['maintainance_text']) {
 	$SITEDOWN_TABLE_MAINTAINANCETEXT = $tp->toHTML($pref['maintainance_text'], TRUE, 'parse_sc', 'admin');
-}
-else
-{
+} else {
 	$SITEDOWN_TABLE_MAINTAINANCETEXT = "<b>- ".SITENAME." ".LAN_00." -</b><br /><br />".LAN_01 ;
 }
 
 $SITEDOWN_TABLE_PAGENAME = PAGE_NAME;
 	
-if (!$SITEDOWN_TABLE)
-{
-	if (file_exists(THEME."sitedown_template.php"))
-	{
+if (!$SITEDOWN_TABLE) {
+	if (file_exists(THEME."sitedown_template.php")) {
 		require_once(THEME."sitedown_template.php");
-	}
-	else
-	{
+	} else {
 		require_once(e_THEME."templates/sitedown_template.php");
 	}
 }
