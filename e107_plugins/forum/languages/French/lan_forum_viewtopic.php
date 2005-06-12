e24fef8b29537941bdf0b58446f04aabd69ca67a
tpruvot <tpruvot@localhost>
2005-06-12 01:12:34 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3777

---
+++ e107_plugins/forum/languages/French/lan_forum_viewtopic.php
@@ -0,0 +1,88 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_viewtopic.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 01:12:34 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "Forum");
+
+define("LAN_01", "Forums");
+define("LAN_02", "Aller à la page");
+define("LAN_03", "Go");
+define("LAN_04", "Précédent");
+define("LAN_05", "Suivant");
+define("LAN_06", "Inscrit le");
+define("LAN_07", "Localisation");
+define("LAN_08", "Site Internet");
+define("LAN_09", "Visites depuis son inscription");
+define("LAN_10", "Revenir en haut");
+define("LAN_65", "Aller à");
+define("LAN_66", "Ce sujet est désormais fermé");
+define("LAN_67", "messages");
+define("LAN_194", "Invité");
+define("LAN_195", "Membre enregistré");
+define("LAN_321", "Modérateurs: ");
+define("LAN_389", "Sujet précédent");
+define("LAN_390", "Sujet suivant");
+define("LAN_391", "Suivre ce sujet");
+define("LAN_392", "Arrêter le suivi de ce sujet");
+define("LAN_393", "Réponse rapide");
+define("LAN_394", "Prévisualiser");
+define("LAN_395", "Répondre au Sujet");
+define("LAN_396", "Site Internet");
+define("LAN_397", "Email");
+define("LAN_398", "Profil");
+define("LAN_399", "Message Privé");
+define("LAN_400", "Editer");
+define("LAN_401", "Citer");
+
+define("LAN_402", "Auteur");
+define("LAN_403", "Message");
+define("LAN_404", "Pas de sujet avant");
+define("LAN_405", "Pas de sujet après");
+
+define("LAN_406", "Modérateur: Modifier");
+define("LAN_407", "Modérateur: Supprimer");
+define("LAN_408", "Modérateur: Déplacer");
+define("LAN_409", "Etes-vous sûr de vouloir supprimer ce sujet et ses réponses ?");
+define("LAN_410", "Etes-vous sûr de vouloir supprimer cette réponse ?");
+define("LAN_411", "posté par ");
+
+//v.616
+define("LAN_412", "Titre");
+define("LAN_413", "Signaler");
+define("LAN_414", "Signaler ce sujet à un modérateur");
+define("LAN_415", "Titre du sujet");
+define("LAN_416", "Entrez votre rapport");
+define("LAN_417", "L'administrateur va être mis au courant de ce sujet. Vous devez joindre un message pour expliquer ce que vous y trouvez de répréhensible.");
+define("LAN_418", "<b>Ne pas</b> utiliser ce formulaire afin de contacter un administrateur pour une autre raison.");
+define("LAN_419", "Envoyer le rapport");
+define("LAN_420", "Cliquez pour voir le message");
+define("LAN_421", "Rapport du sujet du forum par");
+define("LAN_422", "Ce message a été signalé sur le site ");
+define("LAN_423", "Le message ne peut être envoyé. ");
+define("LAN_424", "Votre rapport a été transmis à un modérateur.<br />Merci.");
+define("LAN_425", "Message de: ");
+define("LAN_426", "Rapport du message du sujet: ");
+define("LAN_427", "Erreur lors de l'envoi du courier");
+define("LAN_428", "Le message a été signalé");
+define("LAN_429", "Cliquez ici pour retourner sur le forum");
+define("LAN_430", "sondage");
+define("FORLAN_26", "Réponse supprimée");
+define("FORLAN_10", "Commencer un Nouveau Sujet");
+define("LAN_29", "Modifié");
+
+define("LAN_431", "Flux RSS 0.92 du sujet");
+define("LAN_432", "Flux RSS 2.0 du sujet");
+define("LAN_433", "Flux RDF du sujet");
+	
+define("FORLAN_101", "Sujet Email");
+define("FORLAN_102", "Vue impression");
+define('FORLAN_103', '[membre supprimé]');
+
+?>
