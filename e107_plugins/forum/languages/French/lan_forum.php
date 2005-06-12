72cfab149b13b1adcc1279f911e818172bd1fb3c
tpruvot <tpruvot@localhost>
2005-06-12 00:13:18 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3770

---
+++ e107_plugins/forum/languages/French/lan_forum.php
@@ -0,0 +1,115 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:13:18 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("e_PAGETITLE", "Forum");
+
+define("LAN_30", "Bienvenue");
+define("LAN_31", "Pas de nouveaux messages ");
+define("LAN_32", "Il y a 1 nouveau message ");
+define("LAN_33", "Il y a");
+define("LAN_34", "nouveaux messages");
+define("LAN_35", "depuis votre dernière visite.");
+define("LAN_36", "Votre dernière visite date du ");
+define("LAN_37", "Maintenant, il est ");
+define("LAN_38", ", les heures sont ");
+define("LAN_41", "Dernier membre: ");
+define("LAN_42", "Membres enregistrés: ");
+define("LAN_44", "Ces forums peuvent être visités par des utilisateurs non-enregistrés, mais votre adresse IP sera enregistrée lorsque vous envoyez un message.<br />Pour accéder à toutes les fonctionnalités du forum vous devez vous");
+define("LAN_45", "Ces forums sont réservés uniquement aux membres enregistrés, merci de cliquer");
+define("LAN_46", "Forum");
+define("LAN_47", "Sujets");
+define("LAN_48", "Réponses");
+define("LAN_49", "Dernier message");
+define("LAN_51", "Aucun forum pour le moment. Revenez plus tard.");
+define("LAN_52", "Aucun forum dans cette catégorie pour le moment. Revenez plus tard.");
+define("LAN_79", "Nouveaux messages");
+define("LAN_80", " Pas de nouveaux messages");
+define("LAN_81", "Sujet verouillé");
+define("LAN_100", "articles");
+define("LAN_180", "Rechercher");
+define("LAN_191", "Information");
+define("LAN_192", "Les utilisateurs de ce forum ont fait un total de ");
+define("LAN_196", "Vous avez lu ");
+define("LAN_197", " de ces messages.");
+define("LAN_198", " Tous les nouveaux messages ont été lus.");
+define("LAN_199", "Marquer tous les messages comme lu");
+define("LAN_204", "Vous <b>pouvez</b> poster un nouveau sujet");
+define("LAN_205", "Vous <b>ne pouvez pas</b> poster un nouveau sujet");
+define("LAN_206", "Vous <b>pouvez</b> répondre");
+define("LAN_207", "Vous <b>ne pouvez pas</b> répondre");
+define("LAN_208", "Vous <b>pouvez</b> éditer vos messages");
+define("LAN_209", "Vous <b>ne pouvez pas</b> éditer vos messages");
+define("LAN_392", "stopper le suivi de ce sujet");
+define("LAN_393", "Liste des sujets suivis");
+define("LAN_394", "Forum fermé");
+define("LAN_397", "Suivre ce sujet");
+define("LAN_398", "Fermé");
+define("LAN_399", "Restreint");
+define("LAN_400", "Ce forum ne peut être visité que par des membres enregistrés");
+define("LAN_401", "Membres seulement");
+
+define("LAN_402", "Ce forum est en lecture seule");
+
+define("LAN_403", "Pas de messages pour le moment");
+define("LAN_404", "messages");
+define("LAN_405", "Lecture seule");
+
+define("LAN_406", "Ce forum est restreint aux administrateurs");
+define("LAN_407", "Ce forum est restreint aux membres enregistrés");
+define("LAN_408", "Ce forum est en lecture seule");
+define("LAN_409", "Ce forum est restreint à un groupe");
+define("LAN_410", "Bienvenue invité");
+	
+define("LAN_411", "sujet");
+define("LAN_412", "réponse");
+define("LAN_413", "sujets");
+define("LAN_414", "réponses");
+
+define("LAN_415", " utilisateur lit ce forum en ce moment");
+define("LAN_416", " utilisateurs lisent ce forum en ce moment");
+
+define("LAN_417", "membre");
+define("LAN_418", "invité");
+define("LAN_419", "membres");
+define("LAN_420", "invités");
+
+define("LAN_421", "Voir les nouveaux messages");
+define("LAN_422", "Nouveaux messages depuis votre dernière visite");
+define("LAN_423", "Posté par");
+define("LAN_424", "Nouveaux sujets");
+define("LAN_425", "Re:");
+
+//v.616
+define("LAN_426", "Qui est en ligne: ");
+define("LAN_427", "Voir la liste détaillée.");
+define("LAN_428", "Re:");
+define("LAN_429", "Meilleurs posteurs");
+define("LAN_430", "Sujets les plus actifs");
+define("LAN_431", "Mes messages");
+define("LAN_432", "Mes Préférences");
+define("LAN_433", "Règles du forum");
+define("LAN_434", "Retourner sur le forum");
+define("LAN_435", "Mon Profil");
+define("LAN_436", " (S'ouvrira dans une nouvelle fenêtre.)");
+
+define("LAN_437", "enregistrer");
+define("LAN_438", "et s'identifier.");
+define("LAN_439", "ici");
+define("LAN_440", "pour aller à la page d'inscription.");	
+	
+define("LAN_441", "Statistiques");
+	
+define('FORLAN_441', 'Aucune règle définie.');
+define('FORLAN_442', 'Mes Envois');
+define('FORLAN_443', '[Membre Supprimé]');
+define('FORLAN_444', 'sous-forums');
+
+?>
