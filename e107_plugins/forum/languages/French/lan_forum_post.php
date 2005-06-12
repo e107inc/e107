5adc90266ee12e2c9ea5aa6a9d355505018b2439
tpruvot <tpruvot@localhost>
2005-06-12 01:54:51 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3780

---
+++ e107_plugins/forum/languages/French/lan_forum_post.php
@@ -0,0 +1,114 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_post.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 01:54:51 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "Forum");
+
+define("LAN_01", "Forums");
+define("LAN_02", "Répondre à: ");
+define("LAN_03", "Nouveau Sujet");
+define("LAN_1", "Normal");
+define("LAN_2", "Post-it");
+define("LAN_3", "Annonce");
+define("LAN_4", "Poster un Sondage");
+define("LAN_5", "Question du sondage:");
+define("LAN_6", "Ajouter une proposition");
+define("LAN_7", "Option de vote:");
+define("LAN_8", "Autoriser tout le monde à voter");
+define("LAN_9", "Autoriser seulement les membres à voter");
+define("LAN_10", "Identification");
+define("LAN_11", "Connexion automatique");
+define("LAN_16", "Nom d'utilisateur: ");
+define("LAN_17", "Mot de passe: ");
+define("LAN_20", "Erreur");
+define("LAN_27", "Vous avez laissé vide un champ obligatoire");
+define("LAN_28", "Vous n'avez rien envoyé ...");
+define("LAN_29", "Modifié");
+define("LAN_45", "Ces forums sont réservés aux membres enregistrés et connectés");
+define("LAN_60", "Commencer un nouveau sujet");
+define("LAN_61", "Votre nom: ");
+define("LAN_62", "Sujet: ");
+define("LAN_63", "Message: ");
+define("LAN_64", "Envoyer le nouveau sujet");
+define("LAN_73", "Répondre: ");
+define("LAN_74", "Répondre au sujet");
+define("LAN_77", "Mettre à jour le sujet");
+define("LAN_78", "Mettre à jour la réponse");
+define("LAN_94", "Ecrit par");
+define("LAN_95", "Non autorisé");
+define("LAN_96", "Vous n'êtes pas autorisé à modifier ce message.");
+define("LAN_100", "Titre du sujet");
+define("LAN_101", "Dernier ");
+define("LAN_102", " réponses");
+define("LAN_103", "Revoir le fil complet de la discussion. (dans une nouvelle fenêtre)");
+define("LAN_133", "Merci");
+define("LAN_174", "Enregistrement");
+define("LAN_175", "Connexion");
+define("LAN_212", "Mot de passe oublié ?");
+define("LAN_310", "Impossible d'accepter ce message car ce nom d'utilisateur appartient à un membre - si c'est votre nom d'utilisateur, merci de vous connecter.");
+define("LAN_311", "Anonyme");
+define("LAN_322", "Envoyé: ");
+define("LAN_323", "Prévisualiser");
+define("LAN_324", "Votre message a été publié.");
+define("LAN_325", "Cliquez ici pour voir votre message");
+define("LAN_326", "Cliquez ici pour retourner à la liste des sujets du forum");
+define("LAN_327", "Revue");
+define("LAN_380", "Si vous voulez être averti lorsqu'une réponse est postée dans votre sujet, cochez cette case ");
+define("LAN_381", "Réponse de ");
+define("LAN_382", "Message écrit le: ");
+define("LAN_383", "Cliquez sur le lien suivant pour voir le sujet en entier ...");
+define("LAN_384", "Réponse le ");
+define("LAN_385", "Message: ");
+define("LAN_386", "Si vous ne souhaitez pas ajouter de sondage à votre sujet, laissez les champs vides ");
+define("LAN_387", "Go");
+define("LAN_388", "Revenir en haut");
+define("LAN_389", "Message dupliqué, redirection ...");
+define("LAN_390", "Joindre un fichier / une image");
+define("LAN_391", "Option");
+define("LAN_392", "Fichier à joindre");
+define("LAN_393", "<b>Veuillez noter</b><br />Types de fichiers autorisés:");
+define("LAN_394", "Tout autre fichier envoyé sera supprimé instantanément.");
+define("LAN_395", "Taille maximum du fichier");
+define("LAN_396", "octets");
+define("LAN_397", "Ce sujet est vérouillé.");
+define("LAN_398", "Ce forum est en lecture seule");
+define("LAN_399", "Vous n'êtes pas autorisé à poster dans ce forum.");
+define("LAN_400", "poster le sujet comme");
+define("LAN_401", "Aller au forum");
+
+define("LAN_402", "sondage");
+define("LAN_403", "annonce");
+define("LAN_404", "post-it");
+define("LAN_405", "Forums");
+define("LAN_406", "Re:");
+
+//v.616
+define("LAN_407", "Redirection");
+define("LAN_408", "Si votre navigateur ne supporte pas la redirection, veuillez cliquer");
+define("LAN_409", "ICI");
+define("LAN_410", "pour être redirigé");
+define("LAN_411", "ici");
+define("LAN_412", "pour aller à la page d'inscription.");
+
+define("LAN_413", "Votre sondage a été publié.");
+define("LAN_414", "Cliquez ici pour voir votre sondage");
+define("LAN_415", "Votre réponse a été envoyée.");
+
+define("LAN_416", "Joindre un fichier");
+define("LAN_417", "Ajouter un autre fichier");
+
+define("POLL_506", "Autoriser les choix multiples?");
+define("POLL_507", "oui");
+define("POLL_508", "non");
+
+define("LAN_FORUM_1", "Envoi de fichier désactivé: le répertoire ".e_FILE."public n'est pas accessible en écriture");
+define("LAN_FORUM_2", "Dupliquer le message");
+
+?>
