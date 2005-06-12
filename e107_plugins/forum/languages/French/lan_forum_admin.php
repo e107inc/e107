346efa6e609590016fd5d3ce9a4451a5c45d8e46
tpruvot <tpruvot@localhost>
2005-06-12 16:08:32 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3786

---
+++ e107_plugins/forum/languages/French/lan_forum_admin.php
@@ -0,0 +1,187 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_admin.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 16:08:32 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("FORLAN_1", "Vous devez entrer le nombre de jours que vous voulez délester.");
+define("FORLAN_2", "Veuillez choisir si vous voulez supprimer complétement les messages déletés, ou seulement les rendre inactifs (invisibles dans les forums)");
+define("FORLAN_3", "supprimer");
+define("FORLAN_4", "Rendre inactif");
+define("FORLAN_5", "Délestage");
+define("FORLAN_6", "Annuler");
+define("FORLAN_7", "Options du Forum");
+define("FORLAN_8", "Forums délestés.");
+define("FORLAN_9", "Prune non nécessaire.");
+define("FORLAN_10", "Options Enregistrées");
+define("FORLAN_11", "Forum créé");
+define("FORLAN_12", "Forum mis à jour");
+define("FORLAN_13", "Catégorie créée");
+define("FORLAN_14", "Catégorie de forum mise à jour");
+define("FORLAN_15", "Veuillez cocher cette case pour confirmer la suppression du forum");
+define("FORLAN_16", "Forums");
+define("FORLAN_17", "Pas de catégories pour le moment");
+define("FORLAN_18", "Catégories existantes");
+define("FORLAN_19", "Modifier");
+define("FORLAN_20", "Supprimer");
+define("FORLAN_21", "cochez pour confirmer");
+define("FORLAN_22", "Catégorie");
+define("FORLAN_23", "Visible pour");
+define("FORLAN_24", "Indique qui peut lire ce forum");
+define("FORLAN_25", "Modifier la Catégorie");
+define("FORLAN_26", "Créer la Catégorie");
+define("FORLAN_27", "Vous devez définir au moins une catégorie avant de créer un forum.");
+define("FORLAN_28", "Forums");
+define("FORLAN_29", "Pas de forums pour le moment.");
+define("FORLAN_30", "Forums Existants");
+define("FORLAN_31", "Nom");
+define("FORLAN_32", "Description");
+define("FORLAN_33", "Modérateurs");
+define("FORLAN_34", "Sélectionner le groupe modérateur pour ce forum");
+define("FORLAN_35", "Modifier le Forum");
+define("FORLAN_36", "Créer le Forum");
+define("FORLAN_37", "Ordre");
+define("FORLAN_38", "Vérouillé");
+define("FORLAN_39", "Membres Uniquement");
+define("FORLAN_40", "Restreint");
+define("FORLAN_41", "remonter");
+define("FORLAN_42", "descendre");
+define("FORLAN_43", "Prévisualiser / Ordre des Forum");
+define("FORLAN_44", "Inclure dans une table");
+define("FORLAN_45", "Cochez cette case si vous souhaitez afficher le forum dans une table utilisant votre thème");
+define("FORLAN_46", "Titre affiché en légende si 'Inclure dans une table' est coché");
+define("FORLAN_47", "Activer la notification par email");
+define("FORLAN_48", "Cochez cette case pour permettre à vos membres d'avoir le choix de la réception d'email lorsque quelqu'un répond à l'un de ses messages");
+define("FORLAN_49", "Activer les sondages");
+define("FORLAN_50", "Cochez cette case pour permettre à vos membres de créer des sondages dans les forums - le plugin Sondages doit être installé pour utiliser cette fonctionalité");
+define("FORLAN_51", "Activer le suivi");
+define("FORLAN_52", "Cochez cette case pour permettre à vos membres de suivre les sujets et d'être contacté par mail quand une réponse est postée à un sujet");
+define("FORLAN_53", "Préfixe Email");
+define("FORLAN_54", "Le texte entré précédera le sujet de tous les emails envoyés par le forum");
+define("FORLAN_55", "Indice de Sujet Populaire");
+define("FORLAN_56", "Nombre de messages nécessaires à un sujet pour est marqué comme populaire");
+define("FORLAN_57", "Messages par page");
+define("FORLAN_58", "Nombre de messages affichés sur une page");
+define("FORLAN_59", "Délestage");
+define("FORLAN_60", "Cela supprimera tous les sujets qui n'ont pas reçus de réponse pendant le nombre de jours spécifié. <br /><b>Faîtes attention en utilisant cette fonction!</b>");
+define("FORLAN_61", "Options de mise à jour");
+define("FORLAN_62", "Options du Forum");
+define("FORLAN_63", "Rangs");
+define("FORLAN_64", "Entrez les niveaux ici, laissez à blanc pour utiliser les étoiles génériques. Séperez les niveaux par des virgules. Maximum de 10 niveaux, le plus bas en premier.");
+define("FORLAN_65", "Titre du Forum");
+	
+define("FORLAN_70", "Activer les fichiers / images jointes");
+define("FORLAN_71", "Autoriser les utilisateurs à un fichier ou une image afin de les inclure dans leurs messages,");
+define("FORLAN_72", "Modifier l'ordre");
+define("FORLAN_73", "Ordre mis à jour");
+	
+define("FORLAN_75", "Catégories");
+define("FORLAN_76", "Page d'acceuil du Forum");
+define("FORLAN_77", "Création de Forums");
+define("FORLAN_78", "Ordre du Forum");
+define("FORLAN_79", "Préférences");
+define("FORLAN_80", "Options");
+define("FORLAN_81", "Etes-vous sûr de vouloir supprimer cette catégorie? - Les FORUMS de cette catégorie seront AUSSI supprimé");
+define("FORLAN_82", "Etes-vous sûr de vouloir supprimer ce forum?");
+define("FORLAN_83", "Création de Catégories");
+define("FORLAN_84", "Membres Seulement");
+define("FORLAN_85", "Lecture seule");
+define("FORLAN_86", "Admin Seulement");
+define("FORLAN_87", "Délester les sujets sans réponse depuis ce nombre de jours:");
+	
+define("FORLAN_88", "Délester les sujets sans réponse depuis ce nombre de jours:");
+	
+define("FORLAN_89", "Supprimer complétement les messages");
+define("FORLAN_90", "Désactiver les messages");
+	
+define("FORLAN_91", "Messages inactifs");
+define("FORLAN_92", "sujet(s) supprimé(s)");
+define("FORLAN_93", "réponse(s) supprimé(es)");
+	
+define("FORLAN_94", "Définition des Rangs");
+define("FORLAN_95", "Rangs enregistrés");
+define("FORLAN_96", "Forum supprimé");
+define("FORLAN_97", "Catégorie supprimée");
+	
+define("FORLAN_98", "Nom du Rang");
+define("FORLAN_99", "nombre de points avant le changement de niveau");
+define("FORLAN_100", "envoyez les images vers e107_themes/Your_Theme/forum/");
+define("FORLAN_101", "Admin principal du site");
+define("FORLAN_102", "Indice");
+define("FORLAN_103", "Admin du site");
+define("FORLAN_104", "Image du Rang");
+define("FORLAN_105", "Modérateur du forum");
+	
+	
+define("FORLAN_106", "Type de délestage:");
+define("FORLAN_107", "Forum");
+define("FORLAN_108", " supprimé");
+define("FORLAN_109", "jours:");
+define("FORLAN_110", "Délester");
+define("FORLAN_111", "désactiver");
+	
+define("FORLAN_112", "Activer la Redirection");
+define("FORLAN_113", "Cochez cela pour rediriger vers la page du forum après une réponse");
+define("FORLAN_114", "Titre perso. du membre");
+define("FORLAN_115", "Cochez cela pour permettre aux membres de changer leur titre perso.");
+define("FORLAN_116", "Messages signalés");
+define("FORLAN_117", "Cela supprimera l'enregistrement du rapport de message. Pas le message lui-même.");
+define("FORLAN_118", "Message signalé supprimé");
+define("FORLAN_119", "Les liens ouvriront le forum dans une nouvelle fenêtre.");
+define("FORLAN_120", "Cochez cela pour permettre aux membres de changer leur titre perso");
+define("FORLAN_121", "Pas de messages signalés");
+define("FORLAN_122", "Cliquez ici pour envoyer un email à l'admin quand quelqu'un signale un message sur le forum");
+define("FORLAN_123", "Règles du Forum");
+define("WMGLAN_1", "Règles pour les Invités");
+define("WMGLAN_2", "Règles pour les Membres");
+define("WMGLAN_3", "Règles pour les Administrateurs");
+define("WMGLAN_4", "Envoyer");
+define("WMGLAN_5", "Définir les Règles du Forum");
+define("WMGLAN_6", "Activer?");
+define("FORLAN_124", "Liens dans nouvelle fenêtre");
+define("FORLAN_125", "Cochez cela pour ouvrir tous les liens dans nouvelle fenêtre (<i>ce sera appliqué à tout le site</i>). ");
+	
+//v.617
+define("FORLAN_126", "Afficher les info-bulles");
+define("FORLAN_127", "Cochez cela pour afficher une info-bulle contenant le premier message du sujet quand la souris passe au dessus du titre des Sujets. ");
+define("FORLAN_128", "Longueur des info-bulles");
+define("FORLAN_129", "Cela détermine le nombre de caractères à afficher dans les info-bulles. ");
+define("FORLAN_130", "cliquez ici");
+define("FORLAN_131", "pour définir la taille maxi des fichiers, les types autorisés etc");
+define("FORLAN_132", "Souligner les sujets Post-It");
+define("FORLAN_133", "Ajoute un soulignement supplémentaire aux sujets Post-it (sépare les sections du sujet des entêtes)");
+	
+define("FORLAN_134", "Largeur maximum des images envoyées");
+define("FORLAN_135", "Laisser à blanc pour désactiver le redimensionnement automatique des images");
+	
+define("FORLAN_136", "Créer un lien pour voir l'image en taille réelle");
+define("FORLAN_137", "Activer cela pour redimensionnement les images trop grandes et pour créer le lien vers l'image originale. Si vous désactivez cela, l'image originale ne sera pas conservée");
+
+define("FORLAN_138", "Délester ces forums");
+define("FORLAN_139", "Pour utiliser cette fonctionalité, vous devez aussi cocher \"Activer l'envoi d'images\" sur la page <a href='".e_ADMIN."image.php'>images</a>");	
+
+define("FORLAN_140", "Voir");	
+define("FORLAN_141", "Message");	
+
+define("FORLAN_142", "Permissions d'envoi de message");
+define("FORLAN_143", "Indique qui peut publier sur le forum");
+
+define("FORLAN_144", "Modérateur défini");
+define("FORLAN_145", "Configurer les sous-forums");
+
+define("FORLAN_146", "Pas de sous-forums pour le moment");
+define("FORLAN_147", "Modifier les sous-forums");
+define("FORLAN_148", "Créer les sous-forums");
+define("FORLAN_149", "sous-forums");
+define("FORLAN_150", "sous-forum");
+define("FORLAN_151", "ID");
+define("FORLAN_152", "messages");
+define("FORLAN_153", "Outils");
+define("FORLAN_154", "Réponse Supprimée");
+
+?>
