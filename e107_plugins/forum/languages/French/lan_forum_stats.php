bf7bca0545c53060ddff985c28f101bab17d6be5
tpruvot <tpruvot@localhost>
2005-06-12 00:42:15 +0000

    enhanced some expressions
    svn path=/trunk/e107_langpacks/; revision=3774

--- e107_plugins/forum/languages/French/lan_forum_stats.php
+++ e107_plugins/forum/languages/French/lan_forum_stats.php
@@ -4,19 +4,19 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_stats.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-12 00:37:22 $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-12 00:42:15 $
 |     $Author: tpruvot $
 +----------------------------------------------------------------------------+
 */
 define("e_PAGETITLE", "Statistiques du Forum");
 
 define("FSLAN_1", "Général");
-define("FSLAN_2", "Forum ouvert");
+define("FSLAN_2", "Création du Forum");
 define("FSLAN_3", "Ouvert depuis");
-define("FSLAN_4", "Total de messages");
-define("FSLAN_5", "Nombre de Sujets");
-define("FSLAN_6", "Nombre de Réponses");
+define("FSLAN_4", "Nombre total de messages");
+define("FSLAN_5", "de sujets");
+define("FSLAN_6", "de réponses");
 define("FSLAN_7", "Lectures de Sujets");
 define("FSLAN_8", "Taille de la base de données (tables du forum)");
 define("FSLAN_9", "Taille moyenne d'une ligne dans la table du forum");
@@ -28,11 +28,11 @@ define("FSLAN_14", "Commencé par");
 define("FSLAN_15", "Date");
 define("FSLAN_16", "Sujets les plus lus");
 define("FSLAN_17", "Lectures");
-define("FSLAN_18", "Les plus bavards");
+define("FSLAN_18", "Membres les plus bavards");
 define("FSLAN_19", "Nom");
 define("FSLAN_20", "Messages");
-define("FSLAN_21", "Les plus entreprenants");
-define("FSLAN_22", "Ceux qui ont répondu le plus");
+define("FSLAN_21", "Membres les plus entreprenants");
+define("FSLAN_22", "Membres qui ont le plus répondu");
 define("FSLAN_23", "Statistiques du Forum");
 define("FSLAN_24", "Moyenne de messages par jour");
 
