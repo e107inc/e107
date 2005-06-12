e90ad051a9fd81605f5a554f01f14d2ff366b279
tpruvot <tpruvot@localhost>
2005-06-12 00:49:59 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3775

---
+++ e107_plugins/forum/languages/French/lan_forum_search.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_search.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:49:59 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("FOR_SCH_LAN_1", "Forum");
+
+?>
---
+++ e107_plugins/forum/languages/French/lan_newforumposts_menu.php
@@ -0,0 +1,31 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_newforumposts_menu.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:49:59 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+	
+define("NFP_1", "Aucun des derniers messages n'est accessible à votre groupe, impossible d'afficher.");
+define("NFP_2", "Aucun message pour le moment");
+define("NFP_3", "Nouvelle configuration du menu \"Messages du Forum\" enregistrée");
+define("NFP_4", "Titre");
+define("NFP_5", "Nombre de messages à afficher?");
+define("NFP_6", "Nombre de caractères à afficher?");
+define("NFP_7", "Tronquer les messages trop longs?");
+define("NFP_8", "Afficher les sujets originaux dans le menu?");
+define("NFP_9", "Mettre à jour les Préférences du menu");
+define("NFP_10", "Nouvelle configuration du menu \"Messages du Forum\"");
+	
+?>
