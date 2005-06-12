f2d671b13d6e3a2c7cd9ccf88ffd6141007dc903
tpruvot <tpruvot@localhost>
2005-06-12 01:31:36 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3779

---
+++ e107_plugins/forum/languages/French/lan_forum_viewforum.php
@@ -0,0 +1,95 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_viewforum.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 01:31:36 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "Forum");
+
+define("LAN_01", "Forums");
+define("LAN_02", "Revenir en haut");
+define("LAN_03", "Go");
+define("LAN_53", "Sujet");
+define("LAN_54", "Auteur");
+define("LAN_55", "Réponses");
+define("LAN_56", "Vues");
+define("LAN_57", "Dernier message");
+define("LAN_58", "Il n'y a pas de messages dans ce forum pour le moment.");
+define("LAN_59", "Ces forums sont réservés aux membres enregistrés et connectés.");
+define("LAN_79", "Nouveaux messages");
+define("LAN_80", " Pas de nouveaux messages");
+define("LAN_81", "Sujet vérouillé");
+define("LAN_180", "Rechercher");
+define("LAN_202", "Post-it");
+define("LAN_203", "Post-it/Vérouillé");
+define("LAN_204", "Vous <b>pouvez</b> poster un nouveau sujet");
+define("LAN_205", "Vous <b>ne pouvez pas</b> poster un nouveau sujet");
+define("LAN_206", "Vous <b>pouvez</b> répondre");
+define("LAN_207", "Vous <b>ne pouvez pas</b> répondre");
+define("LAN_208", "Vous <b>pouvez</b> modifier vos messages");
+define("LAN_209", "Vous <b>ne pouvez pas</b> modifier vos messages");
+define("LAN_316", "Aller à la page");
+define("LAN_317", "Aucun");
+define("LAN_321", "Modérateurs: ");
+define("LAN_395", "[populaire]");
+define("LAN_396", "Annonce");
+
+define("LAN_397", "Ce forum est en lecture seule");
+
+define("LAN_398", "Enlever le Post-it de ce sujet");
+define("LAN_399", "Vérouiller ce sujet");
+define("LAN_400", "Dévérouiller ce sujet");
+define("LAN_401", "Mettre ce sujet en Post-it");
+define("LAN_402", "Déplacer le sujet");
+define("LAN_403", "Aller au forum");
+define("LAN_404", "Ce forum est modéré par");
+
+define("LAN_405", "utilisateur lit ce forum en ce moment");
+define("LAN_406", "utilisateurs lisent ce forum en ce moment");
+
+define("LAN_407", "membre");
+define("LAN_408", "invité");
+define("LAN_409", "membres");
+define("LAN_410", "invités");
+
+//v.616
+define("LAN_411", "Sujets important");
+define("LAN_412", "Sujets du forum");
+
+define("LAN_431", "Flux RSS 0.92 du Forum");
+define("LAN_432", "Flux RSS 2.0 du Forum");
+define("LAN_433", "Flux RDF du Forum");
+
+define("LAN_434", "Etes-vous certain de vouloir supprimer ce sujet et les éventuelles réponses?");
+define("LAN_435", "Supprimer le sujet");
+
+//v.617
+define("FORLAN_CLOSE", "Sujet vérouillé.");
+define("FORLAN_OPEN", "Sujet dévérouillé.");
+define("FORLAN_STICK", "Sujet mis en Post-it.");
+define("FORLAN_UNSTICK", "Post-it retiré.");
+define("FORLAN_6", "Sujet supprimé");
+define("FORLAN_7", "réponses supprimées");
+define("FORLAN_8", "ici");
+define("FORLAN_9", "pour vous inscrire ou vous identifier depuis le menu d'identification.");
+
+define("FORLAN_10","Poster un Nouveau Sujet");
+define("FORLAN_11","Nouveaux Messages");
+define("FORLAN_12","Pas de nouveaux messages");
+define("FORLAN_13","Nouveaux messages dans le sujet populaire");
+define("FORLAN_14","Pas de nouveaux messages dans le sujet populaire");
+define("FORLAN_15","Sujet mis en Post-it");
+define("FORLAN_16","Sujet Post-it Vérouillé");
+define("FORLAN_17","Sujet mis en Annonce");
+define("FORLAN_18","Sujet Vérouillé");
+define('FORLAN_19', '[membre supprimé]');
+define('FORLAN_20', 'Sous-forum');
+define('FORLAN_21', 'Sujets');
+define('FORLAN_22', 'Dernier Message');
+	
+?>
