86228a22610554fd867b2a355cfee939848bf9d8
tpruvot <tpruvot@localhost>
2005-06-12 00:56:39 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3776

---
+++ e107_plugins/forum/languages/French/lan_forum_conf.php
@@ -0,0 +1,37 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_conf.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:56:39 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("FORLAN_5", "Sondage supprimé.");
+define("FORLAN_6", "Sujet supprimé");
+define("FORLAN_7", "réponses supprimées");
+define("FORLAN_8", "Suppression annulée.");
+define("FORLAN_9", "Sujet déplacé.");
+define("FORLAN_10", "Déplacement annulé.");
+define("FORLAN_11", "Retourner sur le Forum");
+define("FORLAN_12", "Paramètres du Forum");
+define("FORLAN_13", "Etes-vous sûr de vouloir supprimer ce sondage?<br />Une fois supprimé il <b><u>ne pourra pas</u></b> être récupéré.");
+define("FORLAN_14", "Annuler");
+define("FORLAN_15", "Confirmer la suppression du Message du forum");
+define("FORLAN_16", "Confirmer la suppression du Sondage");
+define("FORLAN_17", "envoyé par");
+define("FORLAN_18", "Etes vous absolument certain de vouloir supprimer ce forum");
+define("FORLAN_19", "sujet et ses messages?");
+define("FORLAN_20", "le sondage sera aussi supprimé");
+define("FORLAN_21", "Une fois supprimés ils");
+define("FORLAN_22", "message?<br />Une fois supprimé il");
+define("FORLAN_23", "ne pourra pas</u></b> être récupéré");
+define("FORLAN_24", "Déplacer le sujet vers le forum");
+define("FORLAN_25", "Déplacer le sujet");
+define("FORLAN_26", "Réponse supprimée");
+	
+define("FORLAN_27", "déplacé");
+	
+?>
