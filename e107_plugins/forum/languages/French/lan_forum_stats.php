6a97958df4ada0b08fc572aca85b898aff47e84d
tpruvot <tpruvot@localhost>
2005-06-12 00:37:22 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=3773

---
+++ e107_plugins/forum/languages/French/lan_forum_stats.php
@@ -0,0 +1,39 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_stats.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:37:22 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("e_PAGETITLE", "Statistiques du Forum");
+
+define("FSLAN_1", "Général");
+define("FSLAN_2", "Forum ouvert");
+define("FSLAN_3", "Ouvert depuis");
+define("FSLAN_4", "Total de messages");
+define("FSLAN_5", "Nombre de Sujets");
+define("FSLAN_6", "Nombre de Réponses");
+define("FSLAN_7", "Lectures de Sujets");
+define("FSLAN_8", "Taille de la base de données (tables du forum)");
+define("FSLAN_9", "Taille moyenne d'une ligne dans la table du forum");
+define("FSLAN_10", "Sujets les plus actifs");
+define("FSLAN_11", "Rang");
+define("FSLAN_12", "Sujet");
+define("FSLAN_13", "Réponses");
+define("FSLAN_14", "Commencé par");
+define("FSLAN_15", "Date");
+define("FSLAN_16", "Sujets les plus lus");
+define("FSLAN_17", "Lectures");
+define("FSLAN_18", "Les plus bavards");
+define("FSLAN_19", "Nom");
+define("FSLAN_20", "Messages");
+define("FSLAN_21", "Les plus entreprenants");
+define("FSLAN_22", "Ceux qui ont répondu le plus");
+define("FSLAN_23", "Statistiques du Forum");
+define("FSLAN_24", "Moyenne de messages par jour");
+
+?>
---
+++ e107_plugins/forum/languages/French/lan_forum_uploads.php
@@ -0,0 +1,25 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/French/lan_forum_uploads.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-12 00:37:22 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "Fichiers du Forum");
+
+define('FRMUP_1','Fichiers envoyés sur le forum');
+define('FRMUP_2','Fichier supprimé');
+define('FRMUP_3','Erreur: Impossible de supprimer ce fichier');
+define('FRMUP_4','Suppression de fichier');
+define('FRMUP_5','Nom du fichier');
+define('FRMUP_6','Résultat');
+define('FRMUP_7','Trouvé dans le sujet');
+define('FRMUP_8','NON TROUVÉ');
+define('FRMUP_9','Aucun fichier envoyé');
+define('FRMUP_10','Supprimer');
+	
+?>
