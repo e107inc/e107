709563f31f37e889506d98441c18ffcadb947c17
mijnheer <mijnheer@localhost>
2005-06-13 20:26:36 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=3803

---
+++ e107_plugins/content/languages/Dutch/lan_content_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/content/languages/Dutch/lan_content_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-13 20:26:36 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+
+define("CONT_FP_1", "Content categorie");
+define("CONT_FP_2", "Hoofdpagina");
+
+?>
