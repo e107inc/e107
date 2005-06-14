0987685aa48bba74d6d9356c97b02fce18c7c0ef
natxocc <natxocc@localhost>
2005-06-14 19:45:28 +0000

    Update from original CVS 14/06/05
    svn path=/trunk/e107_langpacks/; revision=3821

--- e107_languages/Spanish/lan_rate.php
+++ e107_languages/Spanish/lan_rate.php
@@ -4,20 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Spanish/lan_rate.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-10 17:10:53 $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-14 19:45:28 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
-
-define("PAGE_NAME", "Valoración");
-
 define("RATELAN_0", "Voto");
 define("RATELAN_1", "Votos");
 define("RATELAN_2", "¿Como valora este elemento?");
 define("RATELAN_3", "Gracias por su voto");
 define("RATELAN_4", "Sin valorar");
-
-
-
 ?>
