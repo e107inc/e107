331092c4de0cc5533b08aaaacdf70ea5c5cf21ce
lisa_ <lisa_@localhost>
2005-06-15 08:41:54 +0000

    content: image link changed to 'content_16'
    svn path=/trunk/e107_0.7/; revision=3836

--- e107_plugins/content/e_latest.php
+++ e107_plugins/content/e_latest.php
@@ -3,7 +3,7 @@ $plugintable = "pcontent";
 $reported_content = $sql -> db_Count($plugintable, '(*)', "WHERE content_refer='sa' ");
 $text .= "
 <div style='padding-bottom: 2px;'>
-<img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' />
+<img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' />
 ";
 if($reported_content) {
 	$text .= " <a href='".e_PLUGIN."content/admin_content_config.php?submitted'>Submitted Content Items: $reported_content</a>";
--- e107_plugins/content/e_status.php
+++ e107_plugins/content/e_status.php
@@ -3,7 +3,7 @@ $sql2 = new db;
 $maincat = $sql -> db_Select("pcontent", "content_id, content_heading", "LEFT(content_parent,1) = '0' ORDER BY content_heading");
 while($row = $sql -> db_Fetch()){
 		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE content_parent = '".$row['content_id']."' AND content_refer != 'sa' ");
-		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/articles_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ".$row['content_heading'].": ".$count."</div>";
+		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ".$row['content_heading'].": ".$count."</div>";
 }
 
 ?>
