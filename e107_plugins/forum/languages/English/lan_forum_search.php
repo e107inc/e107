1e20d435bc92299301a9951ddebd75e8112e2f33
sweetas <sweetas@localhost>
2005-06-16 05:25:35 +0000

    updated template so its suitable for low res monitors and themes
    svn path=/trunk/e107_0.7/; revision=3877

--- e107_plugins/forum/languages/English/lan_forum_search.php
+++ e107_plugins/forum/languages/English/lan_forum_search.php
@@ -4,14 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/languages/English/lan_forum_search.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-15 15:18:41 $
+|     $Revision: 1.3 $
+|     $Date: 2005-06-16 05:25:35 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 
 define("FOR_SCH_LAN_1", "Forum");
-define("FOR_SCH_LAN_2", "Search in forum");
+define("FOR_SCH_LAN_2", "Select forum");
 define("FOR_SCH_LAN_3", "All Forums");
 define("FOR_SCH_LAN_4", "Whole post");
 define("FOR_SCH_LAN_5", "As part of thread");
