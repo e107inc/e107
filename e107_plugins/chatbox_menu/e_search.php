3a970ec576c84f576fb96a64b1a6bdaaee1f4850
sweetas <sweetas@localhost>
2005-06-16 01:40:40 +0000

    chatbox search lan updated
    svn path=/trunk/e107_0.7/; revision=3876

--- e107_plugins/chatbox_menu/e_search.php
+++ e107_plugins/chatbox_menu/e_search.php
@@ -1,9 +1,9 @@
 <?php
-if (file_exists(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php")) {
-	include_once(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php");
+if (file_exists(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE."_search.php")) {
+	include_once(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE."_search.php");
 } else {
-	include_once(e_PLUGIN."chatbox_menu/languages/English/English.php");
+	include_once(e_PLUGIN."chatbox_menu/languages/English/English_search.php");
 }
 
-$search_info[] = array('sfile' => e_PLUGIN.'chatbox_menu/search/search_parser.php', 'qtype' => CHATBOX_L2, 'refpage' => 'chat.php', 'advanced' => e_PLUGIN.'chatbox_menu/search/search_advanced.php');
+$search_info[] = array('sfile' => e_PLUGIN.'chatbox_menu/search/search_parser.php', 'qtype' => CB_SCH_LAN_1, 'refpage' => 'chat.php', 'advanced' => e_PLUGIN.'chatbox_menu/search/search_advanced.php');
 ?>
