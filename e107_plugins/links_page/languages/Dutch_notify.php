beb83de1ea8b9b7aa3cc06a36e6328d02336346f
mijnheer <mijnheer@localhost>
2005-06-19 06:29:11 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=3940

---
+++ e107_plugins/links_page/languages/Dutch_notify.php
@@ -0,0 +1,18 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/links_page/languages/Dutch_notify.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-19 06:29:11 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+
+define("NT_LAN_LP_1", "Links pagina gebeurtenissen");
+define("NT_LAN_LP_2", "Link aangemeld door gebruiker");
+define("NT_LAN_LP_3", "Link aangemeld");
+
+
+?>
