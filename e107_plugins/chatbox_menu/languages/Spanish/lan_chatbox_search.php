7f7524c530e124812fbb32c6bc303436cfd75d43
natxocc <natxocc@localhost>
2005-06-19 17:18:09 +0000

    Update from original CVS 19/06/05
    svn path=/trunk/e107_langpacks/; revision=3949

---
+++ e107_plugins/chatbox_menu/languages/Spanish/lan_chatbox_search.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/chatbox_menu/languages/Spanish/lan_chatbox_search.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-19 17:18:09 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+
+define("CB_SCH_LAN_1", "Chatbox");
+
+?>
