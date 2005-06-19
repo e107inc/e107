dab358553dc8c8e6dd806d575a33511bbcdc6044
mijnheer <mijnheer@localhost>
2005-06-19 06:49:28 +0000

    Update june 19
    svn path=/trunk/e107_langpacks/; revision=3943

--- e107_languages/Dutch/lan_user_extended.php
+++ e107_languages/Dutch/lan_user_extended.php
@@ -8,4 +8,12 @@ define("UE_LAN_6", "Integer");
 define("UE_LAN_7", "Datum");
 define("UE_LAN_8", "Taal");
 define("UE_LAN_HIDE", "Verbergen voor gebruikers");
+define("UE_LAN_LOCATION", "Woonplaats");
+define("UE_LAN_AIM", "AIM");
+define("UE_LAN_ICQ", "ICQ");
+define("UE_LAN_YAHOO", "Yahoo!");
+define("UE_LAN_MSN", "MSN");
+define("UE_LAN_HOMEPAGE", "Homepage");
+define("UE_LAN_BIRTHDAY", "Geboortedatum");
+
 ?>
