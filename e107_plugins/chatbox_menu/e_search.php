5da94a9b2b35853614466442e0cfac4aa8a542a7
e107steved <steved@e107.org>
2005-06-19 09:12:53 +0000

    Bugtracker #1004
    svn path=/trunk/e107_0.7/; revision=3947

--- e107_plugins/chatbox_menu/e_search.php
+++ e107_plugins/chatbox_menu/e_search.php
@@ -1,8 +1,8 @@
 <?php
-if (file_exists(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE."_search.php")) {
-	include_once(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE."_search.php");
+if (file_exists(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/lan_chatbox_search.php")) {
+	include_once(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/lan_chatbox_search.php");
 } else {
-	include_once(e_PLUGIN."chatbox_menu/languages/English/English_search.php");
+	include_once(e_PLUGIN."chatbox_menu/languages/English/lan_chatbox_search.php");
 }
 
 $search_info[] = array('sfile' => e_PLUGIN.'chatbox_menu/search/search_parser.php', 'qtype' => CB_SCH_LAN_1, 'refpage' => 'chat.php', 'advanced' => e_PLUGIN.'chatbox_menu/search/search_advanced.php');
