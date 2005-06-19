d1111ab386b260d018f35bcbb6401538ffb88955
e107steved <steved@e107.org>
2005-06-19 09:12:53 +0000

    Bugtracker #1004
    svn path=/trunk/e107_0.7/; revision=3947

---
+++ e107_plugins/chatbox_menu/languages/English/lan_chatbox_search.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/chatbox_menu/languages/English/lan_chatbox_search.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-19 09:12:53 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
+
+define("CB_SCH_LAN_1", "Chatbox");
+
+?>
