6d67e252c02c6ac4f242f8a608d5c5658be4fece
mijnheer <mijnheer@localhost>
2005-06-19 06:45:26 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=3942

---
+++ e107_plugins/newsfeed/languages/Dutch_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/Dutch_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-19 06:45:26 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "News Feeds");
+define("NWSF_FP_2", "hoofdpagina");
+
+?>
