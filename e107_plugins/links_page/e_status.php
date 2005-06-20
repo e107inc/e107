c6c4d9f15fb58d3703bb72ffce7abc5ef8fa8aa2
lisa_ <lisa_@localhost>
2005-06-20 14:31:58 +0000

    links page: correction to e_status, changed wrong image url
    svn path=/trunk/e107_0.7/; revision=3957

--- e107_plugins/links_page/e_status.php
+++ e107_plugins/links_page/e_status.php
@@ -1,5 +1,5 @@
 <?php
 $sql2 = new db;
 $count = $sql2 -> db_Count("links_page", "(*)");
-$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> Links: ".$count."</div>";
+$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."links_page/images/linkspage_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> Links: ".$count."</div>";
 ?>
