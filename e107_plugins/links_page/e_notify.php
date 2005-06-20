ff68bcf02528a622b9393e0a0fd2935cc47e8dd6
lisa_ <lisa_@localhost>
2005-06-20 14:14:57 +0000

    Links_page : fix wrong lan paths + added e_latest + e_status
    svn path=/trunk/e107_0.7/; revision=3955

--- e107_plugins/links_page/e_notify.php
+++ e107_plugins/links_page/e_notify.php
@@ -11,15 +11,17 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/links_page/e_notify.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-05-31 21:38:58 $
-|     $Author: sweetas $
+|     $Revision: 1.4 $
+|     $Date: 2005-06-20 14:14:57 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 
-@include_once(e_PLUGIN.'links_page/languages/'.e_LANGUAGE.'_notify.php');
-@include_once(e_PLUGIN.'links_page/languages/English_notify.php');
-
+if (file_exists(e_PLUGIN."links_page/languages/".e_LANGUAGE."/lan_links_page_notify.php")) {
+	@include_once(e_PLUGIN."links_page/languages/".e_LANGUAGE."/lan_links_page_notify.php");
+	} else {
+	@include_once(e_PLUGIN."links_page/languages/English/lan_links_page_notify.php");
+}
 $config_category = NT_LAN_LP_1;
 $config_events = array('linksub' => NT_LAN_LP_2);
 
