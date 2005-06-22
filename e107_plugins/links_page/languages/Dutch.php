5431d567e4874a763644f43c50f6357bbbd6b3fb
e107coders <e107coders@localhost>
2005-06-22 18:26:26 +0000

    Dutch cleanup
    svn path=/trunk/e107_langpacks/; revision=3991

--- e107_plugins/links_page/languages/Dutch.php
+++ e107_plugins/links_page/languages/Dutch.php
@@ -3,10 +3,10 @@
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
-|     $Source: /cvs_backup/e107_langpacks/e107_plugins/links_page/languages/Dutch_new.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-21 20:44:57 $
-|     $Author: mijnheer $
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/links_page/languages/Dutch.php,v $
+|     $Revision: 1.5 $
+|     $Date: 2005-06-22 18:26:25 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Links");
