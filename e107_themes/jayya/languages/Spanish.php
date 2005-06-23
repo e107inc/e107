0cf06a90428119a68165c0b162b4da21983dba79
natxocc <natxocc@localhost>
2005-06-23 23:31:51 +0000

    Update from original CVS 23/06/05
    svn path=/trunk/e107_langpacks/; revision=4035

--- e107_themes/jayya/languages/Spanish.php
+++ e107_themes/jayya/languages/Spanish.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_themes/jayya/languages/Spanish.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-03 22:41:15 $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-23 23:31:51 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
@@ -13,6 +13,7 @@ define("LAN_THEME_1", "Comentarios desactivados ");
 define("LAN_THEME_2", "Leer/enviar comentario: ");
 define("LAN_THEME_3", "Leer el resto...");
 define("LAN_THEME_4", "Trackbacks: ");
-
+define("LAN_THEME_5", "Enviado por");
+define("LAN_THEME_6", "el");
 
 ?>
