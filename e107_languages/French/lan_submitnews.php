c87c26dad2579382bbfb82c33476c3ad1862f634
tpruvot <tpruvot@localhost>
2005-06-24 21:15:46 +0000

    Real French
    svn path=/trunk/e107_langpacks/; revision=4057

--- e107_languages/French/lan_submitnews.php
+++ e107_languages/French/lan_submitnews.php
@@ -4,29 +4,29 @@
 |     e107 website system - French Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_submitnews.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-05-24 11:44:22 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-24 21:15:46 $
+|     $Author: tpruvot $
 |     Encoding: UTF-8
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Soumettre une Nouvelle");
-define("LAN_7", " Nom de connection: ");
-define("LAN_62", "Sujet: ");
+define("LAN_7", " Pseudo: ");
+define("LAN_62", "Titre: ");
 define("LAN_112", "Adresse email: ");
 define("LAN_133", "Merci");
-define("LAN_134", "Votre élément a été soumis et sera examiné par un des administrateurs de site en temps voulu.");
-define("LAN_135", "Eléments de Nouvelle: ");
-define("LAN_136", "Soumettre un éléments de nouvelle");
-define("NWSLAN_6", "Catégories");
+define("LAN_134", "Votre nouvelle a été soumise et sera examinée par un des administrateurs du site en temps voulu.");
+define("LAN_135", "Contenu: ");
+define("LAN_136", "Soumettre la nouvelle");
+define("NWSLAN_6", "Catégorie");
 define("NWSLAN_10", "Pas de catégories de nouvelle");
 define("NWSLAN_11", "Vous n'avez pas accès à cette zone.");
 define("NWSLAN_12", "Accès refusé.");
 
-define("SUBNEWSLAN_1", "Vous devez inclure un titre.\\n");
-define("SUBNEWSLAN_2", "Vous devez inclure du texte dans une élément de nouvelle.\\n");
-define("SUBNEWSLAN_3", "Votre attachement doit être soit un fichier jpg, gif ou png");
-define("SUBNEWSLAN_4", "Fichier trop larg");
+define("SUBNEWSLAN_1", "Vous devez inclure le titre.\\n");
+define("SUBNEWSLAN_2", "Vous devez inclure du contenu dans la nouvelle.\\n");
+define("SUBNEWSLAN_3", "Votre fichier attaché doit être un fichier jpg, gif ou png");
+define("SUBNEWSLAN_4", "Fichier trop gros");
 define("SUBNEWSLAN_5", "Fichier image ");
 define("SUBNEWSLAN_6", "(jpg, gif ou png)");
 
