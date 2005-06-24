c8710200a2a350e018b5dc76bd2cc5f1517a3e46
tpruvot <tpruvot@localhost>
2005-06-24 20:41:07 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4054

---
+++ e107_themes/lamb/languages/French.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_themes/lamb/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-24 20:41:07 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "'lamb' par <a href='http://e107.org' rel='external'>jalist</a>");
+define("LAN_THEME_2", "Commentaires désactivés pour cet élément");
+define("LAN_THEME_3", "Commentaires: ");
+define("LAN_THEME_4", "Lire la suite ...");
+define("LAN_THEME_5", "Trackbacks: ");
+
+
+?>
