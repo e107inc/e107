e99fa379f0b2da87d766f81be805a022ac5c8062
mcfly_e107 <mcfly_e107@localhost>
2005-06-24 11:23:45 +0000

    Bugtracker #1041, also added a 'short' option to computeLapse()
    svn path=/trunk/e107_0.7/; revision=4039

--- e107_languages/English/lan_date.php
+++ e107_languages/English/lan_date.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_date.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-03-14 15:48:57 $
-|     $Author: stevedunstan $
+|     $Revision: 1.4 $
+|     $Date: 2005-06-24 11:23:45 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 
@@ -24,6 +24,12 @@ define("LANDT_04s", "days");
 define("LANDT_05s", "hours");
 define("LANDT_06s", "minutes");
 define("LANDT_07s", "seconds");
+
+define("LANDT_08", "min");
+define("LANDT_08s", "mins");
+define("LANDT_09", "sec");
+define("LANDT_09s", "secs");
+define("LANDT_AGO", "ago");
 
 
 ?>
