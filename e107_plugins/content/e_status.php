4b9b31f25f62008ae0bb87d54edf26560d5ea1ca
lisa_ <lisa_@localhost>
2005-06-25 20:13:30 +0000

    content: changed e_status to avoid long lists in the admin status area
    svn path=/trunk/e107_0.7/; revision=4079

--- e107_plugins/content/e_status.php
+++ e107_plugins/content/e_status.php
@@ -1,9 +1,12 @@
 <?php
 $sql2 = new db;
+$total = $sql -> db_Count("pcontent", "(*)", "WHERE LEFT(content_parent,1) != '0' AND content_refer != 'sa'");
+$text .= "<div style='padding-bottom: 2px;'><a style='cursor: pointer; cursor: hand' onclick=\"expandit('content');\"><img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> Content: ".$total."</a></div>";
 $maincat = $sql -> db_Select("pcontent", "content_id, content_heading", "LEFT(content_parent,1) = '0' ORDER BY content_heading");
+$text .= "<div id='content' style='display: none;'>";
 while($row = $sql -> db_Fetch()){
-		$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE content_parent = '".$row['content_id']."' AND content_refer != 'sa' ");
-		$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ".$row['content_heading'].": ".$count."</div>";
+	$count = $sql2 -> db_Count("pcontent", "(*)", "WHERE content_parent = '".$row['content_id']."' AND content_refer != 'sa' ");
+	$text .= "<div style='padding-bottom: 2px;'><img src='".e_PLUGIN."content/images/content_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ".$row['content_heading'].": ".$count."</div>";
 }
-
+$text .= "</div>";
 ?>
