5478f7ef3e0c66e0d485faf638f236cf8463fba9
tpruvot <tpruvot@localhost>
2005-06-25 20:05:45 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4078

--- e107_plugins/clock_menu/languages/admin/French.php
+++ e107_plugins/clock_menu/languages/admin/French.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/clock_menu/languages/admin/French.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-25 20:03:58 $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-25 20:05:45 $
 |     $Author: tpruvot $
 +----------------------------------------------------------------------------+
 */
@@ -16,12 +16,12 @@ define("CLOCK_AD_L4", "Configuration du menu");
 define("CLOCK_AD_L5", "AM/PM");
 define("CLOCK_AD_L6", "Activé, l'heure sera affichée au format US (0-12 AM/PM). Désactivé, elle sera affiché au format 24H 0-24");
 define("CLOCK_AD_L7", "Préfixe de la date");
-define("CLOCK_AD_L8", "Si vous langue nécessite une courte particule avant la date (Exemple 'le' en Français ou 'den' en Allemand...), remplissez ce champ.");
+define("CLOCK_AD_L8", "Si votre langue nécessite une courte particule avant la date (Exemple 'le' en Français ou 'den' en Allemand...), remplissez ce champ.");
 define("CLOCK_AD_L9", "Suffixe 1");
 define("CLOCK_AD_L10", "Suffixe 2");
 define("CLOCK_AD_L11", "Suffixe 3");
 define("CLOCK_AD_L12", "Suffixe 4 et +");
-define("CLOCK_AD_L13", "Si vous langue nécessite un suffixe après le jour du mois, remplissez ces champs (Exemple: 'st' pour 1, 'nd' pour 2, 'rd' pour 3 et 'th' pour les autres en anglais).");
+define("CLOCK_AD_L13", "Si votre langue nécessite un suffixe après le jour du mois, remplissez ces champs (Exemple: 'st' pour 1, 'nd' pour 2, 'rd' pour 3 et 'th' pour les autres en anglais).");
 define("CLOCK_AD_L14", "");
 define("CLOCK_AD_L15", "");
 define("CLOCK_AD_L16", "");
