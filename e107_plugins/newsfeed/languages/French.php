b488a6a6bb3c2a9afd2a910e1458b3eb39d89acd
tpruvot <tpruvot@localhost>
2005-06-25 19:38:22 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4075

---
+++ e107_plugins/newsfeed/languages/French.php
@@ -0,0 +1,84 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-25 19:36:06 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+
+define("NFLAN_01", "Newsfeeds");
+define("NFLAN_02", "Ce plugin rapatrie les flux rss d'autres sites web pour les afficher selon vos préférences");
+define("NFLAN_03", "Configurer les flux");
+define("NFLAN_04", "Le plugin Newsfeeds a été correctement installé. Pour ajouter des flux et les paramétrer, retournez au à la page d'administration principale et cliquez sur l'icône newsfeeds dans la section Plugins.");
+define("NFLAN_05", "Modifier");
+define("NFLAN_06", "Supprimer");
+define("NFLAN_07", "Flux de news existants");
+define("NFLAN_08", "Page d'accueil des flux");
+define("NFLAN_09", "Ajouter un flux");
+define("NFLAN_10", "URL du fil RSS");
+define("NFLAN_11", "Chemin de l'image");
+define("NFLAN_12", "Activation");
+define("NFLAN_13", "Nul part (inactif)");
+define("NFLAN_14", "Uniquement dans le menu");
+define("NFLAN_15", "Créer le flux");
+define("NFLAN_16", "Modifier le flux");
+define("NFLAN_17", "entrez 'default' dans le champ pour utiliser l'image définie dans le fil, pour utiliser votre propre image, entrez le chemin (url) complet, laissez à blanc si vous ne voulez pas d'image.");
+define("NFLAN_18", "Interval de mise à jour en secondes");
+define("NFLAN_19", "ex, 3600: pour une mise à jour toutes les heures");
+define("NFLAN_20", "Uniquement sur la page principale du flux");
+define("NFLAN_21", "Dans le menu ansi que sur la page du flux");
+define("NFLAN_22", "spécifiez où vous voulez afficher le flux");
+define("NFLAN_23", "Flux ajouté dans la base de données.");
+define("NFLAN_24", "Des champs obligatoires ont été oubliés.");
+define("NFLAN_25", "Flux modifié dans la base de données.");
+define("NFLAN_26", "Interval de mise à jour");
+define("NFLAN_27", "Options");
+define("NFLAN_28", "URL");
+define("NFLAN_29", "Flux de news disponibles");
+define("NFLAN_30", "Nom du Flux");
+define("NFLAN_31", "Retourner à la liste des flux");
+define("NFLAN_32", "Le flux avec ce numéro d'identification ne peut être trouvé.");
+define("NFLAN_33", "Date de publication: ");
+define("NFLAN_34", "inconnue");
+define("NFLAN_35", "posté par ");
+define("NFLAN_36", "Description");
+define("NFLAN_37", "courte description du flux, entrez 'default' pour utiliser celle définie dans le fil");
+define("NFLAN_38", "Gros Titres");
+define("NFLAN_39", "Détails");
+define("NFLAN_40", "Flux supprimé");
+define("NFLAN_41", "Aucun flux défini pour le moment");
+
+define("NFLAN_42", "<b>&raquo;</b> <u>Nom du flux:</u>
+	Le nom identifiant le flux peut correspondre à tout ce que vous voulez.
+	<br /><br />
+	<b>&raquo;</b> <u>URL du fil rss:</u>
+	L'adresse internet du fil RSS
+	<br /><br />
+	<b>&raquo;</b> <u>Chemin de l'image:</u>
+	Si le flux possède déjà une image, entrez 'default' pour l'utiliser. Pour utiliser une des vôtres, entrez son chemin (url) complet. Laissez à blanc pour ne pas utiliser d'image.
+	<br /><br />
+	<b>&raquo;</b> <u>Description:</u>
+	Entrez une courte description pour le flux, ou 'default' pour concerver celle définie dans le flux (si elle est présente).
+	<br /><br />
+	<b>&raquo;</b> <u>Interval de mise à jour en secondes:</u>
+	C'est le nombre de secondes nécessaire avant la mise à jour du flux, par exemple, 1800: 30 minutes, 3600: une heure.
+	<br /><br />
+	<b>&raquo;</b> <u>Activation:</u>
+	L'endroit où vous souhaitez afficher les résultats, pour voir le menu des flux vous devrez l'activer newsfeeds sur la page d'administration <a href='".e_ADMIN."menus.php'>des menus</a>.
+	<br /><br />Pour obtenir une liste des flux disponibles, visitez <a href='http://www.lamoooche.com/' rel='external'>La Mooche</a>, <a href='http://www.syndic8.com/' rel='external'>syndic8.com</a> ou <a href='http://feedfinder.feedster.com/index.php' rel='external'>feedster.com</a>");
+define("NFLAN_43", "Aide des flux de news");
+define("NFLAN_44", "cliquez pour afficher");
+
+?>
---
+++ e107_plugins/newsfeed/languages/French_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/French_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-25 19:38:22 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "NewsFeeds");
+define("NWSF_FP_2", "Page principale");
+
+?>
