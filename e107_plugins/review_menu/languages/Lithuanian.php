1f4dabd002f08c0149409524b5a38ab8e9db5cd1
g3d45 <g3d45@localhost>
2005-06-26 22:58:15 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4136

---
+++ e107_plugins/review_menu/languages/Lithuanian.php
@@ -0,0 +1,29 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/review_menu/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-26 22:58:15 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+	
+define("LAN_190", "Peteikti apþvalgà");
+define("LAN_RVW_1", "Atnaujinti meniu nuostatas");
+define("LAN_RVW_2", "Apþvalgø meniu sàranka iðsaugota");
+define("LAN_RVW_3", "Pavadinimas");
+define("LAN_RVW_4", "Rodomø apþvalgø skaièius");
+define("LAN_RVW_5", "Rodyti apþvalgø kategorijas meniu lange?");
+define("LAN_RVW_6", "Apþvalgø sàraðo puslapio pavadinimas");
+define("LAN_RVW_7", "Rodyti nuorodà apþvalgos pateikimui?");
+define("LAN_RVW_8", "Apþvalgø meniu sàranka");
+?>
---
+++ e107_plugins/rss_menu/languages/Lithuanian.php
@@ -0,0 +1,23 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/rss_menu/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-26 22:53:19 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+	
+define("BACKEND_MENU_L1", " gali bûti sindikuota naudojant ðá rss tiekimà.");
+define("BACKEND_MENU_L2", "RSS tiekimas");
+
+define("BACKEND_MENU_L3", "Mûsø naujienos");
+define("BACKEND_MENU_L4", "Mûsø komentarai");
+define("BACKEND_MENU_L5", "Mûsø diskusijø temos");
+define("BACKEND_MENU_L6", "Mûsø diskusijø praneðimai");
+
+define("BACKEND_MENU_L7", "Mûsø pokalbiø (chatbox) praneðimai");
+define("BACKEND_MENU_L8", "Møsø bugtracker raportai");
+?>
