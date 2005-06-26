d01036232ab1e347e81a66c43af3640165847f40
tpruvot <tpruvot@localhost>
2005-06-26 04:10:33 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4097

---
+++ e107_plugins/chatbox_menu/languages/French/French.php
@@ -0,0 +1,53 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/chatbox_menu/languages/French/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-26 04:10:33 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("CHATBOX_L1", "Impossible d'utiliser ce nom d'utilisateur car il est utilisé sur ce site - si c'est votre nom d'utilisateur, connectez vous.");
+define("CHATBOX_L2", "Chatbox");
+define("CHATBOX_L3", "Vous devez être connecté pour ajouter des commentaires sur ce site - veuillez vous connecter. Si vous n'êtes pas enregistré cliquez <a href='".e_BASE."signup.php'>ici</a> pour le faire");
+define("CHATBOX_L4", "Envoyer");
+define("CHATBOX_L5", "Vider");
+define("CHATBOX_L6", "[bloqué par l'admin]");
+define("CHATBOX_L7", "Débloquer");
+define("CHATBOX_L8", "Info");
+define("CHATBOX_L9", "Bloquer");
+define("CHATBOX_L10", "Supprimer");
+define("CHATBOX_L11", "Pas de messages.");
+define("CHATBOX_L12", "Voir tous les messages");
+define("CHATBOX_L13", "modérer le Chatbox");
+define("CHATBOX_L14", "Smileys");
+define("CHATBOX_L15", "Message envoyé trop long ou vide");
+define("CHATBOX_L16", "Anonyme");
+define("CHATBOX_L17", "Message dupliqué");
+define("CHATBOX_L18", "Messages du Chatbox modérés");
+
+define("LAN_11", "Chatbox (tous les messages)");
+define("LAN_12", "Messages Chat");
+define("LAN_13", "sur");
+define("LAN_14", "Erreur!");
+define("LAN_15", "Vous n'avez pas la permission de voir cette page.");
+define("LAN_16", "[ ce message a été bloqué par un admin ]");
+// Notify
+define("NT_LAN_CB_1", "Evénements Chatbox");
+define("NT_LAN_CB_2", "Message envoyé");
+define("NT_LAN_CB_3", "Envoyé par");
+define("NT_LAN_CB_4", "Adresse IP");
+define("NT_LAN_CB_5", "Message");
+define("NT_LAN_CB_6", "Message Chatbox envoyé");
+
+?>
---
+++ e107_plugins/chatbox_menu/languages/French/French_config.php
@@ -0,0 +1,48 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/chatbox_menu/languages/French/French_config.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-26 04:10:33 $
+|     $Author: tpruvot $
+|     Encoding:
++----------------------------------------------------------------------------+
+*/
+define("CHBLAN_1", "Préférences du Chatbox mises à jour.");
+define("CHBLAN_2", "Modéré.");
+define("CHBLAN_3", "Pas de messages.");
+define("CHBLAN_4", "Membre");
+define("CHBLAN_5", "Invité");
+define("CHBLAN_6", "débloquer");
+define("CHBLAN_7", "bloquer");
+define("CHBLAN_8", "supprimer");
+define("CHBLAN_9", "Modérer le Chatbox");
+define("CHBLAN_10", "Modérer les messages");
+define("CHBLAN_11", "Nombre de messages à afficher");
+define("CHBLAN_12", "Nombre maximum de messages à afficher dans le Chatbox");
+define("CHBLAN_13", "Remplacer les liens");
+define("CHBLAN_14", "si coché, les liens envoyés seront remplacés par le texte du champ ci-dessous");
+define("CHBLAN_15", "Chaîne de remplacement si activé");
+define("CHBLAN_16", "les liens seront remplacé par cette chaîne de caractères");
+define("CHBLAN_17", "Taille de cessure");
+define("CHBLAN_18", "les mots plus longs que ce nombre seront découpés automatiquement");
+define("CHBLAN_19", "Valider les paramètres du Chatbox");
+define("CHBLAN_20", "Préférences du Chatbox");
+define("CHBLAN_21", "Délester");
+define("CHBLAN_22", "Supprimer les messages plus anciens qu'un certain laps de temps");
+define("CHBLAN_23", "Supprimer les messages agés de plus de");
+
+define("CHBLAN_24", "Un jour");
+define("CHBLAN_25", "Une semaine");
+define("CHBLAN_26", "Un mois");
+define("CHBLAN_27", "- Supprimer tous les messages -");
+define("CHBLAN_28", "Le Chatbox a été délesté.");
+
+define("CHBLAN_29", "Afficher le Chatbox avec une barre de défilement");
+define("CHBLAN_30", "Hauteur du Chatbox");
+define("CHBLAN_31", "Afficher les smileys");
+define("CHBLAN_32", "Groupe modérateur");
+
+?>
