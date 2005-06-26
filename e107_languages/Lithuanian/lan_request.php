e05e777876129eb209cddf6115b6b65623b3b5ff
g3d45 <g3d45@localhost>
2005-06-26 22:22:08 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4129

--- e107_languages/Lithuanian/lan_request.php
+++ e107_languages/Lithuanian/lan_request.php
@@ -4,16 +4,16 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Lithuanian/lan_request.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-06-07 21:01:03 $
-|     $Author: alkas $
+|     $Revision: 1.4 $
+|     $Date: 2005-06-26 22:22:08 $
+|     $Author: g3d45 $
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_dl_61", "Download Error");
-define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded you download quota");
-define("LAN_dl_63", "You do not have the correct permissions to download this file.");
-define("LAN_dl_64", "Back");
-define("LAN_dl_65", "File Not Found");
+define("LAN_dl_61", "Siuntimo klaida");
+define("LAN_dl_62", "Jums neleidþiama siøsti ðio failo, nes virðijote savo siuntimosi kvotà");
+define("LAN_dl_63", "Jûs neturite tinkamo leidimo siøstis ðá failà.");
+define("LAN_dl_64", "Atgal");
+define("LAN_dl_65", "Failas nerastas");
 
 ?>
