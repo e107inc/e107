fd2e55052ec820b03a01a726a2a116fe05297d87
g3d45 <g3d45@localhost>
2005-06-26 23:09:11 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4138

--- e107_languages/Lithuanian/admin/lan_wmessage.php
+++ e107_languages/Lithuanian/admin/lan_wmessage.php
@@ -4,30 +4,30 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Lithuanian/admin/lan_wmessage.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-06-02 22:17:04 $
-|     $Author: alkas $
+|     $Revision: 1.2 $
+|     $Date: 2005-06-26 23:09:11 $
+|     $Author: g3d45 $
 +----------------------------------------------------------------------------+
 */
-// define("WMGLAN_1", "Message for Guests");
-// define("WMGLAN_2", "Message for Members");
-// define("WMGLAN_3", "Message for Administrators");
-// define("WMGLAN_4", "Submit");
-// define("WMGLAN_5", "Set Welcome Message");
-// define("WMGLAN_6", "Activate?");
-// define("WMGLAN_7", "Welcome message settings updated.");
+// define("WMGLAN_1", "Þinutë sveèiams");
+// define("WMGLAN_2", "Þinutë nariams");
+// define("WMGLAN_3", "Þinutë administratoriams");
+// define("WMGLAN_4", "Pateikti");
+// define("WMGLAN_5", "Nustatyti pasisveikinimo þinutæ");
+// define("WMGLAN_6", "Aktyvuoti?");
+// define("WMGLAN_7", "Pasisveikinimo þinutës nustatymai atnaujinti.");
 
-define("WMLAN_00","Welcome Messages");
-define("WMLAN_01","Create New Message");
-define("WMLAN_02","Message");
-define("WMLAN_03","Visibility");
-define("WMLAN_04","Message Text");
+define("WMLAN_00","Pasisveikinimo þinutës");
+define("WMLAN_01","Sukurti naujà þinutæ");
+define("WMLAN_02","Þinutë");
+define("WMLAN_03","Matomumas");
+define("WMLAN_04","Þinutës tekstas");
 
 define("WMLAN_05","Enclose");
-define("WMLAN_06","If ticked, the message will be rendered inside box");
+define("WMLAN_06","Jei paþymëta, þinutë bus pateikta dëþutës viduje");
 define("WMLAN_07","Override standard system to use {WMESSAGE} shortcode:");
-// define("WMLAN_08","Preferences");
+// define("WMLAN_08","Nuostatos");
 
-define("WMLAN_09","No welcome messages set yet");
+define("WMLAN_09","Jokiø pasisveikinimo þinuèiø dar nëra");
 
 ?>
