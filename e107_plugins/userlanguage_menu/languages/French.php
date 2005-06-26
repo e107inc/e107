f61b1d2e43319c9968b1a31b455b4d74c5622e00
tpruvot <tpruvot@localhost>
2005-06-26 03:49:29 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4096

---
+++ e107_plugins/userlanguage_menu/languages/French.php
@@ -0,0 +1,14 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File. é
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/userlanguage_menu/languages/French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-26 03:49:29 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+define("UTHEME_MENU_L1", "Appliquer");
+define("UTHEME_MENU_L2", "Langue du site");
+?>
