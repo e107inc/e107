5effe2e6054bbb44aeca9b24e8ae78b7787faab2
g3d45 <g3d45@localhost>
2005-06-27 16:56:05 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4162

---
+++ e107_plugins/recent/languages/Lithuanian.php
@@ -0,0 +1,210 @@
+<?php
+/*
++---------------------------------------------------------------+
+|	e107 website system
+|
+|	©Steve Dunstan 2001-2002
+|	http://e107.org
+|	jalist@e107.org
+|
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+
+define("RECENT_ADMIN_1", "nauja");
+define("RECENT_ADMIN_2", "atnaujinti nuostatas");
+define("RECENT_ADMIN_3", "nuostatos atnaujintos");
+define("RECENT_ADMIN_4", "sekcija");
+define("RECENT_ADMIN_5", "meniu");
+define("RECENT_ADMIN_6", "puslapis");
+define("RECENT_ADMIN_7", "ágalinta");
+define("RECENT_ADMIN_8", "neágalinta");
+define("RECENT_ADMIN_9", "atidaryta");
+define("RECENT_ADMIN_10", "uþdaryta");
+define("RECENT_ADMIN_11", "atnaujinti");
+define("RECENT_ADMIN_12", "pasirinkti");
+define("RECENT_ADMIN_13", "Sveiki atvykæ á ".SITENAME." puslapá, kuriame rodoma, kas naujo ávyko ðiose svetainës sekcijose nuo paskutinio Jûsø apsilankymo.");
+define("RECENT_ADMIN_14", "naujausi papildymai");
+
+define("RECENT_ADMIN_SECT_1", "sekcijos");
+define("RECENT_ADMIN_SECT_2", "pasirinkite, kurias sekcijas rodyti");
+define("RECENT_ADMIN_SECT_3", "");
+
+define("RECENT_ADMIN_SECT_4", "vaizdavimo stilius");
+define("RECENT_ADMIN_SECT_5", "pasirinkite, kurios sekcijos bus atidarytos, kurios uþdarytos pagal nutylëjimà");
+define("RECENT_ADMIN_SECT_6", "");
+
+define("RECENT_ADMIN_SECT_7", "autorius");
+define("RECENT_ADMIN_SECT_8", "pasirinkite, ar autorius turi bûti rodomas");
+define("RECENT_ADMIN_SECT_9", "");
+
+define("RECENT_ADMIN_SECT_10", "kategorija");
+define("RECENT_ADMIN_SECT_11", "pasirinkite, ar kategorija turi bûti rodoma");
+define("RECENT_ADMIN_SECT_12", "");
+
+define("RECENT_ADMIN_SECT_13", "data");
+define("RECENT_ADMIN_SECT_14", "pasirinkite, ar data turi bûti rodoma");
+define("RECENT_ADMIN_SECT_15", "");
+
+define("RECENT_ADMIN_SECT_16", "dalykø kiekis");
+define("RECENT_ADMIN_SECT_17", "pasirinkite, kiek dallykø turi bûti rodoma kiekvienai sekcijai");
+define("RECENT_ADMIN_SECT_18", "");
+
+define("RECENT_ADMIN_SECT_19", "dalykø tvarka");
+define("RECENT_ADMIN_SECT_20", "pasirinkite, kokia tvarka sekcijose turi bûti vaizduojama");
+define("RECENT_ADMIN_SECT_21", "");
+
+define("RECENT_ADMIN_SECT_22", "piktograma");
+define("RECENT_ADMIN_SECT_23", "pasirinkite piktogramà kiekvienai sekcijai");
+define("RECENT_ADMIN_SECT_24", "");
+
+define("RECENT_ADMIN_SECT_25", "pavadinimas");
+define("RECENT_ADMIN_SECT_26", "sugalvokite pavadinimà kiekvienai sekcijai");
+define("RECENT_ADMIN_SECT_27", "");
+
+
+define("RECENT_ADMIN_MENU_1", "meniu nuostatos");
+
+define("RECENT_ADMIN_MENU_2", "pavadinimas");
+define("RECENT_ADMIN_MENU_3", "sugalvokite pavadinimà");
+define("RECENT_ADMIN_MENU_4", "");
+
+define("RECENT_ADMIN_MENU_5", "piktograma : naudojima");
+define("RECENT_ADMIN_MENU_6", "meniu naudokite piktogramà kiekvienai sekcijai");
+define("RECENT_ADMIN_MENU_7", "");
+
+define("RECENT_ADMIN_MENU_8", "simboliai");
+define("RECENT_ADMIN_MENU_9", "pasirinkite, kiek simboliø pavadinime bus rodoma");
+define("RECENT_ADMIN_MENU_10", "palikite tuðèià, jei norite neriboti pavadinimo ilgio");
+
+define("RECENT_ADMIN_MENU_11", "praneðimo taisymas");
+define("RECENT_ADMIN_MENU_12", "pasirinkite praneðimo taisymà, jei pavadinimas yra ilgesnis nei ribojamas simboliø skaièius");
+define("RECENT_ADMIN_MENU_13", "palikite tuðèià, jei nenorite rodyti praneðimo taisymo");
+
+define("RECENT_ADMIN_MENU_14", "data");
+define("RECENT_ADMIN_MENU_15", "pasirinkite datos stiliø");
+define("RECENT_ADMIN_MENU_16", "Norëdami daugiau indormacijos apie datø formatus aplankykite <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime funkcijos puslapá php.net</a>");
+
+define("RECENT_ADMIN_MENU_17", "ðiandienos data");
+define("RECENT_ADMIN_MENU_18", "pasirinkite datos stiliø, jei data ðiandienos");
+define("RECENT_ADMIN_MENU_19", "Norëdami daugiau indormacijos apie datø formatus aplankykite <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime funkcijos puslapá php.net</a>");
+
+define("RECENT_ADMIN_MENU_20", "rodyti tuðèià");
+define("RECENT_ADMIN_MENU_21", "paþymëkite, jei norite sekcijos be rezultatø naudoti þinutæ");
+define("RECENT_ADMIN_MENU_22", "");
+
+define("RECENT_ADMIN_MENU_23", "piktograma : numatytoji");
+define("RECENT_ADMIN_MENU_24", "naudoti numatytàjá temos þenklelá, jei jokios piktogramos nëra ar piktograma:naudojama yra neágalinta");
+define("RECENT_ADMIN_MENU_25", "");
+
+
+
+define("RECENT_ADMIN_PAGE_1", "puslapio nuostatos");
+
+define("RECENT_ADMIN_PAGE_2", "pavadinimas");
+define("RECENT_ADMIN_PAGE_3", "sugalvokite pavadinimà");
+define("RECENT_ADMIN_PAGE_4", "");
+
+define("RECENT_ADMIN_PAGE_5", "piktograma : naudojima");
+define("RECENT_ADMIN_PAGE_6", "puslapyje naudokite piktogramà ið kiekvienos sekcijos");
+define("RECENT_ADMIN_PAGE_7", "");
+
+define("RECENT_ADMIN_PAGE_8", "characters");
+define("RECENT_ADMIN_PAGE_9", "choose how many characters of the heading will be shown");
+define("RECENT_ADMIN_PAGE_10", "leave blank to show full heading");
+
+define("RECENT_ADMIN_PAGE_11", "postfix");
+define("RECENT_ADMIN_PAGE_12", "choose a postfix if the heading is larger then the given amount of characters");
+define("RECENT_ADMIN_PAGE_13", "leave blank to show no postfix");
+
+define("RECENT_ADMIN_PAGE_14", "date");
+define("RECENT_ADMIN_PAGE_15", "choose a date style");
+define("RECENT_ADMIN_PAGE_16", "For more information on date formats see the <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime function page at php.net</a>");
+
+define("RECENT_ADMIN_PAGE_17", "today date");
+define("RECENT_ADMIN_PAGE_18", "choose a date style if the date is today");
+define("RECENT_ADMIN_PAGE_19", "For more information on date formats see the <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime function page at php.net</a>");
+
+define("RECENT_ADMIN_PAGE_20", "colomns");
+define("RECENT_ADMIN_PAGE_21", "choose an amount of colomns");
+define("RECENT_ADMIN_PAGE_22", "define how many colomns you want to use. the number you specify will seperate the page into an equal amount of colomns");
+
+define("RECENT_ADMIN_PAGE_23", "welcome text");
+define("RECENT_ADMIN_PAGE_24", "define a welcome text that will be rendered at the top of the page");
+define("RECENT_ADMIN_PAGE_25", "");
+
+define("RECENT_ADMIN_PAGE_26", "show empty");
+define("RECENT_ADMIN_PAGE_27", "define if for sections with no results a message needs to be shown");
+define("RECENT_ADMIN_PAGE_28", "");
+
+define("RECENT_ADMIN_PAGE_29", "icon : default");
+define("RECENT_ADMIN_PAGE_30", "use default theme bullet if no icon is present or if icon:use is disabled");
+define("RECENT_ADMIN_PAGE_31", "");
+
+
+
+
+define("RECENT_MENU_1", "recent additions");
+define("RECENT_MENU_2", "by");
+define("RECENT_MENU_3", "on");
+define("RECENT_MENU_4", "in");
+define("RECENT_MENU_5", "");
+define("RECENT_MENU_6", "");
+define("RECENT_MENU_7", "");
+define("RECENT_MENU_8", "");
+define("RECENT_MENU_9", "");
+define("RECENT_MENU_10", "");
+define("RECENT_MENU_11", "");
+define("RECENT_MENU_12", "");
+define("RECENT_MENU_13", "");
+define("RECENT_MENU_14", "");
+define("RECENT_MENU_15", "");
+define("RECENT_MENU_16", "");
+define("RECENT_MENU_17", "");
+define("RECENT_MENU_18", "");
+define("RECENT_MENU_19", "");
+
+define("RECENT_NEWS_1", "news");
+define("RECENT_NEWS_2", "no news items");
+
+define("RECENT_COMMENT_1", "comments");
+define("RECENT_COMMENT_2", "no comments");
+define("RECENT_COMMENT_3", "news");
+define("RECENT_COMMENT_4", "faq");
+define("RECENT_COMMENT_5", "poll");
+define("RECENT_COMMENT_6", "docs");
+define("RECENT_COMMENT_7", "bugtrack");
+define("RECENT_COMMENT_8", "content");
+define("RECENT_COMMENT_9", "download");
+define("RECENT_COMMENT_10", "");
+
+
+
+define("RECENT_DOWNLOAD_1", "downloads");
+define("RECENT_DOWNLOAD_2", "no downloads");
+
+define("RECENT_MEMBER_1", "members");
+define("RECENT_MEMBER_2", "no members");
+
+define("RECENT_CONTENT_1", "content");
+define("RECENT_CONTENT_2", "no content");
+
+define("RECENT_CHATBOX_1", "chatbox");
+define("RECENT_CHATBOX_2", "no chatbox posts");
+
+define("RECENT_CALENDAR_1", "calendar");
+define("RECENT_CALENDAR_2", "no calendar events");
+
+define("RECENT_LINKS_1", "links");
+define("RECENT_LINKS_2", "no links");
+
+define("RECENT_FORUM_1", "forum");
+define("RECENT_FORUM_2", "no forum posts");
+define("RECENT_FORUM_3", "views:");
+define("RECENT_FORUM_4", "replies:");
+define("RECENT_FORUM_5", "lastpost:");
+define("RECENT_FORUM_6", "on:");
+
+
+?>
