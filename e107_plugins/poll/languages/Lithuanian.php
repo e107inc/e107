4ac4d537cfde324d720f1976217067da3ba3188b
g3d45 <g3d45@localhost>
2005-06-27 21:05:04 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4175

---
+++ e107_plugins/poll/languages/Lithuanian.php
@@ -0,0 +1,74 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/poll/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-27 21:05:04 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+
+define("POLL_ADLAN01", "Apklausa");
+define("POLL_ADLAN02", "Apklausos áskiepis leidþia panaudoti apklausas svetainës meniu ar diskusijose.");
+define("POLL_ADLAN03", "Konfiguruoti apklausas");
+define("POLL_ADLAN04", "Apklausos áskiepis buvo sëkmingai ádiegtas. Norint ádëti apklausas spauskite ant Apklausos piktogramos áskiepiø sekcijoje (adminstratoriaus meniu) ir nepamirðkite aktyvuoti meniu administratoriaus Meniu sekcijoje.");
+
+define("POLLAN_MENU_CAPTION", "Apklausa");
+
+define("POLLAN_1", "Esamos apklausos");
+define("POLLAN_2", "Sukurti / keisti apklausas");
+define("POLLAN_3", "Apklausos klausimas");
+define("POLLAN_4", "Parinktys");
+define("POLLAN_5", "Keisti");
+define("POLLAN_6", "Trinti");
+define("POLLAN_7", "Apklausø dar nëra.");
+define("POLLAN_8", "Áterpti dar vienà pasirinkimà");
+define("POLLAN_9", "Leisti daugiariopus pasirinkimus?");
+define("POLLAN_10", "taip");
+define("POLLAN_11", "ne");
+define("POLLAN_12", "Rodyti rezultatus");
+define("POLLAN_13", "po balsavimo");
+define("POLLAN_14", "paspaudus rezultatø rodymo nuorodà - komentarai turi bûti ágalinti");
+define("POLLAN_15", "Leisti balsuoti ðioje apklausoje");
+define("POLLAN_16", "Balsø laikymo metodas");
+define("POLLAN_17", "cookie");
+define("POLLAN_18", "IP adresas");
+define("POLLAN_19", "Vartotojo ID (tik nariai gali balsuoti)");
+define("POLLAN_20", "Leisti komentuoti ðià apklausà?");
+define("POLLAN_21", "Perþiûrëti vël");
+define("POLLAN_22", "Atnaujinti apklausà");
+define("POLLAN_23", "Sukurti apklausà");
+define("POLLAN_24", "Perþiûrëti");
+define("POLLAN_25", "Iðvalyti laukelius");
+define("POLLAN_26", "balsai");
+define("POLLAN_27", "komentarai");
+define("POLLAN_28", "Ankstesnës apklausos");
+define("POLLAN_29", "paskelbta");
+define("POLLAN_30", "Pateikti");
+define("POLLAN_31", "balsai");
+define("POLLAN_32", "Paspauskite norëdami pamatyti rezultatus");
+define("POLLAN_33", "Ankstensiø apklausø nëra.");
+define("POLLAN_34", "Pavadinimas");
+define("POLLAN_35", "Paskelbta");
+define("POLLAN_36", "Aktyvuota");
+define("POLLAN_37", "aktyvuoti ið");
+define("POLLAN_38", "to");
+define("POLLAN_39", "Aèiû uþ balsà!");
+define("POLLAN_40", "Paspauskite norëdami pamatyti rezultatus");
+
+define("POLLAN_41", "Ði apklausa tik nariams");
+define("POLLAN_42", "Ði apklausa tik administratoriams");
+define("POLLAN_43", "Jûs neturite leidimo balsuoti èia");
+
+define("POLLAN_44", "Iðtrinti ðià apklausà?");
+
+?>
