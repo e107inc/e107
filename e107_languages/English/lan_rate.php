93e73dcbf880c78032d6f4c23e96da3d34856272
lisa_ <lisa_@localhost>
2005-06-27 11:45:50 +0000

    rate: forgot one lan
    svn path=/trunk/e107_0.7/; revision=4153

--- e107_languages/English/lan_rate.php
+++ e107_languages/English/lan_rate.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_rate.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-14 16:06:17 $
+|     $Revision: 1.3 $
+|     $Date: 2005-06-27 11:45:50 $
 |     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
@@ -15,5 +15,6 @@ define("RATELAN_1", "votes");
 define("RATELAN_2", "how do you rate this item?");
 define("RATELAN_3", "thank you for your vote");
 define("RATELAN_4", "not rated");
+define("RATELAN_5", "Rate");
 
 ?>
