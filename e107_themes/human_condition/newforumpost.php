47179415193d759dbc895ae21ba3c6fd823e8efc
mcfly_e107 <mcfly_e107@localhost>
2005-06-28 18:59:09 +0000

    language fixes
    svn path=/trunk/e107_0.7/; revision=4217

--- e107_themes/human_condition/newforumpost.php
+++ e107_themes/human_condition/newforumpost.php
@@ -3,7 +3,7 @@
 $NEWFORUMPOSTSTYLE_HEADER = "
 <!-- newforumposts -->\n<ul type='square'>\n";
 
-$NEWFORUMPOSTSTYLE_MAIN = "<li><span class='smalltext'>{THREAD} by {POSTER} [ views: {VIEWS}, replies: {REPLIES}, lastpost: {LASTPOST} ]\n</span>\n</li>\n";
+$NEWFORUMPOSTSTYLE_MAIN = "<li><span class='smalltext'>{THREAD} ".LAN_7." {POSTER} [ ".LAN_3.": {VIEWS}, ".LAN_4.": {REPLIES}, ".LAN_5.": {LASTPOST} ]\n</span>\n</li>\n";
 
 $NEWFORUMPOSTSTYLE_FOOTER = "<br /><br />\n</ul><span class='smalltext'>".LAN_6.": <b>{TOTAL_TOPICS}</b> | ".LAN_4.": <b>{TOTAL_REPLIES}</b> | ".LAN_3.": <b>{TOTAL_VIEWS}</b></span>";
 
