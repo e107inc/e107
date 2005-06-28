8c99e1a943cdeca3f8c772d965a2ce8a052fbfe1
g3d45 <g3d45@localhost>
2005-06-28 22:09:10 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4235

---
+++ e107_plugins/login_menu/languages/Lithuanian.php
@@ -0,0 +1,62 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/login_menu/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-28 22:09:10 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+	
+define("LOGIN_MENU_L1", "Vardas: ");
+define("LOGIN_MENU_L2", "Slaptaþodis: ");
+define("LOGIN_MENU_L3", "Registruotis");
+define("LOGIN_MENU_L4", "Pamirðote slaptaþodá?");
+define("LOGIN_MENU_L5", "Sveiki,");
+define("LOGIN_MENU_L6", "Prisiminti mane");
+define("LOGIN_MENU_L7", "Unikalus vartotojo id neatpaþintas (gali bûti paþeistas slapukas (cookie).<br />Norëdami iðtaisyti ðià klaidà, praðome <a href=\"".e_BASE."index.php?logout\">paspausti èia</a>.");
+define("LOGIN_MENU_L8", "Atsijungti");
+define("LOGIN_MENU_L9", "Prisijungimo klaida");
+define("LOGIN_MENU_L10", "Vyksta prieþiûros darbai; tai reiðkia, kad eiliniai lankytojai nukreipiami á sitedown.php. Norëdami uþbaigti ðiuos darbus eikite á administravimo skyriø -> Prieþiûra.");
+define("LOGIN_MENU_L11", "Administravimas");
+define("LOGIN_MENU_L12", "Nustatymai");
+define("LOGIN_MENU_L13", "Dosjë");
+define("LOGIN_MENU_L14", "naujienø praneðimas");
+define("LOGIN_MENU_L15", "naujienø praneðimai");
+define("LOGIN_MENU_L16", "pokalbiø praneðimas");
+define("LOGIN_MENU_L17", "pokalbiø praneðimai");
+define("LOGIN_MENU_L18", "komentaras");
+define("LOGIN_MENU_L19", "komentarai");
+define("LOGIN_MENU_L20", "praneðimas forume");
+define("LOGIN_MENU_L21", "praneðimai forume");
+define("LOGIN_MENU_L22", "naujas tinklalapio narys");
+define("LOGIN_MENU_L23", "nauji tinklalapio nariai");
+define("LOGIN_MENU_L24", "Spauskite èia, jei norite pamatyti kas naujo");
+define("LOGIN_MENU_L25", "Nuo paskutinio jûsø apsilankymo tinklalapyje atsirado");
+define("LOGIN_MENU_L26", "0");
+define("LOGIN_MENU_L27", "ir");
+define("LOGIN_MENU_L28", "Prisijungti");
+
+define("LOGIN_MENU_L29", "naujas straipsnis");
+define("LOGIN_MENU_L30", "nauji straipsniai");
+	
+// New config options
+define('LOGIN_MENU_L31', 'Rodyti naujus naujienø praneðimus');
+define('LOGIN_MENU_L32', 'Rodyti naujus straipsniø praneðimus');
+define('LOGIN_MENU_L33', 'Rodyti naujus pokalbiø praneðimus');
+define('LOGIN_MENU_L34', 'Rodyti naujus komentarø praneðimus');
+define('LOGIN_MENU_L35', 'Rodyti naujus diskusijø praneðimus');
+define('LOGIN_MENU_L36', 'Rodyti naujus narius');
+define('LOGIN_MENU_L37', 'Atnaujinti nuostatas');
+define('LOGIN_MENU_L38', 'Nuostatos atnaujintos');
+	
+?>
