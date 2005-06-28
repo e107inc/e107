db90fc25b5a39fcc2f8cde64b7b84517bf16dabe
g3d45 <g3d45@localhost>
2005-06-28 21:02:58 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4228

---
+++ e107_plugins/newsfeed/languages/English_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/English_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-28 21:01:01 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "News Feeds");
+define("NWSF_FP_2", "main page");
+
+?>
---
+++ e107_plugins/newsfeed/languages/Lithuanian_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/Lithuanian_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-28 21:02:58 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "Naujienø ðaltinis");
+define("NWSF_FP_2", "pagrindinis puslapis");
+
+?>
