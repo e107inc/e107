759c90eb7d5868ea5f5fc0908543cf89decab4d3
g3d45 <g3d45@localhost>
2005-06-28 20:50:19 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4225

---
+++ e107_plugins/pm_menu/languages/admin/Lithuanian.php
@@ -0,0 +1,52 @@
+﻿<?php
+	
+define(PMLAN_PM, "Asmeninė žinutė");
+define(PM_ADLAN_PM, "Asmeninis susirašinėjimas");
+define(PM_ADLAN_1, "AP sąranka");
+define(PM_ADLAN_2, "AP administravimas");
+define(PM_ADLAN_3, "Atnaujinti AP nuostatas");
+define(PM_ADLAN_4, "Pasirinkti vartotoją");
+define(PM_ADLAN_5, "Visos žinutės ištrintos");
+define(PM_ADLAN_6, "Žinutės kam/nuo");
+define(PM_ADLAN_7, "ištrinta");
+define(PM_ADLAN_8, "Žinutės senesnės nei ");
+define(PM_ADLAN_9, "dienos(ų)");
+define(PM_ADLAN_10, "Tai ištrins visas žinutes daugiau nei ");
+define(PM_ADLAN_11, "Patvirtinti trinimą");
+define(PM_ADLAN_12, "Nutraukti");
+define(PM_ADLAN_13, "Jūs privalote įvesti reikšmę.");
+define(PM_ADLAN_14, "Ištrinti žinutes");
+define(PM_ADLAN_15, "Tai ištrins visas žinutes iš/nuo šio vartotojo: ");
+define(PM_ADLAN_16, "Jūs pirma turite pasirinkti vartotoją.");
+define(PM_ADLAN_17, "Ištrinti žinutes iš/nuo šio vartotojo");
+define(PM_ADLAN_18, "Tai ištrins visas žinutes iš AP sistemos, net jei jos bus neperskaitytos.");
+define(PM_ADLAN_19, "Ištrinti visas žinutes");
+define(PM_ADLAN_20, "AP nuostatos atnaujintos");
+define(PM_ADLAN_21, "Įskiepio pavadinimas");
+define(PM_ADLAN_22, "Rodyti naujo AP animaciją");
+define(PM_ADLAN_23, "Rodyti vartotojo išsiskleidžiantį sąrašą");
+define(PM_ADLAN_24, "Automatiškai ištrinti skaitytas žinutes");
+define(PM_ADLAN_25, "Skaitytų žinučių timeout'as");
+define(PM_ADLAN_26, "Neskaitytų žinučių timeout'as");
+define(PM_ADLAN_27, "Iššokančio lango priminimas gavus naują AP");
+define(PM_ADLAN_28, "Iššokančio lango vėlavimo laikas");
+define(PM_ADLAN_29, "Apriboti AP");
+define(PM_ADLAN_30, "Visi registruoti vartotojai");
+define(PM_ADLAN_31, "Vartotojo kategorija");
+define(PM_ADLAN_32, "Ištrinti žinutes daugiau nei");
+define(PM_ADLAN_33, "Taip");
+define(PM_ADLAN_34, "Ne");
+define(PM_ADLAN_35, "Siųsti e-pašto Send email perspėjimus");
+define(PM_ADLAN_36, "Prašome pažymėti operacijos patvirtinimui");
+define(PM_ADLAN_37, "žinutės sistemoje");
+define(PM_ADLAN_38, "Rasti vartotojo vardą");
+define(PM_ADLAN_39, "dienos(ų)");
+define(PM_ADLAN_40, "neįgalinta");
+define(PM_ADLAN_41, "sekundės(ių)");
+define(PM_ADLAN_42, "Šis įskiepis yra pilnai veikianti asmeninių pranešimų sistema.");
+define(PM_ADLAN_43, "Konfiguruoti asmeninių pranešimų gaviklį");
+define(PM_ADLAN_44, "Norėdami aktyvuoti eikite į savo svetainės administravimo meniu ir parinkite pm_menu viename iš meniu zonų.");
+define(PM_ADLAN_45, "AP sėkmingai patobulintas naujesne versija");
+	
+	
+?>
