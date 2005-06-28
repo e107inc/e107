bd3311d176fbbef539f99d86af697111daad0a1d
mrpiercer <mrpiercer@localhost>
2005-06-28 16:54:56 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4214

--- e107_languages/Swedish/lan_user_extended.php
+++ e107_languages/Swedish/lan_user_extended.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Swedish/lan_user_extended.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-06-26 11:12:22 $
+|     $Revision: 1.4 $
+|     $Date: 2005-06-28 16:54:56 $
 |     $Author: mrpiercer $
 +----------------------------------------------------------------------------+
 */
@@ -18,14 +18,25 @@ define("UE_LAN_6", "Heltal");
 define("UE_LAN_7", "Datum");
 define("UE_LAN_8", "Språk");
 
+define("UE_LAN_9", "Namn");
+define("UE_LAN_10", "Typ");
+define("UE_LAN_11", "Använd");
+
 define("UE_LAN_HIDE", "Dölj från användare");
 
 define("UE_LAN_LOCATION", "Plats");
-define("UE_LAN_AIM", "AIM");
-define("UE_LAN_ICQ", "ICQ");
-define("UE_LAN_YAHOO", "Yahoo!");
+define("UE_LAN_LOCATION_DESC", "Användarens ort");
+define("UE_LAN_AIM", "AIM adress");
+define("UE_LAN_AIM_DESC", "AIM adress");
+define("UE_LAN_ICQ", "ICQ nummer");
+define("UE_LAN_ICQ_DESC", "ICQ nummer");
+define("UE_LAN_YAHOO", "Yahoo! adress");
+define("UE_LAN_YAHOO_DESC", "Yahoo! adress");
 define("UE_LAN_MSN", "MSN");
+define("UE_LAN_MSN_DESC", "MSN adress");
 define("UE_LAN_HOMEPAGE", "Hemsida");
+define("UE_LAN_HOMEPAGE_DESC", "URL till användarens hemsida");
 define("UE_LAN_BIRTHDAY", "Födelsedag");
+define("UE_LAN_BIRTHDAY_DESC", "Födelsedag");
 
 ?>
