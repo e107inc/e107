8de6d740d403e370d554e8d5d02a3c0aa1748855
mrpiercer <mrpiercer@localhost>
2005-06-29 08:26:34 +0000

    New file
    svn path=/trunk/e107_langpacks/; revision=4241

---
+++ e107_plugins/lastseen/languages/Swedish.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/lastseen/languages/Swedish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-29 08:26:34 $
+|     $Author: mrpiercer $
++----------------------------------------------------------------------------+
+*/
+
+define("LSP_LAN_1", "Senaste besökare");
+
+?>
