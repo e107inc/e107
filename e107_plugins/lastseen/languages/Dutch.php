06aef0e1760084adaef786285412dfcd21a5dfc2
mijnheer <mijnheer@localhost>
2005-06-29 04:55:54 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=4239

---
+++ e107_plugins/lastseen/languages/Dutch.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/lastseen/languages/Dutch.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-06-29 04:55:54 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+
+define("LSP_LAN_1", "Laatst gezien");
+
+
+?>
