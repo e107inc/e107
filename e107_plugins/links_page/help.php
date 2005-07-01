4e19a5187d4f1692254bc8eaa7345a5e395cb2d1
lisa_ <lisa_@localhost>
2005-07-01 08:05:47 +0000

    links_page : added the help file
    svn path=/trunk/e107_0.7/; revision=4301

---
+++ e107_plugins/links_page/help.php
@@ -0,0 +1,42 @@
+<?php
+
+if(!e_QUERY){
+	$text = LAN_ADMIN_HELP_1;
+}else{
+	$qs = explode(".", e_QUERY);
+
+	//##### LINK --------------------------------------------------
+		//manage Link items
+		if($qs[0] == "link" && !isset($qs[1]) ){
+			$text = LAN_ADMIN_HELP_3;
+		//edit
+		}elseif($qs[0] == "link" && $qs[1] == "edit" && is_numeric($qs[2]) ){
+			$text = LAN_ADMIN_HELP_9;
+		//view links in cat
+		}elseif($qs[0] == "link" && $qs[1] == "view" && is_numeric($qs[2]) ){
+			$text = LAN_ADMIN_HELP_8;
+		//create
+		}elseif($qs[0] == "link" && $qs[1] == "create" && !isset($qs[2])){
+			$text = LAN_ADMIN_HELP_4;
+		//create/post submitted
+		}elseif($qs[0] == "link" && $qs[1] == "sn" && is_numeric($qs[2])){
+			$text = LAN_ADMIN_HELP_10;
+
+	//##### SUBMITTED --------------------------------------------------
+		}elseif($qs[0] == "sn" && !isset($qs[1]) ){
+			$text = LAN_ADMIN_HELP_5;
+
+	//##### OPTION --------------------------------------------------
+		}elseif($qs[0] == "opt" && !isset($qs[1]) ){
+			$text = LAN_ADMIN_HELP_6;
+
+	//##### CATEGORY --------------------------------------------------
+		}elseif($qs[0] == "cat" && $qs[1] == "create" ){
+			$text = LAN_ADMIN_HELP_2;
+		}elseif($qs[0] == "cat" && $qs[1] == "edit" && is_numeric($qs[2]) ){
+			$text = LAN_ADMIN_HELP_7;
+		}
+}
+$ns -> tablerender(LAN_ADMIN_HELP_0, $text);
+
+?>
