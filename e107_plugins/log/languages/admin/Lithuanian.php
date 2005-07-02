79df40c165dbf102f54e18bda87cb687d9435db8
g3d45 <g3d45@localhost>
2005-07-02 11:37:10 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4333

--- e107_plugins/log/languages/admin/Lithuanian.php
+++ e107_plugins/log/languages/admin/Lithuanian.php
@@ -11,42 +11,42 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/log/languages/admin/Lithuanian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-02 10:45:12 $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-02 11:37:10 $
 |     $Author: g3d45 $
 +----------------------------------------------------------------------------+
 */
 	
-define("ADSTAT_ON", "On");
-define("ADSTAT_OFF", "Off");
-define("ADSTAT_L1", "This plugin will log all visits to your site, and build detailed statistic screens based on the information gathered.");
-define("ADSTAT_L2", "The statistics logger has been successfully installed. To activate, please go to the config screen and click Activate.<br /><b>You must set the permissions of the e107_plugins/log/logs folder to 777 (chmod 777)</b>");
-define("ADSTAT_L3", "Statistic Logging");
-define("ADSTAT_L4", "Activate statistic logging");
-define("ADSTAT_L5", "Statistic types");
-define("ADSTAT_L6", "Browsers");
-define("ADSTAT_L7", "Operating Systems");
-define("ADSTAT_L8", "Screen resolutions / depths");
-define("ADSTAT_L9", "Countries/domains visited from");
-define("ADSTAT_L10", "Referrers");
-define("ADSTAT_L11", "Search queries");
-define("ADSTAT_L12", "Reset stats");
-define("ADSTAT_L13", "this will erase stats - careful!");
-define("ADSTAT_L14", "Page counts");
-define("ADSTAT_L15", "Update Statistic Settings");
-define("ADSTAT_L16", "Site Statistic Settings");
-define("ADSTAT_L17", "Statistic settings updated");
-define("ADSTAT_L18", "Allow access to main statistics page to ...");
-define("ADSTAT_L19", "Recent visitors");
-define("ADSTAT_L20", "Count admin visits");
-define("ADSTAT_L21", "Maximum records to display on stats page");
-define("ADSTAT_L22", "Run update routine");
-define("ADSTAT_L23", "logs from a previous version of e107 have been detected, update them here");
-define("ADSTAT_L24", "Go to update script");
-define("ADSTAT_L25", "Selected stats reset");
+define("ADSTAT_ON", "Ájungti");
+define("ADSTAT_OFF", "Iðjungti");
+define("ADSTAT_L1", "Ðis áskiepis áraðys visà informacijà apie apsilankymus Jûsø svetainëje ir pateiks iðsamius statistinius duomenis.");
+define("ADSTAT_L2", "Statistikos áskiepis sëkmingai ádiegtas. Norëdami aktyvuoti praðome eiti á sàrankos meniu ir paspausti Aktyvuoti.<br /><b>Jûs turite nustatyti katalogo e107_plugins/log/logs prieinamumà á 777 (chmod 777)</b>");
+define("ADSTAT_L3", "Statistikos rinkimas");
+define("ADSTAT_L4", "Aktyvuoti statistikos rinkimas");
+define("ADSTAT_L5", "Statistikos tipai");
+define("ADSTAT_L6", "Narðyklës");
+define("ADSTAT_L7", "Operacinës sistemos");
+define("ADSTAT_L8", "Ekrano raiðka / Screen resolutions / depths");
+define("ADSTAT_L9", "Ðalys/domenai, ið kuriø aplankoma");
+define("ADSTAT_L10", "Ið kur aplanko");
+define("ADSTAT_L11", "Paieðkos uþklausos");
+define("ADSTAT_L12", "Iðvalyti statistikos duomenis");
+define("ADSTAT_L13", "tai iðtrins statistikos duomenis - atsargiai!");
+define("ADSTAT_L14", "Puslapiø iðnarðyta");
+define("ADSTAT_L15", "Atnaujinti statistikos nuostatas");
+define("ADSTAT_L16", "Svetainës statistikos nuostatos");
+define("ADSTAT_L17", "Ssstatistikos nuostato atnaujintoss");
+define("ADSTAT_L18", "Leisti priëjimà prie pagrindinio statistikos puslapio ...");
+define("ADSTAT_L19", "Vëliausi lankytojai");
+define("ADSTAT_L20", "Skaièiuoti administratoriaus apsilankymus");
+define("ADSTAT_L21", "Rodomø áraðø statistikos puslapyje limitas");
+define("ADSTAT_L22", "Paleisti atnaujinimo paprogramæ");
+define("ADSTAT_L23", "áraðus ið ankstenës rastos e107 versijos atnaujinti èia");
+define("ADSTAT_L24", "Eiti á atnaujinimo programà");
+define("ADSTAT_L25", "Pasirinkti statistikos duomenys renkami ið naujo");
 
-define("ADSTAT_L26", "Remove page entries");
-define("ADSTAT_L27", "if your stats have incorrect pages, you can remove them here");
-define("ADSTAT_L28", "Open page");
+define("ADSTAT_L26", "Paðalinti puslpaio áraðus");
+define("ADSTAT_L27", "jei Jûsø statistika turi neteisingus puslapius, Jûs galite paðalinti juos èia");
+define("ADSTAT_L28", "Atverti puslapá");
 
 ?>
