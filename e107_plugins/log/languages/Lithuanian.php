62108e96abecd4ad3cd853459d00cd2119481165
g3d45 <g3d45@localhost>
2005-07-02 08:44:03 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4327

---
+++ e107_plugins/log/languages/Lithuanian.php
@@ -0,0 +1,63 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/log/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-02 08:44:03 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+
+define("PAGE_NAME", "Statistika");
+	
+define("ADSTAT_L1", "Ðis áskiepis áraðys visà informacijà apie apsilankymus Jûsø svetainëje ir pateiks iðsamius statistinius duomenis.");
+define("ADSTAT_L2", "Statistikos áskiepis sëkmingai ádiegtas. Norëdami perversti Jûsø esamus statistikos duomenis praðome <a href='".e_PLUGIN."log/update_routine.php'>paspausti èia ir atnaujinti paprogramæ</a>.");
+define("ADSTAT_L3", "Statistikos prijungimas");
+define("ADSTAT_L4", "Jûs neturite leidimo perþiûrëti ðio puslapio.");
+define("ADSTAT_L5", "Ðio puslapio funkcijos iðjungtos.");
+define("ADSTAT_L6", "Svetainës statisktika");
+define("ADSTAT_L7", "Ðio tipo statistika nerenkama.");
+define("ADSTAT_L8", "Ðiandienos statistika");
+define("ADSTAT_L9", "Bendra statistika");
+define("ADSTAT_L10", "Dienos statistika");
+define("ADSTAT_L11", "Mënesio statistika");
+define("ADSTAT_L12", "Narðykliø statistika");
+define("ADSTAT_L13", "Operaciniø sistemø statistika");
+define("ADSTAT_L14", "Domenø statistika");
+define("ADSTAT_L15", "Ekrano raiðkos statistika");
+define("ADSTAT_L16", "Ið kur ateina lankytojai");
+define("ADSTAT_L17", "Paieðkos statistika");
+define("ADSTAT_L18", "Paskutiniai lankytojai");
+define("ADSTAT_L19", "Puslapis");
+define("ADSTAT_L20", "Apsilankymai ðiandien");
+define("ADSTAT_L21", "Ið viso");
+define("ADSTAT_L22", "Unikalûs");
+define("ADSTAT_L23", "Ið viso apsilankymø");
+define("ADSTAT_L24", "Ið viso unikaliø apsilankymø");
+define("ADSTAT_L25", "Statistikos nesurinkta.");
+define("ADSTAT_L26", "Narðyklës");
+define("ADSTAT_L27", "Operacinës sistemos");
+define("ADSTAT_L28", "Ðalys / domenai");
+define("ADSTAT_L29", "Ekrano raiðka");
+define("ADSTAT_L30", "Ið kur apsilanko");
+define("ADSTAT_L31", "Paieðkos uþklausos");
+define("ADSTAT_L32", "Apsilankë ið");
+define("ADSTAT_L33", "Vëliausi apsilankymai");
+define("ADSTAT_L34", "Apsilankymai");
+define("ADSTAT_L35", "Vëliausi uniklaûs apsilankymai");
+define("ADSTAT_L36", "dienø per puslapá");
+define("ADSTAT_L37", "Apsilankymai per mënesá");
+define("ADSTAT_L38", "Uniklaûs apsilankymai per mënesá");
+define("ADSTAT_L39", "paðalinti ðá áraðà");
+define("ADSTAT_L40", "dienos");
+
+?>
