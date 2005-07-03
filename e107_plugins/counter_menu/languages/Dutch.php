6731148929770a9a6d970692d09f2b1c313ed502
mijnheer <mijnheer@localhost>
2005-07-03 11:46:27 +0000

    Update july 3rd
    svn path=/trunk/e107_langpacks/; revision=4344

--- e107_plugins/counter_menu/languages/Dutch.php
+++ e107_plugins/counter_menu/languages/Dutch.php
@@ -11,13 +11,12 @@
 |	GNU General Public License (http://gnu.org).
 +---------------------------------------------------------------+
 */
-
-define("COUNTER_L1", "Teller");
-define("COUNTER_L2", "Vandaag");
-define("COUNTER_L3", "Altijd");
-define("COUNTER_L4", "Record");
-define("COUNTER_L5", "Logging is niet geactiveerd, ga naar het beheerscherm -> Log Stats om te activeren");
-define("COUNTER_L6", "Klik hier om te activeren.");
-define("COUNTER_L7", "Uniek");
+define("COUNTER_L1", "Beheerbezoek wordt niet geteld.");
+define("COUNTER_L2", "Deze pagina vandaag ...");
+define("COUNTER_L3", "totaal");
+define("COUNTER_L4", "Deze pagina ooit ...");
+define("COUNTER_L5", "uniek");
+define("COUNTER_L6", "Site ...");
+define("COUNTER_L7", "Teller");
 define("COUNTER_L8", "Beheerboodschap: <b>Statistiek logging is gedeactiveerd.</b><br />Om te activeren moet je de Statistiek logging plugin in je <a href='".e_ADMIN."plugin.php'>plugin manager</a> installeren, daarna kun je de functie activeren in het <a href='".e_PLUGIN."log/admin_config.php'>configuratie scherm</a>.");
 ?>
