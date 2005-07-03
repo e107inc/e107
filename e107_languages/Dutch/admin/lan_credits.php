346ba461da8bdbc922f39317edcb1c5b726f3a62
mijnheer <mijnheer@localhost>
2005-07-03 12:22:32 +0000

    Update july 3rd
    svn path=/trunk/e107_langpacks/; revision=4346

--- e107_languages/Dutch/admin/lan_credits.php
+++ e107_languages/Dutch/admin/lan_credits.php
@@ -4,12 +4,12 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Dutch/admin/lan_credits.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-10 21:02:23 $
+|     $Revision: 1.3 $
+|     $Date: 2005-07-03 12:22:32 $
 |     $Author: mijnheer $
 +----------------------------------------------------------------------------+
 */
-define("CRELAN_1", "Credits");
+define("CRELAN_1", "Verantwoording");
 define("CRELAN_2", "Hier een overzicht van de software / componenten van derden die in e107 worden toegepast. Het e107 ontwikkelteam wil de ontwikkelaars van de volgende componenten hartelijk danken voor de toestemming om hun code met e107 te distribueren en voor het onder de GPL licentie publiceren van hun software.");
 define("CRELAN_7", "versie");
 
