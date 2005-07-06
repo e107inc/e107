682a88dff5640acd3fb524f80132861d86364291
e107dk <e107dk@localhost>
2005-07-06 21:54:04 +0000

    Added file to cvs
    svn path=/trunk/e107_langpacks/; revision=4423

---
+++ e107_plugins/admin_menu/languages/Danish.php
@@ -0,0 +1,41 @@
+<?php
+/*
++---------------------------------------------------------------+
+|	e107 website system
+|
+|	�Steve Dunstan 2001-2002
+|	http://e107.org
+|	jalist@e107.org
+|
+|	Released under the terms and conditions of the
+|	GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+
+define("ADMIN_MENU_L1", "Vælge ...");
+define("ADMIN_MENU_L2", "Admin");
+
+define("ADMIN_MENU_L3", "Nyheder");
+define("ADMIN_MENU_L4", "Kategorier Nyheder");
+define("ADMIN_MENU_L5", "Indstillinger");
+define("ADMIN_MENU_L6", "Menuer");
+define("ADMIN_MENU_L7", "Administratorer");
+
+define("ADMIN_MENU_L8", "Admin instillinger");
+define("ADMIN_MENU_L9", "Forums");
+define("ADMIN_MENU_L10", "Artikler");
+define("ADMIN_MENU_L11", "Indhold");
+define("ADMIN_MENU_L12", "Anmeldelser");
+
+define("ADMIN_MENU_L13", "Links");
+define("ADMIN_MENU_L14", "Link Kategorier");
+define("ADMIN_MENU_L15", "Velkomst Besked");
+define("ADMIN_MENU_L16", "Upload");
+define("ADMIN_MENU_L17", "Tilføjede Nyheder");
+
+define("ADMIN_MENU_L18", "Banliste");
+define("ADMIN_MENU_L19", "Brugere");
+define("ADMIN_MENU_L20", "Vedligeholdelse");
+define("ADMIN_MENU_L21", "Logud");
+
+?>
---
+++ e107_plugins/banner_menu/languages/Danish.php
@@ -0,0 +1,34 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|
+|        $Source: /cvs_backup/e107_langpacks/e107_plugins/banner_menu/languages/Danish.php,v $
+|        $Revision: 1.1 $
+|        $Date: 2005-07-06 21:54:04 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
+
+define("BANNER_MENU_L1", "Reklame");
+define("BANNER_MENU_L2", "Banner menu konfiguration gemt");
+
+//v.617
+define("BANNER_MENU_L3", "Titel");
+define("BANNER_MENU_L4", "Kampagne");
+define("BANNER_MENU_L5", "Banner menu konfiguration");
+define("BANNER_MENU_L6", "vælg kampagner der skal vises i menu");
+define("BANNER_MENU_L7", "tilgængelige kampagner");
+define("BANNER_MENU_L8", "valgte kampagner");
+define("BANNER_MENU_L9", "fjern valg");
+define("BANNER_MENU_L10", "hvordan bør de valgte kampagner vises ?");
+define("BANNER_MENU_L11", "vælg fremvisnings type ...");
+define("BANNER_MENU_L12", "en kampagne i en enkelt menu");
+define("BANNER_MENU_L13", "alle valgte kampagner i en enkelt menu");
+define("BANNER_MENU_L14", "alle valgte kampagner i forskellige menuer");
+define("BANNER_MENU_L15", "hvor mange bannere skal der vises ?");
+define("BANNER_MENU_L16", "denne indstilling vil kun blive brugt med mulighederne 2 og 3.<br />hvis færre bannere er til stede vil det maksimalt tilgængelige antal blive brugt.");
+define("BANNER_MENU_L17", "indstil antal ...");
+define("BANNER_MENU_L18", "Opdater menu indstillinger");
+	
+?>
