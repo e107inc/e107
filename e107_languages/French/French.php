3cef818d17be377828281b03ec859906f5e76898
tpruvot <tpruvot@localhost>
2005-07-06 07:23:05 +0000

    utf8 locales
    svn path=/trunk/e107_langpacks/; revision=4394

--- e107_languages/French/French.php
+++ e107_languages/French/French.php
@@ -4,16 +4,16 @@
 |     e107 website system - French Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/French/French.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-05-24 11:44:22 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-06 07:23:05 $
+|     $Author: tpruvot $
 |     Encoding: UTF-8
 +----------------------------------------------------------------------------+
 */
-setlocale(LC_ALL, 'fr');
+setlocale(LC_ALL, 'fr_FR@euro', 'fr_FR.utf8', 'fr_FR', 'fr', 'French');
+define("CHARSET", "utf-8");
 define("CORE_LC", 'fr');
 define("CORE_LC2", 'fr');
-define("CHARSET", "utf-8");
 define("CORE_LAN1","Erreur : le thème est manquant.\\n\\nChanger le thème utilisé dans vos préférences (zone admin) ou charger les fichiers du thème actuel sur le serveur.");
 
 //v.616
