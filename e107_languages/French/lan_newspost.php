9b2a16a634f04f557c12617167d8f0f0f5e2374b
tpruvot <tpruvot@localhost>
2005-07-06 07:55:23 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4397

--- e107_languages/French/lan_newspost.php
+++ e107_languages/French/lan_newspost.php
@@ -4,38 +4,38 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_newspost.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-06 07:45:41 $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-06 07:55:23 $
 |     $Author: tpruvot $
 +----------------------------------------------------------------------------+
 */
-define("NWSLAN_1", "Nouvelle supprimée.");
+define("NWSLAN_1", "Nouvelle supprimÃ©e.");
 define("NWSLAN_2", "Veuillez cocher la case de confirmation pour supprimer cet article.");
 define("NWSLAN_3", "Pas d'articles pour le moment.");
 define("NWSLAN_4", "Articles existants");
-define("NWSLAN_5", "Ouvrir l'éditeur HTML");
-define("NWSLAN_6", "Catégorie");
+define("NWSLAN_5", "Ouvrir l'Ã©diteur HTML");
+define("NWSLAN_6", "CatÃ©gorie");
 define("NWSLAN_7", "Modifier");
 define("NWSLAN_8", "Supprimer");
 define("NWSLAN_9", "cocher pour confirmer");
-define("NWSLAN_10", "Pas de catégories pour le moment.");
-define("NWSLAN_11", "Ajouter/Modifier Catégories");
+define("NWSLAN_10", "Pas de catÃ©gories pour le moment.");
+define("NWSLAN_11", "Ajouter/Modifier CatÃ©gories");
 define("NWSLAN_12", "Titre");
 define("NWSLAN_13", "Contenu");
 define("NWSLAN_14", "Extension");
 define("NWSLAN_15", "Commentaires");
-define("NWSLAN_16", "Activé");
-define("NWSLAN_17", "Désactivé");
+define("NWSLAN_16", "ActivÃ©");
+define("NWSLAN_17", "DÃ©sactivÃ©");
 define("NWSLAN_18", "Autoriser les commentaires sur cette nouvelle");
 define("NWSLAN_19", "Activation");
-define("NWSLAN_20", "Laisser à vide pour désactiver l'activation automatique");
+define("NWSLAN_20", "Laisser Ã  vide pour dÃ©sactiver l'activation automatique");
 define("NWSLAN_21", "Activer entre");
-define("NWSLAN_22", "Visibilité");
+define("NWSLAN_22", "VisibilitÃ©");
 define("NWSLAN_23", "En cochant, cet article ne sera visible que pour les utilisateurs de ce groupe");
-define("NWSLAN_24", "Nouvel aperçu");
-define("NWSLAN_25", "Mise à jour des nouvelles dans la base");
+define("NWSLAN_24", "Nouvel aperÃ§u");
+define("NWSLAN_25", "Mise Ã  jour des nouvelles dans la base");
 define("NWSLAN_26", "Envoyer les nouvelles dans la base");
-define("NWSLAN_27", "Aperçu");
+define("NWSLAN_27", "AperÃ§u");
 define("NWSLAN_28", "Nouvel article");
 define("NWSLAN_29", "Envoi de Nouvelles");
 
---
+++ e107_languages/French/lan_notify.php
@@ -0,0 +1,33 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_notify.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-06 07:55:23 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("NT_LAN_US_1", "Enregistrement Utilisateur");
+
+define("NT_LAN_UV_1", "Enregistrement de l'utilisateur vérifié");
+define("NT_LAN_UV_2", "Châine de session utilisateur");
+
+define("NT_LAN_LI_1", "Utilisateur connecté");
+
+define("NT_LAN_LO_1", "Utilisateur déconnecté");
+define("NT_LAN_LO_2", " s'est déconnecté du site");
+
+define("NT_LAN_FL_1", "Ban Flood");
+define("NT_LAN_FL_2", "Addresse IP bannie pour flood");
+
+define("NT_LAN_SN_1", "Soumission d'une Nouvelle");
+
+define("NT_LAN_NU_1", "Mis à jour");
+
+define("NT_LAN_ND_1", "Nouvelle supprimée");
+define("NT_LAN_ND_2", "id de l'article supprimé");
+
+?>
