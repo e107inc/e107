780adfefacee29853b15ab671724bda72e422a2c
tpruvot <tpruvot@localhost>
2005-07-06 08:11:57 +0000

    FRENCH UTF-8
    svn path=/trunk/e107_langpacks/; revision=4399

---
+++ e107_languages/French/lan_rate.php
@@ -0,0 +1,20 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_rate.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-06 08:11:57 $
+|     $Author: tpruvot $
++----------------------------------------------------------------------------+
+*/
+
+define("RATELAN_0", "vote");
+define("RATELAN_1", "votes");
+define("RATELAN_2", "Comment trouvez vous cet élément?");
+define("RATELAN_3", "merci pour votre vote");
+define("RATELAN_4", "non évalué");
+define("RATELAN_5", "Voter");
+
+?>
--- e107_languages/French/lan_search.php
+++ e107_languages/French/lan_search.php
@@ -4,41 +4,42 @@
 |     e107 website system - French Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_search.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-05-24 11:44:22 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-06 08:11:57 $
+|     $Author: tpruvot $
 |     Encoding: UTF-8
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Rechercher");
 
-define("LAN_20", "Erreur");
-define("LAN_98", "Eléments de Nouvelle");
+define("LAN_98", "Articles des Nouvelles");
 define("LAN_99", "Commentaires");
 define("LAN_140", "Membres enregistrés");
 define("LAN_180", "Rechercher");
 define("LAN_192", "Toutes catégories");
 define("LAN_193", "Calendrier d'évènements");
-define("LAN_194", "Toutes Catégories");
+define("LAN_194", "Toutes catégories");
 define("LAN_195", "Recherche en cours");
 define("LAN_196", "correspondances");
 
 define("LAN_197", "Téléchargements");
-define("LAN_198", "Pas de correspondances trouvées");
+define("LAN_198", "Aucune correspondance trouvée");
 define("LAN_199", "Chercher:");
 define("LAN_200", "Catégories:");
 
-define("LAN_201", "Prière de redéfinir votre demande");
+define("LAN_201", "Veuillez redéfinir votre demande");
 define("LAN_416", "Vous devez être connecté pour accéder à cette page");
 define("LAN_417", "Les termes de la recherche doivent comporter au moins 3 caractères.");
 
+define("LAN_418", "Autres Pages");
+
 define("LAN_SEARCH_1", "Tout sélectionner");
 define("LAN_SEARCH_2", "Tout désélectionner");
-define("LAN_SEARCH_3", "Posté sur ");
-define("LAN_SEARCH_4", "Correspondqnce trouvée dqns le titre de la nouvelle");
+define("LAN_SEARCH_3", "Envoyé sur ");
+define("LAN_SEARCH_4", "Correspondance trouvée dqas le titre de la nouvelle");
 define("LAN_SEARCH_5", "Correspondance trouvée dans le texte de la nouvelle");
-define("LAN_SEARCH_6", "Coorespondance trouvée dans l'extension de texte de la nouvelle");
-define("LAN_SEARCH_7", "Posté par ");
+define("LAN_SEARCH_6", "Correspondance trouvée dans l'extension de la nouvelle");
+define("LAN_SEARCH_7", "Envoyé par ");
 define("LAN_SEARCH_8", " ouvert ");
 define("LAN_SEARCH_9", "Sans titre");
 define("LAN_SEARCH_10", "Aller à la page:");
@@ -48,10 +49,70 @@ define("LAN_SEARCH_13", " dans ");
 define("LAN_SEARCH_14", "Catégorie:");
 define("LAN_SEARCH_15", "Auteur:");
 define("LAN_SEARCH_16", "Recherche restreinte");
-define("LAN_SEARCH_17", "Désolé, la recherche est restreinte à une recherche toutes ");
-define("LAN_SEARCH_18", " les secondes.");
+define("LAN_SEARCH_17", "Désolé, la recherche est restreinte à une recherche toutes les ");
+define("LAN_SEARCH_18", " secondes.");
 define("LAN_SEARCH_19", "Rechercher dans:");
-define("LAN_SEARCH_20", "Requiert une autorisation");
+define("LAN_SEARCH_20", "Nécessite une autorisation");
 define("LAN_SEARCH_21", "Vous n'êtes pas autorisé à voir cette page.");
+
+
+
+define("LAN_SEARCH_22", "Toutes les zones");
+define("LAN_SEARCH_23", "Recherche avancée");
+define("LAN_SEARCH_24", "Doit contenir le(s) mot(s)");
+define("LAN_SEARCH_25", "Ne doit pas contenir le(s) mot(s)");
+define("LAN_SEARCH_26", "Phrase Exacte");
+define("LAN_SEARCH_27", "Mot(s) commencant par");
+define("LAN_SEARCH_28", "Tout n'a pas de recherche avancée");
+define("LAN_SEARCH_29", "Basic");
+define("LAN_SEARCH_30", "Avancé");
+define("LAN_SEARCH_31", "n'a pas de recherche avancée");
+define("LAN_SEARCH_32", "Les mots suivants ont été exclus de la recherche");
+define("LAN_SEARCH_33", "Le mot suivant a été exclu de la recherche");
+define("LAN_SEARCH_34", "Plus récent que");
+define("LAN_SEARCH_35", "Plus vieux que");
+define("LAN_SEARCH_36", "Toute heure");
+define("LAN_SEARCH_37", "Un jour");
+define("LAN_SEARCH_38", "Deux jours");
+define("LAN_SEARCH_39", "Trois jours");
+define("LAN_SEARCH_40", "Une semaine");
+define("LAN_SEARCH_41", "Deux semaine");
+define("LAN_SEARCH_42", "Trois semaine");
+define("LAN_SEARCH_43", "Un mois");
+define("LAN_SEARCH_44", "Deux mois");
+define("LAN_SEARCH_45", "Trois mois");
+define("LAN_SEARCH_46", "Six mois");
+define("LAN_SEARCH_47", "Un an");
+define("LAN_SEARCH_48", "Deux ans");
+define("LAN_SEARCH_49", "Trois ans");
+
+define("LAN_SEARCH_50", "Date d'envoi");
+define("LAN_SEARCH_51", "Toutes les catégories");
+define("LAN_SEARCH_52", "Correspondance dans");
+define("LAN_SEARCH_53", "Tout l'élément");
+define("LAN_SEARCH_54", "Titre seulement");
+define("LAN_SEARCH_55", "Rechercher dans la catégorie de nouvelle");
+define("LAN_SEARCH_56", "Toutes les catégories de nouvelles");
+define("LAN_SEARCH_57", "Commentaires envoyés vers");
+define("LAN_SEARCH_58", "Toutes les zones");
+define("LAN_SEARCH_59", "Tous les Commentaires");
+define("LAN_SEARCH_60", "Commentaires envoyés vers");
+define("LAN_SEARCH_61", "Par auteur");
+define("LAN_SEARCH_62", "Date d'inscription");
+define("LAN_SEARCH_63", "Rechercher dans la catégorie de téléchargement");
+define("LAN_SEARCH_64", "Toutes les catégories de téléchargement");
+define("LAN_SEARCH_65", "Téléchargements");
+define("LAN_SEARCH_66", "Date d'ajout");
+define("LAN_SEARCH_67", "All downloads details");
+define("LAN_SEARCH_68", "Date");
+define("LAN_SEARCH_69", "Relevance");
+
+define("LAN_SEARCH_70", "Envoyé sur un téléchargement");
+define("LAN_SEARCH_71", "Envoyé en réponse d'une nouvelle");
+define("LAN_SEARCH_72", "Signature");
+define("LAN_SEARCH_73", "Pas de Signature.");
+define("LAN_SEARCH_74", "Inscrit le");
+
+define("LAN_SEARCH_75", "Type de Recherche");
 
 ?>
