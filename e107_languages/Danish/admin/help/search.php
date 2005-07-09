1d6378653d34a4e9287f4ca7d6922ffd27a39da7
e107dk <e107dk@localhost>
2005-07-09 11:47:17 +0000

    Added + translatet file
    svn path=/trunk/e107_langpacks/; revision=4467

---
+++ e107_languages/Danish/admin/help/search.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|
+|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/help/search.php,v $
+|        $Revision: 1.1 $
+|        $Date: 2005-07-09 11:47:17 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
+$text = "Hvis din MySql server version tillader det kan du skifte til MySql sorterings metoden der er hurtigere end PHP sorterings metoden.<br /><br />
+
+<b>Tegn</b> Dette er antallet af tekst tegn der vil blive vist i søge resultat opsummeringen.<br /><br />
+<b>Resultater</b> Dette er antallet af resultatet der vil blive vist pr. side.";
+$ns -> tablerender("Søg Hjælp", $text);
+?>
