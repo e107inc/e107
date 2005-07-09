f4491cacb0183d5e32cf69cd7f38badb1eb081cd
e107dk <e107dk@localhost>
2005-07-09 11:34:50 +0000

    Added + translatet file
    svn path=/trunk/e107_langpacks/; revision=4465

---
+++ e107_languages/Danish/admin/help/fileinspector.php
@@ -0,0 +1,18 @@
+<?php
+$text = "<div style='margin-left: 0px; margin-bottom: 1px; margin-top: 2px; vertical-align: top; white-space: nowrap'>
+<img src='".e_IMAGE."fileinspector/file_core.png' alt='".$dir."' style='margin-left: 3px; width: 16px; height: 16px' />&nbsp;Kerne Fil</div>
+<div style='margin-left: 0px; margin-bottom: 1px; margin-top: 2px; vertical-align: top; white-space: nowrap'>
+<img src='".e_IMAGE."fileinspector/file_check.png' alt='".$dir."' style='margin-left: 3px; width: 16px; height: 16px' />&nbsp;Kerne Fil (Integritet Ok)</div>
+<div style='margin-left: 0px; margin-bottom: 1px; margin-top: 2px; vertical-align: top; white-space: nowrap'>
+<img src='".e_IMAGE."fileinspector/file_warning.png' alt='".$dir."' style='margin-left: 3px; width: 16px; height: 16px' />&nbsp;Kerne Fil (Integritet Fejlet)</div>
+<div style='margin-left: 0px; margin-bottom: 1px; margin-top: 2px; vertical-align: top; white-space: nowrap'>
+<img src='".e_IMAGE."fileinspector/file_missing.png' alt='".$dir."' style='margin-left: 3px; width: 16px; height: 16px' />&nbsp;Manglende Kerne Fil</div>
+<div style='margin-left: 0px; margin-bottom: 1px; margin-top: 2px; vertical-align: top; white-space: nowrap'>
+<img src='".e_IMAGE."fileinspector/file_unknown.png' alt='".$dir."' style='margin-left: 3px; width: 16px; height: 16px' />&nbsp;Ikke Kerne Fil</div>";
+$ns -> tablerender("Fil Nøgle", $text);
+
+$text = "Fil inspektøren scanner og analyserer filerne på dit sites server. Når inspektøren møder 
+en e107 kerne fil kontrollerer den for fil regelmæssighed for at sikre at den ikke er korrupt.";
+
+$ns -> tablerender("Fil Inspektør Hjælp", $text);
+?>
