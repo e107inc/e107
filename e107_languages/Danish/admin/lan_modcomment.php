76f88c1822c4fe346dc795a6aa8d85f07cf14734
e107dk <e107dk@localhost>
2005-07-09 10:46:53 +0000

    Added recent lines + removed obsolete ones
    svn path=/trunk/e107_langpacks/; revision=4460

--- e107_languages/Danish/admin/lan_modcomment.php
+++ e107_languages/Danish/admin/lan_modcomment.php
@@ -1,15 +1,34 @@
 <?php
 /*
-+---------------------------------------------------------------+|        e107 website system  Language File|     
-|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/lan_modcomment.php,v $|        $Revision: 1.2 $|        $Date: 2005-07-06 22:12:01 $|        $Author: e107dk $+---------------------------------------------------------------+*/
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|     
+|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/lan_modcomment.php,v $
+|        $Revision: 1.3 $
+|        $Date: 2005-07-09 10:46:53 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
 define("MDCLAN_1", "Administreret.");
 define("MDCLAN_2", "Ingen kommentar til dette element");
 define("MDCLAN_3", "Bruger");
 define("MDCLAN_4", "Gæst");
-define("MDCLAN_5", "slet blokering");
+define("MDCLAN_5", "fjern blokering");
 define("MDCLAN_6", "bloker");
-define("MDCLAN_7", "slet");
-define("MDCLAN_8", "Ret kommentarer");
+
+define("MDCLAN_8", "Administrer kommentarer");
 define("MDCLAN_9", "Advarsel! sletning af kommentargrupper vil også slette alle svar!");
+
+define("MDCLAN_10", "egenskaber");
+define("MDCLAN_11", "kommentar");
+define("MDCLAN_12", "kommentarer");
+define("MDCLAN_13", "blokeret");
+define("MDCLAN_14", "lås kommentarer");
+define("MDCLAN_15", "åben");
+define("MDCLAN_16", "låst");
+define("MDCLAN_17", "");
+define("MDCLAN_18", "");
+define("MDCLAN_19", "");
+define("MDCLAN_20", "");
 
 ?>
