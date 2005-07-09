c91bca3cbf6c20765d7ed5b93f706bd1a6056ec9
e107dk <e107dk@localhost>
2005-07-09 11:17:52 +0000

    Added + translatet file
    svn path=/trunk/e107_langpacks/; revision=4464

---
+++ e107_languages/Danish/admin/help/users_extended.php
@@ -0,0 +1,15 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|
+|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/help/users_extended.php,v $
+|        $Revision: 1.1 $
+|        $Date: 2005-07-09 11:17:52 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
+
+$text = " Udvidede bruger felter tillader digat tilføje yderligere typer data en bruger kan indstille som en del af deres profil.";
+$ns -> tablerender(" Udvidede bruger felter Hjælp", $text);
+?>
