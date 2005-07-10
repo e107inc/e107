7078fc3072221f411a2e88ae14dc6069e8fcb937
natxocc <natxocc@localhost>
2005-07-10 15:07:31 +0000

    New file translated
    svn path=/trunk/e107_langpacks/; revision=4488

---
+++ e107_handlers/phpmailer/language/phpmailer.lang-es.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_handlers/phpmailer/language/phpmailer.lang-es.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-10 15:03:14 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+	
+$PHPMAILER_LANG = array();
+	
+$PHPMAILER_LANG["provide_address"] = 'Debe proveer al menos una ' . 'dirección email.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailes no está soportado.';
+$PHPMAILER_LANG["execute"] = 'No puedo ejecutar: ';
+$PHPMAILER_LANG["instantiate"] = 'No puedo instanciar la función mail.';
+$PHPMAILER_LANG["authenticate"] = 'Error SMTP: No es posible autentificar.';
+$PHPMAILER_LANG["from_failed"] = 'La siguiente dirección Remitente falló: ';
+$PHPMAILER_LANG["recipients_failed"] = 'Error SMTP: Fallarón las siguientes direcciones ' . ': ';
+$PHPMAILER_LANG["data_not_accepted"] = 'Error SMTP: Datos no aceptados.';
+$PHPMAILER_LANG["connect_host"] = 'Error SMTP: No puedo conectarme al servidor SMTP.';
+$PHPMAILER_LANG["file_access"] = 'No puedo acceder al archivo: ';
+$PHPMAILER_LANG["file_open"] = 'Error de archivo: No se puede abrir: ';
+$PHPMAILER_LANG["encoding"] = 'Encoding desconocido: ';
+?>
