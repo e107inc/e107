eebc1610037ea41643d9b87d2bd1c6ad53b2ce5b
mcfly_e107 <mcfly_e107@localhost>
2005-07-11 14:39:27 +0000

    Possible fix for bug #1219
    svn path=/trunk/e107_0.7/; revision=4502

--- e107_plugins/chatbox_menu/chat.php
+++ e107_plugins/chatbox_menu/chat.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/chatbox_menu/chat.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2005-06-24 03:50:47 $
+|     $Revision: 1.7 $
+|     $Date: 2005-07-11 14:39:27 $
 |     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
@@ -23,7 +23,6 @@ if (file_exists(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".p
 	include_once(e_PLUGIN."chatbox_menu/languages/English/English.php");
 }
 require_once(HEADERF);
-
 
 $sql->db_Select("menus", "*", "menu_name='chatbox_menu'");
 $row = $sql->db_Fetch();
@@ -63,49 +62,24 @@ if($_POST['moderate'] && CB_MOD)
 	$message = CHATBOX_L18;
 }
 
-if (strstr(e_QUERY, "fs")) {
-	$cgtm = str_replace(".fs", "", e_QUERY);
-	$fs = TRUE;
-}
-
 if (e_QUERY ? $from = e_QUERY : $from = 0);
-if (!$view) {
-	$view = 30;
-}
 
 $chat_total = $sql->db_Count("chatbox");
-$qry_where = (CB_MOD ? "1" : "cb_blocked=0");
-if ($fs) {
-	$page_count = 0;
-	$row_count = 0;
-	$sql->db_Select("chatbox", "*", "{$qry_where} ORDER BY cb_datestamp DESC");
-	while ($row = $sql -> db_Fetch()) {
-		if ($row['cb_id'] == $cgtm) {
-			$from = $page_count;
-			break;
-		}
-		$row_count++;
-		if ($row_count == 30) {
-			$row_count = 0;
-			$page_count += 30;
-		}
-	}
-}
 
-$sql->db_Select("chatbox", "*", "{$qry_where} ORDER BY cb_datestamp DESC LIMIT $from, ".$view);
-	
+$qry_where = (CB_MOD ? "1" : "cb_blocked=0");
+
+$sql->db_Select("chatbox", "*", "{$qry_where} ORDER BY cb_datestamp DESC LIMIT $from, 30");
 $obj2 = new convert;
 
-while ($row = $sql->db_Fetch()) {
+$chatList = $sql->db_getList();
+foreach ($chatList as $row)
+{
 	$CHAT_TABLE_DATESTAMP = $obj2->convert_date($row['cb_datestamp'], "long");
 	$CHAT_TABLE_NICK = eregi_replace("[0-9]+\.", "", $row['cb_nick']);
 	$cb_message = $tp->toHTML($row['cb_message']);
 	if($row['cb_blocked'])
 	{
 		$cb_message .= "<br />".LAN_16;
-	}
-	if (!eregi("<a href|<img|&#", $cb_message)) {
-		$cb_message = preg_replace("/([^\s]{100})/", "$1\n", $cb_message);
 	}
 	if(CB_MOD)
 	{
@@ -124,9 +98,12 @@ while ($row = $sql->db_Fetch()) {
 	$CHAT_TABLE_FLAG = ($flag ? "forumheader3" : "forumheader4");
 
 	if (!$CHAT_TABLE) {
-		if (file_exists(THEME."chat_template.php")) {
+		if (file_exists(THEME."chat_template.php"))
+		{
 			require_once(THEME."chat_template.php");
-		} else {
+		}
+		else
+		{
 			require_once(e_BASE.$THEMES_DIRECTORY."templates/chat_template.php");
 		}
 	}
@@ -152,7 +129,5 @@ $ns->tablerender(LAN_11, $text);
 require_once(e_HANDLER."np_class.php");
 $ix = new nextprev("chat.php", $from, 30, $chat_total, LAN_12);
 
-
 require_once(FOOTERF);
-
 ?>
