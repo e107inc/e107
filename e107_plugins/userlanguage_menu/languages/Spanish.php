08adb6cad6847069ecbdae753573c849c2701b82
natxocc <natxocc@localhost>
2005-07-11 17:28:20 +0000

    Update from original CVS 11/07/05
    svn path=/trunk/e107_langpacks/; revision=4506

--- e107_plugins/userlanguage_menu/languages/Spanish.php
+++ e107_plugins/userlanguage_menu/languages/Spanish.php
@@ -4,11 +4,12 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/userlanguage_menu/languages/Spanish.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-03 22:34:19 $
+|     $Revision: 1.3 $
+|     $Date: 2005-07-11 17:28:20 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
 define("UTHEME_MENU_L1", "Elegir Idioma");
 define("UTHEME_MENU_L2", "Seleccione Idioma");
+define("UTHEME_MENU_L3", "Tablas");
 ?>
