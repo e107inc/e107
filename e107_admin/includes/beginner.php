0f5c47f250dc0a29879a790e65ffa951e247bba8
e107coders <e107coders@localhost>
2005-07-12 17:33:40 +0000

    Plugins displayed now..
    svn path=/trunk/e107_0.7/; revision=4520

--- e107_admin/includes/beginner.php
+++ e107_admin/includes/beginner.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/includes/beginner.php,v $
-|     $Revision: 1.8 $
-|     $Date: 2005-06-14 22:37:13 $
+|     $Revision: 1.9 $
+|     $Date: 2005-07-12 17:33:40 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -26,10 +26,10 @@ if($_GET['mode'] == "e_advanced"){
 $text = "<div style='text-align:center;vertical-align:middle'><br /><br />
 	<table style='".ADMIN_WIDTH.";margin-top:auto;margin-bottom:auto' >";
 
-	$newarray[28] = array(e_ADMIN."plugin.php", ADLAN_98, ADLAN_99, "Z", 2, E_16_PLUGMANAGER, E_32_PLUGMANAGER);
+ //	$newarray[28] = array(e_ADMIN."plugin.php", ADLAN_98, ADLAN_99, "Z", 2, E_16_PLUGMANAGER, E_32_PLUGMANAGER);
 
-  //	$selection = array(19,10,15,22,5,17,7,25,27,23);
-  $selection = array(21,11,17,24,5,19,7,27,28,25);
+  $selection = array(22,12,17,25,5,19,7,23,28,26);
+ // $selection = array(21,11,17,24,5,19,7,27,28,25);
 	foreach($selection as $id){
 	$text .= render_links($newarray[$id][0],$newarray[$id][1],$newarray[$id][2],$newarray[$id][3],$newarray[$id][6],'beginner');
 	}
@@ -39,5 +39,35 @@ $text .= "\n</tr></table><br /></div>";
 $text .= "<div class='smalltext' style='text-align:center'>".ADLAN_144." <a href='".e_SELF."?mode=e_advanced' >".ADLAN_145."</a>&nbsp;&nbsp;</div>";
 
 $ns->tablerender(ADLAN_47." ".ADMINNAME, $text);
+
+
+
+$text = "<div style='text-align:center'>
+	<table style='".ADMIN_WIDTH."'>";
+
+$text .= render_links(e_ADMIN."plugin.php", ADLAN_98, ADLAN_99, "Z", E_32_PLUGMANAGER, "classis");
+
+if ($sql->db_Select("plugin", "*", "plugin_installflag=1")) {
+	while ($row = $sql->db_Fetch()) {
+		extract($row);
+		include(e_PLUGIN.$plugin_path."/plugin.php");
+		if ($eplug_conffile) {
+			$plugin_icon = $eplug_icon ? "<img src='".e_PLUGIN.$eplug_icon."' alt='' style='border:0px; width: 32px; height: 32px' />" : E_32_CAT_PLUG;
+			$plugin_array[ucfirst($eplug_name)] = array('link' => e_PLUGIN.$plugin_path."/".$eplug_conffile, 'title' => $eplug_name, 'caption' => $eplug_caption, 'perms' => "P".$plugin_id, 'icon' => $plugin_icon);
+		}
+		unset($eplug_conffile, $eplug_name, $eplug_caption, $eplug_icon);
+	}
+}
+
+ksort($plugin_array, SORT_STRING);
+foreach ($plugin_array as $plug_key => $plug_value) {
+	$text .= render_links($plug_value['link'], $plug_value['title'], $plug_value['caption'], $plug_value['perms'], $plug_value['icon'], 'classis');
+}
+
+$text .= render_clean();
+
+$text .= "</table></div>";
+
+$ns->tablerender(ADLAN_CL_7, $text);
 
 ?>
