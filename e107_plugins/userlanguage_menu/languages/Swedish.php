4122bd3ec15b82c8f27bc634f51249c43ab5dc02
mrpiercer <mrpiercer@localhost>
2005-07-12 16:18:11 +0000

    Update
    svn path=/trunk/e107_langpacks/; revision=4517

--- e107_plugins/userlanguage_menu/languages/Swedish.php
+++ e107_plugins/userlanguage_menu/languages/Swedish.php
@@ -4,13 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/userlanguage_menu/languages/Swedish.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-25 11:07:53 $
+|     $Revision: 1.3 $
+|     $Date: 2005-07-12 16:18:11 $
 |     $Author: mrpiercer $
 +----------------------------------------------------------------------------+
 */
 
 define("UTHEME_MENU_L1", "Sätt språk");
 define("UTHEME_MENU_L2", "Välj språk");
+define("UTHEME_MENU_L3", "Tabeller");
 
 ?>
