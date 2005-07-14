60e9160ed742ff46b2301f5f683490d30c7b18dd
lisa_ <lisa_@localhost>
2005-07-14 20:03:19 +0000

    calendar : e_list : added category and author fields
    svn path=/trunk/e107_0.7/; revision=4536

--- e107_plugins/calendar_menu/e_list.php
+++ e107_plugins/calendar_menu/e_list.php
@@ -15,24 +15,38 @@
 
 	if($mode == "new_page" || $mode == "new_menu" ){
 		$lvisit = $this -> getlvisit();
-		$qry = " event_datestamp>".$lvisit;
+		$qry = " event_datestamp>".$lvisit." AND ";
 	}else{
-		$qry = " ";
+		$qry = "";
 	}
-	$qry .= " event_start>='$current' ORDER BY event_start ASC LIMIT 0,".$arr[7];
- 
+
 	$bullet = $this -> getBullet($arr[6], $mode);
 
-	if(!$event_items = $sql -> db_select("event", "*", $qry)){
+	$qry = "
+	SELECT e.*, c.event_cat_name
+	FROM #event AS e 
+	LEFT JOIN #event_cat AS c ON c.event_cat_id = e.event_category 
+	WHERE ".$qry." e.event_start>='$current' AND c.event_cat_class REGEXP '".e_CLASS_REGEXP."' 
+	ORDER BY e.event_start ASC LIMIT 0,".$arr[7];
+
+	if(!$event_items = $sql->db_Select_gen($qry)){
 		$LIST_DATA = LIST_CALENDAR_2;
 	}else{
 		while($row = $sql -> db_Fetch()){
 
+			$tmp = explode(".", $row['event_author']);
+			if($tmp[0] == "0"){
+				$AUTHOR = $tmp[1];
+			}elseif(is_numeric($tmp[0]) && $tmp[0] != "0"){
+				$AUTHOR = (USER ? "<a href='".e_BASE."user.php?id.".$tmp[0]."'>".$tmp[1]."</a>" : $tmp[1]);
+			}else{
+				$AUTHOR = "";
+			}
+
 			$rowheading	= $this -> parse_heading($row['event_title'], $mode);
 			$ICON		= $bullet;
 			$HEADING	= "<a href='".e_BASE."event.php?".$row['event_start']."' title='".$row['event_title']."'>".$rowheading."</a>";
-			$CATEGORY	= "";
-			$AUTHOR		= "";
+			$CATEGORY	= $row['event_cat_name'];
 			$DATE		= ($arr[5] ? ($row['event_start'] ? $this -> getListDate($row['event_start'], $mode) : "") : "");
 			$INFO		= "";
 			$LIST_DATA[$mode][] = array( $ICON, $HEADING, $AUTHOR, $CATEGORY, $DATE, $INFO );
