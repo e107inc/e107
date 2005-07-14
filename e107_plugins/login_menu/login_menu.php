ab06cb78722d0c291c05404f3d40924a8c0ae860
lisa_ <lisa_@localhost>
2005-07-14 20:10:11 +0000

    login_menu : added check to see if the list plugin is installed
    svn path=/trunk/e107_0.7/; revision=4537

--- e107_plugins/login_menu/login_menu.php
+++ e107_plugins/login_menu/login_menu.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/login_menu.php,v $
-|     $Revision: 1.34 $
-|     $Date: 2005-07-09 13:55:17 $
-|     $Author: streaky $
+|     $Revision: 1.35 $
+|     $Date: 2005-07-14 20:10:11 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 if(!defined("e_HANDLER")){ exit; }
@@ -51,120 +51,124 @@ if (USER == TRUE || ADMIN == TRUE) {
 	$new_total = 0;
 	$time = USERLV;
 
-// ------------ News Stats -----------
+	//first check if the list plugin is installed
+	if($list_install = $sql -> db_Select("plugin", "*", "plugin_path = 'list_new' AND plugin_installflag = '1' ")){
+		// ------------ News Stats -----------
 
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_news'] == true) {
-		$new_news = $sql->db_Select('news', '*', '`news_datestamp` > '.$time);
-		while ($row = $sql->db_Fetch()) {
-			if (!check_class($row['news_class'])) {
-				$new_news--;
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_news'] == true) {
+			$new_news = $sql->db_Select('news', '*', '`news_datestamp` > '.$time);
+			while ($row = $sql->db_Fetch()) {
+				if (!check_class($row['news_class'])) {
+					$new_news--;
+				}
 			}
-		}
-		$new_total += $new_news;
-		if (!$new_news) {
-			$new_news = LOGIN_MENU_L26;
-		}
-		$NewItems[] = $new_news.' '.($new_news == 1 ? LOGIN_MENU_L14 : LOGIN_MENU_L15);
-	}
-
-// ------------ Article Stats -----------
-
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_articles'] == true) {
-		$new_articles = 0;
-		$new_articles = $sql->db_Select('content', '(content_class)', 'content_type = 0 AND `content_datestamp` > '.$time);
-		while ($row = $sql->db_Fetch()) {
-			if (!check_class($row['content_class'])) {
-				$new_articles--;
+			$new_total += $new_news;
+			if (!$new_news) {
+				$new_news = LOGIN_MENU_L26;
 			}
+			$NewItems[] = $new_news.' '.($new_news == 1 ? LOGIN_MENU_L14 : LOGIN_MENU_L15);
 		}
-		$new_total += $new_articles;
-		if (!$new_articles) {
-			$new_articles = LOGIN_MENU_L26;
-		}
-		$NewItems[] = $new_articles.' '.($new_articles == 1 ? LOGIN_MENU_L29 : LOGIN_MENU_L30);
-	}
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_comments'] == true) {
-		$new_comments = 0;
-		$new_comments = $sql->db_Select('comments', '*', '`comment_datestamp` > '.$time);
 
-		$handle = opendir(e_PLUGIN);
-		while (false !== ($file = readdir($handle))) {
-			if ($file != '.' && $file != '..' && is_dir(e_PLUGIN.$file)) {
-				$plugin_handle = opendir(e_PLUGIN.$file."/");
-				while (false !== ($file2 = readdir($plugin_handle))) {
-					if ($file2 == 'e_comment.php') {
-						require_once(e_PLUGIN.$file.'/'.$file2);
-						if ($comment_type == $e_plug_table) {
-							$new_comments++;
-							break 2;
+		// ------------ Article Stats -----------
+
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_articles'] == true) {
+			$new_articles = 0;
+			$new_articles = $sql->db_Select('content', '(content_class)', 'content_type = 0 AND `content_datestamp` > '.$time);
+			while ($row = $sql->db_Fetch()) {
+				if (!check_class($row['content_class'])) {
+					$new_articles--;
+				}
+			}
+			$new_total += $new_articles;
+			if (!$new_articles) {
+				$new_articles = LOGIN_MENU_L26;
+			}
+			$NewItems[] = $new_articles.' '.($new_articles == 1 ? LOGIN_MENU_L29 : LOGIN_MENU_L30);
+		}
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_comments'] == true) {
+			$new_comments = 0;
+			$new_comments = $sql->db_Select('comments', '*', '`comment_datestamp` > '.$time);
+
+			$handle = opendir(e_PLUGIN);
+			while (false !== ($file = readdir($handle))) {
+				if ($file != '.' && $file != '..' && is_dir(e_PLUGIN.$file)) {
+					$plugin_handle = opendir(e_PLUGIN.$file."/");
+					while (false !== ($file2 = readdir($plugin_handle))) {
+						if ($file2 == 'e_comment.php') {
+							require_once(e_PLUGIN.$file.'/'.$file2);
+							if ($comment_type == $e_plug_table) {
+								$new_comments++;
+								break 2;
+							}
 						}
 					}
 				}
 			}
+			$new_total += $new_comments;
+			if (!$new_comments) {
+				$new_comments = LOGIN_MENU_L26;
+			}
+			$NewItems[] = $new_comments.' '.($new_comments == 1 ? LOGIN_MENU_L18 : LOGIN_MENU_L19);
 		}
-		$new_total += $new_comments;
-		if (!$new_comments) {
-			$new_comments = LOGIN_MENU_L26;
+
+		// ------------ Chatbox Stats -----------
+
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_chatbox'] == true) {
+			$display_chats = TRUE;
+
+			if(in_array('chatbox_menu',$eMenuActive)){
+				$new_chat = $sql->db_Count('chatbox', '(*)', 'WHERE `cb_datestamp` > '.$time);
+				$new_total += $new_chat;
+			} else {
+				$display_chats = FALSE;
+			}
+			if (isset($new_chat) && !$new_chat) {
+				$new_chat = ($display_chats ? LOGIN_MENU_L26 : '');
+			}
+			if ($display_chats == true) {
+				$NewItems[] = $new_chat.' '.($new_chat == 1 ? LOGIN_MENU_L16 : LOGIN_MENU_L17);
+
+			}
 		}
-		$NewItems[] = $new_comments.' '.($new_comments == 1 ? LOGIN_MENU_L18 : LOGIN_MENU_L19);
-	}
 
-// ------------ Chatbox Stats -----------
+		// ------------ Forum Stats -----------
 
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_chatbox'] == true) {
-		$display_chats = TRUE;
-
-		if(in_array('chatbox_menu',$eMenuActive)){
-			$new_chat = $sql->db_Count('chatbox', '(*)', 'WHERE `cb_datestamp` > '.$time);
-			$new_total += $new_chat;
-		} else {
-			$display_chats = FALSE;
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_forum'] == true) {
+			$qry = "
+			SELECT  count(*) as count FROM #forum_t  as t
+			LEFT JOIN #forum as f
+			ON t.thread_forum_id = f.forum_id
+			WHERE t.thread_datestamp > {$time} and f.forum_class IN (".USERCLASS_LIST.")
+			";
+			if($sql->db_Select_gen($qry))
+			{
+				$row = $sql->db_Fetch();
+				$new_forum = $row['count'];
+				$new_total += $new_forum;
+			}
+			if (!$new_forum) {
+				$new_forum = LOGIN_MENU_L26;
+			}
+			$NewItems[] = $new_forum.' '.($new_forum == 1 ? LOGIN_MENU_L20 : LOGIN_MENU_L21);
 		}
-		if (isset($new_chat) && !$new_chat) {
-			$new_chat = ($display_chats ? LOGIN_MENU_L26 : '');
-		}
-		if ($display_chats == true) {
-			$NewItems[] = $new_chat.' '.($new_chat == 1 ? LOGIN_MENU_L16 : LOGIN_MENU_L17);
 
-}
-	}
+		// ------------ Member Stats -----------
 
-// ------------ Forum Stats -----------
+		if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_members'] == true) {
+			$new_users = $sql->db_Count('user', '(user_join)', 'WHERE user_join > '.$time);
+			$new_total += $new_users;
+			if (!$new_users) {
+				$new_users = LOGIN_MENU_L26;
+			}
+			$NewItems[] = $new_users.' '.($new_users == 1 ? LOGIN_MENU_L22 : LOGIN_MENU_L23);
+		}
+		if (isset($NewItems) && $NewItems) {
+			$text .= '<br /><br /><span class="smalltext">'.LOGIN_MENU_L25.'<br />'.implode(',<br />', $NewItems).'</span>';
+			if ($new_total) {
+					$text .= '<br /><a href="'.e_PLUGIN.'list_new/list.php?new">'.LOGIN_MENU_L24.'</a>';
+			}
+		}
 
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_forum'] == true) {
-		$qry = "
-		SELECT  count(*) as count FROM #forum_t  as t
-		LEFT JOIN #forum as f
-		ON t.thread_forum_id = f.forum_id
-		WHERE t.thread_datestamp > {$time} and f.forum_class IN (".USERCLASS_LIST.")
-		";
-		if($sql->db_Select_gen($qry))
-		{
-			$row = $sql->db_Fetch();
-			$new_forum = $row['count'];
-			$new_total += $new_forum;
-		}
-		if (!$new_forum) {
-			$new_forum = LOGIN_MENU_L26;
-		}
-		$NewItems[] = $new_forum.' '.($new_forum == 1 ? LOGIN_MENU_L20 : LOGIN_MENU_L21);
-	}
-
-// ------------ Member Stats -----------
-
-	if (isset($menu_pref['login_menu']) && $menu_pref['login_menu']['new_members'] == true) {
-		$new_users = $sql->db_Count('user', '(user_join)', 'WHERE user_join > '.$time);
-		$new_total += $new_users;
-		if (!$new_users) {
-			$new_users = LOGIN_MENU_L26;
-		}
-		$NewItems[] = $new_users.' '.($new_users == 1 ? LOGIN_MENU_L22 : LOGIN_MENU_L23);
-	}
-	if (isset($NewItems) && $NewItems) {
-		$text .= '<br /><br /><span class="smalltext">'.LOGIN_MENU_L25.'<br />'.implode(',<br />', $NewItems).'</span>';
-		if ($new_total) {
-			$text .= '<br /><a href="'.e_PLUGIN.'list_new/list.php?new">'.LOGIN_MENU_L24.'</a>';
-		}
 	}
 	if (file_exists(THEME.'images/login_menu.png')) {
 		$caption = '<img src="'.THEME_ABS.'images/login_menu.png" alt="" />'.LOGIN_MENU_L5.' '.USERNAME;
