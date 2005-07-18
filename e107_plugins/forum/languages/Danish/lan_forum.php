d9766f07ef833cd092bc46aa7f1907d7d435f531
e107dk <e107dk@localhost>
2005-07-18 16:58:24 +0000

    Added + translatet file
    svn path=/trunk/e107_langpacks/; revision=4570

---
+++ e107_plugins/forum/languages/Danish/lan_forum.php
@@ -0,0 +1,114 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/Danish/lan_forum.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-18 16:58:23 $
+|     $Author: e107dk $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "Forum");
+
+define("LAN_30", "Velkommen");
+define("LAN_31", "Der er ingen nye indlæg ");
+define("LAN_32", "Der er 1 nyt indlæg");
+define("LAN_33", "Der er");
+define("LAN_34", "nye indlæg");
+define("LAN_35", "siden dit sidste besøg.");
+define("LAN_36", "Dit sidste besøg på ");
+define("LAN_37", "Det er nu ");
+define("LAN_38", ", Alle tider er ");
+define("LAN_41", "Nyeste medlem: ");
+define("LAN_42", "Registrerede medlemmer: ");
+define("LAN_44", "I dette forum kan alle frit skrive og læse selvom de ikke er tilmeldt på siden, men vær venligst opmærksom på at din IP Adresse vil blive logget hvis du skriver et indlæg. <br />For at få adgang til alle funktionerne i dette forum skal du");
+define("LAN_45", "I dette forum kan kun medlemmer der er logget ind, læse og skrive, klik venligst");
+define("LAN_46", "Forum");
+define("LAN_47", "Indlæg");
+define("LAN_48", "Svar");
+define("LAN_49", "Sidste indlæg");
+define("LAN_51", "Intet forum endnu, vend snarest tilbage igen.");
+define("LAN_52", "Her er intet forum endnu, vend snarest tilbage igen.");
+define("LAN_79", "Intet forum i denne sektion endnu, vend snarest tilbage igen.");
+define("LAN_80", "Nye indlæg");
+define("LAN_81", "Ingen nye indlæg");
+define("LAN_100", "artikler");
+define("LAN_180", "Søg");
+define("LAN_191", "Informationer");
+define("LAN_192", "Antal indlæg skrevet af brugerene ");
+define("LAN_196", "Du har læst ");
+define("LAN_197", " Af disse indlæg.");
+define("LAN_198", " Alle nye indlæg er læst.");
+define("LAN_199", "Marker alle indlæg som læst");
+define("LAN_204", "Du <b>kan</b> starte nye indlæg");
+define("LAN_205", "Du <b>kan ikke</b> starte nye indlæg");
+define("LAN_206", "Du <b>kan</b> indsende svar");
+define("LAN_207", "Du <b>kan ikke</b> indsende svar");
+define("LAN_208", "Du <b>kan</b> rette i dine indlæg");
+define("LAN_209", "Du <b>kan ikke</b> rette i dine indlæg");
+define("LAN_392", "stop overvågningen af dette indlæg");
+define("LAN_393", "Vis overvågede indlæg");
+define("LAN_394", "Lukket forum");
+define("LAN_397", "Overvågede indlæg");
+define("LAN_398", "Lukket");
+define("LAN_399", "Adgangsbegrænset");
+define("LAN_400", "Dette forum kan kun benyttes af registrerede medlemmer");
+define("LAN_401", "Kun for medlemmer");
+
+define("LAN_402", "Dette forum er skrive beskyttet");
+
+define("LAN_403", "Ingen indlæg endnu");
+define("LAN_404", "inlæg");
+define("LAN_405", "Skrive Beskyttet");
+
+define("LAN_406", "Dette forum er begrændset til administratorer");
+define("LAN_407", "Dette forum er begrændset til medlemmer");
+define("LAN_408", "Dette forum er Skrive Beskyttet");
+define("LAN_409", "Dette er et gruppe beskyttet forum");
+define("LAN_410", "Velkommen Gæst");
+
+define("LAN_411", "indlæg");
+define("LAN_412", "svar");
+define("LAN_413", "indlæg");
+define("LAN_414", "svar");
+define("LAN_415", "bruger er i forums i øjeblikket");
+define("LAN_416", "brugere er i forums i øjeblikket");
+
+define("LAN_417", "medlem");
+define("LAN_418", "gæst");
+define("LAN_419", "medlemmer");
+define("LAN_420", "gæster");
+
+define("LAN_421", "Vis nye indlæg");
+define("LAN_422", "Nye indlæg siden dit sidste besøg");
+define("LAN_423", "Skrevet af");
+define("LAN_424", "Nye indlæg");
+define("LAN_425", "Sv:");
+
+//v.616
+define("LAN_426", "Hvem er Online: ");
+define("LAN_427", "Vis detaljeret liste.");
+define("LAN_428", "Sv:");
+define("LAN_429", "Top Skribenter");
+define("LAN_430", "Mest Aktive Tråde");
+define("LAN_431", "Mine Indlæg");
+define("LAN_432", "Mine Indstillinger");
+define("LAN_433", "Forum Regler");
+define("LAN_434", "Tilbage til forums");
+define("LAN_435", "Min Profil");
+define("LAN_436", " (&Aring;bner i et nyt vindue.)");
+
+define("LAN_437", "tilmeld");
+define("LAN_438", "og login.");
+define("LAN_439", "her");
+define("LAN_440", "for at gå til tilmeldings siden.");
+
+define("LAN_441", "Vis forum statistik");
+	
+define('FORLAN_441', 'Ingen regler defineret.');
+define('FORLAN_442', 'Mine Uploads');
+define('FORLAN_443', '[bruger slettet]');
+define('FORLAN_444', 'under-forums');
+
+?>
---
+++ e107_plugins/forum/languages/Danish/lan_forum_admin.php
@@ -0,0 +1,200 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/Danish/lan_forum_admin.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-18 16:58:24 $
+|     $Author: e107dk $
++----------------------------------------------------------------------------+
+*/
+define("FORLAN_1", "Du skal angive et antal dage du ønsker at beskære.");
+define("FORLAN_2", "Vælg venligst om du vil slette de beskårede indlæg fulstændig, eller markere dem som inaktive (ikke slettet men ej mere vist på forum siden)");
+define("FORLAN_3", "Slet");
+define("FORLAN_4", "Angiv som inaktiv");
+define("FORLAN_5", "Udfør beskæring");
+define("FORLAN_6", "Fortryd");
+define("FORLAN_7", "Forum indstillinger");
+define("FORLAN_8", "Forum beskåret.");
+define("FORLAN_9", "Beskæring er ikke nødvendig.");
+define("FORLAN_10", "&AElig;ndringer er gemt");
+define("FORLAN_11", "Forum oprettet.");
+define("FORLAN_12", "Gruppen er opdateret.");
+define("FORLAN_13", "Gruppen er tilføjet til databasen.");
+define("FORLAN_14", "Forum gruppe er opdateret.");
+define("FORLAN_15", "Marker boksen for at bekræfte sletningen af dette forum");
+define("FORLAN_16", "Forums");
+define("FORLAN_17", "Ingen grupper endnu");
+define("FORLAN_18", "Eksisterende grupper");
+define("FORLAN_19", "Rediger");
+define("FORLAN_20", "Slet");
+define("FORLAN_21", "Marker for bekræftelse");
+define("FORLAN_22", "Gruppe");
+define("FORLAN_23", "Tilgængelig til?");
+define("FORLAN_24", "Marker så det bliver tilgængeligt for brugere i den afmærkede klasse");
+define("FORLAN_25", "Opdater gruppe");
+define("FORLAN_26", "Opret gruppe");
+define("FORLAN_27", "Du skal definere mindst en gruppe og et forum.");
+define("FORLAN_28", "Forum");
+define("FORLAN_29", "Endnu intet forum.");
+define("FORLAN_30", "Eksisterende forums");
+define("FORLAN_31", "Navn");
+define("FORLAN_32", "Beskrivelse");
+define("FORLAN_33", "Bestyrer");
+define("FORLAN_34", "Marker for at aktivere dette forum");
+define("FORLAN_35", "Opdater Forum");
+define("FORLAN_36", "Opret forum");
+define("FORLAN_37", "Rækkefølge");
+define("FORLAN_38", "Lukket");
+define("FORLAN_39", "Kun for medlemmer");
+define("FORLAN_40", "Begrænset");
+define("FORLAN_41", "Flyt op");
+define("FORLAN_42", "Flyt ned");
+define("FORLAN_43", "Fremvis / Forum rækkefølge");
+define("FORLAN_44", "Medtag tabeller");
+define("FORLAN_45", "marker for at lade forummet følge med tema");
+define("FORLAN_46", "Titel vist som overskrift hvis medtag tabeller er afmærket");
+define("FORLAN_47", "Tilslut e-mail overvågning");
+define("FORLAN_48", "Marker denne boks som tillader dine brugere at modtage en e-mail, når der svares på deres indlæg");
+define("FORLAN_49", "Tillad afstemninger");
+define("FORLAN_50", "Marker denne boks som tillader dine brugere at oprette afstemninger på indlæg i forummet");
+define("FORLAN_51", "tillad overvågning");
+define("FORLAN_52", "Marker denne boks som tillader dine brugere at modtage en e-mail, hvis de ønsker at følge et bestemt forum emne");
+define("FORLAN_53", "E-mail prefix");
+define("FORLAN_54", "Teksten du skriver vil være i emnet på e-mails der kommer fra forum indlæg");
+define("FORLAN_55", "Populært indlæg grænse");
+define("FORLAN_56", "Antal af svar til indlæg. Før de markeres som populære");
+define("FORLAN_57", "Indlæg pr. side");
+define("FORLAN_58", "Antal af indlæg vist pr. side");
+define("FORLAN_59", "Beskær");
+define("FORLAN_60", "Dette vil slette alle indlæg der ikke har fået svar i det antal dage du angiver. <b>Brug denne funktion med forsigtighed!</b>");
+define("FORLAN_61", "Opdater indstillinger");
+define("FORLAN_62", "Forum indstillinger");
+define("FORLAN_63", "Niveau");
+define("FORLAN_64", "Angiv dit niveau her, hvis det efterlades blankt vil standard stjerner vil blive brugt til vise niveau. Adskil niveauer med komma. Maksimalt antal niveauer er 10 stk., lavest først.");
+define("FORLAN_65", "Forum Titel");
+
+define("FORLAN_70", "Tillad fil/billede vedhæftninger");
+define("FORLAN_71", "Tillad brugere at uploade filer eller billeder ved forum indlæg");
+define("FORLAN_72", "Opdater Orden");
+define("FORLAN_73", "Orden Opdateret");
+
+define("FORLAN_75", "Grupper");
+define("FORLAN_76", "Forum Forside");
+define("FORLAN_77", "Lav Forums");
+define("FORLAN_78", "Forum Orden");
+define("FORLAN_79", "Indstillinger");
+define("FORLAN_80", "Muligheder");
+define("FORLAN_81", "Er du sikker på du vil slette denne forum gruppe? - forums i gruppen vil også blive slettet");
+define("FORLAN_82", "Er du sikker på du vil slette dette forum?");
+define("FORLAN_83", "Lav forum grupper");
+define("FORLAN_84", "Kun medlemmer");
+define("FORLAN_85", "Skrivebeskytte");
+define("FORLAN_86", "Kun Admin");
+define("FORLAN_87", "Beskær emner uden svar efter så mange dage:");
+
+define("FORLAN_88", "Beskær emner uden svar efter så mange dage:");
+
+define("FORLAN_89", "Slet indlæg helt");
+define("FORLAN_90", "Gør indlæg inaktive");
+
+define("FORLAN_91", "indlæg gjort inaktive");
+define("FORLAN_92", "indlæg slettet");
+define("FORLAN_93", "svar slettet");
+
+define("FORLAN_94", "Sæt Rang");
+define("FORLAN_95", "Rang gemt");
+define("FORLAN_96", "Forum slettet");
+define("FORLAN_97", "Forum gruppe slettet");
+
+define("FORLAN_98", "Rang Navn");
+define("FORLAN_99", "antal point før niveau skift");
+define("FORLAN_100", "upload billeder til e107_images/forum/");
+define("FORLAN_101", "Hoved Admin");
+define("FORLAN_102", "Grænse");
+define("FORLAN_103", "Site Admin");
+define("FORLAN_104", "Rang Billede");
+define("FORLAN_105", "Forum Bestyrer");
+
+
+define("FORLAN_106", "Beskæring Type:");
+define("FORLAN_107", "Forum");
+define("FORLAN_108", " slettet");
+define("FORLAN_109", "dage:");
+define("FORLAN_110", "Beskær");
+define("FORLAN_111", "deaktiver");
+
+define("FORLAN_112", "Slå vidrestilling til");
+define("FORLAN_113", "Marker denne for at vidrestille browseren til forum siden efter svar");
+define("FORLAN_114", "Bruger Egen Titel");
+define("FORLAN_115", "Marker denne for at tillade brugere at ændre deres egen titel");
+define("FORLAN_116", "Raporterede Indlæg");
+define("FORLAN_117", "Dette vil slette indholdet af den raporterede tråd. Ikke selve tråden.");
+define("FORLAN_118", "Raporteret indæg slettet");
+define("FORLAN_119", "Klik på links vil åbne forum i et nyt vindue.");
+define("FORLAN_120", "Marker denne for at tillade brugere at ændre deres egen titel");
+define("FORLAN_121", "Ingen raporterede indlæg");
+define("FORLAN_122", "Klik her for at emaile admin når nogen raporterer et forum indlæg");
+define("FORLAN_123", "Forum Regler");
+define("WMGLAN_1", "Regler for Gæster");
+define("WMGLAN_2", "Regler for Medlemmer");
+define("WMGLAN_3", "Regler for Administratorer");
+define("WMGLAN_4", "Tilføj");
+define("WMGLAN_5", "Indstil Forum Regler");
+define("WMGLAN_6", "Aktiver?");
+define("FORLAN_124", "Links i nyt vindue");
+define("FORLAN_125", "Marker her for at alle links åbner i et nyt vindue (<i>dette vil gælde hele sitet</i>). ");
+define("FORLAN_126", "Vis Værktøjstip");
+define("FORLAN_127", "Marker her for at vise et værktøjstip indeholdende det første indlæg i en tråd når musen peger på tråd navnet.");
+define("FORLAN_128", "Længde på værktøjstip");
+define("FORLAN_129", "Dette vil bestemme antallet af tegn der vises i værktøjstippet.");
+define("FORLAN_130", "klik her");
+define("FORLAN_131", "til at indstille maks fil størrelse, tilladte typer osv. ");
+
+define("FORLAN_132", "Fremhæv Prioriterede Tråde");
+define("FORLAN_133", "Fremhæv prioriterede tråde ekstra (separer tråd sektioner og overskrifter)");
+define("FORLAN_134", "Maks bredde på uploadet billede");
+define("FORLAN_135", "Lad være tom for at slå automatisk ændring af billede størrelse fra");
+define("FORLAN_136", "Opret link til billede i fuld størrelse");
+define("FORLAN_137", "Anvendelse af dette vil ændre størrelsen på større billeder og oprette et link til original billledet i fuld størrelse.  Hvis ej anvendt, vil original billedet blive ignoreret");
+define("FORLAN_138", "Beskær disse forums");
+define("FORLAN_139", "For at bruge denne indstilling skal du også markere 'Tillad billede indlæg' på <a href='".e_ADMIN."image.php'>billeder</a> siden");
+define("FORLAN_140", "Vis");
+define("FORLAN_141", "Post");
+define("FORLAN_142", "Post tilladelse");
+define("FORLAN_143", "Angiver hvem der kan skrive i forummet");
+define("FORLAN_144", "Bestyrers sæt");
+define("FORLAN_145", "Konfigurer under-forums");
+define("FORLAN_146", "Ingen under-forums endnu");
+define("FORLAN_147", "Opdater under-forums");
+define("FORLAN_148", "Opret under-forums");
+define("FORLAN_149", "under-forums");
+define("FORLAN_150", "under-forum");
+define("FORLAN_151", "ID");
+define("FORLAN_152", "poster");
+define("FORLAN_153", "Værktøjer");
+define("FORLAN_154", "Svar Slettet");
+define("FORLAN_155", "Forum Kategorier");
+
+define("FORLAN_156", "Vælg forum(s) at udføre denne handling på");
+define("FORLAN_157", "Alle Forums");
+define("FORLAN_158", "Genberegn forum sidsteindlæg info");
+define("FORLAN_159", "Vælg for at genberegne sidsteindlæg info");
+define("FORLAN_160", "Vælg for kun at udføre dette på forums, ikke tråde");
+define("FORLAN_161", "Genberegn post / svar tal");
+define("FORLAN_162", "Vælg for ar genberegne forum tråd/svar tal");
+define("FORLAN_163", "Genberegn bruger forum poster tal");
+define("FORLAN_164", "Vælg for at genberegne bruger forum tal");
+define("FORLAN_165", "Udfør funktioner");
+define("FORLAN_166", "Forum Værktøjer");
+define("FORLAN_167", "Optællinger opdateret for forum");
+define("FORLAN_168", "Sidsteindlæg info opdateret for forum");
+define("FORLAN_169", "Bruger forum tælling opdateret");
+define("FORLAN_166", "Forum Værktøjer");
+define("FORLAN_166", "Forum Værktøjer");
+define("FORLAN_166", "Forum Værktøjer");
+
+
+
+?>
