797fd771e68424d8327650c3aa3e0d540a734489
e107dk <e107dk@localhost>
2005-07-23 17:16:17 +0000

    translatet file
    svn path=/trunk/e107_langpacks/; revision=4586

--- e107_plugins/list_new/languages/Danish.php
+++ e107_plugins/list_new/languages/Danish.php
@@ -4,18 +4,27 @@
 |        e107 website system  Language File
 |     
 |        $Source: /cvs_backup/e107_langpacks/e107_plugins/list_new/languages/Danish.php,v $
-|        $Revision: 1.1 $
-|        $Date: 2005-07-06 22:12:03 $
+|        $Revision: 1.2 $
+|        $Date: 2005-07-23 17:16:17 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
+
+
+
+
+
+
+
+
 define("PAGE_NAME", "Nyhedsliste");
 
+//all benaeth will be deprecated sometime
 define("LIST_1", "Nyheder");
 define("LIST_2", "ingen titel");
 define("LIST_3", "Nyt siden dit sidste besøg ...");
 define("LIST_4", "Intet nyt fundet");
-define("LIST_5", "Chatbox indlæg");
+define("LIST_5", "Chatboks indlæg");
 define("LIST_6", "blokeret af admin");
 define("LIST_7", "Forum indlæg");
 define("LIST_8", "Nye medlemmer");
@@ -29,11 +38,9 @@ define("LIST_15", "FAQ");
 define("LIST_16", "Fejl");
 define("LIST_17", "Ideer");
 
-
 define("LIST_13", "Profil");
 define("LIST_14", "Artikel");
 define("LIST_15", "Indhold");
-
 
 define("LIST_18", "FAQ");
 define("LIST_18", "Afstemning");
@@ -42,5 +49,195 @@ define("LIST_20", "Fejlsøger");
 
 define("LIST_21", "Artikler");
 define("LIST_22", "Ideer");
+//all above will be deprecated sometime
+
+define("LIST_23", "Denne plugin er ikke installeret.");
+
+
+define("LIST_PLUGIN_1", "Liste");
+define("LIST_PLUGIN_2", "Denne plugin lader dig se en liste over seneste tilføjelser i alle e107 kategorier. Du kan enten se listen med data siden dit sidste besøg, eller se en generel seneste tilføjelser liste. Udover siden er der også en menu tilstede. Hver sektion er kkan konfigureres i admin området.");
+define("LIST_PLUGIN_3", "Konfigurer Hovedmenu");
+define("LIST_PLUGIN_4", "List_new pluginen er nu klar til brug.");
+define("LIST_PLUGIN_5", "liste");
+
+
+define("LIST_ADMIN_1", "seneste");
+define("LIST_ADMIN_2", "opdater indstillinger");
+define("LIST_ADMIN_3", "indstillinger opdateret");
+define("LIST_ADMIN_4", "sektion");
+define("LIST_ADMIN_5", "menu");
+define("LIST_ADMIN_6", "side");
+define("LIST_ADMIN_7", "slået til");
+define("LIST_ADMIN_8", "slået fra");
+define("LIST_ADMIN_9", "åben");
+define("LIST_ADMIN_10", "lukket");
+define("LIST_ADMIN_11", "opdater");
+define("LIST_ADMIN_12", "vælg");
+define("LIST_ADMIN_13", "Velkommen til seneste nyt siden på ".SITENAME." ! Denne side viser de mest almindelige sektioner af dette site en liste med de seneste tilføjelser i disse sektioner.");
+define("LIST_ADMIN_14", "seneste tilføjelser");
+define("LIST_ADMIN_15", "nyt siden dit sidste besøg");
+define("LIST_ADMIN_16", "Velkommen til seneste nyt siden på ".SITENAME." ! Denne side viser de mest almindelige sektioner af dette site en liste med de seneste tilføjelser i disse sektioner siden dit sidste besøg.");
+
+define("LIST_ADMIN_SECT_1", "sektioner");
+define("LIST_ADMIN_SECT_2", "vælg hvilke sektioner der skal vises");
+define("LIST_ADMIN_SECT_3", "");
+
+define("LIST_ADMIN_SECT_4", "fremvisnings stil");
+define("LIST_ADMIN_SECT_5", "vælg hvilke sektioner der åbnes som standard");
+define("LIST_ADMIN_SECT_6", "");
+
+define("LIST_ADMIN_SECT_7", "forfatter");
+define("LIST_ADMIN_SECT_8", "vælg om forfatteren skal vises");
+define("LIST_ADMIN_SECT_9", "");
+
+define("LIST_ADMIN_SECT_10", "kategori");
+define("LIST_ADMIN_SECT_11", "vælg om kategorien skal vises");
+define("LIST_ADMIN_SECT_12", "");
+
+define("LIST_ADMIN_SECT_13", "dato");
+define("LIST_ADMIN_SECT_14", "vælg om datoen skal vises");
+define("LIST_ADMIN_SECT_15", "");
+
+define("LIST_ADMIN_SECT_16", "antal emner");
+define("LIST_ADMIN_SECT_17", "vælg hvor mange emner der skal vises i hver sektion");
+define("LIST_ADMIN_SECT_18", "");
+
+define("LIST_ADMIN_SECT_19", "sorter emner");
+define("LIST_ADMIN_SECT_20", "vælg den orden sektionerne skal vises");
+define("LIST_ADMIN_SECT_21", "");
+
+define("LIST_ADMIN_SECT_22", "ikon");
+define("LIST_ADMIN_SECT_23", "vælg et ikon til hver sektion");
+define("LIST_ADMIN_SECT_24", "");
+
+define("LIST_ADMIN_SECT_25", "overskrift");
+define("LIST_ADMIN_SECT_26", "definer en overskrift til hver sektion");
+define("LIST_ADMIN_SECT_27", "");
+
+
+define("LIST_ADMIN_OPT_1", "generelt");
+define("LIST_ADMIN_OPT_2", "seneste side");
+define("LIST_ADMIN_OPT_3", "seneste menu");
+define("LIST_ADMIN_OPT_4", "nyt side");
+define("LIST_ADMIN_OPT_5", "nyt menu");
+define("LIST_ADMIN_OPT_6", "egenskaber");
+
+define("LIST_ADMIN_MENU_2", "ikon : standard");
+define("LIST_ADMIN_MENU_3", "brug standard tema ikon hvis intet ikon er tilstede eller hvis ikon:brug er slået fra");
+define("LIST_ADMIN_MENU_4", "");
+
+define("LIST_ADMIN_LAN_2", "overskrift");
+define("LIST_ADMIN_LAN_3", "definer en overskrift");
+define("LIST_ADMIN_LAN_4", "");
+
+define("LIST_ADMIN_LAN_5", "ikon : brug");
+define("LIST_ADMIN_LAN_6", "brug ikon fra hver sektion");
+define("LIST_ADMIN_LAN_7", "");
+
+define("LIST_ADMIN_LAN_8", "tegn");
+define("LIST_ADMIN_LAN_9", "vælg hvor mange tegn af overskriften der skal vises");
+define("LIST_ADMIN_LAN_10", "lad være tom for at vise hele overskriften");
+
+define("LIST_ADMIN_LAN_11", "erstatnings tekst");
+define("LIST_ADMIN_LAN_12", "vælg en erstatnings tekst hvis overskriften er større end et vist antal tegn");
+define("LIST_ADMIN_LAN_13", "lad være tomt for ikke at bruge erstatnings tekst");
+
+define("LIST_ADMIN_LAN_14", "dato");
+define("LIST_ADMIN_LAN_15", "vælg en dato stil");
+define("LIST_ADMIN_LAN_16", "For flere informationer om dato formater se <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime funktions siden hos php.net</a>");
+
+define("LIST_ADMIN_LAN_17", "idag dato");
+define("LIST_ADMIN_LAN_18", "vælg dato stil hvis datoen er idag");
+define("LIST_ADMIN_LAN_19", "For flere informationer om dato formater se <a href='http://www.php.net/manual/en/function.strftime.php' rel='external'>strftime funktions siden hos php.net</a>");
+
+define("LIST_ADMIN_LAN_20", "kolonner");
+define("LIST_ADMIN_LAN_21", "vælg et antal kolonner");
+define("LIST_ADMIN_LAN_22", "definer hvor mange kolonner du ønsker at bruge. det nummer du angiver vil seperere siden i en lige del kolonner");
+
+define("LIST_ADMIN_LAN_23", "velkomst tekst");
+define("LIST_ADMIN_LAN_24", "definer en velkomst tekst der vil blive vist i toppen af siden");
+define("LIST_ADMIN_LAN_25", "");
+
+define("LIST_ADMIN_LAN_26", "vis tom");
+define("LIST_ADMIN_LAN_27", "definer hvis en besked skal vises når sektioner ikke har nogle resultater ");
+define("LIST_ADMIN_LAN_28", "");
+
+define("LIST_ADMIN_LAN_29", "ikon : standard");
+define("LIST_ADMIN_LAN_30", "brug standard tema ikon hvis intet ikon er tilstede eller hvis ikon:brug er slået fra");
+define("LIST_ADMIN_LAN_31", "");
+
+define("LIST_ADMIN_LAN_32", "varighed:dage");
+define("LIST_ADMIN_LAN_33", "maksimalt antal dage brugere kan se tilbage");
+define("LIST_ADMIN_LAN_34", "");
+define("LIST_ADMIN_LAN_35", "dage");
+
+define("LIST_ADMIN_LAN_36", "varighed");
+define("LIST_ADMIN_LAN_37", "vis en valgboks med antal dage der skal ses tilbage?");
+define("LIST_ADMIN_LAN_38", "");
+
+
+define("LIST_MENU_1", "seneste tilføjelser");
+define("LIST_MENU_2", "af");
+define("LIST_MENU_3", "den");
+define("LIST_MENU_4", "i");
+define("LIST_MENU_5", "dage");
+define("LIST_MENU_6", "vis indhold for hvor mange dage?");
+define("LIST_MENU_7", "");
+define("LIST_MENU_8", "");
+define("LIST_MENU_9", "");
+define("LIST_MENU_10", "");
+define("LIST_MENU_11", "");
+define("LIST_MENU_12", "");
+define("LIST_MENU_13", "");
+define("LIST_MENU_14", "");
+define("LIST_MENU_15", "");
+define("LIST_MENU_16", "");
+define("LIST_MENU_17", "");
+define("LIST_MENU_18", "");
+define("LIST_MENU_19", "");
+
+define("LIST_NEWS_1", "nyheder");
+define("LIST_NEWS_2", "ingen nyheder");
+
+define("LIST_COMMENT_1", "kommentarer");
+define("LIST_COMMENT_2", "ingen kommentarer");
+define("LIST_COMMENT_3", "nyheder");
+define("LIST_COMMENT_4", "faq");
+define("LIST_COMMENT_5", "afstemning");
+define("LIST_COMMENT_6", "dokumenter");
+define("LIST_COMMENT_7", "fejlsøger");
+define("LIST_COMMENT_8", "indhold");
+define("LIST_COMMENT_9", "download");
+define("LIST_COMMENT_10", "ideer");
+
+
+
+define("LIST_DOWNLOAD_1", "downloads");
+define("LIST_DOWNLOAD_2", "ingen downloads");
+
+define("LIST_MEMBER_1", "medlemmer");
+define("LIST_MEMBER_2", "ingen medlemmer");
+
+define("LIST_CONTENT_1", "indhold");
+define("LIST_CONTENT_2", "intet indhold i");
+define("LIST_CONTENT_3", "ingen gyldig indholds kategori");
+
+
+define("LIST_CHATBOX_1", "chatboks");
+define("LIST_CHATBOX_2", "ingen chatboks indlæg");
+
+define("LIST_CALENDAR_1", "kalender");
+define("LIST_CALENDAR_2", "ingen kalender begivenheder");
+
+define("LIST_LINKS_1", "links");
+define("LIST_LINKS_2", "ingen links");
+
+define("LIST_FORUM_1", "forum");
+define("LIST_FORUM_2", "ingen forum poster");
+define("LIST_FORUM_3", "visninger:");
+define("LIST_FORUM_4", "svar:");
+define("LIST_FORUM_5", "sidste post:");
+define("LIST_FORUM_6", "den:");
+
 
 ?>
