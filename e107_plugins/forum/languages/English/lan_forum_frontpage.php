357025c4f6d59f9fea39becebdf86fc8fa8a169c
g3d45 <g3d45@localhost>
2005-07-24 17:41:01 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4601

---
+++ e107_plugins/forum/languages/English/lan_forum_frontpage.php
@@ -0,0 +1,15 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/forum/languages/English/lan_forum_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-24 17:41:01 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+
+define("FOR_FP_1", "Forum");
+
+?>
--- e107_plugins/headlines_menu/languages/Lithuanian.php
+++ e107_plugins/headlines_menu/languages/Lithuanian.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/headlines_menu/languages/Lithuanian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-24 15:24:34 $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-24 17:38:57 $
 |     $Author: g3d45 $
 +----------------------------------------------------------------------------+
 */
@@ -13,7 +13,6 @@
 define("NFMENU_160", "Tinklalapio administratorius: ");
 define("NFMENU_161", "Naujienų antraštės");
 define("NFMENU_162", "Atnaujinta");
-
 define("NFMENU_163", "Neįmanoma atverti ");
 define("NFMENU_164", "Jūsų serveris neleidžia išorinių nuorodų atverti naudojantis fopen(), susisiekite su savo administratoriumi");
 define("NFMENU_165", "[daugiau]");
