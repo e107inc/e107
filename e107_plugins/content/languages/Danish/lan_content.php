1f5e0796588aa92460ebc28bc0d12df325f332c4
e107dk <e107dk@localhost>
2005-07-24 16:37:16 +0000

    Added + translatet file
    svn path=/trunk/e107_langpacks/; revision=4600

---
+++ e107_plugins/content/languages/Danish/lan_content.php
@@ -0,0 +1,198 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/content/languages/Danish/lan_content.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-24 16:37:16 $
+|     $Author: e107dk $
++----------------------------------------------------------------------------+
+*/
+
+define("CONTENT_EMAILPRINT_LAN_1", "dette indholds emne fra");
+
+define("CONTENT_ICON_LAN_0", "rediger");
+define("CONTENT_ICON_LAN_1", "slet");
+define("CONTENT_ICON_LAN_2", "egenskaber");
+define("CONTENT_ICON_LAN_3", "bruger detaljer");
+define("CONTENT_ICON_LAN_4", "download vedhæftning");
+define("CONTENT_ICON_LAN_5", "ny");
+define("CONTENT_ICON_LAN_6", "tilføj indhold");
+define("CONTENT_ICON_LAN_7", "forfatter liste");
+define("CONTENT_ICON_LAN_8", "advarsel");
+define("CONTENT_ICON_LAN_9", "ok");
+define("CONTENT_ICON_LAN_10", "fejl");
+define("CONTENT_ICON_LAN_11", "sorter emner i kategori");
+define("CONTENT_ICON_LAN_12", "sorter emner i hoved forælder");
+define("CONTENT_ICON_LAN_13", "personlig admin");
+define("CONTENT_ICON_LAN_14", "personlig indholdshåndtering");
+define("CONTENT_ICON_LAN_15", "vis");
+
+define("CONTENT_ADMIN_DATE_LAN_0", "Januar");
+define("CONTENT_ADMIN_DATE_LAN_1", "Februar");
+define("CONTENT_ADMIN_DATE_LAN_2", "Marts");
+define("CONTENT_ADMIN_DATE_LAN_3", "April");
+define("CONTENT_ADMIN_DATE_LAN_4", "Maj");
+define("CONTENT_ADMIN_DATE_LAN_5", "Juni");
+define("CONTENT_ADMIN_DATE_LAN_6", "Juli");
+define("CONTENT_ADMIN_DATE_LAN_7", "August");
+define("CONTENT_ADMIN_DATE_LAN_8", "September");
+define("CONTENT_ADMIN_DATE_LAN_9", "Oktober");
+define("CONTENT_ADMIN_DATE_LAN_10", "November");
+define("CONTENT_ADMIN_DATE_LAN_11", "December");
+define("CONTENT_ADMIN_DATE_LAN_12", "dag");
+define("CONTENT_ADMIN_DATE_LAN_13", "måned");
+define("CONTENT_ADMIN_DATE_LAN_14", "år");
+define("CONTENT_ADMIN_DATE_LAN_15", "start dato");
+define("CONTENT_ADMIN_DATE_LAN_16", "slut dato");
+define("CONTENT_ADMIN_DATE_LAN_17", "Du kan specificere en start dato for dette indholds emne. Hvis du bruger en dato i fremtiden vil indholds emnet være synligt fra den dato og fremover. Hvis du ikke behøver en specifik start dato, kan du blot efterlade disse felter som de er.");
+define("CONTENT_ADMIN_DATE_LAN_18", "Du kan specificere en slut dato for dette indholds emne. Med en slut dato kan du specificere indtil hvilket tidspunkt emnet behøver at være synlig. Hvis du ikke behøver en slut dato for dette indholds emne, kan du blot efterlade disse felter som de er.");
+
+define("CONTENT_PAGETITLE_LAN_0", "Indhold");
+define("CONTENT_PAGETITLE_LAN_1", "Start");
+define("CONTENT_PAGETITLE_LAN_2", "Seneste");
+define("CONTENT_PAGETITLE_LAN_3", "Kategori");
+define("CONTENT_PAGETITLE_LAN_4", "Bedst Bedømt");
+define("CONTENT_PAGETITLE_LAN_5", "Forfatter");
+define("CONTENT_PAGETITLE_LAN_6", "Arkiv");
+define("CONTENT_PAGETITLE_LAN_7", "Tilføj");
+define("CONTENT_PAGETITLE_LAN_8", "Tilføj Indholds Emne");
+define("CONTENT_PAGETITLE_LAN_9", "Personlig Indholds Håndtering");
+define("CONTENT_PAGETITLE_LAN_10", "Vis Emner");
+define("CONTENT_PAGETITLE_LAN_11", "Rediger Emner");
+define("CONTENT_PAGETITLE_LAN_12", "Opret Emne");
+define("CONTENT_PAGETITLE_LAN_13", "Kategorier");
+define("CONTENT_PAGETITLE_LAN_14", "Forfatter liste");
+define("CONTENT_PAGETITLE_LAN_15", "Top Score");
+
+define("CONTENT_SEARCH_LAN_0", "ingen indholds emner fundet med disse nøgleord.");
+
+define("CONTENT_ORDER_LAN_0", "sorter efter ...");
+define("CONTENT_ORDER_LAN_1", "overskrift (Stigende)");
+define("CONTENT_ORDER_LAN_2", "overskrift (Faldende)");
+define("CONTENT_ORDER_LAN_3", "dato (Stigende)");
+define("CONTENT_ORDER_LAN_4", "dato (Faldende)");
+define("CONTENT_ORDER_LAN_5", "henviser (Stigende)");
+define("CONTENT_ORDER_LAN_6", "henviser (Faldende)");
+define("CONTENT_ORDER_LAN_7", "forælder (Stigende)");
+define("CONTENT_ORDER_LAN_8", "forælder (Faldende)");
+define("CONTENT_ORDER_LAN_9", "orden (Stigende)");
+define("CONTENT_ORDER_LAN_10", "orden (Faldende)");
+define("CONTENT_ORDER_LAN_11", "forfatter (Stigende)");
+define("CONTENT_ORDER_LAN_12", "forfatter (Faldende)");
+
+define("CONTENT_LAN_0", "Indhold");
+define("CONTENT_LAN_1", "Seneste Liste");
+define("CONTENT_LAN_2", "Kategori Liste");
+define("CONTENT_LAN_3", "Kategori");
+define("CONTENT_LAN_4", "Forfatter Liste");
+define("CONTENT_LAN_5", "Forfatter");
+define("CONTENT_LAN_6", "alle kategorier");
+define("CONTENT_LAN_7", "alle forfattere");
+define("CONTENT_LAN_8", "bedst bedømte emner");
+define("CONTENT_LAN_9", "i");
+define("CONTENT_LAN_10", "den");
+define("CONTENT_LAN_11", "af");
+define("CONTENT_LAN_12", "bedst scorende emner");
+define("CONTENT_LAN_13", "");
+define("CONTENT_LAN_14", "");
+define("CONTENT_LAN_15", "");
+define("CONTENT_LAN_16", "");
+define("CONTENT_LAN_17", "");
+define("CONTENT_LAN_18", "søg efter nøgleord");
+define("CONTENT_LAN_19", "søg");
+define("CONTENT_LAN_20", "indhold søge resultater");
+define("CONTENT_LAN_21", "ingen indholds typer endnu.");
+define("CONTENT_LAN_22", "indholds typer");
+define("CONTENT_LAN_23", "Seneste Indholds Liste");
+define("CONTENT_LAN_24", "breadcrumb");
+define("CONTENT_LAN_25", "Indholds Kategorier");
+define("CONTENT_LAN_26", "Indholds Kategori");
+define("CONTENT_LAN_27", "underkategorier");
+define("CONTENT_LAN_28", "forælder underkategorier");
+define("CONTENT_LAN_29", "ukendt");
+define("CONTENT_LAN_30", "indholds emne");
+define("CONTENT_LAN_31", "indholds emner");
+define("CONTENT_LAN_32", "Indhold Forfatter Liste");
+define("CONTENT_LAN_33", "Gå Til Side");
+define("CONTENT_LAN_34", "indhold");
+define("CONTENT_LAN_35", "kommentarer");
+define("CONTENT_LAN_36", "moderer kommentarer");
+define("CONTENT_LAN_37", "ingen indholds emner bedømt endnu");
+define("CONTENT_LAN_38", "Bedst Bedømt Indhold");
+define("CONTENT_LAN_39", "forfatter liste");
+define("CONTENT_LAN_40", "forfatter bruger detaljer");
+define("CONTENT_LAN_41", "vedhæftet");
+define("CONTENT_LAN_42", "fil");
+define("CONTENT_LAN_43", "filer");
+define("CONTENT_LAN_44", "hits:");
+define("CONTENT_LAN_45", "forfatter tildelt score:");
+define("CONTENT_LAN_46", "artikel index");
+define("CONTENT_LAN_47", "forfatter");
+define("CONTENT_LAN_48", "indholds emner");
+define("CONTENT_LAN_49", "sidste indholds emne");
+define("CONTENT_LAN_50", "dato");
+define("CONTENT_LAN_51", "Type Liste");
+define("CONTENT_LAN_52", "ingen gyldige forfattere fundet");
+define("CONTENT_LAN_53", "emne");
+define("CONTENT_LAN_54", "emner");
+define("CONTENT_LAN_55", "seneste emne på");
+define("CONTENT_LAN_56", "vis oversigt over");
+define("CONTENT_LAN_57", "kommentarer:");
+define("CONTENT_LAN_58", "hjem");
+define("CONTENT_LAN_59", "indhold");
+define("CONTENT_LAN_60", "seneste");
+define("CONTENT_LAN_61", "vis seneste emner");
+define("CONTENT_LAN_62", "vis alle kategorier");
+define("CONTENT_LAN_63", "vis alle forfattere");
+define("CONTENT_LAN_64", "vis bedst bedømte emner");
+define("CONTENT_LAN_65", "tilføj indhold");
+define("CONTENT_LAN_66", "klik her for at tilføje indhold, du kan vælge kategorien på tilføj siden.");
+define("CONTENT_LAN_67", "personlig indholds håndtering");
+define("CONTENT_LAN_68", "klik her for at håndtere dit personlige indhold.");
+define("CONTENT_LAN_69", "email");
+define("CONTENT_LAN_70", "udskriv");
+define("CONTENT_LAN_71", "indholds emne");
+define("CONTENT_LAN_72", "kategori emne");
+define("CONTENT_LAN_73", "ingen indholds emner endnu");
+define("CONTENT_LAN_74", "");
+define("CONTENT_LAN_75", "tilføj indholds emne");
+define("CONTENT_LAN_76", "opret pdf fil af");
+define("CONTENT_LAN_77", "indhold søg");
+define("CONTENT_LAN_78", "side uden titel");
+define("CONTENT_LAN_79", "side");
+define("CONTENT_LAN_80", "seneste emner : ");
+define("CONTENT_LAN_81", "kategorier");
+define("CONTENT_LAN_82", "ingen emner endnu i");
+define("CONTENT_LAN_83", "emne arkiv");
+define("CONTENT_LAN_84", "Indholds Arkiv");
+define("CONTENT_LAN_85", "forfatter liste");
+define("CONTENT_LAN_86", "vis top scorer emner");
+define("CONTENT_LAN_87", "Top Scorer Indhold");
+define("CONTENT_LAN_88", "ingen indholds emner har en score endnu");
+define("CONTENT_LAN_89", "vælg side");
+
+define("CONTENT_MENU_LAN_0", "indholds Menu :");
+define("CONTENT_MENU_LAN_1", "ingen indhold emner endnu");
+define("CONTENT_MENU_LAN_2", "seneste emner");
+define("CONTENT_MENU_LAN_3", "kategorier");
+define("CONTENT_MENU_LAN_4", "indhold links");
+define("CONTENT_MENU_LAN_5", "");
+define("CONTENT_MENU_LAN_6", "");
+define("CONTENT_MENU_LAN_7", "");
+define("CONTENT_MENU_LAN_8", "");
+define("CONTENT_MENU_LAN_9", "");
+define("CONTENT_MENU_LAN_10", "");
+define("CONTENT_MENU_LAN_11", "");
+define("CONTENT_MENU_LAN_12", "");
+define("CONTENT_MENU_LAN_13", "");
+define("CONTENT_MENU_LAN_14", "");
+define("CONTENT_MENU_LAN_15", "");
+define("CONTENT_MENU_LAN_16", "");
+define("CONTENT_MENU_LAN_17", "");
+define("CONTENT_MENU_LAN_18", "");
+define("CONTENT_MENU_LAN_19", "");
+define("CONTENT_MENU_LAN_20", "");
+
+?>
