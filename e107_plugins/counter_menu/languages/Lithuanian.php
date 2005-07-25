7ec995cd2978b2fe9e8d228f951dd448d7853a96
g3d45 <g3d45@localhost>
2005-07-25 19:07:12 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4607

---
+++ e107_plugins/counter_menu/languages/Lithuanian.php
@@ -0,0 +1,21 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/counter_menu/languages/Lithuanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-25 19:07:12 $
+|     $Author: g3d45 $
++----------------------------------------------------------------------------+
+*/
+	
+define("COUNTER_L1", "Skaitiklis");
+define("COUNTER_L2", "Šiandien");
+define("COUNTER_L3", "Iš viso");
+define("COUNTER_L4", "Užfiksuota daugiausia");
+define("COUNTER_L5", "Statistikos rinkimas išjungtas.");
+define("COUNTER_L6", "Norėdami aktyvuoti, Jums reikia įdiegti Statistikos rinkimo įskiepį iš <a href='".e_ADMIN."plugin.php'>įskiepių sekcijos</a>, tada aktyvuoti iš <a href='".e_PLUGIN."log/admin_config.php'>sąrankos meniu</a>.");
+define("COUNTER_L7", "Unikalūs");
+	
+?>
