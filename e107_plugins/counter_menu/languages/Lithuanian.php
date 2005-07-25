27768068ff78bb91e1dddb80e23b3b332ffdf25f
g3d45 <g3d45@localhost>
2005-07-25 19:14:55 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4608

--- e107_plugins/counter_menu/languages/Lithuanian.php
+++ e107_plugins/counter_menu/languages/Lithuanian.php
@@ -4,18 +4,19 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/counter_menu/languages/Lithuanian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-25 19:07:12 $
+|     $Revision: 1.2 $
+|     $Date: 2005-07-25 19:14:55 $
 |     $Author: g3d45 $
 +----------------------------------------------------------------------------+
 */
 	
-define("COUNTER_L1", "Skaitiklis");
-define("COUNTER_L2", "Šiandien");
-define("COUNTER_L3", "Iš viso");
-define("COUNTER_L4", "Užfiksuota daugiausia");
-define("COUNTER_L5", "Statistikos rinkimas išjungtas.");
-define("COUNTER_L6", "Norėdami aktyvuoti, Jums reikia įdiegti Statistikos rinkimo įskiepį iš <a href='".e_ADMIN."plugin.php'>įskiepių sekcijos</a>, tada aktyvuoti iš <a href='".e_PLUGIN."log/admin_config.php'>sąrankos meniu</a>.");
-define("COUNTER_L7", "Unikalūs");
+define("COUNTER_L1", "Administratoriaus apsilankymai nėra skaičiuojami.");
+define("COUNTER_L2", "Šis puslapis šiandien...");
+define("COUNTER_L3", "iš viso");
+define("COUNTER_L4", "Užfiksuota daugiausia šiame puslapyje");
+define("COUNTER_L5", "unikalūs");
+define("COUNTER_L6", "Svetainė...");
+define("COUNTER_L7", "Skaitliukas");
+define("COUNTER_L8", "<b>Statistikos rinkimas</b> išjungtas. Norėdami aktyvuoti, Jums reikia įdiegti Statistikos rinkimo įskiepį iš <a href='".e_ADMIN."plugin.php'>įskiepių sekcijos</a>, tada aktyvuoti iš <a href='".e_PLUGIN."log/admin_config.php'>sąrankos meniu</a>.");
 	
 ?>
