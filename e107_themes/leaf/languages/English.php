4049eda53ba5af12cdaaec4eccd3a07793517d6f
qnome <qnome@localhost>
2005-07-27 06:19:47 +0000

    first release
    svn path=/trunk/e107_0.7/; revision=4615

---
+++ e107_themes/leaf/languages/English.php
@@ -0,0 +1,28 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_themes/leaf/languages/English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-07-27 06:19:47 $
+|     $Author: qnome $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "Comment(s) ");
+define("LAN_THEME_2", "Comments are turned off for this item");
+define("LAN_THEME_3", "comment(s) ");
+define("LAN_THEME_4", "Read the rest ...");
+define("LAN_THEME_5", "Trackbacks: ");
+define("LAN_THEME_6", "Comment by");
+
+
+?>
