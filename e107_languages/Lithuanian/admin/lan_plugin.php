6cfa20c82a25e7f03b5cd1c5160605165243582b
alkas <alkas@localhost>
2005-08-07 15:16:13 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4640

--- e107_languages/Lithuanian/admin/lan_plugin.php
+++ e107_languages/Lithuanian/admin/lan_plugin.php
@@ -4,70 +4,70 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Lithuanian/admin/lan_plugin.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-03 15:32:34 $
+|     $Revision: 1.3 $
+|     $Date: 2005-08-07 15:16:13 $
 |     $Author: alkas $
 +----------------------------------------------------------------------------+
 */
-define("EPL_ADLAN_0", "Install");
-define("EPL_ADLAN_1", "Uninstall");
-define("EPL_ADLAN_2", "Are you certain you want to uninstall this plugin?");
-define("EPL_ADLAN_3", "Confirm uninstall");
-define("EPL_ADLAN_4", "Uninstall cancelled.");
-define("EPL_ADLAN_5", "The install procedure will create new preference entries.");
-define("EPL_ADLAN_6", "... then click here to begin install procedure");
-define("EPL_ADLAN_7", "Database tables successfully upgraded.");
-define("EPL_ADLAN_8", "Preference settings successfully created.");
-define("EPL_ADLAN_9", "SQL commands failed. Check to be sure all upgrade changes are ok.");
-define("EPL_ADLAN_10", "Name");
-define("EPL_ADLAN_11", "Version");
-define("EPL_ADLAN_12", "Author");
-define("EPL_ADLAN_13", "Compatible");
-define("EPL_ADLAN_14", "Description");
-define("EPL_ADLAN_15", "Read the README file for more information");
-define("EPL_ADLAN_16", "Plugin Information");
-define("EPL_ADLAN_17", "More info...");
-define("EPL_ADLAN_18", "Unable to successfully create table(s) for this plugin.");
-define("EPL_ADLAN_19", "Database tables successfully created.");
-define("EPL_ADLAN_20", "Preference settings successfully created.");
+define("EPL_ADLAN_0", "Įdiegti");
+define("EPL_ADLAN_1", "Pašalinti");
+define("EPL_ADLAN_2", "Ar jūs tikrai norite pašalinti šį įskiepį?");
+define("EPL_ADLAN_3", "Pašalinimo patvirtinimas");
+define("EPL_ADLAN_4", "Pašalinimas sustabdytas.");
+define("EPL_ADLAN_5", "Įdiegimo procedūra įrašys naujus įrašus į pagrindinių nuostatų lentelę.");
+define("EPL_ADLAN_6", "... tuomet pradėkite įdiegimo procedūrą");
+define("EPL_ADLAN_7", "Duomenų bazės lentelės sėkmingai atnaujintos.");
+define("EPL_ADLAN_8", "Įskiepio nuostatos sėkmingai įrašytos į duomenų bazę.");
+define("EPL_ADLAN_9", "SQL komandos neįvykdytos. Patikrinkite ar visi pakeitimai atnaujinimo metu pavyko.");
+define("EPL_ADLAN_10", "Pavadinimas");
+define("EPL_ADLAN_11", "Versija");
+define("EPL_ADLAN_12", "Autorius");
+define("EPL_ADLAN_13", "Suderinamumas");
+define("EPL_ADLAN_14", "Aprašymas");
+define("EPL_ADLAN_15", "Perskaitykite README bylą.");
+define("EPL_ADLAN_16", "Informacija apie įskiepius");
+define("EPL_ADLAN_17", "Daugiau info...");
+define("EPL_ADLAN_18", "Nepavyko sukurti duomenų bazės lentelių šiam įskiepiui.");
+define("EPL_ADLAN_19", "Duomenų bazės lentelės šiam įskiepiui sėkmingai sukurtos.");
+define("EPL_ADLAN_20", "Įrašai nuostatų lentelėje sėkmingai įrašyti.");
 
-define("EPL_ADLAN_21", "Plugin is already installed.");
-define("EPL_ADLAN_22", "Installed");
-define("EPL_ADLAN_23", "Not installed");
-define("EPL_ADLAN_24", "Upgrade available");
-define("EPL_ADLAN_25", "No install required");
-define("EPL_ADLAN_26", "... then click here to begin uninstall procedure");
-define("EPL_ADLAN_27", "Unable to successfully delete ");
-define("EPL_ADLAN_28", "Database tables successfully deleted.");
-define("EPL_ADLAN_29", "Preference settings successfully deleted.");
-define("EPL_ADLAN_30", "please delete it manually.");
-define("EPL_ADLAN_31", "Please now delete the folder ");
-define("EPL_ADLAN_32", "and all files inside it to complete the uninstall process.");
-define("EPL_ADLAN_33", "Plugin successfully installed.");
-define("EPL_ADLAN_34", "Plugin successfully updated.");
+define("EPL_ADLAN_21", "Įskiepis jau yra įdiegtas.");
+define("EPL_ADLAN_22", "Įdiegti įskiepiai");
+define("EPL_ADLAN_23", "Neįdiegti įskiepiai");
+define("EPL_ADLAN_24", "Galimas atnaujinimas");
+define("EPL_ADLAN_25", "Įdiegimo nereikalauja");
+define("EPL_ADLAN_26", "... tuomet pradėkite įskiepio pašalinimo procedūrą");
+define("EPL_ADLAN_27", "Nepavyko ištrinti ");
+define("EPL_ADLAN_28", "Duomenų bazės lentelės sėkmingai ištrintos.");
+define("EPL_ADLAN_29", "Nuostatos sėkmingai ištrintos.");
+define("EPL_ADLAN_30", "prašome ištrinti patiems.");
+define("EPL_ADLAN_31", "Prašome ištrinti katalogą ");
+define("EPL_ADLAN_32", "ir visas jame esančias bylas.");
+define("EPL_ADLAN_33", "Įskiepis sėkmingai įdiegtas.");
+define("EPL_ADLAN_34", "Įskiepis sėkmingai atnaujintas.");
 define("EPL_ADLAN_35", "Parser settings successfully added.");
 define("EPL_ADLAN_36", "Parser code insert failed, incorrectly formatted.");
 
-define("EPL_ADLAN_37", "Upload plugin (.zip or .tar.gz format)");
-define("EPL_ADLAN_38", "Upload Plugin");
+define("EPL_ADLAN_37", "Atsiųsti įskiepio archyvą (.zip arba .tar.gz formatu)");
+define("EPL_ADLAN_38", "Siųsti įskiepį");
 define("EPL_ADLAN_39", "The file could not be uploaded as the ".e_PLUGIN." folder does not have the correct permissions - please CHMOD to 777 and reupload the file.");
-define("EPL_ADLAN_40", "Admin Message");
-define("EPL_ADLAN_41", "That file does not appear to be a valid .zip or .tar archive.");
-define("EPL_ADLAN_42", "An error has occurred, unable to un-archive the file");
-define("EPL_ADLAN_43", "Your plugin has been uploaded and unzipped, please scroll down to see your plugin in the list.");
-define("EPL_ADLAN_44", "Auto plugin upload and extraction is disabled as your plugins folder does not have the correct permissions - please CHMOD your e107_plugins folder to 777.");
-define("EPL_ADLAN_45", "Your menu item has been uploaded and unzipped, to activate go to <a href='".e_ADMIN."menus.php'>your menus page</a>.");
+define("EPL_ADLAN_40", "Administratoriaus pranešimas");
+define("EPL_ADLAN_41", "Ši byla yra ne .zip ir ne .tar formato archyvas.");
+define("EPL_ADLAN_42", "Klaida, bandant išskleisti archyvą.");
+define("EPL_ADLAN_43", "Jūsų įskiepis sėkmingai nusiųstas į serverį ir išarchyvuotas, atraskite jį žemiau esančiame įskiepių saraše.");
+define("EPL_ADLAN_44", "Automatiškas įskiepio archyvo nusiuntimas į serverį ir išarchyvavimas negalimas, nes Auto plugin upload and extraction is disabled as your plugins folder does not have the correct permissions - please CHMOD your e107_plugins folder to 777.");
+define("EPL_ADLAN_45", "Jūsų meniu punktas nusiųstas ir išarchyvuotas, aktyvuokite <a href='".e_ADMIN."menus.php'>meniu administravimo srityje</a>.");
 
 // define("EPL_CANCEL", "Cancel"); use LAN_CANCEL instead !!
 // define("EPL_EMAIL", "email");
-define("EPL_WEBSITE", "Website");
+define("EPL_WEBSITE", "tinklalapio adresas");
 // define("EPL_OPTIONS", "Options"); use LAN_OPTIONS instead!
-define("EPL_NOINSTALL", "No install required, just activate from your menus screen. To uninstall, delete the ");
-define("EPL_DIRECTORY", "directory.");
-define("EPL_NOINSTALL_1", "No install required, to remove delete the ");
-define("EPL_UPGRADE", "Upgrade");
+define("EPL_NOINSTALL", "Įskiepis įdiegimo nereikalauja, aktyvuokite įskiepį meniu administravimo srityje. Norėdami pašalinti, ištrinkite ");
+define("EPL_DIRECTORY", "katalogą.");
+define("EPL_NOINSTALL_1", "Įskiepis įdiegimo nereikalauja, norėdami pašalinti, ištrinkite ");
+define("EPL_UPGRADE", "Atnaujinti");
 
-define("EPL_CONFIGURE", "Configure");
+define("EPL_CONFIGURE", "Konfigūracija");
 
 
 ?>
