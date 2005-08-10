fca903c0644e3b56b746d4176aeb32341b2acb34
streaky <streaky@localhost>
2005-08-10 14:55:48 +0000

    Added in propper header and revision info
    svn path=/trunk/e107_0.7/; revision=4654

--- e107_plugins/log/loginfo.php
+++ e107_plugins/log/loginfo.php
@@ -1,15 +1,20 @@
 <?php
+
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system
 |
-|     ï¿½Steve Dunstan 2001-2002
+|     ©Steve Dunstan 2001-2002
 |     http://e107.org
 |     jalist@e107.org
 |
 |     Released under the terms and conditions of the
 |     GNU General Public License (http://gnu.org).
 |
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/log/loginfo.php,v $
+|     $Revision: 1.5 $
+|     $Date: 2005-08-10 14:55:48 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
