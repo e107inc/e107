ca45829d518680ef5b08146033f0945c38613727
natxocc <natxocc@localhost>
2005-08-28 17:44:53 +0000

    Changes from original CVS 28-08-05
    svn path=/trunk/e107_langpacks/; revision=4748

--- legacy_langpacks/e107_languages/Spanish/admin/lan_administrator.php
+++ legacy_langpacks/e107_languages/Spanish/admin/lan_administrator.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Spanish/admin/lan_administrator.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-08-28 09:26:19 $
+|     $Revision: 1.2 $
+|     $Date: 2005-08-28 17:44:53 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
@@ -16,17 +16,11 @@ define("ADMSLAN_3", "Es el administrador del sitio y no puede ser editado.");
 define("ADMSLAN_4", "Continuar");
 define("ADMSLAN_5", "¡Error!");
 define("ADMSLAN_6", "Es el administrador del sitio y no puede ser borrado.");
-define("ADMSLAN_7", "Por favor, confirme que desea borrarlo");
-define("ADMSLAN_8", "de la base de datos");
-define("ADMSLAN_9", "Cancelar");
-define("ADMSLAN_10", "Confirmar Borrado");
-define("ADMSLAN_11", "Confirmar Borrado de Administrador");
-define("ADMSLAN_12", "Borrado cancelado.");
+
 define("ADMSLAN_13", "Administradores existentes");
-define("ADMSLAN_14", "Borrar");
-define("ADMSLAN_15", "Editar");
-define("ADMSLAN_16", "Nombre");
-define("ADMSLAN_17", "Contraseña");
+
+define("ADMSLAN_16", "Nombre Admin");
+define("ADMSLAN_17", "Contraseña Admin");
 define("ADMSLAN_18", "Permisos");
 define("ADMSLAN_19", "Cambiar preferencias del sitio");
 define("ADMSLAN_20", "Cambiar menús y páginas personales");
@@ -35,7 +29,7 @@ define("ADMSLAN_22", "Moderar usuarios/expulsiones etc");
 define("ADMSLAN_23", "Crear/editar foros");
 define("ADMSLAN_24", "Gestionar categorías de descargas");
 define("ADMSLAN_25", "Transferir/gestionar archivos");
-//define("ADMSLAN_26", "Supervisar categorías de noticias");
+define("ADMSLAN_26", "Supervisar categorías de noticias");
 define("ADMSLAN_27", "Supervisar categorías de enlaces");
 define("ADMSLAN_28", "Apagar sitio por mantenimiento");
 define("ADMSLAN_29", "Gestionar anuncios/publicidad");
@@ -57,7 +51,7 @@ define("ADMSLAN_44", "Añadir Descargas");
 define("ADMSLAN_45", "Añadir Encuestas");
 define("ADMSLAN_46", "Mensaje de Bienvenida");
 define("ADMSLAN_47", "Moderar noticias enviadas");
-define("ADMSLAN_48", "Configurar plugins");
+
 define("ADMSLAN_49", "Seleccionar todo");
 define("ADMSLAN_51", "Quitar seleccion a todo");
 define("ADMSLAN_52", "Actualizar administrador");
@@ -65,13 +59,13 @@ define("ADMSLAN_53", "Añadir administradores");
 define("ADMSLAN_54", "Administradores");
 define("ADMSLAN_55", "Ha dejado campos en blanco");
 define("ADMSLAN_56", "Administrador");
-define("ADMSLAN_57", "Opciones");
+
 define("ADMSLAN_58", "Administrador principal");
 define("ADMSLAN_59", "Quitar estado de administrador");
 define("ADMSLAN_60", "¿Está seguro de querer quitar el estado de administrador?");
 define("ADMSLAN_61", "Administrador borrado");
 define("ADMSLAN_62", "Gestor de Plugins");
-define("ADMSLAN_63", "Plugin");
+
 define("ADMSLAN_64", "Limpiar la caché del sistema");
 define("ADMSLAN_65", "Configurar las opciones de Mail");
 define("ADMSLAN_66", "Configurar búsquedas");
