d7d9db19890e02f1bc13af5d5cf5207e75cfe3de
mijnheer <mijnheer@localhost>
2005-08-29 13:09:01 +0000

    Initial utf-8 upload
    svn path=/trunk/e107_langpacks/; revision=4755

---
+++ e107_languages/Dutch/lan_prefs.php
@@ -0,0 +1,19 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Dutch/lan_prefs.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-29 13:09:01 $
+|     $Author: mijnheer $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_PREF_1", "e107 powered website");
+define("LAN_PREF_2", "e107 website systeem");
+define("LAN_PREF_3", "Alle merknamen zijn auteursrechtelijk beschermd door hun respectievelijke eigenaars, alle andere content is eigendom van deze e107 powered website.<br />e107 is © e107.org 2002-2005 en is gepubliceerd onder de <a href=\"http://www.gnu.org/\" rel=\"external\">GNU GPL licentie</a>.");
+define("LAN_PREF_4", "gecensureerd");
+define("LAN_PREF_5", "Forums");
+
+?>
