79995ed7c719654986382ac28cfa194c334549e7
streaky <streaky@localhost>
2005-08-29 16:22:56 +0000

    XHTML Fixup
    svn path=/trunk/e107_0.7/; revision=4759

--- e107_themes/kubrick/languages/English.php
+++ e107_themes/kubrick/languages/English.php
@@ -11,13 +11,13 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_themes/kubrick/languages/English.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-05-13 15:12:12 $
-|     $Author: qnome $
+|     $Revision: 1.3 $
+|     $Date: 2005-08-29 16:22:56 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_THEME_1", "'kubrick' by <a href='http://e107.org' title='e107.org' rel='external'>jalist</a> & <a href='http://e107themes.org' title='e107themes.org' rel='external'>Que</a>, Based on original theme by Michael Heilemann (<a href='http://binarybonsai.com/kubrick/' title='http://binarybonsai.com/kubrick/' rel='external'>http://binarybonsai.com/kubrick/</a>. ).");
+define("LAN_THEME_1", "'kubrick' by <a href='http://e107.org' title='e107.org' rel='external'>jalist</a> &amp; <a href='http://e107themes.org' title='e107themes.org' rel='external'>Que</a>, Based on original theme by Michael Heilemann (<a href='http://binarybonsai.com/kubrick/' title='http://binarybonsai.com/kubrick/' rel='external'>http://binarybonsai.com/kubrick/</a>. ).");
 define("LAN_THEME_2", "Comments are turned off for this item");
 define("LAN_THEME_3", "comment: ");
 define("LAN_THEME_4", "Read the rest ...");
