a40f7e1dec45c5f4c69111b8b3f5c5ea30a03c88
matan_elgbsi <matan_elgbsi@users.sourceforge.net>
2005-08-31 13:09:17 +0000

    translatet to Hebrew
    svn path=/trunk/e107_langpacks/; revision=4787

---
+++ e107_languages/Hebrew/Hebrew.php
@@ -0,0 +1,12 @@
+<?php
+
+setlocale(LC_ALL, 'he');
+define("CORE_LC", 'he');
+define("CORE_LC2", 'il');
+define("CHARSET", "utf-8");  // for a true multi-language site. :)
+define("CORE_LAN1","שגיאה : ערכה חסרה.\\n\\nשנה את הערכה הנוכחית בהגדרות (אגור הניהול) או העלה קבצים של הערכה הנוכחית לשרת.");
+
+//v.616
+define("CORE_LAN2"," \\1 כתב:");// "\\1" מתייחס לשם המשתמש.
+define("CORE_LAN3","הצמדת קבצים מבוטלת");
+?>
