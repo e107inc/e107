bfa4c7a6d98a83cab241603dab5d5d213cc5cef6
adriansilea <adriansilea@localhost>
2005-08-31 11:07:52 +0000

    Adding "Romanian.php" and commiting.
    svn path=/trunk/e107_langpacks/; revision=4782

---
+++ e107_languages/Romanian/Romanian.php
@@ -0,0 +1,22 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Romanian/Romanian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-31 11:07:52 $
+|     $Author: adriansilea $
++----------------------------------------------------------------------------+
+*/
+setlocale(LC_ALL, 'ro');
+define("CORE_LC", 'ro');
+define("CORE_LC2", 'ro');
+define("CHARSET", "iso-8859-1");
+// define("CHARSET", "utf-8");
+define("CORE_LAN1","Eroare: tema aleasa lipseste.\\n\\nVa rugam, schimbati tema folosita din preferintele dvs. (Zona Admin) ori faceti upload fisierelor temei cu pricina, pe server.");
+
+//v.616
+define("CORE_LAN2"," \\1 a scris:");// "\\1" reprezinta username-ul.
+define("CORE_LAN3","atasarea de fisiere este dezactivata");
+?>
