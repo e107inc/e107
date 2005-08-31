87b7f2c22b17dbb498d99c0a83295ad3c1dd56c3
natxocc <natxocc@localhost>
2005-08-31 18:07:25 +0000

    Changes from original CVS 31-08-05
    svn path=/trunk/e107_langpacks/; revision=4793

---
+++ e107_plugins/pm/languages/Spanish.php
@@ -0,0 +1,25 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pm/languages/Spanish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-31 18:07:25 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+define('LAN_PM', "Mensajería Privada");
+define('LAN_PM_1', "Enviar mensaje privado");
+define('LAN_PM_2', "A:");
+define('LAN_PM_3', "Previsualizar");
+define('LAN_PM_4', "ClaseUsuario");
+define('LAN_PM_5', "Asunto");
+define('LAN_PM_6', "Mensaje");
+define('LAN_PM_7', "Caritas");
+define('LAN_PM_8', "Adjuntos");
+define('LAN_PM_9', "Confirmación lectura");
+define('LAN_PM_10', "Envíame un mensaje cuando se lea este MP");
+define('LAN_PM_11', "Añadir nueva transferencia");
+
+?>
---
+++ e107_plugins/pm/languages/admin/Spanish.php
@@ -0,0 +1,12 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pm/languages/admin/Spanish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-31 18:07:25 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+?>
