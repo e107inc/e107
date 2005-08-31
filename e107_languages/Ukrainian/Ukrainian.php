060e2b8812d25cd7f686545d6366c23ebc01cb61
ukie <ukie@localhost>
2005-08-31 06:01:01 +0000

    English yet UTF-8 for all files
    svn path=/trunk/e107_langpacks/; revision=4772

---
+++ e107_languages/Ukrainian/Ukrainian.php
@@ -0,0 +1,21 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/Ukrainian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-31 06:01:01 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+setlocale(LC_ALL, 'ua');
+define("CORE_LC", 'ua');
+define("CORE_LC2", 'ua');
+define("CHARSET", "utf-8");  // for a true multi-language site. :)
+define("CORE_LAN1","Помилка : такого Вигляду не існує.\\n\\nЗмініть Вигляд у Ваших Налаштунках (адміністративна частина) або завантажте файли цього Вигляду на сервер.");
+
+//v.616
+define("CORE_LAN2"," \\1 написав (-ла):");// "\\1" represents the username.
+define("CORE_LAN3","приєднані файли недоступні");
+?>
