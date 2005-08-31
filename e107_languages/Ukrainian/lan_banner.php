1d06f8dab9e287661323ba52c46bcd867bab2f3e
ukie <ukie@localhost>
2005-08-31 07:37:41 +0000

    v1.1 UTF-8 for all files
    svn path=/trunk/e107_langpacks/; revision=4776

--- e107_languages/Ukrainian/lan_banner.php
+++ e107_languages/Ukrainian/lan_banner.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_banner.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-08-31 06:42:28 $
+|     $Revision: 1.2 $
+|     $Date: 2005-08-31 07:37:41 $
 |     $Author: ukie $
 +----------------------------------------------------------------------------+
 */
