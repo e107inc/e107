7224783975de2981e9ea2453f625ad45815386e7
ukie <ukie@localhost>
2005-08-31 15:57:03 +0000

    ���� ������������� �� ��.
    svn path=/trunk/e107_langpacks/; revision=4788

--- e107_languages/Ukrainian/lan_download.php
+++ e107_languages/Ukrainian/lan_download.php
@@ -4,95 +4,95 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_download.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-08-31 07:57:59 $
+|     $Revision: 1.2 $
+|     $Date: 2005-08-31 15:57:03 $
 |     $Author: ukie $
 +----------------------------------------------------------------------------+
 */
-define("PAGE_NAME", "Закачка");
+define("PAGE_NAME", "Файли до завантаження");
 
-define("LAN_dl_1", "(Restricted)");
-define("LAN_dl_2", "No downloads yet, please check back soon");
-define("LAN_dl_3", "No downloads in this category yet");
-define("LAN_dl_4", "Files available: ");
-define("LAN_dl_5", "Total size of files: ");
-define("LAN_dl_6", "Files downloaded: ");
-define("LAN_dl_7", "Description");
-define("LAN_dl_8", "Get");
-define("LAN_dl_9", "Return to Category List");
-define("LAN_dl_10", "Filesize");
-define("LAN_dl_11", "Image");
-define("LAN_dl_12", "Rating");
-define("LAN_dl_13", "Not rated");
-define("LAN_dl_14", "Rate this download");
-define("LAN_dl_15", "Thanks for rating");
-define("LAN_dl_16", "download(s) from");
-define("LAN_dl_17", "files");
-define("LAN_dl_18", "Downloads");
-define("LAN_dl_19", "Category");
-define("LAN_dl_20", "Files");
-define("LAN_dl_21", "Size");
-define("LAN_dl_22", "Date");
-define("LAN_dl_23", "Filename");
-define("LAN_dl_24", "Author");
-define("LAN_dl_25", "Ascending");
-define("LAN_dl_26", "Descending");
-define("LAN_dl_27", "Go");
-define("LAN_dl_28", "Name");
-define("LAN_dl_29", "DL's");
-define("LAN_dl_30", "Author email");
-define("LAN_dl_31", "Author website");
-define("LAN_dl_32", "Download");
-define("LAN_dl_33", "Previous");
-define("LAN_dl_34", "Next");
-define("LAN_dl_35", "Back to list");
-define("LAN_dl_36", "New downloads");
+define("LAN_dl_1", "(Обмежено)");
+define("LAN_dl_2", "Наразі немає файлів, завітайте пізніше");
+define("LAN_dl_3", "У ций категорії немає поки файлів до завантаження");
+define("LAN_dl_4", "Файли до завантаження: ");
+define("LAN_dl_5", "Загальний розмір файлів: ");
+define("LAN_dl_6", "Завантажено файлів: ");
+define("LAN_dl_7", "Деталі");
+define("LAN_dl_8", "Взяти");
+define("LAN_dl_9", "Повернутися до Переліку Категорій");
+define("LAN_dl_10", "Довжина файлу");
+define("LAN_dl_11", "Картинка");
+define("LAN_dl_12", "Щабель");
+define("LAN_dl_13", "Не оцінювалось");
+define("LAN_dl_14", "Оцінити завантаження");
+define("LAN_dl_15", "Дякуємо за оцінку");
+define("LAN_dl_16", "завантаження з");
+define("LAN_dl_17", "файлів");
+define("LAN_dl_18", "Завантаження");
+define("LAN_dl_19", "Категорія");
+define("LAN_dl_20", "Файлів");
+define("LAN_dl_21", "Розмір");
+define("LAN_dl_22", "Число");
+define("LAN_dl_23", "Ім'я");
+define("LAN_dl_24", "Автор");
+define("LAN_dl_25", "До збільшення");
+define("LAN_dl_26", "До зменьшення");
+define("LAN_dl_27", "Зроби");
+define("LAN_dl_28", "Ім'я");
+define("LAN_dl_29", "Завантажень");
+define("LAN_dl_30", "е-пошта автора");
+define("LAN_dl_31", "Сторінка автора");
+define("LAN_dl_32", "Завантажити");
+define("LAN_dl_33", "Попередній(-ня)");
+define("LAN_dl_34", "Наступний(-на)");
+define("LAN_dl_35", "Повернутися до переліку");
+define("LAN_dl_36", "Нові файли");
 
-define("LAN_dl_37", "View");
-define("LAN_dl_38", "Order by");
-define("LAN_dl_39", "Sort");
+define("LAN_dl_37", "Перегляд");
+define("LAN_dl_38", "Сортування як");
+define("LAN_dl_39", "Сортувати");
 
-define("LAN_dl_40", "Click here for screenshot");
-define("LAN_dl_41", "Search Downloads");
+define("LAN_dl_40", "Натисніть для перегляду");
+define("LAN_dl_41", "Шукати у файлах до завантаження");
 
-define("LAN_dl_42", "Sub-category");
-define("LAN_dl_43", "vote");
-define("LAN_dl_44", "votes");
-define("LAN_dl_45", "Report broken download");
-define("LAN_dl_46", "click here to download");
+define("LAN_dl_42", "Під-категорії");
+define("LAN_dl_43", "голос");
+define("LAN_dl_44", "голосів");
+define("LAN_dl_45", "Повідомити про некоректні завантаження");
+define("LAN_dl_46", "натисніть до завантаження");
 
-define("LAN_dl_47", "Post has been reported");
-define("LAN_dl_48", "Download has been reported to administrator.<br />Thank You.");
-define("LAN_dl_49", "Click here to return to download");
-define("LAN_dl_50", "Report broken download to an administrator");
-define("LAN_dl_51", "Reporting download: ");
-define("LAN_dl_52", "Guest");
-define("LAN_dl_53", "Click to view download");
-define("LAN_dl_54", "An administrator will be made aware of this download, please leave a message if you feel it necessary.");
-define("LAN_dl_55", "<b>Do not</b> use this form to contact the admin for any other reason.");
-define("LAN_dl_56", "Report broken download");
-define("LAN_dl_57", "reported by");
-define("LAN_dl_58", "This post has been reported from site ");
-define("LAN_dl_59", "Message from: ");
-define("LAN_dl_60", "Broken download report from");
+define("LAN_dl_47", "Відрапортовано");
+define("LAN_dl_48", "Відрапортовано до адміна.<br />Дякуємо.");
+define("LAN_dl_49", "Натисніть тут повернутися до завантажень");
+define("LAN_dl_50", "Рапортувати про неробочі ланки (links) адміну");
+define("LAN_dl_51", "Про файл: ");
+define("LAN_dl_52", "Гість");
+define("LAN_dl_53", "Натисніть передивитися файл");
+define("LAN_dl_54", "Адмін буде попереджений про цей файл, можете також залишити необхідні коментарі.");
+define("LAN_dl_55", "<b>Не використовуйте</b> цю форму для інших повідомлень.");
+define("LAN_dl_56", "Повідомати про неробоче завантаження (ланку, файл)");
+define("LAN_dl_57", "надійшло від");
+define("LAN_dl_58", "Цей репорт надійшов з сайту ");
+define("LAN_dl_59", "Повідомлення від: ");
+define("LAN_dl_60", "Репорт на неробоче завантаження від");
 
-define("LAN_dl_61", "Download Error");
-define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded you download quota");
-define("LAN_dl_63", "You do not have the correct permissions to download this file.");
-define("LAN_dl_64", "Back");
-define("LAN_dl_65", "Not Found");
+define("LAN_dl_61", "Помилка Завантаження");
+define("LAN_dl_62", "Ви вже не можетк завантажувати цей файл, перевищено вашу квоту до завантаження");
+define("LAN_dl_63", "Ви не маєте відповідного дозволу завантажити цей файл.");
+define("LAN_dl_64", "Повернути");
+define("LAN_dl_65", "Не знайдено");
 
-define("LAN_dl_66", "Select download mirror");
+define("LAN_dl_66", "Обрати сервер-близнюк (mirror) для завантажень");
 
-define("LAN_dl_67", "Please select mirror to use ...");
-define("LAN_dl_68", "Mirror Host");
-define("LAN_dl_69", "Download");
-define("LAN_dl_70", "Location");
-define("LAN_dl_71", "About");
+define("LAN_dl_67", "Оберіть сервер-близнюк (mirror) ...");
+define("LAN_dl_68", "Сервер-близнюк (Mirror Host)");
+define("LAN_dl_69", "Завантаження");
+define("LAN_dl_70", "Знаходження");
+define("LAN_dl_71", "Про");
 
-define("LAN_dl_72", "Requesting file: ");
-define("LAN_dl_73", "Downloads from this mirror: ");
-define("LAN_dl_74", "Total downloads from this mirror: ");
+define("LAN_dl_72", "Запрошений файл: ");
+define("LAN_dl_73", "Завантаження з цього близнюка (mirror): ");
+define("LAN_dl_74", "Загалом завангтажено з цього близнюка: ");
 
-define("LAN_dl_75", "no image available ");
+define("LAN_dl_75", "немає картинок ");
 ?>
