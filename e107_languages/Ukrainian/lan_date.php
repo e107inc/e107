b766ea4ba0374458a957658a69f60a208f1d48a0
ukie <ukie@localhost>
2005-08-31 07:57:59 +0000

    v1.1 UTF-8 for all files
    svn path=/trunk/e107_langpacks/; revision=4777

---
+++ e107_languages/Ukrainian/lan_date.php
@@ -0,0 +1,35 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_date.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-08-31 07:55:31 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+define("LANDT_01", "рік");
+define("LANDT_02", "місяць");
+define("LANDT_03", "тиждень");
+define("LANDT_04", "день");
+define("LANDT_05", "година");
+define("LANDT_06", "хвилина");
+define("LANDT_07", "секунда");
+define("LANDT_01s", "років");
+define("LANDT_02s", "місяців");
+define("LANDT_03s", "тижнів");
+define("LANDT_04s", "днів");
+define("LANDT_05s", "годин");
+define("LANDT_06s", "хвилин");
+define("LANDT_07s", "секунд");
+
+define("LANDT_08", "хв");
+define("LANDT_08s", "хвн");
+define("LANDT_09", "сек");
+define("LANDT_09s", "секд");
+define("LANDT_AGO", "тому");
+
+
+?>
