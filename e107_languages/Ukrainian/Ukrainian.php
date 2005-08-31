f91940124f57b562128f5cac1215577e018fcb6d
ukie <ukie@localhost>
2005-08-31 06:20:38 +0000

    v1.1 UTF-8 for all files
    svn path=/trunk/e107_langpacks/; revision=4774

--- e107_languages/Ukrainian/Ukrainian.php
+++ e107_languages/Ukrainian/Ukrainian.php
@@ -1,11 +1,11 @@
-﻿<?php
+?<?php
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/Ukrainian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-08-31 06:01:01 $
+|     $Revision: 1.2 $
+|     $Date: 2005-08-31 06:20:38 $
 |     $Author: ukie $
 +----------------------------------------------------------------------------+
 */
