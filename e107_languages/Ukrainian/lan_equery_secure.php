7cfb7287409f715dfd95c764297649988164c28a
ukie <ukie@localhost>
2005-09-01 15:36:33 +0000

    I reffered myself to http://mova.info/wordlist.aspx?groupid=3&recscount=61&curpage=3
    svn path=/trunk/e107_langpacks/; revision=4806

---
+++ e107_languages/Ukrainian/lan_equery_secure.php
@@ -0,0 +1,19 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_equery_secure.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-01 15:36:33 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+define("EQSEC_LAN1", "Ви переадресовані до адмін функцій. Вважайте, можливо змінення бази данних");
+define("EQSEC_LAN2", "Підтвердіть цю дію:");
+define("EQSEC_LAN3", "Немає референта");
+define("EQSEC_LAN4", "Дія від:");
+define("EQSEC_LAN5", "Дія по відношенню до:");
+define("EQSEC_LAN6", "Підтвердити дію");
+define("EQSEC_LAN7", "Або скасуйте");
+?>
