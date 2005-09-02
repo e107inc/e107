c60d4cb682f690460b67fcf1df5f786ed375a72a
mijnheer <mijnheer@localhost>
2005-09-02 09:25:10 +0000

    Update sept 1st (by Nick)
    svn path=/trunk/e107_langpacks/; revision=4820

---
+++ e107_plugins/pm/languages/Dutch.php
@@ -0,0 +1,59 @@
+<?php
+
+define('LAN_PM', "Prive Bericht");
+define('LAN_PM_1', "verzend Prive Bericht");
+define('LAN_PM_2', "aan");
+define('LAN_PM_3', "Voorproef");
+define('LAN_PM_4', "Userclass");
+define('LAN_PM_5', "Onderwerp");
+define('LAN_PM_6', "Bericht");
+define('LAN_PM_7', "Emotes");
+define('LAN_PM_8', "Bijlagen");
+define('LAN_PM_9', "Gelezen ontvangstbewijs");
+define('LAN_PM_10', "Zend me een e-mail wanneer deze PM gelezen wordt");
+define('LAN_PM_11', "voeg nieuwe bijlage toe");
+define('LAN_PM_12', "u bent niet gemachtigd om het PM systeem te gebruiken");
+define('LAN_PM_13', "Uw outbox is momenteel {PERCENT}% vol, u kan geen PM's verzenden tot u wat items verwijderd.");
+define('LAN_PM_14', "FOUT: Mogelijke dubbel post, PM niet gestuurd");
+define('LAN_PM_15', "u mag niet naar userclass verzenden");
+define('LAN_PM_16', "moet lid van klasse zijn");
+define('LAN_PM_17', "Gebruiker niet gevonden");
+define('LAN_PM_18', "u mag geen PM's verzenden naar: ");
+define('LAN_PM_19', "Uw outbox is vol, u mag geen PM's verzenden");
+define('LAN_PM_21', "het Toevoegen van deze PM zal uw maximum outbox grootte overschrijden, PM niet verzonden.");
+define('LAN_PM_22', "Upload mislukt");
+define('LAN_PM_23', "u mag geen bijlagen verzenden");
+define('LAN_PM_24', "PM verwijderen");
+define('LAN_PM_25', "Inbox");
+define('LAN_PM_26', "Outbox");
+define('LAN_PM_27', "Ongelezen");
+define('LAN_PM_28', "N/A");
+define('LAN_PM_29', "Verzonden Bericht");
+define('LAN_PM_30', "Gelezen Bericht");
+define('LAN_PM_31', "van");
+define('LAN_PM_32', "Ontvangen");
+define('LAN_PM_33', "verzonden");
+define('LAN_PM_34', "Geen Berichten");
+define('LAN_PM_35', "verzend nieuw Prive Bericht");
+define('LAN_PM_36', "totaal");
+define('LAN_PM_37', "ongelezen");
+define('LAN_PM_38', "PM wordt naar userclass gestuurd");
+define('LAN_PM_39', "PM kon niet verzonden worden naar ");
+define('LAN_PM_40', "PM verzenden naar gebruiker");
+
+define('LAN_PM_41', "Er niet in geslaagd om PM aan uw Outbox toe te voegen.");
+define('LAN_PM_42', "PM verwijderd uit inbox");
+define('LAN_PM_43', "PM verwijderd uit outbox");
+define('LAN_PM_44', "Blokkering verwijderd: {UNAME} kan u nu PM's zenden");
+define('LAN_PM_45', "FOUT: Blokkering niet verwijderd. Onbekende error.");
+define('LAN_PM_46', "blokkeer niet op zijn plaats voor {UNAME}");
+define('LAN_PM_47', "Geblokkeerd: {UNAME} wordt niet meer toegestaan om u PM's te verzenden");
+
+define('LAN_PM_48', "FOUT: blokkering niet uitgevoerd, onbekende error.");
+define('LAN_PM_49', "FOUT: blokkering niet uitgevoerd {UNAME} wordt reeds geblokkeerd.");
+
+define('LAN_PM_50', "Blokkeer gebruiker");
+define('LAN_PM_51', "Gebruiker deblokkeren");
+define('LAN_PM_52', "Verwijderen");
+define('LAN_PM_53', "Verwijder Geselecteerd");
+?>
---
+++ e107_plugins/pm/languages/admin/Dutch.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ADLAN_PM', "Prive Berichten");
+define('ADLAN_PM_1', "Om te activeren ga naar je menu scherm e, kies het 'private_msg' menu in een van je menu arena's. <br /><br />Als je oude berichten moet converteren ga dan naar de config pagina en kies de 'convert' link.");
+define('ADLAN_PM_2', "Configureer Prive messenger");
+
+?>
