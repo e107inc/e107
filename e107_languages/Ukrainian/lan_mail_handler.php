2ccf5c90c31fe67b3eca8efbdd7f20296d99fbb2
ukie <ukie@localhost>
2005-09-06 17:03:08 +0000

    thank you OGall. It's 90% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4900

---
+++ e107_languages/Ukrainian/lan_mail_handler.php
@@ -0,0 +1,28 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_mail_handler.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-06 17:03:08 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("LANMAILH_1", "Створено за допомогою e107 ");
+define("LANMAILH_2", "Повідомлення у форматі MIME.");
+define("LANMAILH_3", " має неправильний формат");
+define("LANMAILH_4", "Сервер не сприймає таку е-адресу");
+define("LANMAILH_5", "Сервера не відповідає");
+define("LANMAILH_6", "Неможлтво знайти SMTP сервер.");
+define("LANMAILH_7", " повинно бути правильним.");
+define("LANMAILH_8", "");
+define("LANMAILH_9", "");
+
+
+?>
