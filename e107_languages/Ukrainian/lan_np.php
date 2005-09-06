fc0747acd341113b5ba324565093fb80d7474cd4
ukie <ukie@localhost>
2005-09-06 19:07:41 +0000

    thank you OGall. It's 100% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4908

---
+++ e107_languages/Ukrainian/lan_np.php
@@ -0,0 +1,21 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_np.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-06 19:07:41 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("NP_1", "Попередня сторінка");
+define("NP_2", "Наступна сторінка");
+define("NP_3", "Перехід на сорінку");
+
+?>
