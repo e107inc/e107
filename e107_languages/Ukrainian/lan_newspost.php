f4f69a9d46a352409fdf8e7c66e4874d745b9852
ukie <ukie@localhost>
2005-09-06 18:52:45 +0000

    thank you OGall. It's 90% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4905

---
+++ e107_languages/Ukrainian/lan_newspost.php
@@ -0,0 +1,49 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_newspost.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-06 18:52:45 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("NWSLAN_1", "Новину видалено.");
+define("NWSLAN_2", "Зазначити для підтверждення видалення.");
+define("NWSLAN_3", "Поки що новин немає.");
+define("NWSLAN_4", "Новини");
+define("NWSLAN_5", "Відкрити HTML редактор");
+define("NWSLAN_6", "Категорія");
+define("NWSLAN_7", "Редагувати");
+define("NWSLAN_8", "Видалити");
+define("NWSLAN_9", "зазначте для продовження");
+define("NWSLAN_10", "Немає доступних категорій.");
+define("NWSLAN_11", "Додавти/Редагувати категорію");
+define("NWSLAN_12", "Заголовок");
+define("NWSLAN_13", "Текст");
+define("NWSLAN_14", "Доповнення");
+define("NWSLAN_15", "Коментарі");
+define("NWSLAN_16", "Увімкнено");
+define("NWSLAN_17", "Вимкнено");
+define("NWSLAN_18", "Дозволити публікацію коментарів");
+define("NWSLAN_19", "Активація");
+define("NWSLAN_20", "Залиште порожнім для вимкнення автоактивації");
+define("NWSLAN_21", "Активувати між");
+define("NWSLAN_22", "Видимість");
+define("NWSLAN_23", "Зазначити для перегляду певним типом користувачів");
+define("NWSLAN_24", "Переглянути знову");
+define("NWSLAN_25", "Оновити новину в базі");
+define("NWSLAN_26", "Опублікувати новину");
+define("NWSLAN_27", "Переглянути");
+define("NWSLAN_28", "Нове повідомлення");
+define("NWSLAN_29", "Нова публікація");
+
+define("NWSLAN_30", "Показувати тільки заголовки");
+?>
