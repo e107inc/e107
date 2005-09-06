047aec07edceb97ce3788cb844f015810c65933f
ukie <ukie@localhost>
2005-09-06 19:05:08 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4906

---
+++ e107_languages/Ukrainian/lan_notify.php
@@ -0,0 +1,33 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_notify.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-06 19:05:08 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+define("NT_LAN_US_1", "Підписка Користувача");
+
+define("NT_LAN_UV_1", "Підписку Користувача Перевірено");
+define("NT_LAN_UV_2", "Запис відвідувань (session) користувачів");
+
+define("NT_LAN_LI_1", "Користувач увійшов");
+
+define("NT_LAN_LO_1", "Користувач вийшов");
+define("NT_LAN_LO_2", " вийшов з сайту");
+
+define("NT_LAN_FL_1", "Заборона Критичного Відхилення Від Теми (Flood Ban)");
+define("NT_LAN_FL_2", "IP адреса заборонена за Критчне Віжхилення Від Теми (banned for flooding)");
+
+define("NT_LAN_SN_1", "Новину Прийнято");
+
+define("NT_LAN_NU_1", "Поновлено");
+
+define("NT_LAN_ND_1", "Новину Видалено");
+define("NT_LAN_ND_2", "Номер (ІD) новини видалено");
+
+?>
