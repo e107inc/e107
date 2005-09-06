1958abbca81c7aa6d6651c515101a99f67d0fc0b
mcfly_e107 <mcfly_e107@localhost>
2005-09-06 01:52:25 +0000

    half of Bugtracker #1485
    svn path=/trunk/e107_0.7/; revision=4884

--- print.php
+++ print.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/print.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-06-29 14:07:02 $
-|     $Author: streaky $
+|     $Revision: 1.6 $
+|     $Date: 2005-09-06 01:52:25 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
@@ -84,7 +84,9 @@ else
 
 echo "
 	<div style=\"text-align:center\">
-	<img src=\"".e_IMAGE."logo.png\" alt=\"Logo\" />
+	";
+	echo $tp->parseTemplate("{LOGO}", TRUE);
+	echo "
 	</div>
 	<hr />
 	<br />
