e205264e5f9e621907f47d1631cc5f470b584dab
ukie <ukie@localhost>
2005-09-06 17:07:01 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4901

---
+++ e107_languages/Ukrainian/lan_membersonly.php
@@ -0,0 +1,25 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_membersonly.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-06 17:07:01 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Тільки для користувачів");
+
+define("LAN_MEMBERS_0", "закрита частина сайту");
+define("LAN_MEMBERS_1", "Це закрита частина".LAN_MEMBERS_0);
+define("LAN_MEMBERS_2","щоб увійти потрібно ввести <a href='login.php'>логін</a> або");
+define("LAN_MEMBERS_3","зареєструватись на сайті");
+define("LAN_MEMBERS_4","Натисніть, щоб повернутись на головну сторінку");
+
+?>
