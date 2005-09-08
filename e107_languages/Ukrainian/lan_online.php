44d01e3b142f0f2915c704f3c9b788c8218044b8
ukie <ukie@localhost>
2005-09-08 02:16:56 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4949

---
+++ e107_languages/Ukrainian/lan_online.php
@@ -0,0 +1,72 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_online.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-08 02:16:56 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+//v.616
+
+define("ONLINE_EL1", "Гостей: ");
+define("ONLINE_EL2", "Користувачів: ");
+define("ONLINE_EL3", "На цій сторінці: ");
+define("ONLINE_EL4", "В мережі: ");
+define("ONLINE_EL5", "Користувачів");
+define("ONLINE_EL6", "Останнім підписавяс користувач");
+define("ONLINE_EL7", "перегляд");
+define("ONLINE_EL8", "рекорд користувачів одночасно: ");
+define("ONLINE_EL9", "Дата: ");
+define("ONLINE_EL10", "Ім'я користувача");
+define("ONLINE_EL11", "Перегляд сторінки");
+define("ONLINE_EL12", "Відповісти на");
+define("ONLINE_EL13", "Форум");
+define("ONLINE_EL14", "Тема");
+define("ONLINE_EL15", "Сторінка");
+
+
+
+define("CLASSRESTRICTED", "Сторінка тільки для певної групи користувачів");
+define("ARTICLEPAGE", "Статті/Огляди");
+define("CHAT", "Чат");
+define("COMMENT", "Коментарі");
+define("DOWNLOAD", "Файли до завантаження");
+define("EMAIL", "email.php");
+
+
+define("FORUM", "Головна сторінка форуму");
+define("LINKS", "Ланки");
+define("NEWS", "Новини");
+define("OLDPOLLS", "Старi опитування");
+define("POLLCOMMENT", "Опитування");
+define("PRINTPAGE", "Друкувати");
+define("LOGIN", "Увійти у");
+define("SEARCH", "Пошук");
+define("STATS", "Статистика");
+define("SUBMITNEWS", "Додати новину");
+define("UPLOAD", "Завантаження файлів");
+define("USERPAGE", "Профiль користувача");
+define("USERSETTINGS", "Налаштування");
+define("ONLINE", "Користувачі, що під'єдналися");
+define("LISTNEW", "Переглянути оновлення");
+define("USERPOSTS", "Повідомлення користувача");
+define("SUBCONTENT", "Помістити статтю\огляд");
+define("TOP", "Активні дописувачі\гарячі теми");
+
+define("ADMINAREA", "Адмінка");
+define("BUGTRACKER", "Повідомлення про помилки");
+define("EVENT", "Список подій");
+define("CALENDAR", "Календар подій");
+define("FAQ", "ПоП");
+define("PM", "Приват (ПП)");
+define("SURVEY", "Форма опитування");
+define("ARTICLE", "Стаття");
+define("CONTENT", "Зміст");
+define("REVIEW", "Перегляд");
+
+
+?>
