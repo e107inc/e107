31afe9895f4792ad7a73ce50704f201d7b177927
ukie <ukie@localhost>
2005-09-08 02:26:37 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4950

---
+++ e107_languages/Ukrainian/lan_parser_functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_parser_functions.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-08 02:26:37 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+define("LAN_GUEST", "Гість");
+
+?>
