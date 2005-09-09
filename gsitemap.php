fb362845708efba724e0034ace88f88436756eef
e107steved <steved@e107.org>
2005-09-09 16:51:31 +0000

    Added header.
    svn path=/trunk/e107_0.7/; revision=4977

--- gsitemap.php
+++ gsitemap.php
@@ -1,4 +1,21 @@
 <?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/gsitemap.php,v $
+|     $Revision: 1.4 $
+|     $Date: 2005-09-09 16:51:31 $
+|     $Author: stevedunstan $
++----------------------------------------------------------------------------+
+*/
 require_once("class2.php");
 
 if(e_QUERY == "show")
