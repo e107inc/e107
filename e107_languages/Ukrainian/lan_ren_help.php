5c4ebdeb73e4f943308b8003fddec35b2cdfc68a
ukie <ukie@localhost>
2005-09-10 03:01:58 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=4986

---
+++ e107_languages/Ukrainian/lan_ren_help.php
@@ -0,0 +1,60 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_ren_help.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-10 03:01:58 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("LANHELP_1", "Чорний");
+define("LANHELP_2", "Блакитний");
+define("LANHELP_3", "Коричневий");
+define("LANHELP_4", "Ціан");
+define("LANHELP_5", "Синій");
+define("LANHELP_6", "Бордовий");
+define("LANHELP_7", "Зелений");
+define("LANHELP_8", "Індіго");
+define("LANHELP_9", "Оливковий");
+define("LANHELP_10", "Оранжевий");
+define("LANHELP_11", "Червоний");
+define("LANHELP_12", "Фіолетовий");
+define("LANHELP_13", "Білий");
+define("LANHELP_14", "Жовтий");
+
+define("LANHELP_15", "Дрібний");
+define("LANHELP_16", "Маленький");
+define("LANHELP_17", "Нормальний");
+define("LANHELP_18", "Великий");
+define("LANHELP_19", "Більший");
+define("LANHELP_20", "Дуже великий");
+
+define("LANHELP_21", "Обрати колір ...");
+define("LANHELP_22", "Обрати розмір ..");
+
+
+define("LANHELP_23", "Вставити лінк: [link]http://mysite.com[/link] або  [link=http://yoursite.com]Перейти до сайту[/link]");
+define("LANHELP_24", "Товстий шрифт: [b]Цей текст буде товстим[/b]", "font-weight:bold; width: 20px");
+define("LANHELP_25", "Курсив: [i]Цей текст буде нахиленим[/i]", "font-style:italic; width: 20px");
+define("LANHELP_26", "Підкреслений текст: [u]Цей текст будет підкресленим[/u]", "text-decoration: underline; width: 20px");
+define("LANHELP_27", "Вставити зображення: [img]image.jpg[/img]");
+define("LANHELP_28", "Відцентрувати: [center]Цей текст буде розміщено у центрі[/center]");
+define("LANHELP_29", "Розмістити слева: [left]Цей текст буде розміщено зліва[/left]");
+define("LANHELP_30", "Розмістити зправа: [right]Цей текст буде розміщено зправа[/right]");
+define("LANHELP_31", "Цитувати текст: [blockquote]Цей текст буде процитовано (indented)[/blockquote]");
+define("LANHELP_32", "Код - попередньо форматований код: [code]\$foo = bah;[/code]");
+define("LANHELP_33", "HTML - видаляє пробіли між стрічками: [html]<table><tr><td> etc[/html]");
+define("LANHELP_34", "Розмістити статтю більш, ніж на одну сторінку");
+
+
+define("LANHELP_35", "ланка url");
+define("LANHELP_36", "Невпорядковано: [list]line1*line2*line3[/list] Впорядковано: [list=type]line1*line2*line3[/list]");
+
+
+?>
