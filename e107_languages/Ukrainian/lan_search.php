d4e6b3c2beb778af4e494f20c87738f931fae261
ukie <ukie@localhost>
2005-09-10 06:14:11 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4991

---
+++ e107_languages/Ukrainian/lan_search.php
@@ -0,0 +1,131 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_search.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-10 06:14:11 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Пошук");
+
+define("LAN_98", "Теми новин");
+define("LAN_99", "Коментарі");
+
+define("LAN_140", "Користувачі");
+
+define("LAN_180", "Пошук");
+
+
+define("LAN_192", "Всі категорії");
+define("LAN_193", "Календар подій");
+define("LAN_194", "Всі категорії");
+define("LAN_195", "Пошук");
+define("LAN_196", "знайдено");
+
+
+define("LAN_197", "Завантаження");
+define("LAN_198", "Нічого не знайдено");
+define("LAN_199", "Шукати:");
+define("LAN_200", "Категорії:");
+
+define("LAN_201", "Будь-ласка, перегляньте Ваші критерії пошуку");
+define("LAN_416", "Ви повинні увійти в систему, щоб отримати доступ до цієї сторінки");
+
+define("LAN_417", "Введіть мінімум 3 символи для пошуку.");
+define("LAN_418", "Ви повинні авторизуватися для перегляду цієї сторінки.");
+define("LAN_419", "Ви повинні авторизуватися для перегляду цієї сторінки.");
+
+
+
+define("LAN_418", "Інші сторінки");
+
+define("LAN_SEARCH_1", "Вибрати Усе");
+define("LAN_SEARCH_2", "Очистити Усе");
+define("LAN_SEARCH_3", "Написано до ");
+define("LAN_SEARCH_4", "Відповідні знайдено у заголовку новини");
+define("LAN_SEARCH_5", "Відповідні знайдено у тексту новин");
+define("LAN_SEARCH_6", "Відповідні знайдено у Розширеному Тексті Новин");
+define("LAN_SEARCH_7", "Доправлено від ");
+define("LAN_SEARCH_8", " на ");
+define("LAN_SEARCH_9", "Без Назви");
+define("LAN_SEARCH_10", "Перейти до сторінки:");
+define("LAN_SEARCH_11", "Результати");
+define("LAN_SEARCH_12", "  "); //"of"
+define("LAN_SEARCH_13", " в ");
+define("LAN_SEARCH_14", "Категорія:");
+define("LAN_SEARCH_15", "Автор:");
+define("LAN_SEARCH_17", "Вибачте, пошук обмежено до однієї спроби кожні");
+define("LAN_SEARCH_18", " секунди.");
+define("LAN_SEARCH_19", "Шукати Посеред:");
+define("LAN_SEARCH_20", "Потрібно авторизуватися");
+define("LAN_SEARCH_21", "Ви не авторизовані, щоб переглядати цю сторінку.");
+
+
+
+define("LAN_SEARCH_22", "Усе");
+define("LAN_SEARCH_23", "Розширена Форма Запиту");
+define("LAN_SEARCH_24", "Повинно містити слово (-ва)");
+define("LAN_SEARCH_25", "Не повинно містити слово (-ва)");
+define("LAN_SEARCH_26", "Фразу Цілком");
+define("LAN_SEARCH_27", "Слово(-ва) починаються з ");
+define("LAN_SEARCH_28", "Усі НЕ Мають розширений пошук");
+define("LAN_SEARCH_29", "Базис");
+define("LAN_SEARCH_30", "Розширений");
+define("LAN_SEARCH_31", "Не Має Розширеного пошуку");
+define("LAN_SEARCH_32", "Наступні слова було виключено з пошуку");
+define("LAN_SEARCH_33", "Наступне слово було виключено з пошуку");
+define("LAN_SEARCH_34", "Новіше від");
+define("LAN_SEARCH_35", "Старше від");
+define("LAN_SEARCH_36", "Будь коли");
+define("LAN_SEARCH_37", "Один день");
+define("LAN_SEARCH_38", "Два дні");
+define("LAN_SEARCH_39", "Три дні");
+define("LAN_SEARCH_40", "Один тиждень");
+define("LAN_SEARCH_41", "Два тиждні");
+define("LAN_SEARCH_42", "Три тижні");
+define("LAN_SEARCH_43", "Один місяць");
+define("LAN_SEARCH_44", "Два місяці");
+define("LAN_SEARCH_45", "Три місяці");
+define("LAN_SEARCH_46", "Пів року");
+define("LAN_SEARCH_47", "Один рік");
+define("LAN_SEARCH_48", "Два роки");
+define("LAN_SEARCH_49", "Три роки");
+
+define("LAN_SEARCH_50", "Дата повідомлення");
+define("LAN_SEARCH_51", "Усі категорії");
+define("LAN_SEARCH_52", "Співпало у");
+define("LAN_SEARCH_53", "Повідомоення цілком");
+define("LAN_SEARCH_54", "Лише заголовок");
+define("LAN_SEARCH_55", "Пошук у розділі Новин");
+define("LAN_SEARCH_56", "Всі Новини Впорядковано по категоріях");
+define("LAN_SEARCH_57", "Коментар відправлено до");
+define("LAN_SEARCH_58", "Всі Області");
+define("LAN_SEARCH_59", "Всі Коментарі");
+define("LAN_SEARCH_60", "Коментарі відправлено до");
+define("LAN_SEARCH_61", "Автором");
+define("LAN_SEARCH_62", "Date joined");
+define("LAN_SEARCH_63", "Пошук у категорії");
+define("LAN_SEARCH_64", "Всі Категорії Завантаження");
+define("LAN_SEARCH_65", "Завантаження");
+define("LAN_SEARCH_66", "Дату додано");
+define("LAN_SEARCH_67", "Подробиці всіх завантажень");
+define("LAN_SEARCH_68", "Дата");
+define("LAN_SEARCH_69", "Відповідність");
+
+define("LAN_SEARCH_70", "Написане, щоб завантажити (Posted to download item)");
+define("LAN_SEARCH_71", "Написане у відповідь на новину");
+define("LAN_SEARCH_72", "Підпис");
+define("LAN_SEARCH_73", "Підпису немає.");
+define("LAN_SEARCH_74", "Додано на");
+
+define("LAN_SEARCH_75", "Search type");
+
+?>
