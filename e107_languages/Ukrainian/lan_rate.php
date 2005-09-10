a8bc702d02bfbdde85b616c79ad20a73ee29d61e
ukie <ukie@localhost>
2005-09-10 02:42:19 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=4985

---
+++ e107_languages/Ukrainian/lan_rate.php
@@ -0,0 +1,20 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_rate.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-10 02:42:19 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+define("RATELAN_0", "голоси");
+define("RATELAN_1", "голосів");
+define("RATELAN_2", "як ви оцінюєте це?");
+define("RATELAN_3", "дякуємо за ваш голос");
+define("RATELAN_4", "не оцінювалося");
+define("RATELAN_5", "Оцінка");
+
+?>
