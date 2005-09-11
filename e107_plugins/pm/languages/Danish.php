cffd414080b4f6ed772676d17b615cccd435335b
e107dk <e107dk@localhost>
2005-09-11 18:29:08 +0000

    added translated file
    svn path=/trunk/e107_langpacks/; revision=5012

---
+++ e107_plugins/pm/languages/Danish.php
@@ -0,0 +1,76 @@
+<?php
+
+define('LAN_PM', "Privat Besked");
+define('LAN_PM_1', "Send Privat Besked");
+define('LAN_PM_2', "Til");
+define('LAN_PM_3', "Fremvis");
+define('LAN_PM_4', "Brugergruppe");
+define('LAN_PM_5', "Emne");
+define('LAN_PM_6', "Besked");
+define('LAN_PM_7', "Smileys");
+define('LAN_PM_8', "Vedhæftninger");
+define('LAN_PM_9', "Læst kvitering");
+define('LAN_PM_10', "Send mig en email når denne PB er læst");
+define('LAN_PM_11', "tilføj nyt upload");
+define('LAN_PM_12', "Du er ikke autoriseret til at bruge PB systemet");
+define('LAN_PM_13', "Din udbakke er pt. {PROCENT}% fuld, du kan ikke sende PBer før du sletter nogle");
+define('LAN_PM_14', "FEJL: Mulig dobbelt post, PB ikke sendt");
+define('LAN_PM_15', "Du har ikke tilladelse til at sende til brugergruppe");
+define('LAN_PM_16', "Skal være medlem af gruppe");
+define('LAN_PM_17', "Bruger ikke fundet");
+define('LAN_PM_18', "Du har ikke tilladelse til at sende PBer til: ");
+define('LAN_PM_19', "Din udbakke er fuld, du kan ikke sende PBer");
+define('LAN_PM_21', "Tilføjelse af denne PB vil overskride din maksimale udbakke størrelse, PB ikke postet");
+define('LAN_PM_22', "Fil upload slog fejl");
+define('LAN_PM_23', "Du har ikke tilladelse til at sende vedhæftninger");
+define('LAN_PM_24', "Sletter PB");
+define('LAN_PM_25', "Indbakke");
+define('LAN_PM_26', "Udbakke");
+define('LAN_PM_27', "Ulæst");
+define('LAN_PM_28', "N/A");
+define('LAN_PM_29', "Besked Sendt");
+define('LAN_PM_30', "Besked Læst");
+define('LAN_PM_31', "Fra");
+define('LAN_PM_32', "Modtaget");
+define('LAN_PM_33', "Sendt");
+define('LAN_PM_34', "Ingen Beskeder");
+define('LAN_PM_35', "Send ny besked");
+define('LAN_PM_36', "ialt");
+define('LAN_PM_37', "ulæst");
+define('LAN_PM_38', "PB Sendt til brugergruppe");
+define('LAN_PM_39', "Kunne ikke sende PB til");
+define('LAN_PM_40', "PB Sendt til bruger");
+define('LAN_PM_41', "Kunne ikke poste PB til din Udbakke");
+
+define('LAN_PM_42', "PB slettet fra indbakke");
+define('LAN_PM_43', "PB slettet fra udbakke");
+define('LAN_PM_44', "Blokering fjernet: {UNAME} er nu tilladt at sende PBer til dig");
+define('LAN_PM_45', "FEJL: Blokering ikke fjernet, ukendt fejl");
+define('LAN_PM_46', "Blokering ikke tilstede for {UNAME}");
+define('LAN_PM_47', "Blokering tilføjet: {UNAME} er ikke længere i stand til at sende PBer til dig");
+define('LAN_PM_48', "FEJL: Blokering ikke tilføjet, ukendt fejl");
+define('LAN_PM_49', "FEJL: Blokering er allerede oprette for {UNAME}");
+define('LAN_PM_50', "Bloker Bruger");
+define('LAN_PM_51', "Afbloker Bruger");
+define('LAN_PM_52', "Slet");
+define('LAN_PM_53', "Slet Valgte");
+define('LAN_PM_54', "Citer original");
+define('LAN_PM_55', "Send Svar");
+define('LAN_PM_56', "Du har ikke tilladelse til at svare på denne besked");
+define('LAN_PM_57', "Besked ikke fundet");
+define('LAN_PM_58', "Sv: ");
+define('LAN_PM_59', "Gå til side: ");
+define('LAN_PM_60', "Du har ikke tilladelse til at se denne besked");
+define('LAN_PM_61', "Intet emne");
+
+define("LAN_PM_100", "Ny PB fra ");
+define("LAN_PM_101", "Du har modtaget en ny Privat Besked fra ");
+define("LAN_PM_102", "Besked sendt fra: ");
+define("LAN_PM_103", "Besked emne: ");
+define("LAN_PM_104", "Antal vedhæftninger: ");
+define("LAN_PM_105", "Du kan se PBen på: ");
+define("LAN_PM_106", "PB læst af ");
+define("LAN_PM_107", "Den Private Besked du sendte til {UNAME} blev læst ");
+define("LAN_PM_108", "Besked sendt: ");
+
+?>
