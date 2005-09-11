88bab0d6dd2bac096fdb3769b0503c3de708302f
e107dk <e107dk@localhost>
2005-09-11 15:16:04 +0000

    added recent lines and changed modul to plugin
    svn path=/trunk/e107_langpacks/; revision=5003

--- e107_plugins/log/languages/admin/Danish.php
+++ e107_plugins/log/languages/admin/Danish.php
@@ -4,16 +4,23 @@
 |     e107 website system  Language File
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/log/languages/admin/Danish.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-06 22:12:03 $
+|     $Revision: 1.2 $
+|     $Date: 2005-09-11 15:16:04 $
 |     $Author: e107dk $
 +----------------------------------------------------------------------------+
 */
+
+
+
+
+
+
+
 	
 define("ADSTAT_ON", "Til");
 define("ADSTAT_OFF", "Fra");
-define("ADSTAT_L1", "Statistik modul logger alle besøg på webstedet, og danner en detaljeret statistik på grundlag af de indsamlede informationer.");
-define("ADSTAT_L2", "Statistik modul er installeret. Aktiver på indstillingssiden.<br /><b> e107_plugins/log/logs folderen skal have 777 tilladelser (chmod 777)</b>");
+define("ADSTAT_L1", "Statistik plugin logger alle besøg på webstedet, og danner en detaljeret statistik på grundlag af de indsamlede informationer.");
+define("ADSTAT_L2", "Statistik plugin er installeret. Aktiver på indstillingssiden.<br /><b> e107_plugins/log/logs folderen skal have 777 tilladelser (chmod 777)</b>");
 define("ADSTAT_L3", "Statistik logning");
 define("ADSTAT_L4", "Aktiver statistik logning");
 define("ADSTAT_L5", "Statistik typer");
@@ -37,9 +44,17 @@ define("ADSTAT_L22", "Kør opdatering");
 define("ADSTAT_L23", "logs fra en tiligere version af e107 er fundet, opdater dem her");
 define("ADSTAT_L24", "Gå til opdatering");
 define("ADSTAT_L25", "Valgte statistikker nulstilles");
-
 define("ADSTAT_L26", "Fjern side poster");
 define("ADSTAT_L27", "hvis statistikken har ukorrekte sider kan de fjernes her");
 define("ADSTAT_L28", "Åben side");
+
+
+define("ADSTAT_L29", "Side Navn");
+define("ADSTAT_L30", "Marker for at fjerne");
+define("ADSTAT_L31", "Fjern valgte sider");
+define("ADSTAT_L32", "Side Trim");
+
+
+
 
 ?>
