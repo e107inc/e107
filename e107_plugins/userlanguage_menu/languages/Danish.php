0d74f2c31c0f513fe3183ed12e915de5cb2b5c03
e107dk <e107dk@localhost>
2005-09-11 17:50:08 +0000

    added recent line
    svn path=/trunk/e107_langpacks/; revision=5009

--- e107_plugins/userlanguage_menu/languages/Danish.php
+++ e107_plugins/userlanguage_menu/languages/Danish.php
@@ -4,13 +4,20 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/userlanguage_menu/languages/Danish.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-07-06 22:12:04 $
+|     $Revision: 1.2 $
+|     $Date: 2005-09-11 17:50:08 $
 |     $Author: e107dk $
 +----------------------------------------------------------------------------+
 */
 
+
+
+
+
+
+
+
 define("UTHEME_MENU_L1", "Sprogvalg");
 define("UTHEME_MENU_L2", "Vælg sprog");
-
+define("UTHEME_MENU_L3", "tabeller");
 ?>
