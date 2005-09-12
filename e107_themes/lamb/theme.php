0963d73706c9cf349d7e8ca0964f3453e0f68ca6
mcfly_e107 <mcfly_e107@localhost>
2005-09-12 23:50:17 +0000

    returning lamb theme to previous
    svn path=/trunk/e107_0.7/; revision=5036

--- e107_themes/lamb/theme.php
+++ e107_themes/lamb/theme.php
@@ -122,7 +122,14 @@ define('LINKDISPLAY', 2);
 define('LINKALIGN', "left");
 
 
+//	[tablestyle]
 
+function tablestyle($caption, $text, $mode)
+{
+	echo "<h4><img src='".THEME_ABS."images/bluearrow_greybg.png' alt='' width='6' height='9' /> {$caption}</h4>\n<br />
+	{$text}
+	<br /><br />\n";	
+}
 
 $COMMENTSTYLE = "
 <table style='width: 100%;'>
