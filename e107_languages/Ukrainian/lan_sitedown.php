a7b07f4ad8e85c287edff16b0b227c07e9e19900
ukie <ukie@localhost>
2005-09-12 17:00:56 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5021

---
+++ e107_languages/Ukrainian/lan_sitedown.php
@@ -0,0 +1,24 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_sitedown.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-12 16:59:43 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Сайт тимчасово замкнено");
+'define("LAN_00", "<table border='0' width='100%' height='100%' cellspacing='0' cellpadding='0'><tr><td height='100%' width='100%' align='center'><font size='7'>[ <font color='#808080'>Сайт тимчасово недосутпний</font> ]</font><hr><font size='4'>Прийміть наші щирі вибачення. Відвідайте нас пізніше.</font></td></tr></table>");
+
+'define("LAN_01", "<table border='0' width='100%' height='100%' cellspacing='0' cellpadding='0'><tr><td height='100%' width='100%' align='center'><font size='7'>[ <font color='#808080'>Сайт тимчасово недосутпний по технічним причинам</font> ]</font><hr><font size='4'>Прийміть наші щирі вибачення. Відвідайте нас пізніше.</font></td></tr></table>");
+
+define("LAN_00", "тичасово замкнено");
+define("LAN_01", "Сайт тимчасово замкнено для доробки.. Це не повинно бути довго - будьласка, завітайте незабаром. Вибачте за незручності.");
+?>
