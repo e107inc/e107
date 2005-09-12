46246dd82aa4ffe2ffe9c5e799276a0ff01d92d1
ukie <ukie@localhost>
2005-09-12 17:21:07 +0000

    thank you OGall. It's 90% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=5024

---
+++ e107_languages/Ukrainian/lan_top.php
@@ -0,0 +1,35 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_top.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-12 17:21:07 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Рейтинг користувачів");
+
+define("TOP_LAN_0", "Рейтинг користувачів форуму");
+define("TOP_LAN_1", "Ім'я користувача");
+define("TOP_LAN_2", "Повідомлень");
+define("TOP_LAN_3", "Рейтинг коментаторів");
+define("TOP_LAN_4", "Коментарів");
+define("TOP_LAN_5", "Рейтинг міні-чату");
+define("TOP_LAN_6", "Рейтинг");
+
+
+define("LAN_1", "Гілки");
+define("LAN_2", "Повідомлення");
+define("LAN_3", "Перегляди");
+define("LAN_4", "Відповіді");
+define("LAN_5", "Останнє ");
+define("LAN_6", "Теми (гілки)");
+define("LAN_7", "Найактивніша тема");
+define("LAN_8", "Рейтинг повідомлень");
+?>
