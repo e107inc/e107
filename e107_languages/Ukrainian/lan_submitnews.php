c27c9d138ebed8a6584215edd2f7558224219324
ukie <ukie@localhost>
2005-09-12 17:17:32 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=5023

---
+++ e107_languages/Ukrainian/lan_submitnews.php
@@ -0,0 +1,42 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_submitnews.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-12 17:17:32 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Розміщення новин");
+
+define("LAN_7", "Логін ім'я: ");
+define("LAN_62", "Тема: ");
+define("LAN_112", "адреса ел.пошти: ");
+define("LAN_133", "Дякуємо");
+define("LAN_134", "Вашу новину збережено і, можливо, її опублікують після перевірки адміністрацією сайту.");
+define("LAN_135", "Текст новини: ");
+define("LAN_136", "Розмістити новину");
+
+
+
+define("NWSLAN_6", "Категорії");
+define("NWSLAN_10", "Немає категорій новин");
+
+define("NWSLAN_11", "У Вас немає прав для перегляду.");
+define("NWSLAN_12", "Доступ заборонено.");
+
+define("SUBNEWSLAN_1", "Ви повинні додати заголовок.\\n");
+define("SUBNEWSLAN_2", "Ви повинні заповнити вміст новини.\\n");
+define("SUBNEWSLAN_3", "Завантажувані файли повинні бути у форматі jpg, gif або png");
+define("SUBNEWSLAN_4", "Завеликий файл");
+
+define("SUBNEWSLAN_5", "Графічний файл");
+define("SUBNEWSLAN_6", "(jpg, gif або png)");
+
+?>
