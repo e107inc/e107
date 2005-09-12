309050171761ca019df6de442dfe57eb12359993
ukie <ukie@localhost>
2005-09-12 17:32:23 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=5026

---
+++ e107_languages/Ukrainian/lan_upload_handler.php
@@ -0,0 +1,30 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_upload_handler.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-12 17:32:23 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("LANUPLOAD_1", "Тип файлу ");
+define("LANUPLOAD_2", "не дозволений і був знищений");
+define("LANUPLOAD_3", "Завантажений вдало");
+define("LANUPLOAD_4", "Вказана папка не існує або недозволена для читання.");
+define("LANUPLOAD_5", "Завантажуванийы файл завеликий відповідно до налаштувань php.ini.");
+define("LANUPLOAD_6", "Завантажуванийы файл завеликий відповідно до налаштувань форми html.");
+define("LANUPLOAD_7", "Файл завантажено лише частково.");
+define("LANUPLOAD_8", "Файл не завантажено.");
+define("LANUPLOAD_9", "Завантажуваний файл порожній (0 байтів)");
+
+define("LANUPLOAD_10", "Завантаження перервано [Duplicate filename] - Файл з таким іменем вже існує.");
+define("LANUPLOAD_11", "Файл не завантажено. Ім'я файлу: ");
+define("LANUPLOAD_12", "Помилка");
+
+?>
