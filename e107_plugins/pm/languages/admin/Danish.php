a20a39483a58919e24c0d3ae01cb7396d1a30ed3
e107dk <e107dk@localhost>
2005-09-14 18:53:10 +0000

    added translated file
    svn path=/trunk/e107_langpacks/; revision=5055

---
+++ e107_plugins/pm/languages/admin/Danish.php
@@ -0,0 +1,71 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|     
+|        $Source: /cvs_backup/e107_langpacks/e107_plugins/pm/languages/admin/Danish.php,v $
+|        $Revision: 1.1 $
+|        $Date: 2005-09-14 18:53:10 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
+
+define('ADLAN_PM', "Private Beskeder");
+define('ADLAN_PM_1', "To activate please go to your menus screen and select the private_msg into one of your menu areas. <br /><br />If you need to convert messages from a previous version, please go to the main config page for this plugin and select the 'convert' link.");
+define('ADLAN_PM_2', "Konfigurer Private Beskeder");
+define('ADLAN_PM_3', "PB indstillinger ikke fundet, standard værdier sat");
+define('ADLAN_PM_4', "Egenskaber Opdateret");
+define('ADLAN_PM_5', "Grænse for valgt brugergruppe eksisterer allerede");
+define('ADLAN_PM_6', "Grænse tilføjet med succes");
+define('ADLAN_PM_7', "Grænse ikke tilføjet - ukendt fejl");
+define('ADLAN_PM_8', "Grænse status opdateret");
+define('ADLAN_PM_9', " - Grænse fjernet med succes");
+define('ADLAN_PM_10', " - Grænse ikke fjernet - ukendt fejl");
+define('ADLAN_PM_11', " - Grænse opdateret med succes");
+define('ADLAN_PM_12', "PB Egenskaber");
+define('ADLAN_PM_13', "PB konvertering");
+define('ADLAN_PM_14', "PB Grænser");
+define('ADLAN_PM_15', "Tilføj PB Grænse");
+define('ADLAN_PM_16', "Plugin Titel");
+define('ADLAN_PM_17', "Vis ny PB animation");
+define('ADLAN_PM_18', "Vis bruger rullegardin");
+define('ADLAN_PM_19', "LÆST besked tidsudløb");
+define('ADLAN_PM_20', "ULÆST besked tidsudløb");
+define('ADLAN_PM_21', "popup besked ved ny PB");
+define('ADLAN_PM_22', "Popup forsinkelse tidsudløb");
+define('ADLAN_PM_23', "Begræns PB brug til");
+define('ADLAN_PM_24', "Antal PBer skal vises pr. side");
+define('ADLAN_PM_25', "Slå PB email besked til");
+define('ADLAN_PM_26', "Tillad bruger at bede om læst kvitering email besked");
+define('ADLAN_PM_27', "Tillad brug af vedhæftede filer");
+define('ADLAN_PM_28', "Maksimal vedhæftning størrelse");
+define('ADLAN_PM_29', "Tillad at sende til alle melemmer");
+define('ADLAN_PM_30', "Tillad at sende til flere modtagere");
+define('ADLAN_PM_31', "Slå send til brugergruppe TIL");
+define('ADLAN_PM_32', "Opdater Indstillinger");
+define('ADLAN_PM_33', "Inaktiv (ingen grænser)");
+define('ADLAN_PM_34', "PB tælling");
+define('ADLAN_PM_35', "PB books størrelse");
+define('ADLAN_PM_36', "Brugergruppe");
+define('ADLAN_PM_37', "Tæl grænser");
+define('ADLAN_PM_38', "Størrelse grænser (i KB)");
+define('ADLAN_PM_39', "Indbakke :");
+define('ADLAN_PM_40', "Udbakke :");
+define('ADLAN_PM_41', "Der er pt. ikke indstillet nogen grænser.");
+define('ADLAN_PM_42', "Opdater Grænser");
+define('ADLAN_PM_43', "Tilføj Ny Grænse");
+define('ADLAN_PM_44', "sekunder");
+define('ADLAN_PM_45', "Grænse PB Af: ");
+define('ADLAN_PM_46', "PB Konvertering");
+define('ADLAN_PM_47', "Du lader ikke til at have nogen gamle beskedder fra tidligere versioner, det er sikkert at afinstallere den gamle plugin");
+define('ADLAN_PM_48', "Du har {OLDCOUNT} beskeder fra den gamle version, bestem venligst hvad du ønsker at gøre med disse beskader<br /><br />Hvis konverter beskedder, vilen hver besked konverteret med succes blive fjernet fra det gamle system.");
+define('ADLAN_PM_49', "Konverter til ny PB");
+define('ADLAN_PM_50', "Se bort fra gamle beskeder");
+define('ADLAN_PM_51', "PB #{PMNUM} ikke konverteret");
+define('ADLAN_PM_52', "beskeder konverteret");
+define('ADLAN_PM_53', "Ingen poster fundet til konvertering.");
+
+define('ADLAN_PM_54', "Hoved indstillinger");
+define('ADLAN_PM_55', "Grænser");
+define('ADLAN_PM_56', "Konvertering");
+?>
