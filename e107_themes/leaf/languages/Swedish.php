af0792cc286075d1abba8fd948255e686c490528
mrpiercer <mrpiercer@localhost>
2005-09-17 13:04:48 +0000

    New files
    svn path=/trunk/e107_langpacks/; revision=5090

---
+++ e107_themes/leaf/languages/Swedish.php
@@ -0,0 +1,21 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|
+|     Swedish language file.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_themes/leaf/languages/Swedish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-17 13:04:48 $
+|     $Author: mrpiercer $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "Kommentar(er) ");
+define("LAN_THEME_2", "Kommentarer är inaktiverade för detta objekt");
+define("LAN_THEME_3", "kommentar(er) ");
+define("LAN_THEME_4", "Läs resten ...");
+define("LAN_THEME_5", "Bakåtlänkar: ");
+define("LAN_THEME_6", "Kommentar av");
+
+?>
