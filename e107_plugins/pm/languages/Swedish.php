9fad8b275eb0100eea500cc66872d4637f33405c
mrpiercer <mrpiercer@localhost>
2005-09-17 12:52:54 +0000

    New files
    svn path=/trunk/e107_langpacks/; revision=5088

---
+++ e107_plugins/pm/languages/Swedish.php
@@ -0,0 +1,87 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pm/languages/Swedish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-17 12:52:54 $
+|     $Author: mrpiercer $
++----------------------------------------------------------------------------+
+*/
+
+define('LAN_PM', "Privat meddelande");
+define('LAN_PM_1', "Skicka privat meddelande");
+define('LAN_PM_2', "Till");
+define('LAN_PM_3', "Förhandsgranska");
+define('LAN_PM_4', "Användarklass");
+define('LAN_PM_5', "Ämne");
+define('LAN_PM_6', "Meddelande");
+define('LAN_PM_7', "Smajlys");
+define('LAN_PM_8', "Bilagor");
+define('LAN_PM_9', "Läskvitto");
+define('LAN_PM_10', "Skicka mig e-post när detta meddelande lästs");
+define('LAN_PM_11', "Lägg till ny uppladdning");
+define('LAN_PM_12', "Du har inte tillåtelse att använda PM systemet");
+define('LAN_PM_13', "Din utkorg är för närvarande fylld till {PERCENT}%, du kan inte skicka nya PM förrän du raderar några");
+define('LAN_PM_14', "FEL: Möjlig dublettpostning, PM skickades inte");
+define('LAN_PM_15', "Du får inte lov att skicka till användarklass");
+define('LAN_PM_16', "Måste vara medlem i användarklass");
+define('LAN_PM_17', "Användare hittades inte");
+define('LAN_PM_18', "Du får inte lov att skicka PM till: ");
+define('LAN_PM_19', "Din utkorg är full, så du får inte skicka fler PM");
+define('LAN_PM_21', "Detta PM kommer att överskrida din maximala storlek på utkorgen, PM postas inte");
+define('LAN_PM_22', "Filuppladdning misslyckades");
+define('LAN_PM_23', "Du har inte lov att skicka med bilagor");
+define('LAN_PM_24', "Raderar PM");
+define('LAN_PM_25', "Inkorg");
+define('LAN_PM_26', "Utkorg");
+define('LAN_PM_27', "Oläst");
+define('LAN_PM_28', "E/A");
+define('LAN_PM_29', "Meddelande sänt");
+define('LAN_PM_30', "Meddelande läst");
+define('LAN_PM_31', "Från");
+define('LAN_PM_32', "Mottaget");
+define('LAN_PM_33', "Sänt");
+define('LAN_PM_34', "Inga meddelanden");
+define('LAN_PM_35', "Skicka nytt meddelande");
+define('LAN_PM_36', "totalt");
+define('LAN_PM_37', "olästa");
+define('LAN_PM_38', "PM sänt till användarklass");
+define('LAN_PM_39', "Kunde inte skicka PM till");
+define('LAN_PM_40', "PM sänt till användare");
+define('LAN_PM_41', "Misslyckades att posta PM till din utkorg");
+
+define('LAN_PM_42', "PM raderat från inkorg");
+define('LAN_PM_43', "PM raderat från utkorg");
+define('LAN_PM_44', "Blockering borttagen: {UNAME} kan nu skicka PM till dig");
+define('LAN_PM_45', "FEL: Blockering inte borttagen, okänt fel");
+define('LAN_PM_46', "Finns blockering ingen för {UNAME}");
+define('LAN_PM_47', "Blockering tillagd: {UNAME} kan inte längre skicka PM till dig");
+define('LAN_PM_48', "FEL: Blockering inte inlagd, okänt fel");
+define('LAN_PM_49', "FEL: Blockering finns redan för {UNAME}");
+define('LAN_PM_50', "Blockera användare");
+define('LAN_PM_51', "Avblockera användare");
+define('LAN_PM_52', "Radera");
+define('LAN_PM_53', "Radera markerade");
+define('LAN_PM_54', "Citera original");
+define('LAN_PM_55', "Skicka svar");
+define('LAN_PM_56', "Du har inte lov att svara på detta meddelande");
+define('LAN_PM_57', "Meddelandet hittades inte");
+define('LAN_PM_58', "Ang: ");
+define('LAN_PM_59', "Gå till sida: ");
+define('LAN_PM_60', "Du har inte lov att se detta meddelande");
+define('LAN_PM_61', "Inget ämne");
+define('LAN_PM_62', "Filen: [{FILENAME}] överskrider storleksbegränsningen - bifogas inte");
+
+define("LAN_PM_100", "Nytt PM från ");
+define("LAN_PM_101", "Du har fått ett nytt privat meddelande från ");
+define("LAN_PM_102", "Meddelande sänt från: ");
+define("LAN_PM_103", "Meddelandets ämne: ");
+define("LAN_PM_104", "Antal bilagor: ");
+define("LAN_PM_105", "Du kan läsa ditt PM på: ");
+define("LAN_PM_106", "PM läst av ");
+define("LAN_PM_107", "Det privata meddelande du skickade till {UNAME} lästes ");
+define("LAN_PM_108", "Meddelande skickades: ");
+
+?>
