9a72c67e9ded3bf25c922a6cca955b344b570c9f
plastish <plastish@localhost>
2005-09-17 23:31:20 +0000

    Updated some files
    svn path=/trunk/e107_langpacks/; revision=5093

---
+++ e107_languages/Hebrew/admin/lan_search.php
@@ -0,0 +1,40 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
++----------------------------------------------------------------------------+
+*/
+define("SEALAN_1", "הגדרות חיפוש");
+define("SEALAN_2", "מספר התווים שיוצגו בתקציר תוצאת חיפוש:");
+define("SEALAN_3", "שיטת סידור החיפוש:");
+// define("SEALAN_4", "Searchable areas:");
+// define("SEALAN_5", "News");
+define("SEALAN_6", "הערות");
+define("SEALAN_7", "חברים רשומים");
+// define("SEALAN_8", "Downloads");
+// define("SEALAN_9", "Number of results to display per page:");
+define("SEALAN_10", "הצגת ערך רלוונטיות:");
+define("SEALAN_11", "לאפשר למשתמשים לבחור את אזורי החיפוש:");
+define("SEALAN_12", "להגביל זמן מינימלי בין חיפושים (מקסימום 5 דקות):");
+define("SEALAN_13", "להגביל לחיפוש אחד בכל");
+define("SEALAN_14", "שניות");
+define("SEALAN_15", "דף החיפוש נגיש לסיווג משתמש");
+define("SEALAN_16", "מופעל");
+define("SEALAN_17", "כבוי");
+define("SEALAN_18", "אזורי הערות שניתן לחפש בהם (כשחיפוש בהערות מופעל)");
+define("SEALAN_19", "לאפשר למשתמשים לחפש ביותר מאזור אחד בפעם אחת:");
+define("SEALAN_20", "הגדרות כלליות");
+define("SEALAN_21", "אזורים שניתן לבחור בהם");
+define("SEALAN_22", "ברירת מחדל");
+define("SEALAN_23", "חליפי:");
+define("SEALAN_24", "סוג");
+define("SEALAN_25", "סיווג משתמש");
+define("SEALAN_26", "טקסט שלפני הכותרת");
+define("SEALAN_27", "תווים");
+define("SEALAN_28", "תוצאות");
+// define("SEALAN_29", "Order");
+define("SEALAN_30", "להדגיש מילות מפתח בדף היעד:");
+define("SEALAN_31", "PHP מוגבל ל-");
+define("SEALAN_32", "תוצאות (השאר ריק כדי לא לקבוע הגבלה)");
+
+?>
---
+++ e107_languages/Hebrew/admin/lan_users_extended.php
@@ -0,0 +1,97 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
++----------------------------------------------------------------------------+
+*/
+define("EXTLAN_1", "שם");
+define("EXTLAN_2", "תצוגה מקדימה");
+define("EXTLAN_3", "ערכים");
+define("EXTLAN_4", "נדרש");
+define("EXTLAN_5", "מתאים");
+define("EXTLAN_6", "גישת קריאה");
+define("EXTLAN_7", "גישת כתיבה");
+define("EXTLAN_8", "פעולה");
+define("EXTLAN_9", "שדות משתמש מורחבים");
+
+define("EXTLAN_10", "שם השדה");
+define("EXTLAN_11", "זה השם של השדה כפי שהוא נשמר בטבלה, הוא חייב להיות שונה מכל השדות האחרים");
+define("EXTLAN_12", "טקסט השדה");
+define("EXTLAN_13", "זה השם שיוצג עבור השדה בדפים");
+define("EXTLAN_14", "סוג השדה");
+define("EXTLAN_15", "טקסט נכלל בשדה");
+define("EXTLAN_16", "ערך ברירת מחדל");
+define("EXTLAN_17", "הכנס כל ערך אפשרי בשורה חדשה <br /> עבור טבלה בבסיס הנתונים, קרא בעזרה.");
+define("EXTLAN_18", "נדרש");
+define("EXTLAN_19", "משתמשים יחויבו למלא ערך בשדה זה כשהם מעדכנים את הפרטים שלהם.");
+define("EXTLAN_20", "קובע על אילו משתמשים יחול השדה הזה.");
+define("EXTLAN_21", "זה קובע מי יראו את השדה הזה בפרטי המשתמש שלהם.");
+define("EXTLAN_22", "זה קובע מי יכול לצפות בערך בדף המשתמש.");
+define("EXTLAN_23", "הוסף שדה מורחב");
+define("EXTLAN_24", "עדכן שדה מורחב");
+define("EXTLAN_25", "הזז למטה");
+define("EXTLAN_26", "הזז למעלה");
+define("EXTLAN_27", "אשר מחיקה");
+define("EXTLAN_28", "לא מוגדרים שדות מורחבים");
+define("EXTLAN_29", "שדות משתמש מורחבים נשמרו.");
+define("EXTLAN_30", "שדה מורחב נמחק");
+// define("EXTLAN_31", "Extended Field Menu");
+// define("EXTLAN_32", "Extended front page");
+define("EXTLAN_33", "בטל עריכה");
+define("EXTLAN_34", "שדות מורחבים");
+define("EXTLAN_35", "קטגוריות");
+define("EXTLAN_36", "לא הוגדרה קטגוריה");
+define("EXTLAN_37", "לא מוגדרות קטגוריות");
+define("EXTLAN_38", "שם קטגוריה");
+define("EXTLAN_39", "הוספת קטגוריה");
+define("EXTLAN_40", "הקטגוריה נוצרה");
+define("EXTLAN_41", "הקטגוריה נמחקה");
+define("EXTLAN_42", "עדכון קטגוריה");
+define("EXTLAN_43", "הקטגוריה עודכנה");
+define("EXTLAN_44", "קטגוריה");
+define("EXTLAN_45", "הוספת שדה חדש");
+define("EXTLAN_46", "עזרה");
+define("EXTLAN_47", "הוספת פרמטר חדש");
+define("EXTLAN_48", "הוספת ערך חדש");
+define("EXTLAN_49", "לאפשר למשתמש להסתיר");
+define("EXTLAN_50", "הפעלת אפשרות זו תאפשר למשתמש להסתיר את הערך הזה ממשתמשים שאינם מנהלים");
+define("EXTLAN_51", "ניתן לכתוב כאן כל פרמטר w3c תקין<br />למשל: <i><b>class='tbox' size='40' maxlength='80'</i></b>");
+define("EXTLAN_52", "קוד ביטוי רגולרי לווידוא");
+define("EXTLAN_53", "הכנס את קוד הביטוי הרגולרי (RegEx) שאליו הערך יצטרך להתאים כדי שהרשומה תתקבל <br />**חובה לכניס תווים מגבילים של ביטויים רגולריים**");
+define("EXTLAN_54", "טקסט של כישלון התאמת ביטוי רגולרי");
+define("EXTLAN_55", "הכנס את הודעה השגיאה שתוצג אם וידוא הביטוי הרגולרי נכשל.");
+define("EXTLAN_56", "שדות מוגדרים מראש");
+define("EXTLAN_57", "פעיל");
+define("EXTLAN_58", "אינו פעיל");
+define("EXTLAN_59", "הפעל");
+define("EXTLAN_60", "בטל");
+define("EXTLAN_61", "None");
+
+define("EXTLAN_62", "בחר טבלה");
+define("EXTLAN_63", "בחר מזהה שדה");
+define("EXTLAN_64", "בחר ערך תצוגה");
+
+
+
+//textbox
+define("EXTLAN_HELP_1", "<b><i>פרמטרים:</i></b><br />size - גודל השדה<br />maxlength - האורך המקסימלי של השדה<br /><br />class - class ה-CSS של השדה<br />style - מחרוזת סגנון CSS<br /><br />regex - קוד ביטוי רגולרי לווידוא<br />regexfail - טקסט הכשלון בווידוא");
+//radio buttons
+define("EXTLAN_HELP_2", "זה יהיה טקסט העזרה של כפתורי הרדיו");
+//dropdown
+define("EXTLAN_HELP_3", "זה יהיה טקסט העזרה של התיבה הנגללת");
+//db field
+define("EXTLAN_HELP_4", "<b><i>ערכים:</i></b><br />יש שלושה ערכים שחובה לציין תמיד:<br /><ol><li>הטבלה בבסיס הנתונים</li><li>השדה שמכיל את המזהה (id)</li><li>השדה שמכיל את הערך</li></ol><br />");
+//textarea
+define("EXTLAN_HELP_5", "זה יהיה טקסט העזרה של אזור הטקסט");
+//integer
+define("EXTLAN_HELP_6", "זה יהיה טקסט העזרה של המספר השלם");
+//date
+define("EXTLAN_HELP_7", "זה יהי טקסט העזרה של התאריך");
+
+define("EXTLAN_PRE1", "מיקום");
+define("EXTLAN_PRE2", "AIM");
+define("EXTLAN_PRE3", "ICQ");
+define("EXTLAN_PRE4", "Yahoo!");
+define("EXTLAN_PRE5", "דף בית");
+
+?>
