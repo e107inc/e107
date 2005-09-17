304458594b562344923c9be3923bebdb7ea3861c
mrpiercer <mrpiercer@localhost>
2005-09-17 11:50:20 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5086

---
+++ e107_handlers/phpmailer/language/phpmailer.lang-sv.php
@@ -0,0 +1,28 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_handlers/phpmailer/language/phpmailer.lang-sv.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-17 11:50:20 $
+|     $Author: mrpiercer $
++----------------------------------------------------------------------------+
+*/
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'Du måste ange minst en ' . 'mottagar e-postadress.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailer stöds inte.';
+$PHPMAILER_LANG["execute"] = 'Kan inte exekvera: ';
+$PHPMAILER_LANG["instantiate"] = 'Kunde inte skapa instans för mail funktion.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP fel: Kunde inte autentisera.';
+$PHPMAILER_LANG["from_failed"] = 'Följande Från-adress misslyckades: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP fel: Följande ' . 'mottagare misslyckades: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP fel: Data accepterades inte.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP fel: Kunde inte ansluta till SMTP värd.';
+$PHPMAILER_LANG["file_access"] = 'Kunde inte komma åt filen: ';
+$PHPMAILER_LANG["file_open"] = 'Fil fel: Kunde inte öppna filen: ';
+$PHPMAILER_LANG["encoding"] = 'Okänd kodning: ';
+
+?>
