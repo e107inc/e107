6374a479f01b14f6740555319979599d7b600b33
e107dk <e107dk@localhost>
2005-09-17 10:02:47 +0000

    added translated file (converted)
    svn path=/trunk/e107_langpacks/; revision=5083

---
+++ legacy_langpacks/e107_plugins/newsfeed/languages/Danish_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_plugins/newsfeed/languages/Danish_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-17 10:02:47 $
+|     $Author: e107dk $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "Nyheds Feeds");
+define("NWSF_FP_2", "hovedside");
+
+?>
