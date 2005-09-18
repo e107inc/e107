9156de24917db2ea54ef493fe347f63c0aa8853f
mrpiercer <mrpiercer@localhost>
2005-09-18 09:26:12 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5105

--- legacy_langpacks/e107_languages/Swedish/Swedish.php
+++ legacy_langpacks/e107_languages/Swedish/Swedish.php
@@ -4,15 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Swedish/Swedish.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-09-18 09:24:00 $
+|     $Revision: 1.4 $
+|     $Date: 2005-09-18 09:26:12 $
 |     $Author: mrpiercer $
 +----------------------------------------------------------------------------+
 */
 setlocale(LC_ALL, 'sv_SE.ISO_8859-1', 'swedish', 'sve_swe');
 define("CORE_LC", 'sv');
 define("CORE_LC2", 'se');
-define("CHARSET", "ISO8859-1");
+define("CHARSET", "iso-8859-1");
 define("CORE_LAN1","Fel: Tema saknas.\\n\\nByt det anv&auml;nda temat i dina preferenser (admin area) eller ladda upp filerna f&ouml;r det aktuella temat till din server.");
 
 //v.616
