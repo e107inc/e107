0da42924471329dd0c15d78c720d94b71916d88a
ukie <ukie@localhost>
2005-09-18 02:01:37 +0000

    thank you OGall. It's 70% your translation. Thanx!
    svn path=/trunk/e107_langpacks/; revision=5096

---
+++ e107_languages/Ukrainian/lan_userposts.php
@@ -0,0 +1,36 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_userposts.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-18 02:01:37 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+## Ukrainian Language Pack for e107 Version 0.617
+##      Copyright © 2005 - Ogall © Ukrainian e107 ©
+##	     	  http://www.ogall.com
+
+define("PAGE_NAME", "Повідомлення користувачів");
+
+define("UP_LAN_0", "Всі повідомлення для  ");
+define("UP_LAN_1", "Всі коментарі для  ");
+define("UP_LAN_2", "Гілка");
+define("UP_LAN_3", "Перегляд");
+define("UP_LAN_4", "Відповіді");
+define("UP_LAN_5", "Останнє повідомлення");
+define("UP_LAN_6", "Гілки");
+define("UP_LAN_7", "Немає коментарів");
+define("UP_LAN_8", "немає повідомлень");
+define("UP_LAN_9", " на ");
+define("UP_LAN_10", "Відповідь на:");
+define("UP_LAN_11", "Написано на: ");
+define("UP_LAN_12", "Пошук");
+
+define("UP_LAN_13", "Коментарі");
+define("UP_LAN_14", "З форуму");
+define("UP_LAN_15", "Відповідь");
+define("UP_LAN_16", "Адреса IP");
+?>
