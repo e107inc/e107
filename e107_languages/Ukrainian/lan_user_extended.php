af0c154b943e107685da1acff5b752371975d19c
ukie <ukie@localhost>
2005-09-18 01:54:58 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5094

---
+++ e107_languages/Ukrainian/lan_user_extended.php
@@ -0,0 +1,42 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Ukrainian/lan_user_extended.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-09-18 01:54:58 $
+|     $Author: ukie $
++----------------------------------------------------------------------------+
+*/
+define("UE_LAN_1", "Текстова Площадка");
+define("UE_LAN_2", "Радіо Кнопки");
+define("UE_LAN_3", "Розкладне Меню");
+define("UE_LAN_4", "Поля таблиць БД");
+define("UE_LAN_5", "Текст");
+define("UE_LAN_6", "Ціле");
+define("UE_LAN_7", "Дата");
+define("UE_LAN_8", "Мова");
+
+define("UE_LAN_9", "Ім'я");
+define("UE_LAN_10", "Тип");
+define("UE_LAN_11", "Використання");
+
+define("UE_LAN_HIDE", "Приховати від користувачів");
+
+define("UE_LAN_LOCATION", "Розташування");
+define("UE_LAN_LOCATION_DESC", "Звідки сам");
+define("UE_LAN_AIM", "Адресв AIM");
+define("UE_LAN_AIM_DESC", "Адреса AIM");
+define("UE_LAN_ICQ", "ICQ");
+define("UE_LAN_ICQ_DESC", "ICQ");
+define("UE_LAN_YAHOO", "Адреса Yahoo!");
+define("UE_LAN_YAHOO_DESC", "Адреса Yahoo!");
+define("UE_LAN_MSN", "MSN");
+define("UE_LAN_MSN_DESC", "MSN");
+define("UE_LAN_HOMEPAGE", "Домашня сторінка");
+define("UE_LAN_HOMEPAGE_DESC", "Домашня сторінка (url)");
+define("UE_LAN_BIRTHDAY", "День народження");
+define("UE_LAN_BIRTHDAY_DESC", "День народження");
+
+?>
