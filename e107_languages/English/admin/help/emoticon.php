5fce6e8c54f83b8e96edbb6c83635e8f40a0016f
e107steved <steved@e107.org>
2005-09-18 09:14:42 +0000

    Removed link to emote_create, depracated file no longer in core.
    svn path=/trunk/e107_0.7/; revision=5102

--- e107_languages/English/admin/help/emoticon.php
+++ e107_languages/English/admin/help/emoticon.php
@@ -1,6 +1,6 @@
 <?php
 
-$text = "You can create emote packs that will install seamlessly into e107. Go to <a href='".e_FILE."emote_create/emotecreate.php'>emote create</a> and follow the instructions.";
+$text = "";
 
 $ns -> tablerender("Emoticon Help", $text);
 ?>
