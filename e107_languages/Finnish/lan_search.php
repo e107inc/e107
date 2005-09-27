7e5c08e089129a87bdcd4097d130f1cdc70eafba
timmerbacka <timmerbacka@localhost>
2005-09-27 22:37:00 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5156

--- e107_languages/Finnish/lan_search.php
+++ e107_languages/Finnish/lan_search.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Finnish/lan_search.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-09-05 18:25:51 $
+|     $Revision: 1.2 $
+|     $Date: 2005-09-27 22:37:00 $
 |     $Author: timmerbacka $
 +----------------------------------------------------------------------------+
 */
@@ -16,26 +16,26 @@ define("LAN_98", "Uutiset");
 define("LAN_99", "Kommentit");
 define("LAN_140", "Käyttäjät");
 define("LAN_180", "Etsi");
-define("LAN_192", "All categories");
+define("LAN_192", "Kaikki aihepiirit");
 define("LAN_193", "Tapahtuma kalenteri");
-define("LAN_194", "All Categories");
+define("LAN_194", "Kaikki Aihepiirit");
 define("LAN_195", "Etsintä");
 define("LAN_196", "löytyneitä");
 
 define("LAN_197", "Tiedostojen lataus");
-define("LAN_198", "Ei löytyneitä found");
-define("LAN_199", "Search For:");
+define("LAN_198", "Osumia ei löytynyt");
+define("LAN_199", "Etsi merkkijonoa:");
 define("LAN_200", "Aiheista:");
 
 define("LAN_201", "Ole ystävällinen ja tarkenna etsittävää merkkijonoa");
 define("LAN_416", "Sinun tulee olla kirjautunut käyttäjäksi päästäksesi tälle sivulle");
 define("LAN_417", "Etsittävä merkkijono pitää olla vähintää 3-merkkiä.");
 
-define("LAN_418", "Other Pages");
+define("LAN_418", "Muilla sivuilla");
 
 define("LAN_SEARCH_1", "Valitse kaikki");
 define("LAN_SEARCH_2", "Poista valinta kaikista");
-define("LAN_SEARCH_3", "Posted on ");
+define("LAN_SEARCH_3", "Lähetetty ");
 define("LAN_SEARCH_4", "Match found in news title");
 define("LAN_SEARCH_5", "Match found in news text");
 define("LAN_SEARCH_6", "Match found in extended news text");
@@ -43,28 +43,28 @@ define("LAN_SEARCH_7", "Lähettäjä ");
 define("LAN_SEARCH_8", " on ");
 define("LAN_SEARCH_9", "Nimetön");
 define("LAN_SEARCH_10", "Siirry sivulle:");
-define("LAN_SEARCH_11", "Results");
+define("LAN_SEARCH_11", "Löytyneitä");
 define("LAN_SEARCH_12", " of ");
-define("LAN_SEARCH_13", " in ");
+define("LAN_SEARCH_13", " kohteessa ");
 define("LAN_SEARCH_14", "Aihepiiri:");
-define("LAN_SEARCH_15", "Author:");
+define("LAN_SEARCH_15", "Kirjoittaja:");
 define("LAN_SEARCH_17", "Sorry, search is restricted to one search every ");
 define("LAN_SEARCH_18", " seconds.");
-define("LAN_SEARCH_19", "Search In:");
+define("LAN_SEARCH_19", "Etsi kohteesta:");
 define("LAN_SEARCH_20", "Authorisation Required");
 define("LAN_SEARCH_21", "You are not authorised to view this page.");
 
 
 
-define("LAN_SEARCH_22", "All Areas");
+define("LAN_SEARCH_22", "Kaikki alueet");
 define("LAN_SEARCH_23", "Enhanced Query Form");
-define("LAN_SEARCH_24", "Must contain word(s)");
-define("LAN_SEARCH_25", "Must not contain word(s)");
-define("LAN_SEARCH_26", "Exact Phrase");
+define("LAN_SEARCH_24", "Tulee sisältää sanat");
+define("LAN_SEARCH_25", "Ei saa sisältää sanoja");
+define("LAN_SEARCH_26", "Sisältää lauseen");
 define("LAN_SEARCH_27", "Word(s) beginning with");
 define("LAN_SEARCH_28", "All Has No Advanced Search");
-define("LAN_SEARCH_29", "Basic");
-define("LAN_SEARCH_30", "Advanced");
+define("LAN_SEARCH_29", "Yksinkertainen");
+define("LAN_SEARCH_30", "Monipuolinen");
 define("LAN_SEARCH_31", "Has No Advanced Search");
 define("LAN_SEARCH_32", "The following words were excluded from the search");
 define("LAN_SEARCH_33", "The following word was excluded from the search");
@@ -86,14 +86,14 @@ define("LAN_SEARCH_48", "Two years");
 define("LAN_SEARCH_49", "Three Years");
 
 define("LAN_SEARCH_50", "Date posted");
-define("LAN_SEARCH_51", "All categories");
+define("LAN_SEARCH_51", "Kaikki aihepiirit");
 define("LAN_SEARCH_52", "Match in");
 define("LAN_SEARCH_53", "Whole item");
 define("LAN_SEARCH_54", "Title only");
 define("LAN_SEARCH_55", "Search in news category");
 define("LAN_SEARCH_56", "All News Categories");
 define("LAN_SEARCH_57", "Kommentteja to");
-define("LAN_SEARCH_58", "All Areas");
+define("LAN_SEARCH_58", "Kaikki alueet");
 define("LAN_SEARCH_59", "All Comments");
 define("LAN_SEARCH_60", "Kommentteja to");
 define("LAN_SEARCH_61", "By author");
@@ -101,7 +101,7 @@ define("LAN_SEARCH_62", "Date joined");
 define("LAN_SEARCH_63", "Search in category");
 define("LAN_SEARCH_64", "All Download Categories");
 define("LAN_SEARCH_65", "Tiedostojen lataus");
-define("LAN_SEARCH_66", "Date added");
+define("LAN_SEARCH_66", "Lisätty");
 define("LAN_SEARCH_67", "All downloads details");
 define("LAN_SEARCH_68", "Päivä");
 define("LAN_SEARCH_69", "Relevance");
