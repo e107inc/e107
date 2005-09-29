050f9384f6aa78c1f2fe01229edffb2411634bc5
mrpiercer <mrpiercer@localhost>
2005-09-29 13:53:16 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5166

--- e107_handlers/tiny_mce/plugins/flash/langs/sv.js
+++ e107_handlers/tiny_mce/plugins/flash/langs/sv.js
@@ -3,18 +3,16 @@
 |     Swedish (SE) language variables
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_handlers/tiny_mce/plugins/flash/langs/sv.js,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-09-17 11:39:40 $
+|     $Revision: 1.3 $
+|     $Date: 2005-09-29 13:53:16 $
 |     $Author: mrpiercer $
 +----------------------------------------------------------------------------+
 */
 
-tinyMCE.addToLang('flash',{
-title : 'Infoga / redigera Flash film',
-desc : 'Infoga / redigera Flash film',
-file : 'Flash-fil (.swf)',
-size : 'Storlek',
-list : 'Flash filer',
-props : 'Flash attribut',
-general : 'Generelllt'
+tinyMCE.addToLang('',{
+insert_flash : 'Skapa/uppdatera flash-film',
+insert_flash_file : 'Flash-film (.swf)',
+insert_flash_size : 'Storlek',
+insert_flash_list : 'Flash-filer',
+flash_props : 'Flash egenskaper'
 });
