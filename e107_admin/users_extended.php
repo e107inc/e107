89eb0fa73f1501889f2e036601be24e2b0cbd3b7
mcfly_e107 <mcfly_e107@localhost>
2005-09-30 02:23:27 +0000

    Added ability to set an extended field as applicable to 'nobody'.  The makes the field only readable/writable via code (plugin, etc)
    svn path=/trunk/e107_0.7/; revision=5167

--- e107_admin/users_extended.php
+++ e107_admin/users_extended.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/users_extended.php,v $
-|     $Revision: 1.23 $
-|     $Date: 2005-09-19 17:45:40 $
-|     $Author: sweetas $
+|     $Revision: 1.24 $
+|     $Date: 2005-09-30 02:23:27 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 require_once("../class2.php");
@@ -532,7 +532,7 @@ class users_ext
 			<tr>
 			<td style='width:30%;vertical-align:top' class='forumheader3'>".EXTLAN_5."</td>
 			<td style='width:70%' class='forumheader3' colspan='3'>
-			".r_userclass("user_applicable", $current['user_extended_struct_applicable'], 'off', 'member, admin, classes')."<br /><span class='smalltext'>".EXTLAN_20."</span>
+			".r_userclass("user_applicable", $current['user_extended_struct_applicable'], 'off', 'member, admin, classes, nobody')."<br /><span class='smalltext'>".EXTLAN_20."</span>
 			</td>
 			</tr>
 
