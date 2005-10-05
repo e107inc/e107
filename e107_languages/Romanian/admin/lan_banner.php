a1fc56bab9f44d5e4fc00e1be3cb5bfc08301e63
i2gh0st <i2gh0st@localhost>
2005-10-05 15:22:25 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5190

--- e107_languages/Romanian/admin/lan_banner.php
+++ e107_languages/Romanian/admin/lan_banner.php
@@ -1,54 +1,54 @@
-<?php
+﻿<?php
 
-define("BNRLAN_1", "Banner deleted.");
-define("BNRLAN_2", "Please confirm you wish to delete this banner - once deleted it cannot be retrieved");
-define("BNRLAN_3", "Cancel");
-define("BNRLAN_4", "Confirm Delete");
-define("BNRLAN_5", "Confirm Delete Banner");
-define("BNRLAN_6", "Delete cancelled."); 
-define("BNRLAN_7", "Existing Banners");
-define("BNRLAN_8", "Banner ID");
+define("BNRLAN_1", "Sterge Bandiera.");
+define("BNRLAN_2", "Te rog confirma stergerea acestei bandiere - Odata stearsa nu mai poate fi recuperata");
+define("BNRLAN_3", "Anuleaza");
+define("BNRLAN_4", "Confirma Stergerea");
+define("BNRLAN_5", "Confirma Stergerea Bandierei");
+define("BNRLAN_6", "Stergere anulata."); 
+define("BNRLAN_7", "Bandiere existente");
+define("BNRLAN_8", "ID Bandiera");
 define("BNRLAN_9", "Client");
 
-define("BNRLAN_10", "Clickthroughs");
-define("BNRLAN_11", "Click %");
-define("BNRLAN_12", "Impressions");
-define("BNRLAN_13", "Impressions Left");
-define("BNRLAN_14", "Options");
-define("BNRLAN_15", "No banners yet.");
-define("BNRLAN_16", "Unlimited");
-define("BNRLAN_17", "None");
-define("BNRLAN_18", "Delete");
-define("BNRLAN_19", "Yes");
-define("BNRLAN_20", "No");
-define("BNRLAN_21", "Ends");
-define("BNRLAN_22", "Update Banner");
-define("BNRLAN_23", "Add New Banner");
-define("BNRLAN_24", "Campaign");
+define("BNRLAN_10", "???Clickthroughs");
+define("BNRLAN_11", "???Click %");
+define("BNRLAN_12", "Impresii");
+define("BNRLAN_13", "Impresii ramase");
+define("BNRLAN_14", "Optiuni");
+define("BNRLAN_15", "Nici bandiera inca.");
+define("BNRLAN_16", "Nelimitate");
+define("BNRLAN_17", "Nimic");
+define("BNRLAN_18", "Sterge");
+define("BNRLAN_19", "Da");
+define("BNRLAN_20", "Nu");
+define("BNRLAN_21", "???Ends");
+define("BNRLAN_22", "Updateaza bandiera");
+define("BNRLAN_23", "Adauga Bandiera noua");
+define("BNRLAN_24", "Campanie");
 
-define("BNRLAN_25", "choose existing campaign");
-define("BNRLAN_26", "enter new campaign");
+define("BNRLAN_25", "alege campanie existenta");
+define("BNRLAN_26", "Adauga campanie noua");
 define("BNRLAN_27", "Client");
-define("BNRLAN_28", "choose existing client");
-define("BNRLAN_29", "enter new client");
+define("BNRLAN_28", "alege client existent");
+define("BNRLAN_29", "adauga client nou");
 define("BNRLAN_30", "Client Login");
-define("BNRLAN_31", "Client Password");
-define("BNRLAN_32", "Banner Image");
+define("BNRLAN_31", "Parola Client");
+define("BNRLAN_32", "Imagine Bandiera");
 define("BNRLAN_33", "Click URL");
-define("BNRLAN_34", "Impressions Purchased");
-define("BNRLAN_35", "unlimited");
-define("BNRLAN_36", "Start Date");
+define("BNRLAN_34", "Impresii Cumparate");
+define("BNRLAN_35", "nelimitate");
+define("BNRLAN_36", "Data inceput");
 
-define("BNRLAN_37", "End Date");
-define("BNRLAN_38", "blank = no limit");
-define("BNRLAN_39", "Active");
-define("BNRLAN_40", "Update Banner");
-define("BNRLAN_41", "Create New Banner");
-define("BNRLAN_42", "Banner Rotation System");
+define("BNRLAN_37", "Data sfarsit");
+define("BNRLAN_38", "gol = fara limita");
+define("BNRLAN_39", "Activ");
+define("BNRLAN_40", "Updateaza Bandiera");
+define("BNRLAN_41", "Creaza bandiera noua");
+define("BNRLAN_42", "Sistemul rotire bandiera");
 
-define("BNRLAN_43", "Choose banner image");
-define("BNRLAN_44", "Edit");
-define("BNRLAN_45", "Starts");
-define("BNRLAN_46", "Code");
+define("BNRLAN_43", "Alege imaginea bandierei");
+define("BNRLAN_44", "Modifica");
+define("BNRLAN_45", "Starturi";
+define("BNRLAN_46", "Cod");
 
 ?>
