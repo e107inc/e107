89283da81669773a7e0060fea65da0afbd511ad6
e107dk <e107dk@localhost>
2005-10-12 17:16:16 +0000

    added recent changes + minor ajustments
    svn path=/trunk/e107_langpacks/; revision=5219

--- e107_plugins/tree_menu/languages/Danish.php
+++ e107_plugins/tree_menu/languages/Danish.php
@@ -1,12 +1,30 @@
 <?php
 /*
-+---------------------------------------------------------------+|        e107 website system - Language File|     
-|        $Source: /cvs_backup/e107_langpacks/e107_plugins/tree_menu/languages/Danish.php,v $|        $Revision: 1.1 $|        $Date: 2005-07-06 22:12:04 $|        $Author: e107dk $+---------------------------------------------------------------+*/
-	
++---------------------------------------------------------------+
+|        e107 website system - Language File
+|     
+|        $Source: /cvs_backup/e107_langpacks/e107_plugins/tree_menu/languages/Danish.php,v $
+|        $Revision: 1.2 $
+|        $Date: 2005-10-12 17:16:15 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
+
+
+
+
+
+
+
+
 define("TREE_L1", "Konfigurer træmenu");
 define("TREE_L2", "Opdater træmenu indstillinger");
-define("TREE_L3", "Konfiguration gemt.");
+define("TREE_L3", "Træ menu konfiguration gemt.");
 define("TREE_L4", "Til");
 define("TREE_L5", "Fra");
+define("TREE_L6", "CSS klasse der skal bruges til links der ikke kan åbnes");
+define("TREE_L7", "CSS klasse der skal bruged til links der kan åbnes");
+define("TREE_L8", "CSS klasse der skal bruges til åbnede links");
+define("TREE_L9", "Brug afstandsholder klasse mellem hoved links");
 
 ?>
--- legacy_langpacks/e107_plugins/tree_menu/languages/Danish.php
+++ legacy_langpacks/e107_plugins/tree_menu/languages/Danish.php
@@ -4,16 +4,27 @@
 |        e107 website system - Language File
 |     
 |        $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_plugins/tree_menu/languages/Danish.php,v $
-|        $Revision: 1.1 $
-|        $Date: 2005-09-17 09:52:22 $
+|        $Revision: 1.2 $
+|        $Date: 2005-10-12 17:16:16 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
-	
+
+
+
+
+
+
+
+
 define("TREE_L1", "Konfigurer tr&aelig;menu");
 define("TREE_L2", "Opdater tr&aelig;menu indstillinger");
-define("TREE_L3", "Konfiguration gemt.");
+define("TREE_L3", "Tr&aelig; menu konfiguration gemt.");
 define("TREE_L4", "Til");
 define("TREE_L5", "Fra");
+define("TREE_L6", "CSS klasse der skal bruges til links der ikke kan &aring;bnes");
+define("TREE_L7", "CSS klasse der skal bruged til links der kan &aring;bnes");
+define("TREE_L8", "CSS klasse der skal bruges til &aring;bnede links");
+define("TREE_L9", "Brug afstandsholder klasse mellem hoved links");
 
 ?>
