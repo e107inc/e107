50a33837d7030385772a674fb765dc387b9503a9
streaky <streaky@localhost>
2005-10-28 06:54:27 +0000

    Tidied up some code, added new header version, removed unused vars and some other random stuff.
    svn path=/trunk/e107_0.7/; revision=5321

--- e107_admin/db.php
+++ e107_admin/db.php
@@ -1,17 +1,22 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|        e107 website system
-|        /admin/db.php
++ ----------------------------------------------------------------------------+
+|     e107 website system
 |
-|        ©Steve Dunstan 2001-2002
-|        http://e107.org
-|        jalist@e107.org
+|     ï¿½Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
 |
-|        Released under the terms and conditions of the
-|        GNU General Public License (http://gnu.org).
-+---------------------------------------------------------------+
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_admin/db.php,v $
+|     $Revision: 1.13 $
+|     $Date: 2005-10-28 06:54:27 $
+|     $Author: streaky $
++----------------------------------------------------------------------------+
 */
+
 require_once("../class2.php");
 $e_sub_cat = 'database';
 
