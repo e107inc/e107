e2ae153c0f5dfcbb55e3beeba2235002968b40e2
sweetas <sweetas@localhost>
2005-10-31 20:44:02 +0000

    removal of update all facility
    svn path=/trunk/e107_0.7/; revision=5377

--- e107_languages/English/admin/lan_e107_update.php
+++ e107_languages/English/admin/lan_e107_update.php
@@ -4,9 +4,9 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_e107_update.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2005-07-01 16:31:21 $
-|     $Author: e107coders $
+|     $Revision: 1.5 $
+|     $Date: 2005-10-31 20:44:02 $
+|     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 
@@ -14,7 +14,6 @@ define("LAN_UPDATE_2", "Action");
 define("LAN_UPDATE_3", "Not Needed");
 
 define("LAN_UPDATE_5", "Update available");
-define("LAN_UPDATE_6", "Update all");
 define("LAN_UPDATE_7", "Executed");
 define("LAN_UPDATE_8", "Update from");
 define("LAN_UPDATE_9", "to");
