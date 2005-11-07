139b2b88e7af1510b4f4688eb1a3094c5e9fcca8
e107dk <e107dk@localhost>
2005-11-07 16:32:26 +0000

    minor ajustments
    svn path=/trunk/e107_langpacks/; revision=5442

--- e107_languages/Danish/admin/lan_upload.php
+++ e107_languages/Danish/admin/lan_upload.php
@@ -1,22 +1,30 @@
 <?php
 /*
-+---------------------------------------------------------------+|        e107 website system  Language File|     
-|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/lan_upload.php,v $|        $Revision: 1.2 $|        $Date: 2005-07-06 22:12:01 $|        $Author: e107dk $+---------------------------------------------------------------+*/
++---------------------------------------------------------------+
+|        e107 website system  Language File
+|     
+|        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/lan_upload.php,v $
+|        $Revision: 1.3 $
+|        $Date: 2005-11-07 16:32:26 $
+|        $Author: e107dk $
++---------------------------------------------------------------+
+*/
 define("UPLLAN_1", "Upload fjernet frs listen.");
 define("UPLLAN_2", "Indstillinger gemt");
 define("UPLLAN_3", "Upload ID");
-define("UPLLAN_4", "Dato");
+// define("UPLLAN_4", "Dato");
 define("UPLLAN_5", "Indsender");
 define("UPLLAN_6", "E-mail");
 define("UPLLAN_7", "Websted");
 define("UPLLAN_8", "Filnavn");
+
 define("UPLLAN_9", "Version");
 define("UPLLAN_10", "Fil");
 define("UPLLAN_11", "Filstørrelse");
 define("UPLLAN_12", "Skærmbillede");
 define("UPLLAN_13", "Beskrivelse");
 define("UPLLAN_14", "Demo");
-define("UPLLAN_15", "Indstillinger");
+// define("UPLLAN_15", "Indstillinger");
 define("UPLLAN_16", "Kopier til nyhed");
 define("UPLLAN_17", "fjern upload fra listen");
 define("UPLLAN_18", "Vis detaljer");
@@ -28,27 +36,31 @@ define("UPLLAN_23", "Navn");
 define("UPLLAN_24", "Filtype");
 define("UPLLAN_25", "Uploads aktiv?");
 define("UPLLAN_26", "Ingen offentlige uploads vil blive tilladt hvis det er slået fra");
-define("UPLLAN_27", "Ja");
-define("UPLLAN_28", "Nej");
+// define("UPLLAN_27", "Ja");
+// define("UPLLAN_28", "Nej");
+
 define("UPLLAN_29", "Opbevarings type");
 define("UPLLAN_30", "Vælg hvordan uploadede filer skal gemmes, enten som normale filer på serveren eller som binære i databasen<br /><b>OBS</b> binære er kun egnet til mindre filer mindre end ca. 500kb");
 define("UPLLAN_31", "Almindelig fil ");
 define("UPLLAN_32", "Binære");
 define("UPLLAN_33", "Maks filstørrelse");
-define("UPLLAN_34", " Maks upload størrelse i bytes  blank =php.ini indstillingerne ( php.ini indstillinger er");
+define("UPLLAN_34", " Maks upload størrelse i bytes - lad være tom for at benytte php.ini indstillingerne ( php.ini indstillinger er");
 define("UPLLAN_35", "Tillad alle typer filer");
 define("UPLLAN_36", "Angiv en type pr. linje");
 define("UPLLAN_37", "Tilladelser");
 define("UPLLAN_38", "Vælg at bestemte brugere kan uploade");
 define("UPLLAN_39", "Udfør");
-define("UPLLAN_40", "Indstillinger");
+// define("UPLLAN_40", "Indstillinger");
 define("UPLLAN_41", "Bemærk - fil uploads er slået fra i din php.ini, det vil ikke være muligt at uploade filer før det er slået til.");
+
 define("UPLLAN_42", "Handlinger");
 define("UPLLAN_43", "Uploads");
 define("UPLLAN_44", "Upload");
+
 define("UPLLAN_45", "Slet følgende filer...");
-define("UPLLAN_46", "Slet");
-define("UPLLAN_47", "Annuller");
+// define("UPLLAN_46", "Slet");
+// define("UPLLAN_47", "Annuller");
+
 define("UPLAN_COPYTODLM", "kopier til Downloads");
 define("UPLAN_IS", "er");
 define("UPLAN_ARE", "er");
--- legacy_langpacks/e107_languages/Danish/admin/lan_upload.php
+++ legacy_langpacks/e107_languages/Danish/admin/lan_upload.php
@@ -4,26 +4,27 @@
 |        e107 website system  Language File
 |     
 |        $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Danish/admin/lan_upload.php,v $
-|        $Revision: 1.1 $
-|        $Date: 2005-09-17 09:29:27 $
+|        $Revision: 1.2 $
+|        $Date: 2005-11-07 16:32:26 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
 define("UPLLAN_1", "Upload fjernet frs listen.");
 define("UPLLAN_2", "Indstillinger gemt");
 define("UPLLAN_3", "Upload ID");
-define("UPLLAN_4", "Dato");
+// define("UPLLAN_4", "Dato");
 define("UPLLAN_5", "Indsender");
 define("UPLLAN_6", "E-mail");
 define("UPLLAN_7", "Websted");
 define("UPLLAN_8", "Filnavn");
+
 define("UPLLAN_9", "Version");
 define("UPLLAN_10", "Fil");
 define("UPLLAN_11", "Filst&oslash;rrelse");
 define("UPLLAN_12", "Sk&aelig;rmbillede");
 define("UPLLAN_13", "Beskrivelse");
 define("UPLLAN_14", "Demo");
-define("UPLLAN_15", "Indstillinger");
+// define("UPLLAN_15", "Indstillinger");
 define("UPLLAN_16", "Kopier til nyhed");
 define("UPLLAN_17", "fjern upload fra listen");
 define("UPLLAN_18", "Vis detaljer");
@@ -35,27 +36,31 @@ define("UPLLAN_23", "Navn");
 define("UPLLAN_24", "Filtype");
 define("UPLLAN_25", "Uploads aktiv?");
 define("UPLLAN_26", "Ingen offentlige uploads vil blive tilladt hvis det er sl&aring;et fra");
-define("UPLLAN_27", "Ja");
-define("UPLLAN_28", "Nej");
+// define("UPLLAN_27", "Ja");
+// define("UPLLAN_28", "Nej");
+
 define("UPLLAN_29", "Opbevarings type");
 define("UPLLAN_30", "V&aelig;lg hvordan uploadede filer skal gemmes, enten som normale filer p&aring; serveren eller som bin&aelig;re i databasen<br /><b>OBS</b> bin&aelig;re er kun egnet til mindre filer mindre end ca. 500kb");
 define("UPLLAN_31", "Almindelig fil ");
 define("UPLLAN_32", "Bin&aelig;re");
 define("UPLLAN_33", "Maks filst&oslash;rrelse");
-define("UPLLAN_34", " Maks upload st&oslash;rrelse i bytes  blank =php.ini indstillingerne ( php.ini indstillinger er");
+define("UPLLAN_34", " Maks upload st&oslash;rrelse i bytes - lad v&aelig;re tom for at benytte php.ini indstillingerne ( php.ini indstillinger er");
 define("UPLLAN_35", "Tillad alle typer filer");
 define("UPLLAN_36", "Angiv en type pr. linje");
 define("UPLLAN_37", "Tilladelser");
 define("UPLLAN_38", "V&aelig;lg at bestemte brugere kan uploade");
 define("UPLLAN_39", "Udf&oslash;r");
-define("UPLLAN_40", "Indstillinger");
+// define("UPLLAN_40", "Indstillinger");
 define("UPLLAN_41", "Bem&aelig;rk - fil uploads er sl&aring;et fra i din php.ini, det vil ikke v&aelig;re muligt at uploade filer f&oslash;r det er sl&aring;et til.");
+
 define("UPLLAN_42", "Handlinger");
 define("UPLLAN_43", "Uploads");
 define("UPLLAN_44", "Upload");
+
 define("UPLLAN_45", "Slet f&oslash;lgende filer...");
-define("UPLLAN_46", "Slet");
-define("UPLLAN_47", "Annuller");
+// define("UPLLAN_46", "Slet");
+// define("UPLLAN_47", "Annuller");
+
 define("UPLAN_COPYTODLM", "kopier til Downloads");
 define("UPLAN_IS", "er");
 define("UPLAN_ARE", "er");
