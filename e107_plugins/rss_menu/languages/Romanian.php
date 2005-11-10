9004f968b5d53caf3a1967b1be56447b8d188a07
i2gh0st <i2gh0st@localhost>
2005-11-10 21:46:35 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5458

--- e107_plugins/rss_menu/languages/Romanian.php
+++ e107_plugins/rss_menu/languages/Romanian.php
@@ -4,20 +4,35 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/rss_menu/languages/Romanian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-10-23 17:27:48 $
-|     $Author: Bogdan M. Botezatu (Sphynx)
+|     $Revision: 1.2 $
+|     $Date: 2005-11-10 21:42:47 $
+|     $Author: i2gh0st $
 +----------------------------------------------------------------------------+
 */
-	
-define("BACKEND_MENU_L1", " pot fi transmise folosind transmisia de ştriri RSS.");
-define("BACKEND_MENU_L2", "Ştiri RSS");
 
-define("BACKEND_MENU_L3", "Ştirile noastre");
+define("BACKEND_MENU_L1", " pot fi transmise folosind transmisia de striri RSS.");
+define("BACKEND_MENU_L2", "Stiri RSS");
+
+define("BACKEND_MENU_L3", "Stirile noastre");
 define("BACKEND_MENU_L4", "Comentariile noastre");
-define("BACKEND_MENU_L5", "Firele forumurilor noastre");
-define("BACKEND_MENU_L6", "Mesajele forumurilor noastre");
+define("BACKEND_MENU_L5", "Subiectele forumului nostru");
+define("BACKEND_MENU_L6", "Mesajele forumului nostru");
 
-define("BACKEND_MENU_L7", "Mesajele de pe Chatbox");
-define("BACKEND_MENU_L8", "Rapoartele noastre");
+define("BACKEND_MENU_L7", "Posturile din chatbox");
+define("BACKEND_MENU_L8", "Rapoartelle noastre backtrack");
+define("BACKEND_MENU_L9", "Downloadurile noastre");
+
+define("RSS_LAN01", "Activeaza feeduri separate pentru fiecare categorie de stiri");
+define("RSS_LAN02", "Activeaza feeduri separate pentru fiecare categorie de downloaduri?");
+
+define("RSS_NEWS","Stiri");
+define("RSS_COM","Comentarii"); 
+define("RSS_ART","Articole");
+define("RSS_REV", "Revizuiri");
+define("RSS_FT","Subiectele Forumului");
+define("RSS_FP","Posturile Forumului");
+define("RSS_FSP","Anumite posturi ale Forumului");
+define("RSS_BUG","Bugtracker");
+define("RSS_FOR","Forum");
+define("RSS_DL","Downloaduri");
 ?>
