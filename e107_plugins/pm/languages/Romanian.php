6fea93ec2f1a63782dc3d754e130379d7fbbff92
i2gh0st <i2gh0st@localhost>
2005-11-11 07:00:58 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5462

--- e107_plugins/pm/languages/Romanian.php
+++ e107_plugins/pm/languages/Romanian.php
@@ -1,4 +1,14 @@
-<?php
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pm/languages/Romanian.php,v $
+|     $Revision: 1.4 $
+|     $Date: 2005-11-11 07:00:58 $
+|     $Author: i2gh0st $
++----------------------------------------------------------------------------+
+*/
 
 define('LAN_PM', "Mesaje private");
 define('LAN_PM_1', "Trimite mesaj privat");
@@ -11,18 +21,18 @@ define('LAN_PM_7', "Emoticonuri");
 define('LAN_PM_8', "Atasamente");
 define('LAN_PM_9', "Recipient citite");
 define('LAN_PM_10', "Trimite-mi un email cand acest PM este citit");
-define('LAN_PM_11', "adauga upload nou");
+define('LAN_PM_11', "adauga fisier nou");
 define('LAN_PM_12', "Nu esti autorizat sa folosesti sistemul PM");
-define('LAN_PM_13', "Outbox este curent {PERCENT}% plina, nu poti trimite PM-uri pana nu mai stergi din ele");
+define('LAN_PM_13', "Afara este curent {PERCENT}% plina, nu poti trimite PM-uri pana nu mai stergi din ele");
 define('LAN_PM_14', "ERROARE: Posibil poast duplicat, PM netrimis");
 define('LAN_PM_15', "Nu esti autorizat sa trimitzi catre clasa de utilizatori");
 define('LAN_PM_16', "Trebuie sa fi membru al clasei");
 define('LAN_PM_17', "Utilizator negasit");
-define('LAN_PM_18', "You are not allowed to send PMs to: ");
-define('LAN_PM_19', "Your outbox is full, you are not allowed to send PMs");
-define('LAN_PM_21', "Adding this PM will exceed your maximum outbox size, PM not posted");
-define('LAN_PM_22', "File upload failed");
-define('LAN_PM_23', "You are not allowed to send attachments");
+define('LAN_PM_18', "Nu iti este permis sa trimiti PM-uri catre: ");
+define('LAN_PM_19', "Afara este plina, nu mai poti trimite PM-uri is full, you are not allowed to send PMs");
+define('LAN_PM_21', "Adaugarea acestui PM va depasi valoarea maxima a casutei afara, PM nepostat");
+define('LAN_PM_22', "Uploadare fisier nereusita");
+define('LAN_PM_23', "Nu iti este permis sa trimiti atasamente");
 define('LAN_PM_24', "Sterg PM");
 define('LAN_PM_25', "Inauntru");
 define('LAN_PM_26', "Afara");
@@ -37,41 +47,41 @@ define('LAN_PM_34', "Nu exista mesaje");
 define('LAN_PM_35', "Trimite mesaj nou");
 define('LAN_PM_36', "total");
 define('LAN_PM_37', "necitite");
-define('LAN_PM_38', "PM Sent to userclass");
-define('LAN_PM_39', "Failed to send PM to");
-define('LAN_PM_40', "PM Sent to user");
-define('LAN_PM_41', "Failed to post PM to your Outbox");
+define('LAN_PM_38', "PM trimis catre clasa de useri");
+define('LAN_PM_39', "Nereusita trimiterea PM-ului catre");
+define('LAN_PM_40', "PM trimis catre user");
+define('LAN_PM_41', "Nereusita postarea PM-ului in casutza afara");
 
-define('LAN_PM_42', "PM deleted from inbox");
-define('LAN_PM_43', "PM deleted from outbox");
-define('LAN_PM_44', "Block removed: {UNAME} is now allowed to send you PMs");
-define('LAN_PM_45', "ERROR: Block not removed, unknown error");
-define('LAN_PM_46', "Block not in place for {UNAME}");
-define('LAN_PM_47', "Block added: {UNAME} is no longer allowed to send you PMs");
-define('LAN_PM_48', "ERROR: Block not added, unknown error");
-define('LAN_PM_49', "ERROR: Block already in place for {UNAME}");
-define('LAN_PM_50', "Block User");
-define('LAN_PM_51', "Unblock User");
-define('LAN_PM_52', "Delete");
-define('LAN_PM_53', "Delete Selected");
-define('LAN_PM_54', "Quote original");
-define('LAN_PM_55', "Send Reply");
-define('LAN_PM_56', "You are not permitted to reply to this message");
-define('LAN_PM_57', "Message not found");
+define('LAN_PM_42', "PM sters din casuta inauntru";
+define('LAN_PM_43', "PM sters din casutza afara");
+define('LAN_PM_44', "Blocare retrasa: {UNAME} ii este permis sa-ti trimita PM-uri");
+define('LAN_PM_45', "ERORE: Blocare neretrasa, eroare necunoscuta");
+define('LAN_PM_46', "Blocarea este acum activata pentru {UNAME}");
+define('LAN_PM_47', "Blocare adaugata: {UNAME} nu ii mai este permis sa-ti trimita PM-uri");
+define('LAN_PM_48', "ERORE: Blocarea nu a fost adaugata, eroare necunoscuta");
+define('LAN_PM_49', "ERROR: Blocare deja adaugata pentru {UNAME}");
+define('LAN_PM_50', "Blocare user");
+define('LAN_PM_51', "Deblocare user");
+define('LAN_PM_52', "Sterge");
+define('LAN_PM_53', "Sterge selectia");
+define('LAN_PM_54', "Citeaza originalul");
+define('LAN_PM_55', "Trimite Raspuns");
+define('LAN_PM_56', "Nu iti este permis sa raspunzi la acest mesaj");
+define('LAN_PM_57', "Mesaj negasit");
 define('LAN_PM_58', "Re: ");
-define('LAN_PM_59', "Go to page: ");
-define('LAN_PM_60', "You are not permitted to view this message");
-define('LAN_PM_61', "No subject");
-define('LAN_PM_62', "File: [{FILENAME}] exceeds size limit - not attached");
+define('LAN_PM_59', "Dute la pagina: ");
+define('LAN_PM_60', "Nu iti este permis sa vezi acest mesaj");
+define('LAN_PM_61', "Fara subiect");
+define('LAN_PM_62', "Fisier: [{FILENAME}] excede limita de marime - neatasat");
 
-define("LAN_PM_100", "New PM from ");
-define("LAN_PM_101", "You have received a new Private Message from ");
-define("LAN_PM_102", "Message sent from: ");
-define("LAN_PM_103", "Message subject: ");
-define("LAN_PM_104", "Number of attachments: ");
-define("LAN_PM_105", "You can view the PM at: ");
-define("LAN_PM_106", "PM read by ");
-define("LAN_PM_107", "The Private Message you sent to {UNAME} was read on ");
-define("LAN_PM_108", "Message sent on: ");
+define("LAN_PM_100", "PM nou de la ");
+define("LAN_PM_101", "Ai primit un nou Mesaj Privat de la ");
+define("LAN_PM_102", "Mesaj trimis de la: ");
+define("LAN_PM_103", "Subiectul mesajului: ");
+define("LAN_PM_104", "Numarul atasamentelor: ");
+define("LAN_PM_105", "Poti vedea PM-ul la: ");
+define("LAN_PM_106", "PM citit de ");
+define("LAN_PM_107", "Mesajul privat trimis catre {UNAME} a fost citit ");
+define("LAN_PM_108", "Mesaj trimis: ");
 
 ?>
