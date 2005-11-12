1b151dbf349af6b8059e23abb99dac2159fae095
e107dk <e107dk@localhost>
2005-11-12 13:38:32 +0000

    added recent changes
    svn path=/trunk/e107_langpacks/; revision=5476

--- e107_languages/Danish/admin/help/emoticon.php
+++ e107_languages/Danish/admin/help/emoticon.php
@@ -4,12 +4,13 @@
 |        e107 website system  Language File
 |
 |        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/help/emoticon.php,v $
-|        $Revision: 1.3 $
-|        $Date: 2005-11-03 16:54:04 $
+|        $Revision: 1.4 $
+|        $Date: 2005-11-12 13:37:43 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
-$text = "";
+$text = "Med smileys aktiveret, vil standard smiley tekst strenge blive erstattet af deres
+respektive smiley billede i alt indhold på dit site.";
 
 $ns -> tablerender("Smiley Hjælp", $text);
 ?>
--- legacy_langpacks/e107_languages/Danish/admin/help/emoticon.php
+++ legacy_langpacks/e107_languages/Danish/admin/help/emoticon.php
@@ -4,12 +4,13 @@
 |        e107 website system  Language File
 |
 |        $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Danish/admin/help/emoticon.php,v $
-|        $Revision: 1.2 $
-|        $Date: 2005-11-03 16:54:04 $
+|        $Revision: 1.3 $
+|        $Date: 2005-11-12 13:38:32 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
-$text = "";
+$text = "Med smileys aktiveret, vil standard smiley tekst strenge blive erstattet af deres
+respektive smiley billede i alt indhold p&aring; dit site.";
 
 $ns -> tablerender("Smiley Hj&aelig;lp", $text);
 ?>
