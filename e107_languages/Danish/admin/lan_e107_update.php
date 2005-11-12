e838f2b27002c0502c8d7b3f871802de11bb33e6
e107dk <e107dk@localhost>
2005-11-12 13:38:50 +0000

    minor ajustments
    svn path=/trunk/e107_langpacks/; revision=5477

--- e107_languages/Danish/admin/lan_e107_update.php
+++ e107_languages/Danish/admin/lan_e107_update.php
@@ -4,8 +4,8 @@
 |        e107 website system  Language File
 |     
 |        $Source: /cvs_backup/e107_langpacks/e107_languages/Danish/admin/lan_e107_update.php,v $
-|        $Revision: 1.4 $
-|        $Date: 2005-11-03 18:12:35 $
+|        $Revision: 1.5 $
+|        $Date: 2005-11-12 13:38:03 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
@@ -14,8 +14,8 @@ define("LAN_UPDATE_2", "Handling");
 define("LAN_UPDATE_3", "Ikke nødvendig");
 
 define("LAN_UPDATE_5", "Opdateringer er tilgængelig");
-define("LAN_UPDATE_7", "Udført");
-define("LAN_UPDATE_8", "Opdater fra");
+define("LAN_UPDATE_7", "Gennemført");
+define("LAN_UPDATE_8", "opdatering fra");
 define("LAN_UPDATE_9", "til");
 define("LAN_UPDATE_10", "Tilgængelige opdateringer");
 
--- legacy_langpacks/e107_languages/Danish/admin/lan_e107_update.php
+++ legacy_langpacks/e107_languages/Danish/admin/lan_e107_update.php
@@ -4,8 +4,8 @@
 |        e107 website system  Language File
 |     
 |        $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Danish/admin/lan_e107_update.php,v $
-|        $Revision: 1.2 $
-|        $Date: 2005-11-03 18:12:35 $
+|        $Revision: 1.3 $
+|        $Date: 2005-11-12 13:38:50 $
 |        $Author: e107dk $
 +---------------------------------------------------------------+
 */
@@ -14,8 +14,8 @@ define("LAN_UPDATE_2", "Handling");
 define("LAN_UPDATE_3", "Ikke n&oslash;dvendig");
 
 define("LAN_UPDATE_5", "Opdateringer er tilg&aelig;ngelig");
-define("LAN_UPDATE_7", "Udf&oslash;rt");
-define("LAN_UPDATE_8", "Opdater fra");
+define("LAN_UPDATE_7", "Gennemf&oslash;rt");
+define("LAN_UPDATE_8", "Opdatering fra");
 define("LAN_UPDATE_9", "til");
 define("LAN_UPDATE_10", "Tilg&aelig;ngelige opdateringer");
 
