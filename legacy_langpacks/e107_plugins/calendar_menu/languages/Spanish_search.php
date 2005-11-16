559176e248d260e3005518f216752a691e231f08
natxocc <natxocc@localhost>
2005-11-16 21:34:01 +0000

    Updated from original CVS 16-11-05
    svn path=/trunk/e107_langpacks/; revision=5499

---
+++ legacy_langpacks/e107_plugins/calendar_menu/languages/Spanish_search.php
@@ -0,0 +1,13 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_plugins/calendar_menu/languages/Spanish_search.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-11-16 21:34:01 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+define("CM_SCH_LAN_1", "Calendario");
+?>
