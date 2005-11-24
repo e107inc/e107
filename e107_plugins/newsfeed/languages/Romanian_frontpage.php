e73df7b27947f916d86b3781d2d13472a26ca8de
i2gh0st <i2gh0st@localhost>
2005-11-24 08:07:47 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=5546

---
+++ e107_plugins/newsfeed/languages/Romanian_frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/newsfeed/languages/Romanian_frontpage.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-11-24 08:07:47 $
+|     $Author: i2gh0st $
++----------------------------------------------------------------------------+
+*/
+
+define("NWSF_FP_1", "Stiri");
+define("NWSF_FP_2", "Pagina principala");
+
+?>
