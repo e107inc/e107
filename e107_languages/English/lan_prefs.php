4b12320ee7e215b36881e6f01bfb02cf575a4b7b
streaky <streaky@localhost>
2005-11-25 12:10:32 +0000

    Updates for lan files regarding default setting for some site prefs.
    svn path=/trunk/e107_0.7/; revision=5562

--- e107_languages/English/lan_prefs.php
+++ e107_languages/English/lan_prefs.php
@@ -4,15 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_prefs.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-08-24 03:40:33 $
-|     $Author: sweetas $
+|     $Revision: 1.2 $
+|     $Date: 2005-11-25 12:10:32 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_PREF_1", "e107 powered website");
-define("LAN_PREF_2", "e107 website system");
-define("LAN_PREF_3", "All trademarks are © their respective owners, all other content is © e107 powered website.<br />e107 is © e107.org 2002-2005 and is released under the <a href=\"http://www.gnu.org/\" rel=\"external\">GNU GPL license</a>.");
+define("LAN_PREF_1", "e107 Powered Website");
+define("LAN_PREF_2", "e107 Website System");
+define("LAN_PREF_3", "This site is powered by <a href=&quot;http://e107.org/&quot; rel=&quot;external&quot;>e107</a>, which is released under the terms of the <a href=&quot;http://www.gnu.org/&quot; rel=&quot;external&quot;>GNU</a> GPL License.");
 define("LAN_PREF_4", "censored");
 define("LAN_PREF_5", "Forums");
 
