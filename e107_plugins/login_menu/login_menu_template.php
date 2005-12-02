92c09cbd37254f89fc6859e3ea22970b0906b663
e107coders <e107coders@localhost>
2005-12-02 07:23:06 +0000

    added template
    svn path=/trunk/e107_0.7/; revision=5609

---
+++ e107_plugins/login_menu/login_menu_template.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!isset($LOGIN_MENU_FORM)){
+
+	$LOGIN_MENU_FORM = "\n
+    <div style='text-align: center'>
+    {LM_USERNAME_TXT}
+	<br />\n
+	{LM_USERNAME_INPUT}
+	<br />
+	{LM_PASSWORD_TXT}
+	<br />\n
+    {LM_PASSWORD_INPUT}
+	<br />\n
+    {LM_IMAGECODE}
+	{LM_LOGINBUTTON}
+    {LM_REMEMBERME}
+	<br /><br />
+	[ {LM_SIGNUP_LINK} ]
+	<br />
+	[ {LM_FPW_LINK} ]
+
+	</div>
+	";
+}
+
+
+?>
