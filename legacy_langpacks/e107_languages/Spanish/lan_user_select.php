a42f6752219e199affceaf7a30701fe13d0e43d8
natxocc <natxocc@localhost>
2005-12-08 21:03:35 +0000

    Update from original CVS 08-12-05
    svn path=/trunk/e107_langpacks/; revision=5639

---
+++ legacy_langpacks/e107_languages/Spanish/lan_user_select.php
@@ -0,0 +1,13 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Spanish/lan_user_select.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-12-08 21:03:35 $
+|     $Author: natxocc $
++----------------------------------------------------------------------------+
+*/
+define("US_LAN_1", "Selecciona usuario");
+?>
