e68f8945455a59face7d8d8517aeb6f588220bf7
e107dk <e107dk@localhost>
2005-12-14 16:16:48 +0000

    added file translated
    svn path=/trunk/e107_langpacks/; revision=5669

---
+++ legacy_langpacks/e107_languages/Danish/lan_user_select.php
@@ -0,0 +1,13 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Danish/lan_user_select.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005-12-14 16:16:48 $
+|     $Author: e107dk $
++----------------------------------------------------------------------------+
+*/
+define("US_LAN_1", "V&aelig;lg bruger");
+?>
