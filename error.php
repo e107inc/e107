98b3e66dec05557c8b5a6fe07cf3eaaffe851f5f
streaky <streaky@localhost>
2005-12-15 12:23:27 +0000

    Added HTTP 1.1 response code for 404 Not Found pages, to help with google maps, search angines and other useful stuff
    svn path=/trunk/e107_0.7/; revision=5681

--- error.php
+++ error.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/error.php,v $
-|     $Revision: 1.9 $
-|     $Date: 2005-07-09 13:32:03 $
+|     $Revision: 1.10 $
+|     $Date: 2005-12-15 12:23:27 $
 |     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
@@ -21,15 +21,15 @@ require_once("class2.php");
 if(!e_QUERY || (e_QUERY != 401 && e_QUERY != 403 && e_QUERY != 404 && e_QUERY != 500))
 {
 	echo "<script type='text/javascript'>document.location.href='index.php'</script>\n";
-	header("location: index.php");
+	header("location: ".e_HTTP."index.php");
 	exit;
 }
 
 require_once(HEADERF);
-	
+
 $errFrom = $_SERVER['HTTP_REFERER'];
 $errTo = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	
+
 switch(e_QUERY) {
 	case 401:
 	$text = "<div class='installe'><img src='".e_IMAGE_ABS."icons/icon3.png' alt='Error Icon'> ".LAN_1."</div><br /><div class='installh'>".LAN_2."</div><br /><div class='smalltext'>".LAN_3."</div>
@@ -41,20 +41,21 @@ switch(e_QUERY) {
 	break;
 	default:
 
-	
+
 	case 404:
+	header("HTTP/1.1 404 Not Found");
 	$text = "<h3><img src='".e_IMAGE_ABS."icons/icon3.png' alt='Error Icon'> ".LAN_7."</h3><br />".LAN_ERROR_01."<br /><br />".LAN_ERROR_03."<b>{$errTo}</b>".LAN_ERROR_04."<br /><br />";
-	
+
 	if (strlen($errFrom)) $text .= LAN_9." ( <a href='{$errFrom}' rel='external'>{$errFrom}</a> ) -- ".LAN_19."<br />";
-	
+
 	$base_path = e_HTTP;
-	
+
 	$text .= "<br /><a href='{$base_path}index.php'>".LAN_20."</a><br />";
 	$text .= "<a href='{$base_path}search.php'>".LAN_ERROR_02."</a></p>";
-	
+
 	break;
-	
-	
+
+
 	case 500:
 	$text = "<div class='installe'><img src='".e_IMAGE_ABS."icons/icon3.png' alt='Error Icon'> ".LAN_10."</div><br /><div class='installh'>".LAN_11."</div><br /><div class='smalltext'>".LAN_12."</div>
 		<br /><div class='installh'>".LAN_2."<a href='index.php'>".LAN_20."</a></div>";
