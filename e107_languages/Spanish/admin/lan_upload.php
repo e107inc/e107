5e80dec4fae47f56b39d61fc88dcb677e37a2e6c
natxocc <natxocc@localhost>
2005-12-21 23:26:27 +0000

    Update from original CVS 22-12-05
    svn path=/trunk/e107_langpacks/; revision=5712

--- e107_languages/Spanish/admin/lan_upload.php
+++ e107_languages/Spanish/admin/lan_upload.php
@@ -4,15 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Spanish/admin/lan_upload.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2005-11-11 23:57:40 $
+|     $Revision: 1.7 $
+|     $Date: 2005-12-21 23:26:27 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
 define("UPLLAN_1", "Actualizado: Borrado.");
 define("UPLLAN_2", "Configuración guardada en base de datos");
 define("UPLLAN_3", "Actualizar ID");
-//define("UPLLAN_4", "Fecha");
 define("UPLLAN_5", "Cartel");
 define("UPLLAN_6", "Email");
 define("UPLLAN_7", "Sitio web");
@@ -23,7 +22,6 @@ define("UPLLAN_11", "Tamaño de archivo");
 define("UPLLAN_12", "Captura de pantalla");
 define("UPLLAN_13", "Descripción");
 define("UPLLAN_14", "Demo");
-//define("UPLLAN_15", "Opciones");
 define("UPLLAN_16", "Copiar a panel de noticias");
 define("UPLLAN_17", "Borrar");
 define("UPLLAN_18", "Ver detalles");
@@ -35,8 +33,6 @@ define("UPLLAN_23", "Nombre");
 define("UPLLAN_24", "Tipo de fichero");
 define("UPLLAN_25", "¿Permitir transferencia de ficheros?");
 define("UPLLAN_26", "No se permitirá transferir ningún fichero si está deshabilitado");
-//define("UPLLAN_27", "Sí");
-//define("UPLLAN_28", "No");
 define("UPLLAN_29", "Tipo de almacenamiento");
 define("UPLLAN_30", "Elige cómo almacenar ficheros transferidos, tanto como ficheros normales transferidos al servidor o como información binaria en base de datos");
 define("UPLLAN_31", "Fichero plano<br>");
@@ -48,17 +44,17 @@ define("UPLLAN_36", "Por favor introduce un tipo por línea");
 define("UPLLAN_37", "Permisos");
 define("UPLLAN_38", "Selecciona que tipo de usuarios pueden transferir ficheros");
 define("UPLLAN_39", "Enviar");
-//define("UPLLAN_40", "Opciones");
 define("UPLLAN_41", "Por favor tenga en cuenta - la transferencia de ficheros está deshabilitada en su php.ini, no será posible transferir ficheros hasta que no lo cambie a On.");
 define("UPLLAN_42", "Acciones");
 define("UPLLAN_43", "Transferidos");
 define("UPLLAN_44", "Transferir");
 define("UPLLAN_45", "¿Esta seguro de eliminar el siguiente archivo...");
-//define("UPLLAN_46", "Borrar");
-//define("UPLLAN_47", "Cancelar");
 
 define("UPLAN_COPYTODLM", "Copiar a descargas");
 define("UPLAN_IS", " ");
 define("UPLAN_ARE", " ");
 define("UPLAN_COPYTODLS", "Copiar a descargas");
+
+define("UPLLAN_48", "Por razones de seguridad, los archivos permitidos has sido eliminados de la base de datos a un archivo planoque se encuentra en el directorio admin. Para usarlo, renombra el archivo e107_admin/filetypes_.php a e107_admin/filetypes.php
+y añade una lista delimitada de las extensiones en él. No aconsejamos que permita transferir .html, .txt, etc ya que un atacante podría transferir un archivo de este tipo que incluyese código javascript malicioso. No debería tampoco, por supuesto, permitir transferir archivos .php o otro que permitiera scripts ejecutables.");
 ?>
