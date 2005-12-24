1698fa9f10420ba08d5cf58c1fafdb9b32c32338
e107coders <e107coders@localhost>
2005-12-24 00:22:22 +0000

    more of the TinyMce 2.01 update
    svn path=/trunk/e107_0.7/; revision=5722

---
+++ e107_handlers/tiny_mce/plugins/emoticons/langs/da.js
@@ -0,0 +1,4 @@
+// UK lang variables
+
+tinyMCELang['lang_insert_emotions_title'] = 'Indsæt smiley';
+tinyMCELang['lang_emotions_desc'] = 'Smileys';
---
+++ e107_handlers/tiny_mce/plugins/ibrowser/langs/da.js
@@ -0,0 +1,38 @@
+// DA_DK lang variables
+
+tinyMCELang['lang_ibrowser_title'] = 'Indsæt / Rediger Billede';
+tinyMCELang['lang_ibrowser_desc'] = 'Indsæt / Rediger Billede';
+tinyMCELang['lang_ibrowser_library'] = 'Bibliotek';
+tinyMCELang['lang_ibrowser_preview'] = 'Fremvisning';
+tinyMCELang['lang_ibrowser_img_sel'] = 'Billede valg';
+tinyMCELang['lang_ibrowser_img_info'] = 'Billede informationer';
+tinyMCELang['lang_ibrowser_img_upload'] = 'Billede upload';
+tinyMCELang['lang_ibrowser_images'] = 'Billeder';
+tinyMCELang['lang_ibrowser_src'] = 'Kilde';
+tinyMCELang['lang_ibrowser_alt'] = 'Beskrivelse';
+tinyMCELang['lang_ibrowser_size'] = 'Størrelse';
+tinyMCELang['lang_ibrowser_align'] = 'Tekst flow';
+tinyMCELang['lang_ibrowser_height'] = 'Højde';
+tinyMCELang['lang_ibrowser_width'] = 'Bredde';
+tinyMCELang['lang_ibrowser_reset'] = 'Nulstil Dimensioner';
+tinyMCELang['lang_ibrowser_border'] = 'Kant';
+tinyMCELang['lang_ibrowser_hspace'] = 'HRum';
+tinyMCELang['lang_ibrowser_vspace'] = 'VRum';
+tinyMCELang['lang_ibrowser_select'] = 'Gem';
+tinyMCELang['lang_ibrowser_delete'] = 'Slet';
+tinyMCELang['lang_ibrowser_cancel'] = 'Annuller';
+tinyMCELang['lang_ibrowser_uploadtxt'] = 'Fil';
+tinyMCELang['lang_ibrowser_uploadbt'] = 'Upload';
+// error messages
+tinyMCELang['lang_ibrowser_error'] = 'Fejl';
+tinyMCELang['lang_ibrowser_errornoimg'] = 'Vælg et billede';
+tinyMCELang['lang_ibrowser_errornodir'] = 'Bibliotek eksisterer ikke fysisk';
+tinyMCELang['lang_ibrowser_errorupload'] = 'En fejl opstod under fil upload.\nPrøv igen senere';
+tinyMCELang['lang_ibrowser_errortype'] = 'Forkert billede fil type';
+tinyMCELang['lang_ibrowser_errordelete'] = 'Slet fejlede';
+tinyMCELang['lang_ibrowser_confirmdelete'] = 'Klik OK for at slette billede!';
+tinyMCELang['lang_ibrowser_error_width_nan'] = 'Bredde er ikke et tal!';
+tinyMCELang['lang_ibrowser_error_height_nan'] = 'Højde er ikke et tal!';
+tinyMCELang['lang_ibrowser_error_border_nan'] = 'Kant er ikke et tal!';
+tinyMCELang['lang_ibrowser_error_hspace_nan'] = 'Horisontalt rum er ikke et tal!';
+tinyMCELang['lang_ibrowser_error_vspace_nan'] = 'Verticalt rum er ikke et tal!';	
