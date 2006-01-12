c284b353c2bf548a25d7dd282a4de8107032546e
natxocc <natxocc@localhost>
2006-01-12 01:23:30 +0000

    New file from CVS 11-06-05
    svn path=/trunk/e107_langpacks/; revision=5869

---
+++ e107_plugins/pdf/languages/Spanish.php
@@ -0,0 +1,37 @@
+<?php
+/*
++ -----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pdf/languages/Spanish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-01-12 01:22:45 $
+|     $Author: natxocc $
++-----------------------------------------------------------------------------+
+*/
+
+define("PDF_PLUGIN_LAN_1", "PDF");
+define("PDF_PLUGIN_LAN_2", "Soporte de creación de PDF");
+define("PDF_PLUGIN_LAN_3", "PDF");
+define("PDF_PLUGIN_LAN_4", "Este plugin esta listo para ser usado.");
+
+define("PDF_LAN_1", "PDF");
+define("PDF_LAN_2", "Preferencias PDF");
+define("PDF_LAN_3", "Activado");
+define("PDF_LAN_4", "Desactivado");
+define("PDF_LAN_5", "Margen de pag. izquierda");
+define("PDF_LAN_6", "Margen de pag. derecha");
+define("PDF_LAN_7", "Margen de pag. arriba");
+define("PDF_LAN_8", "Tipo de fuente");
+define("PDF_LAN_9", "Tamaño fuente por def.");
+define("PDF_LAN_10", "Tipo fuente del sitio");
+define("PDF_LAN_11", "Tamaño fuente pag. url");
+define("PDF_LAN_12", "Tamaño fuente número de pag.");
+define("PDF_LAN_13", "¿Mostrar logo en el pdf?");
+define("PDF_LAN_14", "¿Mostrar nombre del sitio en el PDF?");
+define("PDF_LAN_15", "¿Mostrar creador de la página en el PDF?");
+define("PDF_LAN_16", "¿Mostrar numeros de página en el PDF?");
+define("PDF_LAN_17", "Actualizar");
+define("PDF_LAN_18", "Prferencias de PDF actualizadas correctamente");
+define("PDF_LAN_19", "Página");
+?>
