178c435ed8733cac1508b478b0449891a18fc0ef
lisa_ <lisa_@localhost>
2006-01-16 10:55:14 +0000

    content: improved pdf creation
    svn path=/trunk/e107_0.7/; revision=5961

--- e107_plugins/content/e_emailprint.php
+++ e107_plugins/content/e_emailprint.php
@@ -77,7 +77,7 @@ function email_item($id)
 
 function print_item_pdf($id){
 	global $tp, $content_shortcodes;
-	global $CONTENT_PRINT_IMAGES, $row, $content_image_path, $content_pref, $mainparent;
+	global $CONTENT_PRINT_IMAGES, $row, $content_image_path, $content_pref, $mainparent, $eArrayStorage;
 
 	//in this section you decide what to needs to be output to the pdf file
 	$con = new convert;
@@ -88,7 +88,7 @@ function print_item_pdf($id){
 	$aa = new content;
 	
 	if(!is_object($sql)){ $sql = new db; }
-	$sql -> db_Select($plugintable, "content_id, content_heading, content_subheading, content_text, content_image, content_author, content_parent, content_datestamp, content_class", "content_id='".intval($id)."' ");
+	$sql -> db_Select($plugintable, "content_id, content_heading, content_subheading, content_text, content_image, content_author, content_parent, content_datestamp, content_class, content_pref", "content_id='".intval($id)."' ");
 	$row = $sql -> db_Fetch();
 
 	if(!check_class($row['content_class'])){
@@ -104,12 +104,52 @@ function print_item_pdf($id){
 	$content_image_path			= $tp -> replaceConstants($content_pref["content_image_path_{$mainparent}"]);
 	$img						= $tp -> parseTemplate('{CONTENT_PDF_IMAGES}', FALSE, $content_shortcodes);
 
+	//add custom and preset tags if present
+	$custom						= $eArrayStorage->ReadArray($row['content_pref']);
+	$months = array(CONTENT_ADMIN_DATE_LAN_0, CONTENT_ADMIN_DATE_LAN_1, CONTENT_ADMIN_DATE_LAN_2, CONTENT_ADMIN_DATE_LAN_3, CONTENT_ADMIN_DATE_LAN_4, CONTENT_ADMIN_DATE_LAN_5, CONTENT_ADMIN_DATE_LAN_6, CONTENT_ADMIN_DATE_LAN_7, CONTENT_ADMIN_DATE_LAN_8, CONTENT_ADMIN_DATE_LAN_9, CONTENT_ADMIN_DATE_LAN_10, CONTENT_ADMIN_DATE_LAN_11);
+
+	$CONTENT_CONTENT_TABLE_CUSTOM_TAGS = "";
+	if(!empty($custom)){
+		foreach($custom as $k => $v){
+			if($k == "content_custom_presettags"){
+				if(isset($content_pref["content_content_presettags_{$mainparent}"]) && $content_pref["content_content_presettags_{$mainparent}"]){
+					foreach($v as $ck => $cv){
+						if(is_array($cv)){	//date
+							if(!($cv['day']=="" && $cv['month']=="" && $cv['year']=="")){
+								$vv = $cv['day']." ".$months[($cv['month']-1)]." ".$cv['year'];
+							}
+						}else{
+							$vv = $cv;
+						}
+						if( isset($ck) && $ck != "" && isset($vv) && $vv!="" ){
+							$CUSTOM_TAGS = TRUE;
+							$CONTENT_CONTENT_TABLE_CUSTOM_KEY		= $tp->toHTML($ck, true);
+							$CONTENT_CONTENT_TABLE_CUSTOM_VALUE		= $tp->toHTML($vv, true);
+							$CONTENT_CONTENT_TABLE_CUSTOM_TAGS .= $CONTENT_CONTENT_TABLE_CUSTOM_KEY." : ".$CONTENT_CONTENT_TABLE_CUSTOM_VALUE."<br />";
+						}
+					}
+				}
+			}else{
+				if(isset($content_pref["content_content_customtags_{$mainparent}"]) && $content_pref["content_content_customtags_{$mainparent}"]){
+					$key = substr($k,15);
+					if( isset($key) && $key != "" && isset($v) && $v!="" ){
+						$CUSTOM_TAGS = TRUE;
+						$CONTENT_CONTENT_TABLE_CUSTOM_KEY		= $tp->toHTML($key, true);
+						$CONTENT_CONTENT_TABLE_CUSTOM_VALUE		= $tp->toHTML($v, true);
+						$CONTENT_CONTENT_TABLE_CUSTOM_TAGS .= $CONTENT_CONTENT_TABLE_CUSTOM_KEY." : ".$CONTENT_CONTENT_TABLE_CUSTOM_VALUE."<br />";
+					}
+				}
+			}
+		}
+	}
+
 	$text = "
 	<b>".$row['content_heading']."</b><br />
 	".$row['content_subheading']."<br />
 	".$authordetails[1].", ".$row['content_datestamp']."<br />
 	<br />
 	".$row['content_text']."<br />
+	".($CONTENT_CONTENT_TABLE_CUSTOM_TAGS ? $CONTENT_CONTENT_TABLE_CUSTOM_TAGS : "")."
 	<div style='float:left; padding-left:10px;'>".$img."</div>
 	";
 
