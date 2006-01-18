d436500dabf3d04c87054ac22f349a9d7534c1b7
sweetas <sweetas@localhost>
2006-01-18 19:54:06 +0000

    version update
    svn path=/trunk/e107_0.7/; revision=6000

--- e107_admin/ver.php
+++ e107_admin/ver.php
@@ -11,15 +11,15 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/ver.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2006-01-16 13:54:20 $
+|     $Revision: 1.7 $
+|     $Date: 2006-01-18 19:54:06 $
 |     $Author: sweetas $
 +----------------------------------------------------------------------------+
 */
 	
 if (!defined('e107_INIT')) { exit; }
 
-$e107info['e107_version'] = "v0.7";
-$e107info['e107_build'] = "1";
+$e107info['e107_version'] = "v0.7.1";
+$e107info['e107_build'] = "20060118";
 	
 ?>
