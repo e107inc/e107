72fc6efac7572d61ec596a0db0296800d9d8c5e8
secretr <secretr@localhost>
2006-01-24 16:13:52 +0000

    Bulgarian Language Pack - plugin update
    svn path=/trunk/e107_langpacks/; revision=6032

---
+++ e107_plugins/rss_menu/languages/Bulgarian.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/rss_menu/languages/Bulgarian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-01-24 16:13:52 $
+|     $Author: secretr $
++----------------------------------------------------------------------------+
+*/
+
+define("BACKEND_MENU_L1", " могат да бъдат публикувани от Вас чрез следните RSS формати.");
+define("BACKEND_MENU_L2", "Публикации от нас");
+
+define("BACKEND_MENU_L3", "Нашите новини");
+define("BACKEND_MENU_L4", "Нашите коментари");
+define("BACKEND_MENU_L5", "Нашите теми от форума");
+define("BACKEND_MENU_L6", "Нашите отговори от форума");
+
+define("BACKEND_MENU_L7", "Нашите съобщения от чата");
+define("BACKEND_MENU_L8", "Нашите докладвани грешки");
+define("BACKEND_MENU_L9", "Нашите файлове");
+
+define("RSS_LAN01", "Позволи разделянето на RSS източника за всяка категория в новини?");
+define("RSS_LAN02", "Позволи разделянето на RSS източника за всяка категория във Файлове?");
+?>
