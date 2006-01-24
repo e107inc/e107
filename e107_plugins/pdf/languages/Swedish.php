e10e829ffd715f4da586edb74ca2225caddeeb7d
mrpiercer <mrpiercer@localhost>
2006-01-24 13:23:50 +0000

    v0.7.1 RELEASE
    svn path=/trunk/e107_langpacks/; revision=6031

---
+++ e107_plugins/pdf/languages/Swedish.php
@@ -0,0 +1,39 @@
+<?php
+/*
++ -----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/pdf/languages/Swedish.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-01-24 13:23:50 $
+|     $Author: mrpiercer $
++-----------------------------------------------------------------------------+
+*/
+
+define("PDF_PLUGIN_LAN_1", "PDF");
+define("PDF_PLUGIN_LAN_2", "Skapande av PDF");
+define("PDF_PLUGIN_LAN_3", "PDF");
+define("PDF_PLUGIN_LAN_4", "Denna plugin är nu redo att användas.");
+
+define("PDF_LAN_1", "PDF");
+define("PDF_LAN_2", "PDF preferenser");
+define("PDF_LAN_3", "aktiverad");
+define("PDF_LAN_4", "avaktiverad");
+define("PDF_LAN_5", "Sidomarginal vänster");
+define("PDF_LAN_6", "Sidomarginal höger");
+define("PDF_LAN_7", "Sidomarginal topp");
+define("PDF_LAN_8", "Fontfamilj");
+define("PDF_LAN_9", "Standard fontstorlek");
+define("PDF_LAN_10", "Fontstorlek sajtenamn");
+define("PDF_LAN_11", "Fontstorlek sidourl");
+define("PDF_LAN_12", "Fontstorlek sidnummer");
+define("PDF_LAN_13", "Visa logo på pdf?");
+define("PDF_LAN_14", "Visa sajtnamn på pdf?");
+define("PDF_LAN_15", "Visa skapares sidourl på pdf?");
+define("PDF_LAN_16", "Visa sidonummer på pdf?");
+define("PDF_LAN_17", "Uppdatera");
+define("PDF_LAN_18", "PDF preferenser sparades");
+define("PDF_LAN_19", "Sida");
+define("PDF_LAN_20", "Felrapportering");
+
+?>
