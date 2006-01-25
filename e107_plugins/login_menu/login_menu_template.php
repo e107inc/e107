7a3be1a5e2909be010f6ac4e5a7fae1c19a49f7a
e107coders <e107coders@localhost>
2006-01-25 20:17:09 +0000

    Added DIZ
    svn path=/trunk/e107_0.7/; revision=6043

--- e107_plugins/login_menu/login_menu_template.php
+++ e107_plugins/login_menu/login_menu_template.php
@@ -1,5 +1,21 @@
 <?php
-
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/login_menu_template.php,v $
+|     $Revision: 1.3 $
+|     $Date: 2006-01-25 20:17:09 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
 $sc_style['LM_SIGNUP_LINK']['pre'] = "<br />[ ";
 $sc_style['LM_SIGNUP_LINK']['post'] = " ]";
 if (!isset($LOGIN_MENU_FORM)){
