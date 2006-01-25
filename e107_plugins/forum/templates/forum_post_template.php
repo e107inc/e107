9a10731d30a48772e2a45e74181c6827eebe538d
mcfly_e107 <mcfly_e107@localhost>
2006-01-25 20:31:07 +0000

    Fully templated forum_post
    svn path=/trunk/e107_0.7/; revision=6044

--- e107_plugins/forum/templates/forum_post_template.php
+++ e107_plugins/forum/templates/forum_post_template.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/forum/templates/forum_post_template.php,v $
-|     $Revision: 1.14 $
-|     $Date: 2005-12-21 17:03:51 $
-|     $Author: sweetas $
+|     $Revision: 1.15 $
+|     $Date: 2006-01-25 20:31:07 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 
@@ -21,29 +21,39 @@ if (!defined('e107_INIT')) { exit; }
 
 // the user box and subject box are not always displayed, therefore we need to define them /in case/ they are, if not they'll be ignored.
 
+if(!$userbox)
+{
 $userbox = "<tr>
 <td class='forumheader2' style='width:20%'>".LAN_61."</td>
 <td class='forumheader2' style='width:80%'>
 <input class='tbox' type='text' name='anonname' size='71' value='".$anonname."' maxlength='20' style='width:95%' />
 </td>
 </tr>";
+}
 
+if(!$subjectbox)
+{
 $subjectbox = "<tr>
 <td class='forumheader2' style='width:20%'>".LAN_62."</td>
 <td class='forumheader2' style='width:80%'>
 <input class='tbox' type='text' name='subject' size='71' value='".$subject."' maxlength='100' style='width:95%' />
 </td>
 </tr>";
-
-// the poll is optional, be careful when changing the values here, only change if you know what you're doing ...
-if(is_readable(e_PLUGIN."poll/poll_class.php")) {
-	require_once(e_PLUGIN."poll/poll_class.php");
-	$pollo = new poll;
-	$poll = $pollo -> renderPollForm("forum");
 }
 
+// the poll is optional, be careful when changing the values here, only change if you know what you're doing ...
+if(!$poll)
+{
+	if(is_readable(e_PLUGIN."poll/poll_class.php")) {
+		require_once(e_PLUGIN."poll/poll_class.php");
+		$pollo = new poll;
+		$poll = $pollo -> renderPollForm("forum");
+	}
+}
 
 // finally, file attach is optional, again only change this if you know what you're doing ...
+if(!$fileattach)
+{
 $fileattach = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post'] ? LAN_390 : LAN_416)."</td></tr>
 <tr><td style='width:20%' class='forumheader3'>".LAN_392."</td>
 <td style='width:80%' class='forumheader3'>".LAN_393." | ".$allowed_filetypes." |<br />".LAN_394."<br />".LAN_395.": ".($pref['upload_maxfilesize'] ? $pref['upload_maxfilesize'].LAN_396 : ini_get('upload_max_filesize'))."
@@ -58,12 +68,16 @@ $fileattach = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post']
 </td>
 </tr>
 ";
-
+}
 // If the upload directory is not writable, we need to alert the user about this.
-$fileattach_alert = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post'] ? LAN_390 : LAN_416)."</td></tr><tr><td colspan='2' class='forumheader3'>".LAN_FORUM_1."</td></tr>\n";
-
+if(!$fileattach_alert)
+{
+	$fileattach_alert = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post'] ? LAN_390 : LAN_416)."</td></tr><tr><td colspan='2' class='forumheader3'>".LAN_FORUM_1."</td></tr>\n";
+}
 // ------------
 
+if(!$FORUMPOST)
+{
 $FORUMPOST = "
 <div style='text-align:center'>
 <div class='spacer'>
@@ -101,5 +115,95 @@ $FORUMPOST = "
 </table>
 </div></div>
 ";
+}
+
+if(!$FORUMPOST_REPLY)
+{
+$FORUMPOST_REPLY = "
+<div style='text-align:center'>
+<div class='spacer'>
+{FORMSTART}
+<table style='width:95%' class='fborder'>
+<tr>
+<td colspan='2' class='fcaption'>{BACKLINK}
+</td>
+</tr>
+{USERBOX}
+{SUBJECTBOX}
+<tr>
+<td class='forumheader2' style='width:20%'>{POSTTYPE}</td>
+<td class='forumheader2' style='width:80%'>
+{POSTBOX}<br />{EMOTES}<br />{EMAILNOTIFY}<br />{POSTTHREADAS}
+</td>
+</tr>
+
+{POLL}
+
+{FILEATTACH}
+
+<tr style='vertical-align:top'>
+<td colspan='2' class='forumheader' style='text-align:center'>
+{BUTTONS}
+</table>
+{FORMEND}
+
+<table style='width:95%'>
+<tr>
+<td>
+{FORUMJUMP}
+</td>
+</tr>
+</table>
+</div></div>
+<div style='text-align:center'>
+{THREADTOPIC}
+{LATESTPOSTS}
+</div>
+";
+}
+
+if(!$LASTFORUMPOSTS_START)
+{
+$LATESTPOSTS_START = "
+<table style='width:100%' class='fborder'>
+<tr>
+<td colspan='2' class='fcaption' style='vertical-align:top'>".
+LAN_101."{LATESTPOSTSCOUNT}".LAN_102."
+</td>
+</tr>";
+}
+
+if(!$LATESTPOSTS_POST)
+{
+$LATESTPOSTS_POST = "
+<tr>
+<td class='forumheader3' style='width:20%' style='vertical-align:top'><b>{POSTER}</b></td>
+<td class='forumheader3' style='width:80%'>
+<div class='smallblacktext' style='text-align:right'>".IMAGE_post2." ".LAN_322."{THREADDATESTAMP}</div>{POST}</td>
+</tr>
+";
+}
+
+if(!$LATESTPOSTS_END)
+{
+$LATESTPOSTS_END = "
+</table>
+";
+}
+
+if(!$THREADTOPIC_REPLY)
+{
+$THREADTOPIC_REPLY = "
+<table style='width:100%' class='fborder'>
+<tr>
+<td colspan='2' class='fcaption' style='vertical-align:top'>".LAN_100."</td></tr>
+<tr>
+<td class='forumheader3' style='width:20%' style='vertical-align:top'><b>{POSTER}</b></td>
+<td class='forumheader3' style='width:80%'>
+<div class='smallblacktext' style='text-align:right'>".IMAGE_post2." ".LAN_322."{THREADDATESTAMP}</div>{POST}</td>
+</tr>
+</table>
+";
+}
 
 ?>
