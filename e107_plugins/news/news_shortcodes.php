f3040389b0947f10727188c7e8330fb6308f2886
streaky <streaky@localhost>
2006-01-28 08:05:40 +0000

    Fixed a minor error with shortcode handling
    svn path=/branches/news_rewrite/e107_0.7/; revision=6062

--- e107_plugins/news/news_shortcodes.php
+++ e107_plugins/news/news_shortcodes.php
@@ -2,7 +2,7 @@
 
 if (!defined('e107_INIT')) { exit; }
 include_once(e_HANDLER.'shortcode_handler.php');
-$forum_shortcodes = $tp->e_sc->parse_scbatch(__FILE__);
+$news_shortcodes = $tp->e_sc->parse_scbatch(__FILE__);
 /*
 NEWS_POSTS
 return $news_posts;
