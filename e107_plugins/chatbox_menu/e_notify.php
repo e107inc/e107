3155be6b574030b7851ce07a76c195218522e4e9
mcfly_e107 <mcfly_e107@localhost>
2006-02-02 06:37:52 +0000

    No longer loading lan files unless necessary
    svn path=/trunk/e107_0.7/; revision=6077

--- e107_plugins/chatbox_menu/e_notify.php
+++ e107_plugins/chatbox_menu/e_notify.php
@@ -11,25 +11,26 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/chatbox_menu/e_notify.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2005-12-14 19:28:43 $
-|     $Author: sweetas $
+|     $Revision: 1.7 $
+|     $Date: 2006-02-02 06:37:52 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-if (defined("e_LANGUAGE") && is_readable(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php")) {
-	include_once(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php");
-} else {
-	include_once(e_PLUGIN."chatbox_menu/languages/English/English.php");
+if(ADMIN_PAGE === true)
+{
+	include_lan(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php");
+	$config_category = NT_LAN_CB_1;
+	$config_events = array('cboxpost' => NT_LAN_CB_2);
 }
-$config_category = NT_LAN_CB_1;
-$config_events = array('cboxpost' => NT_LAN_CB_2);
+
 
 if (!function_exists('notify_cboxpost')) {
 	function notify_cboxpost($data) {
 		global $nt;
+		include_lan(e_PLUGIN."chatbox_menu/languages/".e_LANGUAGE."/".e_LANGUAGE.".php");
 		$message = NT_LAN_CB_3.': '.USERNAME.' ('.NT_LAN_CB_4.': '.$data['ip'].' )<br />';
 		$message .= NT_LAN_CB_5.':<br />'.$data['cmessage'].'<br /><br />';
 		$nt -> send('cboxpost', NT_LAN_CB_6, $message);
