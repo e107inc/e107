1b168e1a84cd7be4a13fe3ceb9813c4826b0a2a9
whoisrich <whoisrich@localhost>
2006-02-15 15:00:07 +0000

    Added a linked message at the top pointing to the File Inspector.
    svn path=/trunk/e107_0.7/; revision=6147

--- e107_plugins/integrity_check/admin_integrity_check.php
+++ e107_plugins/integrity_check/admin_integrity_check.php
@@ -392,7 +392,10 @@ unset($t_array);
 $text = "<div style='text-align:center'>
 	<form method='post' action='".e_SELF."?".e_QUERY."' name='integrity_check'>
 	<table style='width:95%'>
-	 
+
+	<tr>
+	<td class='fcaption' colspan='2'><br /><a href='".e_ADMIN."fileinspector.php'>".Integ_27."</a><br /><br />
+	</td>	 
 	<tr>
 	<td class='fcaption' colspan='2'>".Integ_06."
 	</td>
