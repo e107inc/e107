85f3c14f748c8a5d71507e745ec493f76d46ecc8
sweetas <sweetas@localhost>
2006-02-18 18:47:35 +0000

    matchall forum pages as search referrer pages
    svn path=/trunk/e107_0.7/; revision=6181

--- e107_plugins/forum/e_search.php
+++ e107_plugins/forum/e_search.php
@@ -9,7 +9,7 @@ if (file_exists(e_PLUGIN."forum/languages/".e_LANGUAGE."/lan_forum_search.php"))
 $search_info[] = array(
 	'sfile' => e_PLUGIN.'forum/search/search_parser.php', 
 	'qtype' => FOR_SCH_LAN_1, 
-	'refpage' => 'forum.php', 
+	'refpage' => 'forum', 
 	'advanced' => e_PLUGIN.'forum/search/search_advanced.php', 
 	'id' => 'forum'
 	);
