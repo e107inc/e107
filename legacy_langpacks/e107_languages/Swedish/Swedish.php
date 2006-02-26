8cf5ec04bf114e3d3d46df6d8a63a53ba25a9130
mrpiercer <mrpiercer@localhost>
2006-02-26 12:20:28 +0000

    Bugfix
    svn path=/trunk/e107_langpacks/; revision=6217

--- legacy_langpacks/e107_languages/Swedish/Swedish.php
+++ legacy_langpacks/e107_languages/Swedish/Swedish.php
@@ -4,15 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_languages/Swedish/Swedish.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2006-02-25 13:24:52 $
+|     $Revision: 1.6 $
+|     $Date: 2006-02-26 12:20:28 $
 |     $Author: mrpiercer $
 +----------------------------------------------------------------------------+
 */
-setlocale(LC_ALL, 'sv_SE.UTF-8', 'swedish', 'sve_swe');
+setlocale(LC_ALL, 'sv_SE', 'swedish', 'sve_sv');
 define("CORE_LC", 'sv');
 define("CORE_LC2", 'se');
-define("CHARSET", "utf-8");  // F&ouml;r en &auml;kta flerspr&aring;kig sajt. :)
+define("CHARSET", "iso8859-1");  // F&ouml;r en &auml;kta flerspr&aring;kig sajt. :)
 define("CORE_LAN1","Fel: Tema saknas.\\n\\nByt det anv&auml;nda temat i dina preferenser (admin area) eller ladda upp filerna f&ouml;r det aktuella temat till din server.");
 
 //v.616
