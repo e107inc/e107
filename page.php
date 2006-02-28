4842319c7c55ff8aac794b8d31ea9dd49225ce7c
whoisrich <whoisrich@localhost>
2006-02-28 22:23:26 +0000

    bug #2493 and more relating to the page.php and its admin area, words are no longer hardcoded, and comment_class improvements.
    svn path=/trunk/e107_0.7/; revision=6230

--- page.php
+++ page.php
@@ -11,66 +11,58 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/page.php,v $
-|     $Revision: 1.21 $
-|     $Date: 2006-01-11 03:35:37 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.22 $
+|     $Date: 2006-02-28 22:23:26 $
+|     $Author: whoisrich $
 +----------------------------------------------------------------------------+
 */
+
 require_once("class2.php");
+
+require_once(HEADERF);
+
 $page = new pageClass();
-if(isset($_POST['enterpw']))
-{
-	$page -> setPageCookie();
-}
 
 if(!e_QUERY)
 {
-	require_once(HEADERF);
-	$tmp = $page -> listPages();
-	if(is_array($tmp))
-	{
-		$ns -> tablerender($tmp['title'], $tmp['text']);
-		require_once(FOOTERF);
-		exit;
-	}
+	$page -> listPages();
 }
 else
 {
-	$tmp = $page -> showPage();
-	define("e_PAGETITLE", $tmp['title']);
-	require_once(HEADERF);
-	$ns -> tablerender($tmp['title'], $tmp['text']);
-	if($tmp['comment']){
-		$ns -> tablerender($tmp['comment_caption'], $tmp['comment']);
-    }
+	$pagedata = $page -> showPage();
+	$ns -> tablerender($pagedata['title'], $pagedata['text']);
+
+	if($pagedata['comment'])
+	{
+		echo $pagedata['comment'];
+	}
 }
+
 require_once(FOOTERF);
 
-
-/* EOF */
+//---------------------------------------------------------------------------+
 
 class pageClass
 {
-
-	var $bullet;																/* bullet image */
-	var $pageText;														/* main text of selected page, not parsed */
-	var $multipageFlag;												/* flag - true if multiple page page, false if not */
-	var $pageTitles;														/* array containing page titles */
-	var $pageID;															/* id number of page to be displayed */
-	var $pageSelected;												/* selected page of multiple page page */
-	var $pageToRender;												/* parsed page to be sent to screen */
-	var $debug;															/* temp debug flag */
-	var $title;																/* title of page, it if has one (as defined in [newpage=title] tag */
+	var $bullet;						/* bullet image */
+	var $pageText;						/* main text of selected page, not parsed */
+	var $multipageFlag;					/* flag - true if multiple page page, false if not */
+	var $pageTitles;					/* array containing page titles */
+	var $pageID;						/* id number of page to be displayed */
+	var $pageSelected;					/* selected page of multiple page page */
+	var $pageToRender;					/* parsed page to be sent to screen */
+	var $debug;							/* temp debug flag */
+	var $title;							/* title of page, it if has one (as defined in [newpage=title] tag */
 
 
 	function pageClass($debug=FALSE)
 	{
-		global $e_QUERY;
 		/* constructor */
-		$this -> bullet = (defined("BULLET") ? "<img src='".THEME."images/".BULLET."' alt='' style='vertical-align: middle;' />" : "<img src='".THEME."images/bullet2.gif' alt='bullet' style='vertical-align: middle;' />");
+
 		$tmp = explode(".", e_QUERY);
 		$this -> pageID = intval($tmp[0]);
 		$this -> pageSelected = (isset($tmp[1]) ? intval($tmp[1]) : 0);
+		$this -> bullet = (defined("BULLET") ? "<img src='".THEME."images/".BULLET."' alt='' style='vertical-align: middle;' />" : "<img src='".THEME."images/bullet2.gif' alt='bullet' style='vertical-align: middle;' />");
 		$this -> debug = $debug;
 
 		if($this -> debug)
@@ -86,13 +78,13 @@ class pageClass
 
 		if(!$pref['listPages'])
 		{
-			return $this -> pageError(1);
+			message_handler("MESSAGE", LAN_PAGE_1);
 		}
 		else
 		{
 			if(!$sql -> db_Select("page", "*", "page_theme='' AND page_class IN (".USERCLASS_LIST.") "))
 			{
-				$text = "No custom pages yet.";
+				$text = LAN_PAGE_2;
 			}
 			else
 			{
@@ -102,59 +94,31 @@ class pageClass
 					extract($page);
 					$text .= $this -> bullet." <a href='".e_BASE."page.php?".$page_id."'>".$page_title."</a><br />";
 				}
-				$ns -> tablerender("Pages", $text);
+				$ns -> tablerender(LAN_PAGE_11, $text);
 			}
 		}
-
 	}
-
-	function pageError($val)
-	{
-		global $ns;
-		$text = "<div style='text-align:center; margin-left:auto; margin-right: auto;'>
-		";
-		switch ($val)
-		{
-			case 1:
-			$text .= "No page selected.";
-			break;
-			case 2:
-			$text .= "Invalid page.";
-			break;
-			case 3:
-			$text .= "You do not have the correct permissions to view this page.";
-			break;
-		}
-		$text .= "</div>
-		";
-		return array('title' => 'error', 'text' => $text);
-	}
-
 
 	function showPage()
 	{
 		global $sql, $ns;
-		$query = "SELECT p.*, u.user_id, u.user_name FROM #page AS p
-		LEFT JOIN #user AS u ON p.page_author = u.user_id
-		WHERE p.page_id='".intval($this -> pageID)."' AND p.page_class IN (".USERCLASS_LIST.") ";
+
+		$query = "	SELECT p.*, u.user_id, u.user_name FROM #page AS p
+					LEFT JOIN #user AS u ON p.page_author = u.user_id
+					WHERE p.page_id='".$this -> pageID."'";
 
 		if(!$sql -> db_Select_gen($query) && !$_GET['elan'])
 		{
-			return $this -> pageError(2);
+			message_handler("MESSAGE", LAN_PAGE_3);
+			require_once(FOOTERF); exit;
 		}
 
 		extract($sql -> db_Fetch());
-
-		if($page_password)
-		{
-			if(!$this -> pageCheckPerms($page_password))
-			{
-				return FALSE;
-			}
-		}
-
-
+		
 		$this -> pageText = $page_text;
+
+		$this -> pageCheckPerms($page_class, $page_password);
+
 		if($this -> debug)
 		{
 			echo "<b>pageText</b> ".$this -> pageText." <br />";
@@ -163,23 +127,25 @@ class pageClass
 		$this -> parsePage();
 
 		$gen = new convert;
+
 		if($page_author)
 		{
-			$text = "<span class='smalltext'>by ".$user_name.", ".$gen->convert_date($page_datestamp, "long")."</span><br /><br />";
+			$text = "<div class='smalltext' style='text-align:right'>".$user_name.", ".$gen->convert_date($page_datestamp, "long")."</div><br />";
 		}
+		
 		if($this -> title)
 		{
 			$text .= "<b>".$this -> title."</b><br /><br />";
 		}
-		$text .= $this -> pageToRender;
-		$text .= $this -> pageRating($page_rating_flag);
-		$text .= $this -> pageIndex();
 
-		$ret['title'] = $page_title;
-		$ret['text'] = $text;
-		$comment = $this -> pageComment($page_comment_flag);
+		$text .= $this -> pageToRender;
+		$text .= $this -> pageIndex();
+		$text .= $this -> pageRating($page_rating_flag);
+
+		$ret['title']   = $page_title;
+		$ret['text']    = $text;
+		$comment        = $this -> pageComment($page_comment_flag);
 		$ret['comment'] = $comment['comment'].$comment['comment_form'];
-		$ret['comment_caption'] = $comment['caption'];
 
 	 	return $ret;
 	}
@@ -237,8 +203,6 @@ class pageClass
 		$this -> pageToRender = $tp -> toHTML($pages[$this -> pageSelected], TRUE, 'parse_sc, constants');
 		$this -> title = (substr($this -> pageTitles[$this -> pageSelected], -1) == ";" ? "" : $this -> pageTitles[$this -> pageSelected]);
 
-
-
 		if($this -> debug)
 		{
 			echo "<b>multipageFlag</b> ".$this -> multipageFlag." <br />";
@@ -250,7 +214,6 @@ class pageClass
 				echo "<pre>"; print_r($this -> pageTitles); echo "</pre>";
 			}
 		}
-
 	}
 
 	function pageIndex()
@@ -265,17 +228,14 @@ class pageClass
 		return $itext;
 	}
 
-
 	function pageRating($page_rating_flag)
 	{
 		if($page_rating_flag)
 		{
 			require_once(e_HANDLER."rate_class.php");
 			$rater = new rater;
-			$rate_text = "<br /><br />
-				<table style='width:100%'>
-				<tr>
-				<td style='width:50%'>";
+			$rate_text = "<br /><table style='width:100%'><tr><td style='width:50%'>";
+			
 			if ($ratearray = $rater->getrating("page", $this -> pageID))
 			{
 				if ($ratearray[2] == "")
@@ -288,27 +248,27 @@ class pageClass
 			}
 			else
 			{
-				$rating .= LAN_dl_13;
+				$rating .= LAN_PAGE_dl_13;
 			}
 			$rate_text .= "</td><td style='width:50%; text-align:right'>";
 
 			if (!$rater->checkrated("page", $this -> pageID) && USER) {
-				$rate_text .= $rater->rateselect("&nbsp;&nbsp;&nbsp;&nbsp; <b>Rate this page</b>", "page", $this -> pageID);
+				$rate_text .= $rater->rateselect("&nbsp;&nbsp;&nbsp;&nbsp; <b>".LAN_PAGE_4."</b>", "page", $this -> pageID);
 			}
 			else if(!USER) {
 				$rate_text .= "&nbsp;";
 			} else {
-				$rate_text .= "thankyou for rating this page";
+				$rate_text .= LAN_PAGE_5;
 			}
 			$rate_text .= "</td></tr></table>";
 		}
 		return $rate_text;
 	}
 
-
 	function pageComment($page_comment_flag)
 	{
 		global $sql, $ns, $e107cache, $tp, $comment_shortcodes;
+
 		if($page_comment_flag)
 		{
 			require_once(e_HANDLER."comment_class.php");
@@ -316,66 +276,73 @@ class pageClass
 
 			if (isset($_POST['commentsubmit']))
 			{
-				if ($sql->db_Select("page", "page_comment_flag", "page_id='".intval($this -> pageID)."' "))
-				{
-					$row = $sql->db_Fetch();
-					if ($row[0] && (ANON === TRUE || USER === TRUE)) {
-
-						$clean_authorname = $_POST['author_name'];
-						$clean_comment = $_POST['comment'];
-						$clean_subject = $_POST['subject'];
-
-					 	$cobj->enter_comment($clean_authorname, $clean_comment, "page", $this -> pageID, $pid, $clean_subject);
-						$e107cache->clear("comment.page.".$this -> pageID);
-					}
-				}
+				$cobj->enter_comment($_POST['author_name'], $_POST['comment'], "page", $this -> pageID, $pid, $_POST['subject']);
+				$e107cache->clear("comment.page.".$this -> pageID);
 			}
-			return $cobj->compose_comment("page", "comment", $this -> pageID, $width, $subject, $showrate=FALSE,$return=TRUE);
+
+			return $cobj->compose_comment("page", "comment", $this -> pageID, $width="", $subject="", $showrate=FALSE, $return=TRUE);
 		}
 	}
 
-	function pageCheckPerms($page_password)
+	function pageCheckPerms($page_class, $page_password)
 	{
 		global $ns, $HEADER, $FOOTER, $sql;
-		$cookiename = "e107page_".$this -> pageID;
-		if(isset($_COOKIE[$cookiename]))
+		
+		if (!check_class($page_class))
 		{
+			message_handler("MESSAGE", LAN_PAGE_6);
+			require_once(FOOTERF); exit;
+		}
+		
+		if (!$page_password)
+		{
+			return TRUE;
+		}
 
-			if($_COOKIE[$cookiename] != md5($page_password.USERID))
+		if($_POST['submit_page_pw'])
+		{
+			if($_POST['page_pw'] == $page_password)
 			{
-				return $this -> pageError(3);
+				$this -> setPageCookie();
 			}
-			else
+		}
+		else
+		{
+			$cookiename = "e107page_".$this -> pageID;
+
+			if($_COOKIE[$cookiename] == md5($page_password.USERID))
 			{
 				return TRUE;
 			}
-		} else {
-			$HEADER = ""; $FOOTER = "";
-			require_once(HEADERF);
-			$text = "
-			<div style='text-align:center; margin-left:auto; margin-right: auto;'>
+		}
+
+		if ($_POST['submit_page_pw'])
+		{
+			message_handler("MESSAGE", LAN_PAGE_7);
+		}	
+
+		$text = "
+			<div style='text-align:center; margin-left:auto; margin-right:auto;'>
 			<form method='post' action='".e_SELF."?".e_QUERY."' id='pwform'>
-			<table style='width:50%;' class='fborder'>
+			<table style='width:100%;' class='fborder'>
 			<tr>
-			<td class='forumheader' style='text-align:center;' colspan='3'>This page is password protected - please enter password to continue</td>
+			<td class='forumheader' colspan='3' style='text-align:center; white-space:nowrap'>".LAN_PAGE_8."</td>
 			</tr>
 			<tr>
-			<td class='forumheader3' style='width: 20%;'>Password:</td>
-			<td class='forumheader3' style='width: 60%;'><input type='password' id='page_pw' name='page_pw' style='width: 90%;'/></td>
-			<td class='forumheader3' width='20%' style='vertical-align: middle; margin-left: auto; margin-right: auto; text-align: center;'><img src='".e_IMAGE."generic/".IMODE."/password.png' alt='' /></td>
+			<td class='forumheader3' style='width:20%;'>".LAN_PAGE_9.":</td>
+			<td class='forumheader3' style='width:60%;'><input type='password' id='page_pw' name='page_pw' style='width: 90%;'/></td>
+			<td class='forumheader3' style='width:20%; vertical-align:middle; margin-left:auto; margin-right:auto; text-align:center;'><img src='".e_IMAGE."generic/".IMODE."/password.png' alt='' /></td>
 			</tr>
 			<tr>
-			<td class='forumheader' style='text-align:center;' colspan='3'><input class='button' type='submit' name='enterpw' value='Submit' /></td>
+			<td class='forumheader' colspan='3' style='text-align:center;'><input class='button' type='submit' name='submit_page_pw' value='".LAN_PAGE_10."' /></td>
 			</tr>
 			</table>
 			</form>
 			</div>
 			";
-			$ns->tablerender("&nbsp;", $text);
-
-			require_once(FOOTERF);
-			exit;
-		}
+	
+		$ns->tablerender("&nbsp;", $text);
+		require_once(FOOTERF); exit;
 	}
 
 	function setPageCookie()
@@ -387,8 +354,6 @@ class pageClass
 		header("location:".e_SELF."?".e_QUERY);
 		exit;
 	}
-
 }
-
 
 ?>
