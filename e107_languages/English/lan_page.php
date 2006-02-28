9d60bc1518217ac475946be1f090d222aed89f70
whoisrich <whoisrich@localhost>
2006-02-28 23:02:59 +0000

    language file for page.php
    svn path=/trunk/e107_0.7/; revision=6231

---
+++ e107_languages/English/lan_page.php
@@ -0,0 +1,25 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_page.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-02-28 23:02:59 $
+|     $Author: whoisrich $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_PAGE_1", "Page listing is turned off");
+define("LAN_PAGE_2", "There are no pages");
+define("LAN_PAGE_3", "Requested page does not exist");
+define("LAN_PAGE_4", "Rate this page");
+define("LAN_PAGE_5", "Thankyou for rating this page");
+define("LAN_PAGE_6", "You do not have permission to view this page");
+define("LAN_PAGE_7", "Incorrect password");
+define("LAN_PAGE_8", "Password Protected Page");
+define("LAN_PAGE_9", "Password");
+define("LAN_PAGE_10", "Submit");
+define("LAN_PAGE_11", "Page List");
+
+?>
