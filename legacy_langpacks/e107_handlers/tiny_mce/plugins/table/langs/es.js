035d6a3d25e1aeec8455e3c2422e0e060460417e
natxocc <natxocc@localhost>
2006-03-01 21:41:55 +0000

    News Updates
    svn path=/trunk/e107_langpacks/; revision=6234

--- legacy_langpacks/e107_handlers/tiny_mce/plugins/table/langs/es.js
+++ legacy_langpacks/e107_handlers/tiny_mce/plugins/table/langs/es.js
@@ -3,48 +3,88 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/legacy_langpacks/e107_handlers/tiny_mce/plugins/table/langs/es.js,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-11-11 23:57:58 $
+|     $Revision: 1.2 $
+|     $Date: 2006-03-01 21:41:55 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
 
-tinyMCELang['lang_table_desc'] = 'Insertar una nueva tabla';
-tinyMCELang['lang_table_insert_row_before_desc'] = 'Insertar una fila antes';
-tinyMCELang['lang_table_insert_row_after_desc'] = 'Insertar una fila después';
-tinyMCELang['lang_table_delete_row_desc'] = 'Eliminar una fila';
-tinyMCELang['lang_table_insert_col_before_desc'] = 'Insertar una columna antes';
-tinyMCELang['lang_table_insert_col_after_desc'] = 'Insertar columna después';
-tinyMCELang['lang_table_delete_col_desc'] = 'Eliminar una columna';
-tinyMCELang['lang_insert_table_title'] = 'Insertar/Modificar tabla';
-tinyMCELang['lang_insert_table_width'] = 'Anchura';
-tinyMCELang['lang_insert_table_height'] = 'Altura';
-tinyMCELang['lang_insert_table_cols'] = 'Columnas';
-tinyMCELang['lang_insert_table_rows'] = 'Filas';
-tinyMCELang['lang_insert_table_cellspacing'] = 'Espacio entre celdas';
-tinyMCELang['lang_insert_table_cellpadding'] = 'Desplazamiento entre celdas';
-tinyMCELang['lang_insert_table_border'] = 'Borde';
-tinyMCELang['lang_insert_table_align'] = 'Alineamiento';
-tinyMCELang['lang_insert_table_align_default'] = 'Por defecto';
-tinyMCELang['lang_insert_table_align_left'] = 'Izquierda';
-tinyMCELang['lang_insert_table_align_right'] = 'Derecha';
-tinyMCELang['lang_insert_table_align_middle'] = 'Centro';
-tinyMCELang['lang_insert_table_class'] = 'Clase';
-tinyMCELang['lang_table_row_title'] = 'Propiedades de la fila';
-tinyMCELang['lang_table_cell_title'] = 'Propiedades de la celda';
-tinyMCELang['lang_table_row_desc'] = 'Propiedades de la fila';
-tinyMCELang['lang_table_cell_desc'] = 'Propiedades de la celda';
-tinyMCELang['lang_insert_table_valign'] = 'Alinear verticalmente';
-tinyMCELang['lang_insert_table_align_top'] = 'Arriba';
-tinyMCELang['lang_insert_table_align_bottom'] = 'Abajo';
-tinyMCELang['lang_table_props_desc'] = 'Propiedades de la tabla';
-tinyMCELang['lang_table_bordercolor'] = 'Color del borde';
-tinyMCELang['lang_table_bgcolor'] = 'Color de fondo';
-tinyMCELang['lang_table_merge_cells_title'] = 'Unir celdas';
-tinyMCELang['lang_table_split_cells_desc'] = 'Separar celdas';
-tinyMCELang['lang_table_merge_cells_desc'] = 'Unir celdas';
-tinyMCELang['lang_table_cut_row_desc'] = 'Cortar fila';
-tinyMCELang['lang_table_copy_row_desc'] = 'Copiar fila';
-tinyMCELang['lang_table_paste_row_before_desc'] = 'Pegar fila antes';
-tinyMCELang['lang_table_paste_row_after_desc'] = 'Pegar fila después';
-tinyMCELang['lang_table_insert_desc'] = 'Insertar nueva tabla';
+tinyMCE.addToLang('table',{
+general_tab : 'General',
+advanced_tab : 'Avanzado',
+general_props : 'Propiedades generales',
+advanced_props : 'Propiedades avanzadas',
+desc : 'Insertar/Modificar tabla',
+row_before_desc : 'Insertar una fila antes',
+row_after_desc : 'Insert una fila después',
+delete_row_desc : 'Eliminar fila',
+col_before_desc : 'Insertar una columna antes',
+col_after_desc : 'Insertar una columna después',
+delete_col_desc : 'Eliminar columna',
+rowtype : 'File en parte de tabla',
+title : 'Insertar/Modificar tabla',
+width : 'Ancho',
+height : 'ALto',
+cols : 'Columnas',
+rows : 'Filas',
+cellspacing : 'Espacio entre celdas',
+cellpadding : 'Borde interior de las celdas',
+border : 'Borde',
+align : 'Alineación horizontal',
+align_default : 'Defecto',
+align_left : 'Izquierda',
+align_right : 'Derecha',
+align_middle : 'Centrado',
+row_title : 'Propiedades de la fila',
+cell_title : 'Propiedades de la celda',
+cell_type : 'Tipo de celda',
+row_desc : 'Propiedades de la fila',
+cell_desc : 'Propiedades de la celda',
+valign : 'Alineación vertical',
+align_top : 'Arriba',
+align_bottom : 'Abajo',
+props_desc : 'Propiedades de la tabla',
+bordercolor : 'Color del borde',
+bgcolor : 'Color del fondo',
+merge_cells_title : 'Combinar celdas',
+split_cells_desc : 'Dividir celdas',
+merge_cells_desc : 'Combinar celdas',
+cut_row_desc : 'Cortar fila',
+copy_row_desc : 'Copiar fila',
+paste_row_before_desc : 'Pegar fila antes',
+paste_row_after_desc : 'Pegar fila después',
+id : 'Id',
+style: 'Estilo',
+langdir : 'Dirección del texto',
+langcode : 'Código del idioma',
+mime : 'Tipo MIME del target',
+ltr : 'De izquierda a derecha',
+rtl : 'De derecha a izquierda',
+bgimage : 'Imagen de fondo',
+summary : 'Resumen',
+td : "Datos",
+th : "Cabecera",
+cell_cell : 'Actualizar la celda actual',
+cell_row : 'Actualizar todas las celdas de la fila',
+cell_all : 'Actualizar todas las celdas de la tabla',
+row_row : 'Actualizar la fila actual',
+row_odd : 'Actualizar las filas impares de la tabla',
+row_even : 'Actualizar las filas pares de la tabla',
+row_all : 'Actualizar todas las filas de la tabla',
+thead : 'Cabecera de la tabla',
+tbody : 'Cuerpo de la tabla',
+tfoot : 'Pie de la tabla',
+del : 'Eliminar tabla',
+scope : 'Alcance',
+row : 'Fila',
+col : 'Columna',
+rowgroup : 'Grupo de filas',
+colgroup : 'Grupo de columnas',
+missing_scope: '¿Estás seguro que quieres continuar sin especificar el alcance para esta celda de tipo cabecera? Sin ello, puede dificultar a algunos usuarios con discapacidades el poder entender el contenido o los datos mostrados en la tabla.',
+cellprops_delta_width : 55
+});
+tinyMCE.addToLang('',{
+class_name : ' Estilo',
+not_set : 'Sin definir',
+theme_colorpicker_title : 'Seleccionar color'
+});
