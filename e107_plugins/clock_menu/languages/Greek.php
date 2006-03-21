c82f2ba55651ab1b29a75598fe27ae567d8fd662
e107coders <e107coders@localhost>
2006-03-21 22:23:38 +0000

    Initial Greek translation.
    svn path=/trunk/e107_langpacks/; revision=6273

---
+++ e107_plugins/clock_menu/languages/Greek.php
@@ -0,0 +1,44 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/clock_menu/languages/Greek.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-03-21 22:23:38 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+	
+define('CLOCK_MENU_L1', 'Ρύθμιση ρολογιού αποθηκεύτηκε');
+define('CLOCK_MENU_L2', 'Λεζάντα');
+define('CLOCK_MENU_L3', 'Ενημέρωση Ρυθμίσεων Μενού');
+define('CLOCK_MENU_L4', 'Ρύθμιση Ρολογιού');
+define('CLOCK_MENU_L5', 'Δευτέρα,');
+define('CLOCK_MENU_L6', 'Τρίτη,');
+define('CLOCK_MENU_L7', 'Τετάρτη,');
+define('CLOCK_MENU_L8', 'Πέμπτη,');
+define('CLOCK_MENU_L9', 'Παρασκευή,');
+define('CLOCK_MENU_L10', 'Σάββατο,');
+define('CLOCK_MENU_L11', 'Κυριακή,');
+define('CLOCK_MENU_L12', 'Ιανουαρίου');
+define('CLOCK_MENU_L13', 'Φεβρουαρίου');
+define('CLOCK_MENU_L14', 'Μαρτίου');
+define('CLOCK_MENU_L15', 'Απριλίου');
+define('CLOCK_MENU_L16', 'Μαΐου');
+define('CLOCK_MENU_L17', 'Ιουνίου');
+define('CLOCK_MENU_L18', 'Ιουλίου');
+define('CLOCK_MENU_L19', 'Αυγούστου');
+define('CLOCK_MENU_L20', 'Σεπτεμβρίου');
+define('CLOCK_MENU_L21', 'Οκτωβρίου');
+define('CLOCK_MENU_L22', 'Νοεμβρίου');
+define('CLOCK_MENU_L23', 'Δεκεμβρίου');
+define('CLOCK_MENU_L24', '');
+?>
