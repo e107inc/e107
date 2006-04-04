b7f4398fe4f285f58eb4b922381e481a311361a3
mcfly_e107 <mcfly_e107@localhost>
2006-04-04 23:13:42 +0000

    Bugtracker #2609, I think this'll fix it
    svn path=/trunk/e107_0.7/; revision=6286

--- e107_languages/English/lan_prefs.php
+++ e107_languages/English/lan_prefs.php
@@ -1,12 +1,12 @@
-﻿<?php
+<?php
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_prefs.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-11-25 12:10:32 $
-|     $Author: streaky $
+|     $Revision: 1.3 $
+|     $Date: 2006-04-04 23:13:42 $
+|     $Author: mcfly_e107 $
 +----------------------------------------------------------------------------+
 */
 
