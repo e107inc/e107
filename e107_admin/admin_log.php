b586c10dd536a13a1fe22d9a9eaaa1b46d8bd2bb
e107coders <e107coders@localhost>
2006-04-13 14:58:21 +0000

    Added Next/Prev and display of username when applicable.
    svn path=/trunk/e107_0.7/; revision=6333

--- e107_admin/admin_log.php
+++ e107_admin/admin_log.php
@@ -11,27 +11,32 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/admin_log.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2006-02-13 02:03:57 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.3 $
+|     $Date: 2006-04-13 14:58:21 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 require_once("../class2.php");
 
 require_once("auth.php");
 $text = "";
+$amount = 30;
+$from = ($_GET['fm']) ? intval($_GET['fm']) : 0;
 
 if(e_QUERY == 'purge')
 {
 	$sql->db_Delete('dblog');
 }
 
-$sql -> db_Select("dblog", "*", "ORDER BY `dblog_datestamp` DESC", "no_where");
+$total = $sql -> db_Select("dblog", "*", "ORDER BY `dblog_datestamp` DESC", "no_where");
+$query = "SELECT l.*, u.user_name FROM #dblog AS l LEFT JOIN #user AS u ON l.dblog_user_id = u.user_id  ORDER BY l.dblog_datestamp DESC LIMIT $from,$amount";
+$sql -> db_Select_gen($query);
 
 if(!is_object($gen)) {
 	$gen = new convert;
 }
-
+	$parms = $total.",".$amount.",".$from.",".e_SELF.'?fm=[FROM]';
+	$text .= "<div style='text-align:center'><br />".$tp->parseTemplate("{NEXTPREV={$parms}}")."<br /><br /></div>";
 $text .= "<div id='admin_log'><table>\n";
 
 //
@@ -55,11 +60,13 @@ while ($row = $sql -> db_Fetch()) {
 	$text .= "    <td>{$row['dblog_query']}</td>\n";
 	$text .= "    <td>{$row['dblog_remarks']}</td>\n";
 	$text .= "    <td>{$row['dblog_ip']}</td>\n";
-	$text .= "    <td>{$row['dblog_user_id']}</td>\n";
+	$text .= ($row['user_name']) ? "    <td><a href='".e_BASE."user.php?id.{$row['dblog_user_id']}'>{$row['user_name']}</a></td>\n" : "    <td>{$row['dblog_user_id']}</td>\n";
 	$text .= "  </tr>\n";
 }
 
 $text .= "</table></div>\n";
+
+	$text .= "<div style='text-align:center'><br />".$tp->parseTemplate("{NEXTPREV={$parms}}")."</div>";
 
 $ns->tablerender("Admin Log", $text);
 require_once("footer.php");
@@ -78,6 +85,7 @@ function get_log_img($log_type) {
 		case E_LOG_FATAL:
 			return "<img src='".e_IMAGE_ABS."admin_images/nopreview_16.png' alt='Fatal Icon' title='Fatal Error Message' />";
 		break;
+			return $log_type;
 	}
 }
 
