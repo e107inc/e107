7ed3648f0fef70dcb3a1720bba0659213324b9bd
daddycool78 <daddycool78@localhost>
2006-04-19 03:31:55 +0000

    Main CVS update, bugtrack fixed
    svn path=/trunk/e107_langpacks/; revision=6383

---
+++ e107_plugins/gsitemap/languages/gsitemap_French.php
@@ -0,0 +1,53 @@
+<?php
+/*
++---------------------------------------------------------------+
+| Fichiers de langage Français e107 CMS (utf-8). License GNU/PGL
+| Traducteurs: communauté française e107
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/gsitemap/languages/gsitemap_French.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-19 03:31:55 $
+|     $Author: daddycool78 $
++---------------------------------------------------------------+
+*/
+  define("GSLAN_Name", "Plan du site");
+  define("GSLAN_1", "Lien du site");
+  define("GSLAN_2", "Importer?");
+  define("GSLAN_3", "Type");
+  define("GSLAN_4", "Nom");
+  define("GSLAN_5", "Url");
+  define("GSLAN_6", "Cocher les liens à importer ...");
+  define("GSLAN_7", "Importer les liens");
+  define("GSLAN_8", "Importer avec:");
+  define("GSLAN_9", "Priorité");
+  define("GSLAN_10", "Fréquence");
+  define("GSLAN_11", "toujours");
+  define("GSLAN_12", "chaque heure");
+  define("GSLAN_13", "chaque jour");
+  define("GSLAN_14", "chaque semaine");
+  define("GSLAN_15", "mensuellement");
+  define("GSLAN_16", "annuellement");
+  define("GSLAN_17", "jamais");
+  define("GSLAN_18", "Importer les liens cochés");
+  define("GSLAN_19", "Plan Google Sitemap");
+  define("GSLAN_20", "Liste des entrées");
+  define("GSLAN_21", "Instructions");
+  define("GSLAN_22", "Créer une nouvelle entrée");
+  define("GSLAN_23", "Importer");
+  define("GSLAN_24", "Entrées Google Sitemap");
+  define("GSLAN_25", "Nom");
+  define("GSLAN_26", "URL");
+  define("GSLAN_27", "Dernière modif");
+  define("GSLAN_28", "Fréq.");
+  define("GSLAN_29", "Configuration du protocole Google Sitemap");
+  define("GSLAN_30", "Ordre d'affichage");
+  define("GSLAN_31", "Visible pour");
+  define("GSLAN_32", "Comment utiliser Google Sitemaps");
+  define("GSLAN_33", "Instructions pour GSiteMap");
+  define("GSLAN_34", "D'abord, créez les liens que vous voulez avoir inscrit dans votre plan de site. Vous pouvez importer la plupart de vos liens en cliquant sur le bouton 'Import' à droite");
+  define("GSLAN_35", "Si vous choisissez d'importer vos liens, cliquez sur 'Importer les liens' et vérifiez ensuite les liens que vous voulez importer");
+  define("GSLAN_36", "Vous pouvez aussi entrer des liens individuelles manuellement en cliquant ' Créer une Nouvelle Entrée '");
+  define("GSLAN_37", "Une fois que vous avez quelques entrées, accéder à la page <a href='https://www.google.com/webmasters/sitemaps/stats'>https://www.google.com/webmasters/sitemaps/stats</a> et entrer l'URL -> <strong>".SITEURL."gsitemap.php</strong> - Si cet url ne vous semble pas correcte, assurez-vous que l'url de votre site est correcte dans admin - > préférences");
+  define("GSLAN_38", "Pour plus d'information sur le protocole Google Sitemaps , consulter la page <a href='http://www.google.com/webmasters/sitemaps/docs/fr/protocol.html'>http://www.google.com/webmasters/sitemaps/docs/fr/protocol.html</a>.");
+  define("GSLAN_39", "Pas de liens dans le plan du site - importer les liens du site?");
+  define("GSLAN_40", "Entrées Google Sitemap");
+  ?>
