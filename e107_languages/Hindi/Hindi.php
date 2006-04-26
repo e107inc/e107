ba599e1ff68e7d4962fc29bb2ae81f19b67359da
arunshekher <arunshekher@localhost>
2006-04-26 22:33:00 +0000

    Commiting Hindi for the first time. - Arun
    svn path=/trunk/e107_langpacks/; revision=6454

--- e107_languages/Hindi/Hindi.php
+++ e107_languages/Hindi/Hindi.php
@@ -1,26 +1,21 @@
-<?php
+﻿<?php
 /*
 + ----------------------------------------------------------------------------+
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/Hindi.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2006-04-26 19:43:29 $
-|     $Author: e107coders $
+|     $Revision: 1.2 $
+|     $Date: 2006-04-26 22:33:00 $
+|     $Author: arunshekher $
 +----------------------------------------------------------------------------+
 */
-setlocale(LC_ALL, 'en');
-define("CORE_LC", 'en');
-define("CORE_LC2", 'gb');
-// define("TEXTDIRECTION","rtl");
+setlocale(LC_ALL, 'hi');
+//define("CORE_LC", 'en');
+//define("CORE_LC2", 'gb');
 define("CHARSET", "utf-8");  // for a true multi-language site. :)
-define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
+define("CORE_LAN1","गलती : थीमँ खोया हुआ मालूम पड रहा हैंै।\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
 
 //v.616
 define("CORE_LAN2"," \\1 wrote:");// "\\1" represents the username.
 define("CORE_LAN3","file attachment disabled");
-
-//v0.7+
-define("CORE_LAN4", "Please delete install.php from your server");
-define("CORE_LAN5", "if you do not there is a potential security risk to your website");
 ?>
