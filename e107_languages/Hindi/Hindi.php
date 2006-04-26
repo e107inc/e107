e28e6fd5019ec7179ae723d51571063f212c1fc0
e107coders <e107coders@localhost>
2006-04-26 19:43:29 +0000

    Initial File for folder - needs modification
    svn path=/trunk/e107_langpacks/; revision=6452

---
+++ e107_languages/Hindi/Hindi.php
@@ -0,0 +1,26 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/Hindi.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-26 19:43:29 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+setlocale(LC_ALL, 'en');
+define("CORE_LC", 'en');
+define("CORE_LC2", 'gb');
+// define("TEXTDIRECTION","rtl");
+define("CHARSET", "utf-8");  // for a true multi-language site. :)
+define("CORE_LAN1","Error : theme is missing.\\n\\nChange the used themes in your preferences (admin area) or upload files of the current theme on the server.");
+
+//v.616
+define("CORE_LAN2"," \\1 wrote:");// "\\1" represents the username.
+define("CORE_LAN3","file attachment disabled");
+
+//v0.7+
+define("CORE_LAN4", "Please delete install.php from your server");
+define("CORE_LAN5", "if you do not there is a potential security risk to your website");
+?>
