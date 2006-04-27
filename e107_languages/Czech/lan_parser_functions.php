69afda70f4bc22d84d9e5faf8e4fdb19446b11d1
e107coders <e107coders@localhost>
2006-04-27 19:41:00 +0000

    it's parser_functionS
    svn path=/trunk/e107_langpacks/; revision=6467

---
+++ e107_languages/Czech/lan_parser_functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Czech/lan_parser_functions.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-27 19:41:00 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+define("LAN_GUEST", "Host");
+
+?>
