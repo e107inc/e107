eeac822a31f7475426fe48683ac7d181ed4158db
arunshekher <arunshekher@localhost>
2006-04-27 23:03:02 +0000

    First Commit -Arun
    svn path=/trunk/e107_langpacks/; revision=6469

---
+++ e107_languages/Hindi/lan_date.php
@@ -0,0 +1,35 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_date.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-27 23:03:02 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+
+define("LANDT_01", "साल");
+define("LANDT_02", "महीना");
+define("LANDT_03", "हफ्ता");
+define("LANDT_04", "दिन");
+define("LANDT_05", "घंटा");
+define("LANDT_06", "मिनट");
+define("LANDT_07", "सेक्कंड");
+define("LANDT_01s", "years");
+define("LANDT_02s", "months");
+define("LANDT_03s", "weeks");
+define("LANDT_04s", "days");
+define("LANDT_05s", "hours");
+define("LANDT_06s", "मिनट्स");
+define("LANDT_07s", "सेक्कंड्स");
+
+define("LANDT_08", "min");
+define("LANDT_08s", "mins");
+define("LANDT_09", "sec");
+define("LANDT_09s", "secs");
+define("LANDT_AGO", "पूर्व");
+
+
+?>
