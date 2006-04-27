49d3954b49b2a0f7f1f006d64bd35aab06ebccf8
arunshekher <arunshekher@localhost>
2006-04-27 22:49:19 +0000

    First Commit - Arun
    svn path=/trunk/e107_langpacks/; revision=6468

---
+++ e107_languages/Hindi/lan_comment.php
@@ -0,0 +1,59 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_comment.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-27 22:49:19 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+
+define("PAGE_NAME", "टिप्पणियॉं");
+
+define("LAN_0", "[प्रशासक द्वारा रोका गया है]");
+define("LAN_1", "खोलियें");
+define("LAN_2", "ब्लाक");
+define("LAN_3", "मिटाए");
+define("LAN_4", "जानकारी");
+define("LAN_5", "टिप्पणियॉं ...");
+define("LAN_6", "टिप्पणियॉं प्रस्तुत करने के लिये आपको लॉग इन होना होगा - कृपया लॉग इन कीजियेे अथवा रजिस्टर कीजिये");
+define("LAN_7", "मुख्य जाल स्थल प्रशासक");
+define("LAN_8", "टिप्पणी");
+define("LAN_9", "ैैटिप्पणी उपस्थित करें");
+define("LAN_10", "प्रशासक");
+define("LAN_11", "Was unable to enter your comment into the database - please retype leaving out any non-standard characters.");
+define("LAN_16", "प्रयोक्ता नाम: ");
+define("LAN_99", "टिप्पणियॉं");
+define("LAN_100", "समाचार");
+define("LAN_101", "निर्वाचन");
+define("LAN_102", "जवाब दे रहे हैं: ");
+define("LAN_103", "लेख");
+define("LAN_104", "पुनरवलोकन");
+define("LAN_105", "अंश");
+define("LAN_145", "रजिस्ट्रेड: ");
+define("LAN_194", "अतिथि");
+define("LAN_195", "रजिस्ट्रेड सदस्य");
+define("LAN_310", "Unable to accept post as that username is registered - if it is your username please login to post.");
+define("LAN_312", "प्रतिलिपि निवेदन - स्वीकार नहीं कर सकते.");
+define("LAN_313", "स्थान");
+define("LAN_314", "टिप्पणीयों का मध्यम करें");
+define("COMLAN_1", "यहां,");
+define("COMLAN_2", "नयी हिसाब खोलने के लिये");
+define("COMLAN_3", "गलती!");
+define("COMLAN_4", 'विषय');
+define("COMLAN_5", 'जवाब:');
+define("COMLAN_6", 'इसके जवाब दें');
+define("COMLAN_7", 'मूल्य निरूपण');
+define("COMLAN_8", 'टिप्पणीयों पर ताला लगा दिये गयें हैं');
+
+define("LAN_315", "ट्राकबाक्स");
+define("LAN_316", "इस समाचार को ट्राकबाक्स नहीं हैं.");
+define("LAN_317", "ट्राकबाक्स को मध्यम करें");
+
+define("LAN_318", "टिप्पणी के संपादन करें");
+define("LAN_319", "संपादन किया गया");
+define("LAN_320", "टिप्पणी सुधार करें");
+
+?>
