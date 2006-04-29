eb8513cb63995dc5ff35c91cae3058e4f6957d94
arunshekher <arunshekher@localhost>
2006-04-29 05:02:44 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=6478

--- e107_languages/Hindi/Hindi.php
+++ e107_languages/Hindi/Hindi.php
@@ -4,14 +4,14 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/Hindi.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2006-04-27 16:11:26 $
+|     $Revision: 1.4 $
+|     $Date: 2006-04-29 05:02:44 $
 |     $Author: arunshekher $
 +----------------------------------------------------------------------------+
 */
 setlocale(LC_ALL, 'hi');
-//define("CORE_LC", 'en');
-//define("CORE_LC2", 'gb');
+define("CORE_LC", 'hi');
+define("CORE_LC2", 'hi');
 define("CHARSET", "utf-8");  // for a true multi-language site. :)
 define("CORE_LAN1","गलती : थीमँ खोया हुआ मालूम पड रहा हैंै।\\n\\nैेेकाम में लिया हुआ थीमँ थीमँ मैनेजर के जरिये बदलिये (प्रबंधक क्षेत्र के भीतर) या प्रयुक्त थीम के फयल्सं दुबारा सेरवर पर अपलोड करें।");
 
