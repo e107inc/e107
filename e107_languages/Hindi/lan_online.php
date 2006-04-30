9142a33791bb15a5c3c23c49d7a3ffffec8b9006
arunshekher <arunshekher@localhost>
2006-04-30 12:40:11 +0000

    First Commit -Arun
    svn path=/trunk/e107_langpacks/; revision=6496

---
+++ e107_languages/Hindi/lan_online.php
@@ -0,0 +1,64 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_online.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-04-30 12:40:11 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+
+//v.616
+define("ONLINE_EL1", "अतिथि: ");
+define("ONLINE_EL2", "उपयोगकर्ता: ");
+define("ONLINE_EL3", "इस पृष्ठ मे: ");
+define("ONLINE_EL4", "Online");
+define("ONLINE_EL5", "सदस्य");
+define("ONLINE_EL6", "नया सदस्य");
+define("ONLINE_EL7", "देख रहे हैं");
+define("ONLINE_EL8", "सब से अधिक उपयोगकर्ता online, कभी भी: ");
+define("ONLINE_EL9", "on");
+define("ONLINE_EL10", "सदस्य का नाम");
+define("ONLINE_EL11", "Viewing Page");
+define("ONLINE_EL12", "Replying to");
+define("ONLINE_EL13", "Forum");
+define("ONLINE_EL14", "Thread");
+define("ONLINE_EL15", "Page");
+define("CLASSRESTRICTED", "कक्षा सीमित पन्ना");
+define("ARTICLEPAGE", "Article/Review");
+define("CHAT", "बातचीत");
+define("COMMENT", "टिप्पणियॉं");
+define("DOWNLOAD", "Downloads");
+define("EMAIL", "email.php");
+define("FORUM", "Main Forum Index");
+define("LINKS", "Links");
+define("NEWS", "समाचार");
+define("OLDPOLLS", "Old Polls");
+define("POLLCOMMENT", "Poll");
+define("PRINTPAGE", "छापें");
+define("LOGIN", "लॉगिगं इन");
+define("SEARCH", "खोज क्रिया");
+define("STATS", "जाल स्थल सांख्यिकी");
+define("SUBMITNEWS", "Submit News");
+define("UPLOAD", "Uploads");
+define("USERPAGE", "User Profiles");
+define("USERSETTINGS", "User Settings");
+define("ONLINE", "Online Users");
+define("LISTNEW", "List New Items");
+define("USERPOSTS", "User Posts");
+define("SUBCONTENT", "Submit Article/Review");
+define("TOP", "Top Posters/Most Active Threads");
+define("ADMINAREA", "Admin Area");
+define("BUGTRACKER", "Bugtracker");
+define("EVENT", "Events List");
+define("CALENDAR", "Events Calendar");
+define("FAQ", "Faq");
+define("PM", "Private Messaging");
+define("SURVEY", "Survey");
+define("ARTICLE", "Article");
+define("CONTENT", "Content Page");
+define("REVIEW", "Review");
+
+?>
