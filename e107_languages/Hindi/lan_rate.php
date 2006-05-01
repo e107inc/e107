a20bc3f507d1f2af93846cf779cca3e848809ffb
arunshekher <arunshekher@localhost>
2006-05-01 06:29:06 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=6503

---
+++ e107_languages/Hindi/lan_rate.php
@@ -0,0 +1,20 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_rate.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-01 06:29:06 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+
+define("RATELAN_0", "सम्मति");
+define("RATELAN_1", "सम्मतियाँ");
+define("RATELAN_2", "how do you rate this item?");
+define("RATELAN_3", "thank you for your vote");
+define("RATELAN_4", "मूल्य निरूपण नही किया गया है");
+define("RATELAN_5", "मूल्य निरूपण करें");
+
+?>
