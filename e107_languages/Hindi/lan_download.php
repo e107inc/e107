70d63a0e0b4b2587193466d96a92fa70f3f9003f
arunshekher <arunshekher@localhost>
2006-05-01 20:12:31 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=6506

---
+++ e107_languages/Hindi/lan_download.php
@@ -0,0 +1,100 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_download.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-01 20:12:31 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "डाउनलोड्स");
+
+define("LAN_dl_1", "(सीमित)");
+define("LAN_dl_2", "अभी कोई डाउनलोड्स उपलब्ध नही, कृपया जल्दी हीं पुनः जंचिए");
+define("LAN_dl_3", "इस वर्ग में अभी कोई डाउनलोड्स उपलब्ध नही");
+define("LAN_dl_4", "उपलब्ध फाइल्स: ");
+define("LAN_dl_5", "Total size of files: ");
+define("LAN_dl_6", "Files downloaded: ");
+define("LAN_dl_7", "विवरण");
+define("LAN_dl_8", "पाएं");
+define("LAN_dl_9", "वर्ग सूची की ओर लौटें");
+define("LAN_dl_10", "Filesize");
+define("LAN_dl_11", "Image");
+define("LAN_dl_12", "मूल्य निरूपण");
+define("LAN_dl_13", "Not rated");
+define("LAN_dl_14", "Rate this download");
+define("LAN_dl_15", "Thanks for rating");
+define("LAN_dl_16", "download(s) from");
+define("LAN_dl_17", "फाइल्स");
+define("LAN_dl_18", "डाउनलोड्स");
+define("LAN_dl_19", "Category");
+define("LAN_dl_20", "फाइल्स");
+define("LAN_dl_21", "Size");
+define("LAN_dl_22", "तारिख");
+define("LAN_dl_23", "Filename");
+define("LAN_dl_24", "रचयिता");
+define("LAN_dl_25", "आरोही क्रम");
+define("LAN_dl_26", "अवरोही क्रम");
+define("LAN_dl_27", "जा");
+define("LAN_dl_28", "नाम");
+define("LAN_dl_29", "DL's");
+define("LAN_dl_30", "रचयिता के ई-मेल");
+define("LAN_dl_31", "रचयिता के जाल स्थल");
+define("LAN_dl_32", "डाउनलोड");
+define("LAN_dl_33", "पिछला");
+define("LAN_dl_34", "अगला");
+define("LAN_dl_35", "वापस सूची की ओर");
+define("LAN_dl_36", "नया डाउनलोड");
+
+define("LAN_dl_37", "ताकें");
+define("LAN_dl_38", "क्रम में रखें, के अनुसार");
+define("LAN_dl_39", "छांटें");
+
+define("LAN_dl_40", "Click here for screenshot");
+define("LAN_dl_41", "Search Downloads");
+
+define("LAN_dl_42", "Sub-category");
+define("LAN_dl_43", "vote");
+define("LAN_dl_44", "votes");
+define("LAN_dl_45", "Report broken download");
+define("LAN_dl_46", "click here to download");
+
+define("LAN_dl_47", "Post has been reported");
+define("LAN_dl_48", "Download has been reported to administrator.<br />Thank You.");
+define("LAN_dl_49", "Click here to return to download");
+define("LAN_dl_50", "Report broken download to an administrator");
+define("LAN_dl_51", "Reporting download: ");
+define("LAN_dl_52", "Guest");
+define("LAN_dl_53", "Click to view download");
+define("LAN_dl_54", "An administrator will be made aware of this download, please leave a message if you feel it necessary.");
+define("LAN_dl_55", "<b>Do not</b> use this form to contact the admin for any other reason.");
+define("LAN_dl_56", "Report broken download");
+define("LAN_dl_57", "reported by");
+define("LAN_dl_58", "The following download has been reported as broken from site");
+define("LAN_dl_59", "Reported by: ");
+define("LAN_dl_60", "Broken download report from");
+
+define("LAN_dl_61", "Download Error");
+define("LAN_dl_62", "You have been prevented from downloading this file, you have exceeded your download quota");
+define("LAN_dl_63", "You do not have the correct permissions to download this file.");
+define("LAN_dl_64", "Back");
+define("LAN_dl_65", "Not Found");
+
+define("LAN_dl_66", "Select download mirror");
+
+define("LAN_dl_67", "Please select mirror to use ...");
+define("LAN_dl_68", "Mirror Host");
+// define("LAN_dl_69", "Download"); // duplicate
+define("LAN_dl_70", "Location");
+define("LAN_dl_71", "About");
+
+define("LAN_dl_72", "Requesting file: ");
+define("LAN_dl_73", "Downloads from this mirror: ");
+define("LAN_dl_74", "Total downloads from this mirror: ");
+
+define("LAN_dl_75", "no image available ");
+define("LAN_dl_76", "Go to page");
+
+?>
