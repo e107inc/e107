d162563ce61196696af4e5d89187f28520094d11
natxocc <natxocc@localhost>
2006-05-01 21:20:34 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=6507

--- e107_languages/Spanish/Spanish.php
+++ e107_languages/Spanish/Spanish.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Spanish/Spanish.php,v $
-|     $Revision: 1.7 $
-|     $Date: 2005-11-11 23:57:40 $
+|     $Revision: 1.8 $
+|     $Date: 2006-05-01 21:20:34 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
@@ -16,4 +16,8 @@ define("CHARSET", "utf-8");
 define("CORE_LAN1","Error : tema no encontrado.\\n\\nCambie el tema usado en preferencias (administración) o copie los archivos del tema seleccionado al servidor.");
 define("CORE_LAN2"," \\1 escribió:");// "\\1" representa el nombre de usuario.
 define("CORE_LAN3","archivo adjunto desactivado");
+
+//v0.7+
+define("CORE_LAN4", "Por favor, elimine install.php de su servidor");
+define("CORE_LAN5", "Si no lo hace, pone en riesgo potencial su sitio web");
 ?>
