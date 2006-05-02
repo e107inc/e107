e38c0d337f1feefdce29e517caa48206282f950a
mcfly_e107 <mcfly_e107@localhost>
2006-05-02 00:00:04 +0000

    allowed turning off of sending contact email to self
    svn path=/trunk/e107_0.7/; revision=6508

--- e107_themes/templates/contact_template.php
+++ e107_themes/templates/contact_template.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: contact_template.php,v 1.2 2006-04-25 18:01:18 e107coders Exp $
+// $Id: contact_template.php,v 1.3 2006-05-01 23:58:47 mcfly_e107 Exp $
 
 if (!defined('e107_INIT')) { exit; }
 
@@ -14,6 +14,16 @@ if(!$CONTACT_INFO){
 	</table>";
 }
 
+// This must be defined outside $CONTACT_FORM so it can be turned off depending on the $pref
+if(!$CONTACT_EMAIL_COPY)
+{
+	$CONTACT_EMAIL_COPY = "
+	<tr><td>
+	<input type='checkbox' name='email_copy'  value='1'  />
+	".LANCONTACT_07."
+	</td></tr>
+	";
+}	
 
 if(!$CONTACT_FORM){
   $CONTACT_FORM = "
@@ -28,14 +38,10 @@ if(!$CONTACT_FORM){
 	<tr><td>
 	".LANCONTACT_05."<br />
 	<input type='text' name='subject' size='30' class='tbox' value='' />
-	</td></tr>
+	</td></tr>{$CONTACT_EMAIL_COPY}
 	<tr><td>
     ".LANCONTACT_06."<br />
 	<textarea cols='50' rows='10' name='body' class='tbox'></textarea>
-	</td></tr>
-	<tr><td>
-	<input type='checkbox' name='email_copy'  value='1'  />
-	".LANCONTACT_07."
 	</td></tr>
 	<tr><td>
 	<input type='submit' name='send-contactus' value=\"".LANCONTACT_08."\" class='button' />
