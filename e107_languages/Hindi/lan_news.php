ec0af3bd882cdffa6d2a744ed5adb1854a753c8e
arunshekher <arunshekher@localhost>
2006-05-06 21:18:30 +0000

    First Commit
    svn path=/trunk/e107_langpacks/; revision=6529

---
+++ e107_languages/Hindi/lan_news.php
@@ -0,0 +1,48 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_news.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-06 21:18:30 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "समाचार");
+
+define("LAN_82", "समाचार - विभाग");
+define("LAN_83", "कोई समाचार मदें इस क्षण मौजूद नहीं - कृपया जल्दी ही पुनः जंच करें ।");
+define("LAN_84", "समाचार मदें");
+define("LAN_99", "टिप्पणियॉं");
+define("LAN_100", "On");
+define("LAN_307", "Total posts in this category: ");
+
+define("LAN_NEWS_1", "News for specific members only");
+define("LAN_NEWS_2", "You are not allowed to see this news");
+//define("LAN_NEWS_3", "Please delete install.php from your server");
+//define("LAN_NEWS_4", "if you do not there is a potential security risk to your website");
+
+define("LAN_NEWS_5", "<b>Error!</b> Was unable to update news item into database!</b>");
+define("LAN_NEWS_6", "News entered into database.");
+define("LAN_NEWS_7", "<b>Error!</b> Was unable to enter news item into database!</b>");
+define("LAN_NEWS_8", "News entered into database for all languages. ID: ");
+define("LAN_NEWS_9", "Title only is set - <b>only the news title will be shown</b><br />");
+define("LAN_NEWS_10", "This news post is <b>inactive</b> (It will be not shown on front page). ");
+define("LAN_NEWS_11", "This news post is <b>active</b> (it will be shown on front page). ");
+define("LAN_NEWS_12", "Comments are turned <b>on</b>. ");
+define("LAN_NEWS_13", "Comments are turned <b>off</b>. ");
+define("LAN_NEWS_14", "<br />Activation period: ");
+define("LAN_NEWS_15", "Body length: ");
+define("LAN_NEWS_16", "b. Extended length: ");
+define("LAN_NEWS_17", "b.");
+define("LAN_NEWS_18", "Info:");
+define("LAN_NEWS_19", "अब(Now)");
+define("LAN_NEWS_20", "News updated in database for the following language: ");
+define("LAN_NEWS_21", "News updated in database.");
+// define("LAN_NEWS_22", "Go to page: ");
+define("LAN_NEWS_23", "News Categories");
+define("LAN_NEWS_24", "create pdf of this news item");
+define("LAN_462", "No news items for specified month");
+
+?>
