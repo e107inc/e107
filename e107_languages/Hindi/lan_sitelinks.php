460fd912383eca93deaa8c6d556eab3723f3e211
arunshekher <arunshekher@localhost>
2006-05-07 10:22:41 +0000

    First Commit -Arun
    svn path=/trunk/e107_langpacks/; revision=6531

---
+++ e107_languages/Hindi/lan_sitelinks.php
@@ -0,0 +1,15 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_sitelinks.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-07 10:22:41 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+define("LAN_183", "मुख्य मेन्यू");
+define("LAN_502", "प्रशासक क्षेत्र");
+
+?>
