f61d484ef188969abc417956f01c0f3840bf3128
arunshekher <arunshekher@localhost>
2006-05-17 13:51:57 +0000

    First Commit - Arun
    svn path=/trunk/e107_langpacks/; revision=6571

---
+++ e107_languages/Hindi/lan_signup.php
@@ -0,0 +1,101 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_signup.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-17 13:51:57 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "रजिस्टर");
+define("LAN_7", "डिस्प्ल नाम: ");
+define("LAN_8", "जाल स्थल मे प्रदर्शित करनेवाला नाम");
+define("LAN_9", "लॉग इन नाम: ");
+define("LAN_10", "लॉग इन करने के लिये उपयोग करनेवाला नाम");
+define("LAN_17", "संकेत-शब्द: ");
+define("LAN_103", "That user name cannot be accepted as valid, please choose a different user name");
+define("LAN_104", "That login name already exists in the database, please choose a different login name");
+define("LAN_105", "दोनों संकेत-शब्द तुलना नहीं करतें");
+define("LAN_106", "वह एक सही ई-मेल पता नहीं दिखतें");
+define("LAN_107", "धन्यवाद! अब आप एक पंजिबद्ध किया सदस्य् हैं, का");
+define("LAN_108", "पंजियन पूरा हुवा।");
+define("LAN_109", "This site complies with The Children's Online Privacy Protection Act of 1998 (COPPA) and as such cannot accept registrations from users under the age of 13 without a written permission document from their parent or guardian. For more information you can read the legislation");
+define("LAN_110", "पंजियन");
+define("LAN_111", "संकेत-शब्द फिर से टाइप करे: ");
+define("LAN_112", "ई-मेल पता: ");
+define("LAN_113", "ई-मेल पता को छिपायें?: ");
+define("LAN_114", "यह आपके ई-मेल पता को इस जाल स्थल पर दिखाने से रोकेगा");
+define("LAN_123", "रजिस्टर");
+define("LAN_185", "आपने आवश्यक क्षेत्र(क्षेत्रें) खाली छोड़ दियें हैं");
+define("LAN_201", "हाँ");
+define("LAN_200", "नहीं");
+define("LAN_202", "You already have an account. If you have forgotten your password, please click the \'forgot password\' link.");
+define("LAN_309", "कृपया आपके विस्तृत सूचनायें नीचे भर्ति करें । ");
+define("LAN_399", "आगे बढ़ें");
+define("LAN_400", "प्रयोगकर्त्ता  नाम और संकेत-शब्द <b>'केस'-निजी</b> हैं");
+define("LAN_401", "आपके हिसाब अब सक्रिय किया गया हैं, कृपया");
+define("LAN_402", "पंजियन सक्रिय किया गया।");
+define("LAN_403", "स्वागत की ओर");
+define("LAN_404", "पंजियन सूचनाए के लिए");
+define("LAN_405", "This stage of registation is complete. You will receive a confirmation email containing your login details. Please follow the link in the email to complete the signup process and activate your account.");
+define("LAN_406", "धन्यवाद!");
+define("LAN_407", "Please keep this email for your own information. Your password has been encrypted and cannot be retrieved if you misplace or forget it. You can however request a new password if this happens.\n\nThanks for your registration.\n\nFrom");
+define("LAN_408", "A user with that email address already exists. Please use the 'forgot password' screen to retrieve your password.");
+define("LAN_SIGNUP_1", "कम से कम.");
+define("LAN_SIGNUP_2", "अक्षरें.");
+define("LAN_SIGNUP_3", "कूट भाषाअ प्रमाणीकरण असफल हुवा।");
+define("LAN_SIGNUP_4", "आपके संकेत-शब्द कम से कम ");
+define("LAN_SIGNUP_5", " अक्षरें लंबा होना चाहिये.");
+define("LAN_SIGNUP_6", "आपका ");
+define("LAN_SIGNUP_7", " आवश्यक हैं");
+define("LAN_SIGNUP_8", "धन्यवाद!");
+define("LAN_SIGNUP_9", "आगे बढने को असमर्थ।");
+define("LAN_SIGNUP_10", "हाँ");
+define("LAN_SIGNUP_11", ".");
+
+define("LAN_409", "Invalid characters in username");
+define("LAN_410", "Enter code visible in the image");
+define("LAN_411", "That display name already exists in the database, please choose a different display name");
+
+
+define("LAN_SIGNUP_12", "please keep your username and password written down in a safe place as if lost they cannot be retrieved.");
+define("LAN_SIGNUP_13", "You can now log in from the Login box, or from <a href='".e_BASE."login.php'>here</a>.");
+define("LAN_SIGNUP_14", "इधर");
+define("LAN_SIGNUP_15", "Please contact the main site admin");
+define("LAN_SIGNUP_16", "if you require assistance.");
+define("LAN_SIGNUP_17", "Please certify you are 13 or over the age of 13.");
+define("LAN_SIGNUP_18", "Your registration has been received and created with the following login information ...");
+define("LAN_SIGNUP_19", "प्रयोगकर्त्ता  नाम:");
+define("LAN_SIGNUP_20", "संकेत-शब्द:");
+define("LAN_SIGNUP_21", "Your account is currently marked as being inactive, to activate your account please go to the following link ...");
+define("LAN_SIGNUP_22", "इधर क्लिक करें");
+define("LAN_SIGNUP_23", "लॉग इन करने के लिये.");
+define("LAN_SIGNUP_24", "Thankyou for registering at");
+define("LAN_SIGNUP_25", "Upload your avatar");
+define("LAN_SIGNUP_26", "Upload your photograph");
+define("LAN_SIGNUP_27", "दिखाए");
+define("LAN_SIGNUP_28", "choice of Content/Mail-lists");
+define("LAN_SIGNUP_29", "A verification email will be sent to the email address you enter here so it must be valid.");
+define("LAN_SIGNUP_30", "If you do not wish to display your email address on this site, please tick the 'hide email address' box.");
+
+define("LAN_SIGNUP_31", "URL to your XUP file");
+define("LAN_SIGNUP_32", "What's an XUP file?");
+define("LAN_SIGNUP_33", "Type path or choose avatar");
+define("LAN_SIGNUP_34", "Please note: Any image uploaded to this server that is deemed inappropriate by the administrators will be deleted immediately.");
+define("LAN_SIGNUP_35", "Click here to register using an XUP file");
+define("LAN_SIGNUP_36", "An error has occured creating your user information, please contact the site admin");
+
+define("LAN_LOGINNAME", "लॉग इन नाम");
+define("LAN_PASSWORD", "संकेत-शब्द");
+define("LAN_USERNAME", "प्रदर्शित नाम");
+define("LAN_EMAIL_01", "प्रिय");
+define("LAN_EMAIL_04", "Please keep this email for your own information.");
+define("LAN_EMAIL_05", "Your password has been encrypted and cannot be retrieved if you misplace or forget it. You can however request a new password if this happens.");
+define("LAN_EMAIL_06", "Thanks for your registration.");
+
+define("LAN_SIGNUP_37", "This stage of registation is complete. The site admin will need to approve your membership.  Once this has been done you will receive a confirmation email alerting you that your membership has been approved.");
+define("LAN_SIGNUP_38", "You entered two different email addresses. Please enter a valid email address in the two fields provided");
+define("LAN_SIGNUP_39", "ई-मेल पता फिर से टाइप करें:");
+?>
---
+++ e107_languages/Hindi/lan_userposts.php
@@ -0,0 +1,31 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Hindi/lan_userposts.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-17 13:51:57 $
+|     $Author: arunshekher $
++----------------------------------------------------------------------------+
+*/
+define("PAGE_NAME", "प्रयोगकर्त्ता विज्ञापन");
+
+define("UP_LAN_0", "All Forum Posts for ");
+define("UP_LAN_1", "All Comments for ");
+define("UP_LAN_2", "डोरा");
+define("UP_LAN_3", "परखें");
+define("UP_LAN_4", "जवाबें");
+define("UP_LAN_5", "आखिरी विज्ञापन");
+define("UP_LAN_6", "Threads");
+define("UP_LAN_7", "कोई टिप्पणियॉं नहीं");
+define("UP_LAN_8", "कोई विज्ञापन  नहीं");
+define("UP_LAN_9", " on ");
+define("UP_LAN_10", "जवाब");
+define("UP_LAN_11", "Posted on");
+define("UP_LAN_12", "खोजें");
+define("UP_LAN_13", "टिप्पणियॉं");
+define("UP_LAN_14", "वादसभा विज्ञापन");
+define("UP_LAN_15", "जवाब");
+define("UP_LAN_16", "IP पता");
+?>
