5ade446e7eec1becec19578b79a5bb7f129196eb
marcelis_pl <marcelis_pl@localhost>
2006-05-21 15:50:54 +0000

    Update - e107 v0.7.4 utf-8 Polish
    svn path=/trunk/e107_langpacks/; revision=6581

--- e107_handlers/calendar/language/Polish.js
+++ e107_handlers/calendar/language/Polish.js
@@ -11,14 +11,14 @@
 
 // full day names
 Calendar._DN = new Array
-("Sunday",
- "Monday",
- "Tuesday",
- "Wednesday",
- "Thursday",
- "Friday",
- "Saturday",
- "Sunday");
+("Niedziela",
+ "Poniedziałek",
+ "Wtorek",
+ "Środa",
+ "Czwartek",
+ "Piątek",
+ "Sobota",
+ "Niedziela");
 
 // Please note that the following array of short day names (and the same goes
 // for short month names, _SMN) isn't absolutely necessary.  We give it here
@@ -34,14 +34,14 @@ Calendar._DN = new Array
 
 // short day names
 Calendar._SDN = new Array
-("Sun",
- "Mon",
- "Tue",
- "Wed",
- "Thu",
- "Fri",
- "Sat",
- "Sun");
+("Nie",
+ "Pon",
+ "Wto",
+ "Śro",
+ "Czw",
+ "Pią",
+ "Sob",
+ "Nie");
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
@@ -49,79 +49,79 @@ Calendar._FD = 0;
 
 // full month names
 Calendar._MN = new Array
-("January",
- "February",
- "March",
- "April",
- "May",
- "June",
- "July",
- "August",
- "September",
- "October",
- "November",
- "December");
+("Styczeń",
+ "Luty",
+ "Marzec",
+ "Kwiecień",
+ "Maj",
+ "Czerwiec",
+ "Lipiec",
+ "Sierpień",
+ "Wrzesień",
+ "Październik",
+ "Listopad",
+ "Grudzień");
 
 // short month names
 Calendar._SMN = new Array
-("Jan",
- "Feb",
+("Sty",
+ "Lut",
  "Mar",
- "Apr",
- "May",
- "Jun",
- "Jul",
- "Aug",
- "Sep",
- "Oct",
- "Nov",
- "Dec");
+ "Kwi",
+ "Maj",
+ "Cze",
+ "Lip",
+ "Sie",
+ "Wrz",
+ "Paź",
+ "Lis",
+ "Gru");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "About the calendar";
+Calendar._TT["INFO"] = "O Kalendarzu";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
 "(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
-"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+"Rozpowszechniane zgodnie z licencją GNU LGPL.  Zobacz stronę http://gnu.org/licenses/lgpl.html , aby uzyskać więcj informacji." +
 "\n\n" +
 "Date selection:\n" +
 "- Use the \xab, \xbb buttons to select year\n" +
 "- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
 "- Hold mouse button on any of the above buttons for faster selection.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Time selection:\n" +
+"Wybór czasu:\n" +
 "- Click on any of the time parts to increase it\n" +
 "- or Shift-click to decrease it\n" +
 "- or click and drag for faster selection.";
 
-Calendar._TT["PREV_YEAR"] = "Prev. year (hold for menu)";
-Calendar._TT["PREV_MONTH"] = "Prev. month (hold for menu)";
-Calendar._TT["GO_TODAY"] = "Go Today";
-Calendar._TT["NEXT_MONTH"] = "Next month (hold for menu)";
-Calendar._TT["NEXT_YEAR"] = "Next year (hold for menu)";
-Calendar._TT["SEL_DATE"] = "Select date";
+Calendar._TT["PREV_YEAR"] = "Poprzedni rok";
+Calendar._TT["PREV_MONTH"] = "Poprzedni miesiąc";
+Calendar._TT["GO_TODAY"] = "Dzisiaj";
+Calendar._TT["NEXT_MONTH"] = "Następny miesiąc";
+Calendar._TT["NEXT_YEAR"] = "Następny rok";
+Calendar._TT["SEL_DATE"] = "Wybierz datę";
 Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
-Calendar._TT["PART_TODAY"] = " (today)";
+Calendar._TT["PART_TODAY"] = " (dzisiaj)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Display %s first";
+Calendar._TT["DAY_FIRST"] = "Wyświetlaj pierwszy %s";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 Calendar._TT["WEEKEND"] = "0,6";
 
-Calendar._TT["CLOSE"] = "Close";
-Calendar._TT["TODAY"] = "Today";
-Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
+Calendar._TT["CLOSE"] = "Zamknij";
+Calendar._TT["TODAY"] = "Dzisiaj";
+Calendar._TT["TIME_PART"] = "(Shift-)kliknij aby zmienić wartość";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
 Calendar._TT["WK"] = "wk";
-Calendar._TT["TIME"] = "Time:";
+Calendar._TT["TIME"] = "Czas:";
---
+++ e107_handlers/phpmailer/language/phpmailer.lang-pl.php
@@ -0,0 +1,33 @@
+﻿<?php
+/*
++-----------------------------------------------------------------------------+
+|     e107 website system - Language File.
++-----------------------------------------------------------------------------+
+|     Spolszczenie systemu e107 v0.7
+|     Polskie wsparcie: http://e107.org.pl - http://e107poland.org
+|
+|     $Revision: 1.3 $
+|     $Date: 2006-05-21 15:50:54 $
+|     $Author: marcelis_pl $
+|     $Source: /cvs_backup/e107_langpacks/e107_handlers/phpmailer/language/phpmailer.lang-pl.php,v $
++-----------------------------------------------------------------------------+
+|     Zgodne z: /e107_handlers/phpmailer/language/phpmailer.lang-en.php rev. 1.2 (27 jun 2005)
++-----------------------------------------------------------------------------+
+*/
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'Musisz podać przynajmniej jeden' . 'adres email odbiorcy.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailer nie jest wspierany.';
+$PHPMAILER_LANG["execute"] = 'Nie mogę wykonać: ';
+$PHPMAILER_LANG["instantiate"] = 'Nie mogę odnaleźć funkcji poczty.';
+$PHPMAILER_LANG["authenticate"] = 'Błąd SMTP: Brak autoryzacji.';
+$PHPMAILER_LANG["from_failed"] = 'Następujący adres nadawacy jest niepoprawny: ';
+$PHPMAILER_LANG["recipients_failed"] = 'Błąd SMTP: Następujący adres ' . 'odbiorcy jest niepoprawny: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'Błąd SMTP: Dane nie zostały zaakceptowane.';
+$PHPMAILER_LANG["connect_host"] = 'Błąd SMTP: Nie mogę nawiązać połączenia z hostem SMTP.';
+$PHPMAILER_LANG["file_access"] = 'Brak dostępu do pliku: ';
+$PHPMAILER_LANG["file_open"] = 'Błąd pliku: nie mogę otworzyć pliku: ';
+$PHPMAILER_LANG["encoding"] = 'Nierozpoznane kodowanie: ';
+
+?>
