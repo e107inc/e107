f31e9f03794e6dec8088098b7f6cc4b58b40995d
e107coders <e107coders@localhost>
2006-05-23 06:51:27 +0000

    added Korean and Italian.
    svn path=/trunk/e107_langpacks/; revision=6590

---
+++ e107_plugins/login_menu/languages/Italian.php
@@ -0,0 +1,64 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/login_menu/languages/Italian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-23 06:51:27 $
+|     $Author: e107coders $
+|	Traduzione Italiana a cura di:
+|   e107 italian team http://e107it.org
++----------------------------------------------------------------------------+
+*/
+
+define("LOGIN_MENU_L1", "Nome di Login: ");
+define("LOGIN_MENU_L2", "Password: ");
+define("LOGIN_MENU_L3", "Registrati");
+define("LOGIN_MENU_L4", "Password Dimenticata?");
+define("LOGIN_MENU_L5", "Ciao");
+define("LOGIN_MENU_L6", "Ricordami");
+define("LOGIN_MENU_L7", "Utente Univoco non riconosciuto (possibilità di cookie corrotto).<br />Per favore <a href=\"".e_BASE."index.php?logout\">premi qui</a> per eliminare il cookie.");
+define("LOGIN_MENU_L8", "Logout");
+define("LOGIN_MENU_L9", "Errore di login");
+define("LOGIN_MENU_L10", "Il Flag Manutenzione è attivo - ciò significa che i visitatori saranno rediretti a sitedown.php. Per resettare il Flag vai in amministrazione.");
+define("LOGIN_MENU_L11", "Amministrazione");
+define("LOGIN_MENU_L12", "Modifica Profilo");
+define("LOGIN_MENU_L13", "Profilo");
+define("LOGIN_MENU_L14", "news ");
+define("LOGIN_MENU_L15", "news ");
+define("LOGIN_MENU_L16", "messaggi chatbox");
+define("LOGIN_MENU_L17", "messaggi chatbox");
+define("LOGIN_MENU_L18", "commenti");
+define("LOGIN_MENU_L19", "commenti");
+define("LOGIN_MENU_L20", "messaggi forum");
+define("LOGIN_MENU_L21", "messaggi forum");
+define("LOGIN_MENU_L22", "nuovi utenti registrati");
+define("LOGIN_MENU_L23", "nuovi utenti registrati");
+define("LOGIN_MENU_L24", "Vai alle novità...");
+define("LOGIN_MENU_L25", "Novità dalla tua ultima visita");
+define("LOGIN_MENU_L26", "0");
+define("LOGIN_MENU_L27", "e");
+define("LOGIN_MENU_L28", "Login");
+
+define("LOGIN_MENU_L29", "nuovi articoli");
+define("LOGIN_MENU_L30", "nuovi articoli");
+
+// New config options
+define('LOGIN_MENU_L31', 'Mostra le nuove News');
+define('LOGIN_MENU_L32', 'Mostra i nuovi Articoli');
+define('LOGIN_MENU_L33', 'Mostra i nuovi messaggi della Chatbox');
+define('LOGIN_MENU_L34', 'Mostra i nuovi Commenti');
+define('LOGIN_MENU_L35', 'Mostra i nuovi messaggi dal Forum');
+define('LOGIN_MENU_L36', 'Mostra i nuovi Utenti Registrati');
+define('LOGIN_MENU_L37', 'Aggiorna le impostazioni');
+define('LOGIN_MENU_L38', 'Impostazioni Aggiornate');
+
+?>
---
+++ e107_plugins/login_menu/languages/Korean.php
@@ -0,0 +1,71 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     쯘teve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/login_menu/languages/Korean.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-05-23 06:51:27 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+	
+define("LOGIN_MENU_L1", "사용자 이름: ");
+define("LOGIN_MENU_L2", "비밀번호: ");
+define("LOGIN_MENU_L3", "회원등록");
+define("LOGIN_MENU_L4", "비밀번호 분실");
+define("LOGIN_MENU_L5", "환영합니다.");
+define("LOGIN_MENU_L6", "자동 로그인");
+define("LOGIN_MENU_L7", "
+쿠키가 훼손된 듯 합니다. 단일 사용자 인증이 되지 않습니다.
+<br />훼손된 쿠키 제거 하실려면
+<a href=\"".e_BASE."index.php?logout\">여기</a>를 클릭하세요.
+");
+define("LOGIN_MENU_L8", "로그 아웃");
+define("LOGIN_MENU_L9", "로그인 오류");
+define("LOGIN_MENU_L10", "
+사이트 정비 중 입니다.
+- 일반 사용자는 sitedown.php 페이지를 보게 됩니다.
+사이트 정비를 해제할려면 admin/maintenance 로 이동하십시오.
+
+ ");
+define("LOGIN_MENU_L11", "관리자 영역");
+define("LOGIN_MENU_L12", "개인정보 설정");
+define("LOGIN_MENU_L13", "포로필");
+define("LOGIN_MENU_L14", "뉴스 아이템");
+define("LOGIN_MENU_L15", "뉴스 아이템");
+define("LOGIN_MENU_L16", "채팅박스 등록글");
+define("LOGIN_MENU_L17", "채팅박스 등록글");
+define("LOGIN_MENU_L18", "꼬릿말");
+define("LOGIN_MENU_L19", "꼬릿말");
+define("LOGIN_MENU_L20", "포럼 등록글");
+define("LOGIN_MENU_L21", "포럼 등록글");
+define("LOGIN_MENU_L22", "신규 회원");
+define("LOGIN_MENU_L23", "신규 회원");
+define("LOGIN_MENU_L24", "신규 아이템 보실려면 클릭하세요.");
+define("LOGIN_MENU_L25", "지난 방문 이후");
+define("LOGIN_MENU_L26", "없음");
+define("LOGIN_MENU_L27", "그리고");
+define("LOGIN_MENU_L28", "로그인");
+	
+define("LOGIN_MENU_L29", "신규 사설");
+define("LOGIN_MENU_L30", "신규 사설");
+	
+// New config options
+define('LOGIN_MENU_L31', '신규 뉴스 보기');
+define('LOGIN_MENU_L32', '신규 사설 보기');
+define('LOGIN_MENU_L33', '신규 채팅박스 내용 보기');
+define('LOGIN_MENU_L34', '신규 꼬릿말 보기');
+define('LOGIN_MENU_L35', '신규 포럼 글 보기');
+define('LOGIN_MENU_L36', '신규 회원 보기');
+define('LOGIN_MENU_L37', '개인정보 갱신하기');
+define('LOGIN_MENU_L38', '갱인정보 갱신되었음');
+	
+?>
