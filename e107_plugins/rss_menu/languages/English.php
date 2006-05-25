4a39fda78329e305d98f107d56aac14d7d7d275e
e107coders <e107coders@localhost>
2006-05-25 03:35:17 +0000

    added a check to ensure that the e107_config file doesn't contain space before <? . This was causing rss validation issues for some.
    svn path=/trunk/e107_0.7/; revision=6598

--- e107_plugins/rss_menu/languages/English.php
+++ e107_plugins/rss_menu/languages/English.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/rss_menu/languages/English.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2005-10-13 18:55:26 $
+|     $Revision: 1.6 $
+|     $Date: 2006-05-25 03:35:17 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -26,7 +26,7 @@ define("RSS_LAN01", "Enable seperate feeds for each news category?");
 define("RSS_LAN02", "Enable seperate feeds for each download category?");
 
 define("RSS_NEWS","News");
-define("RSS_COM","Comments"); 
+define("RSS_COM","Comments");
 define("RSS_ART","Articles");
 define("RSS_REV", "Reviews");
 define("RSS_FT","Forum Threads");
@@ -35,4 +35,6 @@ define("RSS_FSP","Forum Specific Post");
 define("RSS_BUG","Bugtracker");
 define("RSS_FOR","Forum");
 define("RSS_DL","Downloads");
+
+define("RSS_LAN03","Your e107_config.php file contains space before the &lt;? characters. You should remove this space if you wish to have a valid RSS feed.");
 ?>
