16941a9e23ac3a078c4ab7dcc669d1f70e261450
daddycool78 <daddycool78@localhost>
2006-06-01 12:34:53 +0000

    Update of the french translation files to comply to the v0.7.5
    svn path=/trunk/e107_langpacks/; revision=6618

---
+++ e107_languages/French/lan_contact.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_contact.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-06-01 12:34:53 $
+|     $Author: daddycool78 $
++----------------------------------------------------------------------------+
+*/
+
+define("LANCONTACT_01", "Contact);
+define("LANCONTACT_02", "Formulaire Contact");
+define("LANCONTACT_03", "Entrez votre nom:");
+define("LANCONTACT_04", "Email:");
+define("LANCONTACT_05", "Sujet du message:");
+define("LANCONTACT_06", "Votre message:");
+define("LANCONTACT_07", "Recevoir une copie du mail à votre adresse ");
+define("LANCONTACT_08", "Envoyer");
+define("LANCONTACT_09", "Message envoyé.");
+define("LANCONTACT_10", "Erreur, votre message n'a pas pu nous parvenir.");
+define("LANCONTACT_11", "Votre adresse email ne semble pas être valide.\\nVeuillez vérifier et recommencer.");
+define("LANCONTACT_12", "Message trop court.");
+define("LANCONTACT_13", "Aucun sujet !? Veuillez le présicer."); 
+
+?>
