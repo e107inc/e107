9adbb0f0f019ef053e708e67ba57bbb3f31ea646
daddycool78 <daddycool78@localhost>
2006-06-01 12:48:13 +0000

    Update of the french translation files to comply to the v0.7.5
    svn path=/trunk/e107_langpacks/; revision=6620

---
+++ e107_themes/reline/languages/French.php
@@ -0,0 +1,9 @@
+<?php
+define("LAN_THEME_1", "Les commentaires sont désactivés pour cet item");
+define("LAN_THEME_2", "Lire/Poster des commentaires: ");
+define("LAN_THEME_3", "Lire la suite...");
+define("LAN_THEME_4", "Trackbacks: ");
+define("LAN_THEME_5", "Posté par");
+define("LAN_THEME_6", "le");
+define("LAN_THEME_7", "Rechercher...");
+?>
