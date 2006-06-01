d94ee90aa512d4b695f7cf6b219ccbdd129f8e4a
lisa_ <lisa_@localhost>
2006-06-01 11:25:50 +0000

    bugfix #2773 : lan definition of emoticons in tiny_mce corrected
    svn path=/trunk/e107_0.7/; revision=6616

--- e107_handlers/tiny_mce/plugins/emoticons/langs/en.js
+++ e107_handlers/tiny_mce/plugins/emoticons/langs/en.js
@@ -1,5 +1,5 @@
 // UK lang variables
 
-tinyMCELang['lang_insert_emotions_title'] = 'Insert emotion';
-tinyMCELang['lang_emotions_desc'] = 'Emotions';
+tinyMCELang['lang_insert_emoticons_title'] = 'Insert emotion';
+tinyMCELang['lang_emoticons_desc'] = 'Emotions';
 
