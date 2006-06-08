05e3608327b9331bea8086b035853c95416ee5df
e107coders <e107coders@localhost>
2006-06-08 02:32:33 +0000

    cleaned up the default news_feed_menu template. Number of items preference on feed menu was not working. Fixed.  (may still be overridden with $items in the template)
    svn path=/trunk/e107_0.7/; revision=6661

--- e107_plugins/newsfeed/templates/newsfeed_menu_template.php
+++ e107_plugins/newsfeed/templates/newsfeed_menu_template.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/newsfeed/templates/newsfeed_menu_template.php,v $
-|     $Revision: 1.6 $
-|     $Date: 2006-02-08 20:50:47 $
-|     $Author: streaky $
+|     $Revision: 1.7 $
+|     $Date: 2006-06-08 02:32:33 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
@@ -21,18 +21,20 @@ if (!defined('e107_INIT')) { exit; }
 
 $truncate = 100;
 $truncate_string = " ...[more]";
-$items = 10;
+// $items = 2;
 
-$NEWSFEED_MENU_START = "
-<div style='text-align: center; margin-left: auto; margin-right: auto;'>{FEEDIMAGE}<br /><b>{FEEDTITLE}</b></div>\n<br />\n";
+$NEWSFEED_MENU_START = "\n\n<!-- Start NewsFeed Menu -->
+<div style='text-align: center; margin-left: auto; margin-right: auto;'>{FEEDIMAGE}<br /><b>{FEEDTITLE}</b></div>\n<br />
+<table style='width:100%'>
+";
 
 $NEWSFEED_MENU = "
-<b>&raquo;</b>{FEEDITEMLINK}<br /><span class='smalltext'>{FEEDITEMTEXT}</span><br />\n";
+<tr><td style='vertical-align:top;width:5%'><b>&raquo;</b></td><td>{FEEDITEMLINK}<br /><span class='smalltext'>{FEEDITEMTEXT}</span></td></tr>\n";
 
+$NEWSFEED_MENU_END = "</table><br />
 
-$NEWSFEED_MENU_END = "
-
-<div style='text-align: right;'><span class='smalltext'>{FEEDLASTBUILDDATE}<br />{LINKTOMAIN}</span></div>\n<hr />";
+<div style='text-align: center;'><hr /><span class='smalltext'>{FEEDLASTBUILDDATE}<br />{LINKTOMAIN}</span></div>\n
+<!-- End News Feed Menu -->\n\n";
 
 
 ?>
