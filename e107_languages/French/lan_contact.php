04a5e2995351d370fd8fe5a439b9eb2b0806b6d1
e107coders <e107coders@localhost>
2006-06-16 21:30:07 +0000

    Syntax error, breaking page load. Fixed.
    svn path=/trunk/e107_langpacks/; revision=6701

--- e107_languages/French/lan_contact.php
+++ e107_languages/French/lan_contact.php
@@ -4,13 +4,13 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/French/lan_contact.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2006-06-10 03:15:42 $
-|     $Author: daddycool78 $
+|     $Revision: 1.3 $
+|     $Date: 2006-06-16 21:30:07 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
-define("LANCONTACT_01", "Contact);
+define("LANCONTACT_01", "Contact");
 define("LANCONTACT_02", "Formulaire Contact");
 define("LANCONTACT_03", "Entrez votre nom:");
 define("LANCONTACT_04", "Email:");
@@ -22,6 +22,6 @@ define("LANCONTACT_09", "Message envoyé.");
 define("LANCONTACT_10", "Erreur, votre message n'a pas pu nous parvenir.");
 define("LANCONTACT_11", "Votre adresse email ne semble pas être valide.\\nVeuillez vérifier et recommencer.");
 define("LANCONTACT_12", "Message trop court.");
-define("LANCONTACT_13", "Aucun sujet !? Veuillez le présicer."); 
+define("LANCONTACT_13", "Aucun sujet !? Veuillez le présicer.");
 
 ?>
