3feda8b348ba4f56d88bff7992341c6c8d336184
mcfly_e107 <mcfly_e107@localhost>
2006-06-16 16:53:23 +0000

    Increase field size due to issue reported on IRC
    svn path=/trunk/e107_0.7/; revision=6698

--- e107_plugins/alt_auth/plugin.php
+++ e107_plugins/alt_auth/plugin.php
@@ -52,7 +52,7 @@ $eplug_tables = array(
 "CREATE TABLE ".MPREFIX."alt_auth (
   auth_type varchar(20) NOT NULL default '',
   auth_parmname varchar(30) NOT NULL default '',
-  auth_parmval varchar(60) NOT NULL default ''
+  auth_parmval varchar(120) NOT NULL default ''
 ) TYPE=MyISAM;");
 
 
