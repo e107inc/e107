ecbc84c73544e619e0008684a23f6d3c13bcd682
lisa_ <lisa_@localhost>
2006-06-20 08:15:14 +0000

    rss: e_rss for calendar
    svn path=/trunk/e107_0.7/; revision=6717

---
+++ e107_plugins/calendar_menu/e_rss.php
@@ -0,0 +1,56 @@
+<?php
+
+if (!defined('e107_INIT')) { exit; }
+
+//##### create feed for admin, return array $eplug_rss_feed --------------------------------
+$feed['name']		= 'calendar';
+$feed['url']		= 'calendar';			//the identifier for the rss feed url
+$feed['topic_id']	= '';					//the topic_id, empty on default (to select a certain category)
+$feed['path']		= 'calendar_menu';		//this is the plugin path location
+$feed['text']		= 'this is the rss feed for the calendar entries';
+$feed['class']		= '0';
+$feed['limit']		= '9';
+//##### ------------------------------------------------------------------------------------
+
+
+//##### create rss data, return as array $eplug_rss_data -----------------------------------
+$todayarray		= getdate();
+$current_day	= $todayarray['mday'];
+$current_month	= $todayarray['mon'];
+$current_year	= $todayarray['year'];
+$current		= mktime(0,0,0,$current_month, $current_day, $current_year);
+
+$qry = "
+SELECT e.*, c.event_cat_name
+FROM #event AS e 
+LEFT JOIN #event_cat AS c ON c.event_cat_id = e.event_category 
+WHERE e.event_start>='$current' AND c.event_cat_class REGEXP '".e_CLASS_REGEXP."' 
+ORDER BY e.event_start ASC LIMIT 0,".$this->limit;
+
+$rss = array();
+$sqlrss = new db;
+if($items = $sqlrss->db_Select_gen($qry)){
+	$i=0;
+	while($rowrss = $sqlrss -> db_Fetch()){
+		$tmp						= explode(".", $rowrss['event_author']);
+		$rss[$i]['author']			= $tmp[1];
+		$rss[$i]['author_email']	= '';
+		$rss[$i]['link']			= $e107->base_path.$PLUGINS_DIRECTORY."calendar_menu/event.php?".$rowrss['event_start'].".event.".$rowrss['event_id'];
+		$rss[$i]['linkid']			= $rowrss['event_id'];
+		$rss[$i]['title']			= $rowrss['event_title'];
+		$rss[$i]['description']		= '';
+		$rss[$i]['category_name']	= $rowrss['event_cat_name'];
+		$rss[$i]['category_link']	= '';
+		$rss[$i]['datestamp']		= $rowrss['event_start'];
+		$rss[$i]['enc_url']			= "";
+		$rss[$i]['enc_leng']		= "";
+		$rss[$i]['enc_type']		= "";
+		$i++;
+	}
+}
+//##### ------------------------------------------------------------------------------------
+
+$eplug_rss_feed[] = $feed;
+$eplug_rss_data[] = $rss;
+
+?>
