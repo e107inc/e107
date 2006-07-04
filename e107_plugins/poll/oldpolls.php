e7a81f324755213fade6fea613c4fef37c0d0d32
e107coders <e107coders@localhost>
2006-07-04 17:04:58 +0000

    bug #2932 - missing LAN added.
    svn path=/trunk/e107_0.7/; revision=6816

--- e107_plugins/poll/oldpolls.php
+++ e107_plugins/poll/oldpolls.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/poll/oldpolls.php,v $
-|     $Revision: 1.8 $
-|     $Date: 2006-01-05 09:06:46 $
-|     $Author: sweetas $
+|     $Revision: 1.9 $
+|     $Date: 2006-07-04 17:04:58 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 require_once("../../class2.php");
@@ -28,7 +28,7 @@ $gen = new convert;
 
 if(e_QUERY)
 {
-	$query = "SELECT p.*, u.user_name FROM #polls AS p 
+	$query = "SELECT p.*, u.user_name FROM #polls AS p
 	LEFT JOIN #user AS u ON p.poll_admin_id = u.user_id
 	WHERE p.poll_type=1 AND p.poll_id=".intval(e_QUERY);
 
@@ -39,7 +39,7 @@ if(e_QUERY)
 		extract($row);
 
 		$optionArray = explode(chr(1), $poll_options);
-		$optionArray = array_slice($optionArray, 0, -1);      
+		$optionArray = array_slice($optionArray, 0, -1);
 		$voteArray = explode(chr(1), $poll_votes);
 		$voteArray = array_slice($voteArray, 0, -1);
 
@@ -59,7 +59,7 @@ if(e_QUERY)
 		<b>".$tp -> toHTML($poll_title)."</b>
 		<div class='smalltext'>".POLLAN_35." <a href='".e_BASE."user.php?id.$user_id'>".$user_name."</a>.<br /> ".POLLAN_37." ".$start_datestamp." ".POLLAN_38." ".$end_datestamp.".<br />".POLLAN_26.": $voteTotal</div>
 		<br />
-		 
+
 		</td>
 		</tr>";
 
@@ -74,15 +74,15 @@ if(e_QUERY)
 			$text .= "<tr>
 			<td style='width:40%; text-align: right' class='mediumtext'><b>".$tp -> toHTML($option)."</b>&nbsp;&nbsp;</td>
 			<td class='smalltext'>
-			
-		 
+
+
 			<div style='background-image: url($barl); width: 5px; height: 14px; float: left;'></div><div style='background-image: url($bar); width: ".(floor($percentage[$count]) != 100 ? floor($percentage[$count]) : 95)."%; height: 14px; float: left;'></div><div style='background-image: url($barr); width: 5px; height: 14px; float: left;'></div>".$percentage[$count]."% [Votes: ".$voteArray[$count]."]</div>
 			</td>
 			</tr>\n";
 			$count++;
 
 		}
-	
+
 		if($comment_total = $sql -> db_Select("comments", "*", "comment_item_id=".intval($poll_id)." AND comment_type=4 ORDER BY comment_datestamp"))
 		{
 			$text .= "<tr><td colspan='2'><br /><br />";
@@ -91,15 +91,15 @@ if(e_QUERY)
 			}
 			$text .= "</td></tr>";
 		}
-	 
+
 		$text .= "</table>";
 		$ns->tablerender(POLL_ADLAN01." #".$poll_id, $text);
 	}
 }
 
-$query = "SELECT p.*, u.user_name FROM #polls AS p 
+$query = "SELECT p.*, u.user_name FROM #polls AS p
 LEFT JOIN #user AS u ON p.poll_admin_id = u.user_id
-WHERE p.poll_type=1 
+WHERE p.poll_type=1
 ORDER BY p.poll_datestamp DESC";
 
 if(!$sql->db_Select_gen($query))
@@ -135,10 +135,10 @@ foreach($oldpollArray as $oldpoll)
 	$text .= "<tr>
 	<td class='forumheader3' style='width: 55%;'><a href='".e_SELF."?$poll_id'>$poll_title</a></td>
 	<td class='forumheader3' style='width: 15%;'><a href='".e_BASE."user.php?id.$poll_admin_id'>$user_name</a></td>
-	<td class='forumheader3' style='width: 30%;'>$from to $to</td>
+	<td class='forumheader3' style='width: 30%;'>$from ".POLLAN_38." $to</td>
 	</tr>\n";
 }
-	
+
 $text .= "</table>";
 $ns->tablerender(POLLAN_28, $text);
 require_once(FOOTERF);
