6ec150298f83bc61d50741627f2ea05c725b1ce5
e107coders <e107coders@localhost>
2006-07-16 19:41:11 +0000

    templated the 'logged in' status.
    svn path=/trunk/e107_0.7/; revision=6907

--- e107_plugins/login_menu/login_menu_shortcodes.php
+++ e107_plugins/login_menu/login_menu_shortcodes.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/login_menu_shortcodes.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2006-04-24 19:04:36 $
-|     $Author: sweetas $
+|     $Revision: 1.4 $
+|     $Date: 2006-07-16 19:41:11 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 if (!defined('e107_INIT')) { exit; }
@@ -47,7 +47,7 @@ SC_BEGIN LM_REMEMBERME
 global $pref;
 if($pref['user_tracking'] != "session")
 {
-	return "<br /><input type='checkbox' name='autologin' value='1' checked='checked' />".LOGIN_MENU_L6;
+	return "<input type='checkbox' name='autologin' value='1' checked='checked' />".LOGIN_MENU_L6;
 }
 SC_END
 
@@ -63,6 +63,70 @@ SC_END
 
 SC_BEGIN LM_FPW_LINK
 return "<a class='login_menu_link' href='".e_BASE."fpw.php'>".LOGIN_MENU_L4."</a>";
+SC_END
+
+SC_BEGIN LM_MAINTENANCE
+global $pref;
+if(ADMIN == TRUE){
+	return ($pref['maintainance_flag'] == 1 ? '<div style="text-align:center"><strong>'.LOGIN_MENU_L10.'</strong></div><br />' : '' );
+}
+SC_END
+
+SC_BEGIN LM_ADMINLINK_BULLET
+global $bullet;
+if(ADMIN==TRUE && $bullet !='bullet'){
+	return $bullet;
+}
+SC_END
+
+SC_BEGIN LM_ADMINLINK
+global $ADMIN_DIRECTORY;
+if(ADMIN == TRUE){
+		if (strpos(e_SELF, $ADMIN_DIRECTORY) === FALSE)
+		{
+			return '<a class="login_menu_link" href="'.e_ADMIN_ABS.'admin.php">'.LOGIN_MENU_L11.'</a>';
+		}
+		else
+		{
+			return '<a class="login_menu_link" href="'.e_BASE.'index.php">'.LOGIN_MENU_L39.'</a>';
+		}
+}
+SC_END
+
+
+
+SC_BEGIN LM_BULLET
+global $bullet;
+return $bullet;
+
+SC_END
+
+SC_BEGIN LM_USERSETTINGS
+$text = ($parm) ? $parm : LOGIN_MENU_L12;
+return '<a class="login_menu_link" href="'.e_HTTP.'usersettings.php">'.$text.'</a>';
+SC_END
+
+SC_BEGIN LM_PROFILE
+$text = ($parm) ? $parm : LOGIN_MENU_L13;
+return '<a class="login_menu_link" href="'.e_HTTP.'user.php?id.'.USERID.'">'.$text.'</a>';
+SC_END
+
+SC_BEGIN LM_LOGOUT
+$text = ($parm) ? $parm : LOGIN_MENU_L8;
+return '<a class="login_menu_link" href="'.e_HTTP.'index.php?logout">'.$text.'</a>';
+SC_END
+
+SC_BEGIN LM_MESSAGE
+global $tp;
+if($parm == "popup"){
+	$srch = array("<br />","'");
+	$rep = array("\\n","\'");
+	return "<script type='text/javascript'>
+		alert('".$tp->toJS(LOGINMESSAGE)."');
+		</script>";
+}else{
+	return LOGINMESSAGE;
+}
 SC_END
 
 */
