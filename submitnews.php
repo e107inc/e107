5b6916474ed62ff8588f1425bd5ec69e02722feb
lisa_ <lisa_@localhost>
2006-07-21 09:54:14 +0000

    bugfix #3055 : submitnews html error
    svn path=/trunk/e107_0.7/; revision=6932

--- submitnews.php
+++ submitnews.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/submitnews.php,v $
-|     $Revision: 1.14 $
-|     $Date: 2005-12-24 22:53:38 $
-|     $Author: sweetas $
+|     $Revision: 1.15 $
+|     $Date: 2006-07-21 09:54:14 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
@@ -164,7 +164,7 @@ if ($pref['subnews_htmlarea'])
     echo wysiwyg("item");
 } 
 
-$insertjs = (!$pref['subnews_htmlarea'])?"rows='15' onselect='storeCaret(this);' onclick='storeCaret(this);' onkeyup='storeCaret(this);'" : "rows='25' style='width:100%' ";
+$insertjs = (!$pref['subnews_htmlarea'])?"rows='15' onselect='storeCaret(this);' onclick='storeCaret(this);' onkeyup='storeCaret(this);'" : "rows='25' ";
 $text .= "
 	 
 	<tr>
