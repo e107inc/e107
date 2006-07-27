3ae6f36b3e6418e91191f7e4807164f5bea8207e
e107coders <e107coders@localhost>
2006-07-27 01:42:22 +0000

    Contact page can now be completely disabled. Added option to allow users to choose specific admins, userclass members etc to contact.
    svn path=/trunk/e107_0.7/; revision=6954

--- e107_languages/English/lan_contact.php
+++ e107_languages/English/lan_contact.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/lan_contact.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2006-04-25 18:01:18 $
+|     $Revision: 1.3 $
+|     $Date: 2006-07-27 01:42:22 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -22,9 +22,9 @@ define("LANCONTACT_09", "Your message was sent.");
 define("LANCONTACT_10", "There was a problem sending your message.");
 define("LANCONTACT_11", "Your email address doesn't appear to be valid.\\nPlease check it and try again.");
 define("LANCONTACT_12", "Your message is too short.");
-define("LANCONTACT_13", "Please include a subject."); 
+define("LANCONTACT_13", "Please include a subject.");
 
-
+define("LANCONTACT_14", "Send message to:");
 
 
 
