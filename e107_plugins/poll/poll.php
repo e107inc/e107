5539d85eea66b4bfbfc6caa787cb803c60bcee58
e107coders <e107coders@localhost>
2006-08-10 16:39:11 +0000

    Allow poll to be used as a plugin as well as a menu.
    svn path=/trunk/e107_0.7/; revision=6992

---
+++ e107_plugins/poll/poll.php
@@ -0,0 +1,28 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.7/e107_plugins/poll/poll.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-08-10 16:39:11 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+require_once("../../class2.php");
+require_once(HEADERF);
+
+require_once(e_PLUGIN."poll/poll_menu.php");
+
+
+require_once(FOOTERF);
+exit;
+
+?>
