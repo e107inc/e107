3d69ff91abe5aa34ebfb1738143a8067bbb59821
verant <verant@localhost>
2006-09-15 10:16:03 +0000

    Help files added
    svn path=/trunk/e107_langpacks/; revision=7061

---
+++ e107_languages/Russian/admin/help/cpage.php
@@ -0,0 +1,17 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     Russian Language Pack for e107 0.7
+|     $Revision: 1.1 $
+|     $Date: 2006-09-15 10:16:03 $
+|     $Author: verant $
++----------------------------------------------------------------------------+
+*/
+
+if (!defined('e107_INIT')) { exit; }
+
+$text = "На этой странице вы можете создать собственные меню и выборочные страницы со своим собственным содержанием.<br /><br />";
+//Для подробностей смотрите <a href='http://docs.e107.org/Using Custom Pages and Custom Menus'>http://docs.e107.org/Using Custom Pages and Custom Menus</a>.";
+
+$ns -> tablerender(CUSLAN_18, $text);
+?>
