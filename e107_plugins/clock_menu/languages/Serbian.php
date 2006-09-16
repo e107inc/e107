7111b2d3870ed0d0cdcb85972e246700161fe8ac
e107coders <e107coders@localhost>
2006-09-16 18:13:13 +0000

    Couple of Serbian plugin translations
    svn path=/trunk/e107_langpacks/; revision=7073

---
+++ e107_plugins/clock_menu/languages/Serbian.php
@@ -0,0 +1,46 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/clock_menu/languages/Serbian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-09-16 18:13:13 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+
+define('CLOCK_MENU_L1', 'Satne Menu Konfiguracije sačuvane');
+define('CLOCK_MENU_L2', 'Natpis');
+define('CLOCK_MENU_L3', 'Obnovite Menu Postavke');
+define('CLOCK_MENU_L4', 'Satne Menu Konfiguracije');
+define('CLOCK_MENU_L5', 'Ponedjeljak,');
+define('CLOCK_MENU_L6', 'Utorak,');
+define('CLOCK_MENU_L7', 'Sreda,');
+define('CLOCK_MENU_L8', 'Četvrtak,');
+define('CLOCK_MENU_L9', 'Petak,');
+define('CLOCK_MENU_L10', 'Subota,');
+define('CLOCK_MENU_L11', 'Nedelja,');
+define('CLOCK_MENU_L12', 'Januar');
+define('CLOCK_MENU_L13', 'Februar');
+define('CLOCK_MENU_L14', 'Mart');
+define('CLOCK_MENU_L15', 'April');
+define('CLOCK_MENU_L16', 'Maj');
+define('CLOCK_MENU_L17', 'Jun');
+define('CLOCK_MENU_L18', 'Jul');
+define('CLOCK_MENU_L19', 'Avgust');
+define('CLOCK_MENU_L20', 'Septembar');
+define('CLOCK_MENU_L21', 'Oktobar');
+define('CLOCK_MENU_L22', 'Novembar');
+define('CLOCK_MENU_L23', 'decembar');
+define('CLOCK_MENU_L24', '');
+
+
+?>
