4353d8eb8eb7d0266e0c24afd715322ce4dd21dd
verant <verant@localhost>
2006-09-17 07:00:30 +0000

    Some corrections
    svn path=/trunk/e107_langpacks/; revision=7078

--- e107_languages/Russian/admin/lan_banlist.php
+++ e107_languages/Russian/admin/lan_banlist.php
@@ -2,21 +2,20 @@
 /*
 + ----------------------------------------------------------------------------+
 |     Russian Language Pack for e107 0.7
-|     $Revision: 1.1 $
-|     $Date: 2006-09-11 14:35:47 $
+|     $Revision: 1.2 $
+|     $Date: 2006-09-17 07:00:17 $
 |     $Author: verant $
 +----------------------------------------------------------------------------+
 */
 
-define("BANLAN_1", "Бан удалён.");
-define("BANLAN_2", "Нет банов.");
-define("BANLAN_3", "Существующие баны");
-define("BANLAN_4", "Удалить бан");
+define("BANLAN_1", "Запрет удалён."); //Ban removed.
+define("BANLAN_2", "Нет запретов."); //No bans.
+define("BANLAN_3", "Существующие запреты"); //Existing Bans
+define("BANLAN_4", "Удалить запрет"); //Remove ban
 define("BANLAN_5", "Введите IP, email-адрес или хост");
 define("BANLAN_7", "Причина");
-define("BANLAN_8", "Банить пользователя");
-define("BANLAN_9", "Забаненные на сайте пользователи");
+define("BANLAN_8", "Запретить пользователя"); //Ban User
+define("BANLAN_9", "Запретить пользователей с сайта"); //Ban users from site
 define("BANLAN_10", "IP / Email / Причина");
-define("BANLAN_11", "Авто-бан: Более чем 10 неудавшихся попыток логина");
-
+define("BANLAN_11", "Авто-запрет: Более чем 10 неудавшихся попыток логина"); //Auto-ban
 ?>
