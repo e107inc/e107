8c2fb9bce7abc9dea67e8fd15f189a97429c26f1
lisa_ <lisa_@localhost>
2006-09-18 08:57:42 +0000

    content: added category heading to search results
    svn path=/trunk/e107_0.7/; revision=7084

--- e107_plugins/content/languages/English/lan_content_search.php
+++ e107_plugins/content/languages/English/lan_content_search.php
@@ -4,14 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/content/languages/English/lan_content_search.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-15 15:18:40 $
-|     $Author: sweetas $
+|     $Revision: 1.3 $
+|     $Date: 2006-09-18 08:57:42 $
+|     $Author: lisa_ $
 +----------------------------------------------------------------------------+
 */
 
 define("CONT_SCH_LAN_1", "Content");
 define("CONT_SCH_LAN_2", "All Content Categories");
 define("CONT_SCH_LAN_3", "Posted in reply to item");
+define("CONT_SCH_LAN_4", "in");
 
 ?>
