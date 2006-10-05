9c9ee87483b58491c424b722b2068f001cf3c4a9
verant <verant@localhost>
2006-10-05 14:25:52 +0000

    New translation files
    svn path=/trunk/e107_langpacks/; revision=7119

---
+++ e107_plugins/alt_auth/languages/Russian/lan_ldap_auth.php
@@ -0,0 +1,22 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     Russian Language Pack for e107 0.7
+|     $Revision: 1.1 $
+|     $Date: 2006-10-05 14:25:52 $
+|     $Author: verant $
++----------------------------------------------------------------------------+
+*/
+
+define("LDAPLAN_1", "Адрес сервера");
+define("LDAPLAN_2", "BaseDN или Domain<br />Если LDAP - введите BaseDN<br />Если AD - введите Domain");
+define("LDAPLAN_3", "LDAP Browsing user<br />Полный контекст пользователя, который может искать в директории"); //Full context of the user who is able to search the directory.
+define("LDAPLAN_4", "LDAP Browsing password<br />Пароль для LDAP Browsing user.");
+define("LDAPLAN_5", "LDAP Version");
+define("LDAPLAN_6", "Конфигурировать LDAP auth");
+
+define("LDAPLAN_7", "eDirectory фильтр поиска:");
+define("LDAPLAN_8", "Будет использоваться, чтобы убедиться, что имя пользователя в правильном дереве<br />т. е. '(objectclass=inetOrgPerson)'"); //This will be used to ensure the username is in the correct tree, <br />ie '(objectclass=inetOrgPerson)'
+define("LDAPLAN_9", "Текущия фильтр поиска будет:"); //Current search filter will be
+
+?>
