0ef02f34165a1b596931c709a35a67fc2ce0b26a
verant <verant@localhost>
2006-10-15 06:01:56 +0000

    Some corrections
    svn path=/trunk/e107_langpacks/; revision=7169

--- e107_themes/jayya/languages/Russian.php
+++ e107_themes/jayya/languages/Russian.php
@@ -1,13 +1,18 @@
-﻿<?php
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     Russian Language Pack for e107 0.7
+|     $Revision: 1.2 $
+|     $Date: 2006-10-15 06:01:56 $
+|     $Author: verant $
++----------------------------------------------------------------------------+
+*/
 
-## Russian Language Pack for e107 Version 0.700
-##      Copyright © 2005 - Russian e107
-##	     	  http://e107.org.ru
-
-define("LAN_THEME_1", "Комментарии выключенны");
-define("LAN_THEME_2", "Читать/Добавить комментарий: ");
-define("LAN_THEME_3", "Читать далее...");
-define("LAN_THEME_4", "Отслеживания: ");
-
+define("LAN_THEME_1", "Комментарии выключенны"); //Comments are turned off for this item
+define("LAN_THEME_2", "Читать/Добавить комментарий: "); //Read/Post Comment: 
+define("LAN_THEME_3", "Читать полностью..."); //Read the rest...
+define("LAN_THEME_4", "Ссылок: "); //Trackbacks: 
+define("LAN_THEME_5", "Прислано "); //Posted by
+define("LAN_THEME_6", "от "); //on 
 
 ?>
