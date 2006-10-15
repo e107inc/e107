e95f42a2bcea027d92c21bb7d65a3dd8db3938f9
yarodin <yarodin@localhost>
2006-10-15 14:23:35 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7174

--- e107_plugins/rss_menu/languages/Russian.php
+++ e107_plugins/rss_menu/languages/Russian.php
@@ -2,23 +2,23 @@
 /*
 + ----------------------------------------------------------------------------+
 |     Russian Language Pack for e107 0.7
-|     $Revision: 1.1 $
-|     $Date: 2006-09-20 14:49:20 $
-|     $Author: verant $
+|     $Revision: 1.2 $
+|     $Date: 2006-10-15 14:23:35 $
+|     $Author: yarodin $
 +----------------------------------------------------------------------------+
 */
 
-define("RSS_LAN05","Кол-во элементов (0=неактивно)"); //Number of items (0=inactive)
+define("RSS_LAN05","Кол-во элементов (0=неактивно)");
 
-define("RSS_MENU_L1", " могут транслироваться, используя rss-транслятор."); //can be syndicated by using these rss feeds.
-define("RSS_MENU_L2", "RSS-Транслятор"); //RSS Feeds
+define("RSS_MENU_L1", " могут транслироваться, используя rss.");
+define("RSS_MENU_L2", "RSS трансляции");
 define("RSS_MENU_L3", "Наши новости");
 define("RSS_MENU_L4", "Наши комментарии");
 define("RSS_MENU_L5", "Темы нашего форума");
 define("RSS_MENU_L6", "Сообщения нашего форума");
-define("RSS_MENU_L7", "Сообщения нашего миничата"); //Our chatbox posts
-define("RSS_MENU_L8", "Сообщения нашего bugtracker'a"); //Our bugtracker reports
-define("RSS_MENU_L9", "Наши загрузки"); //Our downloads
+define("RSS_MENU_L7", "Сообщения нашего чата");
+define("RSS_MENU_L8", "Сообщения нашей системы отслеживания ошибок");
+define("RSS_MENU_L9", "Наши загрузки");
 
 define("RSS_NEWS", "Новости");
 define("RSS_COM", "Комментарии");
@@ -27,63 +27,63 @@ define("RSS_ART", "Статьи");
 define("RSS_REV", "Обзоры");
 define("RSS_FT","Темы форума");
 define("RSS_FP","Сообщения форума");
-define("RSS_FSP","Специфические сообщения форума");
-define("RSS_BUG","Bugtracker");
+define("RSS_FSP","Определенные сообщения форума");
+define("RSS_BUG","Отслеживание ошибок");
 define("RSS_FOR","Форум");
 define("RSS_DL","Загрузки");
 
 define("RSS_PLUGIN_LAN_1", "RSS");
 
-define("RSS_PLUGIN_LAN_6", "Ссылки трансляции"); //Feed Links
-define("RSS_PLUGIN_LAN_7", "Трансляция rss для новостей");
-define("RSS_PLUGIN_LAN_8", "Трансляция rss для загрузок");
-define("RSS_PLUGIN_LAN_9", "Трансляция rss для комментариев");
-define("RSS_PLUGIN_LAN_10", "Трансляция rss для категорий новостей:");
-define("RSS_PLUGIN_LAN_11", "Трансляция rss для категорий загрузок:");
+define("RSS_PLUGIN_LAN_6", "Транслируемые ленты");
+define("RSS_PLUGIN_LAN_7", "Трансляция новостей");
+define("RSS_PLUGIN_LAN_8", "Трансляция загрузок");
+define("RSS_PLUGIN_LAN_9", "Трансляция комментариев");
+define("RSS_PLUGIN_LAN_10", "Трансляция категорий новостей:");
+define("RSS_PLUGIN_LAN_11", "Трансляция категорий загрузок:");
 
 define("RSS_PLUGIN_LAN_14", "Комментарии");
 
-define("RSS_LAN_ADMINMENU_1", "RSS Свойства");
+define("RSS_LAN_ADMINMENU_1", "Свойства RSS");
 define("RSS_LAN_ADMINMENU_2", "Список"); 
-define("RSS_LAN_ADMINMENU_4", "Импортировать"); //Import
+define("RSS_LAN_ADMINMENU_4", "Импортировать");
 
-define("RSS_LAN_ERROR_1", "Это не является правильной rss трансляцией<br /><br /><a href='".e_SELF."'><< вернуться к списку rss трансляций</a>");
+define("RSS_LAN_ERROR_1", "Некорректная rss трансляция<br /><br /><a href='".e_SELF."'><< вернуться к списку rss трансляций</a>");
 //Not translated yet:
-define("RSS_LAN_ERROR_2", "Your e107_config.php file contains space before the &lt;? characters. You should remove this space if you wish to have a valid RSS feed.");
-define("RSS_LAN_ERROR_3", "No rss feeds are present yet<br />please use the import feature to import available rss feeds or create a rss feed manually.");
-define("RSS_LAN_ERROR_4", "No rss feeds are available yet");
-define("RSS_LAN_ERROR_5", "This rss entry does not exist");
-define("RSS_LAN_ERROR_6", "There are no rss feeds to import");
-define("RSS_LAN_ERROR_7", "Some required fields are missing.");
+define("RSS_LAN_ERROR_2", "В файле e107_config.php перед символами &lt;? находятся пробелы. Вы должны убрать эти пробелы, чтобы получить корректные RSS трансляции.");
+define("RSS_LAN_ERROR_3", "Пока еще не существует rss трансляций,<br />пожалуйста, используйте функцию импорта для того, чтобы импортировать доступные rss трансляции или создайте rss трансляцию вручную.");
+define("RSS_LAN_ERROR_4", "Пока еще нет доступных rss траснляций");
+define("RSS_LAN_ERROR_5", "Эта rss трансялция не существует");
+define("RSS_LAN_ERROR_6", "Нет rss трансляций для импорта");
+define("RSS_LAN_ERROR_7", "Некоторые необходимые поля не заполнены.");
 
-define("RSS_LAN_ADMIN_1", "Existing RSS feeds");
-define("RSS_LAN_ADMIN_2", "Id");
-define("RSS_LAN_ADMIN_3", "Path");
-define("RSS_LAN_ADMIN_4", "Name");
-define("RSS_LAN_ADMIN_5", "Url");
-define("RSS_LAN_ADMIN_6", "Text");
-define("RSS_LAN_ADMIN_7", "Limit");
-define("RSS_LAN_ADMIN_8", "Visibility");
-define("RSS_LAN_ADMIN_9", "Type");
-define("RSS_LAN_ADMIN_10", "rss feed create entry");
-define("RSS_LAN_ADMIN_11", "rss feed import feeds");
-define("RSS_LAN_ADMIN_12", "Topic id");
+define("RSS_LAN_ADMIN_1", "Существующие RSS трансляции");
+define("RSS_LAN_ADMIN_2", "Ид");
+define("RSS_LAN_ADMIN_3", "Путь");
+define("RSS_LAN_ADMIN_4", "Имя");
+define("RSS_LAN_ADMIN_5", "Адрес");
+define("RSS_LAN_ADMIN_6", "Текст");
+define("RSS_LAN_ADMIN_7", "Лимит");
+define("RSS_LAN_ADMIN_8", "Видимость");
+define("RSS_LAN_ADMIN_9", "Тип");
+define("RSS_LAN_ADMIN_10", "Создание rss трансляции");
+define("RSS_LAN_ADMIN_11", "Импортирование rss трансляции");
+define("RSS_LAN_ADMIN_12", "Ид темы");
 
-define("RSS_LAN_ADMIN_13", "Include Other-news items in News Feed?");
-define("RSS_LAN_ADMIN_14", "Enable");
-define("RSS_LAN_ADMIN_15", "Tick links to mark them for import ...");
-define("RSS_LAN_ADMIN_16", "import?");
-define("RSS_LAN_ADMIN_17", "import ticked links");
-define("RSS_LAN_ADMIN_18", "rss feed(s) imported.");
+define("RSS_LAN_ADMIN_13", "Включить пункт Другие-новости в трансляцию Новостей?");
+define("RSS_LAN_ADMIN_14", "Включено");
+define("RSS_LAN_ADMIN_15", "Выберите трансляции для импорта ...");
+define("RSS_LAN_ADMIN_16", "Импортировать?");
+define("RSS_LAN_ADMIN_17", "Импортировать выбранные трансляции");
+define("RSS_LAN_ADMIN_18", "rss трансляция(ии) импортированы.");
 
-define("RSS_LAN_ADMIN_21", "active and visible in rss feed list");
-define("RSS_LAN_ADMIN_22", "active and not visible in rss feed list");
-define("RSS_LAN_ADMIN_23", "inactive");
+define("RSS_LAN_ADMIN_21", "активна и видима в списке rss трансляций");
+define("RSS_LAN_ADMIN_22", "активна и невидима в списке rss трансляций");
+define("RSS_LAN_ADMIN_23", "неактивна");
 
-define("RSS_LAN_ADMIN_26", "Check All");
-define("RSS_LAN_ADMIN_27", "Uncheck All");
+define("RSS_LAN_ADMIN_26", "Выбрать Все");
+define("RSS_LAN_ADMIN_27", "Отменить выбор");
 
-define("RSS_LAN_ADMIN_31", "rss entries limit updated");
+define("RSS_LAN_ADMIN_31", "Лимит записей rss трансляции обновлен");
 
 define("RSS_LAN_0", "RSS");
 define("RSS_LAN_2", "@nospam.com");
