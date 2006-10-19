0f5b72b09c46231753eaf14adc3b434ffc102a69
verant <verant@localhost>
2006-10-19 15:44:00 +0000

    New file added
    svn path=/trunk/e107_langpacks/; revision=7195

---
+++ e107_plugins/alt_auth/languages/Russian/lan_otherdb_auth.php
@@ -0,0 +1,24 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     Russian Language Pack for e107 0.7
+|     $Revision: 1.1 $
+|     $Date: 2006-10-19 15:44:00 $
+|     $Author: verant $
++----------------------------------------------------------------------------+
+*/
+
+//Not translated yet:
+define("OTHERDB_LAN_1", "Database Type:");
+define("OTHERDB_LAN_2", "Server:");
+define("OTHERDB_LAN_3", "Username:");
+define("OTHERDB_LAN_4", "Password:");
+define("OTHERDB_LAN_5", "Database");
+define("OTHERDB_LAN_6", "Table");
+define("OTHERDB_LAN_7", "Username Field:");
+define("OTHERDB_LAN_8", "Password Field:");
+define("OTHERDB_LAN_9", "Password Method:");
+define("OTHERDB_LAN_10", "Configure otherdb auth");
+define("OTHERDB_LAN_11", "** The following fields are not required if using an e107 database");
+
+?>
