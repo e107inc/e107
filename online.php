846ab7c1071f38738d0c84a7b02f2eaca61bcfb3
mrpete <mrpete@localhost>
2006-10-20 20:57:15 +0000

    Fix php isset warning
    svn path=/trunk/e107_0.7/; revision=7205

--- online.php
+++ online.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/online.php,v $
-|     $Revision: 1.13 $
-|     $Date: 2005-12-24 22:53:38 $
-|     $Author: sweetas $
+|     $Revision: 1.14 $
+|     $Date: 2006-10-20 20:57:15 $
+|     $Author: mrpete $
 +----------------------------------------------------------------------------+
 */
 require_once("class2.php");
@@ -250,7 +250,7 @@ if ((MEMBERS_ONLINE + GUESTS_ONLINE) > ($menu_pref['most_members_online'] + $men
 	$sysprefs->setArray('menu_pref');
 }
 	
-if (!is_object($gen)) {
+if (!isset($gen) || !is_object($gen)) {
 	$gen = new convert;
 }
 	
