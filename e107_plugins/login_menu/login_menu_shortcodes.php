16dd393d2cfab8750d688ae8e07a2580fe04b7b6
e107coders <e107coders@localhost>
2006-10-21 15:06:48 +0000

    Added re-send activation link
    svn path=/trunk/e107_0.7/; revision=7224

--- e107_plugins/login_menu/login_menu_shortcodes.php
+++ e107_plugins/login_menu/login_menu_shortcodes.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/login_menu/login_menu_shortcodes.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2006-07-26 00:46:51 $
+|     $Revision: 1.6 $
+|     $Date: 2006-10-21 15:06:48 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -59,13 +59,17 @@ global $pref;
 if ($pref['user_reg']) {
 	if (!$pref['auth_method'] || $pref['auth_method'] == 'e107')
 	{
-		return "<a class='login_menu_link' href='".e_SIGNUP."'>".LOGIN_MENU_L3."</a>";
+		return "<a class='login_menu_link' href='".e_SIGNUP."' title=\"".LOGIN_MENU_L3."\">".LOGIN_MENU_L3."</a>";
 	}
 }
 SC_END
 
 SC_BEGIN LM_FPW_LINK
-return "<a class='login_menu_link' href='".e_BASE."fpw.php'>".LOGIN_MENU_L4."</a>";
+return "<a class='login_menu_link' href='".e_BASE."fpw.php' title=\"".LOGIN_MENU_L4."\">".LOGIN_MENU_L4."</a>";
+SC_END
+
+SC_BEGIN LM_RESEND_LINK
+return "<a class='login_menu_link' href='".e_SIGNUP."?resend' title=\"".LOGIN_MENU_L40."\">".LOGIN_MENU_L40."</a>";
 SC_END
 
 SC_BEGIN LM_MAINTENANCE
