4c096d6bef34f379bad3c56d5ffdc1968e83ec07
e107coders <e107coders@localhost>
2006-10-25 17:05:15 +0000

    Bug #2521 - missing LAN.
    svn path=/trunk/e107_0.7/; revision=7266

--- e107_plugins/forum/e_latest.php
+++ e107_plugins/forum/e_latest.php
@@ -6,9 +6,9 @@ $text .= "<div style='padding-bottom: 2px;'>
 <img src='".e_PLUGIN."forum/images/forums_16.png' style='width: 16px; height: 16px; vertical-align: bottom' alt='' /> ";
 
 if ($reported_posts) {
-	$text .= "<a href='".e_PLUGIN."forum/forum_admin.php?sr'>Reported forum posts: ".$reported_posts."</a>";
+	$text .= "<a href='".e_PLUGIN."forum/forum_admin.php?sr'>".ADLAN_LAT_6.": ".$reported_posts."</a>";
 } else {
-	$text .= 'Reported forum posts: '.$reported_posts;
+	$text .= ADLAN_LAT_6.": ".$reported_posts;
 }
 
 $text .= '</div>';
