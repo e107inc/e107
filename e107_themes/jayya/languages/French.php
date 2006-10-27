5305858437bd124e746268c80c407dd7158eb5ea
daddycool78 <daddycool78@localhost>
2006-10-27 14:42:10 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7286

--- e107_themes/jayya/languages/French.php
+++ e107_themes/jayya/languages/French.php
@@ -3,6 +3,6 @@
   define("LAN_THEME_2", "Lire/poster Commentaires: ");
   define("LAN_THEME_3", "Suite...");
   define("LAN_THEME_4", "Trackbacks: ");
-  define("LAN_THEME_5", "Posté par");
+  define("LAN_THEME_5", "Posté par ");
   define("LAN_THEME_6", "");
   ?>
