d0e61b0a6ba296d25b5be98275bc60d400c0f1d6
e107coders <e107coders@localhost>
2006-10-30 02:16:58 +0000

    Bug #3363 - gsitemap frequency values corrected.
    svn path=/trunk/e107_0.7/; revision=7329

--- e107_plugins/gsitemap/admin_config.php
+++ e107_plugins/gsitemap/admin_config.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/gsitemap/admin_config.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2006-06-07 22:27:40 $
+|     $Revision: 1.11 $
+|     $Date: 2006-10-30 02:16:58 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -135,7 +135,7 @@ class gsitemap
 
 				$text .= "<tr>
 				<td class='forumheader3' style='; text-align: center;'>".$row2['gsitemap_id'] ."</td>
-				<td class='forumheader3'>".$row2['gsitemap_name']."</td>
+				<td class='forumheader3'>".$tp->toHTML($row2['gsitemap_name'],"","defs")."</td>
 				<td class='forumheader3'>".$row2['gsitemap_url']."</td>
 				<td class='forumheader3' style='; text-align: center;'>".$datestamp."</td>
 				<td class='forumheader3' style='; text-align: center;'>".$row2['gsitemap_freq'] ."</td>
@@ -207,11 +207,19 @@ class gsitemap
 		<td class='forumheader3'>
 		<select class='tbox' name='gsitemap_freq' >\n";
 
-		$freq_list = array(GSLAN_11, GSLAN_12, GSLAN_13,GSLAN_14,GSLAN_15,GSLAN_16,GSLAN_17);
+		$freq_list = array(
+			"always"=>GSLAN_11,
+			"hourly"=>GSLAN_12,
+			"daily"=>GSLAN_13,
+			"weekly"=>GSLAN_14,
+			"monthly"=>GSLAN_15,
+			"yearly"=>GSLAN_16,
+			"never"=>GSLAN_17
+		);
 
-		foreach($freq_list as $fq){
-			$sel = ($editArray['gsitemap_freq'] == $fq)? "selected='selected'" : "";
-			$text .= "<option value='$fq' $sel>$fq</option>\n";
+		foreach($freq_list as $k=>$fq){
+			$sel = ($editArray['gsitemap_freq'] == $k)? "selected='selected'" : "";
+			$text .= "<option value='$k' $sel>".$fq."</option>\n";
 		}
 
 		$text.="</select>
