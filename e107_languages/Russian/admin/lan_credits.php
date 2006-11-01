52337aae43a35600287f6eea1f272a6fdfddcebe
yarodin <yarodin@localhost>
2006-11-01 16:38:45 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7357

--- e107_languages/Russian/admin/lan_credits.php
+++ e107_languages/Russian/admin/lan_credits.php
@@ -2,9 +2,9 @@
 /*
 + ----------------------------------------------------------------------------+
 |     Russian Language Pack for e107 0.7
-|     $Revision: 1.2 $
-|     $Date: 2006-11-01 13:53:46 $
-|     $Author: verant $
+|     $Revision: 1.3 $
+|     $Date: 2006-11-01 16:32:14 $
+|     $Author: yarodin $
 +----------------------------------------------------------------------------+
 */
 
@@ -12,42 +12,39 @@ define("PAGE_NAME", "Благодарности e107");
 
 define("CRELAN_1", "Благодарности");
 define("CRELAN_2", "Это список стороннего ПО / ресурсов, используемых в e107. Команда разработчиков e107 хотела бы лично поблагодарить всех разработчиков за предоставленную возможность использовать их творение совместно с e107, и за выпуск их ПО по лицензии GPL.");
-define("CRELAN_3", "все права сохраняются"); //CheckIt: all rights reserved
+define("CRELAN_3", "авторские права защищены"); 
 define("CRELAN_4", "Показать команду разработчиков e107");
-define("CRELAN_5", "Показать сторонние скрипты"); //CheckIt: Show third party scripts
+define("CRELAN_5", "Показать скрипты сторонних разработчиков");
 define("CRELAN_6", "e107 v0.7 была предоставлена вам благодаря ...");
 define("CRELAN_7", "версия");
-define("CRELAN_8", "разрешения получены"); //CheckIt: permission granted
+define("CRELAN_8", "разрешается");
 define("CRELAN_9", "Лицензия");
-
-
-//Not translated yet: (may be not all need?)
 
 // third party scripts
 
-define("CRELAN_10", "MagpieRSS provides an XML-based (expat) RSS parser in PHP.");
-define("CRELAN_11", "PclZip library offers compression and extraction functions for Zip formatted archives (WinZip, PKZIP).");
-define("CRELAN_12", "PclTar offer the ability to archive a list of files or directories with or without compression. The archives created by PclTar are readable by most of gzip/tar applications and by the Windows WinZip application.");
-define("CRELAN_13", "TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor control released as Open Source under LGPL by Moxiecode Systems AB. It has the ability to convert HTML TEXTAREA fields or other HTML elements to editor instances.");
-define("CRELAN_14", "Icons used in e107");
-define("CRELAN_15", "Full featured email transfer class for PHP");
-define("CRELAN_16", "Menu system used in Jayya theme");
-define("CRELAN_17", "Popup calendar widget");
-define("CRELAN_18", "PDF support");
-define("CRELAN_19", "UTF-8 PDF support");
+define("CRELAN_10", "MagpieRSS реализует основанный на XML (expat) RSS парсер написанный на PHP.");
+define("CRELAN_11", "PclZip библиотека содержит функции предназначенные для работы с Zip архивами (WinZip, PKZIP).");
+define("CRELAN_12", "PCLTar предоставляет возможность архивирования файлов или директорий без сжатия. Архивы создаваемые PclTar поддерживаются большинством gzip/tar приложений, а также Windows WinZip'ом.");
+define("CRELAN_13", "TinyMCE платформо независимый Javascript HTML WYSIWYG веб-редактор, выпущенный как Open Source под лицензией LGPL компанией Moxiecode Systems AB. В нем есть возможность использовать HTML текстовые поля или другие HTML элементы для редактирования текста.");
+define("CRELAN_14", "Иконки используемые в e107");
+define("CRELAN_15", "Многофункциональный PHP класc для работы с электронной почтой.");
+define("CRELAN_16", "Система меню, используемая в теме Jaya.");
+define("CRELAN_17", "Виджет всплывающего календаря.");
+define("CRELAN_18", "Поддержка PDF");
+define("CRELAN_19", "Поддержка UTF-8 в PDF");
 
 // end third party scripts
 
 // dev team
 
 define("CRELAN_20", ""); // asperon
-define("CRELAN_21", "Always a pressure..err..pleasure!"); // CaMer0n
-define("CRELAN_22", "\"MTVhNjMyZDgxN2QwM2Q3ZTI<br />5ODM2NDU3YWI0ZjM1NGILJT<br />yarrrrrr! wtf matey!\""); // jalist
+define("CRELAN_21", "Всегда давление..ээээээ..наслаждение!"); // CaMer0n
+define("CRELAN_22", "\"MTVhNjMyZDgxN2QwM2Q3ZTI<br />5ODM2NDU3YWI0ZjM1NGILJT<br />Йо-хо-хо! что за хуйня, дружок!\""); // jalist
 define("CRELAN_23", ""); // lisa
 define("CRELAN_24", ""); // McFly
 define("CRELAN_25", ""); // que
 define("CRELAN_26", ""); // streaky
-define("CRELAN_27", "\"Wot? No tea?? 0_0\""); // SweetAs
+define("CRELAN_27", "\"Шо? Нет чая?? 0_0\""); // SweetAs
 define("CRELAN_28", ""); // MrPete
 
 // end dev team
