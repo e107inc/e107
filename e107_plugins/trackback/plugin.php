6a10632e895ad2a35c8f65f54ce0aa870d30beab
e107coders <e107coders@localhost>
2006-11-04 19:26:24 +0000

    Bug #2961 - added an index to the trackback plugin table, to help resolve performance issues.
    svn path=/trunk/e107_0.7/; revision=7375

--- e107_plugins/trackback/plugin.php
+++ e107_plugins/trackback/plugin.php
@@ -11,20 +11,19 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/trackback/plugin.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2005-12-14 19:28:52 $
-|     $Author: sweetas $
+|     $Revision: 1.4 $
+|     $Date: 2006-11-04 19:26:24 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-@include_once(e_PLUGIN."trackback/languages/".e_LANGUAGE.".php");
-@include_once(e_PLUGIN."trackback/languages/English.php");
+include_lan(e_PLUGIN."trackback/languages/".e_LANGUAGE.".php");
 
 // Plugin info -------------------------------------------------------------------------------------------------------
 $eplug_name = "Trackback";
-$eplug_version = "1.0";
+$eplug_version = "1.1";
 $eplug_author = "jalist";
 $eplug_url = "http://e107.org";
 $eplug_email = "jalist@e107.org";
@@ -32,31 +31,31 @@ $eplug_description = TRACKBACK_L2;
 $eplug_compatible = "e107v0.7+";
 $eplug_readme = "";
 // leave blank if no readme file
-	
+
 // Name of the plugin's folder -------------------------------------------------------------------------------------
 $eplug_folder = "trackback";
-	
+
 // Mane of menu item for plugin ----------------------------------------------------------------------------------
 $eplug_menu_name = "";
-	
+
 // Name of the admin configuration file --------------------------------------------------------------------------
 $eplug_conffile = "admin_config.php";
-	
+
 // Icon image and caption text ------------------------------------------------------------------------------------
 $eplug_icon = $eplug_folder."/images/trackback_32.png";
 $eplug_icon_small = $eplug_folder."/images/trackback_16.png";
 $eplug_caption = TRACKBACK_L1;
-	
+
 // List of preferences -----------------------------------------------------------------------------------------------
 $eplug_prefs = array(
-"trackbackEnabled" => 0, 
+"trackbackEnabled" => 0,
 "trackbackString" => "<span class='smalltext'><b>".TRACKBACK_L11."</b></span>"
 );
-	
+
 // List of table names -----------------------------------------------------------------------------------------------
 $eplug_table_names = array(
 "trackback");
-	
+
 // List of sql requests to create tables -----------------------------------------------------------------------------
 $eplug_tables = array(
 "CREATE TABLE ".MPREFIX."trackback (
@@ -66,17 +65,21 @@ $eplug_tables = array(
   trackback_excerpt varchar(250) NOT NULL default '',
   trackback_url varchar(150) NOT NULL default '',
   trackback_blogname varchar(150) NOT NULL default '',
-  PRIMARY KEY  (trackback_id)
+  PRIMARY KEY  (trackback_id),
+  KEY trackback_pid (trackback_pid)
 ) TYPE=MyISAM;");
-	
+
 // Create a link in main menu (yes=TRUE, no=FALSE) -------------------------------------------------------------
 $eplug_link = FALSE;
 $eplug_link_name = "";
 $ec_dir = "";
 $eplug_link_url = "";
-	
-	
+
+
 // Text to display after plugin successfully installed ------------------------------------------------------------------
 $eplug_done = TRACKBACK_L3;
-	
+$upgrade_alter_tables	= array(
+	"ALTER TABLE ".MPREFIX."trackback ADD INDEX (trackback_pid);"
+);
+
 ?>
