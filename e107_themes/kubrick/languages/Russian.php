ea93fa911225d4f2b4157dbd4380861b41641dc0
yarodin <yarodin@localhost>
2006-11-05 06:35:08 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7384

--- e107_themes/kubrick/languages/Russian.php
+++ e107_themes/kubrick/languages/Russian.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_themes/kubrick/languages/Russian.php,v $
+|     $Revision: 1.3 $
+|     $Date: 2006-11-05 06:35:08 $
+|     $Author: yarodin $
++----------------------------------------------------------------------------+
+*/
+
 define("LAN_THEME_1", "'kubrick' от <a href='http://e107.org' rel='external'>jalist</a>, Основано на теме Michael Heilemann (<a href='http://binarybonsai.com/kubrick/' rel='external'>http://binarybonsai.com/kubrick/</a>).");
 define("LAN_THEME_2", "Комментарии выключенны");
 define("LAN_THEME_3", "комментарии: ");
