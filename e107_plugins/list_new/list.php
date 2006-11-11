14e1b5449a561a1ac1b684e6f96f2dcd6dadbf1b
lisa_ <lisa_@localhost>
2006-11-11 15:29:16 +0000

    bugfix #3416 : list_new 'days' now has '$url' redirect
    svn path=/trunk/e107_0.7/; revision=7501

--- e107_plugins/list_new/list.php
+++ e107_plugins/list_new/list.php
@@ -11,9 +11,9 @@
 |       GNU General Public License (http://gnu.org).
 |
 |		$Source: /cvs_backup/e107_0.7/e107_plugins/list_new/list.php,v $
-|		$Revision: 1.11 $
-|		$Date: 2006-11-11 04:57:28 $
-|		$Author: mcfly_e107 $
+|		$Revision: 1.12 $
+|		$Date: 2006-11-11 15:29:16 $
+|		$Author: lisa_ $
 +---------------------------------------------------------------+
 */
 require_once("../../class2.php");
@@ -75,7 +75,7 @@ if(isset($qs[0]) && $qs[0] == "new"){
 		$selectjs	= "onchange=\"if(this.options[this.selectedIndex].value != 'none'){ return document.location=this.options[this.selectedIndex].value; }\"";
 
 		$LIST_TIMELAPSE = LIST_MENU_6;
-		$LIST_TIMELAPSE .= $rs -> form_select_open("timelapse", $selectjs).$rs -> form_option(LIST_MENU_5, 0, 'none');
+		$LIST_TIMELAPSE .= $rs -> form_select_open("timelapse", $selectjs).$rs -> form_option(LIST_MENU_5, 0, $url);
 		for($a=1; $a<=$days; $a++){
 			$LIST_TIMELAPSE .= $rs -> form_option($a, ($timelapse == $a ? "1" : "0"), $url.".".$a);
 		}
