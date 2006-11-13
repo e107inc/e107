18b241c321200cd99376b83d7e10af85a29fa453
e107coders <e107coders@localhost>
2006-11-13 10:21:17 +0000

    PHP Notice removal
    svn path=/trunk/e107_0.7/; revision=7518

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/index.php,v $
-|     $Revision: 1.25 $
-|     $Date: 2006-11-13 09:37:14 $
+|     $Revision: 1.26 $
+|     $Date: 2006-11-13 10:21:17 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
@@ -82,14 +82,14 @@ if(isset($pref['frontpage_method']) && $pref['frontpage_method'] == "include") {
 		exit();
 	}
 } else {
-	list($page,$str) = explode("?",$location); // required to prevent infinite looping when queries are  used on index.php.
+	list($page,$str) = explode("?",$location."?"); // required to prevent infinite looping when queries are  used on index.php.
 	if($page == "index.php") // Welcome Message is the front-page.
 	{
       	require_once(HEADERF);
 	 	require_once(FOOTERF);
 	  	exit;
 	}
-	elseif($page != "index.php") // redirect to different frontpage. 
+	elseif($page != "index.php") // redirect to different frontpage.
 	{
 		header("Location: {$location}");
 	}
