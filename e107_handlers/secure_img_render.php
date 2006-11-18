325ca4caebbfe5b84cd5b80015c42042d8e3cb2c
streaky <streaky@localhost>
2006-11-18 14:58:59 +0000

    Fixed an unreported issue on PHP 5.1.2+ by the change -
    "PNG compression support added to the GD extension."
    
    Was causing secure render image not to show due to a zlib error - imagepng's third argument only takes numbers from 0 to 9.
    
    Also cleaned the code up a bit.
    
    svn path=/trunk/e107_0.7/; revision=7565

--- e107_handlers/secure_img_render.php
+++ e107_handlers/secure_img_render.php
@@ -3,7 +3,7 @@
 + ----------------------------------------------------------------------------+
 |     e107 website system
 |
-|     ©Steve Dunstan 2001-2002
+|     ï¿½Steve Dunstan 2001-2002
 |     http://e107.org
 |     jalist@e107.org
 |
@@ -11,126 +11,128 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/secure_img_render.php,v $
-|     $Revision: 1.14 $
-|     $Date: 2006-10-29 03:26:40 $
-|     $Author: e107coders $
+|     $Revision: 1.15 $
+|     $Date: 2006-11-18 14:58:59 $
+|     $Author: streaky $
 +----------------------------------------------------------------------------+
 */
 
-while (list($global) = each($GLOBALS))
-{
+while (@ob_end_clean());
+
+// setup some php options
+e107_ini_set('magic_quotes_runtime',     0);
+e107_ini_set('magic_quotes_sybase',      0);
+e107_ini_set('arg_separator.output',     '&amp;');
+e107_ini_set('session.use_only_cookies', 1);
+e107_ini_set('session.use_trans_sid',    0);
+
+while (list($global) = each($GLOBALS)) {
 	if (!preg_match('/^(_SERVER|GLOBALS)$/', $global)) {
-                unset($$global);
-        }
+		unset($$global);
+	}
 }
+
 unset($global);
-$imgtypes=array("jpeg", "png", "gif");
+
+$imgtypes = array("jpeg", "png", "gif");
 
 define("e_QUERY", preg_replace("#&|/?PHPSESSID.*#i", "", $_SERVER['QUERY_STRING']));
+
 $recnum = preg_replace("#\D#","",e_QUERY);
-if(!$recnum){
-  exit;
+
+if($recnum == false){
+	exit;
 }
 
 $mySQLserver = "";
 
 @include_once(dirname(__FILE__)."e107_config.php");
 
-$a=0;
-$p="";
+$a = 0;
+$p = "";
 
-while(!$mySQLserver && $a<5){
-        $a++;
-        $p.="../";
+while(!$mySQLserver && $a < 5){
+	$a ++;
+	$p .= "../";
 	@include_once($p."e107_config.php");
 }
+
 mysql_connect($mySQLserver, $mySQLuser, $mySQLpassword);
 mysql_select_db($mySQLdefaultdb);
+
 $result = mysql_query("SELECT tmp_info FROM {$mySQLprefix}tmp WHERE tmp_ip = '{$recnum}'");
-if(!$row = mysql_fetch_array($result)){
-  exit;
+if(!$row = mysql_fetch_array($result)) {
+	exit;
 }
 
-list($code,$url) = explode(",",$row['tmp_info']);
-$type="none";
-foreach($imgtypes as $t)
-{
-        if(function_exists("imagecreatefrom".$t))
-{
-                $type = $t;
-                break;
-        }
+list($code, $url) = explode(",",$row['tmp_info']);
+
+$type = "none";
+
+foreach($imgtypes as $t) {
+	if(function_exists("imagecreatefrom".$t)) {
+		$type = $t;
+		break;
+	}
 }
 
 $path = realpath(dirname(__FILE__)."/../")."/".$IMAGES_DIRECTORY;
 
-if(is_readable($path."secure_image_custom.php"))
-{
-  	require_once($path."secure_image_custom.php");
+if(is_readable($path."secure_image_custom.php")) {
+	require_once($path."secure_image_custom.php");
 	/*   Example secure_image_custom.php file:
 
-		$secureimg['image'] = "code_bg_custom";  // filename excluding the .ext
-		$secureimg['size']	= "15";
-		$secureimg['angle']	= "0";
-		$secureimg['x']		= "6";
-		$secureimg['y']		= "22";
-		$secureimg['font'] 	= "imagecode.ttf";
-		$secureimg['color'] = array(90,90,90); // red,green,blue
+	$secureimg['image'] = "code_bg_custom";  // filename excluding the .ext
+	$secureimg['size']	= "15";
+	$secureimg['angle']	= "0";
+	$secureimg['x']		= "6";
+	$secureimg['y']		= "22";
+	$secureimg['font'] 	= "imagecode.ttf";
+	$secureimg['color'] = array(90,90,90); // red,green,blue
 
 	*/
 	$bg_file = $secureimg['image'];
-}
-else
-{
-    $bg_file = "generic/code_bg";
+} else {
+	$bg_file = "generic/code_bg";
 }
 
-switch($type)
-{
-        case "jpeg":
+switch($type) {
+	case "jpeg":
 		$image = ImageCreateFromJPEG($path.$bg_file.".jpg");
-                break;
-        case "png":
+		break;
+	case "png":
 		$image = ImageCreateFromPNG($path.$bg_file.".png");
-                break;
-        case "gif":
+		break;
+	case "gif":
 		$image = ImageCreateFromGIF($path.$bg_file.".gif");
-                break;
+		break;
 }
 
-if(isset($secureimg['color']))
-{
+if(isset($secureimg['color'])) {
 	$tmp = explode(",",$secureimg['color']);
 	$text_color = ImageColorAllocate($image,$tmp[0],$tmp[1],$tmp[2]);
-}
-else
-{
+} else {
 	$text_color = ImageColorAllocate($image, 90, 90, 90);
 }
 
-
-header("Content-type: image/".$type);
-
+header("Content-type: image/{$type}");
 
 if(isset($secureimg['font']) && is_readable($path.$secureimg['font'])){
 	imagettftext($image, $secureimg['size'],$secureimg['angle'], $secureimg['x'], $secureimg['y'], $text_color,$path.$secureimg['font'], $code);
-}else{
-	ImageString ($image, 5, 12, 2, $code, $text_color);
+} else {
+	imagestring ($image, 5, 12, 2, $code, $text_color);
 }
 
-switch($type)
-{
-        case "jpeg":
-                ImageJPEG($image,'',80);
-                break;
-        case "png":
-                ImagePNG($image,'',80);
-                break;
-        case "gif":
-                ImageGIF($image,'',80);
-                break;
+switch($type) {
+	case "jpeg":
+		imagejpeg($image);
+		break;
+	case "png":
+		imagepng($image);
+		break;
+	case "gif":
+		imagegif($image);
+		break;
 }
-ImageDestroy($image);
-die();
 
-?>
+imagedestroy($image);
