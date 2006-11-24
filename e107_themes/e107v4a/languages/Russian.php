6d31b6a5a7c92aff86a77c8c1e5379c5e0c00b61
yarodin <yarodin@localhost>
2006-11-24 18:34:06 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7631

--- e107_themes/e107v4a/languages/Russian.php
+++ e107_themes/e107v4a/languages/Russian.php
@@ -11,17 +11,17 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_themes/e107v4a/languages/Russian.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2006-11-05 07:19:42 $
+|     $Revision: 1.6 $
+|     $Date: 2006-11-24 18:26:54 $
 |     $Author: yarodin $
 +----------------------------------------------------------------------------+
 */
-
 define("LAN_THEME_1", "Читать/Добавить комментарии: ");
 define("LAN_THEME_2", "Комментарии выключены");
 define("LAN_THEME_3", "Читать далее...");
 define("LAN_THEME_4", "Добавил");
-define("LAN_THEME_5", "");
+define("LAN_THEME_5", "на");
 define("LAN_THEME_6", "e107.v4 от <a href='http://e107.org' rel='external'>jalist</a>");
+
 
 ?>
