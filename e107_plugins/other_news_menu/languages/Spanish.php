39d3038489e086da1d88c770e047e5cfbfb05b59
natxocc <natxocc@localhost>
2006-11-26 09:15:55 +0000

    Updated 26-11-06 by NatxoCC (http://www.e107-es.com)
    svn path=/trunk/e107_langpacks/; revision=7638

--- e107_plugins/other_news_menu/languages/Spanish.php
+++ e107_plugins/other_news_menu/languages/Spanish.php
@@ -4,11 +4,11 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_plugins/other_news_menu/languages/Spanish.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-06-03 22:34:18 $
+|     $Revision: 1.3 $
+|     $Date: 2006-11-26 09:15:55 $
 |     $Author: natxocc $
 +----------------------------------------------------------------------------+
 */
 define("TD_MENU_L1", "Otras noticias");
-
+define("TD_MENU_L2", "Otras noticias");
 ?>
