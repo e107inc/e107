6cf0d2800943b241eb98c438e9cb7917660cfcaf
mrpete <mrpete@localhost>
2006-12-04 13:51:52 +0000

    Bugfix: chatbox admin help doesn't fit the form anymore. Describes options that were  removed long ago!
    svn path=/trunk/e107_0.7/; revision=7722

--- e107_languages/English/admin/help/chatbox.php
+++ e107_languages/English/admin/help/chatbox.php
@@ -11,15 +11,15 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/help/chatbox.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2005-12-14 17:37:43 $
-|     $Author: sweetas $
+|     $Revision: 1.3 $
+|     $Date: 2006-12-04 13:51:52 $
+|     $Author: mrpete $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "Set your chatbox preferences from here.<br />If the replace link box is ticked, any links entered will be replaced by the text you enter in the textbox, this stops long links causing display problems. Wordwrap will auto wrap text that is longer than the length specified here.";
+$text = "Set your chatbox preferences from here.";
 
 $ns -> tablerender("Chatbox", $text);
 ?>
