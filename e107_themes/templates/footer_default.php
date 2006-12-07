5d4458a3cec1f477a89ccaa9ac98908d40b803fa
mrpete <mrpete@e107.org>
2006-12-07 12:53:49 +0000

    Bugfix #3568 - wysiwyg was not being tested for properly. This has been an ongoing  issue, now resolved. admin and user code need not download wysiwyg js etc -- use  e_WYSIWYG constant to see if it is enabled. Also fixed minor popup bug in header/footer (two different issues)

--- e107_themes/templates/footer_default.php
+++ e107_themes/templates/footer_default.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_themes/templates/footer_default.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2006-12-05 09:33:20 $
+|     $Revision: 1.3 $
+|     $Date: 2006-12-07 12:53:49 $
 |     $Author: mrpete $
 +----------------------------------------------------------------------------+
 */
@@ -107,6 +107,8 @@ if(varset($e107_popup)!=1){
 		echo "</table>";
 	}
 
+} // End of regular-page footer (the above NOT done for popups)
+
 //
 // D Close DB connection. We're done talking to underlying MySQL
 //
@@ -123,8 +125,6 @@ if(varset($e107_popup)!=1){
 		$ns->tablerender('Quick Admin Timer',"Results: {$tmp} microseconds");
 	}
 	
-} // End of regular-page footer (the above NOT done for popups)
-
 if ($pref['developer']) {
 	global $oblev_at_start,$oblev_before_start;
 	if (ob_get_level() != $oblev_at_start) {
