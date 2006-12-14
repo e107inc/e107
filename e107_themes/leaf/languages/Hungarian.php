26f16e2ff17feeecfdef8b8eff9fb75fb8d48f5b
e107hun-lac <e107hun-lac@localhost>
2006-12-14 20:37:32 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=7806

--- e107_themes/leaf/languages/Hungarian.php
+++ e107_themes/leaf/languages/Hungarian.php
@@ -3,7 +3,7 @@
 + ----------------------------------------------------------------------------+
 |     e107 website system
 |
-|     ŠSteve Dunstan 2001-2002
+|     ©Steve Dunstan 2001-2002
 |     http://e107.org
 |     jalist@e107.org
 |
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_themes/leaf/languages/Hungarian.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2006-12-13 20:44:37 $
+|     $Revision: 1.2 $
+|     $Date: 2006-12-14 20:37:32 $
 |     $Author: e107hun-lac $
 +----------------------------------------------------------------------------+
 */
@@ -23,6 +23,6 @@ define("LAN_THEME_3", "Hozzászólás ");
 define("LAN_THEME_4", "Tovább ...");
 define("LAN_THEME_5", "Trackback: ");
 define("LAN_THEME_6", "Hozzászólást írta");
-
+define("LAN_THEME_7", "Hírek");
 
 ?>
