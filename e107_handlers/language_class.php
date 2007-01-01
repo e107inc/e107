f65721e91dbdf86487b4f3ebf4b5849167e0d8f1
CaMer0n <cameron@e107coders.org>
2007-01-01 15:31:54 +0000

    Native Thai wasn't displaying correctly. Also cleaned up the class a little.

--- e107_handlers/language_class.php
+++ e107_handlers/language_class.php
@@ -4,198 +4,176 @@
 |     e107 website system - Language Class.
 |
 |     $Source: /cvs_backup/e107_0.8/e107_handlers/language_class.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2006-12-10 12:47:23 $
+|     $Revision: 1.3 $
+|     $Date: 2007-01-01 15:31:54 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
 class language{
 
-// Converts iso to language-name and visa-versa.
-
-	function convert($data){
-
-         $lang = array(
+	var $list = array(
             "aa" => "Afar",
-			"ab" => "Abkhazian",
-			"af" => "Afrikaans",
-			"am" => "Amharic",
-			"ar" => "Arabic",
-			"as" => "Assamese",
-			"ae" => "Avestan",
-			"ay" => "Aymara",
-			"az" => "Azerbaijani",
-			"ba" => "Bashkir",
-			"be" => "Belarusian",
-			"bn" => "Bengali",
-			"bh" => "Bihari",
-			"bi" => "Bislama",
-			"bo" => "Tibetan",
-			"bs" => "Bosnian",
-			"br" => "Breton",
-			"bg" => "Bulgarian",
-			"ca" => "Catalan",
-			"cs" => "Czech",
-			"ch" => "Chamorro",
-			"ce" => "Chechen",
-			"cn" => "ChineseSimp",
-			"cv" => "Chuvash",
-			"kw" => "Cornish",
-			"co" => "Corsican",
-			"cy" => "Welsh",
-			"da" => "Danish",
-			"de" => "German",
-			"dz" => "Dzongkha",
-			"el" => "Greek",
-			"en" => "English",
-			"eo" => "Esperanto",
-			"et" => "Estonian",
-			"eu" => "Basque",
-			"fo" => "Faroese",
-			"fa" => "Persian",
-			"fj" => "Fijian",
-			"fi" => "Finnish",
-			"fr" => "French",
-			"fy" => "Frisian",
-			"gd" => "Gaelic",
-			"ga" => "Irish",
-			"gl" => "Gallegan",
-			"gv" => "Manx",
-			"gn" => "Guarani",
-			"gu" => "Gujarati",
-			"ha" => "Hausa",
-			"he" => "Hebrew",
-			"hz" => "Herero",
-			"hi" => "Hindi",
-			"ho" => "Hiri Motu",
-			"hr" => "Croatian",
-			"hu" => "Hungarian",
-			"hy" => "Armenian",
-			"iu" => "Inuktitut",
-			"ie" => "Interlingue",
-			"id" => "Indonesian",
-			"ik" => "Inupiaq",
-			"is" => "Icelandic",
-			"it" => "Italian",
-			"jw" => "Javanese",
-			"ja" => "Japanese",
-			"kl" => "Kalaallisut",
-			"kn" => "Kannada",
-			"ks" => "Kashmiri",
-			"ka" => "Georgian",
-			"kk" => "Kazakh",
-			"km" => "Khmer",
-			"ki" => "Kikuyu",
-			"rw" => "Kinyarwanda",
-			"ky" => "Kirghiz",
-			"kv" => "Komi",
-			"ko" => "Korean",
-			"ku" => "Kurdish",
-			"lo" => "Lao",
-			"la" => "Latin",
-			"lv" => "Latvian",
-			"ln" => "Lingala",
-			"lt" => "Lithuanian",
-			"lb" => "Letzeburgesch",
-			"mh" => "Marshall",
-			"ml" => "Malayalam",
-	        "mr" => "Marathi",
-	        "mk" => "Macedonian",
-	        "mg" => "Malagasy",
-	        "mt" => "Maltese",
-	        "mo" => "Moldavian",
-	        "mn" => "Mongolian",
-	        "mi" => "Maori",
-	        "ms" => "Malay",
-	        "my" => "Burmese",
-	        "na" => "Nauru",
-	        "nv" => "Navajo",
+            "ab" => "Abkhazian",
+            "af" => "Afrikaans",
+            "am" => "Amharic",
+            "ar" => "Arabic",
+            "as" => "Assamese",
+            "ae" => "Avestan",
+            "ay" => "Aymara",
+            "az" => "Azerbaijani",
+            "ba" => "Bashkir",
+            "be" => "Belarusian",
+            "bn" => "Bengali",
+            "bh" => "Bihari",
+            "bi" => "Bislama",
+            "bo" => "Tibetan",
+            "bs" => "Bosnian",
+            "br" => "Breton",
+            "bg" => "Bulgarian",
+            "ca" => "Catalan",
+            "cs" => "Czech",
+            "ch" => "Chamorro",
+            "ce" => "Chechen",
+            "cn" => "ChineseSimp",
+            "cv" => "Chuvash",
+            "kw" => "Cornish",
+            "co" => "Corsican",
+            "cy" => "Welsh",
+            "da" => "Danish",
+            "de" => "German",
+            "dz" => "Dzongkha",
+            "el" => "Greek",
+            "en" => "English",
+            "eo" => "Esperanto",
+            "et" => "Estonian",
+            "eu" => "Basque",
+            "fo" => "Faroese",
+            "fa" => "Persian",
+            "fj" => "Fijian",
+            "fi" => "Finnish",
+            "fr" => "French",
+            "fy" => "Frisian",
+            "gd" => "Gaelic",
+            "ga" => "Irish",
+            "gl" => "Gallegan",
+            "gv" => "Manx",
+            "gn" => "Guarani",
+            "gu" => "Gujarati",
+            "ha" => "Hausa",
+            "he" => "Hebrew",
+            "hz" => "Herero",
+            "hi" => "Hindi",
+            "ho" => "Hiri Motu",
+            "hr" => "Croatian",
+            "hu" => "Hungarian",
+            "hy" => "Armenian",
+            "iu" => "Inuktitut",
+            "ie" => "Interlingue",
+            "id" => "Indonesian",
+            "ik" => "Inupiaq",
+            "is" => "Icelandic",
+            "it" => "Italian",
+            "jw" => "Javanese",
+            "ja" => "Japanese",
+            "kl" => "Kalaallisut",
+            "kn" => "Kannada",
+            "ks" => "Kashmiri",
+            "ka" => "Georgian",
+            "kk" => "Kazakh",
+            "km" => "Khmer",
+            "ki" => "Kikuyu",
+            "rw" => "Kinyarwanda",
+            "ky" => "Kirghiz",
+            "kv" => "Komi",
+            "ko" => "Korean",
+            "ku" => "Kurdish",
+            "lo" => "Lao",
+            "la" => "Latin",
+            "lv" => "Latvian",
+            "ln" => "Lingala",
+            "lt" => "Lithuanian",
+            "lb" => "Letzeburgesch",
+            "mh" => "Marshall",
+            "ml" => "Malayalam",
+            "mr" => "Marathi",
+            "mk" => "Macedonian",
+            "mg" => "Malagasy",
+            "mt" => "Maltese",
+            "mo" => "Moldavian",
+            "mn" => "Mongolian",
+            "mi" => "Maori",
+            "ms" => "Malay",
+            "my" => "Burmese",
+            "na" => "Nauru",
+            "nv" => "Navajo",
 
-	        "ng" => "Ndonga",
-	        "ne" => "Nepali",
-	        "nl" => "Dutch",
-	        "nb" => "Norwegian",
+            "ng" => "Ndonga",
+            "ne" => "Nepali",
+            "nl" => "Dutch",
+            "nb" => "Norwegian",
 
-	        "ny" => "Chichewa",
-	        "or" => "Oriya",
-	        "om" => "Oromo",
-	        "pa" => "Panjabi",
-	        "pi" => "Pali",
-	        "pl" => "Polish",
-	        "pt" => "Portuguese",
-	        "ps" => "Pushto",
-	        "qu" => "Quechua",
-	        "ro" => "Romanian",
-	        "rn" => "Rundi",
-	        "ru" => "Russian",
-	        "sg" => "Sango",
-	        "sa" => "Sanskrit",
-	        "si" => "Sinhalese",
-	        "sk" => "Slovak",
-	        "sl" => "Slovenian",
+            "ny" => "Chichewa",
+            "or" => "Oriya",
+            "om" => "Oromo",
+            "pa" => "Panjabi",
+            "pi" => "Pali",
+            "pl" => "Polish",
+            "pt" => "Portuguese",
+            "ps" => "Pushto",
+            "qu" => "Quechua",
+            "ro" => "Romanian",
+            "rn" => "Rundi",
+            "ru" => "Russian",
+            "sg" => "Sango",
+            "sa" => "Sanskrit",
+            "si" => "Sinhalese",
+            "sk" => "Slovak",
+            "sl" => "Slovenian",
 
-	        "sm" => "Samoan",
-	        "sn" => "Shona",
-	        "sd" => "Sindhi",
-	        "so" => "Somali",
+            "sm" => "Samoan",
+            "sn" => "Shona",
+            "sd" => "Sindhi",
+            "so" => "Somali",
 
-	        "es" => "Spanish",
-	        "sq" => "Albanian",
-	        "sc" => "Sardinian",
-	        "sr" => "Serbian",
-	        "ss" => "Swati",
-	        "su" => "Sundanese",
-	        "sw" => "Swahili",
-	        "sv" => "Swedish",
-	        "ty" => "Tahitian",
-	        "ta" => "Tamil",
-	        "tt" => "Tatar",
-	        "te" => "Telugu",
-	        "tg" => "Tajik",
-	        "tl" => "Tagalog",
-	        "th" => "Thai",
-	        "ti" => "Tigrinya",
+            "es" => "Spanish",
+            "sq" => "Albanian",
+            "sc" => "Sardinian",
+            "sr" => "Serbian",
+            "ss" => "Swati",
+            "su" => "Sundanese",
+            "sw" => "Swahili",
+            "sv" => "Swedish",
+            "ty" => "Tahitian",
+            "ta" => "Tamil",
+            "tt" => "Tatar",
+            "te" => "Telugu",
+            "tg" => "Tajik",
+            "tl" => "Tagalog",
+            "th" => "Thai",
+            "ti" => "Tigrinya",
 
-	        "tn" => "Tswana",
-	        "ts" => "Tsonga",
-	        "tk" => "Turkmen",
-	        "tr" => "Turkish",
-			"tw" => "ChineseTrad",
-	        "ug" => "Uighur",
-	        "uk" => "Ukrainian",
-	        "ur" => "Urdu",
-	        "uz" => "Uzbek",
-	        "vi" => "Vietnamese",
+            "tn" => "Tswana",
+            "ts" => "Tsonga",
+            "tk" => "Turkmen",
+            "tr" => "Turkish",
+            "tw" => "ChineseTrad",
+            "ug" => "Uighur",
+            "uk" => "Ukrainian",
+            "ur" => "Urdu",
+            "uz" => "Uzbek",
+            "vi" => "Vietnamese",
 
-	        "wo" => "Wolof",
-	        "xh" => "Xhosa",
-	        "yi" => "Yiddish",
-	        "yo" => "Yoruba",
-	        "za" => "Zhuang",
-	        "zh" => "Chinese",
-	        "zu" => "Zulu"
-		);
+            "wo" => "Wolof",
+            "xh" => "Xhosa",
+            "yi" => "Yiddish",
+            "yo" => "Yoruba",
+            "za" => "Zhuang",
+           // "zh" => "Chinese",
+            "zu" => "Zulu"
+        );
 
-		if(strlen($data) > 2)
-		{
-        	$tmp = array_flip($lang);
-			return $tmp[$data];
-		}
-		else
-		{
-			return $lang[$data];
-		}
-	}
-
-
-
-
-   // -------------------------------------------------------------------
-
-	function toNative($lang){
-
-		$name = array(
+		var $names = array(
 			"Arabic" 		=> "العربية",
 			"Bosnian"		=> "Bosanski",
 			"Bulgarian"		=> "Български",
@@ -226,12 +204,30 @@ class language{
 			"Slovakian"		=> "Slovensky",
 			"Slovak"		=> "Slovensky",
 			"Swedish"		=> "Svenska",
-			"Thai"			=> "� าษาไทย",
+			"Thai"			=> "ภาษาไทย",
 			"Turkish"		=> "Türkçe"
 		);
 
-       return ($name[$lang]) ? $name[$lang] : $lang;
+// ---  Converts iso to language-name and visa-versa. ----------------
 
+	function convert($data){
+
+		if(strlen($data) > 2)
+		{
+        	$tmp = array_flip($this->list);
+			return $tmp[$data];
+		}
+		else
+		{
+			return $this->list[$data];
+		}
+	}
+
+// -------------------------------------------------------------------
+
+	function toNative($lang)
+	{
+		return ($this->names[$lang]) ? $this->names[$lang] : $lang;
 	}
 
 
