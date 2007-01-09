2abd53d4e4a6c8f15537fc3697c29962d5b38b7c
e107steved <steved@e107.org>
2007-01-09 20:28:44 +0000

    Bugtracker #2007 - can edit text containing HTML tags (e.g. textarea)

--- e107_admin/cpage.php
+++ e107_admin/cpage.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_admin/cpage.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:33:19 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.2 $
+|     $Date: 2007-01-09 20:28:44 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
@@ -223,7 +223,7 @@ class page
 
 		require_once(e_HANDLER."ren_help.php");
 		$insertjs = (!e_WYSIWYG)?"rows='15' onselect='storeCaret(this);' onclick='storeCaret(this);' onkeyup='storeCaret(this);' style='width:95%'": "rows='25' style='width:100%' ";
-		$data = $tp->toForm($data);
+		$data = $tp->toForm($data,FALSE,TRUE);	// Make sure we convert HTML tags to entities
 		$text .= "<textarea class='tbox' id='data' name='data' cols='80'   $insertjs>".(strstr($data, "[img]http") ? $data : str_replace("[img]../", "[img]", $data))."</textarea>";
 
 		$text .= "<br />".display_help('',"cpage")."</td>
