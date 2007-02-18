b1c3214dd0224fe0cca1c57ead066faad00c429e
e107hun-lac <e107hun-lac@localhost>
2007-02-18 01:38:59 +0000

    Added for CVS
    svn path=/trunk/e107_langpacks/; revision=8129

---
+++ e107_themes/newsroom/languages/Hungarian.php
@@ -0,0 +1,25 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_themes/newsroom/languages/Hungarian.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2007-02-18 01:38:59 $
+|     $Author: e107hun-lac $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "Comments");
+define("LAN_THEME_2", "Comments off");
+define("LAN_THEME_3", "FULL STORY");
+define("LAN_THEME_4", "Posted on");
+
+?>
