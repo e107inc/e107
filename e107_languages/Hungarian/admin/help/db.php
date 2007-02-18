4fdad7bdbc7f184099190f9e302e1b8b1304e79f
e107hun-lac <e107hun-lac@localhost>
2007-02-18 01:54:26 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=8130

--- e107_languages/Hungarian/admin/help/db.php
+++ e107_languages/Hungarian/admin/help/db.php
@@ -1,24 +1,17 @@
 <?php
 /*
 + ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
+|     e107 website system - Hungarian Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Hungarian/admin/help/db.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2006-12-22 21:27:05 $
-|     $Author: e107hun-lac $
+|     $Revision: 1.4 $
+|     $Date: 2007-02-18 01:54:26 $
+|     $Author: lisa_
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "These collection of tools allow you to manage your database.";
-$ns -> tablerender("Database Tools", $text);
+$text = "Ezen eszközök segítségével karbantarthatod az adatbázisodat.";
+$ns -> tablerender("Adatbázis Eszközök", $text);
 ?>
