42151c6bb575dad8d40962b07dba012cda73aa8f
e107steved <steved@e107.org>
2007-02-19 22:26:00 +0000

    Bugtracker #3773 - lamb forum_post template had fallen by the wayside

--- e107_themes/lamb/forum_post_template.php
+++ e107_themes/lamb/forum_post_template.php
@@ -11,38 +11,52 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_themes/lamb/forum_post_template.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2007-01-20 16:31:55 $
-|     $Author: mrpete $
+|     $Revision: 1.3 $
+|     $Date: 2007-02-19 22:26:00 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
+if(!defined("USER_WIDTH")){ define("USER_WIDTH","width:95%"); }
 
 // the user box and subject box are not always displayed, therefore we need to define them /in case/ they are, if not they'll be ignored.
 
+if(!isset($userbox))
+{
 $userbox = "<tr>
 <td class='forumheader2' style='width:20%'>".LAN_61."</td>
 <td class='forumheader2' style='width:80%'>
 <input class='tbox' type='text' name='anonname' size='71' value='".$anonname."' maxlength='20' />
 </td>
 </tr>";
+}
 
+if(!isset($subjectbox))
+{
 $subjectbox = "<tr>
 <td class='forumheader2' style='width:20%'>".LAN_62."</td>
 <td class='forumheader2' style='width:80%'>
 <input class='tbox' type='text' name='subject' size='71' value='".$subject."' maxlength='100' />
 </td>
 </tr>";
+}
+
 
 // the poll is optional, be careful when changing the values here, only change if you know what you're doing ...
+if(!isset($poll_form))
+{
+	if(is_readable(e_PLUGIN."poll/poll_class.php")) {
+		require_once(e_PLUGIN."poll/poll_class.php");
+		$pollo = new poll;
+		$poll_form = $pollo -> renderPollForm("forum");
+	}
+}
 
-require_once(e_PLUGIN."poll/poll_class.php");
-$pollo = new poll;
-$poll = $pollo -> renderPollForm("forum");
 
 // finally, file attach is optional, again only change this if you know what you're doing ...
-
+if(!isset($fileattach))
+{
 $fileattach = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post'] ? LAN_390 : LAN_416)."</td></tr>
 <tr><td style='width:20%' class='forumheader3'>".LAN_392."</td>
 <td style='width:80%' class='forumheader3'>".LAN_393." | ".$allowed_filetypes." |<br />".LAN_394."<br />".LAN_395.": ".($pref['upload_maxfilesize'] ? $pref['upload_maxfilesize'].LAN_396 : ini_get('upload_max_filesize'))."
@@ -56,10 +70,12 @@ $fileattach = "<tr><td colspan='2' class='nforumcaption2'>".($pref['image_post']
 </tr>
 </td>
 </tr>";
-
+}
 
 // ------------
 
+if (!isset($FORUMPOST))
+{
 $FORUMPOST = "
 <div style='text-align:center'>
 {FORMSTART}
@@ -89,7 +105,125 @@ $FORUMPOST = "
 </div>
 {FORUMJUMP}
 ";
+}
 
+
+if(!isset($FORUMPOST_REPLY))
+{
+$FORUMPOST_REPLY = "
+<div style='text-align:center'>
+<div class='spacer'>
+{FORMSTART}
+<table style='".USER_WIDTH."' class='fborder'>
+<tr>
+<td colspan='2' class='fcaption'>{BACKLINK}
+</td>
+</tr>
+{USERBOX}
+{SUBJECTBOX}
+<tr>
+<td class='forumheader2' style='width:20%'>{POSTTYPE}</td>
+<td class='forumheader2' style='width:80%'>
+{POSTBOX}<br />{EMAILNOTIFY}<br />{POSTTHREADAS}
+</td>
+</tr>
+
+{POLL}
+
+{FILEATTACH}
+
+<tr style='vertical-align:top'>
+<td colspan='2' class='forumheader' style='text-align:center'>
+{BUTTONS}
+</td>
+</tr>
+</table>
+{FORMEND}
+
+<table style='".USER_WIDTH."'>
+<tr>
+<td>
+{FORUMJUMP}
+</td>
+</tr>
+</table>
+</div></div>
+<div style='text-align:center'>
+{THREADTOPIC}
+{LATESTPOSTS}
+</div>
+";
+}
+
+// From here copied in from default templates
+if(!$LATESTPOSTS_START)
+{
+$LATESTPOSTS_START = "
+<table style='".USER_WIDTH."' class='fborder'>
+<tr>
+<td colspan='2' class='fcaption' style='vertical-align:top'>".
+LAN_101."{LATESTPOSTSCOUNT}".LAN_102."
+</td>
+</tr>";
+}
+
+if(!$LATESTPOSTS_POST)
+{
+$LATESTPOSTS_POST = "
+<tr>
+<td class='forumheader3' style='width:20%;vertical-align:top'><b>{POSTER}</b></td>
+<td class='forumheader3' style='width:80%'>
+	<div class='smallblacktext' style='text-align:right'>".IMAGE_post2." ".LAN_322."{THREADDATESTAMP}</div>
+	{POST}
+</td>
+</tr>
+";
+}
+
+if(!$LATESTPOSTS_END)
+{
+$LATESTPOSTS_END = "
+</table>
+";
+}
+
+if(!$THREADTOPIC_REPLY)
+{
+$THREADTOPIC_REPLY = "
+<table style='".USER_WIDTH."' class='fborder'>
+<tr>
+	<td colspan='2' class='fcaption' style='vertical-align:top'>".LAN_100."</td>
+</tr>
+<tr>
+	<td class='forumheader3' style='width:20%;vertical-align:top'><b>{POSTER}</b></td>
+	<td class='forumheader3' style='width:80%'>
+		<div class='smallblacktext' style='text-align:right'>".IMAGE_post2." ".LAN_322."{THREADDATESTAMP}</div>{POST}
+	</td>
+</tr>
+</table>
+";
+}
+
+$FORUM_CRUMB['sitename']['value'] = "<a class='forumlink' {SITENAME_HREF}>{SITENAME}</a>";
+$FORUM_CRUMB['sitename']['sep'] = " :: ";
+
+$FORUM_CRUMB['forums']['value'] = "<a class='forumlink' {FORUMS_HREF}>{FORUMS_TITLE}</a>";
+$FORUM_CRUMB['forums']['sep'] = " :: ";
+
+$FORUM_CRUMB['parent']['value'] = "{PARENT_TITLE}";
+$FORUM_CRUMB['parent']['sep'] = " :: ";
+
+$FORUM_CRUMB['subparent']['value'] = "<a class='forumlink' {SUBPARENT_HREF}>{SUBPARENT_TITLE}</a>";
+$FORUM_CRUMB['subparent']['sep'] = " :: ";
+
+$FORUM_CRUMB['forum']['value'] = "<a class='forumlink' {FORUM_HREF}>{FORUM_TITLE}</a>";
+$FORUM_CRUMB['forum']['sep'] = " :: ";
+
+$FORUM_CRUMB['thread']['value'] = "{THREAD_TITLE}";
+
+
+/*
+This lot not in the default template, so presumably redundant
 $FORUMTHREADPOSTED = "
 <table style='width:100%' class='fborder'>
 <tr>
@@ -115,6 +249,6 @@ $FORUMREPLYPOSTED = "
 <a href='".e_PLUGIN."forum/forum_viewforum.php?".$forum_id."'>".LAN_326."</a></span><br /><br />
 </td></tr></table>";
 
-
+*/
 
 ?>
