31ac73a0354fbd7690d1e96fd5378177302b931c
e107steved <steved@e107.org>
2007-02-21 20:30:17 +0000

    Bugtracker #3780 - missing quote actually wasn't, so roll back

--- e107_plugins/tinymce/wysiwyg.php
+++ e107_plugins/tinymce/wysiwyg.php
@@ -4,8 +4,8 @@
 |     e107 website system - Tiny MCE controller file.
 |
 |     $Source: /cvs_backup/e107_0.8/e107_plugins/tinymce/wysiwyg.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2007-02-20 21:09:30 $
+|     $Revision: 1.4 $
+|     $Date: 2007-02-21 20:30:12 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -59,7 +59,7 @@ $text .= "'\n"; // end of plugins list.
 
 $text .= ",theme_advanced_buttons1 : 'fontsizeselect,separator,bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,outdent, indent,separator, forecolor,cut,copy,paste'";
 
-$text .= ",theme_advanced_buttons2   : 'tablecontrols,separator,undo,redo,separator,link,unlink'";
+$text .= ",theme_advanced_buttons2   : 'tablecontrols,separator,undo,redo,separator,link,unlink";
 $text .= ($pref['smiley_activate']) ? ",emoticons" : "";
 $text .= ",charmap,iespell,media";
 $text .= (ADMIN) ? ",ibrowser," : ",image";
