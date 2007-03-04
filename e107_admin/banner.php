1ad57a0c90590da907c19c6182731853017d5435
e107steved <e107steved@localhost>
2007-03-04 17:53:58 +0000

    Bugtracker #3820 possible fix
    svn path=/trunk/e107_0.7/; revision=8191

--- e107_admin/banner.php
+++ e107_admin/banner.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/banner.php,v $
-|     $Revision: 1.28 $
-|     $Date: 2006-10-24 13:34:38 $
-|     $Author: mrpete $
+|     $Revision: 1.29 $
+|     $Date: 2007-03-04 17:53:58 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 require_once("../class2.php");
@@ -38,8 +38,9 @@ if(e_QUERY)
 	list($action, $sub_action, $id) = explode(".", e_QUERY);
 }
 
-$reject = array('$.','$..','/','CVS','thumbs.db','*._$',"thumb_", 'index', '.DS_Store');
-$images = $fl->get_files(e_IMAGE."banners/","",$reject);
+//$reject = array('$.','$..','/','CVS','thumbs.db','*._$',"thumb_", 'index', '.DS_Store');
+//$images = $fl->get_files(e_IMAGE."banners/","",$reject);
+$images = $fl->get_files(e_IMAGE."banners/","","standard");
 
 if (isset($_POST['update_menu'])) {
 
