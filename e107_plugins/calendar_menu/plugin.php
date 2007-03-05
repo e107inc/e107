b78ded3a3211bc56746825504aea8b02afeacc1a
e107steved <steved@e107.org>
2007-03-05 20:09:30 +0000

    Bugtracker #3829 - wrong LAN

--- e107_plugins/calendar_menu/plugin.php
+++ e107_plugins/calendar_menu/plugin.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_plugins/calendar_menu/plugin.php,v $
-|     $Revision: 1.1.1.1 $ - with mods to hopefully trigger upgrade to new version
-|     $Date: 2006-12-02 04:34:48 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.2 $ - with mods to hopefully trigger upgrade to new version
+|     $Date: 2007-03-05 20:09:30 $
+|     $Author: e107steved $
 |
 | 22.07.06 - Mods for V3.6 upgrade, including log directory
 | 02.08.06 - Support for category icon display added
@@ -81,7 +81,7 @@ $eplug_prefs = array(
 "eventpost_datenext"   => 1,
 "eventpost_eventdatecustom" => "&A %d %B %Y",
 "eventpost_nextdatecustom"   => "%d %b",
-"eventpost_mailsubject" => EC_ADLAN_12,
+"eventpost_mailsubject" => EC_ADLAN_A12,
 "eventpost_mailfrom" => EC_ADLAN_A151,
 "eventpost_mailaddress" => EC_ADLAN_A152,
 "eventpost_asubs" => 1,
