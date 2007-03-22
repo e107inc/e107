26a2f6efee5a091b7f3a4787c642281e431020d4
e107coders <e107coders@localhost>
2007-03-22 01:01:32 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=8228

---
+++ e107_themes/interfectus/languages/Japanese.php
@@ -0,0 +1,26 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ｩSteve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_themes/interfectus/languages/Japanese.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2007-03-22 01:01:31 $
+|     $Author: e107coders $
++-----Translation Updated by: difda on 2005/12/13----------------------------+
+*/
+
+define("LAN_THEME_1", "'interfectus' by <a href='http://e107.org' rel='external'>jalist</a>");
+define("LAN_THEME_2", "コメントの閲覧/投稿: ");
+define("LAN_THEME_3", "この項目へのコメントはオフになっています");
+define("LAN_THEME_4", "残りを読む...");
+define("LAN_THEME_5", "トラックバック: ");
+
+?>
