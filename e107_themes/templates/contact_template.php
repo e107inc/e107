63f2f8ba112ba8b0f3779cd8865c6034ff4b5b3c
e107steved <e107steved@localhost>
2007-04-12 19:18:34 +0000

    Get rid of obsolete code
    svn path=/trunk/e107_0.7/; revision=8284

--- e107_themes/templates/contact_template.php
+++ e107_themes/templates/contact_template.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: contact_template.php,v 1.7 2006-11-09 19:57:47 e107coders Exp $
+// $Id: contact_template.php,v 1.8 2007-04-12 19:18:34 e107steved Exp $
 
 if (!defined('e107_INIT')) { exit; }
 if (!defined("USER_WIDTH")){ define("USER_WIDTH","width:97%"); }
@@ -20,9 +20,6 @@ $sc_style['CONTACT_EMAIL_COPY']['post'] = LANCONTACT_07."</td></tr>";
 
 $sc_style['CONTACT_PERSON']['pre'] = "<tr><td>".LANCONTACT_14."<br />   ";
 $sc_style['CONTACT_PERSON']['post'] = "</td></tr>";
-
-$sc_style['CONTACT_IMAGECODE']['pre'] = "<tr><td>".LANCONTACT_14."</td><td>";
-$sc_style['CONTACT_IMAGECODE']['post'] = "</td>";
 
 $sc_style['CONTACT_IMAGECODE']['pre'] = "<tr><td>".LANCONTACT_16."<br />";
 $sc_style['CONTACT_IMAGECODE']['post'] = "";
