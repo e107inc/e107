0b14509c423df0c3a3b0622a783a3a7963dd57f3
whoisbig <whoisbig@localhost>
2007-05-31 13:58:56 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=8386

--- e107_languages/Turkish/lan_links.php
+++ e107_languages/Turkish/lan_links.php
@@ -4,41 +4,41 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_langpacks/e107_languages/Turkish/lan_links.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2007-05-31 13:47:57 $
+|     $Revision: 1.2 $
+|     $Date: 2007-05-31 13:58:56 $
 |     $Author: whoisbig $
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Links");
 
-define("LAN_61", "Link Categories");
-define("LAN_62", "categories");
-define("LAN_63", "category");
-define("LAN_64", "in this category");
+define("LAN_61", "Link Kategorileri");
+define("LAN_62", "kategoriler");
+define("LAN_63", "kategori");
+define("LAN_64", "bu kategoride");
 define("LAN_65", "link");
-define("LAN_66", "links");
-define("LAN_67", "Show All Links");
-define("LAN_68", "edit");
-define("LAN_69", "delete");
-define("LAN_86", "Category:");
-define("LAN_88", "Referrals:");
-define("LAN_89", "Admin: ");
-define("LAN_90", "add new link in this category");
-define("LAN_91", "add new category");
+define("LAN_66", "linkler");
+define("LAN_67", "Tüm linkleri göster");
+define("LAN_68", "düzenle");
+define("LAN_69", "sil");
+define("LAN_86", "Kategori:");
+define("LAN_88", "Öneren:");
+define("LAN_89", "Yönetici: ");
+define("LAN_90", "Bu kategoriye yeni link ekle");
+define("LAN_91", "yeni kategori ekle");
 
-define("LAN_92", "Submit a link");
+define("LAN_92", "Link gönder");
 define("LAN_93", "After submitting your link it will be reviewed by a site admin and if appropriate it will be added to the main links page.");
-define("LAN_94", "Link Name:");
+define("LAN_94", "Link Adý:");
 define("LAN_95", "Link URL:");
-define("LAN_96", "Link Description:");
-define("LAN_97", "URL to link button:");
-define("LAN_98", "Submit Link");
+define("LAN_96", "Link Açýklamasý:");
+define("LAN_97", "Link butonuna URL:");
+define("LAN_98", "Link gönder");
 
-define("LAN_99", "Thank you");
+define("LAN_99", "Teþekkürler");
 define("LAN_100", "Your link has been saved and will be reviewed by a site administrator.");
 define("LAN_101", "Please click here to submit a link");
 
-define("LAN_102", "There");
+define("LAN_102", "var");
 define("LAN_103", "is");
 define("LAN_104", "are");
 define("LAN_105", "total in");
