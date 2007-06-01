3bd516ab50e3cb5eb9172498f4d649e517a76db5
whoisbig <whoisbig@localhost>
2007-06-01 13:21:32 +0000

    *** empty log message ***
    svn path=/trunk/e107_langpacks/; revision=8395

---
+++ e107_languages/Turkish/lan_request.php
@@ -0,0 +1,20 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_languages/Turkish/lan_request.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2007-06-01 13:21:31 $
+|     $Author: whoisbig $
+|			
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_dl_61", "Ýndirme Hatasý");
+define("LAN_dl_62", "Bu dosyayý indirmeniz engellendi, indirme kotasýný aþmýþsýnýz.");
+define("LAN_dl_63", "Bu dosyayý indirmek için gerekli izine sahip deðilsiniz.");
+define("LAN_dl_64", "Geri");
+define("LAN_dl_65", "Dosya Bulunamadý");
+
+?>
