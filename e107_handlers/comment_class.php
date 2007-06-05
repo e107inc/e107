eac14639f94c1cad3875ed4691550e1ab87a6a08
e107steved <steved@e107.org>
2007-06-05 18:54:56 +0000

    Bugtracker #3968 possible fix

--- e107_handlers/comment_class.php
+++ e107_handlers/comment_class.php
@@ -12,8 +12,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_handlers/comment_class.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2007-05-04 19:32:22 $
+|     $Revision: 1.6 $
+|     $Date: 2007-06-05 18:54:56 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -22,6 +22,7 @@ if (!defined('e107_INIT')) { exit; }
 
 @include_once(e_LANGUAGEDIR.e_LANGUAGE."/lan_comment.php");
 @include_once(e_LANGUAGEDIR."English/lan_comment.php");
+global $comment_shortcodes;
 require_once(e_FILE."shortcode/batch/comment_shortcodes.php");
 
 /**
