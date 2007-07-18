348365f5f1a1fc24fb5effb0c96d5025407616ab
e107steved <e107steved@localhost>
2007-07-18 20:20:27 +0000

    Option to disable update check
    svn path=/trunk/e107_0.7/; revision=8512

--- e107_admin/prefs.php
+++ e107_admin/prefs.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/prefs.php,v $
-|     $Revision: 1.99 $
-|     $Date: 2007-06-29 20:13:53 $
+|     $Revision: 1.100 $
+|     $Date: 2007-07-18 20:20:05 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -938,6 +938,14 @@ $text .= "<div id='advanced' style='display:none; text-align:center'>
 	<tr>
 	<td style='width:50%' class='forumheader3'>".PRFLAN_150."<br /><span class='smalltext'>&nbsp;</span></td>
 	<td style='width:50%; text-align:right' class='forumheader3'>{$auth_dropdown}</td>
+	</tr>
+
+	<tr>
+	<td style='width:50%' class='forumheader3'>".PRFLAN_173."<br /><span class='smalltext'>&nbsp;</span></td>
+	<td style='width:50%; text-align:right' class='forumheader3'>
+	<input type='radio' name='check_updates' value='1'".($pref['check_updates'] ? " checked='checked'" : "")." /> ".PRFLAN_112."&nbsp;&nbsp;
+	<input type='radio' name='check_updates' value='0'".(!$pref['check_updates'] ? " checked='checked'" : "")." /> ".PRFLAN_113."
+	</td>
 	</tr>
 
 	";
