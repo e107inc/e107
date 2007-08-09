574b03e0f9157848cde24c5e6f5bcea81bb5f631
e107steved <e107steved@localhost>
2007-08-09 19:10:00 +0000

    Bugtracker #3799 - slow loading of image popup in tiny_mce
    svn path=/trunk/e107_0.7/; revision=8539

--- e107_handlers/tiny_mce/plugins/ibrowser/ibrowser.php
+++ e107_handlers/tiny_mce/plugins/ibrowser/ibrowser.php
@@ -9,13 +9,13 @@
 // ------------------------------------------------
 //                                   www.j-cons.com
 // ================================================
-// $Revision: 1.8 $Date: 2004/10/04
+// $Revision: 1.9 $Date: 2004/10/04
 // ================================================
 //
 // $Source: /cvs_backup/e107_0.7/e107_handlers/tiny_mce/plugins/ibrowser/ibrowser.php,v $
-// $Revision: 1.8 $
-// $Date: 2006-10-24 13:39:45 $
-// $Author: mrpete $
+// $Revision: 1.9 $
+// $Date: 2007-08-09 19:10:00 $
+// $Author: e107steved $
 // +----------------------------------------------------------------------------+
 // Major Re-work by CaMer0n
 
@@ -320,7 +320,7 @@ $errors = array();
 	  sort($arr_tinyMCE_image_files);
 	  for($k=0; $k<count($arr_tinyMCE_image_files); $k++){
       $entry = $arr_tinyMCE_image_files[$k][file_name];
-	  $size = getimagesize($tinyMCE_base_url.$imglib.$entry);
+	  $size = getimagesize($_root.$imglib.$entry);
 	  $fsize = filesize($_root.$imglib.$entry);
    ?>
             <option  value="<?php echo $size[0]; ?>|<?php echo $size[1]; ?>|<?php echo filesize_h($fsize,2); ?>|<?php echo $entry?>" <?php echo ($entry == $img)?'selected':''?>><?php echo $entry?></option>
