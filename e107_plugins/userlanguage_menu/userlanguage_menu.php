85eeadf4db22f9951fb3d24e376400e3ecb588e2
e107coders <e107coders@localhost>
2007-09-01 02:29:25 +0000

    bugfix: userlanguage_menu was not functioning correctly when using subdomain language selection.
    svn path=/trunk/e107_0.7/; revision=8577

--- e107_plugins/userlanguage_menu/userlanguage_menu.php
+++ e107_plugins/userlanguage_menu/userlanguage_menu.php
@@ -11,36 +11,52 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_plugins/userlanguage_menu/userlanguage_menu.php,v $
-|     $Revision: 1.11 $
-|     $Date: 2005-12-14 19:28:52 $
-|     $Author: sweetas $
+|     $Revision: 1.12 $
+|     $Date: 2007-09-01 02:25:11 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
+require_once(e_HANDLER."language_class.php");
+$slng = new language;
 require_once(e_HANDLER."file_class.php");
 	$fl = new e_file;
 	$lanlist = $fl->get_dirs(e_LANGUAGEDIR);
 	sort($lanlist);
 
-	$action = (e_QUERY && !$_GET['elan']) ? e_SELF."?".e_QUERY : e_SELF;
-	$text = "<form method='post' action='".$action."'>
-		<div style='text-align:center'>
-		<select name='sitelanguage' class='tbox' >";
-	foreach($lanlist as $langval)
+	if(isset($pref['multilanguage_subdomain']) && $pref['multilanguage_subdomain'])
 	{
-		$selected ="";
-		if($langval == USERLAN || ($langval == $pref['sitelanguage'] && USERLAN == ""))
-		{
-			$selected = "selected='selected'";
-		}
-		$text .= "<option value='".$langval."' $selected>".$langval."</option>\n ";
+        	$text = "<div style='text-align:center'><select class='tbox' name='lang_select' style='width:95%' onchange=\"location.href=this.options[selectedIndex].value\">";
+			foreach($lanlist as $lng)
+			{
+				$selected = ($lng  == USERLAN || ($lng == $pref['sitelanguage'] && USERLAN == "")) ? "selected='selected'" : "";
+                $urlval = $slng->subdomainUrl($lng);
+				$text .= "<option value='".$urlval."' $selected>$lng</option>\n";
+			}
+			$text .= "</select></div>";
 	}
+	else
+	{
+		$action = (e_QUERY && !$_GET['elan']) ? e_SELF."?".e_QUERY : e_SELF;
+		$text = "<form method='post' action='".$action."'>
+			<div style='text-align:center'>
+			<select name='sitelanguage' class='tbox' >";
+		foreach($lanlist as $langval)
+		{
+			$selected ="";
+			if($langval == USERLAN || ($langval == $pref['sitelanguage'] && USERLAN == ""))
+			{
+				$selected = "selected='selected'";
+			}
+			$text .= "<option value='".$langval."' $selected>".$langval."</option>\n ";
+		}
 
-	$text .= "</select>";
-	$text .= "<br /><br /><input class='button' type='submit' name='setlanguage' value='".UTHEME_MENU_L1."' />";
-	$text .= "</div></form>	";
+		$text .= "</select>";
+		$text .= "<br /><br /><input class='button' type='submit' name='setlanguage' value='".UTHEME_MENU_L1."' />";
+		$text .= "</div></form>";
+	}
 
 $ns->tablerender(UTHEME_MENU_L2, $text, 'user_lan');
 
