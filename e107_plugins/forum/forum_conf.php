a65320d175536f832e98c7f9b8bb637882f53f78
e107steved <e107steved@localhost>
2007-09-26 19:28:47 +0000

    Bugtracker #4099 - correct footer
    svn path=/trunk/e107_0.7/; revision=8644

--- e107_plugins/forum/forum_conf.php
+++ e107_plugins/forum/forum_conf.php
@@ -11,9 +11,9 @@
 | GNU General Public License (http://gnu.org).
 |
 | $Source: /cvs_backup/e107_0.7/e107_plugins/forum/forum_conf.php,v $
-| $Revision: 1.10 $
-| $Date: 2006-12-26 13:33:25 $
-| $Author: mrpete $
+| $Revision: 1.11 $
+| $Date: 2007-09-26 19:28:41 $
+| $Author: e107steved $
 +---------------------------------------------------------------+
 */
 require_once("../../class2.php");
@@ -123,7 +123,7 @@ if ($action == "delete_poll")
 		</form>
 		</div>";
 	$ns->tablerender(FORLAN_16, $text);
-	require_once("footer.php");
+	require_once(FOOTERF);
 	exit;
 }
 	
