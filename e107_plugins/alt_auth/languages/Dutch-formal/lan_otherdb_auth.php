6fc46af9b8bb664c224fa4e46a85d629a96d6da1
mijnheer <mijnheer@localhost>
2007-10-12 19:25:07 +0000

    Versie 0.7.9 dd 12 okt 2007
    svn path=/trunk/e107_langpacks/; revision=8697

---
+++ e107_plugins/alt_auth/languages/Dutch-formal/lan_otherdb_auth.php
@@ -0,0 +1,13 @@
+<?php
+define("OTHERDB_LAN_1", "Database Type:");
+define("OTHERDB_LAN_2", "Server:");
+define("OTHERDB_LAN_3", "Gebruikersnaam:");
+define("OTHERDB_LAN_4", "Wachtwoord:");
+define("OTHERDB_LAN_5", "Database");
+define("OTHERDB_LAN_6", "Tabel");
+define("OTHERDB_LAN_7", "Veld Gebruikersnaam:");
+define("OTHERDB_LAN_8", "Veld Wachtwoord:");
+define("OTHERDB_LAN_9", "Wachtwoord method:");
+define("OTHERDB_LAN_10", "Configureren otherdb auth");
+define("OTHERDB_LAN_11", "** De volgende velden zijn niet verplicht bij gebruik van een e107 database");
+?>
