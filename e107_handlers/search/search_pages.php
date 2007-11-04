1d58d92af2b58c720eb3c9987d28b836a4f7afdd
e107steved <steved@e107.org>
2007-11-04 20:41:41 +0000

    Bugtracker #4176 - don't search menus

--- e107_handlers/search/search_pages.php
+++ e107_handlers/search/search_pages.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_handlers/search/search_pages.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:34:05 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.2 $
+|     $Date: 2007-11-04 20:41:41 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
@@ -31,7 +31,7 @@ $search_fields = array('page_title', 'page_text');
 $weights = array('1.2', '0.6');
 $no_results = LAN_198;
 
-$where = "page_class IN (".USERCLASS_LIST.") AND".$advanced_where;
+$where = "page_class IN (".USERCLASS_LIST.") AND `menu_name`='' AND".$advanced_where;
 $order = array('page_datestamp' => DESC);
 $table = "page";
 
