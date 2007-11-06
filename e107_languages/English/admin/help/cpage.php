8aa1498e0d37ac847dfa9ed111538b31bdb2b753
e107steved <e107steved@localhost>
2007-11-06 20:08:23 +0000

    Bugtracker #4192 - help title for custom menus/pages
    svn path=/trunk/e107_0.7/; revision=8772

--- e107_languages/English/admin/help/cpage.php
+++ e107_languages/English/admin/help/cpage.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/help/cpage.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2006-06-29 22:15:28 $
-|     $Author: e107coders $
+|     $Revision: 1.5 $
+|     $Date: 2007-11-06 20:08:19 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
@@ -22,5 +22,5 @@ if (!defined('e107_INIT')) { exit; }
 $text = "From this screen you can create custom menus or custom pages with your own content in them.<br /><br />";
 // $text .= "Please see <a href='http://docs.e107.org/Using Custom Pages and Custom Menus'>http://docs.e107.org/Using Custom Pages and Custom Menus</a> for an explanation of all the features.";
 
-$ns -> tablerender(CUSLAN_18, $text);
+$ns -> tablerender('Custom Menus/Pages Help', $text);
 ?>
