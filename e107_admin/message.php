531e88cd7c7a70c275955616a591d0b67733708f
e107steved <steved@e107.org>
2007-12-09 13:55:21 +0000

    Tidy up redundant code

--- e107_admin/message.php
+++ e107_admin/message.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_admin/message.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:33:25 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.2 $
+|     $Date: 2007-12-09 13:55:21 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 require_once("../class2.php");
@@ -106,86 +106,4 @@ $ns->tablerender(MESSLAN_1, $text);
 require_once("footer.php");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-if ($action != "edit") {
-	$text = $rs->form_open("post", e_SELF, "ban_form")."<div style='text-align:center'>".$rs->form_hidden("ban_secure", "1")."<div style='padding : 1px; ".ADMIN_WIDTH."; height : 170px; overflow : auto; margin-left: auto; margin-right: auto;'>\n";
-	if (!$ban_total = $sql->db_Select("banlist")) {
-		$text .= "<div style='text-align:center'>".BANLAN_2."</div>";
-	} else {
-		$text .= "<table class='fborder' style='width:99%;'>
-			<tr>
-			<td style='width:70%' class='fcaption'>".BANLAN_10."</td>
-			<td style='width:30%' class='fcaption'>".BANLAN_11."</td>
-			</tr>";
-		$count = 0;
-		while ($row = $sql->db_Fetch()) {
-			extract($row);
-			$text .= "<tr><td style='width:70%' class='forumheader3'>$banlist_ip<br />".BANLAN_7.": $banlist_reason</td>
-				<td style='width:30%; text-align:center' class='forumheader3'>".$rs->form_button("submit", "main_edit_$count", BANLAN_12, "onclick=\"document.getElementById('ban_form').action='".e_SELF."?edit-$banlist_ip'\"").$rs->form_button("submit", "main_delete_$count", BANLAN_4, "onclick=\"document.getElementById('ban_form').action='".e_SELF."?remove-$banlist_ip'\"")."</td>\n</tr>";
-			$count++;
-		}
-		$text .= "</table>\n";
-	}
-	$text .= "</div></div>".$rs->form_close();
-	$ns->tablerender(BANLAN_3, $text);
-}
-
-if ($action == "edit") {
-	$sql2->db_Select("banlist", "*", "banlist_ip='$sub_action'");
-	$row = $sql2->db_Fetch();
-	extract($row);
-} else {
-	unset($banlist_ip, $banlist_reason);
-	if (e_QUERY && strpos($_SERVER["HTTP_REFERER"], "userinfo")) {
-		$banlist_ip = $action;
-	}
-}
-$text = "<div style='text-align:center'>
-	<form method='post' action='".e_SELF."'>
-	<table style='".ADMIN_WIDTH."' class='fborder'>
-
-	<tr>
-	<td style='width:30%' class='forumheader3'>".BANLAN_5.": </td>
-	<td style='width:70%' class='forumheader3'>
-	<input class='tbox' type='text' name='ban_ip' size='40' value='".$banlist_ip."' maxlength='200' />
-	</td>
-	</tr>
-
-	<tr>
-	<td style='width:20%' class='forumheader3'>".BANLAN_7.": </td>
-	<td style='width:80%' class='forumheader3'>
-	<textarea class='tbox' name='ban_reason' cols='50' rows='4'>$banlist_reason</textarea>
-	</td>
-	</tr>
-
-	<tr style='vertical-align:top'>
-	<td colspan='2' style='text-align:center' class='forumheader'>".
-($action == "edit" ? "<input type='hidden' name='old_ip' value='$banlist_ip' /><input class='button' type='submit' name='update_ban' value='".BANLAN_13."' />" : "<input class='button' type='submit' name='add_ban' value='".BANLAN_8."' />")."
-
-	</td>
-	</tr>
-	</table>
-	</form>
-	</div>";
-
-$ns->tablerender(BANLAN_9, $text);
-
-require_once("footer.php");
-*/
 ?>
