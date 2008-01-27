04b52c1bd9715cdbc350b402ff9d180ee6b74095
CaMer0n <cameron@e107coders.org>
2008-01-27 11:12:59 +0000

    Only load when ajax $pref is on.

--- e107_plugins/chatbox_menu/e_header.php
+++ e107_plugins/chatbox_menu/e_header.php
@@ -1,9 +1,9 @@
 <?php
+// $Id: e_header.php,v 1.2 2008-01-27 11:12:59 e107coders Exp $   
 if (!defined('e107_INIT')) { exit; }
 
-if($eMenuActive['chatbox_menu'])
+if($eMenuActive['chatbox_menu'] && ($pref['cb_layer']==2))
 {
-	$eplug_js[] = e_FILE_ABS."e_ajax.php";
 	$eplug_js[] = e_FILE_ABS."e_ajax.php";
 }
 
