3c662622f911dfdc874581ee3222d030ee4b7ba5
CaMer0n <cameron@e107coders.org>
2008-02-24 00:06:48 +0000

    new language-file naming convention added.

---
+++ e107_themes/e107v4a/languages/e107v4a_English.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_0.8/e107_themes/e107v4a/languages/e107v4a_English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2008-02-24 00:06:48 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "Read/Post Comment: ");
+define("LAN_THEME_2", "Comments are turned off for this item");
+define("LAN_THEME_3", "Read the rest...");
+define("LAN_THEME_4", "Posted by");
+define("LAN_THEME_5", "on");
+define("LAN_THEME_6", "e107.v4 theme by <a href='http://e107.org' rel='external'>jalist</a>");
+
+?>
