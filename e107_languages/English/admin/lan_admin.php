5f3763e6199e3c560edc07d86f1e5078487f8f62
e107steved <steved@e107.org>
2008-06-29 10:13:29 +0000

    Bugtracker #4442 - delete obsolete LANs - thanks marj

--- e107_languages/English/admin/lan_admin.php
+++ e107_languages/English/admin/lan_admin.php
@@ -4,15 +4,15 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.8/e107_languages/English/admin/lan_admin.php,v $
-|     $Revision: 1.7 $
-|     $Date: 2008-06-17 05:39:30 $
-|     $Author: e107coders $
+|     $Revision: 1.8 $
+|     $Date: 2008-06-29 10:13:29 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 define("ADLAN_0", "News");
 define("ADLAN_1", "Add/edit/delete news items");
-define("ADLAN_2", "News Categories");
-define("ADLAN_3", "Add/edit/delete news categories");
+//define("ADLAN_2", "News Categories");
+//define("ADLAN_3", "Add/edit/delete news categories");
 define("ADLAN_4", "Preferences");
 define("ADLAN_5", "Edit Site Preferences");
 define("ADLAN_6", "Menus");
@@ -21,26 +21,26 @@ define("ADLAN_8", "Administrators");
 define("ADLAN_9", "Add/delete site administrators");
 define("ADLAN_10", "Admin password");
 define("ADLAN_11", "Change your password");
-define("ADLAN_12", "Forums");
-define("ADLAN_13", "Add/Edit Forums");
-define("ADLAN_14", "Articles");
-define("ADLAN_15", "Add new/edit/delete articles");
-define("ADLAN_16", "Content");
-define("ADLAN_17", "Add new/edit/delete content pages");
-define("ADLAN_18", "Reviews");
-define("ADLAN_19", "Add new/edit/delete reviews");
-define("ADLAN_22", "Link Categories");
-define("ADLAN_23", "Add new/edit/delete link categories");
+//define("ADLAN_12", "Forums");
+//define("ADLAN_13", "Add/Edit Forums");
+//define("ADLAN_14", "Articles");
+//define("ADLAN_15", "Add new/edit/delete articles");
+//define("ADLAN_16", "Content");
+//define("ADLAN_17", "Add new/edit/delete content pages");
+//define("ADLAN_18", "Reviews");
+//define("ADLAN_19", "Add new/edit/delete reviews");
+//define("ADLAN_22", "Link Categories");
+//define("ADLAN_23", "Add new/edit/delete link categories");
 define("ADLAN_24", "Downloads");
 define("ADLAN_25", "Manage Downloads");
-define("ADLAN_26", "Download Categories");
-define("ADLAN_27", "Add new/edit/delete download categories");
+//define("ADLAN_26", "Download Categories");
+//define("ADLAN_27", "Add new/edit/delete download categories");
 define("ADLAN_28", "Welcome Message");
 define("ADLAN_29", "Set static welcome message");
 define("ADLAN_30", "File Manager");
 define("ADLAN_31", "Manage/upload files");
-define("ADLAN_32", "Submitted News");
-define("ADLAN_33", "Review user submitted news items");
+//define("ADLAN_32", "Submitted News");
+//define("ADLAN_33", "Review user submitted news items");
 define("ADLAN_34", "Banlist");
 define("ADLAN_35", "Ban visitors");
 define("ADLAN_36", "Users");
@@ -57,53 +57,53 @@ define("ADLAN_46", "Logout");
 define("ADLAN_47", "Welcome");
 define("ADLAN_48", "Logged in");
 define("ADLAN_49", "main site administrator");
-define("ADLAN_50", "permissions");
+//define("ADLAN_50", "permissions");
 define("ADLAN_51", "Please log in to gain access to administration area");
 define("ADLAN_52", "Admin Front Page");
 define("ADLAN_53", "Leave Admin");
 define("ADLAN_54", "Banners");
 define("ADLAN_55", "Configure banners");
-define("ADLAN_56", "Chatbox");
-define("ADLAN_57", "Configure chatbox");
+//define("ADLAN_56", "Chatbox");
+//define("ADLAN_57", "Configure chatbox");
 define("ADLAN_58", "Emoticons");
 define("ADLAN_59", "Configure emoticons");
 define("ADLAN_60", "Front Page");
 define("ADLAN_61", "Configure front page content");
-define("ADLAN_62", "News Feeds");
-define("ADLAN_63", "Configure news feeds");
-define("ADLAN_64", "Log Stats");
-define("ADLAN_65", "Log stats/counter etc");
+//define("ADLAN_62", "News Feeds");
+//define("ADLAN_63", "Configure news feeds");
+//define("ADLAN_64", "Log Stats");
+//define("ADLAN_65", "Log stats/counter etc");
 define("ADLAN_66", "Meta Tags");
 define("ADLAN_67", "Add/Edit site meta tags");
 define("ADLAN_68", "PHP Info");
 define("ADLAN_69", "PHP Info page");
-define("ADLAN_70", "Polls");
-define("ADLAN_71", "Add/Edit Polls");
+//define("ADLAN_70", "Polls");
+//define("ADLAN_71", "Add/Edit Polls");
 define("ADLAN_72", "Public Uploads");
 define("ADLAN_73", "Configure public file uploads");
 define("ADLAN_74", "Cache");
 define("ADLAN_75", "Set cache status");
-define("ADLAN_77", "You have had a news item submitted - please click here to check.");
+//define("ADLAN_77", "You have had a news item submitted - please click here to check.");
 define("ADLAN_78", "Extended User Fields");
 define("ADLAN_79", "Edit extended user fields");
 
 
-define("ADLAN_86", "Incorrect password ");
-define("ADLAN_87", "Administrator name not found in database ");
-define("ADLAN_88", "Unable to login ");
+//define("ADLAN_86", "Incorrect password ");
+//define("ADLAN_87", "Administrator name not found in database ");
+//define("ADLAN_88", "Unable to login ");
 define("ADLAN_89", "Admin Name ");
 define("ADLAN_90", "Admin Password ");
 define("ADLAN_91", "Log In");
 define("ADLAN_92", "Please log in to proceed to administration area ...");
 define("ADLAN_93", "Show Admin Functions");
-define("ADLAN_94", "Show Installed Plugins");
+//define("ADLAN_94", "Show Installed Plugins");
 define("ADLAN_95", "Plugin Management");
-define("ADLAN_96", "None");
-define("ADLAN_97", "Click here for FAQ");
+//define("ADLAN_96", "None");
+//define("ADLAN_97", "Click here for FAQ");
 define("ADLAN_98", "Plugin Manager");
 define("ADLAN_99", "Install/upgrade etc. plugins");
-define("ADLAN_100", "Theme Layout");
-define("ADLAN_101", "Template Creator");
+//define("ADLAN_100", "Theme Layout");
+//define("ADLAN_101", "Template Creator");
 define("ADLAN_102", "It has been more than 30 days since you changed the main administrator password - ");
 define("ADLAN_103", "Click here to change it now");
 define("ADLAN_104", "Security");
@@ -111,14 +111,14 @@ define("ADLAN_104", "Security");
 define("ADLAN_105", "Images");
 define("ADLAN_106", "Image Settings");
 
-define("ADLAN_107", "Unchecked submitted news items");
-define("ADLAN_108", "Unchecked file uploads");
-define("ADLAN_109", "Information");
+//define("ADLAN_107", "Unchecked submitted news items");
+//define("ADLAN_108", "Unchecked file uploads");
+//define("ADLAN_109", "Information");
 define("ADLAN_110", "Registered members");
 define("ADLAN_111", "Unverified members");
-define("ADLAN_112", "Banned users");
+//define("ADLAN_112", "Banned users");
 define("ADLAN_113", "Forum posts");
-define("ADLAN_114", "Comments");
+//define("ADLAN_114", "Comments");
 define("ADLAN_115", "Chatbox posts");
 define("ADLAN_116", "Admin log ...");
 define("ADLAN_117", "Show all entries");
@@ -130,17 +130,17 @@ define("ADLAN_120", "There is a database update available, please click button t
 define("ADLAN_121", "Install");
 
 
-define("ADLAN_123", "Unchecked submitted articles");
-define("ADLAN_124", "Unchecked submitted reviews");
+//define("ADLAN_123", "Unchecked submitted articles");
+//define("ADLAN_124", "Unchecked submitted reviews");
 
-define("ADLAN_125", "Unchecked reported forum posts");
+//define("ADLAN_125", "Unchecked reported forum posts");
 
-define("ADLAN_126", "Main Options");
-define("ADLAN_127", "Users");
-define("ADLAN_128", "Content");
-define("ADLAN_129", "Communication");
-define("ADLAN_130", "Files Management");
-define("ADLAN_131", "Other tools");
+//define("ADLAN_126", "Main Options");
+//define("ADLAN_127", "Users");
+//define("ADLAN_128", "Content");
+//define("ADLAN_129", "Communication");
+//define("ADLAN_130", "Files Management");
+//define("ADLAN_131", "Other tools");
 
 define("ADLAN_132", "Language");
 define("ADLAN_133", "default");
@@ -193,8 +193,8 @@ define('ADLAN_CL_8', 'Docs');
 define("ADLAN_LAT_1", "Latest");
 
 define("ADLAN_LAT_2", "Submitted news items");
-define("ADLAN_LAT_3", "Submitted articles");
-define("ADLAN_LAT_4", "Submitted reviews");
+//define("ADLAN_LAT_3", "Submitted articles");
+//define("ADLAN_LAT_4", "Submitted reviews");
 define("ADLAN_LAT_5", "Submitted links");
 define("ADLAN_LAT_6", "Reported forum posts");
 define("ADLAN_LAT_7", "File uploads");
@@ -261,6 +261,7 @@ define("LAN_INACTIVE","Inactive");
 define("LAN_BAN","Ban");
 define("LAN_RATING", "Rating");
 
+define("LAN_UPLOAD", "Upload");
 define("LAN_UPLOAD_IMAGES","Upload Images");
 define("LAN_UPLOAD_FILES","Upload Files");
 define("LAN_UPLOAD_ADDFILE","Add Another File");
