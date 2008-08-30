73e454107240994685a831687af020667dc2402d
e107steved <e107steved@localhost>
2008-08-30 20:22:31 +0000

    Display message if source file for data table not available when creating db-based extended field
    svn path=/trunk/e107_0.7/; revision=9331

--- e107_languages/English/admin/lan_users_extended.php
+++ e107_languages/English/admin/lan_users_extended.php
@@ -4,8 +4,8 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_users_extended.php,v $
-|     $Revision: 1.21 $
-|     $Date: 2008-06-29 10:00:24 $
+|     $Revision: 1.22 $
+|     $Date: 2008-08-30 20:22:10 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -90,6 +90,7 @@ define("EXTLAN_74", "is a reserved field name and can not be used.");
 define("EXTLAN_75", "Error adding field to database.");
 define("EXTLAN_76", "Invalid characters in field name - only A-Z, a-z, 0-9, '_' allowed.");
 define("EXTLAN_77", "Category not deleted - must delete fields in category first: ");
+define('EXTLAN_78', 'Cannot find file --FILE-- needed to create data table');
 
 
 //textbox
