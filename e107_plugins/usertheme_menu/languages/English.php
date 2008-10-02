5e860fa183ffa303c4954fae8fbb16ac560adbf8
e107steved <e107steved@localhost>
2008-10-02 21:10:34 +0000

    Allow selection of themes available to users
    svn path=/trunk/e107_0.7/; revision=9355

--- e107_plugins/usertheme_menu/languages/English.php
+++ e107_plugins/usertheme_menu/languages/English.php
@@ -1,6 +1,11 @@
 <?php
-	
-define("LAN_350", "Set Theme");
-define("LAN_351", "Select Theme");
+
+define('LAN_UMENU_THEME_1', 'Set Theme');
+define('LAN_UMENU_THEME_2', 'Select Theme');
+define('LAN_UMENU_THEME_3', 'users:');
+define('LAN_UMENU_THEME_4', 'Enable those themes which users may select');
+define('LAN_UMENU_THEME_5', 'Update');
+define('LAN_UMENU_THEME_6', 'Themes available to users');
+define('LAN_UMENU_THEME_7', 'Class which can select themes');
 	
 ?>
