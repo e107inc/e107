ed85ab7c76748fdc93d40ebd6cfb3b441a2ab350
mcfly <mcfly@e107.org>
2008-11-26 14:32:43 +0000

    More schema changes

--- e107_plugins/forum/forum_sql.php
+++ e107_plugins/forum/forum_sql.php
@@ -5,20 +5,20 @@ CREATE TABLE forum (
   `forum_parent` int(10) unsigned NOT NULL default '0',
   `forum_sub` int(10) unsigned NOT NULL default '0',
   `forum_datestamp` int(10) unsigned NOT NULL default '0',
-  `forum_moderators` text,
+  `forum_moderators` tinyint(3) unsigned NOT NULL default '0',
   `forum_threads` int(10) unsigned NOT NULL default '0',
   `forum_replies` int(10) unsigned NOT NULL default '0',
   `forum_lastpost_user` int(10) unsigned default NULL,
   `forum_lastpost_info` varchar(40) default NULL,
-  `forum_class` varchar(100) NOT NULL default '',
+  `forum_class` tinyint(3) unsigned NOT NULL default '0',
   `forum_order` int(10) unsigned NOT NULL default '0',
-  `forum_postclass` text NOT NULL,
-  `forum_threadclass` text,
+  `forum_postclass` tinyint(3) unsigned NOT NULL default '0',
+  `forum_threadclass` tinyint(3) unsigned NOT NULL default '0',
   `forum_options` text,
   PRIMARY KEY  (`forum_id`),
   KEY `forum_parent` (`forum_parent`),
   KEY `forum_sub` (`forum_sub`)
-) Type=MyISAM AUTO_INCREMENT=1 ;
+) Type=MyISAM AUTO_INCREMENT=1;
 
 CREATE TABLE forum_thread (
   `thread_id` int(10) unsigned NOT NULL auto_increment,
@@ -29,7 +29,7 @@ CREATE TABLE forum_thread (
   `thread_active` tinyint(3) unsigned NOT NULL default '0',
   `thread_lastpost` int(10) unsigned NOT NULL default '0',
   `thread_s` tinyint(1) unsigned NOT NULL default '0',
-  `thread_lastuser` int(10) unsigned NOT NULL,
+  `thread_lastuser` int(10) unsigned default NULL,
   `thread_total_replies` int(10) unsigned NOT NULL default '0',
   `thread_options` text,
   PRIMARY KEY  (`thread_id`),
@@ -41,17 +41,19 @@ CREATE TABLE forum_thread (
 CREATE TABLE forum_post (
   `post_id` int(10) unsigned NOT NULL auto_increment,
   `post_entry` text NOT NULL,
-  `post_thread` int(10) unsigned NOT NULL default '0',
+  `post_thread` int(10) unsigned default NULL,
+  `post_forum` int(10) unsigned default NULL,
   `post_datestamp` int(10) unsigned NOT NULL default '0',
   `post_user` int(10) unsigned NOT NULL,
   `post_edit_datestamp` int(10) unsigned NOT NULL default '0',
   `post_edit_user` int(10) unsigned NOT NULL,
   `post_ip` varchar(45) NOT NULL,
-  `post_attachments` text NOT NULL,
+  `post_attachments` text default NULL,
   `post_options` text,
   PRIMARY KEY  (`post_id`),
   KEY `post_ip` (`post_ip`),
   KEY `post_thread` (`post_thread`),
+  KEY `post_forum` (`post_forum`),
   KEY `post_datestamp` (`post_datestamp`),
   KEY `post_user` (`post_user`)
 ) Type=MyISAM AUTO_INCREMENT=1 ;
@@ -59,7 +61,7 @@ CREATE TABLE forum_post (
 CREATE TABLE forum_track (
   `track_userid` int(10) unsigned NOT NULL,
   `track_thread` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`track_userid`,`track_threadid`),
+  PRIMARY KEY  (`track_userid`,`track_thread`),
   KEY `track_userid` (`track_userid`),
   KEY `track_thread` (`track_thread`)
 ) Type=MyISAM;
