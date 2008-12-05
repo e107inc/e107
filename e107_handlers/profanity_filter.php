0fe809f31ee84db351b913eb550dcab3371bc07b
e107steved <e107steved@localhost>
2008-12-05 19:57:00 +0000

    handle 'special' chars in profanity filter
    svn path=/trunk/e107_0.7/; revision=9570

--- e107_handlers/profanity_filter.php
+++ e107_handlers/profanity_filter.php
@@ -11,23 +11,26 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_handlers/profanity_filter.php,v $
-|     $Revision: 1.8 $
-|     $Date: 2006-10-16 22:51:09 $
-|     $Author: e107coders $
+|     $Revision: 1.9 $
+|     $Date: 2008-12-05 19:57:00 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-class e_profanityFilter {
+class e_profanityFilter 
+{
 	var $profanityList;
 
-	function e_profanityFilter() {
+	function e_profanityFilter() 
+	{
 		global $pref;
 
 		$words = explode(",", $pref['profanity_words']);
         $word_array = array();
-		foreach($words as $word) {
+		foreach($words as $word) 
+		{
 			$word = trim($word);
 			if($word != "")
 			{
@@ -36,25 +39,31 @@ class e_profanityFilter {
 		}
 		if(count($word_array))
 		{
-			$this->profanityList = implode("\b|\b", $word_array);
+			$this->profanityList = str_replace('#','\#',implode("\b|\b", $word_array));		// We can get entities in the string - confuse the regex delimiters
 		}
 		unset($words);
 		return TRUE;
 	}
 
-	function filterProfanities($text) {
+	function filterProfanities($text) 
+	{
 		global $pref;
-		if (!$this->profanityList) {
+		if (!$this->profanityList) 
+		{
 			return $text;
 		}
-		if ($pref['profanity_replace']) {
+		if ($pref['profanity_replace']) 
+		{
 			return preg_replace("#\b".$this->profanityList."\b#is", $pref['profanity_replace'], $text);
-		} else {
+		} 
+		else 
+		{
 			return preg_replace_callback("#\b".$this->profanityList."\b#is", array($this, 'replaceProfanities'), $text);
 		}
 	}
 
-	function replaceProfanities($matches) {
+	function replaceProfanities($matches) 
+	{
 		/*!
 		@function replaceProfanities callback
 		@abstract replaces vowels in profanity words with stars
