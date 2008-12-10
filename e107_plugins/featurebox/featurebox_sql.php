b9bd926c65f40f77edfb2bc90541edf529b4cf5b
e107steved <steved@e107.org>
2008-12-10 22:41:39 +0000

    featurebox - admin logging, plugin.xml created

---
+++ e107_plugins/featurebox/featurebox_sql.php
@@ -0,0 +1,10 @@
+CREATE TABLE featurebox (
+  fb_id int(10) unsigned NOT NULL auto_increment,
+  fb_title varchar(200) NOT NULL default '',
+  fb_text text NOT NULL,
+  fb_mode tinyint(3) unsigned NOT NULL default '0',
+  fb_class tinyint(3) unsigned NOT NULL default '0',
+  fb_rendertype tinyint(1) unsigned NOT NULL default '0',
+  fb_template varchar(50) NOT NULL default '',
+  PRIMARY KEY  (fb_id)
+) TYPE=MyISAM;
