4557f1012f7e517c573051419bd0bab4b7e5f6a1
secretr <m.yovchev@gmail.com>
2008-12-21 11:39:34 +0000

    Search  Administration - admin UI conventions

--- e107_languages/English/admin/help/search.php
+++ e107_languages/English/admin/help/search.php
@@ -1,27 +1,24 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_languages/English/admin/help/search.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:34:43 $
-|     $Author: mcfly_e107 $
-+----------------------------------------------------------------------------+
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * $Id: search.php,v 1.2 2008-12-21 11:39:34 secretr Exp $
+ *
+ * Search Admin Help
+ * 
 */
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "If your MySQL server version supports it you can switch 
-to the MySQL sort method which is faster than the PHP sort method. See preferences.<br /><br />
-If your site includes Ideographic languages such as Chinese and Japanese you must 
-use the PHP sort method and switch whole word matching off.";
-$ns -> tablerender("Search Help", $text);
+$caption = "Search Help";
+$text = "
+<p>
+	If your MySQL server version supports it you can switch 
+	to the MySQL sort method which is faster than the PHP sort method. See preferences.
+</p>
+<p>
+	If your site includes Ideographic languages such as Chinese and Japanese you must 
+	use the PHP sort method and switch whole word matching off.
+</p>
+";
+$ns->tablerender($caption, $text);
 ?>
