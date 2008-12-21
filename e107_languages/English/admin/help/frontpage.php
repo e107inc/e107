04d33ef1d049fa101d7b96988176dd3498e1db64
secretr <m.yovchev@gmail.com>
2008-12-21 10:58:35 +0000

    Frontpage  Administration - admin UI conventions

--- e107_languages/English/admin/help/frontpage.php
+++ e107_languages/English/admin/help/frontpage.php
@@ -1,28 +1,33 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_languages/English/admin/help/frontpage.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2008-06-16 21:10:09 $
-|     $Author: e107steved $
-+----------------------------------------------------------------------------+
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * $Id: frontpage.php,v 1.3 2008-12-21 10:58:35 secretr Exp $
+ *
+ * Frontpage Admin Help
+ * 
 */
 
 if (!defined('e107_INIT')) { exit; }
 
 $caption = "Front Page Help";
-$text = "From this screen you can choose what to display as the front page of your site, the default is news. You can also determine whether
-	users are sent to a particular page after logging in.<br /><br />
-	The list of rules are scanned in turn, until the class of the current user matches. This then determines the user's front (home) page, and also 
-	the page he sees immediately after login.";
-$ns -> tablerender($caption, $text);
+/* FIXME - English native speakers: what should stay and what should go? 
+	<p>
+		The list of rules are scanned in turn, until the class of the current user matches. 
+		This then determines the user's front (home) page, and also the page he sees immediately after login.
+	</p>
+ */
+$text = "
+<p>
+	From this screen you can choose what to display as the front page of your site, the default is news. You can also determine whether
+	users are sent to a particular page after logging in.
+</p>
+<p>
+	The rules are searched in order, to find the first where the current user belongs to the class specified in the rule. 
+	That rule then determines the front (home) page and any specific post-login page. If no rule matches, news.php is set as the home page.
+</p>
+<p>
+	The user is sent to the specified &quot;Post-login page&quot; (if specified) immediately following a login.
+</p>
+";
+$ns->tablerender($caption, $text, 'admin_help');
 ?>
