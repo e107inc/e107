d63eb9557ee6be0ddf0b8b837f183d29d2dac548
e107steved <steved@e107.org>
2008-12-21 09:00:02 +0000

    Last part of gsitemap update

--- gsitemap.php
+++ gsitemap.php
@@ -1,22 +1,26 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/gsitemap.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2007-12-24 16:21:00 $
-|     $Author: e107steved $
-+----------------------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2001-2008 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Plugin supporting file - gsitemap
+ *
+ * $Source: /cvs_backup/e107_0.8/gsitemap.php,v $
+ * $Revision: 1.3 $
+ * $Date: 2008-12-21 09:00:02 $
+ * $Author: e107steved $
+ *
 */
 require_once("class2.php");
+if(!plugInstalled('gsitemap'))
+{ 
+	header("location:".e_BASE."index.php"); 
+	exit();
+}
+
 include_lan(e_PLUGIN."gsitemap/languages/gsitemap_".e_LANGUAGE.".php");
 
 if(e_QUERY == "show")
