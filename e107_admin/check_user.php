f490b5804a91cd2ee1d3c59a67e9ad77102a9589
e107steved <e107steved@localhost>
2009-01-04 16:15:50 +0000

    Bugtracker #4643 - make checking utility multi-language
    svn path=/trunk/e107_0.7/; revision=9764

--- e107_admin/check_user.php
+++ e107_admin/check_user.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/check_user.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2009-01-04 09:45:05 $
+|     $Revision: 1.3 $
+|     $Date: 2009-01-04 16:15:44 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -23,22 +23,6 @@ if (!getperms('0'))
 	header('location:'.e_BASE.'index.php');
 	exit;
 }
-
-define('LAN_CKUSER_01','Check user database');
-define('LAN_CKUSER_02','This will check for various potential problems with your user database');
-define('LAN_CKUSER_03','If you have a lot of users, it may take some time, or even time out');
-define('LAN_CKUSER_04','Proceed');
-define('LAN_CKUSER_05','Check for duplicate login names');
-define('LAN_CKUSER_06','Select functions to perform');
-define('LAN_CKUSER_07','Duplicate User Names found');
-define('LAN_CKUSER_08','No duplicates found');
-define('LAN_CKUSER_09','User Name');
-define('LAN_CKUSER_10','User ID');
-define('LAN_CKUSER_11','Display Name');
-define('LAN_CKUSER_12','Check for duplicate email addresses');
-define('LAN_CKUSER_13','Duplicate email addresses found');
-define('LAN_CKUSER_14','Email address');
-define('LAN_CKUSER_15','');
 
 require_once("auth.php");
 
---
+++ e107_languages/English/admin/lan_check_user.php
@@ -0,0 +1,29 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_check_user.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2009-01-04 16:15:50 $
+|     $Author: e107steved $
++----------------------------------------------------------------------------+
+*/
+
+define('LAN_CKUSER_01','Check user database');
+define('LAN_CKUSER_02','This will check for various potential problems with your user database');
+define('LAN_CKUSER_03','If you have a lot of users, it may take some time, or even time out');
+define('LAN_CKUSER_04','Proceed');
+define('LAN_CKUSER_05','Check for duplicate login names');
+define('LAN_CKUSER_06','Select functions to perform');
+define('LAN_CKUSER_07','Duplicate User Names found');
+define('LAN_CKUSER_08','No duplicates found');
+define('LAN_CKUSER_09','User Name');
+define('LAN_CKUSER_10','User ID');
+define('LAN_CKUSER_11','Display Name');
+define('LAN_CKUSER_12','Check for duplicate email addresses');
+define('LAN_CKUSER_13','Duplicate email addresses found');
+define('LAN_CKUSER_14','Email address');
+define('LAN_CKUSER_15','');
+
+?>
