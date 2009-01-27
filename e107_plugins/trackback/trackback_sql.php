51634620d68a312a3016f336417927345b7c7c71
lisa <lisa@e107.org>
2009-01-27 21:12:02 +0000

    trackback sql file

---
+++ e107_plugins/trackback/trackback_sql.php
@@ -0,0 +1,10 @@
+CREATE TABLE trackback (
+  `trackback_id` int(10) unsigned NOT NULL auto_increment,
+  `trackback_pid` int(10) unsigned NOT NULL default '0',
+  `trackback_title` varchar(200) NOT NULL default '',
+  `trackback_excerpt` varchar(250) NOT NULL default '',
+  `trackback_url` varchar(150) NOT NULL default '',
+  `trackback_blogname` varchar(150) NOT NULL default '',
+   PRIMARY KEY  (`trackback_id`),
+   KEY `trackback_pid` (`trackback_pid`)
+) TYPE=MyISAM AUTO_INCREMENT=1;
