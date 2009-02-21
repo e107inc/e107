c365bf391b239e43a32d324fe67c100fae316839
e107steved <e107steved@localhost>
2009-02-21 11:23:50 +0000

    Bugtracker #4679 - hardcoded text
    svn path=/trunk/e107_0.7/; revision=9880

--- e107_themes/sebes/languages/English.php
+++ e107_themes/sebes/languages/English.php
@@ -11,16 +11,17 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_themes/sebes/languages/English.php,v $
-|     $Revision: 1.1 $
-|     $Date: 2005-03-21 12:11:00 $
-|     $Author: stevedunstan $
+|     $Revision: 1.2 $
+|     $Date: 2009-02-21 11:23:42 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_THEME_1", "'sebes' by <a href='http://e107.org' rel='external'>jalist</a>");
-define("LAN_THEME_2", "Read/Post Comment: ");
-define("LAN_THEME_3", "Comments are turned off for this item");
-define("LAN_THEME_4", "Read the rest ...");
-define("LAN_THEME_5", "Trackbacks: ");
+define('LAN_THEME_1', "'sebes' by <a href='http://e107.org' rel='external'>jalist</a>");
+define('LAN_THEME_2', 'Read/Post Comment: ');
+define('LAN_THEME_3', 'Comments are turned off for this item');
+define('LAN_THEME_4', 'Read the rest ...');
+define('LAN_THEME_5', 'Trackbacks: ');
+define('LAN_THEME_6', ' on ');
 
 ?>
