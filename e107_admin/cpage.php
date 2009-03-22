daaad2849af4f34bf60ed4ef8439f1d7080b58a9
e107steved <e107steved@localhost>
2009-03-22 21:17:41 +0000

    Bugtracker #4703 - update date on custom page edit
    svn path=/trunk/e107_0.7/; revision=9900

--- e107_admin/cpage.php
+++ e107_admin/cpage.php
@@ -11,8 +11,8 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/e107_admin/cpage.php,v $
-|     $Revision: 1.42 $
-|     $Date: 2008-07-09 20:07:54 $
+|     $Revision: 1.43 $
+|     $Date: 2009-03-22 21:17:32 $
 |     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
@@ -302,8 +302,7 @@ class page
 
 		if($mode)
 		{	// Don't think $_POST['page_ip_restrict'] is ever set.
-
-			$update = $sql -> db_Update("page", "page_title='$page_title', page_text='$page_text', page_author='$pauthor', page_rating_flag='".intval($_POST['page_rating_flag'])."', page_comment_flag='".intval($_POST['page_comment_flag'])."', page_password='".$_POST['page_password']."', page_class='".$_POST['page_class']."', page_ip_restrict='".varset($_POST['page_ip_restrict'],'')."' WHERE page_id='$mode'");
+			$update = $sql -> db_Update("page", "page_title='{$page_title}', page_text='{$page_text}', page_datestamp='".time()."', page_author='{$pauthor}', page_rating_flag='".intval($_POST['page_rating_flag'])."', page_comment_flag='".intval($_POST['page_comment_flag'])."', page_password='".$_POST['page_password']."', page_class='".$_POST['page_class']."', page_ip_restrict='".varset($_POST['page_ip_restrict'],'')."' WHERE page_id='{$mode}'");
 			$e107cache->clear("page_{$mode}");
 			$e107cache->clear("page-t_{$mode}");
 
