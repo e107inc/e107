b8da62d09a51720b0084e9c48f3032fa3e78b9d7
e107steved <steved@e107.org>
2009-04-21 21:26:59 +0000

    Bugtracker #4716 - submit news tidy up - mostly thanks to whoisrich

--- e107_languages/English/lan_submitnews.php
+++ e107_languages/English/lan_submitnews.php
@@ -4,17 +4,17 @@
 |     e107 website system - Language File.
 |
 |     $Source: /cvs_backup/e107_0.8/e107_languages/English/lan_submitnews.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2007-04-12 23:04:51 $
-|     $Author: e107coders $
+|     $Revision: 1.3 $
+|     $Date: 2009-04-21 21:26:59 $
+|     $Author: e107steved $
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Submit News");
-define("LAN_7", "Username: ");
-define("LAN_62", "Subject: ");
+define("LAN_7", "Name: ");
+define("LAN_62", "News Title: ");
 define("LAN_112", "Email Address: ");
 define("LAN_133", "Thank you");
-define("LAN_134", "Your item has been submitted and will be reviewed by one of the site administrators in due course.");
+define("LAN_134", "Your item has been submitted for review by one of the site administrators.");
 define("LAN_135", "News Item: ");
 define("LAN_136", "Submit News Item");
 define("NWSLAN_6", "Category");
@@ -28,5 +28,6 @@ define("SUBNEWSLAN_3", "Your attachment must be either a jpg, gif or png file");
 define("SUBNEWSLAN_4", "File too Large");
 define("SUBNEWSLAN_5", "Image File");
 define("SUBNEWSLAN_6", "(jpg, gif or png)");
+define('SUBNEWSLAN_7', 'You must give your name and email address');
 
 ?>
