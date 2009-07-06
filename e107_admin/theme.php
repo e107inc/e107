bd8fa412e50c9c250334ac749114e113ab91407b
CaMer0n <cameron@e107coders.org>
2009-07-06 05:59:42 +0000

    Theme Manager re-work.

--- e107_admin/theme.php
+++ e107_admin/theme.php
@@ -11,9 +11,9 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_admin/theme.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:33:28 $
-|     $Author: mcfly_e107 $
+|     $Revision: 1.2 $
+|     $Date: 2009-07-06 05:59:42 $
+|     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
 
@@ -29,9 +29,43 @@ require_once("auth.php");
 require_once(e_HANDLER."theme_handler.php");
 $themec = new themeHandler;
 
+$mode = e_QUERY;
+
+if($_POST['selectadmin'])
+{
+	$mode = "admin";
+}
+
+if($_POST['selectmain'])
+{
+	$mode = "main";
+}
+
+$themec -> showThemes($mode);
 
 
-$themec -> showThemes();
+function theme_adminmenu()
+{
+   	$e107 = &e107::getInstance();
+
+		$var['main']['text'] = TPVLAN_33;
+		$var['main']['link'] = e_SELF;
+
+		$var['admin']['text'] = TPVLAN_34;
+		$var['admin']['link'] = e_SELF."?admin";
+
+		$var['choose']['text'] = TPVLAN_51;
+		$var['choose']['link'] = e_SELF."?choose";
+
+		$var['upload']['text'] = TPVLAN_38;
+		$var['upload']['link'] = e_SELF."?upload";
+
+        $selected = (e_QUERY) ? e_QUERY : "main";
+
+
+		e_admin_menu(TPVLAN_26, $selected, $var);
+}
+
 require_once("footer.php");
 
 
