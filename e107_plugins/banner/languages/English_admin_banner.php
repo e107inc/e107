664e62dbff4039b570288ec746cad515ee109d26
CaMer0n <cameron@e107coders.org>
2009-07-23 16:43:03 +0000

    First step in making 'banners' a plugin.

---
+++ e107_plugins/banner/languages/English_admin_banner.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * $Id: English_admin_banner.php,v 1.1 2009-07-23 16:43:03 e107coders Exp $
+ *
+ * Administration Language File
+ *
+*/
+define("BNRLAN_1", "Banner #%d deleted.");
+define("BNRLAN_2", "Please confirm you wish to delete this banner - once deleted it cannot be retrieved");
+define("BNRLAN_5", "Confirm Delete Banner");
+define("BNRLAN_6", "Delete cancelled.");
+define("BNRLAN_7", "Existing Banners");
+define("BNRLAN_8", "Banner ID");
+define("BNRLAN_9", "Client");
+define("BNRLAN_10", "Clickthroughs");
+define("BNRLAN_11", "Click %");
+define("BNRLAN_12", "Impressions");
+define("BNRLAN_13", "Impressions Left");
+define("BNRLAN_15", "No banners yet.");
+define("BNRLAN_16", "Unlimited");
+define("BNRLAN_17", "None");
+define("BNRLAN_21", "Ends");
+define("BNRLAN_22", "Update Banner");
+define("BNRLAN_23", "Add New Banner");
+define("BNRLAN_24", "Campaign");
+
+define("BNRLAN_27", "Client");
+
+define("BNRLAN_30", "Client Login");
+define("BNRLAN_31", "Client Password");
+define("BNRLAN_32", "Banner Image");
+define("BNRLAN_33", "Click URL");
+define("BNRLAN_34", "Impressions Purchased");
+define("BNRLAN_35", "unlimited");
+define("BNRLAN_36", "Start Date");
+define("BNRLAN_37", "End Date");
+define("BNRLAN_38", "blank = no limit");
+define("BNRLAN_39", "visibility class");
+define("BNRLAN_40", "Update Banner");
+define("BNRLAN_41", "Create New Banner");
+define("BNRLAN_42", "Banner Rotation System");
+define("BNRLAN_43", "Choose banner image");
+define("BNRLAN_45", "Starts");
+define("BNRLAN_46", "Code");
+define("BNRLAN_58", "Banner front page");
+define("BNRLAN_59", "Create new banner");
+define("BNRLAN_60", "campaigns");
+define("BNRLAN_61", "Banner menu");
+define("BNRLAN_62", "Banner Options");
+define("BNRLAN_63", "Banner Created");
+define("BNRLAN_64", "Banner Updated");
+
+define("BANNER_MENU_L1", "Advertisement");
+define("BANNER_MENU_L2", "Banner menu configuration saved");
+define("BANNER_MENU_L3", "Caption");
+//define("BANNER_MENU_L5", "Banner Configuration");
+define("BANNER_MENU_L6", "choose campaigns to show in menu");
+//define("BANNER_MENU_L7", "available campaigns");
+//define("BANNER_MENU_L8", "selected campaigns");
+//define("BANNER_MENU_L9", "remove selection");
+define("BANNER_MENU_L10", "Render type");
+//define("BANNER_MENU_L12", "plain");
+//define("BANNER_MENU_L13", "in captioned box");
+define("BANNER_MENU_L18", "Update Menu Settings");
+define("BANNER_MENU_L19", "number of banners to show:<br />this is only used when multiple campaigns are selected");
+
+define("BNRLAN_25", "add new or choose existing campaign");
+define("BNRLAN_26", "enter new campaign");
+define("BNRLAN_26a", "New Campaign");
+define("BNRLAN_28", "add new or choose existing client");
+define("BNRLAN_29", "enter new client");
+define("BNRLAN_29a", "New Client");
+
+define("BNRLAN_65", "Stats");
+define("BNRLAN_66", "N/A");
+
+define("BNRLAN_67", "No campaigns yet.");
+define("BNRLAN_68", "Banner Menu Configuration");
+?>
