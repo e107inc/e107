bde6283a01b1c8192d92a6b36003e9a62263650f
CaMer0n <cameron@e107coders.org>
2009-09-19 21:48:52 +0000

    Moved othernews into news folder

---
+++ e107_plugins/news/languages/English.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.8/e107_plugins/news/languages/English.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2009-09-19 21:48:52 $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+	
+define("TD_MENU_L1", "Other News");
+define("TD_MENU_L2", "Other News");
+	
+?>
