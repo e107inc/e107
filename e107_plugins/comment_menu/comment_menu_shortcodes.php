f57a35bd04e18d9484b7b0e33c7d5ea68819a876
secretr <m.yovchev@gmail.com>
2009-10-08 10:53:02 +0000

    comment_menu - 0.8 aware

--- e107_plugins/comment_menu/comment_menu_shortcodes.php
+++ e107_plugins/comment_menu/comment_menu_shortcodes.php
@@ -1,25 +1,23 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/comment_menu/comment_menu_shortcodes.php,v $
-|     $Revision: 1.4 $
-|     $Date: 2009-08-23 10:57:51 $
-|     $Author: marj_nl_fr $
-+----------------------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2001-2008 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Comment menu shortcodes
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/comment_menu/comment_menu_shortcodes.php,v $
+ * $Revision: 1.5 $
+ * $Date: 2009-10-08 10:53:02 $
+ * $Author: secretr $
 */
+
 if (!defined('e107_INIT')) { exit; }
 global $tp;
 $comment_menu_shortcodes = $tp -> e_sc -> parse_scbatch(__FILE__);
-
+e107::getRegistry('plugin/comment_menu/current');
 /*
 SC_BEGIN CM_ICON
 //TODO review bullet
@@ -36,38 +34,39 @@ return $bullet;
 SC_END
 
 SC_BEGIN CM_DATESTAMP
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 $gen = new convert;
 return $gen->convert_date($row['comment_datestamp'], "short");
 SC_END
 
 SC_BEGIN CM_HEADING
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 return $row['comment_title'];
 SC_END
 
 SC_BEGIN CM_URL_PRE
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 return ($row['comment_url'] ? "<a href='".$row['comment_url']."'>" : "");
 SC_END
 
 SC_BEGIN CM_URL_POST
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 return ($row['comment_url'] ? "</a>" : "");
 SC_END
 
 SC_BEGIN CM_TYPE
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 return $row['comment_type'];
 SC_END
 
 SC_BEGIN CM_AUTHOR
-global $row;
+$row = e107::getRegistry('plugin/comment_menu/current');
 return $row['comment_author'];
 SC_END
 
 SC_BEGIN CM_COMMENT
-global $row, $menu_pref, $pref, $tp;
+global $menu_pref, $pref, $tp;
+$row = e107::getRegistry('plugin/comment_menu/current');
 $COMMENT = '';
 if($menu_pref['comment_characters']>0)
 {
--- e107_plugins/comment_menu/comment_menu_template.php
+++ e107_plugins/comment_menu/comment_menu_template.php
@@ -1,20 +1,17 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/comment_menu/comment_menu_template.php,v $
-|     $Revision: 1.1.1.1 $
-|     $Date: 2006-12-02 04:34:52 $
-|     $Author: mcfly_e107 $
-+----------------------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2001-2008 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Comment menu default template
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/comment_menu/comment_menu_template.php,v $
+ * $Revision: 1.2 $
+ * $Date: 2009-10-08 10:53:02 $
+ * $Author: secretr $
 */
 
 $sc_style['CM_TYPE']['pre'] = "[";
