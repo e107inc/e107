214450cf6d5cf8e3e944cf26b48d12ff6b0f684b
CaMer0n <cameron@e107coders.org>
2009-10-24 12:01:24 +0000

    Cron "Run Now" is now functional.

--- e107_handlers/cli_class.php
+++ e107_handlers/cli_class.php
@@ -1,21 +1,22 @@
 <?php
 /*
 + ----------------------------------------------------------------------------+
-|     e107 website system
+||     e107 website system
 |
-|     ©Steve Dunstan 2001-2002
+|     Copyright (C) 2008-2009 e107 Inc 
 |     http://e107.org
-|     jalist@e107.org
 |
 |     Released under the terms and conditions of the
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/e107_handlers/cli_class.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2008-01-22 02:06:35 $
+|     $Revision: 1.3 $
+|     $Date: 2009-10-24 12:01:24 $
 |     $Author: e107coders $
 +----------------------------------------------------------------------------+
 */
+
+if (!defined('e107_INIT')) { exit; }
 
 class eCLI
 {
