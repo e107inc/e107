c66e027b6b305ec42e0b0dd569a46e82bfd29892
marj_nl_fr <marj_nl_fr@localhost>
2009-10-24 08:45:43 +0000

    exit after header
    svn path=/trunk/e107_0.7/; revision=10683

--- sitedown.php
+++ sitedown.php
@@ -11,36 +11,35 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.7/sitedown.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2009-08-15 11:54:29 $
+|     $Revision: 1.11 $
+|     $Date: 2009-10-24 08:45:11 $
 |     $Author: marj_nl_fr $
 +----------------------------------------------------------------------------+
 */
-require_once("class2.php");
+require_once('class2.php');
 
-global $pref;
-global $tp;
+header('Content-type: text/html; charset=utf-8', TRUE);
 
-include_lan(e_LANGUAGEDIR.e_LANGUAGE."/lan_sitedown.php");
+include_lan(e_LANGUAGEDIR.e_LANGUAGE.'/lan_'.e_PAGE);
 
 if (!$pref['maintainance_flag'] )
 {
-	header("location: ".SITEURL);
+	header('location: '.SITEURL);
+	exit();
 }
 
-require_once(e_FILE."shortcode/batch/sitedown_shortcodes.php");
+require_once(e_FILE.'shortcode/batch/sitedown_shortcodes.php');
 
-if (!$SITEDOWN_TABLE) {
-	if (file_exists(THEME."sitedown_template.php"))
+if (!$SITEDOWN_TABLE)
+{
+	if (file_exists(THEME.'sitedown_template.php'))
 	{
-		require_once(THEME."sitedown_template.php");
+		require_once(THEME.'sitedown_template.php');
 	}
 	else
 	{
-		require_once(e_THEME."templates/sitedown_template.php");
+		require_once(e_THEME.'templates/sitedown_template.php');
 	}
 }
 
-	echo $tp->parseTemplate($SITEDOWN_TABLE, TRUE, $sitedown_shortcodes);
-
-?>
+echo $tp->parseTemplate($SITEDOWN_TABLE, TRUE, $sitedown_shortcodes);
