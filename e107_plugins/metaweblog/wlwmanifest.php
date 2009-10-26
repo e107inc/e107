9532b4fc1efdb587144d76f0bb2d4afbd268a7a1
iad2003 <iad2003@e107.org>
2009-10-26 16:15:42 +0000

    *** empty log message ***

---
+++ e107_plugins/metaweblog/wlwmanifest.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!defined('e107_INIT'))
+{ 
+	require_once("../../class2.php");
+}
+
+if(e107::isInstalled('metaweblog'))
+{
+
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+?>
+
+
+<manifest xmlns="http://schemas.microsoft.com/wlw/manifest/weblog">
+
+  <options>
+    <clientType>WordPress</clientType>
+	<supportsKeywords>Yes</supportsKeywords>
+	<supportsGetTags>Yes</supportsGetTags>
+  </options>
+  
+  <weblog>
+    <serviceName>e107</serviceName>
+    <imageUrl>images/wlw/e107_icon_32.png</imageUrl>
+    <watermarkImageUrl>images/wlw/e107_icon_32_wat.png</watermarkImageUrl>
+    <homepageLinkText>View site</homepageLinkText>
+    <adminLinkText>Dashboard</adminLinkText>
+    <adminUrl>
+      <![CDATA[ 
+			<?php echo SITEURLBASE.e_ADMIN_ABS ?>
+		]]>
+    </adminUrl>
+    <postEditingUrl>
+      <![CDATA[ 
+			<?php echo SITEURLBASE.e_ADMIN_ABS."newspost.php?create.edit." ?>{post-id} 
+		]]>
+    </postEditingUrl>
+  </weblog>
+
+  <buttons>
+    <button>
+      <id>0</id>
+      <text>Manage Comments</text>
+      <imageUrl>images/wlw/comments_32.png</imageUrl>
+      <clickUrl>
+        <![CDATA[ 
+				<?php echo SITEURLBASE.e_ADMIN_ABS."comment.php" ?>
+			]]>
+      </clickUrl>
+    </button>
+
+  </buttons>
+
+</manifest>
+
+<?php
+
+}
+
+?>
