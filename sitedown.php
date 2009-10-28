24ca20948e6961bc4eab1d174c1bef11b3c1a056
marj <marj@e107.org>
2009-10-28 14:14:11 +0000

    work on sitedown

--- sitedown.php
+++ sitedown.php
@@ -11,22 +11,22 @@
 |     GNU General Public License (http://gnu.org).
 |
 |     $Source: /cvs_backup/e107_0.8/sitedown.php,v $
-|     $Revision: 1.5 $
-|     $Date: 2009-10-24 08:45:43 $
+|     $Revision: 1.6 $
+|     $Date: 2009-10-28 14:14:11 $
 |     $Author: marj_nl_fr $
 +----------------------------------------------------------------------------+
 */
 require_once('class2.php');
 
-header('Content-type: text/html; charset=utf-8', TRUE);
-
-include_lan(e_LANGUAGEDIR.e_LANGUAGE.'/lan_'.e_PAGE);
-
-if (!$pref['maintainance_flag'] )
+if (!varset($pref['maintainance_flag']))
 {
 	header('location: '.SITEURL);
 	exit();
 }
+
+header('Content-type: text/html; charset=utf-8', TRUE);
+
+include_lan(e_LANGUAGEDIR.e_LANGUAGE.'/lan_'.e_PAGE);
 
 require_once(e_FILE.'shortcode/batch/sitedown_shortcodes.php');
 
@@ -41,5 +41,4 @@ if (!$SITEDOWN_TABLE)
 		require_once(e_THEME.'templates/sitedown_template.php');
 	}
 }
-
 echo $tp->parseTemplate($SITEDOWN_TABLE, TRUE, $sitedown_shortcodes);
