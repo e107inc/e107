f89d7a9b656342e474e3f447ec3bec7e4203e5ea
CaMer0n <cameron@e107coders.org>
2009-10-31 01:33:29 +0000

    manifest added

--- e107_plugins/metaweblog/e_header.php
+++ e107_plugins/metaweblog/e_header.php
@@ -2,7 +2,9 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-echo "<link rel=\"EditURI\" type=\"application/rsd+xml\" title=\"RSD\" href=\"".SITEURLBASE.e_PLUGIN_ABS."metaweblog/metaweblog.php?rsd\" />\n";
+echo "<link rel=\"EditURI\" type=\"application/rsd+xml\" title=\"RSD\" href=\"".SITEURLBASE.e_PLUGIN_ABS."metaweblog/metaweblog.php?rsd\" />
+<link rel=\"wlwmanifest\" type=\"application/wlwmanifest+xml\" href=\"".SITEURLBASE.e_PLUGIN_ABS."metaweblog/wlwmanifest.php\" /> 
+";
 
 
 ?>
