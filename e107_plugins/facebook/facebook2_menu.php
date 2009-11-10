3ca5f751bf65ba7fb4b3d081108612530e4e619f
CaMer0n <cameron@e107coders.org>
2009-11-10 06:29:37 +0000

    fixes

--- e107_plugins/facebook/facebook2_menu.php
+++ e107_plugins/facebook/facebook2_menu.php
@@ -1,63 +1,61 @@
 <?php
-//error_reporting(E_ALL); 
+//error_reporting(E_ALL);
 
- 
-require_once (e_BASE.'class2.php');
+if (!defined('e107_INIT'))
+{
+	exit;
+}
 
 include_once (e_PLUGIN.'facebook/facebook_function.php');
 
-
-if ( isset ($_POST['fb_sig_in_canvas']))
+if (isset($_POST['fb_sig_in_canvas']))
 {
-    return;
+	return;
 }
-
 
 /**
  * start the logic...
- *  
- */   
+ *
+ */
 
 global $pref;
 
 $html = '';
 
-if ( ( $pref[ 'Facebook_Api-Key' ] != '' ) && ( $pref[ 'Facebook_Secret-Key' ] != '' ) &&  ( $pref[ 'user_reg' ] == 1 ) )
+$fb_pref = e107::getPlugConfig('facebook')->getPref();
+
+if (($fb_pref['Facebook_Api-Key'] != '') && ($fb_pref['Facebook_Secret-Key'] != '') && ($pref['user_reg'] == 1))
 {
-
- $html = '';
-
-   if ( USER ) {
-
-      if ( USERID == get_id_from_uid ( is_fb() ) ) {
-
-      if ( Facebook_User_Is_Connected() === true ) {
-
-      ///$html .= Render_Facebook_Profile();
-
-      //$caption = 'Welcome, ' . Get_Facebook_Info ( 'name' );
-      
-      $html .= Render_Facebook_Friends_Table();
-
-      $html .= Render_Connect_Invite_Friends(); 
-
-      
-      
-       $caption = 'Friends';
-// $text = $tp->parseTemplate($html, true, $facebook_shortcodes);
-
-$ns->tablerender($caption, $html);
-      
-      } 
-      
-      
-     
-      
-
-      }
-
-}
-
+	
+	$html = '';
+	
+	if (USER)
+	{
+		
+		if (USERID == get_id_from_uid(is_fb()))
+		{
+			
+			if (Facebook_User_Is_Connected() === true)
+			{
+				
+				///$html .= Render_Facebook_Profile();
+				
+				//$caption = 'Welcome, ' . Get_Facebook_Info ( 'name' );
+				
+				$html .= Render_Facebook_Friends_Table();
+				
+				$html .= Render_Connect_Invite_Friends();
+				
+				$caption = 'Friends';
+				// $text = $tp->parseTemplate($html, true, $facebook_shortcodes);
+				
+				$ns->tablerender($caption, $html);
+			
+			}
+		
+		}
+	
+	}
 
 }
 
--- e107_plugins/facebook/facebook3_menu.php
+++ e107_plugins/facebook/facebook3_menu.php
@@ -1,47 +1,39 @@
-<?php 
-
+<?php
 /**
- *  Fun Box - Become a Fun Of...
- * 
- * http://wiki.developers.facebook.com/index.php/Fan_Box
+ *  Fan Box - Become a Fan Of...
  *
- *  this must be later inserted in the Control Panel where User can chose below Parameters  :
- *   
- *   stream = Set to 1 to display stream stories in the Fan Box or 0 to hide stream stories. (Default value is 1.)
- *    
- *   connections = The number of fans to display in the Fan Box. Specifying 0 hides the list of fans in the Fan Box. You cannot display more than 100 fans. (Default value is 10 connections.) 
- *       
+ * http://wiki.developers.facebook.com/index.php/Fan_Box
+ * this must be later inserted in the Control Panel where User can chose below Parameters  :
+ * stream = Set to 1 to display stream stories in the Fan Box or 0 to hide stream stories. (Default value is 1.)
+ * connections = The number of fans to display in the Fan Box. Specifying 0 hides the list of fans in the Fan Box. You cannot display more than 100 fans. (Default value is 10 connections.)
  *
  * see the render_fun_box() function
- *   
- */     
+ *
+ */
 
-require_once (e_BASE.'class2.php');
-
-include_once (e_PLUGIN.'facebook/facebook_function.php');
-
-
-if ( isset ($_POST['fb_sig_in_canvas']))
+if (!defined('e107_INIT'))
 {
-    return;
+	exit;
 }
 
+include_once(e_PLUGIN.'facebook/facebook_function.php');
 
+if (isset($_POST['fb_sig_in_canvas']))
+{
+	return;
+}
 
+if (is_fb())
+{
+	
+	$html = '';
+	
+	$html .= Render_Fun_Box('0', '10', '200px');
+	
+	$caption = 'Fun Box';
+	
+	$ns->tablerender($caption, $html);
 
-      
-
-   if ( is_fb() ) {
-
-      $html = '';
-      
-      $html .= Render_Fun_Box( '0' , '10' , '200px' ); 
-
-            $caption = 'Fun Box';
-
-
-$ns->tablerender($caption, $html);
-      
-      } 
+}
 
 ?>
