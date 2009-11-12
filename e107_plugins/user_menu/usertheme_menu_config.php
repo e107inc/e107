e79e7a6a4533b8df2e32c4621585d31152648aa6
marj <marj@e107.org>
2009-11-12 21:44:15 +0000

    charset and badchars

--- e107_plugins/user_menu/usertheme_menu_config.php
+++ e107_plugins/user_menu/usertheme_menu_config.php
@@ -1,21 +1,19 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/user_menu/usertheme_menu_config.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2009-08-15 11:55:30 $
-|     $Author: marj_nl_fr $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/user_menu/usertheme_menu_config.php,v $
+ * $Revision: 1.3 $
+ * $Date: 2009-11-12 21:44:15 $
+ * $Author: marj_nl_fr $
+ */
+
 $eplug_admin = TRUE;
 require_once("../../class2.php");
 include_lan(e_PLUGIN."user_menu/languages/".e_LANGUAGE.".php");
