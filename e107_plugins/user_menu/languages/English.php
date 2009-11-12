fbfacfd5c5ff0ba83dc3d0e6b73c2c4bd6873f63
marj <marj@e107.org>
2009-11-12 21:44:15 +0000

    charset and badchars

--- e107_plugins/user_menu/languages/English.php
+++ e107_plugins/user_menu/languages/English.php
@@ -1,21 +1,18 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/user_menu/languages/English.php,v $
-|     $Revision: 1.3 $
-|     $Date: 2008-12-16 17:23:25 $
-|     $Author: secretr $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/user_menu/languages/English.php,v $
+ * $Revision: 1.4 $
+ * $Date: 2009-11-12 21:44:15 $
+ * $Author: marj_nl_fr $
+ */
 
 define('UTHEME_MENU_L1', 'Set');
 define('UTHEME_MENU_L2', 'Select Language');
