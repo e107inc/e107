0aed7e5ace1eafade6465a39b71a7a76a96179a1
marj <marj@e107.org>
2009-11-17 12:59:03 +0000

    reencode some files to UTF-8

--- e107_plugins/chatbox_menu/admin_chatbox.php
+++ e107_plugins/chatbox_menu/admin_chatbox.php
@@ -1,21 +1,19 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/chatbox_menu/admin_chatbox.php,v $
-|     $Revision: 1.7 $
-|     $Date: 2009-08-15 11:55:30 $
-|     $Author: marj_nl_fr $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/chatbox_menu/admin_chatbox.php,v $
+ * $Revision: 1.8 $
+ * $Date: 2009-11-17 12:59:03 $
+ * $Author: marj_nl_fr $
+ */
+
 require_once("../../class2.php");
 
 if (!plugInstalled('chatbox_menu') || !getperms("P")) 
