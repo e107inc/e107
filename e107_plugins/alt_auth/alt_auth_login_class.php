64c7ec3c99dda4d34babfb0d9fd5744729acd24d
marj <marj@e107.org>
2009-11-17 11:28:41 +0000

    more work on files' charset and badchars

--- e107_plugins/alt_auth/alt_auth_login_class.php
+++ e107_plugins/alt_auth/alt_auth_login_class.php
@@ -1,21 +1,19 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/alt_auth/alt_auth_login_class.php,v $
-|     $Revision: 1.10 $
-|     $Date: 2009-11-08 10:34:23 $
-|     $Author: e107steved $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/alt_auth/alt_auth_login_class.php,v $
+ * $Revision: 1.11 $
+ * $Date: 2009-11-17 11:28:41 $
+ * $Author: marj_nl_fr $
+ */
+
 define('AA_DEBUG',FALSE);
 define('AA_DEBUG1',FALSE);
 
--- e107_plugins/alt_auth/extended_password_handler.php
+++ e107_plugins/alt_auth/extended_password_handler.php
@@ -1,21 +1,18 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/alt_auth/extended_password_handler.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2009-07-21 19:21:27 $
-|     $Author: e107steved $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/alt_auth/extended_password_handler.php,v $
+ * $Revision: 1.3 $
+ * $Date: 2009-11-17 11:28:41 $
+ * $Author: marj_nl_fr $
+ */
 
 /*
 EXTENDED PASSWORD HANDLER CLASS 
