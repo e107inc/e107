ee3d65bb2fe32a49a3d1e03a512fa8c0e17c2087
marj <marj@e107.org>
2009-11-17 13:27:14 +0000

    reencode some files to UTF-8

--- e107_plugins/content/e_notify.php
+++ e107_plugins/content/e_notify.php
@@ -1,21 +1,18 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/content/e_notify.php,v $
-|     $Revision: 1.2 $
-|     $Date: 2007-03-13 16:51:05 $
-|     $Author: lisa_ $
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2001-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/content/e_notify.php,v $
+ * $Revision: 1.3 $
+ * $Date: 2009-11-17 13:24:00 $
+ * $Author: marj_nl_fr $
+ */
 
 include_lan(e_PLUGIN."content/languages/".e_LANGUAGE."/lan_content.php");
 
