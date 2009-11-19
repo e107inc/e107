769f27af958a8d763a1554b863cbc890ea994693
marj <marj@e107.org>
2009-11-19 15:29:03 +0000

    Switch to include_lan()

--- e107_plugins/forum/e_search.php
+++ e107_plugins/forum/e_search.php
@@ -1,11 +1,8 @@
 <?php
-if (!defined('e107_INIT')) { exit; }
+if (!defined('e107_INIT')) { exit(); }
 
-if (file_exists(e_PLUGIN."forum/languages/".e_LANGUAGE."/lan_forum_search.php")) {
-	include_once(e_PLUGIN."forum/languages/".e_LANGUAGE."/lan_forum_search.php");
-} else {
-	include_once(e_PLUGIN."forum/languages/English/lan_forum_search.php");
-}
+include_lan(e_PLUGIN.'forum/languages/'.e_LANGUAGE.'/lan_forum_search.php');
+
 $search_info[] = array(
 	'sfile' => e_PLUGIN.'forum/search/search_parser.php', 
 	'qtype' => FOR_SCH_LAN_1, 
@@ -13,5 +10,3 @@ $search_info[] = array(
 	'advanced' => e_PLUGIN.'forum/search/search_advanced.php', 
 	'id' => 'forum'
 	);
-
-?>
