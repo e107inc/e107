58b97d9470fbba98477743354a657a6f0ef4040b
CaMer0n <cameron@e107coders.org>
2009-11-19 12:36:22 +0000

    custompage work and some code cleanup

---
+++ e107_plugins/page/templates/page_template.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ + ----------------------------------------------------------------------------+
+ |     e107 website system
+ |
+ |     Copyright (c) e107 Inc. 2008-2009
+ |     http://e107.org
+ |
+ |     Released under the terms and conditions of the
+ |     GNU General Public License (http://gnu.org).
+ |
+ |     $Source: /cvs_backup/e107_0.8/e107_plugins/page/templates/page_template.php,v $
+ |     $Revision: 1.1 $
+ |     $Date: 2009-11-19 12:36:22 $
+ |     $Author: e107coders $
+ +----------------------------------------------------------------------------+
+ */
+  
+	$PAGE_TEMPLATE['default'] = "<!-- Default -->"; 
+	
+	$PAGE_TEMPLATE['other1'] = "<!-- Other -->"; 
+	
+	$PAGE_TEMPLATE['other2'] = "<!-- Other2 -->"; 
+	
+?>
