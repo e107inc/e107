22e9cc8c6e2f9af02ac7ee35855ace53aa054080
marj <marj@e107.org>
2009-11-19 09:52:35 +0000

    Switch to include_lan()

--- e107_plugins/forum/forum_uploads.php
+++ e107_plugins/forum/forum_uploads.php
@@ -9,9 +9,9 @@
  *
  *
  * $Source: /cvs_backup/e107_0.8/e107_plugins/forum/forum_uploads.php,v $
- * $Revision: 1.5 $
- * $Date: 2009-11-18 01:05:36 $
- * $Author: e107coders $
+ * $Revision: 1.6 $
+ * $Date: 2009-11-19 09:52:35 $
+ * $Author: marj_nl_fr $
  */
 
 require_once("../../class2.php");
@@ -21,8 +21,7 @@ if(!USER)
 	header("location:".e_BASE.$PLUGINS_DIRECTORY."forum/forum.php");
 	exit;
 }
-$lan_file = e_PLUGIN.'forum/languages/'.e_LANGUAGE.'/lan_forum_uploads.php';
-include_once(file_exists($lan_file) ? $lan_file : e_PLUGIN.'forum/languages/English/lan_forum_uploads.php');
+include_lan(e_PLUGIN.'forum/languages/'.e_LANGUAGE.'/lan_forum_uploads.php');
 
 if(is_array($_POST['delete']))
 {
