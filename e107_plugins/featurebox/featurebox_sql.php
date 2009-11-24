ea34267e486650f0d9ce9e32b531ad0a739542f4
CaMer0n <cameron@e107coders.org>
2009-11-24 14:48:34 +0000

    featurebox admin rewrite

--- e107_plugins/featurebox/featurebox_sql.php
+++ e107_plugins/featurebox/featurebox_sql.php
@@ -6,5 +6,17 @@ CREATE TABLE featurebox (
   fb_class smallint(5) unsigned NOT NULL default '0',
   fb_rendertype tinyint(1) unsigned NOT NULL default '0',
   fb_template varchar(50) NOT NULL default '',
+  fb_order tinyint(3) unsigned NOT NULL default '0',
+  fb_image varchar(255) NOT NULL default '',
+  fb_imageurl varchar(255) NOT NULL default '',
+  fb_category tinyint(3) unsigned NOT NULL default '0',
   PRIMARY KEY  (fb_id)
+) TYPE=MyISAM;
+
+CREATE TABLE featurebox_cat (
+  fb_cat_id int(10) unsigned NOT NULL auto_increment,
+  fb_cat_title varchar(200) NOT NULL default '',
+  fb_cat_class int(3) unsigned default '0',
+  fb_cat_order int(3) unsigned NOT NULL default '0',
+  PRIMARY KEY  (fb_cat_id)
 ) TYPE=MyISAM;
