6b79e05ccb16d19de24ad79625d1b3367ee17066
e107steved <steved@e107.org>
2010-01-22 21:18:11 +0000

    Add cron files to PM plugin

---
+++ e107_plugins/pm/languages/English_mailer.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Private Messenger - language file for cron-related routines
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/pm/languages/English_mailer.php,v $
+ * $Revision: 1.1 $
+ * $Date: 2010-01-22 21:18:11 $
+ * $Author: e107steved $
+ *
+*/
+
+
+/**
+ *	e107 Private messenger plugin
+ *
+ *	@package	e107_plugins
+ *	@subpackage	pm
+ *	@version 	$Id: English_mailer.php,v 1.1 2010-01-22 21:18:11 e107steved Exp $;
+ */
+define('LAN_EC_PM_01', '');
+define('LAN_EC_PM_02', '');
+define('LAN_EC_PM_03', '');
+define('LAN_EC_PM_04', 'PM Handler');
+define('LAN_EC_PM_05', 'Process large sends of PMs');
+define('LAN_EC_PM_06', 'Start bulk PM processing for --NUM-- recipients ');
+define('LAN_EC_PM_07', '');
+
+
+?>
