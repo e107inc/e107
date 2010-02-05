e94db7c043dfe233271979dfc2d154164cd4849a
secretr <m.yovchev@gmail.com>
2010-02-05 10:52:28 +0000

    imagepreview SC improvements, converted to function; imageselector SC improvements and fixes

---
+++ e107_files/shortcode/imagepreview.php
@@ -0,0 +1,55 @@
+<?php 
+// $Id: imagepreview.php,v 1.1 2010-02-05 10:52:27 secretr Exp $
+
+function imagepreview_shortcode($parm)
+{
+	list($name, $width, $height) = explode("|",$parm, 3);
+	
+	$name = rawurldecode(varset($name));//avoid warnings
+	if(varset($width))
+	{
+		$width = " width: {$width};";
+	}
+	else $width = '';
+	if(varset($height))
+	{
+		$height = " height: {$height};";
+	}
+	else $height = '';
+		
+	// array support
+	if(strpos($name, '[')) //can't be first string
+	{
+		$matches = array(); 
+		$search = $name;
+		
+		$tmp = explode('[', $name, 2);
+		$name = $tmp[0]; unset($tmp);
+		
+		$path = '';
+
+		if(isset($_POST[$name]) && is_array($_POST[$name]) && preg_match_all('#\[([\w]*)\]#', $search, $matches, PREG_PATTERN_ORDER))
+		{
+			$posted = $_POST[$name];
+			foreach ($matches[1] as $varname) 
+			{
+				if(!isset($posted[$varname]))
+				{
+					break;
+				}
+				$posted = $posted[$varname];
+			}
+		}
+		if($posted && is_string($posted)) 
+			$path = e107::getParser()->replaceConstants($posted, 'full');
+	}
+	else  $path = (varset($_POST[$name])/* && deftrue('e_AJAX_REQUEST')*/) ? e107::getParser()->replaceConstants($_POST[$name], 'full') : '';
+	$hide = '';
+	
+	if(!$path) 
+	{
+		$path = e_IMAGE_ABS."generic/blank.gif";
+		$hide = ' style="display: none;"';
+	}
+	return "<a href='{$path}' rel='external' class='e-image-preview'{$hide}><img src='{$path}' alt=\"\" class='image-selector' style='{$width}{$height}' /></a>";
+}
