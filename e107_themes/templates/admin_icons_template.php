f5125cc526d124ce576b854b04e52dd4ece61c25
secretr <m.yovchev@gmail.com>
2010-02-12 16:46:35 +0000

    Admin icon paths converted to absolute, temporary solution - admin icon handler should do the job in the future (see UI handler)

--- e107_themes/templates/admin_icons_template.php
+++ e107_themes/templates/admin_icons_template.php
@@ -15,46 +15,46 @@
 */
 
 	define("ADMIN_TRUE_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/true_16.png' alt='' />");
-	define("ADMIN_TRUE_ICON_PATH", e_IMAGE."admin_images/true_16.png");
+	define("ADMIN_TRUE_ICON_PATH", e_IMAGE_ABS."admin_images/true_16.png");
 
 	define("ADMIN_FALSE_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/false_16.png' alt='' />");
-	define("ADMIN_FALSE_ICON_PATH", e_IMAGE."admin_images/false_16.png");
+	define("ADMIN_FALSE_ICON_PATH", e_IMAGE_ABS."admin_images/false_16.png");
 
 	define("ADMIN_EDIT_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/edit_16.png' alt='' title='".LAN_EDIT."' />");
-	define("ADMIN_EDIT_ICON_PATH", e_IMAGE."admin_images/edit_16.png");
+	define("ADMIN_EDIT_ICON_PATH", e_IMAGE_ABS."admin_images/edit_16.png");
 
 	define("ADMIN_DELETE_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/delete_16.png' alt='' title='".LAN_DELETE."' />");
-	define("ADMIN_DELETE_ICON_PATH", e_IMAGE."admin_images/delete_16.png");
+	define("ADMIN_DELETE_ICON_PATH", e_IMAGE_ABS."admin_images/delete_16.png");
 
 	define("ADMIN_UP_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/up_16.png' alt='' title='".LAN_DELETE."' />");
-	define("ADMIN_UP_ICON_PATH", e_IMAGE."admin_images/up_16.png");
+	define("ADMIN_UP_ICON_PATH", e_IMAGE_ABS."admin_images/up_16.png");
 
 	define("ADMIN_DOWN_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/down_16.png' alt='' title='".LAN_DELETE."' />");
-	define("ADMIN_DOWN_ICON_PATH", e_IMAGE."admin_images/down_16.png");
+	define("ADMIN_DOWN_ICON_PATH", e_IMAGE_ABS."admin_images/down_16.png");
 
 	define("ADMIN_WARNING_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/warning_16.png' alt='' />");
-	define("ADMIN_WARNING_ICON_PATH", e_IMAGE."admin_images/warning_16.png");
+	define("ADMIN_WARNING_ICON_PATH", e_IMAGE_ABS."admin_images/warning_16.png");
 
 	define("ADMIN_INFO_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/info_16.png' alt='' />");
-	define("ADMIN_INFO_ICON_PATH", e_IMAGE."admin_images/info_16.png");
+	define("ADMIN_INFO_ICON_PATH", e_IMAGE_ABS."admin_images/info_16.png");
 
 	define("ADMIN_CONFIGURE_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/configure_16.png' alt='' />");
-	define("ADMIN_CONFIGURE_ICON_PATH", e_IMAGE."admin_images/configure_16.png");
+	define("ADMIN_CONFIGURE_ICON_PATH", e_IMAGE_ABS."admin_images/configure_16.png");
 
 	define("ADMIN_ADD_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/add_16.png' alt='' />");
-	define("ADMIN_ADD_ICON_PATH", e_IMAGE."admin_images/add_16.png");
+	define("ADMIN_ADD_ICON_PATH", e_IMAGE_ABS."admin_images/add_16.png");
 
 	define("ADMIN_VIEW_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/search_16.png' alt='' />");
-	define("ADMIN_VIEW_ICON_PATH", e_IMAGE."admin_images/admin_images/search_16.png");
+	define("ADMIN_VIEW_ICON_PATH", e_IMAGE_ABS."admin_images/admin_images/search_16.png");
 
 	define("ADMIN_URL_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/forums_16.png' alt='' />");
-	define("ADMIN_URL_ICON_PATH", e_IMAGE."admin_images/forums_16.png");
+	define("ADMIN_URL_ICON_PATH", e_IMAGE_ABS."admin_images/forums_16.png");
 
 	define("ADMIN_INSTALLPLUGIN_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/plugin_install_16.png' alt='' />");
-	define("ADMIN_INSTALLPLUGIN_ICON_PATH", e_IMAGE."admin_images/plugin_install_16.png");
+	define("ADMIN_INSTALLPLUGIN_ICON_PATH", e_IMAGE_ABS."admin_images/plugin_install_16.png");
 
 	define("ADMIN_UNINSTALLPLUGIN_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/plugin_uninstall_16.png' alt='' />");
-	define("ADMIN_UNINSTALLPLUGIN_ICON_PATH", e_IMAGE."admin_images/plugin_unstall_16.png");
+	define("ADMIN_UNINSTALLPLUGIN_ICON_PATH", e_IMAGE_ABS."admin_images/plugin_unstall_16.png");
 
 	define("ADMIN_UPGRADEPLUGIN_ICON", "<img class='icon action S16' src='".e_IMAGE_ABS."admin_images/up_16.png' alt='' />");
-	define("ADMIN_UPGRADEPLUGIN_ICON_PATH", e_IMAGE."admin_images/up_16.png");
+	define("ADMIN_UPGRADEPLUGIN_ICON_PATH", e_IMAGE_ABS."admin_images/up_16.png");
