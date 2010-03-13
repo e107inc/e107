0be60935f5caae119728f8598cbafe0a0c542c7d
e107steved <steved@e107.org>
2010-03-13 22:39:39 +0000

    Notice removal, general tidy

--- e107_plugins/log/e_help.php
+++ e107_plugins/log/e_help.php
@@ -2,38 +2,44 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2010 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
+ *	Stats logging plugin - admin help text
  *
+ * $URL$
+ * $Id$
+ */
+
+
+/**
+ *	e107 Stats logging plugin
  *
- * $Source: /cvs_backup/e107_0.8/e107_plugins/log/e_help.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ *	@package	e107_plugins
+ *	@subpackage	log
+ *	@version 	$Id$;
  */
 
 if (!defined('e107_INIT')) { exit; }
 
-define("LOGPATH", e_PLUGIN."log/");
-include_lan(LOGPATH."languages/".e_LANGUAGE."_log_help.php");
+include_lan(e_PLUGIN.'log/languages/'.e_LANGUAGE."_log_help.php");
 
 if (e_QUERY) list($action,$junk) = explode('.',e_QUERY); else $action = 'list';
 
 switch ($action)
 {
-case 'export' :
-  $text = LAN_STAT_HELP_04;
-  break;
-case 'rempage' :
-  $text = LAN_STAT_HELP_03;
-  break;
-case 'history' :
-  $text = LAN_STAT_HELP_02;
-  break;
-default :
-  $text = LAN_STAT_HELP_05;
+	case 'export' :
+		$text = LAN_STAT_HELP_04;
+		break;
+	case 'rempage' :
+		$text = LAN_STAT_HELP_03;
+		break;
+	case 'history' :
+		$text = LAN_STAT_HELP_02;
+		break;
+	default :
+		$text = LAN_STAT_HELP_05;
 }
 $ns -> tablerender(LAN_STAT_HELP_01, $text);
 unset($text);
