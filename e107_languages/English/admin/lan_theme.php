e72242533ebc8aedde149c7003df812066ba315b
e107steved <e107steved@localhost>
2010-05-01 20:59:21 +0000

    Bugtracker #4938 - disable unused LAN
    svn path=/trunk/e107_0.7/; revision=11512

--- e107_languages/English/admin/lan_theme.php
+++ e107_languages/English/admin/lan_theme.php
@@ -1,15 +1,17 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system - Language File.
-|
-|     $Source: /cvs_backup/e107_0.7/e107_languages/English/admin/lan_theme.php,v $
-|     $Revision$
-|     $Date$
-|     $Author$
-+----------------------------------------------------------------------------+
-*/
-define("TPVLAN_1", "You are looking at a preview of the <b>'".PREVIEWTHEMENAME."'</b> theme. It has not been set as the main theme for your site, it has been activated to provide a preview of how the theme looks.<br />To set this theme as your site theme, <a href='".e_ADMIN."theme.php'>return to your theme manager</a> and select 'Set As Site Theme'.<br />To preview more themes please <a href='".e_ADMIN."theme.php'>click here</a>");
+* e107 website system
+*
+* Copyright (C) 2008-2010 e107 Inc (e107.org)
+* Released under the terms and conditions of the
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+*
+* Language file - Theme manager admin
+*
+* $URL$
+* $Id$
+*
+*/define("TPVLAN_1", "You are looking at a preview of the <b>'".PREVIEWTHEMENAME."'</b> theme. It has not been set as the main theme for your site, it has been activated to provide a preview of how the theme looks.<br />To set this theme as your site theme, <a href='".e_ADMIN."theme.php'>return to your theme manager</a> and select 'Set As Site Theme'.<br />To preview more themes please <a href='".e_ADMIN."theme.php'>click here</a>");
 define("TPVLAN_2", "Theme Preview");
 define("TPVLAN_3", "Main site theme set to");
 define("TPVLAN_4", "Author");
@@ -36,7 +38,7 @@ define("TPVLAN_21", "This is the currently selected site theme");
 define("TPVLAN_22", "This theme has multiple stylesheets");
 define("TPVLAN_23", "default stylesheet");
 define("TPVLAN_24", "no information");
-define("TPVLAN_25", "To choose which stylesheet to use, please go to <a href='".e_ADMIN."prefs.php'>preferences</a> and click on 'Theme'.");
+//define("TPVLAN_25", "To choose which stylesheet to use, please go to <a href='".e_ADMIN_ABS."theme.php'>Theme Manager</a> and click on 'Theme'.");
 
 define("TPVLAN_26", "Theme Manager");
 define("TPVLAN_27", "Please select stylesheet to use");
