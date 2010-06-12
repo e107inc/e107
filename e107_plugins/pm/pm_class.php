a279cc738c5c4e92f472c0c1705bde1d42bcd30d
e107steved <e107steved@localhost>
2010-06-12 21:45:26 +0000

    Bugtracker #4973 - may pick wrong member for PM - now looks for exact name match before considering similar names
    svn path=/trunk/e107_0.7/; revision=11576

--- e107_plugins/pm/pm_class.php
+++ e107_plugins/pm/pm_class.php
@@ -333,17 +333,29 @@ class private_message
 		}
 	}
 
+
 	function pm_getuid($var)
 	{
 		global $sql, $tp;
 		$var = trim($var);
-		if($sql->db_Select("user", "user_id, user_name, user_class, user_email", "user_name LIKE '".$sql -> escape(trim($var), TRUE)."'"))
+		$result = $sql->db_Select("user", "user_id, user_name, user_class, user_email", "user_name LIKE '".$sql -> escape($var, TRUE)."'");
+		if ($result === FALSE) return FALSE;
+		$firstRow = $sql->db_Fetch();
+		if (($result == 1) || ($firstRow['user_name'] == $var))
 		{
-			$row = $sql->db_Fetch();
-			return $row;
+			return $firstRow;		// Finished if exact match, or only one entry found
 		}
-		return FALSE;
+		while ($row = $sql->db_Fetch())
+		{
+			if ($row['user_name'] == $var)
+			{
+				return $row;		// Finished on exact match
+			}
+		}
+		
+		return $firstRow;		// Return the first record returned, for BC
 	}
+
 
 	function get_users_inclass($class)
 	{
