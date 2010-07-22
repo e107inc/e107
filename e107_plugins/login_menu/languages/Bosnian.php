b20b6b3636293d13da30d84c971d4f43f718ab93
e107coders <e107coders@localhost>
2010-07-22 23:57:37 +0000

    Bosnian and Hebrew
    svn path=/trunk/e107_langpacks/; revision=11611

---
+++ e107_plugins/login_menu/languages/Bosnian.php
@@ -0,0 +1,63 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvsroot/e107/e107_0.7/e107_plugins/login_menu/languages/English.php,v $
+|     $Revision: 1.4 $
+|     $Date: 2006/04/27 06:02:46 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
+*/
+
+define("LOGIN_MENU_L1", "Korisničko Ime: ");
+define("LOGIN_MENU_L2", " Lozinka: ");
+define("LOGIN_MENU_L3", "Upis");
+define("LOGIN_MENU_L4", " Zaboravljena Lozinka?");
+define("LOGIN_MENU_L5", " Dobrodošli");
+define("LOGIN_MENU_L6", " Upamti me");
+define("LOGIN_MENU_L7", " Vaše jedinstveno korisničko ime nije prepoznato (mogućnost greške sa cookie).<br />Molimo <a href=\"".e_BASE."index.php?logout\">Kliknite ovdje</a> da bi uništili cookie.");
+define("LOGIN_MENU_L8", "Odjava");
+define("LOGIN_MENU_L9", " Greška kod upisivanja");
+define("LOGIN_MENU_L10", "The maintenance flag is true - this means normal visitors are being redirected to sitedown.php. To reset the flag go to admin/maintenance.");
+define("LOGIN_MENU_L11", "Admin Area");
+define("LOGIN_MENU_L12", "Postavke");
+define("LOGIN_MENU_L13", "Profil");
+define("LOGIN_MENU_L14", "novost");
+define("LOGIN_MENU_L15", "novosti");
+define("LOGIN_MENU_L16", "chatbox poruka");
+define("LOGIN_MENU_L17", "chatbox poruke");
+define("LOGIN_MENU_L18", "komentariši");
+define("LOGIN_MENU_L19", "komentari");
+define("LOGIN_MENU_L20", "forum poruka");
+define("LOGIN_MENU_L21", "forum poruke");
+define("LOGIN_MENU_L22", " Nova članska stranica");
+define("LOGIN_MENU_L23", " Nove članske stranice");
+define("LOGIN_MENU_L24", " Kliknite tu za prikaz zadnjih novosti");
+define("LOGIN_MENU_L25", " Od vaše zadnje posjete je prošlo");
+define("LOGIN_MENU_L26", "ne");
+define("LOGIN_MENU_L27", "i");
+define("LOGIN_MENU_L28", "Prijava");
+define("LOGIN_MENU_L29", " nov članak");
+define("LOGIN_MENU_L30", " novi članci");
+
+// New config options
+define('LOGIN_MENU_L31', 'Prikaz Novosti ');
+define('LOGIN_MENU_L32', 'Novi Poslani Članci');
+define('LOGIN_MENU_L33', 'Nove Chatbox Poruke');
+define('LOGIN_MENU_L34', 'Novi Komentari');
+define('LOGIN_MENU_L35', 'Nove Forum Poruke');
+define('LOGIN_MENU_L36', 'Prikaz Novih Članova');
+define('LOGIN_MENU_L37', 'Obnovi Postavke');
+define('LOGIN_MENU_L38', 'Postavke Su Obnovljene');
+
+define('LOGIN_MENU_L39', 'Napusti Admin područje');
+define("LOGIN_MENU_L40", "Ponovno poslan Aktivacijski Email");
+?>
---
+++ e107_plugins/login_menu/languages/Hebrew.php
@@ -0,0 +1,65 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvsroot/e107/e107_0.7/e107_plugins/login_menu/languages/English.php,v $
+|     $Revision: 1.4 $
+|     $Date: 2006/04/27 06:02:46 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
+*/
+
+define("LOGIN_MENU_L1", "שם משתמש: ");
+define("LOGIN_MENU_L2", "סיסמה: ");
+define("LOGIN_MENU_L3", "הירשם");
+define("LOGIN_MENU_L4", "שכחת את הסיסמה?");
+define("LOGIN_MENU_L5", "ברוך הבא");
+define("LOGIN_MENU_L6", "זכור אותי");
+define("LOGIN_MENU_L7", "שם משתמש לא ידוע  (יתכן וקובץ הקוקי פגום).<br />Please <a href=\"".e_BASE."index.php?logout\">הקלק כאן</a> אל הקוקי הפגום.");
+define("LOGIN_MENU_L8", "התנתק");
+define("LOGIN_MENU_L9", "שגיאה בהתחברות");
+define("LOGIN_MENU_L10", "דגל התחזוקה מורם - פירוש הדבר שמבקרים מופנים לדף sitedown.php. לאיפוס הדגל גשו אל admin/maintenance.");
+define("LOGIN_MENU_L11", "אזור של מנהל האתר");
+define("LOGIN_MENU_L12", "הגדרות");
+define("LOGIN_MENU_L13", "פרופיל");
+define("LOGIN_MENU_L14", "ידיעה חדשותית");
+define("LOGIN_MENU_L15", "ידיעות חדשותיות");
+define("LOGIN_MENU_L16", "הודעה לתיבת השיחה");
+define("LOGIN_MENU_L17", "הודעות לתיבת השיחה");
+define("LOGIN_MENU_L18", "הערה");
+define("LOGIN_MENU_L19", "הערות");
+define("LOGIN_MENU_L20", "הודעה לקבוצת הדיון");
+define("LOGIN_MENU_L21", "הודעות לקבוצת הדיון");
+define("LOGIN_MENU_L22", "חבר חדש באתר");
+define("LOGIN_MENU_L23", "חברים חדשים באתר");
+define("LOGIN_MENU_L24", "הקלק כאן כדי לראות רשימה של ההודעות החדשות");
+define("LOGIN_MENU_L25", "הביקור האחרון שלכם היה");
+define("LOGIN_MENU_L26", "לא");
+define("LOGIN_MENU_L27", "ו");
+define("LOGIN_MENU_L28", "התחבר");
+define("LOGIN_MENU_L29", "מאמר חדש");
+define("LOGIN_MENU_L30", "מאמרים חדשים");
+
+// אפשרויות תצורה חדשות
+define('LOGIN_MENU_L31', 'הראה הודעות חדשותיות חדשות');
+define('LOGIN_MENU_L32', 'הראה מאמרים חדשים');
+define('LOGIN_MENU_L33', 'הראה הודעות חדשות בתיבת השיחה');
+define('LOGIN_MENU_L34', 'הראה הערות חדשות שנשלחו');
+define('LOGIN_MENU_L35', 'הראה הודעות חדשות לקבוצת הדיון');
+define('LOGIN_MENU_L36', 'הראה חברים חדשים');
+define('LOGIN_MENU_L37', 'עדכן הגדרות');
+define('LOGIN_MENU_L38', 'ההגדרות עודכנו');
+
+define('LOGIN_MENU_L39', 'צא מאזור זה');
+define("LOGIN_MENU_L40", "שלח מחדש הפעלה מייל");
+define("LOGIN_MENU_L41", "התחברות תפריט הגדרות");
+
+?>
