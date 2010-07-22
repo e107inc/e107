76dd84a96b1fae9f8e1f84cda5aa9e9d0ca1f288
e107coders <e107coders@localhost>
2010-07-22 23:59:59 +0000

    ChineseSimp
    svn path=/trunk/e107_langpacks/; revision=11612

---
+++ e107_plugins/login_menu/languages/ChineseSimp.php
@@ -0,0 +1,61 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvs_backup/e107_langpacks/e107_plugins/login_menu/languages/Chinese-Simp.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2006-10-12 05:13:44 $
+|     $Author: neteasy $
++----------------------------------------------------------------------------+
+*/
+	
+define("LOGIN_MENU_L1", "用户名: ");
+define("LOGIN_MENU_L2", "密码: ");
+define("LOGIN_MENU_L3", "注册");
+define("LOGIN_MENU_L4", "忘记密码?");
+define("LOGIN_MENU_L5", "欢迎");
+define("LOGIN_MENU_L6", "记住我");
+define("LOGIN_MENU_L7", "无法识别用户ID(可能cookie损坏)。<br />请<a href=\"".e_BASE."index.php?logout\">点击这里</a>删除cookie。");
+define("LOGIN_MENU_L8", "退出系统");
+define("LOGIN_MENU_L9", "登录错误");
+define("LOGIN_MENU_L10", "系统正在维护中 - 所有访问将跳转到sitedown.php。要关闭维护状态，进入管理页面/网站维护。");
+define("LOGIN_MENU_L11", "网站管理");
+define("LOGIN_MENU_L12", "修改资料");
+define("LOGIN_MENU_L13", "个人档案");
+define("LOGIN_MENU_L14", "个新项目");
+define("LOGIN_MENU_L15", "个新项目");
+define("LOGIN_MENU_L16", "个聊天发言");
+define("LOGIN_MENU_L17", "个聊天发言");
+define("LOGIN_MENU_L18", "个评论");
+define("LOGIN_MENU_L19", "个评论");
+define("LOGIN_MENU_L20", "个论坛帖子");
+define("LOGIN_MENU_L21", "个论坛帖子");
+define("LOGIN_MENU_L22", "个新会员");
+define("LOGIN_MENU_L23", "个新会员");
+define("LOGIN_MENU_L24", "点击这里查看新项目列表");
+define("LOGIN_MENU_L25", "自上次访问后有");
+define("LOGIN_MENU_L26", "0");
+define("LOGIN_MENU_L27", "和");
+define("LOGIN_MENU_L28", "登入");
+	
+define("LOGIN_MENU_L29", "新文章");
+define("LOGIN_MENU_L30", "新文章");
+	
+// New config options
+define('LOGIN_MENU_L31', '显示新的新闻');
+define('LOGIN_MENU_L32', '显示新的文章');
+define('LOGIN_MENU_L33', '显示新的聊天发言');
+define('LOGIN_MENU_L34', '显示新的评论');
+define('LOGIN_MENU_L35', '显示新的论坛帖子');
+define('LOGIN_MENU_L36', '显示新的会员');
+define('LOGIN_MENU_L37', '更新设置');
+define('LOGIN_MENU_L38', '设置已更新');
+
+
+define('LOGIN_MENU_L39', '离开管理页面');
+	
+?>
