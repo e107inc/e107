111fc1431e18a14597806e6219186646c6f7ad71
e107coders <e107coders@localhost>
2010-08-05 01:18:09 +0000

    Fix for event/trigger communication between plugins.
    svn path=/trunk/e107_0.7/; revision=11649

--- e107_handlers/event_class.php
+++ e107_handlers/event_class.php
@@ -1,54 +1,125 @@
 <?php
 /*
 + ----------------------------------------------------------------------------+
-|     e107 website system
+|	e107 website system
 |
-|     ©Steve Dunstan 2001-2002
-|     http://e107.org
-|     jalist@e107.org
+| 	Copyright (C) 2008-2010 e107 Inc (e107.org)
+| 	Released under the terms and conditions of the
+| 	GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
 |
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.7/e107_handlers/event_class.php,v $
-|     $Revision$
-|     $Date$
-|     $Author$
+|	$Source: /cvs_backup/e107_0.7/e107_handlers/event_class.php,v $
+|	$Revision$
+|	$Date$
+|	$Author$
 +----------------------------------------------------------------------------+
 */
 	
 if (!defined('e107_INIT')) { exit; }
 
-class e107_event {
+class e107_event
+{
 	var $functions = array();
 	var $includes = array();
-	 
-	function register($eventname, $function, $include='') {
-		if ($include!='') {
-			$this->includes[$eventname][] = $include;
+	var $debug = FALSE;
+	
+
+	function e107_event()
+	{
+		// Fix for events triggered between plugins. 
+		// Events will register regardinless of the load order.  
+		
+		if(varset($_SESSION['e_EVENT_functions']))
+		{
+			$this->functions = $_SESSION['e_EVENT_functions'];					
 		}
-		$this->functions[$eventname][] = $function;
+		if(varset($_SESSION['e_EVENT_includes']))
+		{
+			$this->includes = $_SESSION['e_EVENT_includes'];					
+		}			
 	}
-	 
-	function trigger($eventname, &$data) {
-		if (isset($this -> includes[$eventname])) {
-			foreach($this->includes[$eventname] as $evt_inc) {
-				if (file_exists($evt_inc)) {
+	
+	/**
+	 * Register an e107 Event
+	 * @param object $eventname
+	 * @param object $function
+	 * @param object $include [optional]
+	 * Include is required if the trigger is in a plugin that is loaded prior to the plugin that contains the function. 
+	 * @return 
+	 */
+	function register($eventname, $function, $include='')
+	{
+		// 
+		if ($include!='')
+		{
+			// $this->includes[$eventname][] = $include;
+			if(!in_array($include,$_SESSION['e_EVENT_includes'][$eventname]))
+			{
+				$_SESSION['e_EVENT_includes'][$eventname][] = $include;		
+			}			
+		}
+		
+		// $this->functions[$eventname][] = $function;
+		if(!in_array($function,$_SESSION['e_EVENT_functions'][$eventname]))
+		{
+			$_SESSION['e_EVENT_functions'][$eventname][] = $function;
+		}		
+	}
+	
+	/**
+	 * Trigger an e107 Event
+	 * @param object $eventname
+	 * @param object $data
+	 * @return boolean
+	 */
+	function trigger($eventname, &$data)
+	{
+		if (isset($this -> includes[$eventname]))
+		{
+			foreach($this->includes[$eventname] as $evt_inc)
+			{
+				if (file_exists($evt_inc))
+				{
 					include_once($evt_inc);
 				}
 			}
 		}
-		if (isset($this -> functions[$eventname])) {
-			foreach($this->functions[$eventname] as $evt_func) {
-				if (function_exists($evt_func)) {
+		if (isset($this -> functions[$eventname]))
+		{
+			foreach($this->functions[$eventname] as $evt_func)
+			{
+				if (function_exists($evt_func))
+				{
 					$ret = $evt_func($data);
-					if ($ret!='') {
+					$this->logData($evt_func);
+					if ($ret!='')
+					{
 						break;
 					}
 				}
 			}
+			
 		}
 		return (isset($ret) ? $ret : false);
+	}
+	
+	/**
+	 * Enable the event Log for debugging. 
+	 * @return 
+	 */
+	function debug()
+	{
+		$this->debug = TRUE;
+	}
+	
+	
+	function logData($message)
+	{
+		if($this->debug == FALSE){ return; }
+		if($fp = @fopen(e_HANDLER."event.log","a+"))
+		{	
+			$contents = @fwrite($fp, date('r')." :: ".$message ." \n");
+			@fclose($fp);
+		}			
 	}
 }
 	
