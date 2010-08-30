0b044d29898e4b1b0a288bb89e19f3b0d1a7ad4d
e107steved <e107steved@localhost>
2010-08-30 16:59:06 +0000

    Bugtracker #5032 possible fix - path issue
    svn path=/trunk/e107_0.7/; revision=11721

--- e107_languages/English/lan_error.php
+++ e107_languages/English/lan_error.php
@@ -39,7 +39,7 @@ define("LAN_ERROR_24", " was unsuccessful.");
 define("LAN_ERROR_25", "[1]: Unable to read core settings from database - Core settings exist but cannot be unserialized. Attempting to restore core backup ...");
 define("LAN_ERROR_26", "[2]: Unable to read core settings from database - non-existent core settings.");
 define("LAN_ERROR_27", "[3]: Core settings saved - backup made active.");
-define("LAN_ERROR_28", "[4]: No core backup found. Check that your database has valid content. If not, please run the <a href='".e_FILE."resetcore/resetcore.php'>Reset_Core</a> utility to rebuild your core settings. <br />After rebuilding your core please save a backup from the admin/sql screen.");
+define("LAN_ERROR_28", "[4]: No core backup found. Check that your database has valid content. If not, please run the <a href='".e_FILE_ABS."resetcore/resetcore.php'>Reset_Core</a> utility to rebuild your core settings. <br />After rebuilding your core please save a backup from the admin/sql screen.");
 define("LAN_ERROR_29", "[5]: Field(s) have been left blank. Please resubmit the form and fill in the required fields.");
 define("LAN_ERROR_30", "[6]: Unable to form a valid connection to mySQL. Please check that your e107_config.php contains the correct information.");
 define("LAN_ERROR_31", "[7]: mySQL is running but database ({$mySQLdefaultdb}) couldn't be connected to.<br />Please check it exists and that your e107_config.php contains the correct information.");
