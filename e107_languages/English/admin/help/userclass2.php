d30fa0f9a3ccc59f103cbe173577f718f4e4d8cc
e107steved <e107steved@localhost>
2010-09-03 12:18:37 +0000

    Bugtracker #5042 - text correction
    svn path=/trunk/e107_0.7/; revision=11737

--- e107_languages/English/admin/help/userclass2.php
+++ e107_languages/English/admin/help/userclass2.php
@@ -19,7 +19,7 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$caption = "Use Class Help";
+$caption = "User Class Help";
 $text = "You can create or edit/delete existing classes from this page.<br />This is useful for restricting users to certain parts of your site. For example, you could create a class called TEST, then create a forum which only allowed users in the TEST class to access it.";
 $ns -> tablerender($caption, $text);
 ?>
