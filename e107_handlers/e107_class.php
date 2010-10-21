a46ce0dd05f45a013f3b40d6e0f3903c790f3d57
e107steved <e107steved@localhost>
2010-10-21 18:03:31 +0000

    Modify handling of reverse proxies
    svn path=/trunk/e107_0.7/; revision=11909

--- e107_handlers/e107_class.php
+++ e107_handlers/e107_class.php
@@ -186,35 +186,31 @@ class e107{
 	 */
 	function getip() 
 	{
-	  if(!$this->_ip_cache)
-	  {
-		if (getenv('HTTP_X_FORWARDED_FOR')) 
+		if(!$this->_ip_cache)
 		{
-		  $ip=$_SERVER['REMOTE_ADDR'];
-		  if (preg_match("/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", getenv('HTTP_X_FORWARDED_FOR'), $ip3)) 
-		  {
-			$ip2 = array('#^0\..*#', 
-					'#^127\..*#', 							// Local loopbacks
-					'#^192\.168\..*#', 						// RFC1918 - Private Network
-					'#^172\.(?:1[6789]|2\d|3[01])\..*#', 	// RFC1918 - Private network
-					'#^10\..*#', 							// RFC1918 - Private Network
-					'#^169\.254\..*#', 						// RFC3330 - Link-local, auto-DHCP 
-					'#^2(?:2[456789]|[345][0-9])\..*#'		// Single check for Class D and Class E
-					);
-			$ip = preg_replace($ip2, $ip, $ip3[1]);
-		  }
-		} 
-		else 
-		{
-		  $ip = $_SERVER['REMOTE_ADDR'];
+			$ip=$_SERVER['REMOTE_ADDR'];
+			if (getenv('HTTP_X_FORWARDED_FOR')) 
+			{
+				if (preg_match("/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", getenv('HTTP_X_FORWARDED_FOR'), $ip3)) 
+				{
+					$ip2 = array('#^0\..*#', 
+							'#^127\..*#', 							// Local loopbacks
+							'#^192\.168\..*#', 						// RFC1918 - Private Network
+							'#^172\.(?:1[6789]|2\d|3[01])\..*#', 	// RFC1918 - Private network
+							'#^10\..*#', 							// RFC1918 - Private Network
+							'#^169\.254\..*#', 						// RFC3330 - Link-local, auto-DHCP 
+							'#^2(?:2[456789]|[345][0-9])\..*#'		// Single check for Class D and Class E
+							);
+					$ip = preg_replace($ip2, $ip3[1], $ip);
+				}
+			} 
+			if ($ip == "") 
+			{
+				$ip = "x.x.x.x";
+			}
+			$this->_ip_cache = $ip;
 		}
-		if ($ip == "") 
-		{
-		  $ip = "x.x.x.x";
-		}
-		$this->_ip_cache = $ip;
-	  }
-	  return $this->_ip_cache;
+		return $this->_ip_cache;
 	}
 
 	function get_host_name($ip_address) 
