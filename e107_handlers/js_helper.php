83f716ec3ecb8354586bc20aa7f96eafef165da9
secretr <m.yovchev@gmail.com>
2010-12-10 14:20:15 +0000

    php_compatibility_handler - JSON support added

--- e107_handlers/js_helper.php
+++ e107_handlers/js_helper.php
@@ -15,23 +15,6 @@
  *
 */
 
-//PHP < 5.2 compatibility
-if (!function_exists('json_encode'))
-{
-    require_once(e_HANDLER.'json_compat_handler.php');
-    function json_encode($array)
-    {
-        $json = new Services_JSON();
-        return $json->encode($array);
-    }
-
-    function json_decode($json_obj)
-    {
-        $json = new Services_JSON();
-        return $json->decode($json_obj);
-    }
-}
-
 class e_jshelper
 {
     /**
