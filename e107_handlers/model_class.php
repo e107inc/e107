c2ccfb8e910766a5873a03a2ec3345bf2d7e936b
secretr <m.yovchev@gmail.com>
2010-12-29 13:39:15 +0000

    e_model - strict comparison required

--- e107_handlers/model_class.php
+++ e107_handlers/model_class.php
@@ -2,15 +2,13 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2010 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * e107 Base Model
  *
- * $Source: /cvs_backup/e107_0.8/e107_handlers/model_class.php,v $
- * $Revision$
- * $Date$
+ * $Id$
  * $Author$
 */
 
@@ -270,7 +268,7 @@ class e_object
 	 */
 	public function setParam($key, $value)
 	{
-		if(null == $value)
+		if(null === $value)
 		{
 			unset($this->_params[$key]);
 		}
