7d28d7796019b58f300f5987f2bec9be7965e1f2
e107coders <e107coders@localhost>
2011-01-05 23:03:48 +0000

    Bugfix: Inserting bbcode into a pre-populated textarea without a selection would cause content to be removed in some cases.
    svn path=/trunk/e107_0.7/; revision=12028

--- e107_files/e107.js
+++ e107_files/e107.js
@@ -236,7 +236,7 @@ function addtext(text, emote) {
 	if (!window.e107_selectedInputArea) {
 		return; //[SecretR] TODO - alert the user 
 	}
-		
+
 	var eField = e107_selectedInputArea;	
 	var eSelection 	= false;  
 	var tagOpen = '';
@@ -257,6 +257,7 @@ function addtext(text, emote) {
 
 	// Windows user  
 	if (document.selection) {
+	
 		eSelection = document.selection.createRange().text;
 		eField.focus();
 		if (eSelection) {
@@ -277,9 +278,11 @@ function addtext(text, emote) {
 	var selLength = eField.textLength;
 	var selStart = eField.selectionStart;
 	var selEnd = eField.selectionEnd; 
-	if (selEnd <= 2 && typeof(selLength) != 'undefined') {
+	
+	if (selEnd <= 2 && typeof(selLength) != 'undefined' && (selStart != selEnd)) {
 		selEnd = selLength;
 	}
+	
 	var sel1 = (eField.value).substring(0,selStart);
 	var sel2 = (eField.value).substring(selStart, selEnd);
 	var sel3 = (eField.value).substring(selEnd, selLength);
