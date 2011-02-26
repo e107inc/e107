098cf5f687c3923a51326c50f764ff7f466f581b
CaMer0n <cameron@e107coders.org>
2011-02-26 19:58:28 +0000

    Added some additional native language titles.

--- e107_handlers/language_class.php
+++ e107_handlers/language_class.php
@@ -197,11 +197,13 @@ class language{
 			"ChineseSimp"  	=> "简体中文",
 			"Dutch"			=> "Nederlands",
 			"English"		=> "English",
+			"Estonian"		=> "Eesti",
 			"French"		=> "Français",
 			"German"		=> "Deutsch",
 			"Greek"			=> "Ελληνικά",
 			"Hebrew"		=> "עִבְרִית",
 			"Hungarian"		=> "Magyar",
+			"Indonesian"	=> "Bahasa Indonesia",
 			"Italian"		=> "Italiano",
 			"Japanese"		=> "日本語",
 			"Korean"		=> "한국어",
@@ -220,7 +222,8 @@ class language{
 			"Slovak"		=> "Slovensky",
 			"Swedish"		=> "Svenska",
 			"Thai"			=> "ภาษาไทย",
-			"Turkish"		=> "Türkçe"
+			"Turkish"		=> "Türkçe",
+			"Vietnamese"	=> "Tiếng Việt"
 		);
 
 	/**
