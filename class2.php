fd3d9a43411e39311c61157105f94a05e48969d1
nlstart <nlstart@localhost>
2011-03-10 18:46:11 +0000

    Delay in seconds rounded to the lowest quarter hour
    svn path=/trunk/e107_0.7/; revision=12090

--- class2.php
+++ class2.php
@@ -849,7 +849,7 @@ $e_deltaTime=0;
 
 if (isset($_COOKIE['e107_tdOffset'])) {
 	// Actual seconds of delay. See e107.js and footer_default.php
-	$e_deltaTime = $_COOKIE['e107_tdOffset'];
+	$e_deltaTime = (15*floor(($_COOKIE['e107_tdOffset']/60)/15))*60; // Delay in seconds rounded to the lowest quarter hour
 }
 
 if (isset($_COOKIE['e107_tzOffset'])) {
