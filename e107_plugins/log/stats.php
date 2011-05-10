270495d416ad4d9496b55c18c26f0b6b50b5aeaa
e107steved <steved@e107.org>
2011-05-10 21:27:34 +0000

    Bugtracker #5215 - quotes required round URL

--- e107_plugins/log/stats.php
+++ e107_plugins/log/stats.php
@@ -31,7 +31,7 @@ include_lan(e_PLUGIN.'log/languages/'.e_LANGUAGE.'.php');
 $bar = (file_exists(THEME.'images/bar.png') ? THEME_ABS.'images/bar.png' : e_IMAGE_ABS.'generic/bar.png');
 $eplug_css[] = "<style type='text/css'>
 <!--
-.b { background-image: url(".$bar."); border: 1px solid #999; height: 10px; font-size: 0px }
+.b { background-image: url('".$bar."'); border: 1px solid #999; height: 10px; font-size: 0px }
 -->
 </style>";
 
