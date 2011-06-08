dbf47650bc3789fbd326ce6f0775a9a758f9de08
CaMer0n <cameron@e107coders.org>
2011-06-08 03:16:47 +0000

    db-verify routines moved to class and integrated into main admin->database page.

--- e107_languages/English/admin/lan_db_verify.php
+++ e107_languages/English/admin/lan_db_verify.php
@@ -9,29 +9,32 @@
 |     $Author$
 +----------------------------------------------------------------------------+
 */
-define("DBLAN_1", "Unable to read the sql datafile<br /><br />Please ensure the file <b>core_sql.php</b> exists in the <b>/admin/sql</b> directory.");
+define("DBVLAN_1", "Unable to read the sql datafile<br /><br />Please ensure the file <b>core_sql.php</b> exists in the <b>/admin/sql</b> directory.");
 // define("DBLAN_2", "Verifying all");
 
-define("DBLAN_4", "Table");
-define("DBLAN_5", "Field");
-define("DBLAN_6", "Status");
-define("DBLAN_7", "Notes");
-define("DBLAN_8", "Mismatch");
-define("DBLAN_9", "Currently");
-define("DBLAN_10", "should be");
-define("DBLAN_11", "Field missing!");
-define("DBLAN_12", "Extra Field!");
-define("DBLAN_13", "Table missing!");
-define("DBLAN_14", "Choose table(s) to validate");
-define("DBLAN_15", "Start Verify");
-define("DBLAN_16", "SQL Verification");
-define("DBLAN_17", "Back");
-define("DBLAN_18", "tables");
-define("DBLAN_19", "Attempt to Fix");
-define("DBLAN_20", "Attempting to fix tables");
-define("DBLAN_21", "Fix Selected Items");
-define("DBLAN_22", " is not readable");
-define("DBLAN_23", "Database Utilities");
-define("DBLAN_24", "Please select action.");
-define("DBLAN_25", "Index missing!");
+define("DBVLAN_4", "Table");
+define("DBVLAN_5", "Field");
+define("DBVLAN_6", "Status");
+define("DBVLAN_7", "Notes");
+define("DBVLAN_8", "Mismatch");
+define("DBVLAN_9", "Currently");
+define("DBVLAN_10", "should be");
+define("DBVLAN_11", "Field missing!");
+define("DBVLAN_12", "Extra Field!");
+define("DBVLAN_13", "Table missing!");
+define("DBVLAN_14", "Choose table(s) to validate");
+define("DBVLAN_15", "Start Verify");
+define("DBVLAN_16", "SQL Verification");
+define("DBVLAN_17", "Back");
+define("DBVLAN_18", "tables");
+define("DBVLAN_19", "Attempt to Fix");
+define("DBVLAN_20", "Attempting to fix tables");
+define("DBVLAN_21", "Fix Selected Items");
+define("DBVLAN_22", " is not readable");
+define("DBVLAN_23", "Database Utilities");
+define("DBVLAN_24", "Please select action.");
+define("DBVLAN_25", "Index missing!");
+
+// IMPORTANT NOTE: DBLAN has been replaced by DBBLAN in this file since 0.7 due to conflicts with db.php
+
 ?>
