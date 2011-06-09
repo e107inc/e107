d28f36282cf5cb5b18a614f4ca925a3f7ffb4337
CaMer0n <cameron@e107coders.org>
2011-06-09 07:03:55 +0000

    More db-verify refinements.

--- e107_languages/English/admin/lan_db_verify.php
+++ e107_languages/English/admin/lan_db_verify.php
@@ -25,15 +25,15 @@ define("DBVLAN_13", "Table missing!");
 define("DBVLAN_14", "Choose table(s) to validate");
 define("DBVLAN_15", "Start Verify");
 define("DBVLAN_16", "SQL Verification");
-define("DBVLAN_17", "Back");
-define("DBVLAN_18", "tables");
+
 define("DBVLAN_19", "Attempt to Fix");
-define("DBVLAN_20", "Attempting to fix tables");
+
 define("DBVLAN_21", "Fix Selected Items");
-define("DBVLAN_22", " is not readable");
+define("DBVLAN_22", "[x] is not readable");
 define("DBVLAN_23", "Database Utilities");
 define("DBVLAN_24", "Please select action.");
 define("DBVLAN_25", "Index missing!");
+define("DBVLAN_26", "[x] table(s) have problems.");
 
 // IMPORTANT NOTE: DBLAN has been replaced by DBBLAN in this file since 0.7 due to conflicts with db.php
 
