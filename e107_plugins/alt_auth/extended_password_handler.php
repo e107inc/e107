5cb24000b9c85852f4e5b443ce200d065f543422
secretr <m.yovchev@gmail.com>
2011-09-16 11:37:33 +0000

    phpDoc change

--- e107_plugins/alt_auth/extended_password_handler.php
+++ e107_plugins/alt_auth/extended_password_handler.php
@@ -2,14 +2,14 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2011 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  *
  *
  * $URL$
- * $Id $
+ * $Id$
  */
 
 /*
