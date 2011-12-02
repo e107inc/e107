59ae5ad937f62659f2ef264e22831988f29af529
secretr <m.yovchev@gmail.com>
2011-12-02 12:22:33 +0000

    News category menu updated (template, fixes), latest news and navigation by months menus added

--- e107_plugins/news/news_categories_menu.php
+++ e107_plugins/news/news_categories_menu.php
@@ -1,32 +1,25 @@
 <?php
+/**
+ * Copyright (C) 2008-2011 e107 Inc (e107.org), Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * $Id$
+ * 
+ * News categories menu
+ */
 
 if (!defined('e107_INIT')) { exit; }
 
-$cacheString = 'nq_news_categories_menu_'.preg_replace('#[^\w]#', '', $parm);
+$cacheString = 'nq_news_categories_menu_'.md5($parm);
 $cached = e107::getCache()->retrieve($cacheString);
 if(false === $cached)
 {
-	e107::includeLan(e_PLUGIN.'news/languages/'.e_LANGUAGE.'.php');
+	e107::plugLan('news');
 
 	parse_str($parm, $parms);
 	$ctree = e107::getObject('e_news_category_tree', null, e_HANDLER.'news_class.php');
 
-	//TODO real template, menu parameters
-	$sc_style['NEWS_CATEGORY_NEWS_COUNT']['pre'] = '(';
-	$sc_style['NEWS_CATEGORY_NEWS_COUNT']['post'] = ')';
+	$template = e107::getTemplate('news', 'news_menu', 'category');
 
-	$template = array();
-	$template['start'] = '';
-	$template['item'] = '
-		<img src="'.THEME_ABS.'images/bullet2.gif" alt="bullet" class="icon" /> <a class="e-menu-link newscats{active}" href="{NEWS_CATEGORY_URL}">{NEWS_CATEGORY_TITLE} {NEWS_CATEGORY_NEWS_COUNT}</a>
-	';
-	$template['separator'] = '<br />';
-	$template['end'] = '';
-	
-	//always return
-	$parms['return'] = true;
-
-	$cached = $ctree->loadActive()->render($parms, true, $template);
+	$cached = $ctree->loadActive()->render($template, $parms, true);
 	e107::getCache()->set($cacheString, $cached);
 }
 
