65534df6ed8184c48aca0bffb9295621bc8da15d
e107steved <e107steved@localhost>
2011-12-24 15:44:53 +0000

    Bugtracker #5300 - alt_news rewrite
    svn path=/trunk/e107_0.7/; revision=12469

--- e107_plugins/alt_news/alt_news.php
+++ e107_plugins/alt_news/alt_news.php
@@ -1,50 +1,54 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
-|     Copyright (C) 2008-2010 e107 Inc (e107.org)
-|
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $URL$
-|     $Revision$
-|     $Id$
-|     $Author$
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2002-2010 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Alternate news display
+ *
+ * $URL$
+ * $Id$
+ */
 
 if (!defined('e107_INIT')) { exit; }
 
-require_once(e_HANDLER."news_class.php");
+require_once(e_HANDLER.'news_class.php');
 
-function alt_news($news_category) {
-	global $sql, $aj, $ns;
+function alt_news($news_category) 
+{
+	global $sql, $sql2, $tp, $ns;
 	$ix = new news;
-	if (strstr(e_QUERY, "cat")) {
+	$news_category = intval($news_category);
+	if (strstr(e_QUERY, 'cat')) 
+	{
 		$category = $news_category;
-		if ($category != 0) {
+		if ($category != 0) 
+		{
 			$gen = new convert;
-			$sql2 = new db;
-			$sql->db_Select("news_category", "*", "category_id='".intval($category)."'");
+			$sql->db_Select("news_category", "*", "category_id=".$category);
 			list($category_id, $category_name, $category_icon) = $sql->db_Fetch();
-			$category_name = $aj->tpa($category_name);
-			if (strstr($category_icon, "../")) {
+			$category_name = $tp->toHTML($category_name);
+			if (strstr($category_icon, "../")) 
+			{
 				$category_icon = str_replace("../", "", e_BASE.$category_icon);
-			} else {
+			} 
+			else 
+			{
 				$category_icon = THEME.$category_icon;
 			}
 
-			if ($count = $sql->db_Select("news", "*", "news_category='".intval($category)."' ORDER BY news_datestamp DESC")) {
-				while ($row = $sql->db_Fetch()) {
+			if ($count = $sql->db_Select('news', '*', 'news_category='.intval($category).' ORDER BY news_datestamp DESC')) 
+			{
+				while ($row = $sql->db_Fetch()) 
+				{
 					extract($row);
-					if ($news_title == "") {
-						$news_title = "Untitled";
+					if ($news_title == '') 
+					{
+						$news_title = 'Untitled';
 					}
-					$datestamp = $gen->convert_date($news_datestamp, "short");
+					$datestamp = $gen->convert_date($news_datestamp, 'short');
 					$news_body = strip_tags(substr($news_body, 0, 100))." ...";
 					$comment_total = $sql2->db_Count("comments", "(*)", "WHERE comment_item_id='".intval($news_id)."' AND comment_type='0' ");
 					$bullet = '';
@@ -89,36 +93,34 @@ function alt_news($news_category) {
 		return TRUE;
 	}
 
-	if ($sql->db_Select("news", "*", "news_class<255 AND (news_start=0 || news_start < ".time().") AND (news_end=0 || news_end>".time().") AND news_category='".intval($news_category)."' ORDER BY news_datestamp DESC LIMIT 0,".ITEMVIEW)) {
-		$sql2 = new db;
-		while (list($news['news_id'], $news['news_title'], $news['news_body'], $news['news_extended'], $news['news_datestamp'], $news['admin_id'], $news_category, $news['news_allow_comments'], $news['news_start'], $news['news_end'], $news['news_class']) = $sql->db_Fetch()) {
-
-			if (check_class($news['news_class']) || !$news['news_class']) {
-
-				if ($news['admin_id'] == 1 && $pref['siteadmin']) {
-					$news['admin_name'] = $pref['siteadmin'];
-				}
-				else if(!$news['admin_name'] = getcachedvars($news['admin_id'])) {
-					$sql2->db_Select("user", "user_name", "user_id='".intval($news['admin_id'])."' ");
-					list($news['admin_name']) = $sql2->db_Fetch();
-					cachevars($news['admin_id'], $news['admin_name']);
-				}
-
-					$sql2->db_Select("news_category", "*", "category_id='".intval($news_category)."' ");
+	if ($sql->db_Select('news', 'news_id, news_class, news_author, news_comment_total, news_datestamp, news_body, news_extended', 
+	"news_class != ".e_UC_NOBODY."  
+	AND (news_start=0 || news_start < ".time().") 
+	AND (news_end=0 || news_end>".time().") 
+	AND news_category=".$news_category." ORDER BY news_datestamp DESC LIMIT 0,".ITEMVIEW)) 
+	{
+//		while (list($news['news_id'], $news['news_title'], $news['data'], $news['news_extended'], $news['news_datestamp'], $news['admin_id'], $news_category, $news['news_allow_comments'], $news['news_start'], $news['news_end'], $news['news_class']) = $sql->db_Fetch()) {
+		while ($news = $sql->db_Fetch()) 
+		{
+			$news['user_id'] = intval($news['news_author']);
+			if(!$news['user_name'] = getcachedvars($news['user_id'])) 
+			{
+				$sql2->db_Select('user', 'user_name', 'user_id='.$news['user_id']);
+				list($news['user_name']) = $sql2->db_Fetch();
+				cachevars($news['user_id'], $news['user_name']);		// Cache user_id, user_name
+			}
+//			if (check_class($news['news_class']) || !$news['news_class']) 
+			if (check_class($news['news_class'])) 
+			{
+				$sql2->db_Select("news_category", "*", "category_id='".intval($news_category)."' ");
 
 				list($news['category_id'], $news['category_name'], $news['category_icon']) = $sql2->db_Fetch();
-				$news['comment_total'] = $sql2->db_Count("comments", "(*)", "WHERE comment_item_id='".intval($news['news_id'])."' AND comment_type='0' ");
 				$ix->render_newsitem($news);
-			} else {
-				if ($pref['subnews_hide_news'] == 1) {
-					if ($news['admin_id'] == 1 && $pref['siteadmin']) {
-						$news['admin_name'] = $pref['siteadmin'];
-					}
-					else if(!$news['admin_name'] = getcachedvars($news['admin_id'])) {
-						$sql2->db_Select("user", "user_name", "user_id='".intval($news['admin_id'])."' ");
-						list($news['admin_name']) = $sql2->db_Fetch();
-						cachevars($news['admin_id'], $news['admin_name']);
-					}
+			} 
+			else 
+			{
+				if ($pref['subnews_hide_news'] == 1) 
+				{
 
 					$sql2->db_Select("news_category", "*", "category_id='".intval($news_category)."' ");
 
