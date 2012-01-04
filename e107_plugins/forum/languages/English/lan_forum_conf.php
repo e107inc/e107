0036387defc029bb58406dfe64796484da878e79
berckoff <berckoff@localhost>
2012-01-04 16:54:46 +0000

    Bugtracker #5305 : fixed hard-coded LANs in the forum installation file;
    svn path=/trunk/e107_0.7/; revision=12508

--- e107_plugins/forum/languages/English/lan_forum_conf.php
+++ e107_plugins/forum/languages/English/lan_forum_conf.php
@@ -1,44 +1,54 @@
 <?php
-/*
-+ ----------------------------------------------------------------------------+
-|     e107 website system - Language File.
-|
-|     $URL$
-|     $Revision$
-|     $Id$
-|     $Author$
-+----------------------------------------------------------------------------+
-*/
-define("FORLAN_5", "Poll deleted.");
-define("FORLAN_6", "Thread deleted");
-define("FORLAN_7", "replies deleted");
-define("FORLAN_8", "Delete cancelled.");
-define("FORLAN_9", "Thread moved.");
-define("FORLAN_10", "Move cancelled.");
-define("FORLAN_11", "Back To Forums");
-define("FORLAN_12", "Forum Configuration");
-define("FORLAN_13", "Are you absolutely certain you want to delete this poll?<br />Once deleted it <b><u>cannot</u></b> be retrieved.");
-define("FORLAN_14", "Cancel");
-define("FORLAN_15", "Confirm Delete Forum Post");
-define("FORLAN_16", "Confirm Delete Poll");
-define("FORLAN_17", "posted by");
-define("FORLAN_18", "Are you absolutely certain you want to delete this forum");
-define("FORLAN_19", "thread and it's related posts?");
-define("FORLAN_20", "the poll will also be deleted");
-define("FORLAN_21", "Once deleted they");
-define("FORLAN_22", "post?<br />Once deleted it");
-define("FORLAN_23", "cannot</u></b> be retrieved");
-define("FORLAN_24", "Move thread  to forum");
-define("FORLAN_25", "Move Thread");
-define("FORLAN_26", "Reply deleted");
+/**
+ * e107 website system - English Language file
+ * 
+ * Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
+ * Copyright (C) 2008-2010 e107 Inc (e107.org)
+ * 
+ * Released under the terms and conditions of the GNU General Public License
+ * (http://gnu.org).
+ * 
+ * $URL$
+ * $Revision$
+ * $Id$
+ * $Author$
+ */
 
-define("FORLAN_27", "moved");
+# Install LANs
+define('LAN_FORUM_INSTALL_01', 'Forum');
+define('LAN_FORUM_INSTALL_02', 'This plugin is a fully featured Forum system.');
+define('LAN_FORUM_INSTALL_03', 'Configure Forum');
+define('LAN_FORUM_INSTALL_04', 'Your forum is now installed');
+define('LAN_FORUM_INSTALL_05', 'Forum successfully upgraded, now using version: %1$s');
+define('LAN_FORUM_INSTALL_06', '[forum]');
+define('LAN_FORUM_INSTALL_07', '[more...]');
 
-define("FORLAN_28", "Do not rename thread title");
-define("FORLAN_29", "Add");
-define("FORLAN_30", "to title");
-define("FORLAN_31", "Rename to:");
-define("FORLAN_32", "Rename thread options:");
-
-
-?>
+# Config LANs
+define('FORLAN_5', 'Poll deleted.');
+define('FORLAN_6', 'Thread deleted');
+define('FORLAN_7', 'replies deleted');
+define('FORLAN_8', 'Delete cancelled.');
+define('FORLAN_9', 'Thread moved.');
+define('FORLAN_10', 'Move cancelled.');
+define('FORLAN_11', 'Back To Forums');
+define('FORLAN_12', 'Forum Configuration');
+define('FORLAN_13', 'Are you absolutely certain you want to delete this poll?<br />Once deleted, it <b><u>cannot</u></b> be retrieved.');
+define('FORLAN_14', 'Cancel');
+define('FORLAN_15', 'Confirm Delete Forum Post');
+define('FORLAN_16', 'Confirm Delete Poll');
+define('FORLAN_17', 'posted by');
+define('FORLAN_18', 'Are you absolutely certain you want to delete this forum');
+define('FORLAN_19', 'thread and it\'s related posts?');
+define('FORLAN_20', 'the poll will also be deleted');
+define('FORLAN_21', 'Once deleted they');
+define('FORLAN_22', 'post?<br />Once deleted it');
+define('FORLAN_23', 'cannot</u></b> be retrieved');
+define('FORLAN_24', 'Move thread  to forum');
+define('FORLAN_25', 'Move Thread');
+define('FORLAN_26', 'Reply deleted');
+define('FORLAN_27', 'moved');
+define('FORLAN_28', 'Do not rename thread title');
+define('FORLAN_29', 'Add');
+define('FORLAN_30', 'to title');
+define('FORLAN_31', 'Rename to:');
+define('FORLAN_32', 'Rename thread options:');
--- e107_plugins/forum/plugin.php
+++ e107_plugins/forum/plugin.php
@@ -1,73 +1,72 @@
 <?php
-/*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
-|     Copyright (C) 2008-2010 e107 Inc (e107.org)
-|
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $URL$
-|     $Revision$
-|     $Id$
-|     $Author$
-+----------------------------------------------------------------------------+
-*/
+/**
+ * e107 website system
+ * 
+ * Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
+ * Copyright (C) 2008-2010 e107 Inc (e107.org)
+ * 
+ * Released under the terms and conditions of the GNU General Public License
+ * (http://gnu.org).
+ * 
+ * $URL$
+ * $Revision$
+ * $Id$
+ * $Author$
+ */
 
-if (!defined('e107_INIT')) { exit; }
+if (!defined('e107_INIT'))  exit;
 
 include_lan(e_PLUGIN.'forum/languages/'.e_LANGUAGE.'/lan_forum_conf.php');
 
-// Plugin info -------------------------------------------------------------------------------------------------------
-$eplug_name = 'Forum';
-$eplug_version = '1.2';
-$eplug_author = 'e107dev';
-$eplug_url = 'http://e107.org';
-$eplug_email = '';
-$eplug_description = 'This plugin is a fully featured Forum system.';
-$eplug_compatible = 'e107v0.7+';
-$eplug_readme = '';
-$eplug_latest = TRUE; //Show reported threads in admin (use latest.php)
-$eplug_status = TRUE; //Show post count in admin (use status.php)
+# Plugin info
+$eplug_name         = LAN_FORUM_INSTALL_01;
+$eplug_url          = 'http://e107.org';
+$eplug_email        = '';
+$eplug_author       = 'e107dev';
+$eplug_version      = '1.2';
+$eplug_compatible   = 'e107v0.7+';
+$eplug_description  = LAN_FORUM_INSTALL_02;
+$eplug_readme       = '';
 
-// Name of the plugin's folder -------------------------------------------------------------------------------------
-$eplug_folder = "forum";
+# Icon images and caption text
+$eplug_icon         = $eplug_folder.'/images/forums_32.png';
+$eplug_icon_small   = $eplug_folder.'/images/forums_16.png';
+$eplug_caption      = LAN_FORUM_INSTALL_03;
 
-// Name of menu item for plugin ----------------------------------------------------------------------------------
-$eplug_menu_name = "forum";
+# Plugin options
+$eplug_latest       = true;                  # Show reported threads in admin (use latest.php)
+$eplug_status       = true;                  # Show post count in admin (use status.php)
+$eplug_folder       = 'forum';               # Name of the plugin's folder
+$eplug_conffile     = 'forum_admin.php';     # Name of the admin configuration file
+$eplug_menu_name    = 'forum';               # Name of menu item for plugin
 
-// Name of the admin configuration file --------------------------------------------------------------------------
-$eplug_conffile = "forum_admin.php";
+$eplug_link         = true;                  # Create a link in main menu (true=YES, false=NO)
+$eplug_link_url     = e_PLUGIN.'forum/forum.php';
+$eplug_link_name    = LAN_FORUM_INSTALL_01;
 
-// Icon image and caption text ------------------------------------------------------------------------------------
-$eplug_icon = $eplug_folder."/images/forums_32.png";
-$eplug_icon_small = $eplug_folder."/images/forums_16.png";
-$eplug_caption = 'Configure Forum';
+$eplug_done         = LAN_FORUM_INSTALL_04;  # Message for successfull instalation
+$eplug_upgrade_done = sprintf(LAN_FORUM_INSTALL_05, $eplug_version);
 
-// List of preferences -----------------------------------------------------------------------------------------------
-$eplug_prefs = array(
-	"forum_show_topics" => "1",
-	"forum_postfix" => "[more...]",
-	'forum_poll' => '0',
-	'forum_popular' => '10',
-	'forum_track' => '0',
-	'forum_eprefix' => '[forum]',
-	'forum_enclose' => '1',
-	'forum_title' => 'Forums',
-	'forum_postspage' => '10',
-	'forum_hilightsticky' => '1'
- );
 
-// List of table names -----------------------------------------------------------------------------------------------
-$eplug_table_names = array(
-"forum",
-	"forum_t" );
+# List of plugin preferences
+$eplug_prefs        = array(
+	'forum_poll'          => '0',
+	'forum_track'         => '0',
+	'forum_title'         => LAN_FORUM_INSTALL_01,
+	'forum_eprefix'       => LAN_FORUM_INSTALL_06,
+	'forum_postfix'       => LAN_FORUM_INSTALL_07,
+	'forum_enclose'       => '1',
+	'forum_popular'       => '10',
+	'forum_postspage'     => '10',
+	'forum_show_topics'   => '1',
+	'forum_hilightsticky' => '1',
+);
 
-// List of sql requests to create tables -----------------------------------------------------------------------------
-$eplug_tables = array(
+# List of DB tables (names)
+$eplug_table_names  = array('forum', 'forum_t');
+
+# List of SQL requests (create tables)
+$eplug_tables       = array(
 "CREATE TABLE ".MPREFIX."forum (
 	forum_id int(10) unsigned NOT NULL auto_increment,
 	forum_name varchar(250) NOT NULL default '',
@@ -84,8 +83,9 @@ $eplug_tables = array(
 	forum_order int(10) unsigned NOT NULL default '0',
 	forum_postclass tinyint(3) unsigned NOT NULL default '0',
 	PRIMARY KEY  (forum_id)
-	) ENGINE=MyISAM AUTO_INCREMENT=1;",
-	"CREATE TABLE ".MPREFIX."forum_t (
+) ENGINE=MyISAM AUTO_INCREMENT=1;",
+
+"CREATE TABLE ".MPREFIX."forum_t (
 	thread_id int(10) unsigned NOT NULL auto_increment,
 	thread_name varchar(250) NOT NULL default '',
 	thread_thread text NOT NULL,
@@ -104,34 +104,28 @@ $eplug_tables = array(
 	KEY thread_parent (thread_parent),
 	KEY thread_datestamp (thread_datestamp),
 	KEY thread_forum_id (thread_forum_id)
-	) ENGINE=MyISAM AUTO_INCREMENT=1;");
-
-// Create a link in main menu (yes=TRUE, no=FALSE) -------------------------------------------------------------
-$eplug_link = TRUE;
-$eplug_link_name = "Forum";
-$eplug_link_url = e_PLUGIN.'forum/forum.php';
-
-// Text to display after plugin successfully installed ------------------------------------------------------------------
-$eplug_done = 'Your forum is now installed';
-
-$eplug_upgrade_done = 'Forum successfully upgraded, now using version: '.$eplug_version;
-
-$upgrade_alter_tables = array(
-"ALTER TABLE ".MPREFIX."forum ADD forum_postclass TINYINT( 3 ) UNSIGNED DEFAULT '0' NOT NULL ;"
+) ENGINE=MyISAM AUTO_INCREMENT=1;",
 );
 
-if (!function_exists('forum_uninstall')) {
-	function forum_uninstall() {
-		global $sql;
-		$sql -> db_Update("user", "user_forums='0'");
+# Update SQL queries
+$upgrade_alter_tables = array(
+	"ALTER TABLE ".MPREFIX."forum ADD forum_postclass TINYINT(3) UNSIGNED DEFAULT '0' NOT NULL;",
+);
+
+if (!function_exists('forum_install'))
+{
+	function forum_install()
+	{
+		$sql = new db();
+		$sql->db_Update('user', "user_forums='0'");
 	}
 }
 
-if (!function_exists('forum_install')) {
-	function forum_install() {
-		global $sql;
-		$sql -> db_Update("user", "user_forums='0'");
+if (!function_exists('forum_uninstall'))
+{
+	function forum_uninstall()
+	{
+		$sql = new db();
+		$sql->db_Update('user', "user_forums='0'");
 	}
 }
-
-?>
