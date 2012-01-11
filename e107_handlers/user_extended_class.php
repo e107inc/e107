d41c38303329ce97c079355950cb780551049343
secretr <m.yovchev@gmail.com>
2012-01-11 14:37:37 +0000

    PHP notice

--- e107_handlers/user_extended_class.php
+++ e107_handlers/user_extended_class.php
@@ -2,7 +2,7 @@
 /*
 * e107 website system
 *
-* Copyright (C) 2008-2010 e107 Inc (e107.org)
+* Copyright (C) 2008-2012 e107 Inc (e107.org)
 * Released under the terms and conditions of the
 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
 *
@@ -566,6 +566,7 @@ class e107_user_extended
 		  break;
 
 		case EUF_RADIO : //radio
+		  $ret = '';
 		  foreach($choices as $choice)
 		  {
 			$choice = trim($choice);
