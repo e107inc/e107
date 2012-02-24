057f7a0b0a787077c56cbc6f989578e5ca556dfa
secretr <m.yovchev@gmail.com>
2012-02-24 13:22:29 +0000

    user and pages search good URLs

--- e107_handlers/search/search_pages.php
+++ e107_handlers/search/search_pages.php
@@ -2,16 +2,12 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
- *
- *
- * $Source: /cvs_backup/e107_0.8/e107_handlers/search/search_pages.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ * $URL$
+ * $Id$
  */
 
 if (!defined('e107_INIT')) { exit; }
@@ -23,7 +19,7 @@ if (isset($_GET['time']) && is_numeric($_GET['time'])) {
 }
 
 // basic
-$return_fields = 'page_id, page_title, page_text, page_datestamp';
+$return_fields = 'page_id, page_title, page_sef, page_text, page_datestamp';
 $search_fields = array('page_title', 'page_text');
 $weights = array('1.2', '0.6');
 $no_results = LAN_198;
@@ -38,7 +34,7 @@ $results = $ps['results'];
 
 function search_pages($row) {
 	global $con;
-	$res['link'] = "page.php?".$row['page_id'];
+	$res['link'] = e107::getUrl()->create('page/view', $row, array('allow' => 'page_sef,page_title,page_id'));//"page.php?".$row['page_id'];
 	$res['pre_title'] = "";
 	$res['title'] = $row['page_title'];
 	$res['summary'] = $row['page_text'];
