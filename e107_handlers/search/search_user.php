b2a73fb37646258f33669391b060367aed9ef8a5
secretr <m.yovchev@gmail.com>
2012-02-24 13:22:29 +0000

    user and pages search good URLs

--- e107_handlers/search/search_user.php
+++ e107_handlers/search/search_user.php
@@ -2,16 +2,12 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
- *
- *
- * $Source: /cvs_backup/e107_0.8/e107_handlers/search/search_user.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ * $URL$
+ * $Id$
  */
 
 if (!defined('e107_INIT')) { exit; }
@@ -36,7 +32,7 @@ $results = $ps['results'];
 
 function search_user($row) {
 	global $con;
-	$res['link'] = "user.php?id.".$row['user_id'];
+	$res['link'] = e107::getUrl()->create('user/profile/view', array('id' => $row['user_id'], 'name' => $row['user_name']));//"user.php?id.".$row['user_id'];
 	$res['pre_title'] = $row['user_id']." | ";
 	$res['title'] = $row['user_name'];
 	$res['summary'] = $row['user_signature'] ?  LAN_SEARCH_72.": ".$row['user_signature'] : LAN_SEARCH_73;
