9e17f89c05d1304ef4ac51e819adc7d394bd2550
e107coders <e107coders@localhost>
2012-03-30 21:00:24 +0000

    new version: 1.0.1 - auto
    svn path=/trunk/e107_0.7/; revision=12615

--- e107_admin/ver.php
+++ e107_admin/ver.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Copyright (c) 2011 e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* Copyright (c) 2012 e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
 * $URL$
 * $Id$
 *
@@ -9,5 +9,5 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$e107info['e107_version'] = "1.0.1 svn";
+$e107info['e107_version'] = "1.0.1";
 ?>
