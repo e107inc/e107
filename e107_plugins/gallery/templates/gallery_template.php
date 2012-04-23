c41b3e4b3118eabc14a78064416d995b06b4b277
CaMer0n <cameron@e107coders.org>
2012-04-23 09:29:12 +0000

    Added stylesheet to gallery

--- e107_plugins/gallery/templates/gallery_template.php
+++ e107_plugins/gallery/templates/gallery_template.php
@@ -1,40 +1,45 @@
 <?php
-
+/*
+* Copyright (c) 2012 e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id: e_shortcode.php 12438 2011-12-05 15:12:56Z secretr $
+*
+* Gallery Template 
+*/
 
   
 $GALLERY_TEMPLATE['LIST_START'] = 
-	"<div class='gallery-list-start' style='clear:both'>";
+	"<div class='gallery-list-start'>";
 
 		
 $GALLERY_TEMPLATE['LIST_ITEM'] = 
-	"<div style='width:190px;height:180px;float:left;margin:3px;border:1px solid black;background-color:black'>
+	"<div class='gallery-list-item'>
 	<div>{GALLERY_THUMB}</div>
-	<div style='display:block;text-align:center;color:white;'>{GALLERY_CAPTION}</div>
+	<div class='gallery-list-caption'>{GALLERY_CAPTION}</div>
 	</div>
 	";
 
 $GALLERY_TEMPLATE['LIST_END'] = 
 	"</div>
-	<div class='gallery-list-end' style='text-align:center;clear:both'>
+	<div class='gallery-list-end' >
 	<a href='".e_SELF."'>Back to Categories</a>
 	</div>
 	";
 	
 	
 $GALLERY_TEMPLATE['CAT_START'] = 
-	"<div class='gallery-cat-start' style='clear:both'>";
+	"<div class='gallery-cat-start'>";
 
 		
 $GALLERY_TEMPLATE['CAT_ITEM'] = 
-	"<div style='width:190px;height:180px;float:left;margin:3px;border:1px solid black;background-color:black'>
-	<div>{GALLERY_CAT_THUMB}</div>
-	<div style='text-align:center'><h3>{GALLERY_CAT_TITLE}</h3></div>
+	"<div class='gallery-cat-item'>
+	<div class='gallery-cat-thumb'>{GALLERY_CAT_THUMB}</div>
+	<div class='gallery-cat-title'><h3>{GALLERY_CAT_TITLE}</h3></div>
 	</div>
 	";
 
 $GALLERY_TEMPLATE['CAT_END'] = 
 	"</div>
-	<div class='gallery-cat-end' style='text-align:center;clear:both'>
+	<div class='gallery-cat-end'>
 	</div>
 	";
 		
