ade2ee84c5ec2ac32039668664bdfd307725a9dd
CaMer0n <cameron@e107coders.org>
2012-04-25 07:21:07 +0000

    Updated to fixed version of carousel.js

--- e107_plugins/gallery/e_header.php
+++ e107_plugins/gallery/e_header.php
@@ -13,11 +13,15 @@ e107::getJs()->pluginCSS('gallery', 'jslib/lightbox/css/lightbox.css');
 
 e107::getJS()->headerFile("https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js",1);
 e107::getJS()->headerFile("https://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js?load=effects",1);
-e107::getJs()->headerPlugin('gallery', 'jslib/carousel-min.js');
+e107::getJs()->headerPlugin('gallery', 'jslib/carousel.js');
 e107::getJs()->pluginCSS('gallery', 'gallery_style.css');
 
 e107::getJs()->footerInline("		
-	new Carousel('carousel-wrapper', $$('#carousel-content .slide'), $$('a.carousel-control', 'a.carousel-jumper'));
+	new Carousel('carousel-wrapper', $$('#carousel-content .slide'), $$('a.carousel-control', 'a.carousel-jumper' ),
+	{
+		auto: true,
+		circular: true
+	});
 ");
 
 
