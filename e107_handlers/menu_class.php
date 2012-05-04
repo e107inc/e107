a39b3f4787ecfd4e2f0fd8a1cb48c01cc6f29a7f
secretr <m.yovchev@gmail.com>
2012-05-04 15:01:41 +0000

    menu class header information

--- e107_handlers/menu_class.php
+++ e107_handlers/menu_class.php
@@ -2,16 +2,14 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * e107 Menu Class
  *
- * $Source: /cvs_backup/e107_0.8/e107_handlers/menu_class.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ * $URL$
+ * $Id$
 */
 
 if(!defined('e107_INIT'))
