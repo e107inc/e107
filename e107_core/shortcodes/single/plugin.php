0276b7fd43db5c2af2036a89fea2e66c1de3fa21
secretr <m.yovchev@gmail.com>
2012-05-04 15:00:33 +0000

    minor menu render related problems

--- e107_core/shortcodes/single/plugin.php
+++ e107_core/shortcodes/single/plugin.php
@@ -5,17 +5,21 @@ function plugin_shortcode($parm = '')
 {
 	$tp = e107::getParser();
 
-	@list($menu,$parms) = explode('|',$parm.'|');
+	@list($menu,$parms) = explode('|',$parm.'|', 2);
 
-	$path = $tp -> toDB(dirname($menu));
-	$name = $tp -> toDB(basename($menu));
+	$path = $tp->toDB(dirname($menu));
+	$name = $tp->toDB(basename($menu));
 
 	if($path == '.')
 	{
 	  $path = $menu;
 	}
 	/**
+	 * @todo check if plugin is installed when installation required
+	 */
+	
+	/**
 	 *	fixed todo: $mode is provided by the menu itself, return is always true, added optional menu parameters
 	 */
-    return e107::getMenu()->renderMenu($path,$name,$parms,true);
+    return e107::getMenu()->renderMenu($path,$name, trim($parms, '|'),true);
 }
