36c29d1c2cce60a80d198796ef2a95384d783735
secretr <m.yovchev@gmail.com>
2012-05-29 12:40:26 +0000

    file header standards

--- e107_handlers/event_class.php
+++ e107_handlers/event_class.php
@@ -1,21 +1,15 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     Copyright (C) 2008-2009 e107 Inc (e107.org)
-|     http://e107.org
-|
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_handlers/event_class.php,v $
-|     $Revision$
-|     $Date$
-|     $Author$
-+----------------------------------------------------------------------------+
-*/
+ * e107 website system
+ *
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * $URL$
+ * $Id$
+ */
+
 
 if (!defined('e107_INIT')) { exit; }
 
