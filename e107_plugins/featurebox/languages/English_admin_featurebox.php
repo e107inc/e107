be9ac3694c4b66ea03630042343a81727465d441
CaMer0n <cameron@e107coders.org>
2012-08-22 07:13:27 +0000

    More language-file / xml fixes.

--- e107_plugins/featurebox/languages/English_admin_featurebox.php
+++ e107_plugins/featurebox/languages/English_admin_featurebox.php
@@ -9,35 +9,35 @@
 
 define("FBLAN_01", "Feature Box");
 define("FBLAN_02", "Displays an animated area on the top of your page with news-items and other content you would like to feature.");
-define("FBLAN_03", "Configure feature box");
+define("FBLAN_03", "Configure feature box"); //FIXME LAN  - use generic. 
 // define("FBLAN_04", "Feature Box plugin has been successfully installed. To add messages and configure, return to the main admin page and click on the feature box icon in the plugin section.");
 define("FBLAN_05", "No feature box messages defined yet");
-define("FBLAN_06", "Existing Feature Box Messages");
-define("FBLAN_07", "Title / Caption");
+define("FBLAN_06", "Existing Feature Box Messages"); //FIXME LAN  - use generic. 
+define("FBLAN_07", "Title / Caption"); //FIXME LAN  - use generic. 
 define("FBLAN_08", "Message text");
 define("FBLAN_09", "Visibility of message");
-define("FBLAN_10", "Create Feature Box Message");
-define("FBLAN_11", "Update Feature Box Message");
+define("FBLAN_10", "Create Feature Box Message"); //FIXME LAN  - use generic. 
+define("FBLAN_11", "Update Feature Box Message"); //FIXME LAN  - use generic. 
 define("FBLAN_12", "Mode");
 define("FBLAN_13", "Randomly revolve messages");
 define("FBLAN_14", "Show this message only");
-define("FBLAN_15", "Message added to database.");
-define("FBLAN_16", "Message updated in database.");
+define("FBLAN_15", "Message added to database."); //FIXME LAN  - use generic. 
+define("FBLAN_16", "Message updated in database."); //FIXME LAN  - use generic. 
 define("FBLAN_17", "Field(s) left blank");
-define("FBLAN_18", "Feature Box Message deleted");
-define("FBLAN_19", "Options");
-define("FBLAN_20", "Edit");
-define("FBLAN_21", "Delete");
+define("FBLAN_18", "Feature Box Message deleted"); //FIXME LAN  - use generic. 
+define("FBLAN_19", "Options"); //FIXME LAN  - use generic. 
+define("FBLAN_20", "Edit"); //FIXME LAN  - use generic. 
+define("FBLAN_21", "Delete"); //FIXME LAN  - use generic. 
 define("FBLAN_22", "Render type");
 define("FBLAN_23", "In theme box");
 define("FBLAN_24", "Plain");
-define("FBLAN_25", "Template");
+define("FBLAN_25", "Template"); //FIXME LAN  - use generic. 
 define("FBLAN_26", "you can use a different template for each message, add templates to e107_plugins/featurebox/templates/ folder");
 
 // Admin log messages
-define('LAN_AL_FBLAN_01', 'Featurebox created');
-define('LAN_AL_FBLAN_02', 'Featurebox updated');
-define('LAN_AL_FBLAN_03', 'Featurebox deleted');
+define('LAN_AL_FBLAN_01', 'Featurebox created'); //FIXME LAN  - use generic. 
+define('LAN_AL_FBLAN_02', 'Featurebox updated'); //FIXME LAN  - use generic. 
+define('LAN_AL_FBLAN_03', 'Featurebox deleted'); //FIXME LAN  - use generic. 
 
 // Install/uninstall messages
 define('FBLAN_INSTALL_01', 'Adding Default category table data.');
