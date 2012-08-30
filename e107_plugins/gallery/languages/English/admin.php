7e73bd78ee52e07766e22b3604768da01f687781
secretr <m.yovchev@gmail.com>
2012-08-30 13:06:51 +0000

    Gallery language files

---
+++ e107_plugins/gallery/languages/English/admin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+* Copyright (c) e107 Inc 2008-2012 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id$
+*
+* Gallery back-end laguage defines
+*/
+//define('GALLERY_ALAN_1', '');
---
+++ e107_plugins/gallery/languages/English/front.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* Copyright (c) e107 Inc 2008-2012 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id$
+*
+* Gallery front-end laguage defines
+*/
+define('GALLERY_LAN_TITLE', 'Gallery');
+//define('GALLERY_LAN_1', '');
