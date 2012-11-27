0869b942ad9cd1f2ba858d663fe21e05469556d1
Cameron <cameron@e107coders.org>
2012-11-27 14:00:52 -0800

    Added language file.

---
+++ e107_themes/bootstrap/languages/English.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Bootstrap Theme Language file for e107 v2+
+ *
+*/
+define("EMESSLAN_TITLE_INFO", "System Information");
+define("EMESSLAN_TITLE_ERROR", "Error");
+define("EMESSLAN_TITLE_SUCCESS", "Success");
+define("EMESSLAN_TITLE_WARNING", "Warning");
+define("EMESSLAN_TITLE_DEBUG", "System Debug");
+
+define("LAN_THEME_1", "Comments are turned off for this item");
+define("LAN_THEME_2", "Read/Post Comment: ");
+define("LAN_THEME_3", "Read the rest...");
+define("LAN_THEME_4", "Trackbacks: ");
+define("LAN_THEME_5", "Posted by ");
+define("LAN_THEME_6", "on ");
+?>
