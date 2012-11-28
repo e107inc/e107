644e758e0a1ae21e30f09ec2772edb514a20565a
Cameron <cameron@e107coders.org>
2012-11-28 14:01:08 -0800

    GUI fixes on login-menu config and menu-manager help.

--- e107_languages/English/admin/help/menus.php
+++ e107_languages/English/admin/help/menus.php
@@ -19,21 +19,30 @@ if (!getperms("2")) {
 	header("location:".e_BASE."index.php");
 	 exit;
 }
-global $sql;
-if(isset($_POST['reset'])){
-		for($mc=1;$mc<=5;$mc++){
+
+$sql = e107::getDb();
+
+if(isset($_POST['reset']))
+{
+		for($mc=1;$mc<=5;$mc++)
+		{
 			$sql -> db_Select("menus","*", "menu_location='".$mc."' ORDER BY menu_order");
 			$count = 1;
 			$sql2 = new db;
-			while(list($menu_id, $menu_name, $menu_location, $menu_order) = $sql-> db_Fetch()){
+			while(list($menu_id, $menu_name, $menu_location, $menu_order) = $sql-> db_Fetch())
+			{
 				$sql2 -> db_Update("menus", "menu_order='$count' WHERE menu_id='$menu_id' ");
 				$count++;
 			}
 			$text = "<b>Menus reset in database</b><br /><br />";
 		}
-}else{
+}
+else
+{
 	unset($text);
 }
+
+$frm = e107::getForm();
 
 $text .= "
 You can arrange where and in which order your menu items are from here. 
@@ -43,7 +52,8 @@ Use the dropdown menu to move the menus up and down until you are satisfied with
 If you find the menus are not updating properly click on the refresh button.
 <br />
 <form method='post' id='menurefresh' action='".$_SERVER['PHP_SELF']."'>
-<div><input type='submit' class='button' name='reset' value='Refresh' /></div>
+<div>
+".$frm->admin_button('reset','Refresh','cancel')."</div>
 </form>
 <br />
 <div class='indent'><span class='required'>*</span> indicates menu visibility has been modified</div>
