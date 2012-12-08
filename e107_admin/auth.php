ee16f4ab67eddf882df3f14933b34d53be8782eb
SecretR <secretr@e107.org>
2012-12-08 21:09:58 +0200

    #14 User administration rewrite (set user class, quick add user not functional, work in progress); retrieve() DB method update (work in progress);

--- e107_admin/auth.php
+++ e107_admin/auth.php
@@ -45,7 +45,7 @@ if (ADMIN)
 		if(e107::getUser()->getSessionDataAs())
 		{ // TODO - lan
 			$asuser = e107::getSystemUser(e107::getUser()->getSessionDataAs(), false);
-			e107::getMessage()->addInfo('Successfully logged in as '.($asuser && $asuser->getValue('name') ? $asuser->getValue('name') : 'unknown'). ' <a href="'.e_ADMIN_ABS.'users.php?logoutas">[logout]</a>');
+			e107::getMessage()->addInfo('Successfully logged in as '.($asuser->getId()  ? $asuser->getName().' ('.$asuser->getValue('email').')' : 'unknown'). ' <a href="'.e_ADMIN_ABS.'users.php?mode=main&amp;action=logoutas">[logout]</a>');
 		}
 		// NEW, legacy 3rd party code fix, header called inside the footer o.O
 		if(deftrue('e_ADMIN_UI'))
