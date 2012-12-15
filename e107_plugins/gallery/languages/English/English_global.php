4de0dd7403a5b5aa09b5387c3bfc7a9ca9b171c4
Cameron <cameron@e107coders.org>
2012-12-15 13:40:09 -0800

    Language file cleanup

---
+++ e107_plugins/gallery/languages/English/English_global.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Copyright (c) e107 Inc 2008-2012 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id$
+*
+* Gallery Global Language
+*/
+define("LAN_PLUGIN_GALLERY_TITLE",	"Gallery");
+define("LAN_PLUGIN_GALLERY_DIZ", 	"A simple image gallery");
+
