9005235c5f83822fac75a790498cc15c543e57a5
Tijn Kuyper <moc@mochosting.nl>
2012-12-15 16:12:44 +0100

    Minor typo

--- e107_plugins/featurebox/languages/English_front_featurebox.php
+++ e107_plugins/featurebox/languages/English_front_featurebox.php
@@ -3,7 +3,7 @@
 * Copyright (c) e107 Inc 2009 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
 * $Id$
 *
-* Featurebox front-end laguage defines
+* Featurebox front-end language defines
 */
 
 define("FBLAN_01", "Feature Box");
