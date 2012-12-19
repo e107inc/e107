bc4de2290a24914b148eeb66399562acea314efd
nlstart <nlstart@webstartinternet.com>
2012-12-19 21:15:16 +0100

    English_global files for list_new, faqs and import plugins

---
+++ e107_plugins/faqs/languages/English_global.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Copyright (c) e107 Inc 2009 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id$
+*
+* 'FAQ plugin' global language defines
+*/
+define("LAN_PLUGIN_FAQS_NAME", "FAQ plugin");
+define("LAN_PLUGIN_FAQ_DESCRIPTION", "A simple plugin to add Frequently Asked Questions to your website.");
+?>
