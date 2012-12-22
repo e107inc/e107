ab3fda52583cf71e4e1088549552b5fa42e8762c
SteveD <steved@e107.org>
2012-12-22 15:39:54 +0000

    All the new files that should have been added on the last commit (pdf plugin)

---
+++ e107_plugins/pdf/languages/English_log.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * PDF plugin - messages for admin log titles
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/calendar_menu/languages/English_log.php,v $
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+
+/**
+ *	e107 PDF plugin
+ *
+ *	Language file - admin log titles
+ *
+ *	@package	e107_plugins
+ *	@subpackage	pdf
+ *	@version 	$Id$;
+ */
+
+define('LAN_AL_PDF_01', 'PDF Plugin - prefs updated');
+define('LAN_AL_PDF_02', 'PDF plugin - default prefs set');
