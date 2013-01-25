de26f54780c789d42c404fb9152136b36efa520c
STR82U <str82u@str82u.co>
2013-01-24 23:50:15 -0700

    Update e107_plugins/news/templates/news_menu_template.php
    Commented out lines 22, 30 and 40 to suppress <br /> in lists.

--- e107_plugins/news/templates/news_menu_template.php
+++ e107_plugins/news/templates/news_menu_template.php
@@ -19,7 +19,7 @@ $NEWS_MENU_TEMPLATE['category']['end']         = '</ul>';
 $NEWS_MENU_TEMPLATE['category']['item']        = '
 	<li><a class="e-menu-link newscats{active}" href="{NEWS_CATEGORY_URL}">{NEWS_CATEGORY_TITLE} {NEWS_CATEGORY_NEWS_COUNT}</a></li>
 ';
-$NEWS_MENU_TEMPLATE['category']['separator']   = '<br />';
+//$NEWS_MENU_TEMPLATE['category']['separator']   = '<br />';
 
 // months menu
 $NEWS_MENU_TEMPLATE['months']['start']       = '<ul class="nav nav-list news-menu-months">';
@@ -27,7 +27,7 @@ $NEWS_MENU_TEMPLATE['months']['end']         = '</ul>';
 $NEWS_MENU_TEMPLATE['months']['item']        = '
 	<li><a class="e-menu-link newsmonths{active}" href="{url}">{month} ({count})</a></li>
 ';
-$NEWS_MENU_TEMPLATE['months']['separator']   = '<br />';
+//$NEWS_MENU_TEMPLATE['months']['separator']   = '<br />';
 
 // latest menu
 $NEWS_MENU_TEMPLATE['latest']['start']       = '<ul class="nav nav-list news-menu-latest">';
@@ -37,4 +37,4 @@ $NEWS_MENU_TEMPLATE['latest']['end']         = '</ul>';
 $NEWS_MENU_TEMPLATE['latest']['item']        = '
 	<li><a class="e-menu-link newsmonths" href="{NEWSURL}">{NEWSTITLE} ({NEWSCOMMENTCOUNT})</a></li>
 ';
-$NEWS_MENU_TEMPLATE['latest']['separator']   = '<br />'; // Shouldn't be needed. 
+//$NEWS_MENU_TEMPLATE['latest']['separator']   = '<br />'; // Shouldn't be needed. 
