a5f9c97d144016e0fbbe02ed08485056a590a449
Tijn Kuyper <moc@mochosting.nl>
2013-02-07 12:34:37 +0100

    Issue #90 - Newsfeed plugin (also #6 and GUI styling)

--- e107_plugins/newsfeed/languages/English_admin_newsfeed.php
+++ e107_plugins/newsfeed/languages/English_admin_newsfeed.php
@@ -16,44 +16,45 @@
 
 //define("NFLAN_01", "Newsfeeds");
 //define("NFLAN_02", "This plugin will retrieve rss feeds from other websites and display them according to your preferences");
-define("NFLAN_03", "Configure newsfeeds");
-define("NFLAN_04", "Newsfeeds plugin has been successfully installed. To add newsfeeds and configure, return to the main admin page and click on the newsfeeds icon in the plugin section.");
-define("NFLAN_05", "Edit");
-define("NFLAN_06", "Delete");
+define("NFLAN_03", "Configure newsfeeds"); // FIX USE GENERIC
+define("NFLAN_04", "Newsfeeds plugin has been successfully installed. To add newsfeeds and configure, return to the main admin page and click on the newsfeeds icon in the plugin section."); // FIX USE GENERIC
+//define("NFLAN_05", "Edit");
+//define("NFLAN_06", "Delete");
 define("NFLAN_07", "Existing Newsfeeds");
 define("NFLAN_08", "Newsfeeds front page");
 define("NFLAN_09", "Create newsfeed");
-define("NFLAN_10", "URL to rss feed");
+//define("NFLAN_10", "URL to rss feed");
+define("NFLAN_10", "URL to the RSS feed.");
 define("NFLAN_11", "Path to image");
 define("NFLAN_12", "Activation");
 define("NFLAN_13", "Nowhere (inactive)");
 define("NFLAN_14", "In menu only");
-define("NFLAN_15", "Create Newsfeed");
-define("NFLAN_16", "Update Newsfeed");
-define("NFLAN_17", "enter 'default' in box to use the image defined in the feed, to use your own image enter full path, leave blank for no image.");
+//define("NFLAN_15", "Create Newsfeed");
+//define("NFLAN_16", "Update Newsfeed");
+define("NFLAN_17", "Enter 'default' to use the image defined in the feed. To use your own image, enter full path. Leave blank for no image.");
 define("NFLAN_18", "Update interval in seconds");
 define("NFLAN_19", "e.g. 3600: newsfeed will update every hour");
 define("NFLAN_20", "On newsfeed main page only");
 define("NFLAN_21", "In both menu and newsfeed page");
-define("NFLAN_22", "choose where you want the newsfeed displayed");
-define("NFLAN_23", "Newsfeed added to database.");
-define("NFLAN_24", "Required field(s) left blank.");
-define("NFLAN_25", "Newsfeed updated in database.");
+define("NFLAN_22", "Choose where you want the newsfeed displayed.");
+//define("NFLAN_23", "Newsfeed added to database.");
+//define("NFLAN_24", "Required field(s) left blank.");
+//define("NFLAN_25", "Newsfeed updated in database.");
 define("NFLAN_26", "Update Interval");
-define("NFLAN_27", "Options");
-define("NFLAN_28", "URL");
+//define("NFLAN_27", "Options");
+//define("NFLAN_28", "URL");
 //define("NFLAN_29", "Available newsfeeds");
-define("NFLAN_30", "Feed name");
+//define("NFLAN_30", "Feed name");
 //define("NFLAN_31", "Back to newsfeed list");
-define("NFLAN_32", "No feed with that identification number can be found.");
+//define("NFLAN_32", "No feed with that identification number can be found.");
 //define("NFLAN_33", "Date published: ");
 //define("NFLAN_34", "not known");
-define("NFLAN_35", "posted by ");
-define("NFLAN_36", "Description");
-define("NFLAN_37", "short description of feed, enter 'default' to use the description defined in the feed");
+//define("NFLAN_35", "posted by ");
+//define("NFLAN_36", "Description");
+define("NFLAN_37", "Short description of feed. Enter 'default' to use the description defined in the feed");
 //define("NFLAN_38", "Headlines");
 //define("NFLAN_39", "Details");
-define("NFLAN_40", "Newsfeed deleted");
+//define("NFLAN_40", "Newsfeed deleted");
 define("NFLAN_41", "No newsfeeds defined yet");
 
 define("NFLAN_42", "<b>&raquo;</b> <u>Feed Name:</u>
@@ -83,7 +84,7 @@ define("NFLAN_47", "0 or blank to show all");
 
 //define("NFLAN_48", "Unable to save raw data in database.");
 define("NFLAN_49", "Unable to unserialize rss data - uses non-standard syntax");
-define('NFLAN_50', 'Write to database failed: ');
+//define('NFLAN_50', 'Write to database failed: ');
 
 // Admin log messages
 //===================
