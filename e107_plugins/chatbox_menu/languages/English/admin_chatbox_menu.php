b5040470776e2d8d310d8385a2dd0ad0a0beedb8
Tijn Kuyper <moc@mochosting.nl>
2013-02-07 12:41:56 +0100

    Chatbox admin LAN clean-up

--- e107_plugins/chatbox_menu/languages/English/admin_chatbox_menu.php
+++ e107_plugins/chatbox_menu/languages/English/admin_chatbox_menu.php
@@ -1,33 +1,36 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system - Language File.
-|
-|     $Source: /cvs_backup/e107_0.8/e107_plugins/chatbox_menu/languages/English/admin_chatbox_menu.php,v $
-|     $Revision$
-|     $Date$
-|     $Author$
-|     Encoding:
-+----------------------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Plugin - Chatbox
+ *
+ * $URL$
+ * $Id$
+ *
 */
+
 //define("CHBLAN_1", "Chatbox settings updated.");
-define("CHBLAN_2", "Moderated.");
-define("CHBLAN_3", "No chatbox posts yet.");
-define("CHBLAN_4", "Member");
-define("CHBLAN_5", "Guest");
-define("CHBLAN_6", "unblock");
-define("CHBLAN_7", "block");
-define("CHBLAN_8", "delete");
-define("CHBLAN_9", "Moderate Chatbox");
-define("CHBLAN_10", "Moderate posts");
+//define("CHBLAN_2", "Moderated.");
+//define("CHBLAN_3", "No chatbox posts yet.");
+//define("CHBLAN_4", "Member");
+//define("CHBLAN_5", "Guest");
+//define("CHBLAN_6", "unblock");
+//define("CHBLAN_7", "block");
+//define("CHBLAN_8", "delete");
+//define("CHBLAN_9", "Moderate Chatbox");
+//define("CHBLAN_10", "Moderate posts");
 define("CHBLAN_11", "Chatbox posts to display");
 define("CHBLAN_12", "Amount of posts displayed in chatbox");
-define("CHBLAN_13", "Replace links");
-define("CHBLAN_14", "if ticked, posted links will be replaced by text entered in box below");
-define("CHBLAN_15", "Replace string if activated");
-define("CHBLAN_16", "links will be replaced by this string");
-define("CHBLAN_17", "Wordwrap count");
-define("CHBLAN_18", "words longer than the number you set here will be wrapped");
+//define("CHBLAN_13", "Replace links");
+//define("CHBLAN_14", "if ticked, posted links will be replaced by text entered in box below");
+//define("CHBLAN_15", "Replace string if activated");
+//define("CHBLAN_16", "links will be replaced by this string");
+//define("CHBLAN_17", "Wordwrap count");
+//define("CHBLAN_18", "words longer than the number you set here will be wrapped");
 //define("CHBLAN_19", "Update Chatbox Settings");
 define("CHBLAN_20", "Chatbox Settings");
 //define("CHBLAN_21", "Prune");
@@ -52,7 +55,7 @@ define("CHBLAN_35", "Recalculate");
 define("CHBLAN_36", "Chatbox Display options");
 define("CHBLAN_37", "Normal chatbox");
 define("CHBLAN_38", "Use javascript code to update posts dynamically (AJAX)");
-define('CHBLAN_39', 'Nothing changed - not updated');
+//define('CHBLAN_39', 'Nothing changed - not updated');
 // define('CHBLAN_40', 'Chatbox');
 // define('CHBLAN_41', 'Chatbox Menu');
 
