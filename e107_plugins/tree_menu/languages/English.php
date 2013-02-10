2d4ad644f743fecf9a852f772a967672f3231367
Tijn Kuyper <moc@mochosting.nl>
2013-02-10 20:39:48 +0100

    Tree_menu basic code and GUI clean-up. Also LAN (#90, #6)

--- e107_plugins/tree_menu/languages/English.php
+++ e107_plugins/tree_menu/languages/English.php
@@ -2,23 +2,20 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
+ * $URL$
+ * $Id$
  *
- *
- * $Source: /cvs_backup/e107_0.8/e107_plugins/tree_menu/languages/English.php,v $
- * $Revision$
- * $Date$
- * $Author$
- */
+*/
 
-define("TREE_L1", "Configure Tree Menu");
-define("TREE_L2", "Update Tree Menu Settings");
-define("TREE_L3", "Tree Menu configuration saved.");
-define("TREE_L4", "On");
-define("TREE_L5", "Off");
+define("TREE_L1", "Tree Menu");
+//define("TREE_L2", "Update Tree Menu Settings");
+//define("TREE_L3", "Tree Menu configuration saved.");
+//define("TREE_L4", "On");
+//define("TREE_L5", "Off");
 define("TREE_L6", "CSS class to use for non-openable links");
 define("TREE_L7", "CSS class to use for openable links");
 define("TREE_L8", "CSS class to use for opened links");
