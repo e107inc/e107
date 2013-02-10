ab29ef1734bc81873118c6012633336a04f691e0
Tijn Kuyper <moc@mochosting.nl>
2013-02-10 20:39:48 +0100

    Tree_menu basic code and GUI clean-up. Also LAN (#90, #6)

--- e107_plugins/tree_menu/config.php
+++ e107_plugins/tree_menu/config.php
@@ -2,29 +2,16 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * Plugin Administration - Tree menu
  *
- * $Source: /cvs_backup/e107_0.8/e107_plugins/tree_menu/config.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ * $URL$
+ * $Id$
  *
 */
-
-/**
- *	e107 Tree menu plugin
- *
- *	Provides alternative menu style
- *
- *	@package	e107_plugins
- *	@subpackage	online
- *	@version 	$Id$;
- *
- */
 
 $eplug_admin = TRUE;
 require_once('../../class2.php');
@@ -36,6 +23,8 @@ if (!getperms('4'))
 	exit ;
 }
 require_once(e_ADMIN.'auth.php');
+$mes = e107::getMessage();
+$frm = e107::getForm();
 
 $menu_pref = e107::getConfig('menu')->getPref('');
 
@@ -59,43 +48,38 @@ if (isset($_POST['update_menu']))
 		}
 		$menuPref->save(false, true, false);
 	}
-	$ns->tablerender("", "<div style='text-align:center'><b>".TREE_L3."</b></div>");
+	$ns->tablerender($caption, $mes->render() . $text);
 }
 	
 $text = "
-	<form method='post' action='".e_SELF."?".e_QUERY."' name='menu_conf_form'>
-	<table style='".ADMIN_WIDTH."' class='fborder' >
-	 
-	<tr>
-	<td style='width: 50%;' class='forumheader3'>".TREE_L6."</td>
-	<td style='width: 50%;' class='forumheader3'><input class='tbox' type='text' name='tm_class1' size='40' value='".$menu_pref['tm_class1']."' maxlength='20' /></td>
-	</tr>
+<form method='post' action='".e_SELF."?".e_QUERY."' name='menu_conf_form'>
+<table class='table adminform'>
+	<colgroup span='2'>
+		<col class='col-label' />
+		<col class='col-control' />
+	</colgroup>
+<tr>
+	<td>".TREE_L6."</td>
+	<td><input class='tbox' type='text' name='tm_class1' size='40' value='".$menu_pref['tm_class1']."' maxlength='20' /></td>
+</tr>
+<tr>
+	<td>".TREE_L7."</td>
+	<td><input class='tbox' type='text' name='tm_class2' size='40' value='".$menu_pref['tm_class2']."' maxlength='20' /></td>
+</tr>
+<tr>
+	<td>".TREE_L8."</td>
+	<td><input class='tbox' type='text' name='tm_class3' size='40' value='".$menu_pref['tm_class3']."' maxlength='20' /></td>
+</tr>
+<tr>
+	<td>".TREE_L9."</td>
+	<td>".$frm->radio_switch('tm_spacer', $menu_pref['tm_spacer'], LAN_YES, LAN_NO)."</td>
+</tr>
+</table>
+<div class='buttons-bar center'>
+	".$frm->admin_button('update_menu', LAN_UPDATE, 'update')."
+</div>
+</form>";
 
-	<tr>
-	<td style='width: 50%;' class='forumheader3'>".TREE_L7."</td>
-	<td style='width: 50%;' class='forumheader3'><input class='tbox' type='text' name='tm_class2' size='40' value='".$menu_pref['tm_class2']."' maxlength='20' /></td>
-	</tr>
-
-	<tr>
-	<td style='width: 50%;' class='forumheader3'>".TREE_L8."</td>
-	<td style='width: 50%;' class='forumheader3'><input class='tbox' type='text' name='tm_class3' size='40' value='".$menu_pref['tm_class3']."' maxlength='20' /></td>
-	</tr>
-
-	<tr>
-	<td style='width:50%' class='forumheader3'>".TREE_L9."</td>
-	<td style='width:50%; text-align:right' class='forumheader3'>
-	<input type='radio' name='tm_spacer' value='1'".($menu_pref['tm_spacer'] ? " checked='checked'" : "")." /> ".TREE_L4."&nbsp;&nbsp;
-	<input type='radio' name='tm_spacer' value='0'".(!$menu_pref['tm_spacer'] ? " checked='checked'" : "")." /> ".TREE_L5."
-	</td>
-	</tr>
-
-	
-	<tr>
-	<td colspan='2' class='forumheader' style='text-align:center'><input class='button' type='submit' name='update_menu' value='".TREE_L2."' /></td>
-	</tr>
-	</table>
-	</form>
-	</div>";
 $ns->tablerender(TREE_L1, $text);
 	
 require_once(e_ADMIN."footer.php");
