f6b646131ee03ea9358f6c77996960bd929f5c42
Cameron <cameron@e107coders.org>
2013-02-20 22:07:20 -0800

    Made wysiwyg/bbcode switch smaller

--- e107_plugins/tinymce/e_meta.php
+++ e107_plugins/tinymce/e_meta.php
@@ -28,7 +28,7 @@ if(e_WYSIWYG || strpos(e_SELF,"tinymce/admin_config.php") )
 	
 	if(ADMIN)
 	{
-		define("SWITCH_TO_BB","$('#'+id).after('<div><a href=\"#\" id=\"' + id + '\" class=\"e-wysiwyg-toggle btn btn-small\">Switch to bbcode</a></div>');");	
+		define("SWITCH_TO_BB","$('#'+id).after('<div><a href=\"#\" id=\"' + id + '\" class=\"e-wysiwyg-toggle btn btn-mini\">Switch to bbcode</a></div>');");	
 	}
 	else 
 	{
