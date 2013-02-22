c9e52520413022b8f2bd267e423b9ffbaf4aa682
Cameron <cameron@e107coders.org>
2013-02-21 17:46:12 -0800

    Admin infopanel help added

---
+++ e107_languages/English/admin/help/admin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+$caption = "Dashboard";
+$text = "This page provides you with an overview of site activity";
+$ns -> tablerender($caption, $text);
