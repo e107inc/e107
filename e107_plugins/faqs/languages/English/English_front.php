bd775f85965c8fdb8086ec9f0befd4f496757c0b
Cameron <cameron@e107coders.org>
2013-02-23 16:33:36 -0800

    Add missing FAQs terms

--- e107_plugins/faqs/languages/English/English_front.php
+++ e107_plugins/faqs/languages/English/English_front.php
@@ -6,9 +6,12 @@ define(FAQLAN_23, 		"FAQ (Frequently Asked Questions) :: ");
 define(FAQLAN_38, 		"Comment"); //TODO Generic Front-end
 define(FAQLAN_FAQ, 		"FAQ");
 define(FAQLAN_41, 		"FAQ Categories");  
+define(FAQLAN_42, 		"Total");
+
 define(FAQ_ADLAN_29, 	"FAQ entry updated");
 define(FAQ_ADLAN_30, 	"You left a required field blank.");
 define(FAQ_ADLAN_32, 	"New FAQ item created and entered into database.");
+
 define(FAQ_ADLAN_45, 	"Edit"); //TODO Generic Front-end
 define(FAQ_ADLAN_49, 	"Previous FAQ entries: ");
 define(FAQ_ADLAN_50, 	"Delete"); //TODO Generic Front-end
