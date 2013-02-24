d6de1d2e43a70f1d819af0f82c1eccf5348e42a1
Cameron <cameron@e107coders.org>
2013-02-23 16:28:54 -0800

    FAQ language cleanup

--- e107_plugins/faqs/languages/English/English_global.php
+++ e107_plugins/faqs/languages/English/English_global.php
@@ -8,5 +8,5 @@
 
 define("LAN_PLUGIN_FAQS_NAME", 			"FAQs");
 define("LAN_PLUGIN_FAQS_DESCRIPTION", 	"A simple plugin to add Frequently Asked Questions to your website.");
-
+define("LAN_PLUGIN_FAQS_FUNCTIONNAME",	"FAQ Categories");
 ?>
