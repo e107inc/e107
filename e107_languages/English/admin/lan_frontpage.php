23e42d427247f23c2a3130ca75922895d4f0173c
Tijn Kuyper <moc@mochosting.nl>
2013-02-25 18:33:00 +0100

    Frontpage (admin area) - LAN (#6) and code clean-up

--- e107_languages/English/admin/lan_frontpage.php
+++ e107_languages/English/admin/lan_frontpage.php
@@ -8,8 +8,8 @@
 */
 
 
-define("FRTLAN_1", "Front Page settings updated.");
-define("FRTLAN_12", "Update Front Page Settings");
+//define("FRTLAN_1", "Front Page settings updated.");
+//define("FRTLAN_12", "Update Front Page Settings");
 define("FRTLAN_13", "Current Front Page Settings");
 define("FRTLAN_15", "Other (enter url):");
 
@@ -24,23 +24,23 @@ define('FRTLAN_35', 'Post-login page');
 //define('FRTLAN_39', 'If no rule matches, news.php is set as the home page');
 //define('FRTLAN_41', 'The user is sent to the specified &quot;Post-login page&quot; (if specified) immediately following a login');
 */
-define('FRTLAN_40', 'Order');
+//define('FRTLAN_40', 'Order');
 define('FRTLAN_42', 'Add new rule');
 define('FRTLAN_43', 'Class: ');
 // define('FRTLAN_44', 'Go to this page after login');
-define('FRTLAN_45', 'Values not changed');
+//define('FRTLAN_45', 'Values not changed');
 define('FRTLAN_46', 'Edit existing rule');
 define('FRTLAN_47', 'Move up');
 define('FRTLAN_48', 'Move down');
 define('FRTLAN_49', 'Home Page');
 define('FRTLAN_50', '(To disable, select &quot;Other&quot; with a blank URL)');
 define('FRTLAN_51', 'Other');
-define('FRTLAN_52', 'None');
+//define('FRTLAN_52', 'None');
 define('FRTLAN_53', 'User Class');
 
 // 2.0
 define('FRTLAN_PAGE_TITLE', 'Front Page');
-define('FRTLAN_54', 'Are you sure?');
-define('FRTLAN_55', 'Confirm delete rule?');
+//define('FRTLAN_54', 'Are you sure?');
+//define('FRTLAN_55', 'Confirm delete rule?');
 
 ?>
