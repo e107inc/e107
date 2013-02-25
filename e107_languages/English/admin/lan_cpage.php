5e0e15b062ed9ee32dd6bec6e8b5e4e160b0655a
Cameron <cameron@e107coders.org>
2013-02-24 19:53:40 -0800

    Admin Custom-Pages/Menus now running on Admin-ui 100%

--- e107_languages/English/admin/lan_cpage.php
+++ e107_languages/English/admin/lan_cpage.php
@@ -1,62 +1,65 @@
 <?php
 /*
  * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
- * $Id$
  *
  * Admin Language File
  *
 */
-// define("CUSLAN_1", "Title");
-define("CUSLAN_2", "Type");
-define("CUSLAN_3", "Search engine friendly string");
-// define("CUSLAN_4", "Delete this item?");
-define("CUSLAN_5", "Existing Pages");
-define("CUSLAN_7", "Menu Name");
-define("CUSLAN_8", "Title / Caption");
-define("CUSLAN_9", "Text");
-define("CUSLAN_10", "Allow page to be rated");
+
+define("CUSLAN_9", 	"Text");
 define("CUSLAN_11", "Meta description");
 define("CUSLAN_12", "Create page");
-define("CUSLAN_13", "Allow comments");
-define("CUSLAN_14", "Password protect page");
-define("CUSLAN_15", "enter password to protect page");
-define("CUSLAN_16", "Create link in main menu");
-define("CUSLAN_17", "enter link name to create");
-define("CUSLAN_18", "Page / link visible to");
-define("CUSLAN_19", "Update Page");
-define("CUSLAN_20", "Create Page");
-define("CUSLAN_21", "Update Menu");
-define("CUSLAN_22", "Create Menu");
-define("CUSLAN_23", "Edit page");
-define("CUSLAN_24", "Create new page");
-define("CUSLAN_25", "Edit menu");
-define("CUSLAN_26", "Create new menu");
-define("CUSLAN_27", "Page saved to database.");
-define("CUSLAN_28", "Page deleted");
 define("CUSLAN_29", "List pages if no page selected");
 define("CUSLAN_30", "Expiry time for cookie (in seconds)");
 define("CUSLAN_31", "Create menu");
-define("CUSLAN_32", "Meta keywords");
-define("CUSLAN_33", "Page Options");
-define("CUSLAN_34", "Title and SEF string are required fields. Page wasn't saved.");
-define("CUSLAN_35", "SEF string should be unique for every page. Page wasn't saved.");
-define("CUSLAN_36", "Menu name is required field and can't be left empty. Menu wasn't saved.");
+define('CUSLAN_48', "Pages");
+define('CUSLAN_49', "Menus");
+
+// define("CUSLAN_1", "Title");
+// define("CUSLAN_2", "Type");
+// define("CUSLAN_3", "Search engine friendly string");
+// define("CUSLAN_4", "Delete this item?");
+// define("CUSLAN_5", "Existing Pages");
+// define("CUSLAN_7", "Menu Name");
+// define("CUSLAN_8", "Title / Caption");
+// define("CUSLAN_10", "Allow page to be rated");
+// define("CUSLAN_13", "Allow comments");
+// define("CUSLAN_14", "Password protect page");
+// define("CUSLAN_15", "enter password to protect page");
+// define("CUSLAN_16", "Create link in main menu");
+// define("CUSLAN_17", "enter link name to create");
+// define("CUSLAN_18", "Page / link visible to");
+// define("CUSLAN_19", "Update Page");
+// define("CUSLAN_20", "Create Page");
+// define("CUSLAN_21", "Update Menu");
+// define("CUSLAN_22", "Create Menu");
+// define("CUSLAN_23", "Edit page");
+// define("CUSLAN_24", "Create new page");
+// define("CUSLAN_25", "Edit menu");
+// define("CUSLAN_26", "Create new menu");
+// define("CUSLAN_27", "Page saved to database.");
+// define("CUSLAN_28", "Page deleted");
+
+// define("CUSLAN_32", "Meta keywords");
+// define("CUSLAN_33", "Page Options");
+// define("CUSLAN_34", "Title and SEF string are required fields. Page wasn't saved.");
+// define("CUSLAN_35", "SEF string should be unique for every page. Page wasn't saved.");
+// define("CUSLAN_36", "Menu name is required field and can't be left empty. Menu wasn't saved.");
 //define("CUSLAN_37", "Custom Page Update");
 //define("CUSLAN_38", "on");
 //define("CUSLAN_39", "off");
-define("CUSLAN_40", "Save Options");
+// define("CUSLAN_40", "Save Options");
 
-define("CUSLAN_41", "Display author and date information");
-define("CUSLAN_42", "No pages defined yet");
-define('CUSLAN_43', 'untitled menu: ');
-define('CUSLAN_44', 'untitled page');
+// define("CUSLAN_41", "Display author and date information");
+// define("CUSLAN_42", "No pages defined yet");
+// define('CUSLAN_43', 'untitled menu: ');
+// define('CUSLAN_44', 'untitled page');
 //define('CUSLAN_45', 'Settings saved');
 //define('CUSLAN_46', 'Nothing changed - not saved');
-define('CUSLAN_47', 'General Information');
-define('CUSLAN_48', "Pages");
-define('CUSLAN_49', "Menus");
-define('CUSLAN_50', "Existing Menus");
-define("CUSLAN_51", "Menu saved to database.");
-define("CUSLAN_52", "Menu added to menus.");
+// define('CUSLAN_47', 'General Information');
+
+// define('CUSLAN_50', "Existing Menus");
+// define("CUSLAN_51", "Menu saved to database.");
+// define("CUSLAN_52", "Menu added to menus.");
 
 ?>
