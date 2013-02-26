010d9ba9bdd944ef6e4f2f558c5ff05f2e939da2
Cameron <cameron@e107coders.org>
2013-02-26 01:27:38 -0800

    PM Language file fix

--- e107_plugins/pm/languages/English_global.php
+++ e107_plugins/pm/languages/English_global.php
@@ -2,5 +2,6 @@
 
 define("LAN_PLUGIN_PM_NAME", "Private Messenger");
 define("LAN_PLUGIN_PM_DESCRIPTION", "This plugin is a fully featured Private Messaging system.");
-
+define('LAN_PLUGIN_PM_URL_DEFAULT_LABEL', 'Default');
+define('LAN_PLUGIN_PM_URL_DEFAULT_DESCR', 'Default');
 ?>
