77ccca83a4d15b067f976648274bab9eec06a885
SecretR <secretr@e107.org>
2013-02-28 16:07:28 +0200

    Admin UI Default submit action back to "go to list" now

--- e107_handlers/form_handler.php
+++ e107_handlers/form_handler.php
@@ -3463,7 +3463,7 @@ class e_form
 							
 							foreach($defsubmitopt as $k=>$v)
 							{
-								$text .= "<li><a href='#' class='e-noclick'>".$this->radio('__after_submit_action', $k, $selected,"label=".$v)."</a></li>";	
+								$text .= "<li><a href='#' class='e-noclick'>".$this->radio('__after_submit_action', $k, $selected == $v, "label=".$v)."</a></li>";	
 							}
 							
 							//$text .= '
