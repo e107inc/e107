ac263fb9edb3c1a01f054e764bcd21c652478520
Cameron <cameron@e107coders.org>
2013-03-06 15:04:12 -0800

    Simple 'Contact Us' menu item. Needs security image and other options added. Ajax posting?

---
+++ e107_plugins/contact/contact_menu.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!defined('e107_INIT')) { exit; }
+
+$head = '<form id="contact-menu" action="'.e_BASE.'contact.php" method="post" >';
+
+
+//XXX Template must conform to Bootstrap specs: http://twitter.github.com/bootstrap/base-css.html#forms
+//TODO Security Image. 
+
+$template = '
+	<div>
+		<div class="control-group">
+			<label >Name</label>
+   			 {CONTACT_NAME}
+		 </div>
+		 
+		<div class="control-group">
+			<label class="control-label" for="contactEmail">Email</label>
+				{CONTACT_EMAIL}
+		</div>
+		<div class="control-group">
+			<label>Comments</label>
+			{CONTACT_BODY=rows=5&cols=50}							
+		</div>
+		{CONTACT_SUBMIT_BUTTON}
+	</div>       
+  ';
+ 
+$foot = '</form>'; 
+
+
+$contact_shortcodes = e107::getScBatch('contact');                
+$text = $tp->parseTemplate($head. $template . $foot, true, $contact_shortcodes);
+
+
+$ns->tablerender("Contact Us", $text, 'contact-menu');
+
+
+?>
