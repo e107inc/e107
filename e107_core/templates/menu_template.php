ff5f27581ecb394bdd20cca4e71a5be9db89d4e3
Cameron <cameron@e107coders.org>
2013-03-08 02:44:24 -0800

    Menu template added.

---
+++ e107_core/templates/menu_template.php
@@ -0,0 +1,26 @@
+<?php
+
+
+#### Panel Template - Used by menu_class.php  for Custom Menu Content. 
+
+
+	$MENU_TEMPLATE['default']['start'] 					= ''; 
+	$MENU_TEMPLATE['default']['body'] 					= '{CMENUBODY}'; 
+	$MENU_TEMPLATE['default']['noTableRender'] 			= false;
+
+
+	$MENU_TEMPLATE['button']['start'] 					= '<div class="cpage-menu">'; 
+	$MENU_TEMPLATE['button']['body'] 					= '{CMENUBODY}{CPAGEBUTTON}'; 
+	$MENU_TEMPLATE['buttom-image']['end'] 				= '</div>'; 
+	$MENU_TEMPLATE['button']['noTableRender'] 				= false;
+
+	$MENU_TEMPLATE['buttom-image']['start'] 			= '<div class="cpage-menu">'; 
+	$MENU_TEMPLATE['buttom-image']['body'] 				= '{CMENUIMAGE}{CPAGEBUTTON}'; 
+	$MENU_TEMPLATE['buttom-image']['end'] 				= '</div>'; 
+	$MENU_TEMPLATE['buttom-image']['noTableRender'] 	= false;
+
+
+
+
+
+?>
