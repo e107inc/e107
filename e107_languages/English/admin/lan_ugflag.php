e70e09721d7869aa5296ee3544df157c06b9d16a
Moc <moc@mochosting.nl>
2013-03-10 15:21:32 +0100

    Maintenance flag - LAN and code cleanup (#6, #125)

--- e107_languages/English/admin/lan_ugflag.php
+++ e107_languages/English/admin/lan_ugflag.php
@@ -1,28 +1,18 @@
 <?php
 /*
-+ ----------------------------------------------------------------------------+
-|     e107 website system
-|
-|     Copyright (C) 2008-2009 e107 Inc
-|     http://e107.org
-|
-|
-|     Released under the terms and conditions of the
-|     GNU General Public License (http://gnu.org).
-|
-|     $Source: /cvs_backup/e107_0.8/e107_languages/English/admin/lan_ugflag.php,v $
-|     $Revision$
-|     $Date$
-|     $Author$
-+----------------------------------------------------------------------------+
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Maintenance flag - language file
+ *
 */
-define('UGFLAN_1', 'Maintenance settings updated');
+
+//define('UGFLAN_1', 'Maintenance settings updated');
 define('UGFLAN_2', 'Activate maintenance flag');
-define('UGFLAN_3', 'Update Maintenance Setting');
+//define('UGFLAN_3', 'Update Maintenance Setting');
 define('UGFLAN_4', 'Maintenance Setting');
 
 define('UGFLAN_5', 'Text to display when site down');
 define('UGFLAN_6', 'Leave blank to display default message');
-define('UGFLAN_7', 'Update unsuccessful as no changes were made.');
+//define('UGFLAN_7', 'Update unsuccessful as no changes were made.');
 define('UGFLAN_8', 'Limit access to Admins only');
 define('UGFLAN_9', 'Limit access to Main-Admins only');
