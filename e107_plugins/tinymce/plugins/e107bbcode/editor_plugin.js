59bd93441f99dd99e26a4a7237d0fe8b46c90710
Cameron <cameron@e107coders.org>
2013-03-13 03:25:38 -0700

    Partial fix for modal window size

--- e107_plugins/tinymce/plugins/e107bbcode/editor_plugin.js
+++ e107_plugins/tinymce/plugins/e107bbcode/editor_plugin.js
@@ -13,7 +13,7 @@
 				ed.windowManager.open({
 					file : url + '/dialog.php',
 					width : 900 , // + parseInt(ed.getLang('e107bbcode.delta_width', 0)),
-					height : 400, //  + parseInt(ed.getLang('e107bbcode.delta_height', 0)),
+					height : 450, //  + parseInt(ed.getLang('e107bbcode.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url, // Plugin absolute URL
