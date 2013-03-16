10c75e5cc76088329cec198c862088d2a6afe138
Cameron <cameron@e107coders.org>
2013-03-15 20:44:37 -0700

    Changed to bootstrap tags (instead of jquery-ui)

--- e107_admin/header.php
+++ e107_admin/header.php
@@ -76,9 +76,11 @@ function loadJSAddons()
 	
 	e107::js('core', 	'password/jquery.pwdMeter.js', 'jquery', 2);
 	
-	
-	e107::js("core",	"tags/jquery.tagit.js","jquery",3);
-	e107::css('core', 	'tags/jquery.tagit.css', 'jquery');
+	// e107::css('core', 	'bootstrap-tag/bootstrap-tag.css', 'jquery');
+	e107::js('core', 	'bootstrap-tag/bootstrap-tag.js', 'jquery', 2);
+		
+//	e107::js("core",	"tags/jquery.tagit.js","jquery",3);
+//	e107::css('core', 	'tags/jquery.tagit.css', 'jquery');
 	
 	e107::css('core', 	'core/admin.jquery.css', 'jquery');
 	e107::js("core",	"core/admin.jquery.js","jquery",4); // Load all default functions.
--- e107_web/js/core/admin.jquery.js
+++ e107_web/js/core/admin.jquery.js
@@ -137,7 +137,7 @@ $(document).ready(function()
 			
 		});
 		
-		$(".e-tags").tagit();
+		$(".e-tags").tag();
 		
 		
 
