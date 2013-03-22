d0041188da09f43aa48fc29d5c3128596af474f3
Cameron <cameron@e107coders.org>
2013-03-21 18:25:27 -0700

    Credits styling

--- e107_admin/credits.php
+++ e107_admin/credits.php
@@ -14,18 +14,17 @@ require_once("../class2.php");
 
 $css = "body 				{ text-align: left; font-size:13px; line-height:1.5em; font-weight:normal; font-family:Arial, Helvetica, sans-serif; }
 			p 					{ margin:0px 5px 10px 5px; }
-			h1					{ font-size:20px; color:#134B63; font-weight:normal; line-height:1em; margin:20px 0px 5px 0px; }
+		
 			a					{ color:#F6931E; text-decoration:none; }
 			a:hover				{ color:#134B63; text-decoration:none; }
 			.bold				{ font-weight:bold; }
 			.center				{ text-align:center; }
-			.wrapper			{ width:800px; height:415px; margin:0px auto 0px 0px; padding-bottom:10px;  }
-			.wrapper-top		{ height:10px; }
-			.wrapper-bottom		{ height:6px; }
-			.wrapper-middle		{ min-height:389px; background-color:#F6F6F6; }
-			.logo				{  }
-			.credits-content	{ padding:160px 20px 20px 20px; background:url(".e_IMAGE_ABS."admin_images/credits_logo.png) no-repeat 50% 30px; }
-        	.copyright			{ text-align:center; margin-top:30px}
+			.wrapper			{ width:600px;  margin:0px auto 0px 0px; padding-bottom:10px;  }
+			.wrapper-middle		{ min-height:389px;  }
+			.wrapper-text		{  }
+			.logo				{ margin-bottom:20px }
+			.credits-content	{ padding:20px 40px;}
+        	.copyright			{  margin-top:30px}
       ";
 
 e107::css('inline',$css);
@@ -37,19 +36,23 @@ require_once(e_ADMIN."auth.php");
  $text .='<div class="wrapper">
         	<div class="wrapper-middle">
                 <div class="well credits-content">
-                    <h1>v2+ Developers</h1>
-                    <p>
-                        Cameron Hanly, Miroslav Yovchev, Steven Davies,<br />
-                        Henk Jongedijk, James Currie, Martin Nicholls,<br /> 
-                        Steven Davies, Thom Michelbrink, Tijn Kuyper.
-                    </p>
-                    <h1>3rd Parties</h1>
-                    <p>
-                        Twitter Bootstrap, MagpieRSS, PCLZip, PCLTar, TinyMCE,<br />
-                        Nuvolo Icons, PHPMailer, FPDF, UFPDF, PHP UTF8
-                    </p>
-                    <div class="copyright">Copyright <a href="http://e107.org/content/About-Us:The-Team" title="e107 Team">e107 Inc.</a> 2008-2013</div>
-                </div>
+                	<img class="logo" src="'.e_IMAGE_ABS.'admin_images/credits_logo.png" alt="e107 Logo" />
+                	<div class="wrapper-text">
+	                    <h4 class="text-info">Developers</h4>
+	                    <p>
+	                        Senior Developers: Cameron Hanly, Miroslav Yovchev, Steven Davies <br />
+	                        Junior Developers: Tijn Kuyper, Henk Jongedijk <br />
+	                        Early Developers: Thom Michelbrink, James Currie, Martin Nicholls 
+	                    </p>
+	                     <h4 class="text-info">3rd Party</h4>
+	                    <p>
+	                        Twitter Bootstrap, MagpieRSS, PCLZip, PCLTar, TinyMCE,<br />
+	                        Nuvolo Icons, PHPMailer, FPDF, UFPDF, PHP UTF8
+	                    </p>
+                   
+                    	<div class="copyright">Copyright <a href="http://e107.org/content/About-Us:The-Team" title="e107 Team">e107 Inc.</a> 2008-2013</div>
+               		 </div>
+			    </div>
             </div>
         	
 		</div>';
