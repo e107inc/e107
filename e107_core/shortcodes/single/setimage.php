6f025f2df0cd4d47b256b7e6585a86f27035225a
Cameron <cameron@e107coders.org>
2013-03-24 03:03:31 -0700

    {SETIMAGE} can now set Height and also crop the image. eg. {SETIMAGE: w=300&h=200&crop=1}

--- e107_core/shortcodes/single/setimage.php
+++ e107_core/shortcodes/single/setimage.php
@@ -4,6 +4,9 @@
 	function setimage_shortcode($parm, $mode='')
 	{
 		e107::getParser()->thumbWidth = vartrue($parm['w'],100);
+		e107::getParser()->thumbHeight = vartrue($parm['h'],0);
+		e107::getParser()->thumbCrop = vartrue($parm['crop'],0);
+		
 	}
 
 ?>
