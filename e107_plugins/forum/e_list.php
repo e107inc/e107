2ee873c574fe5136f88a4738fb71767e9865e65a
Moc <moc@mochosting.nl>
2013-03-26 14:01:11 +0100

    Fixes two errors from previous clean-up

--- e107_plugins/forum/e_list.php
+++ e107_plugins/forum/e_list.php
@@ -2,14 +2,11 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2012 e107 Inc (e107.org)
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * Forum e_list Handler
- *
- * $URL$
- * $Id$
  *
 */
 if (!defined('e107_INIT')) { exit; }
@@ -61,13 +58,13 @@ class list_forum
 		}
 
 
-		if(!$results = $this->parent->$sql->gen($qry))
+		if(!$results = $this->parent->sql->gen($qry))
 		{
 			$list_data = LIST_FORUM_2;
 		}
 		else
 		{
-			$forumArray = $this->parent->e107->$sql->getList();
+			$forumArray = $this->parent->sql->db_getList();
 			$path = e_PLUGIN."forum/";
 
 			foreach($forumArray as $forumInfo)
