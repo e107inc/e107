6d5fd535c8cfe6ab5bdd6cbf17ef1b900ab5c5f0
Moc <moc@mochosting.nl>
2013-03-26 13:02:28 +0100

    Banner plugin admin LAN fixes

--- e107_plugins/banner/languages/English_admin_banner.php
+++ e107_plugins/banner/languages/English_admin_banner.php
@@ -6,9 +6,7 @@
  *
 */
 
-// CLEANUP IN PROCESS BY Moc - please do NOT edit until this message has been removed!
-
-define("BNRLAN_00", "No banners created yet.");
+define("BNRLAN_00", "No banners created yet."); // 15
 define("BNRLAN_01", "Banner ID not found.");
 
 define("BNRLAN_1", "Client"); //27
@@ -64,77 +62,5 @@ define("BNRLAN_44", "Choose render type ..."); // BANNER_MENU_L11
 define("BNRLAN_45", "One campaign in a single menu"); // BANNER_MENU_L12
 define("BNRLAN_46", "All selected campaigns in one single menu"); // BANNER_MENU_L13
 define("BNRLAN_47", "All selected campaigns in separate menus"); // BANNER_MENU_L14
-
-
-
-// THIS WILL BE REMOVED
-//define("BNRLAN_1", "Banner #%d deleted.");
-//define("BNRLAN_2", "Please confirm you wish to delete this banner - once deleted it cannot be retrieved");  //FIXME Use Generic
-//define("BNRLAN_5", "Confirm Delete Banner");  
-//define("BNRLAN_6", "Delete cancelled.");
-//define("BNRLAN_7", "Existing Banners"); 
-//define("BNRLAN_8", "Banner ID");
-//define("BNRLAN_15", "No banners yet.");
-//define("BNRLAN_16", "Unlimited");
-//define("BNRLAN_17", "None");
-//define("BNRLAN_21", "Ends");
-//define("BNRLAN_22", "Update Banner"); 
-//define("BNRLAN_23", "Add New Banner"); 
-//define("BNRLAN_24", "Campaign");
-//define("BNRLAN_25", "Add new or choose existing campaign");
-//define("BNRLAN_26", "Enter new campaign");
-//define("BNRLAN_26a", "New Campaign");
-//define("BNRLAN_27", "Client");
-//define("BNRLAN_28", "Add new or choose existing client");
-//define("BNRLAN_29", "Enter new client");
-//define("BNRLAN_30", "Client Login");
-//define("BNRLAN_31", "Client Password");
-//define("BNRLAN_32", "Banner Image");
-//define("BNRLAN_33", "Click URL");
-//define("BNRLAN_34", "Number of impressions purchased");
-//define("BNRLAN_35", "unlimited");
-//define("BNRLAN_36", "Start Date");
-//define("BNRLAN_37", "End Date");
-//define("BNRLAN_38", "No limit when left blank.");
-//define("BNRLAN_39", "visibility class");
-//define("BNRLAN_40", "Update Banner");
-//define("BNRLAN_41", "Create New Banner");
-//define("BNRLAN_42", "Banner Rotation System");
-//define("BNRLAN_43", "Choose banner image");
-//define("BNRLAN_45", "Starts");
-//define("BNRLAN_46", "Code");
-//define("BNRLAN_58", "Banner front page");
-//define("BNRLAN_59", "Create new banner");
-//define("BNRLAN_60", "campaigns");
-//define("BNRLAN_61", "Banner menu");
-//define("BNRLAN_62", "Banner Options"); 
-//define("BNRLAN_63", "Banner Created");  
-//define("BNRLAN_64", "Banner Updated");  
-//define("BNRLAN_65", "Stats");
-//define("BNRLAN_66", "N/A");
-//define("BNRLAN_67", "No campaigns yet.");
-//define("BNRLAN_68", "Banner Menu Configuration");
-
-//  Banner Menu Configuration
-//define("BANNER_MENU_L1", "Advertisement");
-//define("BANNER_MENU_L2", "Banner menu configuration saved");
-//define("BANNER_MENU_L3", "Caption");
-//define("BANNER_MENU_L4", "Campaign");
-//define("BANNER_MENU_L5", "Banner Menu Configuration");
-//define("BANNER_MENU_L6", "Choose campaigns to show in menu");
-//define("BANNER_MENU_L7", "available campaigns");
-///define("BANNER_MENU_L8", "selected campaigns");
-//define("BANNER_MENU_L9", "remove selection");
-//define("BANNER_MENU_L10", "how should the selected campaigns be shown ?");
-//define("BANNER_MENU_L11", "Choose render type ..."); // Render type
-//define("BANNER_MENU_L12", "One campaign in single menu");
-//define("BANNER_MENU_L13", "All selected campaigns in one single menu");
-//define("BANNER_MENU_L14", "All selected campaigns in separate menus");
-//define("BANNER_MENU_L15", "how many banners should be shown ?");
-//define("BANNER_MENU_L16", "this setting will only be used with options 2 and 3.<br />if less banners are present the maximum available amount will be used.");
-//define("BANNER_MENU_L17", "set amount ...");
-//define("BANNER_MENU_L18", "Update Menu Settings");
-//define('BANNER_MENU_L19', 'Number of banners to show');
-//define('BANNER_MENU_L20', 'This is only used when multiple campaigns are selected.');
 
 ?>
