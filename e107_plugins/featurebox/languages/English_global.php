f414e29adc1caf9dbc213cf88b566a58d550aaaf
Cameron <cameron@e107coders.org>
2013-03-28 01:04:01 -0700

    Featurebox defaults

--- e107_plugins/featurebox/languages/English_global.php
+++ e107_plugins/featurebox/languages/English_global.php
@@ -15,4 +15,5 @@ define("LAN_PLUGIN_FEATUREBOX_BATCH",  		"Create Featurebox Item");
 define('FBLAN_INSTALL_01', 'Adding Default category table data.'); //XXX Use Generic
 define('FBLAN_INSTALL_02', 'Adding Default table data.'); //XXX Use Generic
 define('FBLAN_INSTALL_03', 'Unassigned'); 
-define('FBLAN_INSTALL_04', 'General'); 
+define('FBLAN_INSTALL_04', 'Carousel'); 
+define('FBLAN_INSTALL_05', 'Tabs'); 
