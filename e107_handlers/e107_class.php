62de1e7337696dee5b48b5807b63ee34b1552e76
Cameron <cameron@e107coders.org>
2013-03-31 04:21:49 -0700

    Simple e107::user($uid) function introduced for quick and easy access to user info.

--- e107_handlers/e107_class.php
+++ e107_handlers/e107_class.php
@@ -1386,6 +1386,27 @@ class e107
 	}
 
 	/**
+	 * Simple replacement for deprecated get_user_data(). e107::user();
+	 * @param $uid integer user_id or leave empty for currently logged in user. 
+	 * @return array of user data
+	 */
+	public static function user($uid=null)
+	{
+		if(!$uid){ return false; }
+			
+		$user = self::getSystemUser($uid, true);
+		$var = array();
+		if($user)
+		{
+			$var = $user->getUserData();
+		}
+		
+		return $var;
+	}
+
+
+
+	/**
 	 * Retrieve current user model object.
 	 *
 	 * @return e_user
