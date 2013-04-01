cebe1810a9714a9b862c2b9a46922a6c2076f808
SecretR <secretr@e107.org>
2013-04-01 15:12:15 +0300

    FAQs category meta description and meta keywords

--- e107_plugins/faqs/languages/English/English_admin.php
+++ e107_plugins/faqs/languages/English/English_admin.php
@@ -15,6 +15,8 @@ define('LANA_FAQ_UNAME', 'User name'); //FIXME Use generic
 define('LANA_FAQ_ULOGINNAME', 'User login'); //FIXME Use generic
 define('LANA_FAQ_TAGS', 'Tags');
 define('LANA_FAQ_TAGS_HELP', 'Comma separated tag list');
+define('LANA_FAQ_METAD', 'Meta Description');
+define('LANA_FAQ_METAK', 'Meta Keywords');
 
 define('LANA_FAQ_PREF_1', 'Allow submitting of FAQs by:');
 define('LANA_FAQ_PREF_2', 'Allow submitting of Questions by:');
