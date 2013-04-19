2dbecdba2d3926ef9229185620cec5255f36dc37
Cameron <cameron@e107coders.org>
2013-04-18 19:05:30 -0700

    Forum admin GUI cleanup

--- e107_plugins/forum/forum_admin_class.php
+++ e107_plugins/forum/forum_admin_class.php
@@ -363,8 +363,8 @@ class forumAdmin
 				{
 					$text .= "
 					<div style='text-align:left; padding-left: 30px'>
-					<a class='btn btn-large' href='".e_SELF."?cat.edit.{$parent['forum_id']}'>".ADMIN_EDIT_ICON."</a>
-					<a class='btn btn-large' href='".e_SELF."?delete.{$parent['forum_id']}'>".ADMIN_DELETE_ICON."</a>
+					<a class='btn ' href='".e_SELF."?cat.edit.{$parent['forum_id']}'>".ADMIN_EDIT_ICON."</a>
+					<a class='btn ' href='".e_SELF."?delete.{$parent['forum_id']}'>".ADMIN_DELETE_ICON."</a>
 					</div>
 					";
 				}
@@ -409,12 +409,14 @@ class forumAdmin
 						}
 						else
 						{
-							$sub_img = count($subList[$forum['forum_parent']][$forum['forum_id']]) ? IMAGE_sub : IMAGE_nosub;
+							//$sub_img = count($subList[$forum['forum_parent']][$forum['forum_id']]) ? IMAGE_sub : IMAGE_nosub;
+							$sub_img = ADMIN_DOWN_ICON;
+							$sub_total = count($subList[$forum['forum_parent']][$forum['forum_id']]);
 							$text .= "
 							<div style='text-align:left; padding-left: 30px'>
-							<a class='btn btn-large' href='".e_SELF."?create.edit.{$forum['forum_id']}'>".ADMIN_EDIT_ICON."</a>
-							<a class='btn btn-large' href='".e_SELF."?delete.{$forum['forum_id']}'>".ADMIN_DELETE_ICON."</a>
-							&nbsp;&nbsp;<a class='btn btn-large' href='".e_SELF."?subs.{$forum['forum_id']}'>".$sub_img."</a> (".count($subList[$forum['forum_parent']][$forum['forum_id']]).")
+							<a class='btn e-tip' href='".e_SELF."?create.edit.{$forum['forum_id']}' title=\"".LAN_EDIT."\">".ADMIN_EDIT_ICON."</a>
+							<a class='btn e-tip' href='".e_SELF."?delete.{$forum['forum_id']}' title=\"".LAN_DELETE."\">".ADMIN_DELETE_ICON."</a>
+							<a class='btn e-tip' href='".e_SELF."?subs.{$forum['forum_id']}' title='Create Sub-Forum. Total: {$sub_total}'>".$sub_img."</a>
 							</div>
 							";
 						}
