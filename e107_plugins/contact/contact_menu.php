7e0f22ac6615e978aea57baf7bef6bc044283e4d
Cameron <cameron@e107coders.org>
2013-04-26 22:25:48 -0700

    Bad link fix.

--- e107_plugins/contact/contact_menu.php
+++ e107_plugins/contact/contact_menu.php
@@ -4,7 +4,7 @@ if (!defined('e107_INIT')) { exit; }
 
 e107::lan('core','contact');
 
-$head = '<form id="contact-menu" action="'.e_BASE.'contact.php" method="post" >';
+$head = '<form id="contact-menu" action="'.e_HTTP.'contact.php" method="post" >';
 
 
 //XXX Template must conform to Bootstrap specs: http://twitter.github.com/bootstrap/base-css.html#forms
