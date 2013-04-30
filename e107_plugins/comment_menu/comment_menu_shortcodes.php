7d968615520ea06a50611740aa5a34e97ec4ab09
Cameron <cameron@e107coders.org>
2013-04-29 19:36:05 -0700

    Comment Menu Template cleanup

--- e107_plugins/comment_menu/comment_menu_shortcodes.php
+++ e107_plugins/comment_menu/comment_menu_shortcodes.php
@@ -15,9 +15,88 @@
 */
 
 if (!defined('e107_INIT')) { exit; }
-global $tp;
-$comment_menu_shortcodes = $tp -> e_sc -> parse_scbatch(__FILE__);
-e107::getRegistry('plugin/comment_menu/current');
+
+//$comment_menu_shortcodes = $tp -> e_sc -> parse_scbatch(__FILE__);
+//e107::getRegistry('plugin/comment_menu/current');
+
+class comment_menu_shortcodes extends e_shortcode
+{
+	/**
+	 * @DEPRECATED - use css styling instead. 
+	 */
+	function sc_cm_icon()
+	{
+		//TODO review bullet
+		$bullet = '';
+		if(defined('BULLET'))
+		{
+			$bullet = '<img src="'.THEME.'images/'.BULLET.'" alt="" class="icon" />';
+		}
+		elseif(file_exists(THEME.'images/bullet2.gif'))
+		{
+			$bullet = '<img src="'.THEME.'images/bullet2.gif" alt="" class="icon" />';
+		}
+		return $bullet;
+	}
+
+	function sc_cm_datestamp()
+	{
+		$gen = new convert;
+		return $gen->convert_date($this->var['comment_datestamp'], "relative");
+	}
+		
+	function sc_cm_heading()
+	{
+		return $this->var['comment_title'];
+	}
+		
+	function sc_cm_url_pre()
+	{
+		return ($this->var['comment_url'] ? "<a href='".$this->var['comment_url']."'>" : "");
+	}
+		
+	function sc_cm_url_post()
+	{
+		return ($this->var['comment_url'] ? "</a>" : "");
+	}
+		
+	function sc_cm_type()
+	{
+		return $this->var['comment_type'];
+	}
+		
+	function sc_cm_author()
+	{
+		return $this->var['comment_author'];
+	}
+	
+	
+	function sc_cm_comment($parm='')
+	{
+		$menu_pref 	= e107::getConfig('menu')->getPref();
+		$tp 		= e107::getParser();
+		$COMMENT 	= '';
+		
+		if($menu_pref['comment_characters'] > 0)
+		{
+			$COMMENT = strip_tags($tp->toHTML($this->var['comment_comment'], TRUE, "emotes_off, no_make_clickable", "", e107::getPref('menu_wordwrap')));
+			if ($tp->ustrlen($COMMENT) > $menu_pref['comment_characters'])
+			{
+				$COMMENT = $tp->text_truncate($COMMENT, $menu_pref['comment_characters'],'').($this->var['comment_url'] ? " <a href='".$this->var['comment_url']."'>" : "").defset($menu_pref['comment_postfix'], $menu_pref['comment_postfix']).($this->var['comment_url'] ? "</a>" : "");
+			}
+		}
+		
+		return $COMMENT;	
+	}
+	
+}
+
+
+
+
+
+
+
 /*
 SC_BEGIN CM_ICON
 //TODO review bullet
@@ -36,7 +115,7 @@ SC_END
 SC_BEGIN CM_DATESTAMP
 $row = e107::getRegistry('plugin/comment_menu/current');
 $gen = new convert;
-return $gen->convert_date($row['comment_datestamp'], "short");
+return $gen->convert_date($row['comment_datestamp'], "relative");
 SC_END
 
 SC_BEGIN CM_HEADING
@@ -69,6 +148,7 @@ $row = e107::getRegistry('plugin/comment_menu/current');
 $menu_pref = e107::getConfig('menu')->getPref();
 $tp = e107::getParser();
 $COMMENT = '';
+
 if($menu_pref['comment_characters'] > 0)
 {
   $COMMENT = strip_tags($tp->toHTML($row['comment_comment'], TRUE, "emotes_off, no_make_clickable", "", e107::getPref('menu_wordwrap')));
