b6edf707a7218e6b077ed5158e64f83f3cbfb9f8
Cameron <cameron@e107coders.org>
2013-05-12 04:13:57 -0700

    Closes #283 whitelist <script> attributes and noscript tag (when script posting is enabled)

--- e107_handlers/e_parse_class.php
+++ e107_handlers/e_parse_class.php
@@ -2433,12 +2433,13 @@ class e_parser
                                     'default'   => array('id', 'style', 'class'),
                                     'img'       => array('id', 'src', 'style', 'class', 'alt', 'title', 'width', 'height'),
                                     'a'         => array('id', 'href', 'style', 'class', 'title'),
+                                    'script'	=> array('type', 'src', 'language')
                                   ); 
     private $badAttrValues       = array("javascript[\s]*?:","alert\(","vbscript[\s]*?:","data:text\/html", "mhtml[\s]*?:", "data:[\s]*?image");
     private $allowedTags        = array('html', 'body','div','a','img','table','tr', 'td', 'th', 'tbody', 'thead', 'colgroup', 'b', 
                                         'i', 'pre','code', 'strong', 'u', 'em','ul','li','img','h1','h2','h3','h4','h5','h6','p',
                                         'div','pre','section','article', 'blockquote','hgroup','aside','figure','span', 'video', 'br',
-                                        'small', 'caption' 
+                                        'small', 'caption', 'noscript'
                                    );
 	private $scriptTags 		= array('script','applet','iframe'); //allowed whem $pref['post_script'] is enabled. 
         
