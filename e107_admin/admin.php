b949c187066f7f9384ee9c9fcd35f1f700857643
Cameron <cameron@e107coders.org>
2013-05-18 02:02:00 -0700

    Check e_MEDIA and e_SYSTEM are writable.

--- e107_admin/admin.php
+++ e107_admin/admin.php
@@ -81,12 +81,31 @@ class admin_start
 	
 	function __construct()
 	{
-	
+		$this->checkWritable();
 		$this->checkHtmlarea();	
 		$this->checkIncompatiblePlugins();
 		$this->checkFileTypes();
 		$this->checkSuspiciousFiles();
+		
 	}	
+
+
+	function checkWritable()
+	{
+		$mes = e107::getMessage();
+		
+		if(deftrue('e_MEDIA') && !is_writable(e_MEDIA))
+		{
+			$mes->addWarning("The folder ".e_MEDIA." is not writable. Please correct before proceeding.");			
+		}	
+		
+		if(deftrue('e_SYSTEM') && !is_writable(e_SYSTEM))
+		{
+			$mes->addWarning("The folder ".e_SYSTEM." is not writable. Please correct before proceeding.");			
+		}			
+		
+	}
+
 
 	
 	
