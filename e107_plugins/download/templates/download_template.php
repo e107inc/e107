0621c40e93cdbb13368ce07a7c332a8c27720adc
Cameron <cameron@e107coders.org>
2013-06-21 01:26:55 -0700

    Bootstrap styling of template.

--- e107_plugins/download/templates/download_template.php
+++ e107_plugins/download/templates/download_template.php
@@ -30,7 +30,7 @@ if(!isset($DOWNLOAD_CAT_TABLE_START))
 {
    $DOWNLOAD_CAT_TABLE_START = "
       <div style='text-align:center'>
-         <table class='table fborder' style='".USER_WIDTH."'>\n
+         <table class='table table-striped fborder' style='".USER_WIDTH."'>\n
 		      <colgroup>
 		         <col style='width:3%'/>
 		         <col style='width:60%'/>
@@ -139,7 +139,7 @@ if(!isset($DOWNLOAD_LIST_TABLE_START))
    $DOWNLOAD_LIST_TABLE_START = "
       <div style='text-align:center'>
          <form method='post' action='".e_SELF."?".e_QUERY."'>
-            <table class='table fborder' style='".USER_WIDTH."'>\n
+            <table class='table table-striped fborder' style='".USER_WIDTH."'>\n
                <colgroup>
                   <col style='width:35%;'/>
                   <col style='width:15%;'/>
@@ -266,7 +266,7 @@ if(!isset($DOWNLOAD_VIEW_TABLE))
 {
 	$DOWNLOAD_VIEW_TABLE = "
       <div style='text-align:center'>
-		   <table class='table fborder' style='".USER_WIDTH."'>
+		   <table class='table table-striped fborder' style='".USER_WIDTH."'>
 		      <colgroup>
 		         <col style='width:30%;'>
 		         <col style='width:70%;'>
