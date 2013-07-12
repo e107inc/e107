99773b4716dd699c35091ea7bbb1359e19622c05
Cameron <cameron@e107coders.org>
2013-07-12 08:16:56 -0700

    Added font-awesome to core. Now reads glyph-list directly from font-awesome file.

--- e107_handlers/media_class.php
+++ e107_handlers/media_class.php
@@ -755,157 +755,27 @@ class e_media
 	}
 
 
-
+	/**
+	 * Get all Glyphs
+	 * @todo Cache 
+	 */
 	function getGlyphs()
 	{
-		$icons = array(
-			"icon-glass",
-            "icon-music",
-            "icon-search",
-            "icon-envelope",
-            "icon-heart",
-            "icon-star",
-            "icon-star-empty",
-            "icon-user",
-            "icon-film",
-            "icon-th-large",
-            "icon-th",
-            "icon-th-list",
-            "icon-ok",
-            "icon-remove",
-            "icon-zoom-in",
-            "icon-zoom-out",
-            "icon-off",
-            "icon-signal",
-            "icon-cog",
-            "icon-trash",
-            "icon-home",
-            "icon-file",
-            "icon-time",
-            "icon-road",
-            "icon-download-alt",
-            "icon-download",
-            "icon-upload",
-            "icon-inbox",
-            "icon-play-circle",
-            "icon-repeat",
-            "icon-refresh",
-            "icon-list-alt",
-            "icon-lock",
-            "icon-flag",
-            "icon-headphones",
-            "icon-volume-off",
-            "icon-volume-down",
-            "icon-volume-up",
-            "icon-qrcode",
-            "icon-barcode",
-            "icon-tag",
-            "icon-tags",
-            "icon-book",
-            "icon-bookmark",
-            "icon-print",
-            "icon-camera",
-            "icon-font",
-            "icon-bold",
-            "icon-italic",
-            "icon-text-height",
-            "icon-text-width",
-            "icon-align-left",
-            "icon-align-center",
-            "icon-align-right",
-            "icon-align-justify",
-            "icon-list",
-
-            "icon-indent-left",
-            "icon-indent-right",
-            "icon-facetime-video",
-            "icon-picture",
-            "icon-pencil",
-            "icon-map-marker",
-            "icon-adjust",
-            "icon-tint",
-            "icon-edit",
-            "icon-share",
-            "icon-check",
-            "icon-move",
-            "icon-step-backward",
-            "icon-fast-backward",
-            "icon-backward",
-            "icon-play",
-            "icon-pause",
-            "icon-stop",
-            "icon-forward",
-            "icon-fast-forward",
-            "icon-step-forward",
-            "icon-eject",
-            "icon-chevron-left",
-            "icon-chevron-right",
-            "icon-plus-sign",
-            "icon-minus-sign",
-            "icon-remove-sign",
-            "icon-ok-sign",
-
-            "icon-question-sign",
-            "icon-info-sign",
-            "icon-screenshot",
-            "icon-remove-circle",
-            "icon-ok-circle",
-            "icon-ban-circle",
-            "icon-arrow-left",
-            "icon-arrow-right",
-            "icon-arrow-up",
-            "icon-arrow-down",
-            "icon-share-alt",
-            "icon-resize-full",
-            "icon-resize-small",
-            "icon-plus",
-            "icon-minus",
-            "icon-asterisk",
-            "icon-exclamation-sign",
-            "icon-gift",
-            "icon-leaf",
-            "icon-fire",
-            "icon-eye-open",
-            "icon-eye-close",
-            "icon-warning-sign",
-            "icon-plane",
-            "icon-calendar",
-            "icon-random",
-            "icon-comment",
-            "icon-magnet",
-
-            "icon-chevron-up",
-            "icon-chevron-down",
-            "icon-retweet",
-            "icon-shopping-cart",
-            "icon-folder-close",
-            "icon-folder-open",
-            "icon-resize-vertical",
-            "icon-resize-horizontal",
-            "icon-hdd",
-            "icon-bullhorn",
-            "icon-bell",
-            "icon-certificate",
-            "icon-thumbs-up",
-            "icon-thumbs-down",
-            "icon-hand-right",
-            "icon-hand-left",
-            "icon-hand-up",
-            "icon-hand-down",
-            "icon-circle-arrow-right",
-            "icon-circle-arrow-left",
-            "icon-circle-arrow-up",
-            "icon-circle-arrow-down",
-            "icon-globe",
-            "icon-wrench",
-            "icon-tasks",
-            "icon-filter",
-            "icon-briefcase",
-            "icon-fullscreen"
-       );		
 		
-		return $icons;
+		$pattern = '/\.(icon-(?:\w+(?:-)?)+):before\s+{\s*content:\s*"(.+)";\s+}/';
+		$subject = file_get_contents(e_WEB_JS.'font-awesome/css/font-awesome.css');
 		
+		preg_match_all($pattern, $subject, $matches, PREG_SET_ORDER);
+		
+		$icons = array();
+		
+		foreach($matches as $match)
+		{
+		    $icons[] = $match[1];
+		}
+				
+		return $icons; 
+	
 	}
 
 
