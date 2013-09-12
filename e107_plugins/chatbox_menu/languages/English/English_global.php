c8824ee28904a27ea1bf51fc7b529bbc8c1d11cd
Moc <moc@mochosting.nl>
2013-09-12 20:10:30 +0200

    Moved Chatbox notify LAN to Global

--- e107_plugins/chatbox_menu/languages/English/English_global.php
+++ e107_plugins/chatbox_menu/languages/English/English_global.php
@@ -12,5 +12,12 @@ define("LAN_AL_CHBLAN_03","Chatbox posts recalculated");
 define("LAN_AL_CHBLAN_04","");
 define("LAN_AL_CHBLAN_05","");
 
+// Notify
+define("NT_LAN_CB_1", "Chatbox Events");
+define("NT_LAN_CB_2", "Message posted");
+define("NT_LAN_CB_3", "Posted by");
+define("NT_LAN_CB_4", "IP Address");
+define("NT_LAN_CB_5", "Message");
+define("NT_LAN_CB_6", "Chatbox Message Posted");
 
 ?>
