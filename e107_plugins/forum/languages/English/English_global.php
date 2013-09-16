98688f5aea706337ee0288771bc2ba0bdd02f3ce
Moc <moc@mochosting.nl>
2013-09-16 13:11:12 +0200

    Updated forum e_notify to v2 standards

--- e107_plugins/forum/languages/English/English_global.php
+++ e107_plugins/forum/languages/English/English_global.php
@@ -4,11 +4,13 @@ define("LAN_PLUGIN_FORUM_NAME", "Forum");
 define("LAN_PLUGIN_FORUM_DESC", "This plugin is a fully featured forum system.");
 
 // Notify
-define('FORUM_NT_1', 'Forum Events');
-define('FORUM_NT_2', 'Thread');
-define('FORUM_NT_3', 'Post');
-define('FORUM_NT_4', 'added by');
-define('FORUM_NT_5', 'moved by');
+// TODO - LAN cleanup (see e_notify)
+
+//define('FORUM_NT_1', 'Forum Events');
+//define('FORUM_NT_2', 'Thread');
+//define('FORUM_NT_3', 'Post');
+//define('FORUM_NT_4', 'added by');
+//define('FORUM_NT_5', 'moved by');
 define('FORUM_NT_6', 'Forum - Thread created');
 define('FORUM_NT_7', 'Forum - Thread created by new user');
 define('FORUM_NT_8', 'Forum - Thread deleted');
@@ -16,10 +18,10 @@ define('FORUM_NT_9', 'Forum - Thread split');
 define('FORUM_NT_10', 'Forum - Post deleted');
 define('FORUM_NT_11', 'Forum - Post reported');
 
-define('FORUM_NT_NEWTHREAD', 'New thread created');
-define('FORUM_NT_NEWTHREAD_PROB', 'New thread created by probationary member');
-define('FORUM_NT_THREAD_DELETED', 'Thread deleted');
-define('FORUM_NT_THREAD_SPLIT', 'Thread split');
+define('FORUM_NT_NEWTOPIC', 'New topic created');
+define('FORUM_NT_NEWTOPIC_PROB', 'New topic created by probationary member');
+define('FORUM_NT_TOPIC_DELETED', 'Topic deleted');
+define('FORUM_NT_TOPIC_SPLIT', 'Topic split');
 define('FORUM_NT_POST_DELETED', 'Post deleted');
 define('FORUM_NT_POST_REPORTED', 'Post reported');
 
