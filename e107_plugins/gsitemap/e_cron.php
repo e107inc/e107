0c7a636066bc69ecda13ae7588cbea29a4bef4fc
Moc <moc@mochosting.nl>
2013-09-16 12:46:52 +0200

    Minor code clean-up (notify LAN are now in global LAN)

--- e107_plugins/gsitemap/e_cron.php
+++ e107_plugins/gsitemap/e_cron.php
@@ -2,25 +2,15 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * Plugin configuration module - gsitemap
  *
- * $Source: /cvs_backup/e107_0.8/e107_plugins/gsitemap/e_cron.php,v $
- * $Revision$
- * $Date$
- * $Author$
- *
 */
 
 if (!defined('e107_INIT')) { exit; }
-/*if(!plugInstalled('gsitemap'))
-{ 
-	return;
-}*/
-
 
 class gsitemap_cron // include plugin-folder in the name.
 {
@@ -48,14 +38,12 @@ class gsitemap_cron // include plugin-folder in the name.
 	}
 	
 	
-
 	function myfunction() 
 	{
 	    // Whatever code you wish.
 	    e107::getMessage()->add("Executed dummy function within gsitemap/e_cron.php");
 	    return ;
 	}
-	
 	
 	
 	function sendEmail()
