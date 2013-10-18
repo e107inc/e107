b12cd43bd2b65a37c508d8a1c74d6c1b376b8aa5
SecretR <secretr@e107.org>
2013-10-18 10:50:12 +0300

    Menu template fixes; Page template lan

--- e107_core/templates/menu_template.php
+++ e107_core/templates/menu_template.php
@@ -6,10 +6,11 @@
 
 	$MENU_TEMPLATE['default']['start'] 					= ''; 
 	$MENU_TEMPLATE['default']['body'] 					= '{CMENUBODY}'; 
+	$MENU_TEMPLATE['default']['end'] 					= ''; 
 
 	$MENU_TEMPLATE['button']['start'] 					= '<div class="cpage-menu">'; 
 	$MENU_TEMPLATE['button']['body'] 					= '{CMENUBODY}{CPAGEBUTTON}'; 
-	$MENU_TEMPLATE['buttom-image']['end'] 				= '</div>'; 
+	$MENU_TEMPLATE['button']['end'] 					= '</div>'; 
 
 	$MENU_TEMPLATE['buttom-image']['start'] 			= '<div class="cpage-menu">'; 
 	$MENU_TEMPLATE['buttom-image']['body'] 				= '{CMENUIMAGE}{CPAGEBUTTON}'; 
