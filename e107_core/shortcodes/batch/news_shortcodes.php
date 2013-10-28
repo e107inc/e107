8d11a8c744e4cab5169d1a4a3373c39a8c97e6ca
Cameron <cameron@e107coders.org>
2013-10-28 10:44:44 -0700

    Open {ADMINOPTIONS} news edit in new browser window by default

--- e107_core/shortcodes/batch/news_shortcodes.php
+++ e107_core/shortcodes/batch/news_shortcodes.php
@@ -230,11 +230,11 @@ class news_shortcodes extends e_shortcode
 	{
 		if (ADMIN && getperms('H'))
 		{
-			
+			//TODO - discuss - a pref for 'new browser window' loading, or a parm or leave 'new browser window' as default?
 			$default = (deftrue('BOOTSTRAP')) ? "<i class='icon-edit'></i>" :  "<img src='".e_IMAGE_ABS."admin_images/edit_16.png' alt='".LAN_NEWS_25."' class='icon' />";
 			
 			$adop_icon = (file_exists(THEME."images/newsedit.png") ? "<img src='".THEME_ABS."images/newsedit.png' alt='".LAN_NEWS_25."' class='icon' />" : $default);
-			return " <a href='".e_ADMIN_ABS."newspost.php?action=create&amp;sub=edit&amp;id=".$this->news_item['news_id']."' title=\"".LAN_NEWS_25."\">".$adop_icon."</a>\n";
+			return " <a rel='external' href='".e_ADMIN_ABS."newspost.php?action=create&amp;sub=edit&amp;id=".$this->news_item['news_id']."' title=\"".LAN_NEWS_25."\">".$adop_icon."</a>\n";
 		}
 		else
 		{
