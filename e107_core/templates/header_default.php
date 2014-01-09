0df0f49cc01f37971075d3fbc51cdf8231a30042
Cameron <cameron@e107coders.org>
2014-01-09 04:23:55 -0800

    HTML5 meta tag 'copyright' fix.

--- e107_core/templates/header_default.php
+++ e107_core/templates/header_default.php
@@ -136,7 +136,7 @@ else
 // C: Send start of HTML
 //
 
-if(vartrue($pref['meta_copyright'][e_LANGUAGE])) e107::meta('copyright',$pref['meta_copyright'][e_LANGUAGE]);
+if(vartrue($pref['meta_copyright'][e_LANGUAGE])) e107::meta('dcterms.rights',$pref['meta_copyright'][e_LANGUAGE]);
 if(vartrue($pref['meta_author'][e_LANGUAGE])) e107::meta('author',$pref['meta_author'][e_LANGUAGE]);
 if($pref['sitebutton']) e107::meta('og:image',$tp->replaceConstants($pref['sitelogo'],'full'));
 if(defined("VIEWPORT")) e107::meta('viewport',VIEWPORT); //BC ONLY 
