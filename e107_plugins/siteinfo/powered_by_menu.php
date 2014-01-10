5c3d84523487007a76bddda5f38430344e1589bf
Cameron <cameron@e107coders.org>
2014-01-10 09:33:04 -0800

    Rework and simplification of the 'powered by' menu when using Bootstrap themes.

--- e107_plugins/siteinfo/powered_by_menu.php
+++ e107_plugins/siteinfo/powered_by_menu.php
@@ -16,17 +16,32 @@
 
 if (!defined('e107_INIT')) { exit; }
 
+
 $text = "
-<div style='text-align: center'>
-<div class='spacer'>
-<a href='http://e107.org' rel='external'><img src='".e_IMAGE_ABS."button.png' alt='e107' style='border: 0px; width: 88px; height: 31px' /></a>
+<div id='powered-by-menu' class='text-center'>
+<a href='http://e107.org' rel='external'>
+	<img class='img-responsive' src='".e_IMAGE_ABS."admin_images/credits_logo.png' alt='e107' style='max-width:100%' />
+</a>
 </div>
-<div class='spacer'>
-<a href='http://php.net' rel='external'><img src='".e_IMAGE_ABS."generic/php-small-trans-light.gif' alt='PHP' style='border: 0px; width: 88px; height: 31px' /></a>
-</div>
-<div class='spacer'>
-<a href='http://mysql.com' rel='external'><img src='".e_IMAGE_ABS."generic/poweredbymysql-88.png' alt='MySQL' style='border: 0px; width: 88px; height: 31px' /></a>
-</div>
-</div>";
+";
+
+
+if(!deftrue("BOOTSTRAP")) // Old Legacy Code. 
+{
+	$text = "
+	<div style='text-align: center'>
+	<div class='spacer'>
+	<a href='http://e107.org' rel='external'><img src='".e_IMAGE_ABS."button.png' alt='e107' style='border: 0px; width: 88px; height: 31px' /></a>
+	</div>
+	<div class='spacer'>
+	<a href='http://php.net' rel='external'><img src='".e_IMAGE_ABS."generic/php-small-trans-light.gif' alt='PHP' style='border: 0px; width: 88px; height: 31px' /></a>
+	</div>
+	<div class='spacer'>
+	<a href='http://mysql.com' rel='external'><img src='".e_IMAGE_ABS."generic/poweredbymysql-88.png' alt='MySQL' style='border: 0px; width: 88px; height: 31px' /></a>
+	</div>
+	</div>";
+}
+
+
 $ns -> tablerender(POWEREDBY_L1,  $text, 'powered_by');
 ?>
