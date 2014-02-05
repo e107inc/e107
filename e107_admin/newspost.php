8efe71dac0eea598502f7f0b3396092f5712039b
Cameron <cameron@e107coders.org>
2014-02-05 06:16:07 -0800

    Fixed BC bug in newspost (breaking Youtube thumbnail images after re-edit)

--- e107_admin/newspost.php
+++ e107_admin/newspost.php
@@ -2396,7 +2396,7 @@ class admin_newspost
 										".$frm->help(LAN_NEWS_23)."</td>
 										<td>
 				";
-		if(vartrue($_POST['news_thumbnail']) && $_POST['news_thumbnail'][0] != "{" && substr($_POST['news_thumbnail'],-8) !== '.youtube')//BC compat
+		if(vartrue($_POST['news_thumbnail']) && (strpos($_POST['news_thumbnail'], ",") == false) && $_POST['news_thumbnail'][0] != "{" && substr($_POST['news_thumbnail'],-8) !== '.youtube')//BC compat
 		{
 			$_POST['news_thumbnail'] = "{e_IMAGE}newspost_images/".$_POST['news_thumbnail'];	
 		}
