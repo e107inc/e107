8011bf2882cb1bcfd7dc6ebcd2755079e7eb3240
Cameron <cameron@e107coders.org>
2014-05-20 16:53:06 -0700

    Initial commit of the highly experimental TinyMce4 CDN plugin.

---
+++ e107_plugins/tinymce4/plugins/e107/mediamanager.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("../../../../class2.php");
+
+$types = array('img','video','glyph');
+
+$bbcode = in_array(e_QUERY,$types) ? e_QUERY : 'img';
+
+header("Location: ".e_ADMIN_ABS.'image.php?mode=main&action=dialog&for='.$_SESSION['media_category'].'&tagid=&iframe=1&bbcode='.$bbcode, true);
+exit; 
+
+
+?>
