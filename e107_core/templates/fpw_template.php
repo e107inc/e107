5b473a1c750e226c473dcb37b23eda11e4469f4b
Moc <tijnkuyper@gmail.com>
2014-08-16 15:55:22 +0200

    #726 - Added CAPTCHA to forgotten password template

--- e107_core/templates/fpw_template.php
+++ e107_core/templates/fpw_template.php
@@ -81,15 +81,16 @@ if(!isset($FPW_TABLE_FOOTER))
 
 $FPW_TEMPLATE['form'] = '
 					<div class="row">
-					<div class="col-sm-12 col-md-8">
-					<p>{FPW_TEXT}</p>
-					<div class="form-group">{FPW_USEREMAIL}</div>
-					<div class="row">
-						<div class="col-xs-12 col-sm-4 col-sm-offset-8">
-						{FPW_SUBMIT}
+						<div class="col-sm-12 col-md-8">
+						<p>{FPW_TEXT}</p>
+						<div class="form-group">{FPW_USEREMAIL}</div>
+						<div class="form-group">{FPW_CAPTCHA_IMG}{FPW_CAPTCHA_INPUT}</div>
+							<div class="row">	
+								<div class="col-xs-12 col-sm-4 col-sm-offset-8">
+								{FPW_SUBMIT}
+								</div>
+							</div>		
 						</div>
-					</div>
-					</div>
 					</div>
 					';
 $FPW_TEMPLATE['header'] = '';
