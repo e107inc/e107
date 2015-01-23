9d187a14eebfd0fe1710e30bf12edb4c3eeaff43
Cameron <e107inc@gmail.com>
2015-01-23 15:26:23 -0800

    Added simple e_url.php example.

---
+++ e107_plugins/faqs/e_url.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * e107 Bootstrap CMS
+ *
+ * Copyright (C) 2008-2015 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * 
+ * IMPORTANT: Make sure the redirect script uses the following code to load class2.php: 
+ * 
+ * 	if (!defined('e107_INIT'))
+ * 	{
+ * 		require_once("../../class2.php");
+ * 	}
+ * 
+ */
+ 
+if (!defined('e107_INIT')) { exit; }
+
+// v2.x Standard  - Simple mod-rewrite module. 
+
+class faqs_url // plugin-folder + '_url' 
+{
+	function config() 
+	{
+		$config = array();
+	
+		$config[] = array(
+			'regex'			=> '^faqs/?$',
+			'redirect'		=> '{e_PLUGIN}faqs/faqs.php',
+		);
+		
+		return $config;
+	}
+	
+}
