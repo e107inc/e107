e53090d6109d3d84837eed82b9b5e9972befae29
Cameron <e107inc@gmail.com>
2015-02-04 20:36:56 -0800

    Issue #682 and other 'wrong avatar' issues in chatbox, online-menu etc. New $tp->toAvatar() method introduced.

--- e107_plugins/online/online_menu.php
+++ e107_plugins/online/online_menu.php
@@ -25,7 +25,11 @@ include_lan(e_PLUGIN.'online/languages/'.e_LANGUAGE.'.php');
 
 require_once(e_PLUGIN.'online/online_shortcodes.php');
 
-if (is_readable(THEME.'online_menu_template.php')) 
+if (is_readable(THEME.'templates/online/online_menu_template.php')) 
+{
+	require(THEME.'templates/online/online_menu_template.php');
+} 
+elseif (is_readable(THEME.'online_menu_template.php')) 
 {
 	require(THEME.'online_menu_template.php');
 } 
