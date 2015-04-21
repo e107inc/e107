f2e4eec7d2fe0c7ebc5947e1353aa2b66c701545
GHswitt <se.witt@gmx.net>
2015-04-21 12:02:28 +0200

    German language file for login_menu

---
+++ e107_plugins/login_menu/languages/German.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: e107_plugins/login_menu/languages/German.php,v $
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+define("LOGIN_MENU_L1", "Benutzername: ");
+define("LOGIN_MENU_L2", "Passwort: ");
+define("LOGIN_MENU_L3", "Registrieren");
+define("LOGIN_MENU_L4", "Passwort vergessen?");
+define("LOGIN_MENU_L5", "Willkommen");
+define("LOGIN_MENU_L6", "Automatisch anmelden");
+define("LOGIN_MENU_L7", "Eindeutige User ID nicht erkannt (möglicherweise korruptes Cookie). Bitte mit dem Link unterhalb ausloggen um das Cookie zu löschen.");
+define("LOGIN_MENU_L8", "Ausloggen");
+define("LOGIN_MENU_L9", "Loginfehler");
+define("LOGIN_MENU_L10", "Wartungsmodus ist aktiv - Dies bedeutet normale Besucher werden weitergeleitet zu sitedown.php. Um den Wartungsmodus zu deaktivieren gehe zu Admin/Wartung.");
+define("LOGIN_MENU_L11", "Adminbereich");
+define("LOGIN_MENU_L12", "Einstellungen");
+define("LOGIN_MENU_L13", "Profil");
+define("LOGIN_MENU_L14", "news item");
+define("LOGIN_MENU_L15", "news items");
+define("LOGIN_MENU_L16", "chatbox Beitrag");
+define("LOGIN_MENU_L17", "chatbox Beiträge");
+define("LOGIN_MENU_L18", "Kommentar");
+define("LOGIN_MENU_L19", "Kommentare");
+define("LOGIN_MENU_L20", "Forumsnachricht");
+define("LOGIN_MENU_L21", "Forumsnachrichten");
+define("LOGIN_MENU_L22", "neues Mitglied");
+define("LOGIN_MENU_L23", "neue Mitglieder");
+define("LOGIN_MENU_L24", "Hier klicken um neue Nachrichten zu sehen");
+define("LOGIN_MENU_L25", "Seit deinem letztem Besuch gab es");
+define("LOGIN_MENU_L26", "nein");
+define("LOGIN_MENU_L27", "und");
+define("LOGIN_MENU_L28", "Anmelden");
+
+//define("LOGIN_MENU_L29", "new article");
+//define("LOGIN_MENU_L30", "new articles");
+
+// New config options
+define('LOGIN_MENU_L31', 'Zeige neue Nachrichten');
+//define('LOGIN_MENU_L32', 'Show New Article Posts');
+//define('LOGIN_MENU_L33', 'Show New Chatbox Posts');
+define('LOGIN_MENU_L34', 'Zeige neue Kommentare');
+//define('LOGIN_MENU_L35', 'Show New Forum Posts');
+define('LOGIN_MENU_L36', 'Zeige neue Mitglieder');
+
+
+define('LOGIN_MENU_L39', 'Adminbereich verlassen');
+define("LOGIN_MENU_L40", "Aktivierungsmail erneut senden");
+define("LOGIN_MENU_L41", "Login Menu Settings");
+
+//new v0.8
+define('LOGIN_MENU_L37', 'Zeige');
+define('LOGIN_MENU_L38', 'Login menu - Additional Links');
+
+define('LOGIN_MENU_L42', 'Login menu - Recent core additions');
+define('LOGIN_MENU_L43', 'Position');
+define('LOGIN_MENU_L44', 'missing link title');
+define('LOGIN_MENU_L45', 'link(s) -');
+define('LOGIN_MENU_L45a', ''); //pre
+define('LOGIN_MENU_L45b', 'plugin'); //post
+define('LOGIN_MENU_L46', 'recent items -');
+define('LOGIN_MENU_L47', 'Login menu - Recent plugin additions');
+define('LOGIN_MENU_L48', 'Menu Config');
+define('LOGIN_MENU_L49', 'Email: ');
+define('LOGIN_MENU_L50', 'Benutzername oder Email: '); 
+define("LOGIN_MENU_L51", "Anmelden");
+
+?>
