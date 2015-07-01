5bf187031bb4022f0e435fb4fd9a5fca33f306e4
MikeyGMT <mikeygmt@gmail.com>
2015-07-01 23:57:22 +0100

    Revert "Insall: Replace LANINS_035 with LAN_CONTINUE and Replace HTML with BB Code"
    This reverts commit fdfac1984807d9049604e670f61a9b3135c44a1e.

--- install.php
+++ install.php
@@ -401,7 +401,7 @@ class e_install
 		$e_forms->start_form("language_select", $_SERVER['PHP_SELF'].($_SERVER['QUERY_STRING'] == "debug" ? "?debug" : ""));
 		$e_forms->add_select_item("language", $this->get_languages(), "English");
 		$this->finish_form();
-		$this->add_button("start", LAN_CONTINUE);
+		$this->add_button("start", LANINS_035);
 		$output = "
 			<div style='text-align: center;'>
 				<div class='alert alert-info alert-block'>
@@ -495,7 +495,7 @@ class e_install
 
 
 		$this->finish_form();
-		$this->add_button("submit", LAN_CONTINUE);
+		$this->add_button("submit", LANINS_035);
 		$this->template->SetTag("stage_content", $page_info.$e_forms->return_form());
 		$this->logLine('Stage 2 completed');
 	}
@@ -617,7 +617,7 @@ class e_install
 			</div>
 			\n";
 			$e_forms->add_plain_html($output);
-			$this->add_button("submit", LAN_CONTINUE);
+			$this->add_button("submit", LANINS_035);
 			$this->template->SetTag("stage_title", LANINS_040);
 		}
 		else
@@ -712,12 +712,12 @@ class e_install
 			{
 
 				// $page_content .= "<br /><br />".LANINS_045."<br /><br />";
-				$this->add_button("submit", LAN_CONTINUE);
+				$this->add_button("submit", LANINS_035);
 				$alertType = 'success';
 			}
 			else 
 			{
-				$this->add_button("back", LAN_CONTINUE);
+				$this->add_button("back", LANINS_035);
 			}
 			$head = $page_content;
 		}
@@ -831,7 +831,7 @@ class e_install
 		}
 		elseif ($perms_pass && !$version_fail && $xml_installed)
 		{
-			$this->add_button("continue_install", LAN_CONTINUE);
+			$this->add_button("continue_install", LANINS_020);
 		}
 		
 		$permColor	= ($perms_pass == true) ? "text-success" : "text-error";
@@ -942,7 +942,7 @@ class e_install
 			\n";
 		$e_forms->add_plain_html($output);
 		$this->finish_form();
-		$this->add_button("submit", LAN_CONTINUE);
+		$this->add_button("submit", LANINS_035);
 		$this->template->SetTag("stage_content", $e_forms->return_form());
 		$this->logLine('Stage 5 completed');
 	}
@@ -1107,7 +1107,7 @@ class e_install
 			\n";
 		$e_forms->add_plain_html($output);
 		$this->finish_form();
-		$this->add_button("submit", LAN_CONTINUE);
+		$this->add_button("submit", LANINS_035);
 		$this->template->SetTag("stage_content", $e_forms->return_form());
 		$this->logLine('Stage 6 completed');
 	}
@@ -1228,7 +1228,7 @@ class e_install
 		$e_forms->start_form("confirmation", $_SERVER['PHP_SELF'].($_SERVER['QUERY_STRING'] == "debug" ? "?debug" : ""));
 		$page = '<div class="alert alert-success">'.nl2br(LANINS_057).'</div>';
 		$this->finish_form();
-		$this->add_button("submit", LAN_CONTINUE);
+		$this->add_button("submit", LANINS_035);
 
 
 
@@ -1290,7 +1290,7 @@ class e_install
 					
 					$page .= "<p class='text-warning'>".$htaccessError."</p>";		
 				}	
-				$this->add_button('submit', LAN_CONTINUE);
+				$this->add_button('submit', LANINS_035);
 			}
 		 
 		$this->finish_form();
