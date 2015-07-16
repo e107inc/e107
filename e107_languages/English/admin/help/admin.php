5c954c6ae7990a066d55645ffd447b07a7bae1c8
Cameron <e107inc@gmail.com>
2015-07-16 12:38:36 -0700

    Issue #1138 - Bootstrap3 remove light theme text.

--- e107_languages/English/admin/help/admin.php
+++ e107_languages/English/admin/help/admin.php
@@ -13,7 +13,7 @@ if (!defined('e107_INIT')) { exit; }
 $caption = "Dashboard";
 $text = "This page provides you with an overview of site activity.<br /><br />";
 
-if(e107::getPref('admincss')=='admin_dark.css')
+if(e107::getPref('admincss')=='admin_dark.css' && deftrue('BOOTSTRAP') !== 3)
 {
 	$text .= "<small>Theme too dark? <a class='btn btn-default btn-xs btn-mini btn-inverse' href='".e_ADMIN."theme.php?mode=admin'>Change it here</a></small>";
 	
