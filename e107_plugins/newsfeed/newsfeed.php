524abd08da3dd0a7ac64babe45632888fb7d7bbf
Jimmi08 <janamicek@gmail.com>
2016-01-13 23:44:52 +0100

    e_frontpage.php - doesn't work for newsfeed plugin

--- e107_plugins/newsfeed/newsfeed.php
+++ e107_plugins/newsfeed/newsfeed.php
@@ -14,7 +14,11 @@
  * $Author$
  *
 */
-require_once('../../class2.php');
+if(!defined('e107_INIT'))
+{
+    require_once('../../class2.php');
+}
+
 if (!e107::isInstalled('newsfeed')) 
 {
 	header("location:".e_BASE."index.php");
