10ebaf6b87b1c4903f2919106b6dc21ba62feb3d
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-15 12:24:20 +0100

    Code clean-up on newsletter plugin

--- e107_plugins/newsletter/languages/English.php
+++ e107_plugins/newsletter/languages/English.php
@@ -2,24 +2,19 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
- * Plugin - newsfeeds
- *
- * $Source: /cvs_backup/e107_0.8/e107_plugins/newsletter/languages/English.php,v $
- * $Revision$
- * $Date$
- * $Author$
+ * Plugin - newsletter
  *
 */
 
 define("NLLAN_MENU_CAPTION", "Newsletter");
 
-define("NLLAN_48", "You are subscribed from this newsletter - if you wish to unsubscribe please click the button below.");
-define("NLLAN_49", "Are you sure you want to un-subscribe from this newsletter?");
-define("NLLAN_50", "Click button to subscribe ( your subscription address is");
+define("NLLAN_48", "You are subscribed to this newsletter - if you wish to unsubscribe please click the button below.");
+define("NLLAN_49", "Are you sure you want to unsubscribe from this newsletter?");
+define("NLLAN_50", "Click button to subscribe (your subscription address is");
 define("NLLAN_51", "Unsubscribe");
 define("NLLAN_52", "Subscribe");
 define("NLLAN_53", "Are you sure you want to subscribe to this newsletter?");
@@ -28,7 +23,7 @@ define("NLLAN_67", "Archive overview");
 define("NLLAN_68", "Invalid parameter defined");
 define("NLLAN_69", "No sent newsletters available.");
 define("NLLAN_70", "Selected newsletter does not exist");
-define("NLLAN_71", "Back"); //TODO User Generic LAN
+//define("NLLAN_71", "Back"); 
 define("NLLAN_72", "View archive");
 
 ?>
--- e107_plugins/newsletter/nl_archive.php
+++ e107_plugins/newsletter/nl_archive.php
@@ -2,22 +2,13 @@
 /*
  * e107 website system
  *
- * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  * Administration - Newsletter archive
  *
 */
-
-
-/**
- *	e107 Newsletter plugin
- *
- *	@package	e107_plugins
- *	@subpackage	newsletter
- */
-
 
 require_once('../../class2.php');
 if (!e107::isInstalled('newsletter') || !ADMIN)
