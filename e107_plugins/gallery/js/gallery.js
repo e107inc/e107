0708c060fa4e942267dbab0b699d89fd8d9327b6
Lóna Lore <lonalore@freemail.hu>
2016-03-30 16:01:16 +0200

    Fixed: proper usage of default values in JS.

--- e107_plugins/gallery/js/gallery.js
+++ e107_plugins/gallery/js/gallery.js
@@ -17,34 +17,34 @@ var e107 = e107 || {'settings': {}, 'behaviors': {}};
 			$(context).find("a[" + pPhook + "^='prettyPhoto']").once('gallery-prettyPhoto').prettyPhoto(
 				{
 					hook: pPhook,
-					animation_speed: pPhoto.animation_speed || 'fast', // fast/slow/normal
-					slideshow: pPhoto.slideshow || 5000, // false OR interval time in ms
-					autoplay_slideshow: pPhoto.autoplay_slideshow || false, // true/false
-					opacity: pPhoto.opacity || 0.80, // Value between 0 and 1
-					show_title: pPhoto.show_title || false, // true/false
-					allow_resize: pPhoto.allow_resize || false, // Resize the photos bigger than viewport. true/false
-					default_width: pPhoto.default_width || 500,
-					default_height: pPhoto.default_height || 344,
-					counter_separator_label: pPhoto.counter_separator_label || '/', // The separator for the gallery counter 1 "of" 2
-					theme: pPhoto.theme || 'pp_default', // light_rounded / dark_rounded / light_square / dark_square / facebook
-					horizontal_padding: pPhoto.horizontal_padding || 20, // The padding on each side of the picture
-					hideflash: pPhoto.hideflash || false, // Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto
-					wmode: pPhoto.wmode || 'opaque', // Set the flash wmode attribute
-					autoplay: pPhoto.autoplay || false, // Automatically start videos: true/false
-					modal: pPhoto.modal || false, // If set to true, only the close button will close the window
-					deeplinking: pPhoto.deeplinking || false, // Allow prettyPhoto to update the url to enable deeplinking.
-					overlay_gallery: pPhoto.overlay_gallery || false, // If set to true, a gallery will overlay the fullscreen image on mouse over
-					keyboard_shortcuts: pPhoto.keyboard_shortcuts || false, // Set to false if you open forms inside prettyPhoto
-					ie6_fallback: pPhoto.ie6_fallback || false, // true/false
-					markup: decodeURIComponent(pPhoto.markup) || null,
-					gallery_markup: decodeURIComponent(pPhoto.gallery_markup) || null,
-					image_markup: decodeURIComponent(pPhoto.image_markup) || null,
-					flash_markup: pPhoto.flash_markup || null,
-					quicktime_markup: pPhoto.quicktime_markup || null,
-					iframe_markup: pPhoto.iframe_markup || null,
-					inline_markup: pPhoto.inline_markup || null,
-					custom_markup: pPhoto.custom_markup || null,
-					social_tools: decodeURIComponent(pPhoto.social_tools) || null,
+					animation_speed: pPhoto.animation_speed, // fast/slow/normal
+					slideshow: pPhoto.slideshow, // false OR interval time in ms
+					autoplay_slideshow: pPhoto.autoplay_slideshow, // true/false
+					opacity: pPhoto.opacity, // Value between 0 and 1
+					show_title: pPhoto.show_title, // true/false
+					allow_resize: pPhoto.allow_resize, // Resize the photos bigger than viewport. true/false
+					default_width: pPhoto.default_width,
+					default_height: pPhoto.default_height,
+					counter_separator_label: pPhoto.counter_separator_label, // The separator for the gallery counter 1 "of" 2
+					theme: pPhoto.theme, // light_rounded / dark_rounded / light_square / dark_square / facebook
+					horizontal_padding: pPhoto.horizontal_padding, // The padding on each side of the picture
+					hideflash: pPhoto.hideflash, // Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto
+					wmode: pPhoto.wmode, // Set the flash wmode attribute
+					autoplay: pPhoto.autoplay, // Automatically start videos: true/false
+					modal: pPhoto.modal, // If set to true, only the close button will close the window
+					deeplinking: pPhoto.deeplinking, // Allow prettyPhoto to update the url to enable deeplinking.
+					overlay_gallery: pPhoto.overlay_gallery, // If set to true, a gallery will overlay the fullscreen image on mouse over
+					keyboard_shortcuts: pPhoto.keyboard_shortcuts, // Set to false if you open forms inside prettyPhoto
+					ie6_fallback: pPhoto.ie6_fallback, // true/false
+					markup: pPhoto.markup,
+					gallery_markup: pPhoto.gallery_markup,
+					image_markup: pPhoto.image_markup,
+					flash_markup: pPhoto.flash_markup,
+					quicktime_markup: pPhoto.quicktime_markup,
+					iframe_markup: pPhoto.iframe_markup,
+					inline_markup: pPhoto.inline_markup,
+					custom_markup: pPhoto.custom_markup,
+					social_tools: pPhoto.social_tools,
 					changepicturecallback: function ()
 					{
 						var $ppContent = $(".pp_content");
