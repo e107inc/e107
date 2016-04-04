8091361baf986c67ad397f133222aaf346d43a82
Cameron <e107inc@gmail.com>
2016-04-03 17:01:05 -0700

    Private Messenger: Moved shortcode file back to avoid upgrade issues with old files. Please delete pm/shortcodes folder if it exists. Removed old attachments folder.

--- e107_plugins/pm/private_msg_menu.php
+++ e107_plugins/pm/private_msg_menu.php
@@ -90,7 +90,7 @@ require_once(e_PLUGIN.'pm/pm_func.php');
 
 e107::getScParser();
 
-require_once(e_PLUGIN.'pm/shortcodes/batch/pm_shortcodes.php');
+require_once(e_PLUGIN.'pm/pm_shortcodes.php');
 
 //setScVar('pm_handler_shortcodes','pmPrefs', $pm_prefs);
 $pmManager = new pmbox_manager($pm_prefs);
