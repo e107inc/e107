87e4f26d7bca59789e7cc32bdcfe52d91ff33b79
Cameron <e107inc@gmail.com>
2016-04-09 10:58:03 -0700

    Added error message for ADMINs if a LAN is missing.

--- e107_core/shortcodes/single/lan.php
+++ e107_core/shortcodes/single/lan.php
@@ -8,4 +8,8 @@ function lan_shortcode($parm = '')
 	{
 		return constant($lan);
 	}
+	elseif(ADMIN)
+	{
+		return "<span class='alert alert-danger'><strong>".$parm ."</strong> is undefined</span>"; // debug info
+	}
 }
