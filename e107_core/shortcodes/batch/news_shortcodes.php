01489804c80dfebda9d15d2586d6c3352331afb3
rica-carv <rica-carv@users.noreply.github.com>
2016-04-16 22:59:33 +0100

    Update news_shortcodes.php

--- e107_core/shortcodes/batch/news_shortcodes.php
+++ e107_core/shortcodes/batch/news_shortcodes.php
@@ -324,9 +324,16 @@ class news_shortcodes extends e_shortcode
 	function sc_newscategory($parm=null)
 	{
 		$category_name = e107::getParser()->toHTML($this->news_item['category_name'], FALSE ,'defs');
-		$category = array('id' => $this->news_item['category_id'], 'name' => $this->news_item['category_sef'] );
-		$categoryClass = varset($GLOBALS['NEWS_CSSMODE'],'');
-		return "<a class='".$categoryClass."_category' style='".(isset($this->param['catlink']) ? $this->param['catlink'] : "#")."' href='".e107::getUrl()->create('news/list/category', $category)."'>".$category_name."</a>";
+		if($parm == 'nolink')
+			{
+				return $category_name;
+			}
+			else
+			{
+				$category = array('id' => $this->news_item['category_id'], 'name' => $this->news_item['category_sef'] );
+				$categoryClass = varset($GLOBALS['NEWS_CSSMODE'],'');
+				return "<a class='".$categoryClass."_category' style='".(isset($this->param['catlink']) ? $this->param['catlink'] : "#")."' href='".e107::getUrl()->create('news/list/category', $category)."'>".$category_name."</a>";
+			}
 	}
 
 	function sc_newsdate($parm)
