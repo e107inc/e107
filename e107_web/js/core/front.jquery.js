b5beba39c421e1120b7d7db2e07c08b5e4e66039
Michael Waskosky <mike@tetonapps.com>
2016-04-22 21:29:00 -0600

    boostrap modal options for frontend
    A way to configure modal options documented here http://getbootstrap.com/javascript/#modals-methods

--- e107_web/js/core/front.jquery.js
+++ e107_web/js/core/front.jquery.js
@@ -402,7 +402,6 @@ $(document).ready(function()
 		/*  Bootstrap Modal window within an iFrame for frontend */
 		$('.e-modal').on('click', function(e)
 		{
-
 			e.preventDefault();
 
             if($(this).attr('data-cache') == 'false')
@@ -414,7 +413,21 @@ $(document).ready(function()
 
 			var url 		= $(this).attr('href');
 			var caption  	= $(this).attr('data-modal-caption');
+			var backdrop  	= $(this).attr('data-modal-backdrop');
+			var keyboard  	= $(this).attr('data-modal-keyboard');
 			var height 		= ($(window).height() * 0.7) - 120;
+
+			var modalOptions = {show: true};
+			
+			if(backdrop !== undefined)
+			{
+				modalOptions['backdrop'] = backdrop;
+			}
+
+			if(keyboard !== undefined)
+			{
+				modalOptions['keyboard'] = keyboard;
+			}
 
             if(caption === undefined)
             {
@@ -428,7 +441,7 @@ $(document).ready(function()
 
     		$('.modal-body').html('<div><iframe id="e-modal-iframe" width="100%" height="'+height+'px" frameborder="0" scrolling="auto" style="display:block;" allowtransparency="true" src="' + url + '"></iframe></div>');
     		$('.modal-caption').html(caption + ' <i id="e-modal-loading" class="fa fa-spin fa-spinner"></i>');
-    		$('.modal').modal('show');
+    		$('.modal').modal(modalOptions);
 
     		$("#e-modal-iframe").on("load", function () {
 				 $('#e-modal-loading').hide();
