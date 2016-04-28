2b61ac19d922873c4eadbbf598fbae35a1b450fc
tgtje <the_guardian2@hotmail.com>
2016-04-28 15:16:06 +0200

    bug rework, array translation causes to login/user on NAVBAR to disappear

--- e107_themes/bootstrap3/languages/English_admin.php
+++ e107_themes/bootstrap3/languages/English_admin.php
@@ -14,10 +14,5 @@ define("LAN_THEMEPREF_03", "Bootswatch Styles:");
 define("LAN_THEMEPREF_04", "Site Name");
 define("LAN_THEMEPREF_05", "Logo");
 define("LAN_THEMEPREF_06", "Logo &amp; Site Name");
-define("LAN_THEMEPREF_07", "left");
-define("LAN_THEMEPREF_08", "right");
-define("LAN_THEMEPREF_09", "top");
-define("LAN_THEMEPREF_10", "bottom");
- 
 
 ?>
