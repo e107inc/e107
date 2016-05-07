0376fc43f9e521c95dd9d611aca8b96db759ec75
Cameron <e107inc@gmail.com>
2016-05-07 09:17:37 -0700

    Closes #1625 - e_sitelink added to Plugin Builder and mega-menu example added.

--- e107_plugins/_blank/e_header.php
+++ e107_plugins/_blank/e_header.php
@@ -14,11 +14,11 @@
 if (!defined('e107_INIT')) { exit; }
 
 
-if(USER_AREA) // do not include JS in the admin area.
+if(USER_AREA) // prevents inclusion of JS/CSS/meta in the admin area.
 {
-	e107::js('_blank', 'js/blank.js');      // loads e107_plugins/_blank/js/blank.js
-	e107::css('_blank', 'css/blank.css');    // loads e107_plugins/_blank/css/blank.css
-	e107::meta('keywords', 'blank,words');
+	e107::js('_blank', 'js/blank.js');      // loads e107_plugins/_blank/js/blank.js on every page.
+	e107::css('_blank', 'css/blank.css');    // loads e107_plugins/_blank/css/blank.css on every page
+	e107::meta('keywords', 'blank,words');   // sets meta keywords on every page.
 }
 
 
