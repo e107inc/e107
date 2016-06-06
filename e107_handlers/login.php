d0a870d1a1c99ff6df1387a0a8c52bc718efef59
Cameron <e107inc@gmail.com>
2016-06-06 13:55:14 -0700

    Issue #1684 - login with email flag.

--- e107_handlers/login.php
+++ e107_handlers/login.php
@@ -371,7 +371,7 @@ class userlogin
 		// User is in DB here
 		$this->userData = e107::getDb()->fetch();		// Get user info
 		$this->userData['user_perms'] = trim($this->userData['user_perms']);
-		$this->lookEmail = $this->lookEmail && ($username == $this->userData['user_email']);		// Know whether login name or email address used now
+		$this->lookEmail = ($username == $this->userData['user_email']) ? 1 : 0;		// Know whether login name or email address used now
 		
 		return TRUE;
 	}
