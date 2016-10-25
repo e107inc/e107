3ba7181969c4f2e8775fcff2184e19645f6422e8
Cameron <e107inc@gmail.com>
2016-10-25 16:24:53 -0700

    Fixes #1780 Plugin Manager now alerts user if a newer version of an installed plugin is available from e107.org

--- e107_handlers/e_marketplace.php
+++ e107_handlers/e_marketplace.php
@@ -223,6 +223,53 @@ class e_marketplace
 		$this->adapter = null;
 		//echo "Adapter destroyed", PHP_EOL;
 	}
+
+
+	public function getVersionList($type='plugin')
+	{
+		$cache = e107::getCache();
+		$cache->setMD5('_', false);
+
+		$tag = 'Versions_'.$type;
+
+		if($data = $cache->retrieve($tag,(60 * 12), true, true))
+		{
+			return e107::unserialize($data);
+		}
+
+	//	$mp = $this->getMarketplace();
+	//	$mp->generateAuthKey($e107SiteUsername, $e107SiteUserpass);
+		e107::getDebug()->log("Retrieving ".$type." version list from e107.org");
+
+		$xdata = $this->call('getList', array(
+			'type' => $type,
+			'params' => array('limit' => 200, 'search' => null, 'from' => 0)
+		));
+
+		$arr = array();
+
+		if(!empty($xdata['data']))
+		{
+
+			foreach($xdata['data'] as $row)
+			{
+				$k = $row['folder'];
+				$arr[$k] = $row;
+			}
+
+		}
+
+// print_a($xdata['data']);
+
+		$data = e107::serialize($arr);
+		$cache->set($tag, $data, true, null, true);
+
+		return $arr;
+
+	}
+
+
+
 }
 
 abstract class e_marketplace_adapter_abstract
