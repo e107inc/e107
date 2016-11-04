469a7ce7ccba574fec0f1380d9ddfd98c419d73f
Gábor <yesszus@gmail.com>
2016-11-04 17:23:51 +0100

    Create Hungarian_admin.php

---
+++ e107_themes/bootstrap3/languages/Hungarian_admin.php
@@ -0,0 +1,26 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/05/06 12:21:54
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_THEMEPREF_00", "Logó");
+define("LAN_THEMEPREF_01", "Megjelenítés");
+define("LAN_THEMEPREF_02", "Regisztráció/Bejelentkezési Terület");
+define("LAN_THEMEPREF_03", "Bootswatch Stilus");
+define("LAN_THEMEPREF_04", "Oldal Neve");
+define("LAN_THEMEPREF_05", "Logó");
+define("LAN_THEMEPREF_06", "Logó &amp; Oldal Név");
+define("LAN_THEMEPREF_07", "balra");
+define("LAN_THEMEPREF_08", "jobbra");
+define("LAN_THEMEPREF_09", "felül");
+define("LAN_THEMEPREF_10", "alul");
+
+
+?>
