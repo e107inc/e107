c9f1d419ca320d13418a5adb3e9d41507d6d527c
lonalore <lonalore@freemail.hu>
2016-11-10 12:22:40 +0100

    Flexpanel layouts with preview images.

---
+++ e107_admin/includes/layouts/flexpanel_one_col.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Layout for "flexpanel" admin dashboard style.
+ */
+
+$FLEXPANEL_LAYOUT = '
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-01">
+			{MENU_AREA_01}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-02">
+			{MENU_AREA_02}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-03">
+			{MENU_AREA_03}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-04">
+			{MENU_AREA_04}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-05">
+			{MENU_AREA_05}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-06">
+			{MENU_AREA_06}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-07">
+			{MENU_AREA_07}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-08">
+			{MENU_AREA_08}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-09">
+			{MENU_AREA_09}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-10">
+			{MENU_AREA_10}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-11">
+			{MENU_AREA_11}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-12">
+			{MENU_AREA_12}
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="draggable-panels bg-blue" id="menu-area-13">
+			{MENU_AREA_13}
+		</div>
+	</div>
+</div>
+';
