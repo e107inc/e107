cb5aff36dccbd69891623425348ef0997330e097
rica-carv <rica-carv@users.noreply.github.com>
2016-11-20 18:09:10 +0000

    Update forum_viewforum.php
    Permisions check should be on post, not thread.
    With thread, the "create new topic" is displayed in the dropdown, and it shouldn't if user is not allowed to create posts....

--- e107_plugins/forum/forum_viewforum.php
+++ e107_plugins/forum/forum_viewforum.php
@@ -280,7 +280,8 @@ if ($pages)
 	}
 }
 
-if($forum->checkPerm($forumId, 'thread')) //new thread access only.
+//if($forum->checkPerm($forumId, 'thread')) //new thread access only.
+if($forum->checkPerm($forumId, 'post')) //new thread access only.
 {
 		$forumSCvars['ntUrl']= e107::url('forum','post')."?f=nt&amp;id=". $forumId;
 /*--
