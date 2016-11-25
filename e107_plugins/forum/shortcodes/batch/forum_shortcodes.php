5690e901db345ff1bcb58dcc5f01cd92697e312e
rica-carv <rica-carv@users.noreply.github.com>
2016-11-25 21:42:29 +0000

    Forum userlist shortcode wrapping
    Removed hardcoded html and use wrapping instead, for userlist shortcode...

--- e107_plugins/forum/shortcodes/batch/forum_shortcodes.php
+++ e107_plugins/forum/shortcodes/batch/forum_shortcodes.php
@@ -138,11 +138,10 @@ class forum_shortcodes extends e_shortcode
 
 	function sc_userlist()
 	{
-		if(!defined('e_TRACKING_DISABLED'))
+		if(defined('e_TRACKING_DISABLED'))
 		{
-		// String candidate for USERLIST wrapper
-			$text = LAN_FORUM_0036.": ";
-
+			return;
+		}
 			global $listuserson;
 			$c = 0;
 			if(is_array($listuserson))
@@ -157,9 +156,6 @@ class forum_shortcodes extends e_shortcode
 				}
 
 			}
-		// String candidate for USERLIST wrapper
-			$text .= "<br /><a rel='external' href='".e_BASE."online.php'>".LAN_FORUM_0037."</a> ".LAN_FORUM_0038;
-		}
 		  return $text;
 	}
 
