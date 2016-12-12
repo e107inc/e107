366d3fe5cf9a4f6869051c5a15327d1c835a3070
Cameron <e107inc@gmail.com>
2016-12-12 08:11:41 -0800

    Styling fixes. Debug timer totals fix.

--- e107_handlers/db_debug_class.php
+++ e107_handlers/db_debug_class.php
@@ -412,11 +412,30 @@ class e107_db_debug {
 		$aSum['%Time']=$totTime ? number_format(100.0 * ($aSum['Time'] / $totTime), 0) : 0;
 		$aSum['%DB Time']=$db_time ? number_format(100.0 * ($aSum['DB Time'] / $db_time), 0) : 0;
 		$aSum['%DB Count']=($sql->db_QueryCount()) ? number_format(100.0 * ($aSum['DB Count'] / ($sql->db_QueryCount())), 0) : 0;
-		$aSum['Time']=number_format($aSum['Time']*1000.0, 1);
+		$aSum['Time']=number_format($aSum['Time'] * 1000.0, 1);
 		$aSum['DB Time']=number_format($aSum['DB Time']*1000.0, 1);
 
-		$text .= "<tr><td class='fcaption'><b>".implode("</b>&nbsp;</td><td class='fcaption' style='text-align:right'><b>", $aSum)."</b>&nbsp;</td><td class='fcaption'>&nbsp;</td></tr>\n";
-		$text .= "\n</table><br />\n";
+
+		$text .= "<tr>
+		<td class='fcaption'>&nbsp;</td>
+		<td class='fcaption' style='text-align:right'><b>Total</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['%Time']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['%DB Time']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['%DB Count']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['Time']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['DB Time']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$aSum['DB Count']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$tMarker['Memory']."</b></td>
+		<td class='fcaption' style='text-align:right'><b>".$tMarker['OB Lev']."</b></td>
+
+		</tr>
+		";
+
+
+	//	$text .= "<tr><td class='fcaption'><b>".implode("</b>&nbsp;</td><td class='fcaption' style='text-align:right'><b>", $aSum)."</b>&nbsp;</td><td class='fcaption'>&nbsp;</td></tr>\n";
+
+			$text .= "\n</table><br />\n";
+
 
 		//
 		// Stats by Table
@@ -461,7 +480,7 @@ class e107_db_debug {
 		$aSum['%DB Time']=$db_time ? number_format(100.0 * ($aSum['DB Time'] / $db_time), 0) : 0;
 		$aSum['%DB Count']=($sql->db_QueryCount()) ? number_format(100.0 * ($aSum['DB Count'] / ($sql->db_QueryCount())), 0) : 0;
 		$aSum['DB Time']=number_format($aSum['DB Time']*1000.0, 1);
-		$text .= "<tr><td class='fcaption'>".implode("&nbsp;</td><td class='fcaption' style='text-align:right'>", array_values($aSum))."&nbsp;</td></tr>\n";
+		$text .= "<tr><td class='fcaption'><b>".implode("&nbsp;</td><td class='fcaption' style='text-align:right'><b>", array_values($aSum))."&nbsp;</b></td></tr>\n";
 		$text .= "\n</table><br />\n";
 
 		return $text;
