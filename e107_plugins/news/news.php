c69a303bce66ba93648cdd168cad231f9ea6a8d4
Cameron <e107inc@gmail.com>
2016-12-14 11:45:39 -0800

    Improved handling of missing news items.

--- e107_plugins/news/news.php
+++ e107_plugins/news/news.php
@@ -1004,9 +1004,17 @@ class news_front
 		//	$action = 'default';
 
 			//XXX item not found, redirect to avoid messing up search-engine data.
-			$defaultUrl = e107::getUrl()->create('news/list/items');
-			e107::getRedirect()->go($defaultUrl, null, 301);
-			exit;
+		//	e107::getRedirect()->go(null, true, 404);
+			header("HTTP/1.0 404 Not Found",true,404);
+			require_once(e_LANGUAGEDIR.e_LANGUAGE."/lan_error.php");
+			$text = e107::getMessage()->setTitle(LAN_ERROR_7, E_MESSAGE_INFO)->addInfo("Perhaps you're looking for one of the news items below?")->render();
+
+			$this->action = 'all';
+		//	$defaultUrl = e107::getUrl()->create('news/list/items');
+			$text .= $this->renderListTemplate();
+
+
+			return $text;
 
 		}
 
