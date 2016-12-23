b79aa791349917c423a905bb79faadd8f6ac8ba9
Cameron <e107inc@gmail.com>
2016-12-23 08:53:47 -0800

    Fix for srcSet sizing. Voux theme alignment fixes.

--- e107_themes/voux/theme_shortcodes.php
+++ e107_themes/voux/theme_shortcodes.php
@@ -206,7 +206,7 @@ class theme_shortcodes extends e_shortcode
 		$text = '
 		
 		<ul class="nav navbar-nav navbar-right'.$direction.'">
-		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">{SETIMAGE: w=20} {USER_AVATAR: shape=circle} '. USERNAME.' <b class="caret"></b></a>
+		<li class="dropdown"><a href="#" class="voux-nav-avatar dropdown-toggle" data-toggle="dropdown">{SETIMAGE: w=20&h=20} {USER_AVATAR: shape=circle} <b class="caret"></b></a>
 		<ul class="dropdown-menu">
 		<li>
 			<a href="{LM_USERSETTINGS_HREF}"><span class="glyphicon glyphicon-cog"></span> '.LAN_SETTINGS.'</a>
