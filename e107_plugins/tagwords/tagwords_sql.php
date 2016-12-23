7e3e26578c49c03e15ba0a03682ee617bc834cdb
Cameron <e107inc@gmail.com>
2016-12-23 08:58:02 -0800

    Tagwords removed from core. Use TagCloud instead.

--- e107_plugins/tagwords/tagwords_sql.php
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tagwords (
-	`tag_id` int(10) unsigned NOT NULL auto_increment,
-	`tag_type` varchar(100) NOT NULL default '',
-	`tag_itemid` int(10) unsigned NOT NULL default '0',
-	`tag_word` varchar(255) NOT NULL default '',
-	PRIMARY KEY (`tag_id`),
-	KEY `tag_word` (`tag_word`)
-) ENGINE=MyISAM AUTO_INCREMENT=1;
