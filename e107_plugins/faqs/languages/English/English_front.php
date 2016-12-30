5c42ba342703309201dd84267284cb5adda2683c
MikeyGMT <mikeygmt@gmail.com>
2016-12-30 19:48:05 +0000

    Text To LANS #6 - FAQ
    Text To LANS #6 - FAQ
    Taking care here - there is more to do.
    Cannot see where to test some of the legacy code.
    Seems to work on Bootstrap and e107v4 front end.

--- e107_plugins/faqs/languages/English/English_front.php
+++ e107_plugins/faqs/languages/English/English_front.php
@@ -1,12 +1,23 @@
 <?php
-	/*
-	* Copyright (c) e107 Inc 2008-2015 - e107.org,
-	* Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
-	*
-	* 'FAQ plugin' global language definitions
-	*/
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * FAQ Plugin Global Language Definitions
+ *
+*/
 
 define("LAN_FAQS_TAGS", "Tags");
 define("LAN_FAQS_FILTER_ACTIVE", "Filter Active");
 define("LAN_FAQS_NONE_AVAILABLE", "There are currently no FAQs available.");
 define("LAN_FAQS_ASKQUESTION_AFTER", "Thank you. Your question has been saved and will be answered as soon as possible.");
+define("LAN_FAQS_LIMIT_REACHED", "You have reached the maximum number of new questions. You may ask more once your existing questions have been answered.");
+define("LAN_FAQS_REMOVE", "Remove");
+define("LAN_FAQS_RESET", "Reset");
+define("LAN_FAQS_X_NOT_FOUND", "[x]: Not found in search results.");
+define("LAN_FAQS_MODERATE_COMMENTS", "moderate comments");
+define("LAN_FAQS_BACK_TO_CATEGORIES", "Back to Categories");
+define("LAN_FAQS_SUBMIT_A_QUESTION", "Submit a Question");
