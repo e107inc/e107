091ae03a949ee70e5a5f2703d5d4607c0787100a
Cameron <e107inc@gmail.com>
2017-01-01 10:32:48 -0800

    PHP Warning Removal

--- e107_core/url/page/url.php
+++ e107_core/url/page/url.php
@@ -116,7 +116,7 @@ class core_page_url extends eUrlConfig
 		return $admin;
 	}
 	
-	public function parse($pathInfo)
+	public function parse($pathInfo, $params = array(), eRequest $request = null, eRouter $router = null, $config = array())
 	{
 		// this config doesn't support parsing, it's done by the module entry script (news.php)
 		// this means News are not available via single entry point if this config is currently active
