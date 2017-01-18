3e99b299dc4038852bb9cb812fa253113167dfe0
Gábor <yesszus@gmail.com>
2017-01-18 17:00:23 +0100

    Update theme_config.php

--- e107_themes/landingzero/theme_config.php
+++ e107_themes/landingzero/theme_config.php
@@ -11,13 +11,12 @@ class theme_config implements e_theme_config
 		// v2.1.4 format.
 
 		$fields = array(
-			'videobackground'       => array('title' => 'Image background for video [1920x1080px]', 'type'=>'image', 'help'=>''),
-			'videomobilebackground' => array('title' => 'Image background for mobile devices', 'type'=>'image', 'help'=>''),
-			'videoposter'           => array('title' => 'First frame of video  [1920x1080px]', 'type'=>'image', 'help'=>''),
-			'videourl'              => array('title' => 'URL path to header video in mp4 format', 'type'=>'text', 'writeParms'=>array('size'=>'xxlarge'),'help'=>''),
-			'usernav_placement'     => array('title' => 'Signup/Login Placement', 'type'=>'dropdown', 'writeParms'=>array('optArray'=>array('top', 'bottom'), 'useValues'=>1)),
-			'cdn'     							=> array('title' => 'CDN', 'type'=>'dropdown', 
-			'writeParms'=>array('optArray'=>array( 'cdnjs' => 'CDNJS (Cloudflare)', 'jsdelivr' => 'jsDelivr' /*, 'local' => 'Local folder'*/)))
+			'videobackground'       => array('title' => LAN_LZ_THEMEPREF_00, 'type'=>'image', 'help'=>''),
+			'videomobilebackground' => array('title' => LAN_LZ_THEMEPREF_01, 'type'=>'image', 'help'=>''),
+			'videoposter'           => array('title' => LAN_LZ_THEMEPREF_02, 'type'=>'image', 'help'=>''),
+			'videourl'              => array('title' => LAN_LZ_THEMEPREF_03, 'type'=>'text', 'writeParms'=>array('size'=>'xxlarge'),'help'=>''),
+			'usernav_placement'     => array('title' => LAN_LZ_THEMEPREF_04, 'type'=>'dropdown', 'writeParms'=>array('optArray'=>array(LAN_LZ_THEMEPREF_05, LAN_LZ_THEMEPREF_06), 'useValues'=>1)),
+			'cdn'   		=> array('title' => 'CDN', 'type'=>'dropdown', 'writeParms'=>array('optArray'=>array( 'cdnjs' => 'CDNJS (Cloudflare)', 'jsdelivr' => 'jsDelivr')))
 		);
 
 		return $fields;
