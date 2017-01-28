96250005d77d4b8fc75937cf72ed362c3757b0a0
Cameron <e107inc@gmail.com>
2017-01-28 08:45:09 -0800

    Issue #2331 Reduce space needed for menu-manager area buttons.

--- e107_languages/English/admin/lan_menus.php
+++ e107_languages/English/admin/lan_menus.php
@@ -73,4 +73,10 @@ define("MENLAN_55", "Menu Layout");
 define("MENLAN_56", "Custom Pages");
 define("MENLAN_57", "Drag-and-Drop Menus");
 define("MENLAN_58", "The Menu-Manager allows you to place and arrange your menus within your theme template. Hover over the sub-areas to modify existing menu items.");
+
+define("MENLAN_59", "Area [x]");
+
+
+
+
 ?>
