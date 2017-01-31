017ee751db5d37e6121db1096d0f59808a11071a
Gábor <yesszus@gmail.com>
2017-01-31 06:49:02 +0100

    Update e_shortcode.php

--- e107_plugins/social/e_shortcode.php
+++ e107_plugins/social/e_shortcode.php
@@ -366,7 +366,7 @@ class social_shortcodes extends e_shortcode
 
 
 			$text = '<div class="social-share btn-group hidden-print '.$dir.'">
-				  <a class="'.$tooltip.' btn btn-dropdown btn-default btn-'.$size.' dropdown-toggle" data-toggle="dropdown" href="#" title="Share">'.$label.'</a>
+				  <a class="'.$tooltip.' btn btn-dropdown btn-default btn-'.$size.' dropdown-toggle" data-toggle="dropdown" href="#" title="'.LAN_SOCIAL_204.'">'.$label.'</a>
 				 
 				  <ul class="dropdown-menu" role="menu" >
 				  
