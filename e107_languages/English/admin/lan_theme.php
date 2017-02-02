cb316c2897d8a1b5eee9dc4574c3d05fb4a85900
Gábor <yesszus@gmail.com>
2017-02-02 17:54:59 +0100

    Update lan_theme.php

--- e107_languages/English/admin/lan_theme.php
+++ e107_languages/English/admin/lan_theme.php
@@ -127,4 +127,3 @@ define("TPVLAN_89", "Apply dashboard preferences to all administrators");
 define("TPVLAN_90","Unable to connect. Please check firewall and/or internet connection.");
 define("TPVLAN_91", "Create a new theme based on");
 define("TPVLAN_92", "New Theme Folder");
-define("TPVLAN_93", "Create New");
