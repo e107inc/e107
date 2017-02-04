3ce12d494258f61837093bf9ead48d885df27290
Cameron <e107inc@gmail.com>
2017-02-04 13:45:11 -0800

    Remove deprecated $HEADER / $FOOTER from bootstrap3 theme to avoid confusing developers.

--- e107_themes/bootstrap3/theme.php
+++ e107_themes/bootstrap3/theme.php
@@ -541,245 +541,9 @@ $LAYOUT['jumbotron_sidebar_right'] =  '
 
 
 /* XXX EVERYTHING BELOW THIS POINT IS UNUSED FOR NOW */
-
-
-
-
-$HEADER['default'] = '
-<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="'.SITEURL.'">{SITENAME}</a>
-          <div class="nav-collapse collapse">
-           {NAVIGATION=main}
-           <div class="pull-right">{BOOTSTRAP_USERNAV}</div>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-<div class="container-fluid">
-	<div class="row-fluid">
-		 <div class="span3">
-           {NAVIGATION|s=side}          
-          {SETSTYLE=menu}
-          {MENU=1}
-        </div><!--/span-->
-		<div class="span9">
-		 {SETSTYLE=default}
-		 	{WMESSAGE}
-';
-
-
-
-$FOOTER['default'] = '
-		 {SETSTYLE=span4}
-      	
-		</div><!--/span-->
-	</div><!--/row-->
-
-<hr>
-
-<footer class="center"> 
-	{SITEDISCLAIMER}
-</footer>
-
-</div><!--/.fluid-container-->';
-
-
-$HEADER['default-home'] = $HEADER['default'];
-
-
-$FOOTER['default-home'] = '
-	
-		 {SETSTYLE=span4}
-		 
-		 <div class="row-fluid">
-            {MENU=2}
-      	</div><!--/row-->
-		 <div class="row-fluid">
-            {MENU=3}
-      	</div><!--/row-->	
-      	
-		</div><!--/span-->
-	</div><!--/row-->
-
-<hr>
-
-<footer class="center"> 
-		{SITEDISCLAIMER} 
-</footer>
-
-</div><!--/.fluid-container-->';
-
-
-
-
-
-
-
-
-
-
-// HERO http://twitter.github.com/bootstrap/examples/hero.html
-//FIXME insert shortcodes while maintaining only bootstrap classes. 
-
-$HEADER['hero'] = '
-
-	 <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="nav-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn">Sign in</button>
-            </form>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-
-      <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">';
-	  
-	  /*
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
-     */
-     
-     
-//FIXME insert shortcodes while maintaining classes. 
-$FOOTER['hero'] = '
- </div>
-
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
-
-      <hr>
-
-      <footer>
-        <p>&copy; Company 2012</p>
-      </footer>
-
-    </div> <!-- /container -->';
-
-
-// Marketing Narrow - http://twitter.github.com/bootstrap/examples/marketing-narrow.html
-//FIXME insert shortcodes while maintaing classes. 
-
-$HEADER['marketing-narrow'] = '
- <div class="container-narrow">
-
-      <div class="masthead">
-        <ul class="nav nav-pills pull-right">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <h3 class="muted">Project name</h3>
-      </div>
-
-      <hr>
-
-      <div class="jumbotron">
-        <h1>Super awesome marketing speak!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <a class="btn btn-large btn-success" href="#">Sign up today</a>   
-';
-
-
-//FIXME insert shortcodes while maintaing classes. 
-
-$FOOTER['marketing-narrow'] = '
-</div>
-
-      <hr>
-
-      <div class="row-fluid marketing">
-        <div class="span6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="span6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="footer">
-        <p>&copy; Company 2012</p>
-      </div>
-
-    </div> <!-- /container -->
-    
-';
-
-
-
-
-
+/**
+* $HEADER AND $FOOTER are deprecated.
+*/
 
 
 /*
@@ -789,9 +553,9 @@ $FOOTER['marketing-narrow'] = '
 
  */
 
+/*
  
- 
-$HEADER['docs'] = <<<TMPL
+$LAYOUT['docs'] = <<<TMPL
 
   <!-- Navbar
     ================================================== -->
@@ -836,13 +600,9 @@ $HEADER['docs'] = <<<TMPL
       <div class="span9">
 
 
+		{---}
 
 
-
-TMPL;
-
-
-$FOOTER['docs'] = <<<TMPL
  	</div>
 	  </div>
  </div>
@@ -869,7 +629,7 @@ $FOOTER['docs'] = <<<TMPL
 TMPL;
  
  
- 
+ */
  
  
  
