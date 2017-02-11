1f4b661a2c345aff678b7ad52512eb14f0a8b00e
MikeyGMT <mikeygmt@gmail.com>
2017-02-11 00:43:39 +0000

    More Duplicate LANS #6 in Submit News, Cron, Mailout
    More Duplicate LANS #6 in Submit News, Cron, Mailout

--- e107_languages/English/lan_submitnews.php
+++ e107_languages/English/lan_submitnews.php
@@ -10,14 +10,14 @@
 +----------------------------------------------------------------------------+
 */
 define("PAGE_NAME", "Submit News");
-define("LAN_7", "Name: ");
+//define("LAN_7", "Name: ");//LAN_NAME
 //define("LAN_62", "News Title: ");//LAN_TITLE
-define("LAN_112", "Email Address: ");
-define("LAN_133", "Thank you");
+//define("LAN_112", "Email Address: ");//LAN_EMAIL
+//define("LAN_133", "Thank you");//LAN_THANK_YOU//NOT USED
 define("LAN_134", "Your item has been submitted for review by one of the site administrators.");
 define("LAN_135", "News Item: ");
 define("LAN_136", "Submit News Item");
-define("NWSLAN_6", "Category");
+//define("NWSLAN_6", "Category");//LAN_CATEGORY
 define("NWSLAN_10", "No news categories");
 define("NWSLAN_11", "You do not have access to this area or you are currently not logged in.");
 define("NWSLAN_12", "Access Denied."); 
@@ -31,7 +31,7 @@ define("SUBNEWSLAN_6", "(jpg, gif or png)");
 define("SUBNEWSLAN_7", "You must give your name and email address");
 define("SUBNEWSLAN_8", "Error uploading image");
 define("SUBNEWSLAN_9", "Keywords");
-define("SUBNEWSLAN_10", "Summary");
+//define("SUBNEWSLAN_10", "Summary");//LAN_SUMMARY
 define("SUBNEWSLAN_11", "Meta Description");
 define("SUBNEWSLAN_12", "Used by Facebook etc.");
 define("SUBNEWSLAN_13", "Media URLs");
