d8c342783ca4ce84092c1a9ddd76fa9989a4a32f
Cameron <e107inc@gmail.com>
2017-02-13 18:14:26 -0800

    News Grid template simplified. Template/Layout combination consolidated into 'layout' only.

--- e107_plugins/news/templates/news_grid_template.php
+++ e107_plugins/news/templates/news_grid_template.php
@@ -9,16 +9,16 @@
 	 */
 
 
-	$NEWS_GRID_TEMPLATE['default']['start'] = '<div class="row news-grid-default news-menu-grid">';
+	$NEWS_GRID_TEMPLATE['col-md-6']['start'] = '<div class="row news-grid-default news-menu-grid">';
 
-	$NEWS_GRID_TEMPLATE['default']['featured'] = '<div class="row featured">
+	$NEWS_GRID_TEMPLATE['col-md-6']['featured'] = '<div class="row featured">
 													<div class="col-sm-12">
 													<div class="item col-sm-6" >
 														{SETIMAGE: w=600&h=400&crop=1}
 														{NEWSTHUMBNAIL=placeholder}
 													</div>
 													<div class="item col-sm-6">
-		                                                <h3>Featured {NEWSTITLE: limit=_titleLimit_}</h3>
+		                                                <h3>{NEWSTITLE}</h3>
 		                                                <p>{NEWSMETADIZ: limit=100}</p>
 		                                                <p class="text-right"><a class="btn btn-primary btn-othernews" href="{NEWSURL}">' . LAN_READ_MORE . '</a></p>
 	                                                </div>
@@ -26,15 +26,49 @@
 	                                               </div>
             							          ';
 
-	$NEWS_GRID_TEMPLATE['default']['item'] = '<div class="item {NEWSGRID}">
+	$NEWS_GRID_TEMPLATE['col-md-6']['item'] = '<div class="item col-md-6">
 												{SETIMAGE: w=400&h=400&crop=1}
 												{NEWSTHUMBNAIL=placeholder}
-              									<h3>{NEWSTITLE: limit=_titleLimit_}</h3>
-              									<p>{NEWSSUMMARY: limit=_summaryLimit_}</p>
+              									<h3>{NEWS_TITLE}</h3>
+              									<p>{NEWS_SUMMARY}</p>
               									<p class="text-right"><a class="btn btn-primary btn-othernews" href="{NEWSURL}">' . LAN_READ_MORE . '</a></p>
             							   </div>';
 
-	$NEWS_GRID_TEMPLATE['default']['end'] = '</div>';
+	$NEWS_GRID_TEMPLATE['col-md-6']['end'] = '</div>';
+
+
+
+
+	$NEWS_GRID_TEMPLATE['col-md-4']['start']    = $NEWS_GRID_TEMPLATE['col-md-6']['start'];
+	$NEWS_GRID_TEMPLATE['col-md-4']['featured'] = $NEWS_GRID_TEMPLATE['col-md-6']['featured'];
+    $NEWS_GRID_TEMPLATE['col-md-4']['item']     = '<div class="item col-md-4">
+													{SETIMAGE: w=400&h=400&crop=1}
+													{NEWSTHUMBNAIL=placeholder}
+	                                                <h3>{NEWS_TITLE}</h3>
+	                                                <p>{NEWS_SUMMARY}</p>
+	                                                <p class="text-right"><a class="btn btn-primary btn-othernews" href="{NEWSURL}">' . LAN_READ_MORE . '</a></p>
+            							        </div>';
+	$NEWS_GRID_TEMPLATE['col-md-4']['end']      = $NEWS_GRID_TEMPLATE['col-md-6']['end'];
+
+
+
+
+
+
+	$NEWS_GRID_TEMPLATE['col-md-3']['start']    = $NEWS_GRID_TEMPLATE['col-md-6']['start'];
+	$NEWS_GRID_TEMPLATE['col-md-3']['featured'] = $NEWS_GRID_TEMPLATE['col-md-6']['featured'];
+    $NEWS_GRID_TEMPLATE['col-md-3']['item']     = '<div class="item col-md-3">
+													{SETIMAGE: w=400&h=400&crop=1}
+													{NEWSTHUMBNAIL=placeholder}
+	                                                <h3>{NEWS_TITLE}</h3>
+	                                                <p>{NEWS_SUMMARY}</p>
+	                                                <p class="text-right"><a class="btn btn-primary btn-othernews" href="{NEWSURL}">' . LAN_READ_MORE . '</a></p>
+            							        </div>';
+	$NEWS_GRID_TEMPLATE['col-md-3']['end']      = $NEWS_GRID_TEMPLATE['col-md-6']['end'];
+
+
+
+
 
 
 
@@ -44,8 +78,8 @@
 	$NEWS_GRID_TEMPLATE['other']['featured'] = '<div class="featured item col-sm-6" >
 														{SETIMAGE: w=600&h=400&crop=1}
 														{NEWSTHUMBNAIL=placeholder}
-														 <h3>Featured {NEWSTITLE: limit=_titleLimit_}</h3>
-														 <p>{NEWSSUMMARY}</p>
+														 <h3>{NEWS_TITLE}</h3>
+														 <p>{NEWS_SUMMARY}</p>
 													</div>
 
 
@@ -62,8 +96,8 @@
 													    </a>
 													  </div>
 													  <div class="media-body">
-													    <h4 class="media-heading">{NEWSTITLE: limit=_titleLimit_}</h4>
-													    <p>{NEWSSUMMARY: limit=_summaryLimit_}</p>
+													    <h4 class="media-heading">{NEWS_TITLE}</h4>
+													    <p>{NEWS_SUMMARY}</p>
 													  </div>
 													  </li>
 													</ul>
