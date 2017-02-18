1bb29246c40f5735bdaeb88ee36a606d4b42d7cf
Gábor <yesszus@gmail.com>
2017-02-18 15:37:21 +0100

    Create English_admin.php

---
+++ e107_plugins/tinymce4/languages/English_admin.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/02/18 11:38:44
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+define("ADtinymce4_LAN_1", "Paste as text by default");
+define("ADtinymce4_LAN_2", "Browser spellcheck");
+define("ADtinymce4_LAN_3", "Enable this if the browser internal spellchecker should be used.");
+define("ADtinymce4_LAN_4", "Visual Blocks");
+define("ADtinymce4_LAN_5", "Enable to make html blocks visible during editing.");
+?>
