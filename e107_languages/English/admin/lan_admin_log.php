d2f52e3687c4953a7db786965475ab43ac5d9d1b
Gábor <yesszus@gmail.com>
2017-03-05 07:14:07 +0100

    Update lan_admin_log.php

--- e107_languages/English/admin/lan_admin_log.php
+++ e107_languages/English/admin/lan_admin_log.php
@@ -93,7 +93,7 @@ define("RL_LAN_094", "Detailed timing analysis");
 // define("RL_LAN_095", "Logs to include");
 define("RL_LAN_096", "Diff (s)");
 // define("RL_LAN_097", "Time");
-define("RL_LAN_098", "Source");
+//define("RL_LAN_098", "Source"); // Moved to lan_admin.php
 // define("RL_LAN_099", "Comments");
 define("RL_LAN_100", "CID");			// Comment ID field
 define("RL_LAN_101", "PID");
