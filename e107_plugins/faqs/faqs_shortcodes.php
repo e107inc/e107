c8805283347a70baf1ab7be4ee5fc1567b0b80e1
Gábor <yesszus@gmail.com>
2017-04-10 10:42:55 +0200

    Update faqs_shortcodes.php

--- e107_plugins/faqs/faqs_shortcodes.php
+++ e107_plugins/faqs/faqs_shortcodes.php
@@ -298,7 +298,7 @@ class faqs_shortcodes extends e_shortcode
 				$text .= $frm->open('faq-ask-question','post');
 				//TODO LAN ie. [x] character limit.
 				$text .= "<div>".$frm->textarea('ask_a_question','',3, 80 ,array('maxlength'=>$this->questionCharLimit, 'size'=>'xxlarge','placeholder'=>LAN_FAQS_ENTER_A_QUESTION_HERE, 'wrap'=>'soft'))."
-				<div class='faq-char-limit'><small>".$this->questionCharLimit." ".LAN_FAQS_CHAR_LIMIT".</small></div>".$frm->submit('submit_a_question',LAN_SUBMIT)."</div>";
+				<div class='faq-char-limit'><small>".$this->questionCharLimit.LAN_FAQS_CHAR_LIMIT".</small></div>".$frm->submit('submit_a_question',LAN_SUBMIT)."</div>";
 
 				$text .= $frm->close();
 			}
