e107inc/e107

commit 9a5ac5b21a0cfda921c52c63086973a4f4b16845

Permissive field validation in db_verify
Even if it's a new installation of e107, `db_verify` will be too strict in
validating database field defaults that are equivalent to each other.

This can happen on some MySQL or MariaDB servers that correct the field
definitions, which were not strictly correct in the table structures built into
e107 in the first place.

This commit introduces db_verify::validateFieldPermissive(), a
backwards-compatible way to validate database field defaults.  It works
regardless of whether the MySQL server stores a corrected representation of the
schema and does not require fixing the schema in any existing table structure
files.

Fixes: #2998

e107_handlers/db_verify_class.php
@@ -132,6 +132,29 @@ class db_verify
 
 
 	}
+
+	/**
+	 * Permissive field validation
+	 */
+	private function validateFieldPermissive($expected, $actual)
+	{
+		// Permit actual text types that default to null even when
+		// expected does not explicitly default to null
+		if(0 === strcasecmp($expected['type'], $actual['type']) &&
+		   1 === preg_match('/[A-Z]*TEXT/i', $expected['type']) &&
+		   0 === strcasecmp($actual['default'], "DEFAULT NULL"))
+		{
+			$expected['default'] = $actual['default'];
+		}
+
+		// Loosely typed default value for int-like types
+		if(1 === preg_match('/[A-Z]*INT/i', $expected['type']))
+		{
+			$expected['default'] = preg_replace("/DEFAULT '(\d+)'/i", 'DEFAULT $1', $expected['default']);
+			$actual['default']   = preg_replace("/DEFAULT '(\d+)'/i", 'DEFAULT $1', $actual['default']  );
+		}
+		return !count($off = array_diff_assoc($expected, $actual));
+	}
 	
 	/**
 	 * Main Routine for checking and rendering results. 
@@ -347,7 +370,7 @@ class db_verify
 					$this->results[$tbl][$field]['_valid'] = $info;
 					$this->results[$tbl][$field]['_file'] = $selection;
 				}
-				elseif(count($off = array_diff_assoc($info,$sqlFieldData[$field])))
+				elseif(!$this->validateFieldPermissive($info, $sqlFieldData[$field]))
 				{
 					$this->errors[$tbl]['_status'] = 'mismatch';
 					$this->results[$tbl][$field]['_status'] = 'mismatch';

commit c959610f7d0f56416021a8b50f6451e0c216ceaa

Fixed regression from 9a5ac5b
Regression fixed: Diff wasn't being set

e107_handlers/db_verify_class.php
@@ -136,7 +136,7 @@ class db_verify
 	/**
 	 * Permissive field validation
 	 */
-	private function validateFieldPermissive($expected, $actual)
+	private function diffFieldPermissive($expected, $actual)
 	{
 		// Permit actual text types that default to null even when
 		// expected does not explicitly default to null
@@ -153,7 +153,8 @@ class db_verify
 			$expected['default'] = preg_replace("/DEFAULT '(\d+)'/i", 'DEFAULT $1', $expected['default']);
 			$actual['default']   = preg_replace("/DEFAULT '(\d+)'/i", 'DEFAULT $1', $actual['default']  );
 		}
-		return !count($off = array_diff_assoc($expected, $actual));
+
+		return array_diff_assoc($expected, $actual);
 	}
 	
 	/**
@@ -370,11 +371,11 @@ class db_verify
 					$this->results[$tbl][$field]['_valid'] = $info;
 					$this->results[$tbl][$field]['_file'] = $selection;
 				}
-				elseif(!$this->validateFieldPermissive($info, $sqlFieldData[$field]))
+				elseif(count($diff = $this->diffFieldPermissive($info, $sqlFieldData[$field])))
 				{
 					$this->errors[$tbl]['_status'] = 'mismatch';
 					$this->results[$tbl][$field]['_status'] = 'mismatch';
-					$this->results[$tbl][$field]['_diff'] = $off;	
+					$this->results[$tbl][$field]['_diff'] = $diff;
 					$this->results[$tbl][$field]['_valid'] = $info;
 					$this->results[$tbl][$field]['_invalid'] = $sqlFieldData[$field];
 					$this->results[$tbl][$field]['_file'] = $selection;
@@ -398,14 +399,14 @@ class db_verify
 					$this->indices[$tbl][$field]['_valid'] = $info;
 					$this->indices[$tbl][$field]['_file'] = $selection;
 				}
-				elseif(count($offin = array_diff_assoc($info,$sqlIndexData[$field]))) // missmatch data
+				elseif(count($diff = $this->diffFieldPermissive($info, $sqlIndexData[$field]))) // mismatched data
 				{
 					// print_a($info);
 					// print_a($sqlIndexData[$field]);
 					
 					$this->errors[$tbl]['_status'] = 'mismatch_index';
 					$this->indices[$tbl][$field]['_status'] = 'mismatch';
-					$this->indices[$tbl][$field]['_diff'] = $offin;	
+					$this->indices[$tbl][$field]['_diff'] = $diff;
 					$this->indices[$tbl][$field]['_valid'] = $info;
 					$this->indices[$tbl][$field]['_invalid'] = $sqlIndexData[$field];
 					$this->indices[$tbl][$field]['_file'] = $selection;