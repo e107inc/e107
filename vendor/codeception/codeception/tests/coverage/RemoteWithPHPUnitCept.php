8318423d11ec76be42fcac308394f3c17ba18b74
Deltik <deltik@gmx.com>
2018-02-12 15:08:16 -0600

    Increased compatibility down to PHP 5.6
    Removed dependencies installed by Composer

--- vendor/codeception/codeception/tests/coverage/RemoteWithPHPUnitCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$I = new CoverGuy($scenario);
-$I->wantTo('try generate remote codecoverage phpunit report');
-$I->amInPath('tests/data/sandbox');
-$I->executeCommand('run remote --coverage-phpunit');
-$I->seeFileFound('index.xml', 'tests/_output/coverage-phpunit');
-$I->seeCoverageStatsNotEmpty();
