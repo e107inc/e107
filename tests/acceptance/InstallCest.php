38dd581c208bd6778f05bfece0c00efe981c21a8
Cameron <e107inc@gmail.com>
2018-02-16 15:42:29 -0800

    start of step 2

--- tests/acceptance/InstallCest.php
+++ tests/acceptance/InstallCest.php
@@ -22,9 +22,22 @@ class InstallCest
 	public function installStep1ToStep2(AcceptanceTester $I)
     {
         $I->amOnPage('/install.php');
-        $I->wantTo("Verify Proceed to Step 1 of the Installation");
+        $I->wantTo("Verify Proceed to Step 2 of the Installation");
 		$I->selectOption("language", 'English');
 		$I->click('start');
 		$I->see("MySQL Server Details", 'h3');
+    }
+
+    public function installStep2ToStep3(AcceptanceTester $I)
+    {
+        $I->amOnPage('/install.php');
+        $I->wantTo("Verify Proceed to Step 3 of the Installation");
+        $this->installStep1ToStep2($I);
+
+		$config = $I->getConfig();
+
+	
+
+		// ....
     }
 }
