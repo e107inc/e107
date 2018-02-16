383358b9bcf1a43359e444522c2d9e0a81de8afe
Deltik <deltik@gmx.com>
2018-02-16 17:32:07 -0600

    APP_PATH is now customizable in config.yml
    Configuration option is "app_path" (string)

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
-define('APP_PATH', realpath(codecept_root_dir()."/e107")); // TODO use 'dir' value from config.yml
+$params = include(codecept_root_dir()."/lib/config.php");
+
+$app_path = $params['app_path'] ?: codecept_root_dir()."/e107";
+
+// Relative path
+if (substr($app_path, 0, 1) !== '/')
+	$app_path = codecept_root_dir() . "/${app_path}";
+
+define('APP_PATH', realpath($app_path));
