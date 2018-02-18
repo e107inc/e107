edf296c940eed8a31071c1ad5997fc98fa444861
Cameron <e107inc@gmail.com>
2018-02-17 16:28:57 -0800

    additional install steps added.

--- tests/acceptance/InstallCest.php
+++ tests/acceptance/InstallCest.php
@@ -14,6 +14,8 @@ class InstallCest
     // tests
     public function installWelcomePageContainsExpectedContent(AcceptanceTester $I)
     {
+
+
     	$I->amOnPage('/install.php');
 		$I->see("e107 Installation :: Step 1");
 		$I->see("Language Selection");
@@ -34,10 +36,94 @@ class InstallCest
         $I->wantTo("Verify Proceed to Step 3 of the Installation");
         $this->installStep1ToStep2($I);
 
-		$config = $I->getConfig();
+		$I->fillField('server',     $I->getDbHostname());
+		$I->fillField('name',       $I->getDbUsername());
+		$I->fillField('password',   $I->getDbPassword());
+		$I->fillField('db',         $I->getDbName());
 
-	
+		$I->uncheckOption('createdb');
+		$I->click('submit');
+
+		$I->see("MySQL Connection Verification", 'h3');
 
 		// ....
+    }
+
+	public function installStep3ToStep4(AcceptanceTester $I)
+    {
+
+        $I->amOnPage('/install.php');
+        $I->wantTo("Verify Proceed to Step 4 of the Installation");
+
+        $this->installStep2ToStep3($I);
+
+     //   $I->see("Connection to the MySQL server established and verified");
+		$I->see("Found existing database");
+
+		$I->click('submit');
+
+		$I->see("PHP and MySQL Versions Check / File Permissions Check");
+
+    }
+
+    public function installStep4ToStep5(AcceptanceTester $I) // TODO Fails due to e107_config.php being present.
+    {
+
+        $I->amOnPage('/install.php');
+        $I->wantTo("Verify Proceed to Step 5 of the Installation");
+
+		$this->installStep3ToStep4($I);
+
+		$I->canSee('You might have an existing installation'); //XXX Triggered if e107_config.php is not empty
+
+        $I->click('continue_install');
+
+		$I->see("Administration", 'h3');
+    }
+
+	public function installStep5ToStep6(AcceptanceTester $I)
+    {
+
+        $I->amOnPage('/install.php');
+         $I->wantTo("Verify Proceed to Step 6 of the Installation");
+        $this->installStep4ToStep5($I);
+
+        $I->fillField('u_name',     'admin');
+		$I->fillField('d_name',     'admin');
+		$I->fillField('pass1',      'admin');
+		$I->fillField('pass2',      'admin');
+		$I->fillField('email',      'admin@admin.com');
+
+        $I->click('submit');
+
+		$I->see("Website Preferences", 'h3');
+    }
+
+    public function installStep6ToStep7(AcceptanceTester $I)
+    {
+
+        $I->amOnPage('/install.php');
+         $I->wantTo("Verify Proceed to Step 7 of the Installation");
+        $this->installStep5ToStep6($I);
+
+        $I->fillField('sitename',     'Test Site');
+
+        $I->click('submit');
+
+		$I->see("Install Confirmation", 'h3');
+    }
+
+      public function installStep7ToStep8(AcceptanceTester $I)
+    {
+
+        $I->amOnPage('/install.php');
+         $I->wantTo("Verify Proceed to Step 8 of the Installation");
+        $this->installStep6ToStep7($I);
+
+        $I->see("Install Confirmation", 'h3');
+
+        $I->click('submit');
+
+		$I->see("Install Confirmation", 'h3');
     }
 }
