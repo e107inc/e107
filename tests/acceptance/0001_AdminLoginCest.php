e15a363f8344a5a3f955c46b815bfcb09e8d55ce
Cameron <e107inc@gmail.com>
2018-03-08 17:49:37 -0800

    Admin login and signup simulation tests added.

---
+++ tests/acceptance/0001_AdminLoginCest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class AdminLoginCest
+{
+	public function _before(AcceptanceTester $I)
+	{
+	}
+
+	public function _after(AcceptanceTester $I)
+	{
+	}
+
+	// tests
+	public function testAdminLogin(AcceptanceTester $I)
+	{
+		$I->amOnPage('/e107_admin/admin.php');
+		$I->wantTo("Test the admin area login process");
+		$I->see("Admin Area");
+		$I->see("login");
+
+		$I->fillField('authname', 'admin');
+		$I->fillField('authpass', 'admin');
+
+		$I->click('authsubmit');
+
+		$I->see("Admin's Control Panel");
+
+		$I->dontSeeInSource('Unauthorized access!');
+
+		$I->see("Latest");
+
+		$I->see("Status");
+
+	//	$I->dontSeeInSource('fa fa-database');
+
+
+	}
+
+}
