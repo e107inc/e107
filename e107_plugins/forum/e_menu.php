bd02bd26f7ddc322154d2b951c830176a9dbd74c
Achim Ennenbach <achim@simsync.de>
2018-06-26 21:20:33 +0200

    Fixes #3132 Fixes issue with new forum posts menu not displaying title and post excerpt properly Added menu config default values, in case some are not properly defined Added default values to the new forum posts config menu

--- e107_plugins/forum/e_menu.php
+++ e107_plugins/forum/e_menu.php
@@ -31,10 +31,10 @@ class forum_menu
 
 		$fields = array();
 		$fields['caption']      = array('title'=> LAN_FORUM_MENU_004, 'type'=>'text', 'multilan'=>true, 'writeParms'=>array('size'=>'xxlarge'));
-		$fields['display']      = array('title'=> LAN_FORUM_MENU_005, 'type'=>'text',  'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*'));
-		$fields['maxage']       = array('title'=> LAN_FORUM_MENU_012, 'type'=>'text', 'help'=>LAN_FORUM_MENU_013, 'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*'));
-		$fields['characters']   = array('title'=> LAN_FORUM_MENU_006, 'type'=>'text', 'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*'));
-		$fields['postfix']      = array('title'=> LAN_FORUM_MENU_007, 'type'=>'text', 'writeParms'=>array('size'=>'mini'));
+		$fields['display']      = array('title'=> LAN_FORUM_MENU_005, 'type'=>'text',  'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*','default' => 10));
+		$fields['maxage']       = array('title'=> LAN_FORUM_MENU_012, 'type'=>'text', 'help'=>LAN_FORUM_MENU_013, 'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*','default' => 0));
+		$fields['characters']   = array('title'=> LAN_FORUM_MENU_006, 'type'=>'text', 'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*','default' => 120));
+		$fields['postfix']      = array('title'=> LAN_FORUM_MENU_007, 'type'=>'text', 'writeParms'=>array('size'=>'mini','default' => '...'));
 		$fields['scroll']        = array('title'=> LAN_FORUM_MENU_014, 'type'=>'text', 'writeParms'=>array('size'=>'mini','pattern'=>'[0-9]*'), 'help'=>LAN_FORUM_MENU_015);
 		$fields['layout']       = array('title'=> LAN_TEMPLATE, 'type'=>'dropdown', 'writeParms'=>array('optArray'=>$layouts[0]));
 		
