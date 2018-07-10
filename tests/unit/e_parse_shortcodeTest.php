43cde547ad84b6727aa4f303464984f4d798490d
Cameron <e107inc@gmail.com>
2018-07-10 14:30:53 -0700

    Shortcode handler tests placeholder added.

---
+++ tests/unit/e_parse_shortcodeTest.php
@@ -0,0 +1,88 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2018 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+
+	class e_parse_shortcodeTest extends \Codeception\Test\Unit
+	{
+/*
+		public function testIsBatchOverride()
+		{
+
+		}
+
+		public function testIsRegistered()
+		{
+
+		}
+
+		public function testIsOverride()
+		{
+
+		}
+
+		public function testResetScClass()
+		{
+
+		}
+
+		public function testDoCode()
+		{
+
+		}
+
+		public function testGetScObject()
+		{
+
+		}
+
+		public function testParseCodes()
+		{
+
+		}
+
+		public function testInitShortcodeClass()
+		{
+
+		}
+
+		public function testRegisterShortcode()
+		{
+
+		}
+
+		public function testSetScVar()
+		{
+
+		}
+
+		public function testCallScFunc()
+		{
+
+		}
+
+		public function testIsScClass()
+		{
+
+		}
+
+		public function testParse_scbatch()
+		{
+
+		}
+
+		public function testLoadThemeShortcodes()
+		{
+
+		}
+
+*/
+
+
+	}
