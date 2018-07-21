1aba3167b9188e685af1f8e96f26f7e8064f584f
Gábor <yesszus@gmail.com>
2018-07-21 10:16:30 +0200

    Update lan_admin.php

--- e107_languages/English/admin/lan_admin.php
+++ e107_languages/English/admin/lan_admin.php
@@ -553,3 +553,4 @@ define("LAN_UI_FILTER_NEXT_YEAR", "Next Year");
 define("LAN_UI_FILTER_IS_EMPTY", "Is Empty");
 define("LAN_RECALCULATE_COMMENT_COUNT", "Recalculate comment count");
 define("LAN_SUCC_RECALCULATE_COMMENT_COUNT", "Comment count successfully recalculated!");
+define("LAN_NO_RESULTS_FOUND", "No Results Found.");
