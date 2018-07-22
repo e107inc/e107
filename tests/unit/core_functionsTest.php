81a20c6337a025401ca38b8c3196755c0c333829
Cameron <e107inc@gmail.com>
2018-07-22 10:41:16 -0700

    Test added for e_array and placeholder file for core_functions.

---
+++ tests/unit/core_functionsTest.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2018 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+
+	class core_functionsTest extends \Codeception\Test\Unit
+	{
+		protected function _before()
+		{
+
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+	}
