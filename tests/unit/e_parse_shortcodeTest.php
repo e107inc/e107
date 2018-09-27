aad7207eda152b4139fdc78d75f11f0987c11a14
Nick Liu <deltik@gmx.com>
2018-09-27 11:32:06 -0500

    Started but couldn't test e_parse_shortcode

--- tests/unit/e_parse_shortcodeTest.php
+++ tests/unit/e_parse_shortcodeTest.php
@@ -1,88 +1,103 @@
 <?php
-	/**
-	 * e107 website system
-	 *
-	 * Copyright (C) 2008-2018 e107 Inc (e107.org)
-	 * Released under the terms and conditions of the
-	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
-	 *
-	 */
+/**
+ * e107 website system
+ *
+ * Copyright (C) 2008-2018 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
 
+class e_parse_shortcodeTest extends \Codeception\Test\Unit
+{
+	/** @var e_parse_shortcode */
+	private $scParser;
 
-	class e_parse_shortcodeTest extends \Codeception\Test\Unit
+	public function _before()
 	{
-/*
-		public function testIsBatchOverride()
-		{
-
+		try {
+			$this->scParser = $this->make('e_parse_shortcode');
+		} catch (Exception $e) {
+			$this->fail("Couldn't create e_parse_shortcode object");
 		}
+		$this->scParser->__construct();
+	}
 
-		public function testIsRegistered()
-		{
+//	public function testShortcode_SITELINKS_ALT()
+//	{
+//		$output = $this->scParser->parseCodes('{SITELINKS_ALT=/e107_themes/jayya/images/arrow.png+noclick}');
+//		var_export($output);
+//	}
 
-		}
-
-		public function testIsOverride()
-		{
-
-		}
-
-		public function testResetScClass()
-		{
-
-		}
-
-		public function testDoCode()
-		{
-
-		}
-
-		public function testGetScObject()
-		{
-
-		}
-
-		public function testParseCodes()
-		{
-
-		}
-
-		public function testInitShortcodeClass()
-		{
-
-		}
-
-		public function testRegisterShortcode()
-		{
-
-		}
-
-		public function testSetScVar()
-		{
-
-		}
-
-		public function testCallScFunc()
-		{
-
-		}
-
-		public function testIsScClass()
-		{
-
-		}
-
-		public function testParse_scbatch()
-		{
-
-		}
-
-		public function testLoadThemeShortcodes()
-		{
-
-		}
-
-*/
-
+	/*
+	public function testIsBatchOverride()
+	{
 
 	}
+
+	public function testIsRegistered()
+	{
+
+	}
+
+	public function testIsOverride()
+	{
+
+	}
+
+	public function testResetScClass()
+	{
+
+	}
+
+	public function testDoCode()
+	{
+
+	}
+
+	public function testGetScObject()
+	{
+
+	}
+
+	public function testParseCodes()
+	{
+
+	}
+
+	public function testInitShortcodeClass()
+	{
+
+	}
+
+	public function testRegisterShortcode()
+	{
+
+	}
+
+	public function testSetScVar()
+	{
+
+	}
+
+	public function testCallScFunc()
+	{
+
+	}
+
+	public function testIsScClass()
+	{
+
+	}
+
+	public function testParse_scbatch()
+	{
+
+	}
+
+	public function testLoadThemeShortcodes()
+	{
+
+	}
+	*/
+}
