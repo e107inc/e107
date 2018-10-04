0d826955039660c01b317d181889eac7a623fa13
Cameron <e107inc@gmail.com>
2018-10-04 16:36:09 -0700

    Fixes #3489 Download Name length increased to 255 chars.

--- e107_plugins/download/download_sql.php
+++ e107_plugins/download/download_sql.php
@@ -3,7 +3,7 @@
 #
 CREATE TABLE download (
   download_id int(10) unsigned NOT NULL auto_increment,
-  download_name varchar(100) NOT NULL default '',
+  download_name varchar(255) NOT NULL default '',
   download_url varchar(255) NOT NULL default '',
   download_sef varchar(255) NOT NULL default '',
   download_author varchar(100) NOT NULL default '',
