9fc5d9e921cf2e12963d3a65948758d2b04197e4
Cameron <e107inc@gmail.com>
2019-01-15 09:16:37 -0800

    Issue #3216 PHP 7 compatibility

--- online.php
+++ online.php
@@ -271,15 +271,21 @@ Think these are no longer used
 	$online_location_page = ADMINAREA;
   }
 	
-  $ONLINE_TABLE_ICON = (vartrue($pref['plug_installed']['pm']) && $oid != USERID ? $tp->parseTemplate("{SENDPM={$oid}}", 'sendpm.sc') : "<img src='".e_PLUGIN."online_extended_menu/images/user.png' alt='' style='vertical-align:middle' />");
+//  $ONLINE_TABLE_ICON = (vartrue($pref['plug_installed']['pm']) && $oid != USERID ? $tp->parseTemplate("{SENDPM={$oid}}", 'sendpm.sc') : "<img src='".e_PLUGIN."online_extended_menu/images/user.png' alt='' style='vertical-align:middle' />");
  
   //$ONLINE_TABLE_USERNAME = "<a href='".e_BASE."user.php?id.$oid'>{$oname}</a>";
   $uparams = array('id' => $oid, 'name' => $oname);
   $link = e107::getUrl()->create('user/profile/view', $uparams);
-  $ONLINE_TABLE_USERNAME = "<a href='".$link."'>".$oname."</a>";
-  $ONLINE_TABLE_LOCATION = ($class_check ? "<a href='{$online_location}'>{$online_location_page}</a>" : $online_location_page);
+//  $ONLINE_TABLE_USERNAME = "<a href='".$link."'>".$oname."</a>";
+//  $ONLINE_TABLE_LOCATION = ($class_check ? "<a href='{$online_location}'>{$online_location_page}</a>" : $online_location_page);
 
-  $textstring .= preg_replace("/\{(.*?)\}/e", '$\1', $ONLINE_TABLE);
+  $scArray = array(
+    'ONLINE_TABLE_ICON'     => (vartrue($pref['plug_installed']['pm']) && $oid != USERID ? $tp->parseTemplate("{SENDPM={$oid}}", 'sendpm.sc') : "<img src='".e_PLUGIN."online_extended_menu/images/user.png' alt='' style='vertical-align:middle' />"),
+    'ONLINE_TABLE_USERNAME' => "<a href='".$link."'>".$oname."</a>",
+    'ONLINE_TABLE_LOCATION' => ($class_check ? "<a href='{$online_location}'>{$online_location_page}</a>" : $online_location_page)
+  );
+
+  $textstring .= $tp->parseTemplate($ONLINE_TABLE, true, $scArray); // preg_replace("/\{(.*?)\}/e", '$\1', $ONLINE_TABLE);
 }
 	
 $ONLINE_TABLE_MEMBERS_ONLINE = ONLINE_EL1.GUESTS_ONLINE;
