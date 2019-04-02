23d8f6f90d1e466809ac8855bf4f027777566d6b
Cameron <e107inc@gmail.com>
2019-04-02 12:09:00 -0700

    Deprecated $FORUMEND RSS code added for reference. Needs to be replaced by a shortcode.

--- e107_plugins/forum/templates/forum_viewtopic_template.php
+++ e107_plugins/forum/templates/forum_viewtopic_template.php
@@ -179,6 +179,13 @@ $FORUMEND = "<tr><td colspan='2' class='forumheader3' style='text-align:center'>
 	</tr>
 	</table>
 	</div>
+	<!--
+	<div style='text-align:center' class='spacer'>
+	<a href='".e_PLUGIN."rss_menu/rss.php?8.1.".e_QUERY."'><img src='".e_PLUGIN."rss_menu/images/rss1.png' alt='".LAN_431."' style='vertical-align: middle; border: 0;' /></a> <a href='".e_PLUGIN."rss_menu/rss.php?8.2.".e_QUERY."'><img src='".e_PLUGIN."rss_menu/images/rss2.png' alt='".LAN_432."' style='vertical-align: middle; border: 0;' /></a> <a href='".e_PLUGIN."rss_menu/rss.php?8.3.".e_QUERY."'><img src='".e_PLUGIN."rss_menu/images/rss3.png' alt='".LAN_433."' style='vertical-align: middle; border: 0;' /></a>
+	</div>
+	-->
+	
+	
 	<div class='nforumdisclaimer' style='text-align:center'>Powered by <b>e107 Forum System</b></div>";
 
 $FORUMREPLYSTYLE = "<tr>
