6fd1aef610a8541891d90c19a4de7b5358f14f58
Tijn Kuyper <github@tijnkuyper.nl>
2019-04-03 12:53:59 +0200

    Forum - Allow styling of button in { SEARCH}
    #3584

--- e107_plugins/forum/shortcodes/batch/forum_shortcodes.php
+++ e107_plugins/forum/shortcodes/batch/forum_shortcodes.php
@@ -164,13 +164,16 @@ class forum_shortcodes extends e_shortcode
 		  return $text;
 	}
 
-	function sc_search()
+	function sc_search($parm='')
 	{
 
 		if(!deftrue('FONTAWESOME') || !$srchIcon = e107::getParser()->toGlyph('fa-search'))
 		{
 			$srchIcon = LAN_SEARCH;
 		}
+
+		$buttonclass = (!empty($parm['buttonclass'])) ? "class='".$parm['buttonclass']."'" : "class='btn btn-default btn-secondary button'";
+
 
 		// String candidate for USERLIST wrapper
 		return "
@@ -181,7 +184,7 @@ class forum_shortcodes extends e_shortcode
 		<input type='hidden' name='forum' value='all' />
 		<input class='tbox form-control' type='text' name='q' size='20' value='' maxlength='50' />
 		<span class='input-group-btn'>
-		<button class='btn btn-default btn-secondary button' type='submit' name='s' value='search' >".$srchIcon."</button>
+		<button ".$buttonclass." type='submit' name='s' value='search' >".$srchIcon."</button>
 		</span>
 		</div>
 
