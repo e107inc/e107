9775361b5168a8c8d473f9467b89db1bf7e21565
Cameron <e107inc@gmail.com>
2019-04-06 15:34:11 -0700

    Menu-Manager theme layout parsing test. Legacy themes added.

---
+++ tests/_data/testcore/languages/English.php
@@ -0,0 +1,33 @@
+<?php
+define('LAN_THEME_1', 'e107 core theme by <a href="http://e107.org" title="e107 CMS" rel="external">e107 Inc.</a>');
+define('LAN_THEME_2', 'Comments: ');
+define('LAN_THEME_3', 'Comments are turned off for this item');
+define('LAN_THEME_4', 'Read the full story');
+define('LAN_THEME_5', 'Trackbacks: '); 
+define('LAN_THEME_8', 'in');
+define('LAN_THEME_9', 'by');
+define("LAN_THEME_11", "Latest news");
+define("LAN_THEME_12", "E-mail to a friend");
+define("LAN_THEME_13", "Create PDF file");
+define("LAN_THEME_14", "Print");
+define("LAN_THEME_15", "Edit");
+define('LAN_THEME_17', 'Login');
+define('LAN_THEME_18', 'Username');
+define('LAN_THEME_19', 'Password');
+define('LAN_THEME_20', 'Register');
+define('LAN_THEME_21', 'Login');
+define('LAN_THEME_22', 'Forgot password?');
+define('LAN_THEME_23', 'Welcome');
+define('LAN_THEME_24', 'Admin');
+define('LAN_THEME_26', 'Settings');
+define('LAN_THEME_27', 'Profile');
+define('LAN_THEME_28', 'Logout');
+define('LAN_THEME_29', 'List new');
+define('LAN_THEME_SING', 'Login');
+define('LAN_THEME_REG', 'Register');
+define("LAN_SEARCH", "Search");
+define("LAN_SEARCH_SUB", "Go");
+define('LAN_THEME_SHARE', 'Share this');
+define('LAN_THEME_VER', 'e107 v.');
+define("CM_L13", "by");
+?>
