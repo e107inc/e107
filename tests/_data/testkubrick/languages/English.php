4cbfc67393f961eb4e6fbef51e9e2b767b40d70f
Cameron <e107inc@gmail.com>
2019-04-06 15:34:11 -0700

    Menu-Manager theme layout parsing test. Legacy themes added.

---
+++ tests/_data/testkubrick/languages/English.php
@@ -0,0 +1,29 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
+|     Copyright (C) 2008-2010 e107 Inc (e107.org)
+|
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $URL: https://e107.svn.sourceforge.net/svnroot/e107/trunk/e107_0.7/e107_themes/kubrick/languages/English.php $
+|     $Revision: 11678 $
+|     $Id: English.php 11678 2010-08-22 00:43:45Z e107coders $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_1", "'kubrick' by <a href='http://e107.org' title='e107.org' rel='external'>jalist</a> &amp; <a href='http://e107themes.org' title='e107themes.org' rel='external'>Que</a>, Based on original theme by Michael Heilemann (<a href='http://binarybonsai.com/kubrick/' title='http://binarybonsai.com/kubrick/' rel='external'>http://binarybonsai.com/kubrick/</a>. ).");
+define("LAN_THEME_2", "Comments are turned off for this item");
+define("LAN_THEME_3", "comment: ");
+define("LAN_THEME_4", "Read the rest ...");
+define("LAN_THEME_5", "Trackbacks: ");
+define('LAN_THEME_6', 'on');
+define('LAN_THEME_7', 'by');
+
+
+?>
---
+++ tests/unit/e_menu_layoutTest.php
@@ -0,0 +1,152 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2019 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+
+	class e_menu_layoutTest extends \Codeception\Test\Unit
+	{
+
+		/** @var e_menu_layout */
+		protected $menu;
+
+		protected function _before()
+		{
+			require_once(e_HANDLER."menumanager_class.php");
+		}
+
+		private function copydir( $src, $dst )
+		{
+			if(!is_dir($src))
+			{
+				return false;
+			}
+
+
+			$dir = opendir($src);
+			@mkdir(dirname($dst));
+
+			$DS = DIRECTORY_SEPARATOR ;
+
+			while(false !== ($file = readdir($dir)))
+			{
+				if($file != '.' && $file != '..')
+				{
+					if(is_dir($src . $DS . $file))
+					{
+						$this->copydir($src . $DS . $file, $dst . $DS . $file);
+					}
+					else
+					{
+						copy($src . $DS . $file, $dst . $DS . $file);
+					}
+				}
+			}
+
+			closedir($dir);
+		}
+
+
+
+/*
+		public function testMenuSelector()
+		{
+
+		}
+
+		*/
+
+
+		public function testGetLayouts()
+		{
+			$src1 = codecept_data_dir()."testcore";
+			$dest1 = e_THEME."testcore";
+
+			$this->copydir($src1,$dest1);
+
+			$src2 = codecept_data_dir()."testkubrick";
+			$dest2 = e_THEME."testkubrick";
+
+			$this->copydir($src2,$dest2);
+
+			$tests = array(
+
+				'bootstrap3'   => array (
+					'templates' => array( // template key and string length
+						'jumbotron_home'            => 2940,
+						'modern_business_home'      => 3746,
+						'jumbotron_full'            => 1949,
+						'jumbotron_sidebar_right'   => 2765
+					),
+					'menus'     => array (
+						'jumbotron_home'            => array ('1','2','3','4','5','6','7','8','9','10','11','12','13','14','100','101','102','103','104','105','106','107',),
+						'modern_business_home'      => array ('10','100','101','102','103','104','105','106','107',),
+						'jumbotron_full'            => array ('1','100','101','102','103','104','105','106','107',),
+						'jumbotron_sidebar_right'   => array ('1','2','3','4','5','6','7','8','100','101','102','103','104','105','106','107',),
+					),
+				),
+
+
+				'testkubrick'   => array (
+					'templates' => array(
+						'legacyCustom' => 283,
+						'legacyDefault' => 328
+					),
+					'menus'     => array(
+						'legacyCustom' => array(),
+						'legacyDefault' => array('1', '2')
+					),
+				),
+
+				'testcore'      => array (
+					'templates' => array (
+						'HOME' => 1635,
+						'FULL' => 1378,
+						'legacyDefault'=> 1807
+					),
+					'menus'     => array(
+						'HOME' => array('2', '3', '4'),
+						'FULL' => array(),
+						'legacyDefault'=> array('1', '2', '3', '4','5','6')
+					),
+				)
+			);
+
+			foreach($tests as $theme=>$vars)
+			{
+				$result = e_menu_layout::getLayouts($theme);
+
+			//	var_dump($result['templates']);
+			//	var_export($result['menus']);
+
+				foreach($vars['templates'] as $key=>$length)
+				{
+
+					$expectedLength = $length;
+					$actualLength = strlen($result['templates'][$key]);
+
+					$this->assertEquals($expectedLength, $actualLength);
+				}
+
+				foreach($vars['menus'] as $key=>$arr)
+				{
+					$this->assertEquals($arr, $result['menus'][$key]);
+				}
+
+
+			}
+
+
+
+
+		}
+
+
+
+
+	}
