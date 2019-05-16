de5142f4d7c4a8dd5c7f757190bf6702b41e08cc
Cameron <e107inc@gmail.com>
2019-05-16 11:06:01 -0700

    Fixes #3806 Incorrect class on radio container

--- e107_plugins/forum/shortcodes/batch/post_shortcodes.php
+++ e107_plugins/forum/shortcodes/batch/post_shortcodes.php
@@ -422,7 +422,7 @@ class plugin_forum_post_shortcodes extends e_shortcode
 
 			$opts = array(0 => LAN_FORUM_3038, 1 => LAN_FORUM_1011, 2 => LAN_FORUM_1013); 
 
-			return "<div class='checkbox'>".e107::getForm()->radio('threadtype',$opts, $thread_sticky)."</div>";
+			return "<div class='radio'>".e107::getForm()->radio('threadtype',$opts, $thread_sticky)."</div>";
 
 		//	return "<br /><span class='defaulttext'>post thread as 
 		//	<input name='threadtype' type='radio' value='0' ".(!$thread_sticky ? "checked='checked' " : "")." />".LAN_1."&nbsp;<input name='threadtype' type='radio' value='1' ".($thread_sticky == 1 ? "checked='checked' " : "")." />".LAN_2."&nbsp;<input name='threadtype' type='radio' value='2' ".($thread_sticky == 2 ? "checked='checked' " : "")." />".LAN_3."</span>";
@@ -448,25 +448,28 @@ class plugin_forum_post_shortcodes extends e_shortcode
 
 	function sc_forum_post_caption()
 	{
-//		global $forumInfo;
-			$tp = e107::getParser();
-//    var_dump ($this);
-//$this->forumObj->threadGet($this->id, false)		
-    if ($this->var['action'] == "rp")
-    {
-      	$pre = LAN_FORUM_1003;
-	$name = $tp->toHTML($this->var['thread_name'], false, 'no_hook, emotes_off');
-	$url = e107::url('forum', 'topic', $this->var);
-      	$post = LAN_FORUM_2006;
-    }
-    if ($this->var['action'] == "nt")
-    {
-      	$pre = LAN_FORUM_1001;
-	$name = $tp->toHTML($this->var['forum_name'], false, 'no_hook, emotes_off');
-	$url = e107::url('forum', 'forum', $this->var);
-      	$post = LAN_FORUM_2005;
-    }
-    return $pre.($url?": <a {$title} href='".$url."'>{$name}</a> - ":$name).$post;
+
+
+		$tp = e107::getParser();
+
+		if($this->var['action'] == "rp")
+		{
+			$pre = LAN_FORUM_1003;
+			$name = $tp->toHTML($this->var['thread_name'], false, 'no_hook, emotes_off');
+			$url = e107::url('forum', 'topic', $this->var);
+			$post = LAN_FORUM_2006;
+		}
+		if($this->var['action'] == "nt")
+		{
+			$pre = LAN_FORUM_1001;
+			$name = $tp->toHTML($this->var['forum_name'], false, 'no_hook, emotes_off');
+			$url = e107::url('forum', 'forum', $this->var);
+			$post = LAN_FORUM_2005;
+		}
+
+		$title = ''; // todo?
+
+		return $pre . ($url ? ": <a {$title} href='" . $url . "'>{$name}</a> - " : $name) . $post;
 	}
 	
 	function sc_noemotes()
