01d87ade4f50881352f60ca690a8b76a1d9b39ae
Cameron <e107inc@gmail.com>
2019-05-27 11:17:41 -0700

    Fixes #1818 - Batch Userclass list missing 'members' in default list.

--- e107_handlers/admin_ui.php
+++ e107_handlers/admin_ui.php
@@ -7526,14 +7526,14 @@ class e_admin_form_ui extends e_form
 					break;
 
 					case 'userclass':
-						$classes = e107::getUserClass()->uc_required_class_list(vartrue($parms['classlist'], 'public,nobody,guest,admin,main,classes'));
+						$classes = e107::getUserClass()->uc_required_class_list(vartrue($parms['classlist'], 'public,nobody,guest,member,admin,main,classes'));
 						foreach($classes as $k => $name)
 						{
 							$option[$key.'__'.$k] = $name;
 						}
 					break;
 					case 'userclasses':
-						$classes = e107::getUserClass()->uc_required_class_list(vartrue($parms['classlist'], 'public,nobody,guest,admin,main,classes'));
+						$classes = e107::getUserClass()->uc_required_class_list(vartrue($parms['classlist'], 'public,nobody,guest,member,admin,main,classes'));
 						$_option = array();
 						
 						if($type === 'batch')
