1d4f1afc25fc6225e07394f1685cd022577898f5
Cameron <e107inc@gmail.com>
2019-05-28 12:04:25 -0700

    Removed $forum global .

--- e107_plugins/forum/shortcodes/batch/view_shortcodes.php
+++ e107_plugins/forum/shortcodes/batch/view_shortcodes.php
@@ -1027,22 +1027,19 @@
 
 		function sc_moderators()
 		{
-			global $forum;
-
 			$modUser = array();
-			foreach($forum->modArray as $user)
+			foreach($this->forum->modArray as $user)
 			{
 				$modUser[] = "<a href='" . e107::getUrl()->create('user/profile/view', $user) . "'>" . $user['user_name'] . "</a>";
 			}
 
-//$tVars->MODERATORS = LAN_FORUM_2003.": ". implode(', ', $modUser);
+
 			return LAN_FORUM_2003 . ": " . implode(', ', $modUser);
-//unset($modUser);
+
 		}
 
 		function sc_threadstatus()
 		{
-//$tVars->THREADSTATUS = (!$thread->threadInfo['thread_active'] ? LAN_FORUM_2004 : '');
 			return (!$this->var['thread_active'] ? LAN_FORUM_2004 : '');
 		}
 
