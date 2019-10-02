9a0335f480cf0e46042e25c343eeebd1a6e7cb86
Tijn Kuyper <github@tijnkuyper.nl>
2019-10-02 15:41:30 +0200

    Revert "Fixes #1933 - Only allow rating a user with full stars (not half a star)"
    This reverts commit dee70490498e0f82acb5c6bfaefd958559f0a6f6.

--- e107_web/js/core/all.jquery.js
+++ e107_web/js/core/all.jquery.js
@@ -1301,7 +1301,7 @@ $(document).ready(function()
 		
     		$('#'+tmp).raty({
     			path		: path,
-    			half  		: false,
+    			half  		: true,
     			score    	: score,
     			readOnly	: readonly,
     			hints		: hintArray,
