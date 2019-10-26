8cbb43f302f8be1f7e834600d1683821b1ad84a2
Tijn Kuyper <github@tijnkuyper.nl>
2019-10-26 15:39:10 +0200

    SEF urls for download search results

--- e107_plugins/download/e_search.php
+++ e107_plugins/download/e_search.php
@@ -51,14 +51,15 @@ class download_search extends e_search // include plugin-folder in the name.
 					'd.download_id', 
 					'd.download_sef',
 					'd.download_category', 
-					'c.download_category_id', 
 					'd.download_name', 
 					'd.download_description', 
 					'd.download_author', 
 					'd.download_author_website', 
 					'd.download_datestamp', 
 					'd.download_class', 
-					'c.download_category_name', 
+					'c.download_category_id',
+					'c.download_category_name',
+					'c.download_category_sef', 
 					'c.download_category_class'
 			),
 
@@ -88,7 +89,9 @@ class download_search extends e_search // include plugin-folder in the name.
 	{
 		$tp = e107::getParser();
 
-		//TODO SEF URLs for list below. 
+		$download_index_url 	= 	e107::url('download', 'index');
+		$download_category_url 	= 	e107::url('download', 'category', $row);
+
 		//TODO Remove html from pre_summary and use additional vars instead. 
 
 		$res = array();
@@ -98,14 +101,11 @@ class download_search extends e_search // include plugin-folder in the name.
 		$res['link'] 		= e107::url('download', 'item', $row);
 		$res['pre_title'] 	= $tp->toHTML($row['download_category_name'],false,'TITLE_PLAIN')." | ";
 		$res['title'] 		= $row['download_name'];
-		$res['pre_summary'] = "<div class='smalltext'><a href='download.php'>".LAN_197."</a> -> <a href='download.php?list.".$row['download_category_id']."'>".$row['download_category_name']."</a></div>";
+		$res['pre_summary'] = "<div class='smalltext'><a href='".$download_index_url."'>".LAN_197."</a> -> <a href='".$download_category_url."'>".$row['download_category_name']."</a></div>";
 		$res['summary'] 	= $row['download_description'];
 		$res['detail'] 		= LAN_SEARCH_15." ".$row['download_author']." | ".LAN_SEARCH_66.": ".$datestamp;
 
-
-
 		return $res;
-		
 	}
 
 
