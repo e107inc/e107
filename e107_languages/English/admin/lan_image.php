5bf921fac33154d3b0b8b1d7d4234724bf6833cc
Gábor <yesszus@gmail.com>
2019-12-09 10:10:26 +0100

    Update lan_image.php

--- e107_languages/English/admin/lan_image.php
+++ e107_languages/English/admin/lan_image.php
@@ -210,3 +210,4 @@ define("IMALAN_182", "Convert to jpeg during import");
 define("IMALAN_183", "PNG and GIF files will be automatically converted to jpeg format. (icons excluded)");
 define("IMALAN_184", "Default Image Sizes");
 define("IMALAN_185", "Maximum height in pixels");
+define("IMALAN_186", "Enter some text to filter results");
