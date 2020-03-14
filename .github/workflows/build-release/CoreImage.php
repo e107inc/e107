f8c77e9c07c2b1f964c18c5ba59d42b5cae2b0d0
Deltik <deltik@gmx.com>
2020-03-14 12:33:30 -0500

    JSON reimplementation of CoreImage

--- .github/workflows/build-release/CoreImage.php
+++ .github/workflows/build-release/CoreImage.php
@@ -12,41 +12,15 @@ require_once("OsHelper.php");
 
 class CoreImage
 {
-	protected $coredir = [];
-
-	public function __construct($_current, $_deprecated, $_image)
+	public function __construct($exportFolder, $currentVersion, $imageFile)
 	{
 		set_time_limit(240);
 
-		$maindirs = array(
-			'admin' => 'e107_admin/',
-			'files' => 'e107_files/',
-			'images' => 'e107_images/',
-			'themes' => 'e107_themes/',
-			'plugins' => 'e107_plugins/',
-			'handlers' => 'e107_handlers/',
-			'languages' => 'e107_languages/',
-			'downloads' => 'e107_files/downloads/',
-			'docs' => 'e107_docs/'
-		);
-
-		foreach ($maindirs as $maindirs_key => $maindirs_value)
-		{
-			$this->coredir[$maindirs_key] = substr($maindirs_value, 0, -1);
-		}
-
-		$this->create_image($_current, $_deprecated, $_image);
+		$this->create_image($exportFolder, $currentVersion, $imageFile);
 	}
 
-	function create_image($_curdir, $_depdir, $_image)
+	function create_image($exportFolder, $currentVersion, $imageFile)
 	{
-		$search = $replace = [];
-		foreach ($this->coredir as $trim_key => $trim_dirs)
-		{
-			$search[$trim_key] = "'" . $trim_dirs . "'";
-			$replace[$trim_key] = "\$coredir['" . $trim_key . "']";
-		}
-
 		$data = "<?php\n";
 		$data .= "/*\n";
 		$data .= "+ ----------------------------------------------------------------------------+\n";
@@ -65,39 +39,32 @@ class CoreImage
 		$data .= "*/\n\n";
 		$data .= "if (!defined('e107_INIT')) { exit; }\n\n";
 
-		$scan_current = $this->scan($_curdir);
+		echo("[Core-Image] Scanning Dir: " . $exportFolder . "\n");
+		$carry = $this->generateCurrentChecksums($exportFolder, $currentVersion);
 
+		echo("[Core-Image] Scanning Removed Files from Git" . "\n");
+		$result = $this->generateRemovedChecksums($carry);
 
-		echo("[Core-Image] Scanning Dir: " . $_curdir . "\n");
+		$json_result = json_encode($result, JSON_PRETTY_PRINT);
+		$json_string_result = var_export($json_result, true);
+		$data .= '$core_image = ' . $json_string_result . ';';
 
-
-		$image_array = var_export($scan_current, true);
-		$image_array = str_replace($search, $replace, $image_array);
-		$data .= "\$core_image = " . $image_array . ";\n\n";
-
-		$scan_deprecated = $this->generateRemovedChecksums();
-		$image_array = var_export($scan_deprecated, true);
-		$image_array = str_replace($search, $replace, $image_array);
-		$data .= "\$deprecated_image = " . $image_array . ";\n\n";
-		$data .= "?>";
-
-		$fp = fopen($_image, 'w');
+		$fp = fopen($imageFile, 'w');
 		fwrite($fp, $data);
 	}
 
-	protected function scan($dir)
+	protected function generateCurrentChecksums($exportFolder, $currentVersion)
 	{
-		$absoluteBase = realpath($dir);
+		$absoluteBase = realpath($exportFolder);
 		if (!is_dir($absoluteBase)) return false;
 
-		$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-		$files = [];
+		$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($exportFolder));
+		$checksums = [];
 
 		/**
 		 * @var $file DirectoryIterator
 		 */
-		foreach ($iterator as $file)
-		{
+		foreach ($iterator as $file) {
 			if ($file->isDir()) continue;
 
 			$absolutePath = $file->getRealPath();
@@ -105,55 +72,15 @@ class CoreImage
 
 			if (empty($relativePath) || $relativePath == $absolutePath) continue;
 
-			self::array_set($files, $relativePath, $this->checksumPath($absolutePath));
+			$checksum = $this->checksumPath($absolutePath);
+			$item = self::array_get($checksums, $relativePath, []);
+			if (!in_array($checksum, $item)) $item["v{$currentVersion}"] = $checksum;
+			self::array_set($checksums, $relativePath, $item);
 		}
 
-		ksort($files);
+		ksort($checksums);
 
-		return $files;
-	}
-
-	/**
-	 * Set an array item to a given value using "slash" notation.
-	 *
-	 * If no key is given to the method, the entire array will be replaced.
-	 *
-	 * Based on Illuminate\Support\Arr::set()
-	 *
-	 * @param array $array
-	 * @param string|null $key
-	 * @param mixed $value
-	 * @return array
-	 * @copyright Copyright (c) Taylor Otwell
-	 * @license https://github.com/illuminate/support/blob/master/LICENSE.md MIT License
-	 */
-	private static function array_set(&$array, $key, $value)
-	{
-		if (is_null($key))
-		{
-			return $array = $value;
-		}
-
-		$keys = explode('/', $key);
-
-		while (count($keys) > 1)
-		{
-			$key = array_shift($keys);
-
-			// If the key doesn't exist at this depth, we will just create an empty array
-			// to hold the next value, allowing us to create the arrays to hold final
-			// values at the correct depth. Then we'll keep digging into the array.
-			if (!isset($array[$key]) || !is_array($array[$key]))
-			{
-				$array[$key] = [];
-			}
-
-			$array = &$array[$key];
-		}
-
-		$array[array_shift($keys)] = $value;
-
-		return $array;
+		return $checksums;
 	}
 
 	protected function checksumPath($filename)
@@ -164,51 +91,6 @@ class CoreImage
 	protected function checksum($body)
 	{
 		return md5(str_replace(array(chr(13), chr(10)), '', $body));
-	}
-
-	protected function generateRemovedChecksums()
-	{
-		$checksums = [];
-
-		$stdout = '';
-		OsHelper::runValidated('git tag --list ' . escapeshellarg("v*"), $stdout);
-		$tags = explode("\n", trim($stdout));
-		$versions = [];
-		foreach ($tags as $tag)
-		{
-			$versions[] = preg_replace("/^v/", "", $tag);
-		}
-		$tags = array_combine($tags, $versions);
-		unset($versions);
-		uasort($tags, function ($a, $b)
-		{
-			return -version_compare($a, $b);
-		});
-		$tags = array_filter($tags, function ($version)
-		{
-			return !preg_match("/[a-z]/i", $version);
-		});
-
-		foreach ($tags as $tag => $version)
-		{
-			OsHelper::runValidated(
-				'git --no-pager diff --name-only --diff-filter D ' . escapeshellarg($tag),
-				$stdout
-			);
-			$removedFiles = explode("\n", trim($stdout));
-			foreach ($removedFiles as $removedFilePath)
-			{
-				OsHelper::runValidated(
-					'git --no-pager show ' . escapeshellarg($tag . ":" . $removedFilePath),
-					$stdout
-				);
-				$checksum = $this->checksum($stdout);
-				$item = self::array_get($checksums, $removedFilePath, []);
-				if (!in_array($checksum, $item)) $item["v{$version}"] = $checksum;
-				self::array_set($checksums, $removedFilePath, $item);
-			}
-		}
-		return $checksums;
 	}
 
 	/**
@@ -229,10 +111,8 @@ class CoreImage
 
 		if (isset($array[$key])) return $array[$key];
 
-		foreach (explode('/', $key) as $segment)
-		{
-			if (!is_array($array) || !array_key_exists($segment, $array))
-			{
+		foreach (explode('/', $key) as $segment) {
+			if (!is_array($array) || !array_key_exists($segment, $array)) {
 				return $default;
 			}
 
@@ -240,5 +120,85 @@ class CoreImage
 		}
 
 		return $array;
+	}
+
+	/**
+	 * Set an array item to a given value using "slash" notation.
+	 *
+	 * If no key is given to the method, the entire array will be replaced.
+	 *
+	 * Based on Illuminate\Support\Arr::set()
+	 *
+	 * @param array $array
+	 * @param string|null $key
+	 * @param mixed $value
+	 * @return array
+	 * @copyright Copyright (c) Taylor Otwell
+	 * @license https://github.com/illuminate/support/blob/master/LICENSE.md MIT License
+	 */
+	private static function array_set(&$array, $key, $value)
+	{
+		if (is_null($key)) {
+			return $array = $value;
+		}
+
+		$keys = explode('/', $key);
+
+		while (count($keys) > 1) {
+			$key = array_shift($keys);
+
+			// If the key doesn't exist at this depth, we will just create an empty array
+			// to hold the next value, allowing us to create the arrays to hold final
+			// values at the correct depth. Then we'll keep digging into the array.
+			if (!isset($array[$key]) || !is_array($array[$key])) {
+				$array[$key] = [];
+			}
+
+			$array = &$array[$key];
+		}
+
+		$array[array_shift($keys)] = $value;
+
+		return $array;
+	}
+
+	protected function generateRemovedChecksums($carry = [])
+	{
+		$checksums = $carry;
+
+		$stdout = '';
+		OsHelper::runValidated('git tag --list ' . escapeshellarg("v*"), $stdout);
+		$tags = explode("\n", trim($stdout));
+		$versions = [];
+		foreach ($tags as $tag) {
+			$versions[] = preg_replace("/^v/", "", $tag);
+		}
+		$tags = array_combine($tags, $versions);
+		unset($versions);
+		uasort($tags, function ($a, $b) {
+			return -version_compare($a, $b);
+		});
+		$tags = array_filter($tags, function ($version) {
+			return !preg_match("/[a-z]/i", $version);
+		});
+
+		foreach ($tags as $tag => $version) {
+			OsHelper::runValidated(
+				'git --no-pager diff --name-only --diff-filter D ' . escapeshellarg($tag),
+				$stdout
+			);
+			$removedFiles = explode("\n", trim($stdout));
+			foreach ($removedFiles as $removedFilePath) {
+				OsHelper::runValidated(
+					'git --no-pager show ' . escapeshellarg($tag . ":" . $removedFilePath),
+					$stdout
+				);
+				$checksum = $this->checksum($stdout);
+				$item = self::array_get($checksums, $removedFilePath, []);
+				if (!in_array($checksum, $item)) $item["v{$version}"] = $checksum;
+				self::array_set($checksums, $removedFilePath, $item);
+			}
+		}
+		return $checksums;
 	}
 }
