404464fe72bbbc333bc0cf3cd56d76e3383cce16
Cameron <e107inc@gmail.com>
2020-05-13 13:48:07 -0700

    Added usersettings bootstrap 4 template file.

---
+++ e107_core/templates/bootstrap4/usersettings_template.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2009 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ *
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_themes/templates/usersettings_template.php,v $
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+if (!defined('e107_INIT')) { exit; }
+	
+	
+// e107 v2. bootstrap4 compatible template. 
+$USERSETTINGS_WRAPPER = array();
+
+$USERSETTINGS_WRAPPER['edit']['USERNAME'] =				"<div class='form-group row'>
+															<label for='username' class='col-sm-3 col-form-label'>".LAN_USER_01."</label>
+														    <div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+
+$USERSETTINGS_WRAPPER['edit']['LOGINNAME'] = 			"<div class='form-group row'>
+															<label for='loginname' class='col-sm-3 col-form-label'>".LAN_USER_81."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+$USERSETTINGS_WRAPPER['edit']['PASSWORD1'] = 			"<div class='form-group row'>
+															<label for='password1' class='col-sm-3 col-form-label'>".LAN_USET_24."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+$USERSETTINGS_WRAPPER['edit']['PASSWORD2'] =			"<div class='form-group row'>
+															<label for='password2' class='col-sm-3 col-form-label'>".LAN_USET_25."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+$USERSETTINGS_WRAPPER['edit']['REALNAME'] =			"<div class='form-group row'>
+															<label for='realname' class='col-sm-3 col-form-label'>".LAN_USER_63.req(e107::getPref('signup_option_realname'))."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+$USERSETTINGS_WRAPPER['edit']['CUSTOMTITLE'] =			"<div class='form-group row'>
+															<label for='customtitle' class='col-sm-3 col-form-label'>".LAN_USER_04.':'.req(e107::getPref('signup_option_customtitle'))."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+$USERSETTINGS_WRAPPER['edit']['USERCLASSES'] = 			"<div class='form-group row'>
+															<label  class='col-sm-3'>".LAN_USER_76.":".req(e107::getPref('signup_option_class'))."</label>
+														       	<div class='col-sm-9'>{---}</div>
+														   </div>
+														";
+
+$USERSETTINGS_WRAPPER['edit']['AVATAR_UPLOAD'] = 		"<div class='form-group row'>
+														<label for='avatar' class='col-sm-3 col-form-label'>".LAN_USET_26."</label>
+												       	<div class='col-sm-9'>{---}</div>
+													   	</div>
+														";
+$USERSETTINGS_WRAPPER['edit']['PHOTO_UPLOAD'] = 		"<div class='form-group row'>
+														<label for='photo' class='col-sm-3 col-form-label'>".LAN_USER_06."</label>
+												       	<div class='col-sm-9'>{---}</div>
+													   	</div>
+														";
+														
+														
+
+$USERSETTINGS_WRAPPER['edit']['SIGNATURE']			= "<div class='form-group row'>
+														<label for='signature' class='col-sm-3 col-form-label'>".LAN_USER_71.req(e107::getPref('signup_option_signature'))."</label>
+												       	<div class='col-sm-9'>{---}</div>
+													   	</div>
+													 ";
+
+	// $USERSETTINGS_WRAPPER['edit']['USEREXTENDED_ALL']	= "<div class='form-group row'>{---}</div>";
+
+
+
+
+
+// Bootstrap 3 only.
+
+$USERSETTINGS_TEMPLATE = array();
+
+$USERSETTINGS_TEMPLATE['edit'] = "
+
+<div>
+
+	{USERNAME}
+
+	{LOGINNAME}
+
+
+	<div class='form-group row'>
+	<label for='email' class='col-sm-3 col-form-label'>".LAN_USER_60.req(!e107::getPref('disable_emailcheck'))."</label>
+	<div class='col-sm-9'>
+		{EMAIL}
+	</div>
+	</div>
+
+	{REALNAME}
+
+	{CUSTOMTITLE}
+
+	{PASSWORD1}
+
+	{PASSWORD2}
+
+
+	<div class='form-group row'>
+	<label for='hideemail' class='col-sm-3'>".LAN_USER_83."</label>
+        <div class='col-sm-9'>
+                {HIDEEMAIL=radio}
+        </div>
+	</div>
+
+	<div class='form-group row'>
+	<label class='col-sm-3 col-form-label'>".LAN_USER_07.req(e107::getPref('signup_option_image'))."</label>
+        <div class='col-sm-9'>
+        {AVATAR_REMOTE}
+        </div>
+	</div>
+
+	{AVATAR_UPLOAD}
+	{PHOTO_UPLOAD}
+
+
+	{USERCLASSES}
+	{USEREXTENDED_ALL=tabs}
+
+	{SIGNATURE}
+	{SIGNATURE_HELP}
+
+	 <div class='form-group row'>
+      <div class='offset-sm-3 col-sm-9'>
+		{UPDATESETTINGSBUTTON}
+		{DELETEACCOUNTBUTTON}
+	</div>
+	</div>
+
+</div>
+";
+
+$USERSETTINGS_TEMPLATE['extended-category'] = "<h3>{CATNAME}</h3>";
+$USERSETTINGS_TEMPLATE['extended-field'] = "<div class='form-group row'>
+	<label class='col-sm-3 col-form-label'>{FIELDNAME} {REQUIRED}</label>
+	<div class='col-sm-9'>
+	{FIELDVAL} {HIDEFIELD}
+	</div>
+	</div>
+											";
+
+
