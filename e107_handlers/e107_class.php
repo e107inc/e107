e107inc/e107

commit e7a79edcd0993f8a4078ceb0321290ac7864ceba

Moved core-image to e107_system/ and renamed to core_image.phar

e107_handlers/e107_class.php
@@ -1615,7 +1615,7 @@ class e107
 	 */
     public static function getFileInspector($type = 'core')
     {
-        $fileInspectorPath = realpath(e_ADMIN . "core_image.php");
+        $fileInspectorPath = realpath(e_SYSTEM . "core_image.phar");
         /** @var e_file_inspector $fileInspector */
         $fileInspector = self::getObject('e_file_inspector_json_phar', $fileInspectorPath);
 

e107_handlers/e_file_inspector.php
@@ -68,7 +68,7 @@ abstract class e_file_inspector implements e_file_inspector_interface
             return realpath($path) ? realpath($path) : $path;
         }, [
                 $appRoot . "e107_config.php",
-                $appRoot . e107::getFolder('admin') . "core_image.php",
+                $appRoot . e107::getFolder('system') . "core_image.phar",
             ]
         );
         $this->existingInsecureFiles = array_filter($this->insecureFiles, function ($path)

.github/workflows/build-release/e107_make.php
@@ -252,7 +252,7 @@ class e107Build
 				$this->createCoreImage(); // Create Image
 			}
 
-			$this->copyCoreImage($this->exportDir . "e107_admin/core_image.php");
+			$this->copyCoreImage($this->exportDir . "e107_system/core_image.phar");
 
 			if (isset($rel['readme']))
 			{

e107_admin/lancheck.php
@@ -354,22 +354,6 @@ class lancheck
 
 		$acceptedLans = explode(",",e_LANLIST);
 	
-		
-		if(!isset($_SESSION['lancheck-core-image']))
-		{
-			$core = array();	
-
-			$coredir = array('admin' => 'e107_admin', 'files' => 'e107_files', 'images' => 'e107_images', 'themes' => 'e107_themes', 'plugins' => 'e107_plugins', 'handlers' => 'e107_handlers', 'languages' => 'e107_languages', 'downloads' => 'e107_downloads', 'docs' => 'e107_docs');
-
-			$core_image = array();
-
-			require_once(e_ADMIN."core_image.php");
-			
-			unset($core_image['e107_images'],$core_image['e107_files'],$core_image['e107_admin']);
-			
-			$_SESSION['lancheck-core-image'] = $core_image;	
-		}
-
 
 		if(!empty($_POST['ziplang']))
 		{

commit 308f5a6b8370b263b9bfc5ef7cad9e3a96435699

core_image.phar was being looked up at the wrong path
Correcting a regression from e7a79edcd0993f8a4078ceb0321290ac7864ceba

Fixes: #4164

e107_handlers/e107_class.php
@@ -1615,7 +1615,7 @@ class e107
 	 */
     public static function getFileInspector($type = 'core')
     {
-        $fileInspectorPath = realpath(e_SYSTEM . "core_image.phar");
+        $fileInspectorPath = realpath(e_SYSTEM_BASE . "core_image.phar");
         /** @var e_file_inspector $fileInspector */
         $fileInspector = self::getObject('e_file_inspector_json_phar', $fileInspectorPath);
 

e107_handlers/e_file_inspector.php
@@ -68,7 +68,7 @@ abstract class e_file_inspector implements e_file_inspector_interface
             return realpath($path) ? realpath($path) : $path;
         }, [
                 $appRoot . "e107_config.php",
-                $appRoot . e107::getFolder('system') . "core_image.phar",
+                $appRoot . e107::getFolder('system_base') . "core_image.phar",
             ]
         );
         $this->existingInsecureFiles = array_filter($this->insecureFiles, function ($path)