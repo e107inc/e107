6174806a064c2e81459a0b9c214b72af9883a022
Cameron <e107inc@gmail.com>
2020-06-15 13:01:09 -0700

    Backward Compat. file added.

---
+++ e107_handlers/emote.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2020 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+     * This empty file has been left here deliberately for backward compatibility.
+	 */
+
