403739543a17e2d781fccbe339f5dd517d793924
Cameron <e107inc@gmail.com>
2020-07-03 13:49:12 -0700

    Issue #4206  - news category menu {active} shortcode typo fix.

--- e107_plugins/tagcloud/tagcloud_menu.php
+++ e107_plugins/tagcloud/tagcloud_menu.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
  * e107 website system
@@ -9,9 +8,12 @@
  *
  */
 
-if (!defined('e107_INIT')) { exit; }
+if (!defined('e107_INIT'))
+{
+	exit;
+}
 
-if (!e107::isInstalled('tagcloud')) 
+if (!e107::isInstalled('tagcloud'))
 {
 	return '';
 }
@@ -20,99 +22,99 @@ require_once('tagcloud_class.php');
 
 
 // http://lotsofcode.github.io/tag-cloud/
-
-class tagcloud_menu
+if (!class_exists('tagcloud_menu'))
 {
-	
-  public $template = array();
-  
-	function __construct()
+	class tagcloud_menu
 	{
-		 $this->template = e107::getTemplate('tagcloud','tagcloud_menu','default');
- 
-	}	
-	
-	function render($parm=null)
-	{
- 
-    $cloud = new TagCloud();
-		$sql = e107::getDb();
-		
-		e107::getCache()->setMD5(e_LANGUAGE);
-		
-		if($text = e107::getCache()->retrieve('tagcloud',5,false))
+
+		public $template = array();
+
+		function __construct()
 		{
-			return $text;	
+			$this->template = e107::getTemplate('tagcloud', 'tagcloud_menu', 'default');
+
 		}
 
-		$nobody_regexp = "'(^|,)(".str_replace(",", "|", e_UC_NOBODY).")(,|$)'";
-		
-		if($result = $sql->retrieve('news','news_id,news_meta_keywords', "news_meta_keywords !='' AND news_class REGEXP '".e_CLASS_REGEXP."' AND NOT (news_class REGEXP ".$nobody_regexp.")
-				AND news_start < ".time()." AND (news_end=0 || news_end>".time().")", true))
+		function render($parm = null)
 		{
-			foreach($result as $row)
+
+			$cloud = new TagCloud();
+			$sql = e107::getDb();
+
+			e107::getCache()->setMD5(e_LANGUAGE);
+
+			if ($text = e107::getCache()->retrieve('tagcloud', 5, false))
 			{
-				
-				$tmp = explode(",", $row['news_meta_keywords']);
-				foreach($tmp as $word)
-				{
-					//$newsUrlparms = array('id'=> $row['news_id'], 'name'=>'a name');
-					$url = e107::getUrl()->create('news/list/tag',array('tag'=>$word)); // SITEURL."news.php?tag=".$word;	
-					$cloud->addTag(array('tag' => $word, 'url' => $url));
-					
-				}
+				return $text;
 			}
-			
+
+			$nobody_regexp = "'(^|,)(" . str_replace(",", "|", e_UC_NOBODY) . ")(,|$)'";
+
+			if ($result = $sql->retrieve('news', 'news_id,news_meta_keywords', "news_meta_keywords !='' AND news_class REGEXP '" . e_CLASS_REGEXP . "' AND NOT (news_class REGEXP " . $nobody_regexp . ")
+					AND news_start < " . time() . " AND (news_end=0 || news_end>" . time() . ")", true))
+			{
+				foreach ($result as $row)
+				{
+
+					$tmp = explode(",", $row['news_meta_keywords']);
+					foreach ($tmp as $word)
+					{
+						//$newsUrlparms = array('id'=> $row['news_id'], 'name'=>'a name');
+						$url = e107::getUrl()->create('news/list/tag', array('tag' => $word)); // SITEURL."news.php?tag=".$word;
+						$cloud->addTag(array('tag' => $word, 'url' => $url));
+
+					}
+				}
+
+			}
+			else
+			{
+				$text = "No tags Found";
+			}
+
+			$cloud->setHtmlizeTagFunction(function ($tag, $size)
+			{
+				$tp = e107::getParser();
+				$var = array('TAG_URL' => $tag['url'],
+					'TAG_SIZE' => $size,
+					'TAG_NAME' => $tag['tag'],
+					'TAG_COUNT' => $tag['size'],
+				);
+
+				$text = $tp->simpleParse($this->template['item'], $var);
+				//$text = "<a class='tag' href='".$tag['url']."'><span class='size".$size."'>".$tag['tag']."</span></a> ";
+
+				return $text;
+			});
+
+			$cloud->setOrder('size', 'DESC');
+
+			$limit = !empty($parm['tagcloud_limit']) ? intval($parm['tagcloud_limit']) : 50;
+
+			$cloud->setLimit($limit);
+
+			$text = $cloud->render();
+
+			e107::getCache()->set('tagcloud', $text, true);
+
+			//$text .= "<div style='clear:both'></div>";   moved to $template['default']['end']
+
+			return $text;
+
 		}
-		else
-		{
-			$text = "No tags Found";
-		}
-		
-		$cloud->setHtmlizeTagFunction( function($tag, $size) 
-		{
-			$tp = e107::getParser();
-      $var = array('TAG_URL' => $tag['url'],
-                   'TAG_SIZE' => $size,
-                   'TAG_NAME' => $tag['tag'],
-                   'TAG_COUNT' => $tag['size'],
-       );
- 
-      $text =  $tp->simpleParse($this->template['item'], $var);
-      //$text = "<a class='tag' href='".$tag['url']."'><span class='size".$size."'>".$tag['tag']."</span></a> ";
- 
-      return $text;
-		});
-		
-		$cloud->setOrder('size','DESC');
 
-		$limit = !empty($parm['tagcloud_limit']) ? intval($parm['tagcloud_limit']) : 50;
 
-		$cloud->setLimit($limit);
-		
-		$text = $cloud->render();
-		
-		e107::getCache()->set('tagcloud', $text, true);
-
-		//$text .= "<div style='clear:both'></div>";   moved to $template['default']['end']
-		
-		return $text;	
-		
 	}
-	
-	
-	
 }
-
 /* TODO: add template type as parm, now always default */
 $tag = new tagcloud_menu;
 $text = $tag->render($parm);
 
 
-if(!empty($parm))
+if (!empty($parm))
 {
 
-	if(isset($parm['tagcloud_caption'][e_LANGUAGE]))
+	if (isset($parm['tagcloud_caption'][e_LANGUAGE]))
 	{
 		$caption = $parm['tagcloud_caption'][e_LANGUAGE];
 	}
@@ -124,14 +126,13 @@ else
 
 $var = array('TAGCLOUD_MENU_CAPTION' => $caption);
 
-$caption =  e107::getParser()->simpleParse($tag->template['caption'], $var);
-       
-$start = $tag->template['start']; 
-$end = $tag->template['end']; 
- 
-e107::getRender()->tablerender($caption, $start.$text.$end, 'tagcloud_menu');
+$caption = e107::getParser()->simpleParse($tag->template['caption'], $var);
+
+$start = $tag->template['start'];
+$end = $tag->template['end'];
+
+e107::getRender()->tablerender($caption, $start . $text . $end, 'tagcloud_menu');
 
 
 
 
-?>
