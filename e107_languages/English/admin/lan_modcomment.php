ae04eb74ecf66b4365c70c5e7c9687a937ead15e
leo <the_guardian2@hotmail.com>
2020-11-21 16:03:18 +0100

    Add files via upload
    missing file from repo

---
+++ e107_languages/English/admin/lan_modcomment.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2020 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Admin Language File
+ *
+*/
+
+define("MDCLAN_1", "Moderated.");
+define("MDCLAN_2", "No comments for this item");
+define("MDCLAN_3", "Member");
+define("MDCLAN_4", "Guest");
+define("MDCLAN_5", "unblock");
+define("MDCLAN_6", "block");
+
+define("MDCLAN_8", "Moderate Comments");
+define("MDCLAN_9", "Warning! Deleting Parent comments will also delete all replies!");
+
+define("MDCLAN_10", "options");
+define("MDCLAN_11", "comment");
+define("MDCLAN_12", "comments");
+define("MDCLAN_13", "blocked");
+define("MDCLAN_14", "lock comments");
+define("MDCLAN_15", "open");
+define("MDCLAN_16", "locked");
