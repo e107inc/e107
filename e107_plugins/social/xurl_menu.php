25b5843e121126afdf8554d4f163b98ab5b41a4e
Cameron <e107inc@gmail.com>
2020-12-28 08:55:20 -0800

    Additional menu config items added.

--- e107_plugins/social/xurl_menu.php
+++ e107_plugins/social/xurl_menu.php
@@ -2,8 +2,10 @@
 
 require_once(e_PLUGIN."social/e_shortcode.php");
 $sc = new social_shortcodes;
-$body = $sc->sc_xurl_icons();
-$title = empty($parm['xurlCaption'][e_LANGUAGE]) ? LAN_PLUGIN_SOCIAL_NAME : $parm['xurlCaption'][e_LANGUAGE];
+
+$body = $sc->sc_xurl_icons($parm);
+
+$title = isset($parm['caption'][e_LANGUAGE]) ? (string) $parm['caption'][e_LANGUAGE] : LAN_PLUGIN_SOCIAL_NAME ;
 
 e107::getRender()->tablerender($title, $body, 'social-xurl-menu');
 
