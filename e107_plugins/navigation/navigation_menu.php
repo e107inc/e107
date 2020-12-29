30c185704b2d3f843833563f93029476df4726b3
Cameron <e107inc@gmail.com>
2020-12-29 12:48:57 -0800

    Navigation Menu added.

---
+++ e107_plugins/navigation/navigation_menu.php
@@ -0,0 +1,14 @@
+<?php
+
+                        
+$parm['type'] = !empty($parm['type']) ? $parm['type'] : 'main';
+$parm['layout'] = !empty($parm['layout']) ? $parm['layout'] : $parm['type'];
+
+require_once(e_CORE."shortcodes/single/navigation.php");
+        
+$text = navigation_shortcode($parm);        
+
+$caption = isset($parm['caption'][e_LANGUAGE]) ? $parm['caption'][e_LANGUAGE] : LAN_PLUGIN_NAVIGATION_NAME;
+
+e107::getRender()->tablerender($caption, $text);
+                            
