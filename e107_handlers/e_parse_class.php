075ce7d68fa61fc27225e0563f0cf6cd105aa0fe
Cameron <e107inc@gmail.com>
2020-12-30 07:57:43 -0800

    BS5 fixes.

--- e107_handlers/e_parse_class.php
+++ e107_handlers/e_parse_class.php
@@ -4521,8 +4521,12 @@ class e_parser
 		}
 		
 		$title = (ADMIN) ? $image : $tp->toAttribute($userData['user_name']);
-		$shape = (!empty($options['shape'])) ? 'img-' .$options['shape'] : 'img-rounded rounded';
+		$shape = (!empty($options['shape'])) ? 'img-' .$options['shape'] : 'img-rounded rounded ';
 
+		if($shape === 'img-circle')
+		{
+			$shape .= " rounded-circle";
+		}
 
 		if(!empty($options['type']) && $options['type'] === 'url')
 		{
