5f1fc512fc7b2538d50f1736229be6083d1026b1
Cameron <e107inc@gmail.com>
2020-12-31 07:29:05 -0800

    Signin template tweaks.

--- e107_plugins/signin/templates/signin_template.php
+++ e107_plugins/signin/templates/signin_template.php
@@ -42,7 +42,7 @@ $SIGNIN_TEMPLATE['signin'] = '
 
 
 
-$SIGNIN_WRAPPER['signout']['SIGNIN_ADMIN_HREF'] = '<li class="list-group-item"><a class="signin-sc admin" id="signin-sc-admin" href="{---}"><span class="fa fa-cogs"></span> {LAN=LAN_LOGINMENU_11}</a></li>';
+$SIGNIN_WRAPPER['signout']['SIGNIN_ADMIN_HREF'] = '<li><a class="dropdown-item signin-sc admin" id="signin-sc-admin" href="{---}"><span class="fa fa-cogs"></span> {LAN=LAN_LOGINMENU_11}</a></li>';
 $SIGNIN_WRAPPER['signout']['SIGNIN_PM_NAV'] = '<li class="dropdown">{---}</li>';
 
 
@@ -52,15 +52,15 @@ $SIGNIN_TEMPLATE['signout'] = '
 			{SIGNIN_PM_NAV}
 			<li class="dropdown dropdown-avatar"><a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" data-toggle="dropdown">{USER_AVATAR: w=30&h=30&crop=1&shape=circle} {SIGNIN_USERNAME} <b class="caret"></b></a>
 				<ul class="dropdown-menu  ">
-				<li class="list-group-item">
-					<a href="{SIGNIN_USERSETTINGS_HREF}"><span class="fa fa-cog"></span> {LAN=LAN_SETTINGS}</a>
+				<li>
+					<a class="dropdown-item" href="{SIGNIN_USERSETTINGS_HREF}"><span class="fa fa-cog"></span> {LAN=LAN_SETTINGS}</a>
 				</li>
-				<li class="list-group-item">
-					<a role="button" href="{SIGNIN_PROFILE_HREF}"><span class="fa fa-user"></span> {LAN=LAN_LOGINMENU_13}</a>
+				<li>
+					<a class="dropdown-item" role="button" href="{SIGNIN_PROFILE_HREF}"><span class="fa fa-user"></span> {LAN=LAN_LOGINMENU_13}</a>
 				</li>
 				<li class="divider"></li>
 				{SIGNIN_ADMIN_HREF}
-				<li class="list-group-item"><a href="{SIGNIN_LOGOUT_HREF}"><span class="fa fa-power-off"></span> {LAN=LAN_LOGOUT}</a></li>
+				<li><a class="dropdown-item" href="{SIGNIN_LOGOUT_HREF}"><span class="fa fa-power-off"></span> {LAN=LAN_LOGOUT}</a></li>
 				</ul>
 			</li>
 		</ul>
