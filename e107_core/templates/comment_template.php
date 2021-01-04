26899dcfbc0a0aacc78d6631419ef4109ed674fc
Cameron <e107inc@gmail.com>
2021-01-04 08:16:21 -0800

    Comment template cleanup.

--- e107_core/templates/comment_template.php
+++ e107_core/templates/comment_template.php
@@ -15,86 +15,18 @@
  */
 
 if (!defined('e107_INIT')) { exit; }
-if (!defined("USER_WIDTH")){ define("USER_WIDTH", "width:100%"); }
-
-global $sc_style;
-global $pref, $comrow, $row2, $tp, $NEWIMAGE, $USERNAME, $RATING;
-
-$sc_style['SUBJECT']['pre'] = "";
-$sc_style['SUBJECT']['post'] = "";
-
-$sc_style['USERNAME']['pre'] = "";
-$sc_style['USERNAME']['post'] = "";
-
-$sc_style['TIMEDATE']['pre'] = "<small>";
-$sc_style['TIMEDATE']['post'] = "</small>";
-
-$sc_style['AVATAR']['pre'] = "";
-$sc_style['AVATAR']['post'] = "";
-
-$sc_style['COMMENTS']['pre'] = "";
-$sc_style['COMMENTS']['post'] = "<br />";
-
-$sc_style['JOINED']['pre'] = "";
-$sc_style['JOINED']['post'] = "<br />";
-
-$sc_style['COMMENT']['pre'] = "";
-$sc_style['COMMENT']['post'] = "<br />";
-
-$sc_style['RATING']['pre'] = "";
-$sc_style['RATING']['post'] = "<br />";
-
-$sc_style['IPADDRESS']['pre'] = "";
-$sc_style['IPADDRESS']['post'] = "<br />";
-
-$sc_style['LEVEL']['pre'] = "";
-$sc_style['LEVEL']['post'] = "<br />";
-
-$sc_style['LOCATION']['pre'] = "";
-$sc_style['LOCATION']['post'] = "<br />";
-
-$sc_style['SIGNATURE']['pre'] = "";
-$sc_style['SIGNATURE']['post'] = "<br />";
 
 
+// Shortcode wrappers.
+$COMMENT_WRAPPER['item']['COMENT_TIMEDATE']     = '<small>{---}</small>';
+$COMMENT_WRAPPER['item']['COMMENT_EDIT']        = '<span class="comment-edit">{---}</span>';
+$COMMENT_WRAPPER['item']['COMENT_REPLY']		= '<span class="comment-reply">{---}</span>';
+$COMMENT_WRAPPER['item']['COMMENT_AVATAR']  	= '<div class="comment-avatar center">{---}</div>';
+$COMMENT_WRAPPER['item']['COMMENT_MODERATE']	= '<span class="comment-moderate">{---}</span>';
 
-// from e107.org 
-$sc_style['REPLY']['pre'] 				= "<span class='comment-reply'>";
-$sc_style['REPLY']['post'] 				= "</span>";
+$COMMENT_WRAPPER['form'] = $COMMENT_WRAPPER['item']; // use the above wrappers for the 'form' as well.
 
-$sc_style['COMMENTEDIT']['pre']  		= '<span class="comment-edit">';
-$sc_style['COMMENTEDIT']['post'] 		= '</span>';
-
-$sc_style['COMMENT_AVATAR']['pre']  	= '<div class="comment-avatar center">';
-$sc_style['COMMENT_AVATAR']['post'] 	= '</div>';
-
-$sc_style['SUBJECT_INPUT']['pre']		= ""; //COMLAN_324
-$sc_style['SUBJECT_INPUT']['post']		= "";
-
-$sc_style['AUTHOR_INPUT']['pre']		= ""; // COMLAN_16
-$sc_style['AUTHOR_INPUT']['post']		= "";
-
-$sc_style['COMMENT_INPUT']['pre']		= "";// COMLAN_8
-$sc_style['COMMENT_INPUT']['post']		= "";
-
-$sc_style['COMMENT_BUTTON']['pre']		= "";
-$sc_style['COMMENT_BUTTON']['post']		= "";
-
-$sc_style['COMMENT_SHARE']['pre']		= "";
-$sc_style['COMMENT_SHARE']['post']		= "";
-
-$sc_style['COMMENT_RATE']['pre']  		= '';
-$sc_style['COMMENT_RATE']['post'] 		= '';
-
-//$sc_style['USER_AVATAR']['pre']  		= '<div class="comment-avatar center">';
-//$sc_style['USER_AVATAR']['post'] 		= '</div>';
-
-$sc_style['COMMENT_MODERATE']['pre']	= '<span class="comment-moderate">';
-$sc_style['COMMENT_MODERATE']['post']	= '</span>';
-
-$sc_style['MODERATE']['pre']	= '<div class="span12" style="padding:10px">';
-$sc_style['MODERATE']['post']	= '</div>';
-
+// Templates
 
 $COMMENT_TEMPLATE['form']			= "
 	{SETIMAGE: w=90&h=90&crop=1}
@@ -118,6 +50,7 @@ $COMMENT_TEMPLATE['form']			= "
 
 
 
+
 $COMMENT_TEMPLATE['item'] = '
 		{SETIMAGE: w=90&h=90&crop=1}
 		<div class="media-object comment-box-left pull-left span1">
@@ -127,7 +60,7 @@ $COMMENT_TEMPLATE['item'] = '
 			<div class="row">
 				
 					<div class="comment-box-username span2 col-xs-6 col-sm-6 col-md-6">{USERNAME}</div>
-					<div class="comment-box-date span2 col-xs-6 col-sm-6  col-md-6 text-right text-muted">{TIMEDATE=relative}</div>
+					<div class="comment-box-date span2 col-xs-6 col-sm-6  col-md-6 text-right text-muted">{COMMENT_TIMEDATE=relative}</div>
 
 			</div>
 			
@@ -140,7 +73,7 @@ $COMMENT_TEMPLATE['item'] = '
 			</div>	
 			<div class="row">
 				<div class="comment-status span2 col-sm-12 col-md-6">{COMMENT_STATUS}</div>
-					<div class="comment-moderate span6 col-sm-12 col-md-6 text-right">{COMMENT_RATE} {REPLY} {COMMENTEDIT} {COMMENT_MODERATE}</div>
+					<div class="comment-moderate span6 col-sm-12 col-md-6 text-right">{COMMENT_RATE} {COMMENT_REPLY} {COMMENT_EDIT} {COMMENT_MODERATE}</div>
 			</div>
 			
 				
