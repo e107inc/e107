452f594ad2c9d2839dc423f84485c49eb69fb853
Cameron <e107inc@gmail.com>
2021-01-08 09:56:33 -0800

    Issue #4336 and tests for e107::plugLan() (e107::lan()) . Error message will be displayed in debug mode when a LAN file failed to load.

--- e107_plugins/page/page_menu.php
+++ e107_plugins/page/page_menu.php
@@ -10,7 +10,14 @@ $tp = e107::getParser();
 $template = e107::getCoreTemplate('page','panel');
 
 //TODO Limits and cache etc. 
-$data = $sql->retrieve("SELECT * FROM #page WHERE page_class IN (".USERCLASS_LIST.") AND FIND_IN_SET('panel', page_template) LIMIT 3", true);
+if(!$data = $sql->retrieve("SELECT * FROM #page WHERE page_class IN (".USERCLASS_LIST.") AND FIND_IN_SET('panel', page_template) LIMIT 3", true))
+{
+	if(ADMIN)
+	{
+		echo "<div class='alert alert-danger'>There are no page items assigned to the 'panel' template.</div>";
+	}
+	return null;
+}
 
 //TODO Use shortcodes and template. 
 foreach($data as $row)
