277f2a915ee6d6fd38a812796b6fe514c89ec1d9
Gábor <yesszus@gmail.com>
2021-01-30 13:10:39 +0100

    Create English_admin.php

---
+++ e107_plugins/hero/languages/English/English_admin.php
@@ -0,0 +1,29 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/12/18 12:42:44
+|
+|        $Author: Yesszus $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_HERO_SUCC", "Default slides imported.");
+define("LAN_HERO_ERR", "Failed to import default slides.");
+define("LAN_HERO_BACKGROUND", "Background");
+define("LAN_HERO_IMAGE_VIDEO", "Image/Video");
+define("LAN_HERO_MEDIA", "Media");
+define("LAN_HERO_LABEL", "Label");
+define("LAN_HERO_HELP", "Be sure to enable the hero menu on your home page using the Menu Manager.");
+define("LAN_HERO_BULLETS", "Bullets");
+define("LAN_HERO_ICON_PACK", "Icon pack");
+define("LAN_HERO_ICON_STYLE", "Icon-style");
+define("LAN_HERO_SLIDE_INTERVAL", "Slide interval");
+define("LAN_HERO_TEXT", "Text");
+define("LAN_HERO_ANIMATION", "Animation");
+define("LAN_HERO_DELAY", "Delay (sec)");
+define("LAN_HERO_ALERT_DANGER", "Hero only runs on the frontpage of your site.");
+define("LAN_HERO_BUTTON", "Button");
+define("LAN_HERO_SEC", "second(s)");
