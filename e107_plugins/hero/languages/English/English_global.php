ff53ab954d1985e5d53575d5fbf81490942d2bbc
Gábor <yesszus@gmail.com>
2021-01-30 13:09:15 +0100

    Create English_global.php

---
+++ e107_plugins/hero/languages/English/English_global.php
@@ -0,0 +1,13 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/12/18 12:42:44
+|
+|        $Author: Yesszus $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_HERO_DIZ", "Image and text slider, animated bullet points for the hero area of your home page.");
