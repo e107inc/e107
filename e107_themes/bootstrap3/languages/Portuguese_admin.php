9314cbcea5c79ecc4c58dc37edef999421a27fae
rica-carv <rica-carv@users.noreply.github.com>
2021-02-12 11:00:20 +0000

    Added Portuguese_admin language file
    Added Portuguese_admin language file

---
+++ e107_themes/bootstrap3/languages/Portuguese_admin.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Portuguese Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2021/02/12 10:54:37
+|
+|        $Author: Ricardo $
++---------------------------------------------------------------+
+*/
+define("LAN_THEMEPREF_00", "Marca:");
+define("LAN_THEMEPREF_01", "Alinhamento da barra de navegação:");
+define("LAN_THEMEPREF_02", "Localização do registo / entrada:");
+define("LAN_THEMEPREF_03", "Estilos do Bootswatch:");
+define("LAN_THEMEPREF_04", "Nome da página");
+define("LAN_THEMEPREF_05", "Logotipo");
+define("LAN_THEMEPREF_06", "Logotipo &amp; Nome da página");
+define("LAN_THEMEPREF_07", "esquerda");
+define("LAN_THEMEPREF_08", "direita");
+define("LAN_THEMEPREF_09", "topo");
+define("LAN_THEMEPREF_10", "base");
