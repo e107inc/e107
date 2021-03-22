79ded794b14e47058ef9489bd4f46a25b87c2be7
Cameron <e107inc@gmail.com>
2021-03-22 11:43:13 -0700

    Initial Bootstrap 5 theme added.

---
+++ e107_themes/bootstrap5/README.md
@@ -0,0 +1,2 @@
+# bootstrap5
+bootstrap5 theme for e107 v2.3.1+
---
+++ e107_themes/bootstrap5/languages/English_admin.php
@@ -0,0 +1,11 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEMEPREF_01", "Bootswatch Styles:");
+define("LAN_THEMEPREF_02", "Display standard menu with card look");
+define("LAN_THEMEPREF_03", "Display Login Page as standard page");
