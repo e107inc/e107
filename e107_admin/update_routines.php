e107inc/e107

commit 402d1f4126286f070c1c4e0d2a9819b3fb5e15ec

Closes #1801 Code cleanup. Deprecated method errors added.

e107_admin/update_routines.php
@@ -1370,7 +1370,7 @@ function update_706_to_800($type='')
 	if ($nt_changed)
 	{
 		$s_prefs = $tp -> toDB($notify_prefs);
-		$s_prefs = $eArrayStorage -> WriteArray($s_prefs);
+		$s_prefs = e107::serialize($s_prefs);
 		// Could we use $sysprefs->set($s_prefs,'notify_prefs') instead - avoids caching problems  ????
 		$status = ($sql -> update("core", "e107_value='".$s_prefs."' WHERE e107_name='notify_prefs'") !== FALSE) ? E_MESSAGE_DEBUG : E_MESSAGE_ERROR;
 		$message = str_replace('[x]',$nt_changed,LAN_UPDATE_20);

e107_admin/banlist.php
@@ -771,7 +771,7 @@ class banlist_form_ui extends e_admin_form_ui
 
 
 
-				return $this->selectbox('banlist_bantype',$ipAdministrator->banTypes, $curVal);
+				return $this->select('banlist_bantype',$ipAdministrator->banTypes, $curVal);
 			break;
 			
 			case 'filter':
@@ -817,7 +817,7 @@ class banlist_form_ui extends e_admin_form_ui
 					$opts[$curVal] = e107::getParser()->toDate($curVal, 'short');
 				}
 
-				return $this->selectbox('banlist_banexpires',$opts, $curVal);
+				return $this->select('banlist_banexpires',$opts, $curVal);
 				// return $frm->text('banlist_banexpires',$curVal);		
 			break;
 			

e107_admin/cache.php
@@ -50,7 +50,7 @@ if (isset($_POST['submit_cache']))
 
 if (isset($_POST['trigger_empty_cache']))
 {
-	e107::getLog()->logSuccess(CACLAN_6);
+	e107::getLog()->addSuccess(CACLAN_6);
 	switch ($_POST['option_clear_cache'])
 	{
 		case 'empty_contentcache':

e107_admin/db.php
@@ -22,7 +22,7 @@ if(!getperms('0'))
 
 if(isset($_POST['back']))
 {
-	header("location: ".e_SELF);
+	e107::redirect(e_SELF);
 	exit();
 }
 

e107_admin/links.php
@@ -247,7 +247,7 @@ class links_admin_ui extends e_admin_ui
 									<option value='{$key}'{$selected}>".$type['title']."</option>
 					";*/
 		}
-		$text .= $ui->selectbox('sublink_type', $optarrayp, $this->getPosted('sublink_type'), '', true);
+		$text .= $ui->select('sublink_type', $optarrayp, $this->getPosted('sublink_type'), '', true);
 
 		$text .= "
 							</td>
@@ -677,7 +677,7 @@ class links_admin_form_ui extends e_admin_form_ui
 				$cats	= $this->getController()->getLinkArray($catid);
 				$ret	= array();
 				$this->_parent_select_array(0, $cats, $ret);
-				return $this->selectbox('link_parent', $ret, $value, array('size'=>'xlarge','default' => LAN_SELECT."..."));
+				return $this->select('link_parent', $ret, $value, array('size'=>'xlarge','default' => LAN_SELECT."..."));
 			break;
 
 			case 'batch':
@@ -700,7 +700,7 @@ class links_admin_form_ui extends e_admin_form_ui
 
 		if($mode == 'write')
 		{			
-			return $this->selectbox('link_function',$this->linkFunctions,$curVal,array('size'=>'xlarge','default'=> "(".LAN_OPTIONAL.")"));
+			return $this->select('link_function',$this->linkFunctions,$curVal,array('size'=>'xlarge','default'=> "(".LAN_OPTIONAL.")"));
 		}
 
 		else

e107_admin/meta.php
@@ -44,11 +44,11 @@ if (isset($_POST['metasubmit']))
 		$meta_author[$lan] = $pref['meta_author'][$lan];
 	}
 
-	$meta_tag[e_LANGUAGE] = strip_if_magic(rtrim($_POST['meta']));
-	$meta_diz[e_LANGUAGE] = strip_if_magic(rtrim($_POST['meta_description']));
-	$meta_keywords[e_LANGUAGE] = strip_if_magic(rtrim($_POST['meta_keywords']));
-	$meta_copyright[e_LANGUAGE] = strip_if_magic(rtrim($_POST['meta_copyright']));
-	$meta_author[e_LANGUAGE] = strip_if_magic(rtrim($_POST['meta_author']));
+	$meta_tag[e_LANGUAGE] = rtrim($_POST['meta']);
+	$meta_diz[e_LANGUAGE] = rtrim($_POST['meta_description']);
+	$meta_keywords[e_LANGUAGE] = rtrim($_POST['meta_keywords']);
+	$meta_copyright[e_LANGUAGE] = rtrim($_POST['meta_copyright']);
+	$meta_author[e_LANGUAGE] = rtrim($_POST['meta_author']);
 
     $pref['meta_news_summary'] = intval($_POST['meta_news_summary']);
 	$pref['meta_tag'] = $meta_tag;

e107_admin/plugin.php
@@ -43,7 +43,7 @@ if(!deftrue('e_DEBUG_PLUGMANAGER'))
 
 if(isset($_POST['uninstall_cancel']))
 {
-	header("location:".e_SELF);
+	e107::redirect(e_SELF);
 	exit;		
 }
 

commit 0057d24ede6b6592889197e2cff3d5157aa91665

Fix upgrade routine
Was broken in https://github.com/e107inc/e107/commit/402d1f4126286f070c1c4e0d2a9819b3fb5e15ec (addslashes was default with WriteArray())

e107_admin/update_routines.php
@@ -1386,7 +1386,7 @@ function update_706_to_800($type='')
 	if ($nt_changed)
 	{
 		$s_prefs = $tp -> toDB($notify_prefs);
-		$s_prefs = e107::serialize($s_prefs);
+		$s_prefs = e107::serialize($s_prefs, true);
 		// Could we use $sysprefs->set($s_prefs,'notify_prefs') instead - avoids caching problems  ????
 		$status = ($sql -> update("core", "e107_value='".$s_prefs."' WHERE e107_name='notify_prefs'") !== FALSE) ? E_MESSAGE_DEBUG : E_MESSAGE_ERROR;
 		$message = str_replace('[x]',$nt_changed,LAN_UPDATE_20);