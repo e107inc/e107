6b4561fc965ac309a6abc5d56fab9bffc639d43e
Cameron <e107inc@gmail.com>
2022-12-10 08:47:01 -0800

    Closes #4919 - Enhancement: plugins may now use their own custom email templates with sendEmail();

--- e107_plugins/_blank/templates/_blank_template.php
+++ e107_plugins/_blank/templates/_blank_template.php
@@ -9,8 +9,17 @@
 	 */
 
 
-	$_BLANK_WRAPPER['default']['BLANK_TEST'] = "[ {---} ]";
-	$_BLANK_TEMPLATE['default'] = "<div>{BLANK_TEST}</div>";
+$_BLANK_WRAPPER['default']['BLANK_TEST'] = "[ {---} ]";
+$_BLANK_TEMPLATE['default'] = "<div>{BLANK_TEST}</div>";
 
 
-	$_BLANK_TEMPLATE['other'] = "<div>{BLANK_TEST}</div>";
+$_BLANK_TEMPLATE['other'] = "<div>{BLANK_TEST}</div>";
+$_BLANK_TEMPLATE['other'] = "<div>{BLANK_TEST}</div>";
+
+/**
+ * Custom Plugin email template 
+ * @see https://github.com/e107inc/e107/issues/4919
+ */
+$_BLANK_TEMPLATE['email']['header'] = '<html lang="en"><body>';
+$_BLANK_TEMPLATE['email']['body'] = "<div><span>{NAME}</span> <small>{DATE}</small></div><div>{BODY}</div>";
+$_BLANK_TEMPLATE['email']['footer'] = '</body></html>';
