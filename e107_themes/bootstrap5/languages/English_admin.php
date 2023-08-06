8f95213773b86987540cf63aaf149e174b6df449
Jimako <janamicek@gmail.com>
2023-08-06 17:53:28 +0200

    Update English_admin.php

--- e107_themes/bootstrap5/languages/English_admin.php
+++ e107_themes/bootstrap5/languages/English_admin.php
@@ -6,6 +6,12 @@
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_THEMEPREF_01", "Bootswatch Styles:");
+define("LAN_THEMEPREF_00", "Branding:");
+define("LAN_THEMEPREF_01", "Navbar Alignment:");
 define("LAN_THEMEPREF_02", "Display standard menu with card look");
 define("LAN_THEMEPREF_03", "Display Login Page as standard page");
+define("LAN_THEMEPREF_04", "Site Name");
+define("LAN_THEMEPREF_05", "Logo");
+define("LAN_THEMEPREF_06", "Logo &amp; Site Name");
+define("LAN_THEMEPREF_07", "left");
+define("LAN_THEMEPREF_08", "right");
