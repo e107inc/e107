f62050cd017ab7ca181e6c4a60c1e3e31e671049
camer0n <e107inc@gmail.com>
2023-11-20 14:31:52 -0800

    Fixes #5108 - newsfeed image field length fix.

--- e107_plugins/newsfeed/newsfeed_sql.php
+++ e107_plugins/newsfeed/newsfeed_sql.php
@@ -5,7 +5,7 @@ CREATE TABLE newsfeed (
   newsfeed_data longtext NOT NULL,
   newsfeed_timestamp int(10) unsigned NOT NULL default '0',
   newsfeed_description text NOT NULL,
-  newsfeed_image varchar(100) NOT NULL default '',
+  newsfeed_image varchar(250) NOT NULL default '',
   newsfeed_active tinyint(1) unsigned NOT NULL default '0',
   newsfeed_updateint int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (newsfeed_id)
