9c3c52e116153f7729f5f257b53a02993972865f
Moc <github@tijnkuyper.nl>
2023-11-30 20:56:51 +0100

    Fixes #3612 - user_ban_flood notification now shows IP in IPv4 format if possible
    (cherry picked from commit 05727b8b52e98893b70d0086adaabbacb619d95d)

--- e107_handlers/online_class.php
+++ e107_handlers/online_class.php
@@ -294,8 +294,9 @@ class e_online
 					e107::lan('core','banlist',true);//e_LANGUAGEDIR.e_LANGUAGE.'/admin/lan_banlist.php'
 					$reason = e107::getParser()->lanVars(BANLAN_78,$row['online_pagecount']); //  str_replace('--HITS--',$row['online_pagecount'], BANLAN_78)
 
-					if (true === e107::getIPHandler()->add_ban(2, $reason, $ip,0))
+					if (true === e107::getIPHandler()->add_ban(2, $reason, $ip, 0))
 					{
+						$ip = e107::getIPHandler()->ipDecode($ip);
 						e107::getEvent()->trigger('flood', $ip); //BC
 						e107::getEvent()->trigger('user_ban_flood', $ip);
 						exit;
