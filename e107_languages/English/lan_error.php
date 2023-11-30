ea8d8084d1bd85c18be96faae27b0b4e6cfe69de
Moc <github@tijnkuyper.nl>
2023-11-30 20:47:01 +0100

    Fixes #3438 - Remove duplicate LAN_ERROR_*

--- e107_languages/English/lan_error.php
+++ e107_languages/English/lan_error.php
@@ -21,16 +21,16 @@ define("LAN_ERROR_3", "Please inform the administrator of the referring page if 
 
 define("LAN_ERROR_4", "Error 403 - Access forbidden");
 define("LAN_ERROR_5", "You are not permitted to retrieve the document or page you requested.");
-define("LAN_ERROR_6", "Please inform the administrator of the referring page if you think this error page has been shown by mistake.");
+//define("LAN_ERROR_6", "Please inform the administrator of the referring page if you think this error page has been shown by mistake."); // use LAN_ERROR_3
 
 define("LAN_ERROR_7", "Error 404 - Document Not Found");
-define("LAN_ERROR_9", "Please inform the administrator of the referring page if you think this error message has been shown by mistake.");
+//define("LAN_ERROR_9", "Please inform the administrator of the referring page if you think this error message has been shown by mistake."); // use LAN_ERROR_3
 define("LAN_ERROR_10", "Error 500 - Internal server error");
 define("LAN_ERROR_11", "The server encountered an internal error or misconfiguration and was unable to complete your request");
-define("LAN_ERROR_12", "Please inform the administrator of the referring page if you think this error page has been shown by mistake.");
+//define("LAN_ERROR_12", "Please inform the administrator of the referring page if you think this error page has been shown by mistake."); // use LAN_ERROR_3
 define("LAN_ERROR_13", "Error - Unknown");
 define("LAN_ERROR_14", "The server encountered an error");
-define("LAN_ERROR_15", "Please inform the administrator of the referring page if you think this error page has been shown by mistake.");
+//define("LAN_ERROR_15", "Please inform the administrator of the referring page if you think this error page has been shown by mistake."); // use LAN_ERROR_3
 define("LAN_ERROR_16", "Your unsuccessful attempt to access");
 define("LAN_ERROR_17", "has been recorded.");
 define("LAN_ERROR_18", "Apparently, you were referred here by");
