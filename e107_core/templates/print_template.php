b568f3aa094ea0b4d1b528ea7f2c5accfe13ef31
rica-carv <rica-carv@users.noreply.github.com>
2025-01-19 22:04:55 +0000

    Update print_template.php

--- e107_core/templates/print_template.php
+++ e107_core/templates/print_template.php
@@ -5,6 +5,6 @@ $PRINT_TEMPLATE['default'] = '<div style="background-color:white">
 							<div style="text-align:{ALIGN}">{LOGO: h=100}</div>
 							<hr />
 							<div style="text-align:{ALIGN}; margin-bottom:50px">{TEXT}</div>
-							<div class="hidden-print" style="text-align:center">{BUTTON}</div>
+							<div style="text-align:center">{BUTTON}</div>
 							</div>';
 
