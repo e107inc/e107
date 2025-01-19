1b16be775f13043df9b5b893a987d599a6bfac24
rica-carv <rica-carv@users.noreply.github.com>
2025-01-19 18:22:41 +0000

    Update emailprint_template.php

--- e107_core/templates/emailprint_template.php
+++ e107_core/templates/emailprint_template.php
@@ -13,5 +13,5 @@ else // BC
 	$EMAILPRINT_TEMPLATE['ICON_PRINT']  = "<img src='".e_IMAGE_ABS."generic/printer.png'  alt='".LAN_PRINT_1."'  />";	
 }
 
-$EMAILPRINT_TEMPLATE['email'] = "<a rel='alternate' class='e-tip hidden-print btn btn-default btn-secondary hidden-print' href='{EMAILICON: url=1}' title='".LAN_EMAIL_7."'>452553523".$EMAILPRINT_TEMPLATE['ICON_EMAIL']."</a>";
+$EMAILPRINT_TEMPLATE['email'] = "<a rel='alternate' class='e-tip hidden-print btn btn-default btn-secondary hidden-print' href='{EMAILICON: url=1}' title='".LAN_EMAIL_7."'>".$EMAILPRINT_TEMPLATE['ICON_EMAIL']."</a>";
 $EMAILPRINT_TEMPLATE['print'] = "<a rel='alternate' class='e-tip btn btn-default btn-secondary hidden-print' href='{PRINTICON: url=1}' title='".LAN_PRINT_1."'>".$EMAILPRINT_TEMPLATE['ICON_PRINT']."</a>";
