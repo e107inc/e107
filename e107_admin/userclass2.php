643ace47af252687e6067cb1149e11be78b6668b
camer0n <e107inc@gmail.com>
2025-04-21 17:54:42 -0700

    Issue #5477 - Escalated default values for userclass visibility and management when creating user classes.

--- e107_admin/userclass2.php
+++ e107_admin/userclass2.php
@@ -126,9 +126,9 @@ e107::coreLan('userclass2', true);
 
 			// Set Defaults for when creating new records.
 			$this->fields['userclass_type']['writeParms']                   = array(UC_TYPE_STD => UCSLAN_80,	UC_TYPE_GROUP => UCSLAN_81);
-			$this->fields['userclass_editclass']['writeParms']['default']   = e_UC_ADMIN;
+			$this->fields['userclass_editclass']['writeParms']['default']   = e_UC_MAINADMIN;
 			$this->fields['userclass_parent']['writeParms']['default']      = e_UC_NOBODY;
-			$this->fields['userclass_visibility']['writeParms']['default']  = e_UC_ADMIN;
+			$this->fields['userclass_visibility']['writeParms']['default']  = e_UC_MAINADMIN;
 			$this->fields['userclass_id']['writeParms']['default']          =$this->getUserClassAdmin()->findNewClassID();
 
 			if(getperms('0'))
