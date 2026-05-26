848ec830dc70afaed19ecf38844ece9d2b461408
leo <the_guardian2@hotmail.com>
2026-05-26 15:59:07 +0200

    Array
    lan file define to array

--- e107_themes/bootstrap3/languages/English_admin.php
+++ e107_themes/bootstrap3/languages/English_admin.php
@@ -6,17 +6,17 @@
 
 +----------------------------------------------------------------------------+
 */
+return[
 
-define("LAN_THEMEPREF_00", "Branding:");
-define("LAN_THEMEPREF_01", "Navbar Alignment:");
-define("LAN_THEMEPREF_02", "Signup/Login Placement:");
-define("LAN_THEMEPREF_03", "Bootswatch Styles:");
-define("LAN_THEMEPREF_04", "Site Name");
-define("LAN_THEMEPREF_05", "Logo");
-define("LAN_THEMEPREF_06", "Logo &amp; Site Name");
-define("LAN_THEMEPREF_07", "left");
-define("LAN_THEMEPREF_08", "right");
-define("LAN_THEMEPREF_09", "top");
-define("LAN_THEMEPREF_10", "bottom");
-
-?>
+'LAN_THEMEPREF_00' => "Branding:",
+'LAN_THEMEPREF_01' => "Navbar Alignment:",
+'LAN_THEMEPREF_02' => "Signup/Login Placement:",
+'LAN_THEMEPREF_03' => "Bootswatch Styles:",
+'LAN_THEMEPREF_04' => "Site Name",
+'LAN_THEMEPREF_05' => "Logo",
+'LAN_THEMEPREF_06' => "Logo &amp; Site Name",
+'LAN_THEMEPREF_07' => "left",
+'LAN_THEMEPREF_08' => "right",
+'LAN_THEMEPREF_09' => "top",
+'LAN_THEMEPREF_10' => "bottom",
+];
