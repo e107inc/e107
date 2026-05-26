362e57f1b41b3e28cb4cf01a0cb10a86cb23438e
leo <the_guardian2@hotmail.com>
2026-05-26 14:32:07 +0200

    Array
    using lan array mode

--- e107_plugins/newforumposts_main/languages/English.php
+++ e107_plugins/newforumposts_main/languages/English.php
@@ -1,35 +1,36 @@
 <?php
+return[
 	
-define("NFPM_LAN_1", "Thread");
-define("NFPM_LAN_2", "Poster");
-define("NFPM_LAN_3", "Views");
-define("NFPM_LAN_4", "Replies");
-define("NFPM_LAN_5", "Lastpost");
-define("NFPM_LAN_6", "Threads");
-define("NFPM_LAN_7", "by");
+'NFPM_LAN_1' => "Thread".
+'NFPM_LAN_2' => "Poster",
+'NFPM_LAN_3' => "Views",
+'NFPM_LAN_4' => "Replies",
+'NFPM_LAN_5' => "Lastpost",
+'NFPM_LAN_6' => "Threads",
+'NFPM_LAN_7' => "by",
 	
-define("NFPM_L1", "This plugin displays a list of new forum posts on your front page");
-define("NFPM_L2", "Latest Forum Posts");
-define("NFPM_L3", "To configure please click on the link in the plugins section of the admin front page");
-define("NFPM_L4", "Activate in which area?");
-define("NFPM_L5", "Inactive");
-define("NFPM_L6", "Top of page");
-define("NFPM_L7", "Bottom of page");
-define("NFPM_L8", "Caption");
-define("NFPM_L9", "Number of new posts to display?");
-define("NFPM_L10", "Display inside scrolling layer?");
-define("NFPM_L11", "Layer height");
-define("NFPM_L12", "New Forum Posts Configuration");
-define("NFPM_L13", "New Forum Posts settings updated.");
-define("NFPM_L14", "Display latest forum posts?");
-define("NFPM_L15", "By default, latest topics are shown.");
-define('NFPM_L16', '[user deleted]');
-define('NFPM_L17', 'New Posts on Popular Thread');
-define('NFPM_L18', 'New Posts');
-define('NFPM_L19', 'No New Posts on Popular Thread');
-define('NFPM_L20', 'No New Posts');
-define('NFPM_L21', 'Sticky Thread');
-define('NFPM_L22', 'Closed Sticky Thread');
-define('NFPM_L23', 'Announcement');
-define('NFPM_L24', 'Closed Thread');	
-	
+'NFPM_L1' => "This plugin displays a list of new forum posts on your front page",
+'NFPM_L2' => "Latest Forum Posts",
+'NFPM_L3' => "To configure please click on the link in the plugins section of the admin front page",
+'NFPM_L4' => "Activate in which area?",
+'NFPM_L5' => "Inactive",
+'NFPM_L6' => "Top of page",
+'NFPM_L7' => "Bottom of page",
+'NFPM_L8' => "Caption",
+'NFPM_L9' => "Number of new posts to display?",
+'NFPM_L10' => "Display inside scrolling layer?",
+'NFPM_L11' => "Layer height",
+'NFPM_L12' => "New Forum Posts Configuration",
+'NFPM_L13' => "New Forum Posts settings updated.",
+'NFPM_L14' => "Display latest forum posts?",
+'NFPM_L15' => "By default, latest topics are shown.",
+'NFPM_L16' => "[user deleted]",
+'NFPM_L17' => "New Posts on Popular Thread",
+'NFPM_L18' => "New Posts",
+'NFPM_L19' => "No New Posts on Popular Thread",
+'NFPM_L20' => "No New Posts",
+'NFPM_L21' => "Sticky Thread",
+'NFPM_L22' => "Closed Sticky Thread",
+'NFPM_L23' => "Announcement",
+'NFPM_L24' => "Closed Thread",
+];
