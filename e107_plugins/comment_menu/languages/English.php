4e4f0b731365cea42ee8ffdf5b2c7ffe73c57613
leo <the_guardian2@hotmail.com>
2026-05-26 15:50:30 +0200

    Array
    lan file define to array

--- e107_plugins/comment_menu/languages/English.php
+++ e107_plugins/comment_menu/languages/English.php
@@ -9,19 +9,17 @@
  * $URL$
  * $Id$
 */
+return[
 
-define("CM_L1", "No comments yet.");
-define("CM_L2", "");
-define("CM_L3", "Caption");
-define("CM_L4", "Number of comments to display?");
-define("CM_L5", "Number of characters to display?");
-define("CM_L6", "Postfix for too long comments?");
-define("CM_L7", "Show original news title in menu?");
-define("CM_L8", "New Comments Menu Configuration");
-//define("CM_L9", "Update Menu Settings");
-//define("CM_L10", "Comments menu configuration saved");
-define("CM_L11", "on");
-define("CM_L12", "Re:");
-define("CM_L13", "Posted by");
-
-
+'CM_L1'=> "No comments yet.",
+'CM_L2'=> "",
+'CM_L3'=> "Caption",
+'CM_L4'=> "Number of comments to display?",
+'CM_L5'=> "Number of characters to display?",
+'CM_L6'=> "Postfix for too long comments?",
+'CM_L7'=> "Show original news title in menu?",
+'CM_L8'=> "New Comments Menu Configuration",
+'CM_L11'=> "on",
+'CM_L12'=> "Re:",
+'CM_L13'=> "Posted by",
+];
