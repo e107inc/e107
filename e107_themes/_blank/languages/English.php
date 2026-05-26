0374130c0ee31177e914459e800d07fc90dc2b20
leo <the_guardian2@hotmail.com>
2026-05-26 14:34:07 +0200

    array
    using lan array mode

--- e107_themes/_blank/languages/English.php
+++ e107_themes/_blank/languages/English.php
@@ -1,13 +1,17 @@
 <?php
-define("EMESSLAN_TITLE_INFO", "System Information");
-//define("EMESSLAN_TITLE_ERROR", "Error");//new > LAN_ERROR
-define("EMESSLAN_TITLE_SUCCESS", "Success");
-define("EMESSLAN_TITLE_WARNING", "Warning");
-define("EMESSLAN_TITLE_DEBUG", "System Debug");
 
-define("LAN_THEME_1", "Comments are turned off for this item");
-define("LAN_THEME_2", "Read/Post Comment: ");
-define("LAN_THEME_3", "Read the rest...");
-// define("LAN_THEME_4", "Trackbacks: ");
-define("LAN_THEME_5", "Posted by ");
-define("LAN_THEME_6", "on ");
+return[
+
+'EMESSLAN_TITLE_INFO' => "System Information",
+//'EMESSLAN_TITLE_ERROR' => "Error",//new > LAN_ERROR
+'EMESSLAN_TITLE_SUCCESS' => "Success",
+'EMESSLAN_TITLE_WARNING' => "Warning",
+'EMESSLAN_TITLE_DEBUG' => "System Debug",
+
+'LAN_THEME_1' => "Comments are turned off for this item",
+'LAN_THEME_2' => "Read/Post Comment: ",
+'LAN_THEME_3' => "Read the rest...",
+// 'LAN_THEME_4' => "Trackbacks: ",
+'LAN_THEME_5' => "Posted by ",
+'LAN_THEME_6' => "on ",
+];
