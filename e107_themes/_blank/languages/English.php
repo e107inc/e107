f8453e9bc384b9add1b7aa7c8f6b4623303c7f7a
leo <the_guardian2@hotmail.com>
2026-05-27 18:48:48 +0200

    Array
    define to array

--- e107_themes/_blank/languages/English.php
+++ e107_themes/_blank/languages/English.php
@@ -1,15 +1,14 @@
 <?php
 
-return[
-
-'EMESSLAN_TITLE_INFO' => "System Information",
-'EMESSLAN_TITLE_SUCCESS' => "Success",
-'EMESSLAN_TITLE_WARNING' => "Warning",
-'EMESSLAN_TITLE_DEBUG' => "System Debug",
-'LAN_THEME_1' => "Comments are turned off for this item",
-'LAN_THEME_2' => "Read/Post Comment: ",
-'LAN_THEME_3' => "Read the rest...",
+return [
+    'EMESSLAN_TITLE_INFO' => "System Information",
+    'EMESSLAN_TITLE_SUCCESS' => "Success",
+    'EMESSLAN_TITLE_WARNING' => "Warning",
+    'EMESSLAN_TITLE_DEBUG' => "System Debug",
+    'LAN_THEME_1' => "Comments are turned off for this item",
+    'LAN_THEME_2' => "Read/Post Comment: ",
+    'LAN_THEME_3' => "Read the rest...",
 // 'LAN_THEME_4' => "Trackbacks: ",
-'LAN_THEME_5' => "Posted by ",
-'LAN_THEME_6' => "on ",
+    'LAN_THEME_5' => "Posted by ",
+    'LAN_THEME_6' => "on ",
 ];
