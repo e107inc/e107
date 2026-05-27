e54f1d7e7824daac0096d0148ff8784e64c020f2
leo <the_guardian2@hotmail.com>
2026-05-27 18:51:17 +0200

    Array
    define to array

--- e107_themes/bootstrap5/languages/English_admin.php
+++ e107_themes/bootstrap5/languages/English_admin.php
@@ -5,15 +5,15 @@
 |
 +----------------------------------------------------------------------------+
 */
-return[
 
-'LAN_THEMEPREF_00' => "Branding:",
-'LAN_THEMEPREF_01' => "Navbar Alignment:",
-'LAN_THEMEPREF_02' => "Display standard menu with card look",
-'LAN_THEMEPREF_03' => "Display Login Page as standard page",
-'LAN_THEMEPREF_04' => "Site Name",
-'LAN_THEMEPREF_05' => "Logo",
-'LAN_THEMEPREF_06' => "Logo &amp; Site Name",
-'LAN_THEMEPREF_07' => "left",
-'LAN_THEMEPREF_08' => "right",
+return [
+    'LAN_THEMEPREF_00' => "Branding:",
+    'LAN_THEMEPREF_01' => "Navbar Alignment:",
+    'LAN_THEMEPREF_02' => "Display standard menu with card look",
+    'LAN_THEMEPREF_03' => "Display Login Page as standard page",
+    'LAN_THEMEPREF_04' => "Site Name",
+    'LAN_THEMEPREF_05' => "Logo",
+    'LAN_THEMEPREF_06' => "Logo &amp; Site Name",
+    'LAN_THEMEPREF_07' => "left",
+    'LAN_THEMEPREF_08' => "right",
 ];
